1f058e396c3c91b31fe10adb7a2210d1c36dd8d7
David Reid <dreid@nowhere.fake>
2002-07-12 09:19:40 +0000

    Small update to bring this up to date.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@114 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/posix/sys/socket.h
+++ headers/posix/sys/socket.h
@@ -1,12 +1,12 @@
 /* sys/socket.h */
 
-#ifndef OBOS_SYS_SOCKET_H
-#define OBOS_SYS_SOCKET_H
 
-#include <kernel/OS.h>
+#ifndef _SYS_SOCKET_H
+#define _SYS_SOCKET_H
 
-/* shoudl bt in types.h */
-typedef uint32 socklen_t;
+#include <OS.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
 
 /* These are the address/protocol families we'll be using... */
 /* NB these should be added to as required... */
@@ -157,17 +157,17 @@ struct sockproto {
         { "iflist", CTLTYPE_STRUCT }, \
 }
 
-				/* Max listen queue for a socket */
+                        /* Max listen queue for a socket */
 #define SOMAXCONN	5	/* defined as 128 in OpenBSD */
 
 struct msghdr {
-	caddr_t	msg_name;	/* address we're using (optional) */
-	uint msg_namelen;	/* length of address */
-	struct iovec *msg_iov;	/* scatter/gather array we'll use */
-	uint msg_iovlen;	/* # elements in msg_iov */
-	caddr_t msg_control;	/* extra data */
-	uint msg_controllen;	/* length of extra data */
-	int msg_flags;		/* flags */
+	char *	msg_name;         /* address we're using (optional) */
+	uint msg_namelen;         /* length of address */
+	struct iovec *msg_iov;    /* scatter/gather array we'll use */
+	uint msg_iovlen;          /* # elements in msg_iov */
+	char * msg_control;       /* extra data */
+	uint msg_controllen;      /* length of extra data */
+	int msg_flags;            /* flags */
 };
 
 /* Defines used in msghdr structure. */
@@ -189,7 +189,44 @@ struct cmsghdr {
 	/* there now follows uchar[] cmsg_data */
 };
 
+#define SIOCSHIWAT      _IOW('s',  0, int)              /* set high watermark */
+#define SIOCGHIWAT      _IOR('s',  1, int)              /* get high watermark */
+#define SIOCSLOWAT      _IOW('s',  2, int)              /* set low watermark */
+#define SIOCGLOWAT      _IOR('s',  3, int)              /* get low watermark */
+#define SIOCATMARK      _IOR('s',  7, int)              /* at oob mark? */
 
+#define SIOCADDRT       _IOW('r', 10, struct ortentry)  /* add route */
+#define SIOCDELRT       _IOW('r', 11, struct ortentry)  /* delete route */
+
+#define SIOCSIFADDR     _IOW('i', 12, struct ifreq)     /* set ifnet address */
+#define OSIOCGIFADDR    _IOWR('i', 13, struct ifreq)    /* get ifnet address */
+#define SIOCGIFADDR     _IOWR('i', 33, struct ifreq)    /* get ifnet address */
+#define SIOCSIFDSTADDR  _IOW('i', 14, struct ifreq)     /* set p-p address */
+#define OSIOCGIFDSTADDR _IOWR('i', 15, struct ifreq)    /* get p-p address */
+#define SIOCGIFDSTADDR  _IOWR('i', 34, struct ifreq)    /* get p-p address */
+#define SIOCSIFFLAGS    _IOW('i', 16, struct ifreq)     /* set ifnet flags */
+#define SIOCGIFFLAGS    _IOWR('i', 17, struct ifreq)    /* get ifnet flags */
+#define OSIOCGIFBRDADDR _IOWR('i', 18, struct ifreq)    /* get broadcast addr */
+#define SIOCGIFBRDADDR  _IOWR('i', 35, struct ifreq)    /* get broadcast addr */
+#define SIOCSIFBRDADDR  _IOW('i', 19, struct ifreq)     /* set broadcast addr */
+#define OSIOCGIFCONF    _IOWR('i', 20, struct ifconf)   /* get ifnet list */
+#define SIOCGIFCONF     _IOWR('i', 36, struct ifconf)   /* get ifnet list */
+#define OSIOCGIFNETMASK _IOWR('i', 21, struct ifreq)    /* get net addr mask */
+#define SIOCGIFNETMASK  _IOWR('i', 37, struct ifreq)    /* get net addr mask */
+#define SIOCSIFNETMASK  _IOW('i', 22, struct ifreq)     /* set net addr mask */
+#define SIOCGIFMETRIC   _IOWR('i', 23, struct ifreq)    /* get IF metric */
+#define SIOCSIFMETRIC   _IOW('i', 24, struct ifreq)     /* set IF metric */
+#define SIOCDIFADDR     _IOW('i', 25, struct ifreq)     /* delete IF addr */
+#define SIOCAIFADDR     _IOW('i', 26, struct ifaliasreq)/* add/chg IF alias */
+#define SIOCGIFDATA     _IOWR('i', 27, struct ifreq)    /* get if_data */
+
+#define SIOCGIFMTU      _IOWR('i', 126, struct ifreq)   /* get ifnet MTU */
+#define SIOCSIFMTU      _IOW('i', 127, struct ifreq)    /* set ifnet MTU */
+
+#define SIOCADDMULTI    _IOW('i', 49, struct ifreq)     /* add m'cast addr */
+#define SIOCDELMULTI    _IOW('i', 50, struct ifreq)     /* del m'cast addr */
+
+#ifndef _KERNEL_MODE
 /* Function declarations */
 int     socket (int, int, int);
 int     bind(int, const struct sockaddr *, int);
@@ -208,9 +245,7 @@ int     setsockopt(int, int, int, const void *, size_t);
 int     getsockopt(int, int, int, void *, size_t *);
 int     getpeername(int, struct sockaddr *, int *);
 int     getsockname(int, struct sockaddr *, int *);
+#endif /* _KERNEL_MODE */
 
-/* is it really part of sockets BSD API?*/
-int     sysctl (int *, uint, void *, size_t *, void *, size_t);
-
-#endif /* OBOS_SYS_SOCKET_H */
+#endif /* _SYS_SOCKET_H */
 
