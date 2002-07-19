ca91310a67a127973acf0b5a6d1fe890349c6de5
David Reid <dreid@nowhere.fake>
2002-07-19 16:00:17 +0000

    Start the change to using beos style interrupt handlers.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@336 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/int.h
+++ headers/private/kernel/int.h
@@ -11,18 +11,33 @@
 int int_init(kernel_args *ka);
 int int_init2(kernel_args *ka);
 int int_io_interrupt_handler(int vector);
-int int_set_io_interrupt_handler(int vector, int (*func)(void*), void* data);
-int int_remove_io_interrupt_handler(int vector, int (*func)(void*), void* data);
 
-#define int_enable_interrupts	arch_int_enable_interrupts
-#define int_disable_interrupts	arch_int_disable_interrupts
-#define int_restore_interrupts	arch_int_restore_interrupts
+#define int_enable_interrupts	  arch_int_enable_interrupts
+#define int_disable_interrupts	  arch_int_disable_interrupts
+#define int_restore_interrupts	  arch_int_restore_interrupts
 #define int_is_interrupts_enabled arch_int_is_interrupts_enabled
 
-enum {
-	INT_NO_RESCHEDULE,
-	INT_RESCHEDULE
-};
+#define B_UNHANDLED_INTERRUPT		0		/* pass to next handler */
+#define B_HANDLED_INTERRUPT			1		/* don't pass on */
+#define B_INVOKE_SCHEDULER			2		/* don't pass on; invoke the scheduler */
 
-#endif
+#define B_NO_ENABLE_COUNTER         1
 
+typedef int32 (*interrupt_handler) (void *data);
+
+/* interrupt handling support for device drivers */
+
+long 	install_io_interrupt_handler (
+	long 				interrupt_number, 
+	interrupt_handler	handler, 
+	void				*data, 
+	ulong 				flags
+);
+
+long 	remove_io_interrupt_handler (
+	long 				interrupt_number,
+	interrupt_handler	handler,
+	void				*data
+);
+
+#endif /* _KERNEL_INT_H */
