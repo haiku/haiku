23ce7baa24ca41dc8a5be4a62dc5588fb5769fa9
Ingo Weinhold <ingo_weinhold@gmx.de>
2002-07-21 22:54:15 +0000

    Added a very basic test to see, if BRoster cooperates smoothly with the registrar to init its messengers. And yes, it does. :-)
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@382 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/tests/servers/registrar/RegistrarTest1.cpp
@@ -0,0 +1,28 @@
+// RegistrarTest1.cpp
+
+#include <stdio.h>
+
+#include <Application.h>
+
+class TestApp : public BApplication {
+public:
+	TestApp(const char *signature)
+		: BApplication(signature)
+	{
+	}
+
+	~TestApp()
+	{
+	}
+};
+
+// main
+int
+main()
+{
+	TestApp *app = new TestApp("application/x-vnd.OBOS-TestApp1");
+	app->Run();
+	delete app;
+	return 0;
+}
+
