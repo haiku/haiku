a3a31e261e742df90a33f67bae8fec02e8f6c334
Ingo Weinhold <ingo_weinhold@gmx.de>
2002-07-23 21:20:39 +0000

    Added library initialization/termination functions.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@398 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/kits/app/InitTerminateLibBe.cpp
@@ -0,0 +1,58 @@
+//------------------------------------------------------------------------------
+//	Copyright (c) 2001-2002, OpenBeOS
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a
+//	copy of this software and associated documentation files (the "Software"),
+//	to deal in the Software without restriction, including without limitation
+//	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//	and/or sell copies of the Software, and to permit persons to whom the
+//	Software is furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//	DEALINGS IN THE SOFTWARE.
+//
+//	File Name:		InitTerminateLibBe.cpp
+//	Author(s):		Ingo Weinhold (bonefish@users.sf.net)
+//	Description:	Global library initialization/termination routines.
+//------------------------------------------------------------------------------
+#include <stdio.h>
+
+#include <Roster.h>
+
+// debugging
+//#define DBG(x) x
+#define DBG(x)
+#define OUT	printf
+
+// initialize_before
+extern "C"
+void
+initialize_before()
+{
+DBG(OUT("initialize_before()\n"));
+
+	_init_roster_();
+
+DBG(OUT("initialize_before() done\n"));
+}
+
+// terminate_after
+extern "C"
+void
+terminate_after()
+{
+DBG(OUT("terminate_after()\n"));
+
+	_delete_roster_();
+
+DBG(OUT("terminate_after() done\n"));
+}
+
