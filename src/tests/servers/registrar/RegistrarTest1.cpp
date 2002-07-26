448187a8c36cfed6504c6c82e176d37c6219a81e
Ingo Weinhold <ingo_weinhold@gmx.de>
2002-07-26 20:06:21 +0000

    Added some now working hooks and some more output.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@467 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/tests/servers/registrar/RegistrarTest1.cpp
+++ src/tests/servers/registrar/RegistrarTest1.cpp
@@ -14,6 +14,30 @@ public:
 	~TestApp()
 	{
 	}
+
+	virtual void ArgvReceived(int32 argc, char **argv)
+	{
+		printf("TestApp::ArgvReceived(%ld)\n", argc);
+		BMessage *message = CurrentMessage();
+		message->PrintToStream();
+		BMessenger returnAddress(message->ReturnAddress());
+		printf("team: %ld\n", returnAddress.Team());
+		for (int32 i = 0; i < argc; i++)
+			printf("arg %ld: `%s'\n", i, argv[i]);
+	}
+
+	virtual void RefsReceived(BMessage *message)
+	{
+		printf("TestApp::RefsReceived()\n");
+		message->PrintToStream();
+	}
+
+	virtual void ReadyToRun()
+	{
+		printf("TestApp::ReadyToRun()\n");
+//		PostMessage(B_QUIT_REQUESTED);
+	}
+
 };
 
 // main
@@ -21,7 +45,7 @@ int
 main()
 {
 	TestApp *app = new TestApp("application/x-vnd.OBOS-TestApp1");
-//	app->Run();
+	app->Run();
 	delete app;
 	return 0;
 }
