09225bd67e366d498e91159ba6fbbb3ae8f4d3be
Matthew Wilber <mwilber@nowhere.fake>
2002-07-27 01:49:19 +0000

    changed text message
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@474 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/translators/bmptranslator/BMPView.cpp
+++ src/add-ons/translators/bmptranslator/BMPView.cpp
@@ -1,6 +1,7 @@
 // BMPView.cpp
 
 #include <stdio.h>
+#include <string.h>
 #include "BMPView.h"
 #include "BMPTranslator.h"
 
@@ -19,7 +20,32 @@ void BMPView::Draw(BRect area)
 	SetFont(be_bold_font);
 	font_height fh;
 	GetFontHeight(&fh);
-	char str[100];
-	sprintf(str, "BMPTranslator %.2f", (float) BMP_TRANSLATOR_VERSION / 100.0);
-	DrawString(str, BPoint(fh.descent + 1, fh.ascent + fh.descent * 2 + fh.leading));
+	float xbold, ybold;
+	xbold = fh.descent + 1;
+	ybold = fh.ascent + fh.descent * 2 + fh.leading;
+	
+	char title[] = "OpenBeOS BMP Image Translator";
+	DrawString(title, BPoint(xbold, ybold));
+	
+	SetFont(be_plain_font);
+	font_height plainh;
+	GetFontHeight(&plainh);
+	float yplain;
+	yplain = plainh.ascent + plainh.descent * 2 + plainh.leading;
+	
+	char detail[100];
+	sprintf(detail, "Version %d.%d.%d %s",
+		BMP_TRANSLATOR_VERSION / 100, (BMP_TRANSLATOR_VERSION / 10) % 10,
+		BMP_TRANSLATOR_VERSION % 10, __DATE__);
+	DrawString(detail, BPoint(xbold, yplain + ybold));
+/*	char copyright[] = "© 2002 OpenBeOS Project";
+	DrawString(copyright, BPoint(xbold, yplain * 2 + ybold));
+	
+	char becopyright[] = "Portions Copyright 1991-1999, Be Incorporated.";
+	DrawString(becopyright, BPoint(xbold, yplain * 4 + ybold));
+	char allrights[] = "All rights reserved.";
+	DrawString(allrights, BPoint(xbold, yplain * 5 + ybold));
+*/	
+	char writtenby[] = "Written by the OBOS Translation Kit Team";
+	DrawString(writtenby, BPoint(xbold, yplain * 7 + ybold));
 }
