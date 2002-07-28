c09fb4b9716af9fe514087b57f5a3a08f9040530
Ingo Weinhold <ingo_weinhold@gmx.de>
2002-07-28 13:37:26 +0000

    Added doxygen comments.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@495 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/servers/registrar/ClipboardHandler.cpp
+++ src/servers/registrar/ClipboardHandler.cpp
@@ -4,18 +4,31 @@
 
 #include "ClipboardHandler.h"
 
+/*!
+	\class ClipboardHandler
+	\brief Handles all clipboard related requests.
+*/
+
 // constructor
+/*!	\brief Creates and initializes a ClipboardHandler.
+*/
 ClipboardHandler::ClipboardHandler()
 				: BHandler()
 {
 }
 
 // destructor
+/*!	\brief Frees all resources associate with this object.
+*/
 ClipboardHandler::~ClipboardHandler()
 {
 }
 
 // MessageReceived
+/*!	\brief Overrides the super class version to handle the clipboard specific
+		   messages.
+	\param message The message to be handled
+*/
 void
 ClipboardHandler::MessageReceived(BMessage *message)
 {
--- src/servers/registrar/MIMEManager.cpp
+++ src/servers/registrar/MIMEManager.cpp
@@ -4,18 +4,33 @@
 
 #include "MIMEManager.h"
 
+/*!
+	\class MIMEManager
+	\brief MIMEManager is the master of the MIME data base.
+
+	All write and non-atomic read accesses are carried out by this class.
+*/
+
 // constructor
+/*!	\brief Creates and initializes a MIMEManager.
+*/
 MIMEManager::MIMEManager()
 		   : BLooper("main_mime")
 {
 }
 
 // destructor
+/*!	\brief Frees all resources associate with this object.
+*/
 MIMEManager::~MIMEManager()
 {
 }
 
 // MessageReceived
+/*!	\brief Overrides the super class version to handle the MIME specific
+		   messages.
+	\param message The message to be handled
+*/
 void
 MIMEManager::MessageReceived(BMessage *message)
 {
