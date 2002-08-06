bf2c044d5699adff54d0ed76535be519856fabbd
Ingo Weinhold <ingo_weinhold@gmx.de>
2002-08-06 22:13:59 +0000

    Added test app for BMessenger tests.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@618 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/tests/kits/app/bmessenger/testapps/BMessengerTestApp1.cpp
@@ -0,0 +1,26 @@
+// BMessengerTestApp1.cpp
+
+#include <stdio.h>
+
+#include <OS.h>
+
+#include "CommonTestApp.h"
+
+int
+main()
+{
+// R5: doesn't set the error variable in case of success
+#ifdef TEST_R5
+	status_t error = B_OK;
+#else
+	status_t error = B_ERROR;
+#endif
+	CommonTestApp app("application/x-vnd.obos-bmessenger-testapp1", &error);
+	init_connection();
+	report("error: %lx\n", error);
+	report("InitCheck(): %lx\n", app.InitCheck());
+	if (error == B_OK)
+		app.Run();
+	return 0;
+}
+
