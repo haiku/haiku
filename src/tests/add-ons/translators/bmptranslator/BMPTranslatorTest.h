c523ad7c3e8c218e9a9e5605e9d6e3ec2cd25f39
Matthew Wilber <mwilber@nowhere.fake>
2002-08-06 20:09:55 +0000

    inital program check in
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@612 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/tests/add-ons/translators/bmptranslator/BMPTranslatorTest.h
@@ -0,0 +1,27 @@
+// BMPTranslatorTest.h
+
+#ifndef BMP_TRANSLATOR_TEST_H
+#define BMP_TRANSLATOR_TEST_H
+
+#include <TestCase.h>
+#include <TestShell.h>
+
+class CppUnit::Test;
+
+class BMPTranslatorTest : public BTestCase {
+public:
+	static CppUnit::Test* Suite();
+	
+	// This function called before *each* test added in Suite()
+	void setUp();
+	
+	// This function called after *each* test added in Suite()
+	void tearDown();
+
+	//------------------------------------------------------------
+	// Test functions
+	//------------------------------------------------------------
+	void DummyTest();
+};
+
+#endif	// BMP_TRANSLATOR_TEST_H
