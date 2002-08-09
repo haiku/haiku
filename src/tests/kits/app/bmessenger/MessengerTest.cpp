e840a8d12275f06d72c1cb284a9ff8126386c8ee
Ingo Weinhold <ingo_weinhold@gmx.de>
2002-08-09 23:30:03 +0000

    Uncommented accidentially commented out tests.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@675 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/tests/kits/app/bmessenger/MessengerTest.cpp
+++ src/tests/kits/app/bmessenger/MessengerTest.cpp
@@ -11,13 +11,13 @@ CppUnit::Test* MessengerTestSuite()
 {
 	CppUnit::TestSuite *testSuite = new CppUnit::TestSuite();
 	
-//	testSuite->addTest(LockTargetTester::Suite());
-//	testSuite->addTest(LockTargetWithTimeoutTester::Suite());
-//	testSuite->addTest(MessengerAssignmentTester::Suite());
-//	testSuite->addTest(MessengerComparissonTester::Suite());
-//	testSuite->addTest(SendMessageTester::Suite());
+	testSuite->addTest(LockTargetTester::Suite());
+	testSuite->addTest(LockTargetWithTimeoutTester::Suite());
+	testSuite->addTest(MessengerAssignmentTester::Suite());
+	testSuite->addTest(MessengerComparissonTester::Suite());
+	testSuite->addTest(SendMessageTester::Suite());
 	testSuite->addTest(TBMessengerTester::Suite());
-//	testSuite->addTest(TargetTester::Suite());
+	testSuite->addTest(TargetTester::Suite());
 
 	return testSuite;
 }
