867a12bf32072826e0163a98b68dec0102ca5091
beveloper <beveloper@nowhere.fake>
2002-08-12 19:38:19 +0000

    removed this file
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@718 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kits/media/DormantNodeManager.h
+++
@@ -1,9 +0,0 @@
-/*
-class DormantNodeManager
-	InstantiateDormantNode(const dormant_node_info & in_info, bool global,
-									 media_node * out_node)
-									 
-									 
-									 
-extern DormantNodeManager *_DormantNodeManager;									 
-*/
