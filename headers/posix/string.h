0c9ee87018a7fd80fb7a972a3cb38b8ee28e7a04
Axel Dörfler <axeld@pinc-software.de>
2002-09-17 14:51:59 +0000

    Added our string.h headers (the one in private/kernel will be removed now)
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@1070 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/posix/string.h
@@ -0,0 +1,82 @@
+#ifndef _STRING_H_
+#define _STRING_H_
+/* 
+** Distributed under the terms of the OpenBeOS License.
+*/
+
+#include <sys/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* memXXX() functions */
+extern void		*memchr(const void *source, int value, size_t length);
+extern int		memcmp(const void *buffer1, const void *buffer2, size_t length);
+extern void		*memcpy(void *dest, const void *source, size_t length);
+extern void		*memmove(void *dest, const void *source, size_t length);
+extern void		*memset(void *dest, int value, size_t length);
+
+/* string functions */
+extern char		*strcpy(char *dest, const char *source);
+extern char		*strncpy(char *dest, const char *source, size_t length);
+extern char		*strcat(char *dest, const char *source);
+extern char		*strncat(char *dest, const char *source, size_t length);
+
+extern size_t	strlen(const char *string);
+extern int		strcmp(const char *string1, const char *string2);
+extern int		strncmp(const char *string1, const char *string2, size_t length);
+
+extern char		*strchr(const char *string, int character);
+extern char		*strrchr(const char *string, int character);
+extern char		*strstr(const char *string, const char *searchString);
+
+extern char		*strpbrk(const char *string, const char *set);
+extern char		*strtok(char *string, const char *set);
+extern char		*strtok_r(char *string, const char *set, char **savePointer);
+extern size_t	strspn(const char *string, const char *set);
+extern size_t	strcspn(const char *string, const char *set);
+
+extern int		strcoll(const char *string1, const char *string2);
+extern size_t	strxfrm(char *string1, const char *string2, size_t length);
+
+extern const char *strerror(int error);
+
+// ToDo: remove bzero(), bcopy() from here - we don't need those
+//	implementations anyway, since BeOS defines them as macros
+//	Of course, we could also just keep them (and bcmp() is missing)
+extern void  *bcopy(void const *source, void *dest, size_t length);
+extern void   bzero(void *buffer, size_t length);
+
+/* non-standard string functions */
+extern int		strcasecmp(const char *string1, const char *string2);
+extern int		strncasecmp(const char *string1, const char *string2, size_t length);
+#define	stricmp(a, b)	strcasecmp(a, b)
+//#define strnicmp(a, b, length) strncasecmp(a, b, length)
+// ToDo: remove the function, and only have the macro
+extern int		strnicmp(const char *string1, const char *string2, size_t length);
+
+extern char		*strcasestr(const char *string, const char *searchString);
+
+extern char		*strdup(const char *string);
+extern char		*stpcpy(char *dest, const char *source);
+extern const char *strtcopy(char *dest, const char *source);
+
+extern size_t	strlcat(char *dest, const char *source, size_t length);
+extern size_t	strlcpy(char *dest, const char *source, size_t length);
+
+extern size_t	strnlen(const char *string, size_t count);
+
+//extern char		*strlwr(char *string);
+//extern char		*strupr(char *string);
+
+//extern char		*strsep(char **stringPointer, const char *delimiter);
+
+//extern const char	*strsignal(int signal);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* _STRING_H_ */
---
+++ headers/posix/strings.h
@@ -0,0 +1,6 @@
+#ifndef _STRINGS_H_
+#define _STRINGS_H_
+
+#include <string.h>
+
+#endif	/* _STRINGS_H_ */
