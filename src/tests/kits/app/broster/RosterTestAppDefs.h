4e84d53de8b078bdea63e9e8a882222227282462
Ingo Weinhold <ingo_weinhold@gmx.de>
2002-09-28 21:01:11 +0000

    First and probably only (as it is quite generic) test app for BRoster::Launch() tests.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@1248 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/tests/kits/app/broster/RosterTestAppDefs.h
@@ -0,0 +1,17 @@
+// RosterTestAppDefs.h
+#ifndef ROSTER_TEST_APP_DEF_H
+#define ROSTER_TEST_APP_DEF_H
+
+// message codes
+enum {
+	MSG_STARTED				= 'strt',	// "path": B_STRING_TYPE
+	MSG_TERMINATED			= 'term',	//
+	MSG_ARGV_RECEIVED		= 'argv',	// "argc": B_INT32_TYPE,
+										// "argv": B_STRING_TYPE[]
+	MSG_REFS_RECEIVED		= 'refs',	// "refs": B_REF_TYPE[]
+	MSG_MESSAGE_RECEIVED	= 'mesg',	// "message": B_MESSAGE_TYPE
+	MSG_QUIT_REQUESTED		= 'quit',	//
+	MSG_READY_TO_RUN		= 'redy',	//
+};
+
+#endif	// ROSTER_TEST_APP_DEF_H
