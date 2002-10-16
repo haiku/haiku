b67f11173f803f79ad22964641803b68512ee28e
Axel Dörfler <axeld@pinc-software.de>
2002-10-16 16:17:28 +0000

    Added syscalls for the attribute VFS calls. Made some subtle changes to fs_attr.h.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@1546 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/kernel/fs_attr.h
+++ headers/os/kernel/fs_attr.h
@@ -24,19 +24,19 @@ extern "C" {
 extern ssize_t	fs_read_attr(int fd, const char *attribute, uint32 type, off_t pos, void *buffer, size_t readBytes);
 extern ssize_t	fs_write_attr(int fd, const char *attribute, uint32 type, off_t pos, const void *buffer, size_t readBytes);
 extern int		fs_remove_attr(int fd, const char *attribute);
-extern int		fs_stat_attr(int fd, const char *name, struct attr_info *attrInfo);
+extern int		fs_stat_attr(int fd, const char *attribute, struct attr_info *attrInfo);
 
 // ToDo: the following three functions are not part of the R5 API, and
 // are only preliminary - they may change or be removed at any point
-extern int		fs_open_attr(const char *path, const char *attribute);
-extern int		fs_fopen_attr(int fd, const char *attribute);
+//extern int	fs_open_attr(const char *path, const char *attribute, uint32 type, int openMode);
+extern int		fs_open_attr(int fd, const char *attribute, uint32 type, int openMode);
 extern int		fs_close_attr(int fd);
 
 extern DIR		*fs_open_attr_dir(const char *path);
 extern DIR		*fs_fopen_attr_dir(int fd);
-extern int		fs_close_attr_dir(DIR *dirp);
-extern struct dirent *fs_read_attr_dir(DIR *dirp);
-extern void		fs_rewind_attr_dir(DIR *dirp);
+extern int		fs_close_attr_dir(DIR *dir);
+extern struct dirent *fs_read_attr_dir(DIR *dir);
+extern void		fs_rewind_attr_dir(DIR *dir);
 
 #ifdef  __cplusplus
 }
