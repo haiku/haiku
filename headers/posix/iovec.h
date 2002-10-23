55f14ba4d9a968cbf85e2af91a80860de76550eb
beveloper <beveloper@nowhere.fake>
2002-10-23 19:35:15 +0000

    another small file for compatibility reasons
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@1628 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/posix/iovec.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2002 Marcus Overhagen
+ * All rights reserved.
+ *
+ * Distributed under the terms of the MIT license
+ */
+#ifndef _IOVEC_H
+#define _IOVEC_H
+
+#include <sys/uio.h>
+
+#endif
