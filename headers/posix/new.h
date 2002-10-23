49a11cb44b0cd800ddac9c6eb72663e930e979f0
beveloper <beveloper@nowhere.fake>
2002-10-23 20:44:36 +0000

    Temporary files to allow compiling. Some should be replaced, some removed.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@1632 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/posix/new.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2002 Marcus Overhagen
+ * All rights reserved.
+ *
+ * Distributed under the terms of the MIT license
+ */
+#ifndef _NEW_H
+#define _NEW_H
+
+#include <new>
+
+using namespace std;
+
+#endif
