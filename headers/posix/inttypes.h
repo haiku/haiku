0f1aeae76c7229c41b6bd072eaa99193f8f46fe2
Axel Dörfler <axeld@pinc-software.de>
2002-10-26 00:35:08 +0000

    Added inttypes.h (includes stuff like int8_t typedef), but it's not complete; the strtoimax() functions and friends are missing.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@1669 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/posix/inttypes.h
@@ -0,0 +1,19 @@
+#ifndef _INTTYPES_H_
+#define _INTTYPES_H_
+/* 
+** Distributed under the terms of the OpenBeOS License.
+*/
+
+
+typedef signed char 		int8_t;
+typedef signed short int 	int16_t;
+typedef signed int 			int32_t;
+typedef signed long long 	int64_t;
+
+typedef	unsigned char 		u_int8_t;
+typedef	unsigned short int 	u_int16_t;
+typedef	unsigned int 		u_int32_t;
+typedef unsigned long long  u_int64_t;
+
+
+#endif	/* _INTTYPES_H_ */
