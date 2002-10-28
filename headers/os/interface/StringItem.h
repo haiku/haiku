a691828008766e8a8054a8407aaf085156f6e8ec
Axel Dörfler <axeld@pinc-software.de>
2002-10-28 01:38:57 +0000

    A slight collision of StringItem.h with our coding style guidelines... (and fixed newline at the end of the file).
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@1726 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/interface/StringItem.h
+++ headers/os/interface/StringItem.h
@@ -25,32 +25,31 @@ namespace OpenBeOS
 class BStringItem : public BListItem
 {
 public:
-  BStringItem(const char* text, uint32 outlineLevel = 0, bool expanded = true) ;
-  BStringItem(BMessage* data) ;
-virtual ~BStringItem() ;
+	BStringItem(const char *text, uint32 outlineLevel = 0, bool expanded = true);
+	BStringItem(BMessage *data);
+	virtual ~BStringItem();
 
-static  BArchivable	*Instantiate(BMessage* data);
-virtual status_t    Archive(BMessage* data, bool deep = true) const ;
+	static BArchivable	*Instantiate(BMessage *data);
+	virtual status_t	Archive(BMessage *data, bool deep = true) const;
 
-virtual	void        DrawItem(BView* owner, BRect frame, bool complete = false) ;
-virtual	void        SetText(const char* text) ;
-		const char* Text() const ;
-virtual	void        Update(BView* owner, const BFont* font) ;
+	virtual	void		DrawItem(BView *owner, BRect frame, bool complete = false);
+	virtual	void		SetText(const char *text);
+	const char			*Text() const;
 
-virtual status_t    Perform(perform_code d, void* arg);
+	virtual	void		Update(BView *owner, const BFont *font);
+
+	virtual status_t	Perform(perform_code code, void *arg);
 
 private:
+	BStringItem(const BStringItem &);
+	BStringItem& operator=(const BStringItem &);
 
-  BStringItem(const BStringItem &) ;
-  BStringItem& operator=(const BStringItem &) ;
-
-  char*   fText;
-  float   fBaselineOffset ;
-} ;
+	char	*fText;
+	float	fBaselineOffset;
+};
 
 #ifdef USE_OPENBEOS_NAMESPACE
 }
 #endif
 
 #endif /* _STRING_ITEM_H */
-	
