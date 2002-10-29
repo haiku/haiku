b026d723ba379fe14896897055b5aedc6d5296bf
Axel Dörfler <axeld@pinc-software.de>
2002-10-29 03:36:14 +0000

    Made the POLLxxx constants compatible with the ones used internally in the VFS - BeOS don't have poll() at all, so we can freely choose them.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@1743 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/posix/poll.h
+++ headers/posix/poll.h
@@ -12,19 +12,19 @@ struct pollfd {
 	int revents;	/* events that occured */
 };
 
-/* events & revents */
+/* events & revents - compatible with the B_SELECT_xxx definitions in Drivers.h */
 #define	POLLIN		0x0001		/* any readable data available */
-#define	POLLPRI		0x0002		/* high priority readable data */
-#define	POLLOUT		0x0004		/* file descriptor is writeable */
-#define	POLLRDNORM	0x0040		/* normal data available */
-#define	POLLRDBAND	0x0080		/* priority readable data */
+#define	POLLOUT		0x0002		/* file descriptor is writeable */
+#define	POLLRDNORM	POLLIN
 #define	POLLWRNORM	POLLOUT
-#define	POLLWRBAND	0x0100		/* priority data can be written */
+#define	POLLRDBAND	0x0008		/* priority readable data */
+#define	POLLWRBAND	0x0010		/* priority data can be written */
+#define	POLLPRI		0x0020		/* high priority readable data */
 
 /* revents only */
-#define	POLLERR		0x0008		/* errors pending */
-#define	POLLHUP		0x0010		/* disconnected */
-#define	POLLNVAL	0x0020		/* invalid file descriptor */
+#define	POLLERR		0x0004		/* errors pending */
+#define	POLLHUP		0x0080		/* disconnected */
+#define	POLLNVAL	0x1000		/* invalid file descriptor */
 
 
 extern
