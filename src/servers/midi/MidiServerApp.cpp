37192de9c05b844d4b44eed00739ba8b8ddf16b6
mahlzeit <mahlzeit@nowhere.fake>
2002-10-30 15:37:29 +0000

    Added the beginnings of the midi_server
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@1790 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/servers/midi/MidiServerApp.cpp
@@ -0,0 +1,34 @@
+
+#include <Alert.h>
+
+#include "MidiServerApp.h"
+
+//------------------------------------------------------------------------------
+
+MidiServerApp::MidiServerApp()
+	: BApplication("application/x-vnd.be-midi-roster")
+{
+	// Do nothing.
+}
+
+//------------------------------------------------------------------------------
+
+void MidiServerApp::AboutRequested()
+{
+	(new BAlert(0,
+		"-write something funny here-",
+		"Okay", 0, 0, B_WIDTH_AS_USUAL, 
+		B_INFO_ALERT))->Go();
+}
+
+//------------------------------------------------------------------------------
+
+int main()
+{
+	MidiServerApp app;
+	app.Run();
+	return 0;
+}
+
+//------------------------------------------------------------------------------
+
---
+++ src/servers/midi/MidiServerApp.h
@@ -0,0 +1,18 @@
+
+#ifndef MIDI_SERVER_APP_H
+#define MIDI_SERVER_APP_H
+
+#include <Application.h>
+
+/**
+ * The BApplication that drives the midi_server.
+ */
+class MidiServerApp : public BApplication
+{
+public:
+	MidiServerApp();
+	virtual void AboutRequested();
+};
+
+#endif // MIDI_SERVER_APP_H
+
