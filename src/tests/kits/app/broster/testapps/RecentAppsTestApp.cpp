c74a0367943fcd8b4786c0d4bc1ebc7e80893cf0
Tyler Dauwalder <tylerdauwalder@nowhere.fake>
2002-10-31 07:13:55 +0000

    Moved R5 suffixes into kRecentAppsTestAppSigs array instead of adding them on elsewhere.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@1800 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/tests/kits/app/broster/testapps/RecentAppsTestApp.cpp
+++ src/tests/kits/app/broster/testapps/RecentAppsTestApp.cpp
@@ -1,11 +1,18 @@
 #include "RecentAppsTestApp.h"
 
+#ifdef TEST_R5
+#define APP_SIG_SUFFIX "-r5"
+#else
+#define APP_SIG_SUFFIX 
+#endif
+
+
 // Test app signatures
 const char *kRecentAppsTestAppSigs[] = {
-	"application/x-vnd.obos-recent-apps-test-qualifying",
-	"application/x-vnd.obos-recent-apps-test-non-qualifying",
-	"application/x-vnd.obos-recent-apps-test-empty",
-	"application/x-vnd.obos-recent-apps-test-control",
+	"application/x-vnd.obos-recent-apps-test-qualifying" APP_SIG_SUFFIX,
+	"application/x-vnd.obos-recent-apps-test-non-qualifying" APP_SIG_SUFFIX,
+	"application/x-vnd.obos-recent-apps-test-empty" APP_SIG_SUFFIX,
+	"application/x-vnd.obos-recent-apps-test-control" APP_SIG_SUFFIX,
 };
 
 const char *kRecentAppsTestAppFilenames[] = {
