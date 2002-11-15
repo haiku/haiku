2935bff6563def3584677544a4fed09dc5bebc8a
Axel Dörfler <axeld@pinc-software.de>
2002-11-15 22:34:27 +0000

    Added utime.h.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@1951 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/posix/utime.h
@@ -0,0 +1,23 @@
+#ifndef _UTIME_H_
+#define _UTIME_H_
+/* 
+** Distributed under the terms of the OpenBeOS License.
+*/
+
+#include <sys/types.h>
+
+
+struct utimbuf
+{
+	time_t actime;		/* access time */
+	time_t modtime;		/* modification time */
+};
+
+
+extern
+#ifdef __cplusplus
+"C"
+#endif
+int utime(const char *path, const struct utimbuf *buffer);
+
+#endif	/* _UTIME_H_ */
