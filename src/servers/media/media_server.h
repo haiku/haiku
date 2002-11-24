08d612b66ba6f06f1f4f4a7cf2136c1f29fa6024
beveloper <beveloper@nowhere.fake>
2002-11-24 00:50:15 +0000

    media_server.h was missing.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@2073 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/servers/media/media_server.h
@@ -0,0 +1,10 @@
+
+class AppManager;
+class NodeManager;
+class BufferManager;
+class NotificationManager;
+
+extern AppManager *				gAppManager;
+extern NodeManager *			gNodeManager;
+extern BufferManager *			gBufferManager;
+extern NotificationManager *	gNotificationManager;
