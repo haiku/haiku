9c2992c73b44eecd0e142d0afc6fa9b3c8b1c79d
beveloper <beveloper@nowhere.fake>
2002-11-28 14:41:06 +0000

    fixed debug function
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@2105 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kernel/core/fd.c
+++ src/kernel/core/fd.c
@@ -39,9 +39,9 @@ void dump_fd(int fd, struct file_descriptor *descriptor);
 void
 dump_fd(int fd,struct file_descriptor *descriptor)
 {
-	dprintf("fd[%d] = %p: type = %d, ref_count = %d, ops = %p, vnode = %p, cookie = %p, dummy = %x\n",
+	dprintf("fd[%d] = %p: type = %ld, ref_count = %ld, ops = %p, u.vnode = %p, u.mount = %p, cookie = %p, open_mode = %lx, pos = %Ld\n",
 		fd, descriptor, descriptor->type, descriptor->ref_count, descriptor->ops,
-		descriptor->vnode, descriptor->cookie, descriptor->dummy);
+		descriptor->u.vnode, descriptor->u.mount, descriptor->cookie, descriptor->open_mode, descriptor->pos);
 }
 #endif
 
