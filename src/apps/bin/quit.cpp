30dbdd96f94ba5b1d2e86363e0db3c376694d153
François Revol <revol@free.fr>
2002-12-03 19:33:52 +0000

    Added quit, code by Carlos Hasan.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@2155 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/apps/bin/quit.cpp
@@ -0,0 +1,33 @@
+/*
+ * quit.cpp
+ * (c) 2002, Carlos Hasan, for OpenBeOS.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <app/Messenger.h>
+
+int main(int argc, char *argv[])
+{
+	status_t status;
+
+	if (argc != 2) {
+		printf("use: %s mime_sig\n", argv[0]);
+		return 1;
+	}
+
+	BMessenger messenger(argv[1]);
+
+	if (!messenger.IsValid()) {
+		printf("could not find running app with sig %s\n", argv[1]);
+		return 1;
+	}
+
+	if ((status = messenger.SendMessage(B_QUIT_REQUESTED)) != B_OK) {
+		printf("could not send message, %s\n", strerror(status));
+		return 1;
+	}
+
+	return 0;
+}
+
