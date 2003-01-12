eadb1c453af7fede743e15cd8cdd1b557a68689b
Axel Dörfler <axeld@pinc-software.de>
2003-01-12 17:13:32 +0000

    Now calls the __init__image() initializer function. INIT_BEFORE_CTORS() is now called initialize_before() to be compatible with BeOS start_dyn.o.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@2438 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kernel/libroot/libroot_init.c
+++ src/kernel/libroot/libroot_init.c
@@ -6,12 +6,16 @@
 #include <user_runtime.h>
 
 
-extern void __init__dlfcn(struct uspace_prog_args_t const *uspa);
+extern void __init__dlfcn(struct uspace_program_args const *args);
+extern void __init__image(struct uspace_program_args const *args);
 
+
+void initialize_before(image_id imageID, struct uspace_program_args const *args);
 
 void
-INIT_BEFORE_CTORS(unsigned imid, struct uspace_prog_args_t const *uspa)
+initialize_before(image_id imageID, struct uspace_program_args const *args)
 {
-	__init__dlfcn(uspa);
+	__init__dlfcn(args);
+	__init__image(args);
 }
 
