9f956562e531023a72dcaae90a8525c02aa26f7b
Axel Dörfler <axeld@pinc-software.de>
2003-01-13 16:36:25 +0000

    Missed that one, obviously.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@2451 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/elf.h
+++ headers/private/kernel/elf.h
@@ -6,10 +6,11 @@
 #define _KERNEL_ELF_H
 
 #include <thread.h>
+#include <image.h>
 
 int elf_load_uspace(const char *path, struct team *t, int flags, addr *entry);
 image_id elf_load_kspace(const char *path, const char *sym_prepend);
-int elf_unload_kspace( const char *path);
+int elf_unload_kspace(const char *path);
 addr elf_lookup_symbol(image_id id, const char *symbol);
 int elf_lookup_symbol_address(addr address, addr *baseAddress, char *text, size_t length);
 int elf_init(kernel_args *ka);
