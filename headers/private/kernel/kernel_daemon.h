8a5543b18c6fa25cad25e6de6c29c19f0ecf332e
Axel Dörfler <axeld@pinc-software.de>
2003-01-26 17:27:42 +0000

    Added the kernel_daemon.h header which defines the init function for the kernel daemon service.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@2560 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/kernel/kernel_daemon.h
@@ -0,0 +1,12 @@
+#ifndef _KERNEL_DAEMON_H
+#define _KERNEL_DAEMON_H
+/*
+** Copyright 2003, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+** Distributed under the terms of the OpenBeOS License.
+*/
+
+#include <OS.h>
+
+extern status_t kernel_daemon_init(void);
+
+#endif	/* _KRENEL_DAEMON_H */
