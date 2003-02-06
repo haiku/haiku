9f7559815d94c5eec6fec66150893f821ca586c8
Ingo Weinhold <ingo_weinhold@gmx.de>
2003-02-06 23:30:14 +0000

    An interface for classes being able to handle messages. More lightweight than BHandler.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@2641 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/servers/registrar/MessageHandler.cpp
@@ -0,0 +1,23 @@
+//----------------------------------------------------------------------
+//  This software is part of the OpenBeOS distribution and is covered 
+//  by the OpenBeOS license.
+//---------------------------------------------------------------------
+
+#include "MessageHandler.h"
+
+// constructor
+MessageHandler::MessageHandler()
+{
+}
+
+// destructor
+MessageHandler::~MessageHandler()
+{
+}
+
+// HandleMessage
+void
+MessageHandler::HandleMessage(BMessage *message)
+{
+}
+
---
+++ src/servers/registrar/MessageHandler.h
@@ -0,0 +1,19 @@
+//----------------------------------------------------------------------
+//  This software is part of the OpenBeOS distribution and is covered 
+//  by the OpenBeOS license.
+//---------------------------------------------------------------------
+
+#ifndef MESSAGE_HANDLER_H
+#define MESSAGE_HANDLER_H
+
+class BMessage;
+
+class MessageHandler {
+public:
+	MessageHandler();
+	virtual ~MessageHandler();
+
+	virtual void HandleMessage(BMessage *message);
+};
+
+#endif	// MESSAGE_HANDLER_H
