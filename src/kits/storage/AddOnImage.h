32de23c976c632eab3b9cbcbe3ddf1107f9cec6a
Ingo Weinhold <ingo_weinhold@gmx.de>
2003-02-22 23:46:59 +0000

    Helper class: Represents a loaded add-on image.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@2797 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/kits/storage/AddOnImage.h
@@ -0,0 +1,32 @@
+//----------------------------------------------------------------------
+//  This software is part of the OpenBeOS distribution and is covered 
+//  by the OpenBeOS license.
+//---------------------------------------------------------------------
+
+#ifndef _ADD_ON_IMAGE_H
+#define _ADD_ON_IMAGE_H
+
+#include <image.h>
+
+namespace BPrivate {
+
+class AddOnImage {
+public:
+	AddOnImage();
+	~AddOnImage();
+
+	status_t Load(const char *path);
+	void Unload();
+
+	void SetID(image_id id);
+	image_id ID() const	{ return fID; }
+
+private:
+	image_id	fID;
+};
+
+}	// namespace BPrivate
+
+using BPrivate::AddOnImage;
+
+#endif	// _ADD_ON_IMAGE_H
