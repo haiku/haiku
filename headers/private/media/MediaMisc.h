906f0b116e22c42e2bfc49312108f9b239790d87
beveloper <beveloper@nowhere.fake>
2003-03-12 02:19:34 +0000

    misc stuff
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@2893 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/media/MediaMisc.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2003 Marcus Overhagen.
+ * All Rights Reserved.
+ *
+ * This file may be used under the terms of the MIT License.
+ */
+
+#ifndef _MEDIA_MISC_H_
+#define _MEDIA_MISC_H_
+
+#define IS_INVALID_NODE(_node) 			((_node).node <= 0 || (_node).port <= 0)
+#define IS_INVALID_NODEID(_id) 			((_id) <= 0)
+#define IS_INVALID_SOURCE(_src)			((_src).port <= 0)
+#define IS_INVALID_DESTINATION(_dest) 	((_dest).port <= 0)
+
+#endif
