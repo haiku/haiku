129a87e630992a9a9b8abd6959686183336563f6
Matthew Wilber <mwilber@nowhere.fake>
2003-03-12 00:54:11 +0000

    initial check-in for TIFFTranslator (doesn't do anything yet)
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@2887 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/add-ons/translators/tifftranslator/TIFFTranslator.cpp
@@ -0,0 +1,393 @@
+/*****************************************************************************/
+// TIFFTranslator
+// Written by Michael Wilber, OBOS Translation Kit Team
+//
+// TIFFTranslator.cpp
+//
+// This BTranslator based object is for opening and writing 
+// TIFF images.
+//
+//
+// Copyright (c) 2003 OpenBeOS Project
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the 
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included 
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+/*****************************************************************************/
+
+#include <string.h>
+#include <stdio.h>
+#include "TIFFTranslator.h"
+#include "TIFFView.h"
+
+// The input formats that this translator supports.
+translation_format gInputFormats[] = {
+	{
+		B_TRANSLATOR_BITMAP,
+		B_TRANSLATOR_BITMAP,
+		BBT_IN_QUALITY,
+		BBT_IN_CAPABILITY,
+		"image/x-be-bitmap",
+		"Be Bitmap Format (TIFFTranslator)"
+	},
+	{
+		B_TIFF_FORMAT,
+		B_TRANSLATOR_BITMAP,
+		TIFF_IN_QUALITY,
+		TIFF_IN_CAPABILITY,
+		"image/tiff",
+		"TIFF Image"
+	}
+};
+
+// The output formats that this translator supports.
+translation_format gOutputFormats[] = {
+	{
+		B_TRANSLATOR_BITMAP,
+		B_TRANSLATOR_BITMAP,
+		BBT_OUT_QUALITY,
+		BBT_OUT_CAPABILITY,
+		"image/x-be-bitmap",
+		"Be Bitmap Format (TIFFTranslator)"
+	},
+	{
+		B_TIFF_FORMAT,
+		B_TRANSLATOR_BITMAP,
+		TIFF_OUT_QUALITY,
+		TIFF_OUT_CAPABILITY,
+		"image/tiff",
+		"TIFF Image"
+	}
+};
+
+// ---------------------------------------------------------------
+// make_nth_translator
+//
+// Creates a TIFFTranslator object to be used by BTranslatorRoster
+//
+// Preconditions:
+//
+// Parameters: n,		The translator to return. Since
+//						TIFFTranslator only publishes one
+//						translator, it only returns a
+//						TIFFTranslator if n == 0
+//
+//             you, 	The image_id of the add-on that
+//						contains code (not used).
+//
+//             flags,	Has no meaning yet, should be 0.
+//
+// Postconditions:
+//
+// Returns: NULL if n is not zero,
+//          a new TIFFTranslator if n is zero
+// ---------------------------------------------------------------
+BTranslator *
+make_nth_translator(int32 n, image_id you, uint32 flags, ...)
+{
+	if (!n)
+		return new TIFFTranslator();
+	else
+		return NULL;
+}
+
+// ---------------------------------------------------------------
+// Constructor
+//
+// Sets up the version info and the name of the translator so that
+// these values can be returned when they are requested.
+//
+// Preconditions:
+//
+// Parameters:
+//
+// Postconditions:
+//
+// Returns:
+// ---------------------------------------------------------------
+TIFFTranslator::TIFFTranslator()
+	:	BTranslator()
+{
+	strcpy(fName, "TIFF Images");
+	sprintf(fInfo, "TIFF image translator v%d.%d.%d %s",
+		TIFF_TRANSLATOR_VERSION / 100, (TIFF_TRANSLATOR_VERSION / 10) % 10,
+		TIFF_TRANSLATOR_VERSION % 10, __DATE__);
+}
+
+// ---------------------------------------------------------------
+// Destructor
+//
+// Does nothing
+//
+// Preconditions:
+//
+// Parameters:
+//
+// Postconditions:
+//
+// Returns:
+// ---------------------------------------------------------------
+TIFFTranslator::~TIFFTranslator()
+{
+}
+
+// ---------------------------------------------------------------
+// TranslatorName
+//
+// Returns the short name of the translator.
+//
+// Preconditions:
+//
+// Parameters:
+//
+// Postconditions:
+//
+// Returns: a const char * to the short name of the translator
+// ---------------------------------------------------------------	
+const char *
+TIFFTranslator::TranslatorName() const
+{
+	return fName;
+}
+
+// ---------------------------------------------------------------
+// TranslatorInfo
+//
+// Returns a more verbose name for the translator than the one
+// TranslatorName() returns. This usually includes version info.
+//
+// Preconditions:
+//
+// Parameters:
+//
+// Postconditions:
+//
+// Returns: a const char * to the verbose name of the translator
+// ---------------------------------------------------------------
+const char *
+TIFFTranslator::TranslatorInfo() const
+{
+	return fInfo;
+}
+
+// ---------------------------------------------------------------
+// TranslatorVersion
+//
+// Returns the integer representation of the current version of
+// this translator.
+//
+// Preconditions:
+//
+// Parameters:
+//
+// Postconditions:
+//
+// Returns:
+// ---------------------------------------------------------------
+int32 
+TIFFTranslator::TranslatorVersion() const
+{
+	return TIFF_TRANSLATOR_VERSION;
+}
+
+// ---------------------------------------------------------------
+// InputFormats
+//
+// Returns a list of input formats supported by this translator.
+//
+// Preconditions:
+//
+// Parameters:	out_count,	The number of input formats
+//							support is returned here.
+//
+// Postconditions:
+//
+// Returns: the list of input formats and the number of input
+// formats through the out_count parameter, if out_count is NULL,
+// NULL is returned
+// ---------------------------------------------------------------
+const translation_format *
+TIFFTranslator::InputFormats(int32 *out_count) const
+{
+	if (out_count) {
+		*out_count = sizeof(gInputFormats) /
+			sizeof(translation_format);
+		return gInputFormats;
+	} else
+		return NULL;
+}
+
+// ---------------------------------------------------------------
+// OutputFormats
+//
+// Returns a list of output formats supported by this translator.
+//
+// Preconditions:
+//
+// Parameters:	out_count,	The number of output formats
+//							support is returned here.
+//
+// Postconditions:
+//
+// Returns: the list of output formats and the number of output
+// formats through the out_count parameter, if out_count is NULL,
+// NULL is returned
+// ---------------------------------------------------------------	
+const translation_format *
+TIFFTranslator::OutputFormats(int32 *out_count) const
+{
+	if (out_count) {
+		*out_count = sizeof(gOutputFormats) /
+			sizeof(translation_format);
+		return gOutputFormats;
+	} else
+		return NULL;
+}
+
+// ---------------------------------------------------------------
+// Identify
+//
+// Examines the data from inSource and determines if it is in a
+// format that this translator knows how to work with.
+//
+// Preconditions:
+//
+// Parameters:	inSource,	where the data to examine is
+//
+//				inFormat,	a hint about the data in inSource,
+//							it is ignored since it is only a hint
+//
+//				ioExtension,	configuration settings for the
+//								translator (not used)
+//
+//				outInfo,	information about what data is in
+//							inSource and how well this translator
+//							can handle that data is stored here
+//
+//				outType,	The format that the user wants
+//							the data in inSource to be
+//							converted to
+//
+// Postconditions:
+//
+// Returns: B_NO_TRANSLATOR,	if this translator can't handle
+//								the data in inSource
+//
+// B_ERROR,	if there was an error converting the data to the host
+//			format
+//
+// B_BAD_VALUE, if the settings in ioExtension are bad
+//
+// B_OK,	if this translator understood the data and there were
+//			no errors found
+//
+// Other errors if BPositionIO::Read() returned an error value
+// ---------------------------------------------------------------
+status_t
+TIFFTranslator::Identify(BPositionIO *inSource,
+	const translation_format *inFormat, BMessage *ioExtension,
+	translator_info *outInfo, uint32 outType)
+{
+	if (!outType)
+		outType = B_TRANSLATOR_BITMAP;
+	if (outType != B_TRANSLATOR_BITMAP && outType != B_TIFF_FORMAT)
+		return B_NO_TRANSLATOR;
+
+	return B_NO_TRANSLATOR;
+		// translator isn't implemented yet
+}
+
+// ---------------------------------------------------------------
+// Translate
+//
+// Translates the data in inSource to the type outType and stores
+// the translated data in outDestination.
+//
+// Preconditions:
+//
+// Parameters:	inSource,	the data to be translated
+// 
+//				inInfo,	hint about the data in inSource (not used)
+//
+//				ioExtension,	configuration options for the
+//								translator
+//
+//				outType,	the type to convert inSource to
+//
+//				outDestination,	where the translated data is
+//								put
+//
+// Postconditions:
+//
+// Returns: B_BAD_VALUE, if the options in ioExtension are bad
+//
+// B_NO_TRANSLATOR, if this translator doesn't understand the data
+//
+// B_ERROR, if there was an error allocating memory or converting
+//          data
+//
+// B_OK, if all went well
+// ---------------------------------------------------------------
+status_t
+TIFFTranslator::Translate(BPositionIO *inSource,
+		const translator_info *inInfo, BMessage *ioExtension,
+		uint32 outType, BPositionIO *outDestination)
+{
+	if (!outType)
+		outType = B_TRANSLATOR_BITMAP;
+	if (outType != B_TRANSLATOR_BITMAP && outType != B_TIFF_FORMAT)
+		return B_NO_TRANSLATOR;
+		
+	return B_NO_TRANSLATOR;
+		// translator isn't implemented yet
+}
+
+// ---------------------------------------------------------------
+// MakeConfigurationView
+//
+// Makes a BView object for configuring / displaying info about
+// this translator. 
+//
+// Preconditions:
+//
+// Parameters:	ioExtension,	configuration options for the
+//								translator
+//
+//				outView,		the view to configure the
+//								translator is stored here
+//
+//				outExtent,		the bounds of the view are
+//								stored here
+//
+// Postconditions:
+//
+// Returns:
+// ---------------------------------------------------------------
+status_t
+TIFFTranslator::MakeConfigurationView(BMessage *ioExtension, BView **outView,
+	BRect *outExtent)
+{
+	if (!outView || !outExtent)
+		return B_BAD_VALUE;
+
+	TIFFView *view = new TIFFView(BRect(0, 0, 225, 175),
+		"TIFFTranslator Settings", B_FOLLOW_ALL, B_WILL_DRAW);
+	*outView = view;
+	*outExtent = view->Bounds();
+
+	return B_OK;
+}
