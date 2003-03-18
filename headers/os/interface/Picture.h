2465c7694798999f5936e320b4a1a22c57b74317
DarkWyrm <darkwyrm@gmail.com>
2003-03-18 22:04:04 +0000

    Imported Marc Flerackers' header for BPicture
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@2944 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/interface/Picture.h
+++ headers/os/interface/Picture.h
@@ -1,28 +1,53 @@
-/*******************************************************************************
-/
-/	File:			Picture.h
-/
-/   Description:    BPicture records a series of drawing instructions that can
-/                   be "replayed" later.
-/
-/	Copyright 1995-98, Be Incorporated, All Rights Reserved
-/
-*******************************************************************************/
+//------------------------------------------------------------------------------
+//	Copyright (c) 2001-2002, OpenBeOS
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a
+//	copy of this software and associated documentation files (the "Software"),
+//	to deal in the Software without restriction, including without limitation
+//	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//	and/or sell copies of the Software, and to permit persons to whom the
+//	Software is furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//	DEALINGS IN THE SOFTWARE.
+//
+//	File Name:		Picture.h
+//	Author:			Marc Flerackers (mflerackers@androme.be)
+//	Description:	BPicture records a series of drawing instructions that can
+//                  be "replayed" later.
+//------------------------------------------------------------------------------
 
 #ifndef	_PICTURE_H
 #define	_PICTURE_H
 
+// Standard Includes -----------------------------------------------------------
+
+// System Includes -------------------------------------------------------------
 #include <BeBuild.h>
 #include <InterfaceDefs.h>
 #include <Rect.h>
 #include <Archivable.h>
 
+// Project Includes ------------------------------------------------------------
+
+// Local Includes --------------------------------------------------------------
+
+// Local Defines ---------------------------------------------------------------
+
+// Globals ---------------------------------------------------------------------
+
 class BView;
 struct _BPictureExtent_;
 
-/*----------------------------------------------------------------*/
-/*----- BPicture class -------------------------------------------*/
-
+// BPicture class --------------------------------------------------------------
 class BPicture : public BArchivable {
 public:
 							BPicture();
@@ -74,8 +99,7 @@ virtual	void				_ReservedPicture3();
 		BPicture			*usurped;
 		uint32				_reserved[3];
 };
+//------------------------------------------------------------------------------
 
-/*-------------------------------------------------------------*/
-/*-------------------------------------------------------------*/
+#endif // _PICTURE_H
 
-#endif /* _PICTURE_H */
