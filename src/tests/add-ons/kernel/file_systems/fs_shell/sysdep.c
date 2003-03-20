db94ee0595c010fa859d7bba723c75aa6db5728e
Axel Dörfler <axeld@pinc-software.de>
2003-03-20 18:35:23 +0000

    Added a userland dprintf() export; the output is simply directed to stdout.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@2968 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/tests/add-ons/kernel/file_systems/fs_shell/sysdep.c
+++ src/tests/add-ons/kernel/file_systems/fs_shell/sysdep.c
@@ -333,3 +333,17 @@ system_time(void)
 */  
 
 #endif  /* unix */
+
+#ifdef __BEOS__
+
+void
+dprintf(const char *format, ...)
+{
+	va_list args;
+	
+	va_start(args, format);
+	vprintf(format, args);
+	va_end(args);
+}
+
+#endif
