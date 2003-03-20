5f16032b01f3d340d82ff53e7afa36fb921a167e
Axel Dörfler <axeld@pinc-software.de>
2003-03-20 18:36:25 +0000

    Added the fs-shell testing suite for the iso9660 file system (iso9660_shell).
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@2969 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/tests/add-ons/kernel/file_systems/iso9660/iso9660_shell/additional_commands.h
@@ -0,0 +1,9 @@
+/* This file is included in fs_shell:fsh.c
+ * Insert your definition of additional commands here
+ *
+ * Format:
+ *	{ commandName, functionName, commandDescription },
+ *
+ * And don't forget the comma after the line :-)
+ */
+
