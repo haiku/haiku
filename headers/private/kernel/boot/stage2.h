2b70a18961627bdbfd61fcefa48a967d4e6cf8fb
Axel Dörfler <axeld@pinc-software.de>
2003-05-03 03:43:30 +0000

    Move boot headers into their own private directory (for now, they are still present in private/kernel/ but this will change soon).
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@3132 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/kernel/boot/stage2.h
@@ -0,0 +1,44 @@
+/* 
+** Copyright 2001-2002, Travis Geiselbrecht. All rights reserved.
+** Distributed under the terms of the NewOS License.
+*/
+
+#ifndef KERNEL_BOOT_STAGE2_H
+#define KERNEL_BOOT_STAGE2_H
+
+// this file declares stuff like addr_range, MAX_*, etc.
+#include <boot/stage2_struct.h>
+#include <arch_stage2.h>
+
+
+// kernel args
+typedef struct ka {
+	uint32		cons_line;
+	char		*str;
+	addr_range	bootdir_addr;
+	addr_range	kernel_seg0_addr;
+	addr_range	kernel_seg1_addr;
+	addr_range	kernel_dynamic_section_addr;
+	uint32		num_phys_mem_ranges;
+
+	addr_range	phys_mem_range[MAX_PHYS_MEM_ADDR_RANGE];
+	uint32		num_phys_alloc_ranges;
+	addr_range	phys_alloc_range[MAX_PHYS_ALLOC_ADDR_RANGE];
+	uint32		num_virt_alloc_ranges;
+	addr_range	virt_alloc_range[MAX_VIRT_ALLOC_ADDR_RANGE];
+	uint32		num_cpus;
+	addr_range	cpu_kstack[MAX_BOOT_CPUS];
+
+	arch_kernel_args arch_args;
+
+	struct framebuffer {
+		int		enabled;
+		int		x_size;
+		int		y_size;
+		int		bit_depth;
+		int		already_mapped;
+		addr_range mapping;
+	} fb;
+} kernel_args;
+
+#endif	/* KERNEL_BOOT_STAGE2_H */
