303c294d21f758f4ba03ba0f8d6fa811fb587b90
beveloper <beveloper@nowhere.fake>
2003-06-03 19:02:14 +0000

    added more debug macros
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@3412 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/media/debug.h
+++ headers/private/media/debug.h
@@ -35,9 +35,22 @@
   	#define CALLED() 			((void)0)
 	#define INFO				if (1) {} else printf
   #endif
-	
+
+  #if DEBUG >= 1
+	#define PRINT_FORMAT(_text, _fmt)	do { char _buf[100]; string_for_format((_fmt), _buf, sizeof(_buf)); printf("%s %s\n", (_text), (_buf)); } while (0)
+	#define PRINT_INPUT(_text, _in) 	do { char _buf[100]; string_for_format((_in).format, _buf, sizeof(_buf)); printf("%s node(node %ld, port %ld); source(port %ld, id %ld); dest(port %ld, id %ld); fmt(%s); name(%s)\n", (_text), (_in).node.node, (_in).node.port, (_in).source.port, (_in).source.id, (_in).destination.port, (_in).destination.id, _buf, (_in).name); } while (0)
+	#define PRINT_OUTPUT(_text, _out)	do { char _buf[100]; string_for_format((_out).format, _buf, sizeof(_buf)); printf("%s node(node %ld, port %ld); source(port %ld, id %ld); dest(port %ld, id %ld); fmt(%s); name(%s)\n", (_text), (_out).node.node, (_out).node.port, (_out).source.port, (_out).source.id, (_out).destination.port, (_out).destination.id, _buf, (_out).name); } while (0)
+  #else
+	#define PRINT_FORMAT(_text, _fmt)	((void)0)
+	#define PRINT_INPUT(_text, _in)		((void)0)
+	#define PRINT_OUTPUT(_text, _out)	((void)0)
+  #endif
+
 #else
 
+	#define PRINT_FORMAT(_text, _fmt)	((void)0)
+	#define PRINT_INPUT(_text, _in)		((void)0)
+	#define PRINT_OUTPUT(_text, _out)	((void)0)
 	#define UNIMPLEMENTED() 	((void)0)
 	#define BROKEN()			((void)0)
 	#define CALLED()			((void)0)
