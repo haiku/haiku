1cbf8fa21b208f5c2da18a20383e46372e65e607
Axel Dörfler <axeld@pinc-software.de>
2003-06-13 03:53:41 +0000

    Add a first non-functional version of the default media theme.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@3490 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/media/DefaultMediaTheme.h
@@ -0,0 +1,33 @@
+/* 
+** Copyright 2002-2003, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+** Distributed under the terms of the OpenBeOS License.
+*/
+#ifndef DEFAULT_MEDIA_THEME_H
+#define DEFAULT_MEDIA_THEME_H
+
+
+#include <MediaTheme.h>
+
+
+namespace BPrivate {
+
+class DefaultMediaTheme : public BMediaTheme {
+	public:
+		DefaultMediaTheme();
+
+		virtual	BControl *MakeControlFor(BParameter *parameter);
+
+		static BControl *MakeViewFor(BParameter *parameter, const BRect *hintRect = NULL);
+			// this is also called by the BMediaTheme::MakeFallbackViewFor()
+			// method - that's why it's a static.
+
+	protected:
+		virtual	BView *MakeViewFor(BParameterWeb *web, const BRect *hintRect = NULL);
+
+	private:
+		BView *MakeViewFor(BParameterGroup *group, const BRect *hintRect = NULL);
+};
+
+}	// namespace BPrivate
+
+#endif	/* DEFAULT_MEDIA_THEME_H */
