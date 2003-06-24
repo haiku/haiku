c9b0bae94cf4438404c83f2652df7f41916bc43f
Axel Dörfler <axeld@pinc-software.de>
2003-06-24 17:27:07 +0000

    Needed to compile the boot loader stuff under x86.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@3640 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/kernel/boot/platform/bios_ia32/platform_stage2_args.h
@@ -0,0 +1,11 @@
+/*
+** Copyright 2003, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+** Distributed under the terms of the OpenBeOS License.
+*/
+#ifndef KERNEL_BOOT_PLATFORM_BIOS_IA32_STAGE2_H
+#define KERNEL_BOOT_PLATFORM_BIOS_IA32_STAGE2_H
+
+struct platform_stage2_args {
+};
+
+#endif	/* KERNEL_BOOT_PLATFORM_BIOS_IA32_STAGE2_H */
