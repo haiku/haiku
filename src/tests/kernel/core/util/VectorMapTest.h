aabf9e6b16f8a8cf9767387c674394e8e2727506
Ingo Weinhold <ingo_weinhold@gmx.de>
2003-07-02 16:25:47 +0000

    Oops, seems that I forgot to commit the header for VectorMapTest. It utilizes the OrderedMapTest class and doesn't do any testing by itself anymore.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@3809 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/tests/kernel/core/util/VectorMapTest.h
+++ src/tests/kernel/core/util/VectorMapTest.h
@@ -8,22 +8,6 @@ public:
 	VectorMapTest(std::string name = "");
 
 	static CppUnit::Test* Suite();
-	
-	void ConstructorTest();
-	void InsertTest();
-	void PutTest();
-	void GetTest();
-	void RemoveTest();
-	void EraseTest();
-	void MakeEmptyTest();
-	void IndexAccessTest();
-	void FindTest();
-	void FindCloseTest();
-	void IteratorTest();
-
-private:
-	template <class List>
-	void TestList(List &list, typename List::ValueType *values, int valueCount);
 };
 
 #endif // _vector_map_test_h_
