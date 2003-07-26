4e7a2f6c8bcd1cb46e7a80a4f655c93886d855e9
shatty <shatty@nowhere.fake>
2003-07-26 21:28:02 +0000

    utf8 conversions file with stub implementations of convert_to_utf8 and convert_from_utf8
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@4090 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/kits/support/utf8_conversions.cpp
@@ -0,0 +1,21 @@
+#include <UTF8.h>
+
+status_t
+convert_to_utf8(uint32 srcEncoding,
+                const char * src, int32 * srcLen, 
+                char * dst, int32 * dstLen,
+                int32 * state, char substitute = B_SUBSTITUTE)
+{
+	// TODO: implement
+	return B_ERROR;
+}
+
+status_t
+convert_from_utf8(uint32 dstEncoding,
+                  const char * src, int32 * srcLen, 
+                  char * dst, int32 * dstLen,
+                  int32 * state, char substitute = B_SUBSTITUTE)
+{
+	// TODO: implement
+	return B_ERROR;
+}
