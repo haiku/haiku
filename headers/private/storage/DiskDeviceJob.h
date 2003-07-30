8954437d0a47237026f6378ffe2e14345f6fe5d2
Ingo Weinhold <ingo_weinhold@gmx.de>
2003-07-30 00:33:32 +0000

    Moved definitions of general interest to DiskDeviceDefs.h. Minor changes.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@4133 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/storage/DiskDeviceJob.h
+++ headers/private/storage/DiskDeviceJob.h
@@ -9,43 +9,6 @@
 #include <DiskDeviceDefs.h>
 #include <String.h>
 
-// disk device job types
-enum {
-	B_DISK_DEVICE_JOB_CREATE,
-	B_DISK_DEVICE_JOB_DELETE,
-	B_DISK_DEVICE_JOB_INITIALIZE,
-	B_DISK_DEVICE_JOB_RESIZE,
-	B_DISK_DEVICE_JOB_MOVE,
-	B_DISK_DEVICE_JOB_DEFRAGMENT,
-	B_DISK_DEVICE_JOB_REPAIR,
-};
-
-// disk device job statuses
-enum {
-	B_DISK_DEVICE_JOB_UNINITIALIZED,
-	B_DISK_DEVICE_JOB_SCHEDULED,
-	B_DISK_DEVICE_JOB_IN_PROGRESS,
-	B_DISK_DEVICE_JOB_SUCCEEDED,
-	B_DISK_DEVICE_JOB_FAILED,
-	B_DISK_DEVICE_JOB_CANCELED,
-};
-
-// disk device job progress info
-struct disk_device_progress_info {
-	int32	task_count;
-	int32	completed_tasks;
-	float	current_task_progress;
-	char	current_task_description[256];
-};
-
-// disk device job cancel properties
-enum {
-	B_DISK_DEVICE_JOB_CAN_CANCEL			= 0x01,
-	B_DISK_DEVICE_JOB_STOP_ON_CANCEL		= 0x02,
-	B_DISK_DEVICE_JOB_REVERSE_ON_CANCEL		= 0x04,
-	B_DISK_DEVICE_JOB_CAN_PAUSE				= 0x08,
-};
-
 class BDiskDeviceJob {
 public:
 	disk_job_id ID() const;
@@ -55,10 +18,10 @@ public:
 	float Progress() const;		// [0.0, 1.0]
 	uint32 Status() const;	
 	const char *Description() const;
-	status_t GetProgressInfo(disk_device_progress_info *info) const;
+	status_t GetProgressInfo(disk_device_job_progress_info *info) const;
 	uint32 InterruptProperties() const;
 
-	status_t Cancel();
+	status_t Cancel(bool reverse = false);
 	status_t Pause();
 	
 private:
