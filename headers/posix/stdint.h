99a35090bd78343522fb580b73da7d85a935d69e
shatty <shatty@nowhere.fake>
2003-08-02 04:54:11 +0000

    added least and fast types
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@4193 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/posix/stdint.h
+++ headers/posix/stdint.h
@@ -19,7 +19,33 @@ typedef signed long long int64_t;
 typedef unsigned long long uintmax_t;
 typedef signed long long intmax_t;
 
-// ToDo: add "least" and "fast" types
+// least types
+
+typedef uint8_t uint_least8_t;
+typedef int8_t int_least8_t;
+
+typedef uint16_t uint_least16_t;
+typedef int16_t int_least16_t;
+
+typedef uint32_t uint_least32_t;
+typedef int32_t int_least32_t;
+
+typedef uint64_t uint_least64_t;
+typedef int64_t int_least64_t;
+
+// fast types assume a 32-bit processor
+
+typedef uint32_t uint_fast8_t;
+typedef int32_t int_fast8_t;
+
+typedef uint32_t uint_fast16_t;
+typedef int32_t int_fast16_t;
+
+typedef uint32_t uint_fast32_t;
+typedef int32_t int_fast32_t;
+
+typedef uint64_t uint_fast64_t;
+typedef int64_t int_fast64_t;
 
 /* BSD compatibility */
 typedef uint8_t u_int8_t;
