f3d6a89559368c31c9dfce37100630dcfbb5aa03
Phil Greenway <sikosis@gmail.com>
2003-09-19 14:09:50 +0000

    Includes BSD License. Ported and Coded by Chris Simmons.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@4766 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/apps/bin/pwd/pwd.c
+++ src/apps/bin/pwd/pwd.c
@@ -1,3 +1,36 @@
+/*
+ * Copyright (c) 1991, 1993, 1994
+ *      The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by the University of
+ *      California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #include <sys/stat.h>
 #include <errno.h>
 #include <getopt.h>
@@ -10,7 +43,8 @@ static char *getcwd_logical(void);
 int usage(void);
 int version(void);
 
-int main(int argc, char *argv[]) 
+int
+main(int argc, char *argv[]) 
 { 
 	int physical = false; 
 	int argOption;
@@ -25,9 +59,9 @@ int main(int argc, char *argv[])
 	help_opt, version_opt, 0
 	};
 
-	while ((argOption = getopt_long(argc, optargv, "LP", options, &indexptr)) != -1)
+	while ((argOption = getopt_long(argc, optargv, "LP", options, &indexptr)) != -1) {
+	
 		switch (argOption) { 
-			case '?':
 			case 'L': // Logical
 				physical = false;
 				break; 
@@ -46,15 +80,14 @@ int main(int argc, char *argv[])
 						break;
 				}
 				break;
-			} 
+		}
+	}
 	argc -= optind;
 
-	if (argc != 0)
-	{
+	if (argc != 0) {
 		usage();
 	}
-	else
-	{
+	else {
 		/*
 		 * If the logical option was specified, and we can not find the logical currrent directory 
 		 * then behave as if -P (physical) was specified.
@@ -68,7 +101,8 @@ int main(int argc, char *argv[])
 }
 
 
-int usage(void)
+int
+usage(void)
 {
 	(void)fprintf(stderr, 
 "pwd OBOS (C)2003 MIT License. (http://www.openbeos.net/)
@@ -83,7 +117,8 @@ Print the current working directory path
 	return;
 }
 
-int version(void)
+int
+version(void)
 {
 	(void)fprintf(stderr, "pwd OBOS (C)2003 MIT License. (http://www.openbeos.net/)\n");
 	return;
