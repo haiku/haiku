58020b9aa92e950a69f1797d7dfe59b09de12be6
Axel Dörfler <axeld@pinc-software.de>
2003-10-01 01:42:31 +0000

    Added dummy for platform_user_menu_requested() (always returns "false" for now).
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@4896 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/tests/kernel/boot/loader/platform_misc.cpp
@@ -0,0 +1,15 @@
+/*
+** Copyright 2003, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+** Distributed under the terms of the OpenBeOS License.
+*/
+
+
+#include <boot/platform.h>
+
+
+bool
+platform_user_menu_requested(void)
+{
+	return false;
+}
+
