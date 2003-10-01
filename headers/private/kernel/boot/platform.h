6e5bc20607315b2d5875de1c4e76e46e9c217be8
Axel Dörfler <axeld@pinc-software.de>
2003-10-01 01:02:36 +0000

    Added some more functions to the platform abstraction API - it's now possible to select a boot device which is used directly.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@4878 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/boot/platform.h
+++ headers/private/kernel/boot/platform.h
@@ -16,15 +16,33 @@ struct stage2_args;
 extern "C" {
 #endif
 
+/* debug functions */
 extern void panic(const char *format, ...);
 extern void dprintf(const char *format, ...);
 
-extern status_t platform_get_boot_devices(struct stage2_args *args, struct list *devicesList);
+/* heap functions */
 extern void platform_release_heap(void *base);
 extern status_t platform_init_heap(struct stage2_args *args, void **_base, void **_top);
 
+/* misc functions */
+extern bool platform_user_menu_requested(void);
+
 #ifdef __cplusplus
 }
+
+class Node;
+namespace boot {
+	class Partition;
+}
+
+/* device functions */
+
+// these functions have to be implemented in C++
+extern status_t platform_get_boot_device(struct stage2_args *args, Node **_device);
+extern status_t platform_add_block_devices(struct stage2_args *args, struct list *devicesList);
+extern status_t platform_get_boot_partition(struct stage2_args *args, struct list *partitions,
+					boot::Partition **_partition);
+
 #endif
 
 #endif	/* KERNEL_BOOT_PLATFORM_H */
