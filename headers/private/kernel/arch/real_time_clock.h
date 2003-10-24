a34e8cc39f95dc493b974a8a5281d6b98c6f40a2
Axel Dörfler <axeld@pinc-software.de>
2003-10-24 11:58:21 +0000

    Applied our style guide, made the headers C++ safe.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@5139 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/arch/real_time_clock.h
+++ headers/private/kernel/arch/real_time_clock.h
@@ -5,11 +5,21 @@
 #ifndef KERNEL_ARCH_REAL_TIME_CLOCK_H
 #define KERNEL_ARCH_REAL_TIME_CLOCK_H
 
+
 #include <kernel.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void arch_rtc_set_hw_time(uint32 seconds);
 	// Set HW clock to 'seconds' since 1/1/1970
 uint32 arch_rtc_get_hw_time(void);
 	// Returns number of seconds since 1/1/1970 as stored in HW
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif	/* KERNEL_ARCH_REAL_TIME_CLOCK_H */
--- headers/private/kernel/real_time_clock.h
+++ headers/private/kernel/real_time_clock.h
@@ -9,14 +9,22 @@
 #include <KernelExport.h>
 
 
-int rtc_init(kernel_args *ka);
-// Initialize the real-time clock.
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-void rtc_set_system_time(uint32 current_time);
-// Set the system time.  'current_time' is the number of seconds elapsed since
-// Jan. 1, 1970.
+status_t rtc_init(kernel_args *ka);
+	// Initialize the real-time clock.
+
+void rtc_set_system_time(uint32 currentTime);
+	// Set the system time.  'currentTime' is the number of seconds
+	// elapsed since Jan. 1, 1970.
 
 bigtime_t rtc_boot_time(void);
-// Returns the time at which the system was booted in microseconds since Jan 1, 1970.
+	// Returns the time at which the system was booted in microseconds since Jan 1, 1970.
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _KERNEL_REAL_TIME_CLOCK_H */
