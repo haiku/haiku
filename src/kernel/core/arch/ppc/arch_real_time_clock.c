60f9bd4a5785ab403c26882b70e5d2768ae2a615
Axel Dörfler <axeld@pinc-software.de>
2003-10-24 12:38:07 +0000

    Adds an empty arch_real_time_clock.c for the PPC build. The Pegasos may need the same RTC code as x86 (but using the ISA bus manager instead of in/out).
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@5143 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/kernel/core/arch/ppc/arch_real_time_clock.c
@@ -0,0 +1,20 @@
+/* 
+** Copyright 2003, Jeff Ward, jeff@r2d2.stcloudstate.edu. All rights reserved.
+** Distributed under the terms of the OpenBeOS License.
+*/
+
+
+#include <arch/real_time_clock.h>
+
+
+uint32
+arch_rtc_get_hw_time(void)
+{
+	return 0;
+}
+
+
+void
+arch_rtc_set_hw_time(uint32 seconds)
+{
+}	
