c75570328b0a61ae1fa5d561b89132de6d392273
Tyler Dauwalder <tylerdauwalder@nowhere.fake>
2003-11-02 01:05:25 +0000

    Initial checkin. Abstract base class for various partition types.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@5229 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/add-ons/kernel/file_systems/udf/Partition.h
@@ -0,0 +1,27 @@
+//----------------------------------------------------------------------
+//  This software is part of the OpenBeOS distribution and is covered 
+//  by the OpenBeOS license.
+//
+//  Copyright (c) 2003 Tyler Dauwalder, tyler@dauwalder.net
+//---------------------------------------------------------------------
+#ifndef _UDF_PARTITION_H
+#define _UDF_PARTITION_H
+
+/*! \file Partition.h
+*/
+
+#include <SupportDefs.h>
+
+namespace Udf {
+
+/*! \brief Abstract base class for various UDF partition types.
+*/
+class Partition {
+public:
+	virtual ~Partition() {}
+	virtual status_t MapBlock(uint32 logicalBlock, uint32 &physicalBlock) = 0;
+};
+
+};	// namespace Udf
+
+#endif	// _UDF_PARTITION_H
