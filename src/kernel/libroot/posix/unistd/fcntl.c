48a7b199b7b67f21cf296f441682756b13bb31ad
Axel Dörfler <axeld@pinc-software.de>
2003-11-06 00:10:57 +0000

    Added fcntl() empty stub implementation.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@5264 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/kernel/libroot/posix/unistd/fcntl.c
@@ -0,0 +1,18 @@
+/* 
+** Copyright 2003, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+** Distributed under the terms of the OpenBeOS License.
+*/
+
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <syscalls.h>
+
+
+int
+fcntl(int fd, int op, ...)
+{
+	// ToDo: implement me!
+	return 0;
+}
+
