3d78db8f17d62c11182f0da539d8f049995ff4f7
Axel Dörfler <axeld@pinc-software.de>
2003-11-11 04:01:31 +0000

    Removed B_KDEBUG_* definitions; they are already part of KernelExport.h. dbg_register_file[][] reserves now space for SMP_MAX_CPUS CPUs. Removed old dbg_{set|get}_serial_debug() functions.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@5308 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/debug.h
+++ headers/private/kernel/debug.h
@@ -7,23 +7,18 @@
 
 
 #include <kernel.h>
-
+#include <arch/smp.h>
 
 struct kernel_args;
 
-extern int dbg_register_file[2][14]; /* XXXmpetit -- must be made generic */
+
+extern int dbg_register_file[SMP_MAX_CPUS][14];
+	/* XXXmpetit -- must be made generic */
 
 int  dbg_init(struct kernel_args *ka);
 int  dbg_init2(struct kernel_args *ka);
 char dbg_putch(char c);
 void dbg_puts(const char *s);
-bool dbg_set_serial_debug(bool new_val);
-bool dbg_get_serial_debug(void);
-
-/* special return codes for kernel debugger command function*/
-#define  B_KDEBUG_CONT   2
-#define  B_KDEBUG_QUIT   3
-
 
 #if DEBUG 
 #	define ASSERT(x) \
