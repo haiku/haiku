f9f5f264735759321cd3eab08de5137a1b63e837
DarkWyrm <darkwyrm@gmail.com>
2003-11-15 20:48:22 +0000

    Added OBOS header. I don't think there was anything else. :)
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@5386 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/interface/ScrollBar.h
+++ headers/os/interface/ScrollBar.h
@@ -1,13 +1,30 @@
-/*******************************************************************************
-/
-/	File:			ScrollBar.h
-/
-/   Description:    BScrollBar displays and controls a scrollbar. 
-/
-/	Copyright 1992-98, Be Incorporated, All Rights Reserved
-/
-*******************************************************************************/
-
+//------------------------------------------------------------------------------
+//	Copyright (c) 2001-2002, OpenBeOS
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a
+//	copy of this software and associated documentation files (the "Software"),
+//	to deal in the Software without restriction, including without limitation
+//	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//	and/or sell copies of the Software, and to permit persons to whom the
+//	Software is furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//	DEALINGS IN THE SOFTWARE.
+//
+//	File Name:		ScrollBar.h
+//	Author:			Marc Flerackers (mflerackers@androme.be)
+//					DarkWyrm (bpmagic@columbus.rr.com)
+//	Description:	Client-side class for scrolling
+//
+//------------------------------------------------------------------------------
 #ifndef	_SCROLL_BAR_H
 #define	_SCROLL_BAR_H
 
@@ -84,6 +101,8 @@ virtual status_t	Perform(perform_code d, void *arg);
 
 private:
 		friend class BScrollArrowButton;
+		friend class BScrollBarPrivateData;
+		friend status_t control_scrollbar(int8 command, void *data, BScrollBar *bar);
 virtual	void		_ReservedScrollBar1();
 virtual	void		_ReservedScrollBar2();
 virtual	void		_ReservedScrollBar3();
