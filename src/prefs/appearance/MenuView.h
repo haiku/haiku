05cc992d17c3a2a576089efcf4294f5641749e45
DarkWyrm <darkwyrm@gmail.com>
2003-11-22 23:57:04 +0000

    Added Menu preferences panel. Works pretty good, too. :) Eventually, this panel will replace the need for the Menu prefs app
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@5445 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/prefs/appearance/MenuView.h
@@ -0,0 +1,36 @@
+#ifndef MENUVIEW_H_
+#define MENUVIEW_H_
+
+#include <Menu.h>
+#include <MenuItem.h>
+#include <MenuBar.h>
+#include <MenuField.h>
+#include <View.h>
+#include <Button.h>
+#include <RadioButton.h>
+#include <Box.h>
+#include <CheckBox.h>
+#include "FontMenu.h"
+
+class MenuView : public BView
+{
+public:
+	MenuView(BRect frame, const char *name, int32 resize, int32 flags);
+	~MenuView(void);
+	void AttachedToWindow(void);
+	void MessageReceived(BMessage *msg);
+	void MarkCommandKey(void);
+	void SetCommandKey(bool use_alt);
+private:
+	BMenuField *fontfield, *fontsizefield;
+	BMenu *fontsizemenu;
+	FontMenu *fontmenu;
+	BButton *defaultsbutton, *revertbutton;
+	BCheckBox *showtriggers;
+	BRadioButton *altcmd, *ctrlcmd;
+	menu_info menuinfo,revertinfo;
+	bool altcommand;
+	bool revert, revertaltcmd;
+};
+
+#endif
