1c828415cfbe92acf9a14ba78833703ea418aef4
shatty <shatty@nowhere.fake>
2003-12-13 20:12:32 +0000

    ogg reader plugin based on mp3
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@5663 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/add-ons/media/plugins/ogg/OggReaderPlugin.h
@@ -0,0 +1,82 @@
+#ifndef _OGG_READER_PLUGIN_H
+#define _OGG_READER_PLUGIN_H
+
+#include "ReaderPlugin.h"
+
+namespace BPrivate { namespace media {
+
+class oggReader : public Reader
+{
+public:
+				oggReader();
+				~oggReader();
+	
+	const char *Copyright();
+	
+	status_t	Sniff(int32 *streamCount);
+
+	void		GetFileFormatInfo(media_file_format *mff);
+
+	status_t	AllocateCookie(int32 streamNumber, void **cookie);
+	status_t	FreeCookie(void *cookie);
+	
+	status_t	GetStreamInfo(void *cookie, int64 *frameCount, bigtime_t *duration,
+							  media_format *format, void **infoBuffer, int32 *infoSize);
+
+	status_t	Seek(void *cookie,
+					 uint32 seekTo,
+					 int64 *frame, bigtime_t *time);
+
+	status_t	GetNextChunk(void *cookie,
+							 void **chunkBuffer, int32 *chunkSize,
+							 media_header *mediaHeader);
+									 
+	BPositionIO *Source() { return fSeekableSource; }
+
+private:
+	bool		ParseFile();
+	bool 		IsMp3File();
+	
+	// checks if the buffer contains a valid ogg stream, length should be
+	bool		IsValidStream(uint8 *buffer, int size);
+	
+	int			GetFrameLength(void *header);
+	int			GetXingVbrLength(uint8 *header);
+	int			GetFraunhoferVbrLength(uint8 *header);
+	int			GetLameVbrLength(uint8 *header);
+	int			GetId3v2Length(uint8 *header);
+	int			GetInfoCbrLength(uint8 *header);
+	
+	bool		FindData();
+
+	void		ParseXingVbrHeader(int64 pos);
+	void		ParseFraunhoferVbrHeader(int64 pos);
+	
+	int64		XingSeekPoint(float percent);
+	
+private:
+	BPositionIO *	fSeekableSource;
+	int64			fFileSize;
+	
+	int64			fDataStart;
+	int64			fDataSize;
+	
+	struct xing_vbr_info;
+	struct fhg_vbr_info;
+	
+	xing_vbr_info *	fXingVbrInfo;
+	fhg_vbr_info *	fFhgVbrInfo;
+};
+
+
+class oggReaderPlugin : public ReaderPlugin
+{
+public:
+	Reader *NewReader();
+};
+
+} } // namespace BPrivate::media
+
+using namespace BPrivate::media;
+
+#endif
