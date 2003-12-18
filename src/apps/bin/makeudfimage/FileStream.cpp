400ac2abb4346944869969d0ca2f67a895ddacc5
Tyler Dauwalder <tylerdauwalder@nowhere.fake>
2003-12-18 09:17:46 +0000

    Made FileStream a subclass of new DataStream class instead of BFile.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@5695 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/apps/bin/makeudfimage/FileStream.cpp
+++ src/apps/bin/makeudfimage/FileStream.cpp
@@ -5,57 +5,71 @@
 //  Copyright (c) 2003 Tyler Dauwalder, tyler@dauwalder.net
 //----------------------------------------------------------------------
 
-/*! \file OutputFile.cpp
-
-	BFile subclass with some handy new output methods. (implementation)
+/*! \file FileStream.cpp
 */
 
-#include "OutputFile.h"
+#include "FileStream.h"
 
 #include <stdlib.h>
 #include <string.h>
 
-OutputFile::OutputFile(const char *path, uint32 open_mode)
-	: BFile(path, open_mode)
+FileStream::FileStream(const char *path, uint32 open_mode)
+	: fFile(path, open_mode)
 {
 }
 
-ssize_t
-OutputFile::Write(const void *buffer, size_t size)
+status_t
+FileStream::InitCheck() const
 {
-	return BFile::Write(buffer, size);
+	return fFile.InitCheck();
+}
+
+ssize_t
+FileStream::Read(void *buffer, size_t size)
+{
+	return fFile.Read(buffer, size);
+}
+
+ssize_t
+FileStream::ReadAt(off_t pos, void *buffer, size_t size)
+{
+	return fFile.ReadAt(pos, buffer, size);
+}
+
+ssize_t
+FileStream::Write(const void *buffer, size_t size)
+{
+	return fFile.Write(buffer, size);
 } 
 
 ssize_t
-OutputFile::WriteAt(off_t pos, const void *buffer, size_t size)
+FileStream::WriteAt(off_t pos, const void *buffer, size_t size)
 {
-	return BFile::WriteAt(pos, buffer, size);
+	return fFile.WriteAt(pos, buffer, size);
 }
 
 /*! \brief Writes \a size bytes worth of data from \a data at the current
 	position in the file, incrementing the file's position marker as it goes.
-	
-	\param bufferSize The size of the memory buffer to be used for writing.
 */
 ssize_t
-OutputFile::Write(BDataIO &data, size_t size, size_t bufferSize)
+FileStream::Write(BDataIO &data, size_t size)
 {
-	status_t error = bufferSize > 0 ? B_OK : B_BAD_VALUE;
+	status_t error = kBufferSize > 0 ? B_OK : B_BAD_VALUE;
 	size_t bytes = 0;
 	if (!error) {
-		void *buffer = malloc(bufferSize);
+		void *buffer = malloc(kBufferSize);
 		error = buffer ? B_OK : B_NO_MEMORY;
 		if (!error) {
 			// Fudge the buffer size from here out if the requested
 			// number of bytes to write is smaller than the buffer
-			bufferSize = (size < bufferSize ? size : bufferSize);
+			size_t bufferSize = (size < kBufferSize ? size : kBufferSize);
 			// Zero
 			memset(buffer, 0, bufferSize);
 			// Write
 			while (bytes < size) {
 				ssize_t bytesRead = data.Read(buffer, bufferSize);
 				if (bytesRead >= 0) {
-					ssize_t bytesWritten = BFile::Write(buffer, bytesRead);
+					ssize_t bytesWritten = fFile.Write(buffer, bytesRead);
 					if (bytesWritten >= 0) {
 						bytes += bytesWritten;
 					} else {
@@ -75,28 +89,26 @@ OutputFile::Write(BDataIO &data, size_t size, size_t bufferSize)
 
 /*! \brief Writes \a size bytes worth of data from \a data at position
 	\a pos in the file without incrementing the file's position marker.
-	
-	\param bufferSize The size of the memory buffer to be used for writing.
 */
 ssize_t
-OutputFile::WriteAt(off_t pos, BDataIO &data, size_t size, size_t bufferSize)
+FileStream::WriteAt(off_t pos, BDataIO &data, size_t size)
 {
-	status_t error = bufferSize > 0 ? B_OK : B_BAD_VALUE;
+	status_t error = kBufferSize > 0 ? B_OK : B_BAD_VALUE;
 	size_t bytes = 0;
 	if (!error) {
-		void *buffer = malloc(bufferSize);
+		void *buffer = malloc(kBufferSize);
 		error = buffer ? B_OK : B_NO_MEMORY;
 		if (!error) {
 			// Fudge the buffer size from here out if the requested
 			// number of bytes to write is smaller than the buffer
-			bufferSize = (size < bufferSize ? size : bufferSize);
+			size_t bufferSize = (size < kBufferSize ? size : kBufferSize);
 			// Zero
 			memset(buffer, 0, bufferSize);
 			// Write
 			while (bytes < size) {
 				ssize_t bytesRead = data.Read(buffer, bufferSize);
 				if (bytesRead >= 0) {
-					ssize_t bytesWritten = BFile::WriteAt(pos, buffer, bytesRead);
+					ssize_t bytesWritten = fFile.WriteAt(pos, buffer, bytesRead);
 					if (bytesWritten >= 0) {
 						bytes += bytesWritten;
 						pos += bytesWritten;
@@ -117,26 +129,24 @@ OutputFile::WriteAt(off_t pos, BDataIO &data, size_t size, size_t bufferSize)
 
 /*! \brief Writes \a size bytes worth of zeros at the current position 
 	in the file, incrementing the file's position marker as it goes.
-	
-	\param bufferSize The size of the memory buffer to be used for writing.
 */
 ssize_t
-OutputFile::Zero(size_t size, size_t bufferSize)
+FileStream::Zero(size_t size)
 {
-	status_t error = bufferSize > 0 ? B_OK : B_BAD_VALUE;
+	status_t error = kBufferSize > 0 ? B_OK : B_BAD_VALUE;
 	size_t bytes = 0;
 	if (!error) {
-		void *buffer = malloc(bufferSize);
+		void *buffer = malloc(kBufferSize);
 		error = buffer ? B_OK : B_NO_MEMORY;
 		if (!error) {
 			// Fudge the buffer size from here out if the requested
 			// number of bytes to write is smaller than the buffer
-			bufferSize = (size < bufferSize ? size : bufferSize);
+			size_t bufferSize = (size < kBufferSize ? size : kBufferSize);
 			// Zero
 			memset(buffer, 0, bufferSize);
 			// Write
 			while (bytes < size) {
-				ssize_t bytesWritten = BFile::Write(buffer, bufferSize);
+				ssize_t bytesWritten = fFile.Write(buffer, bufferSize);
 				if (bytesWritten >= 0) {
 					bytes += bytesWritten;
 				} else {
@@ -152,26 +162,24 @@ OutputFile::Zero(size_t size, size_t bufferSize)
 
 /*! \brief Writes \a size bytes worth of zeros at position \a pos
 	in the file without incrementing the file's position marker.
-	
-	\param bufferSize The size of the memory buffer to be used for writing.
 */
 ssize_t
-OutputFile::ZeroAt(off_t pos, size_t size, size_t bufferSize)
+FileStream::ZeroAt(off_t pos, size_t size)
 {
-	status_t error = bufferSize > 0 ? B_OK : B_BAD_VALUE;
+	status_t error = kBufferSize > 0 ? B_OK : B_BAD_VALUE;
 	size_t bytes = 0;
 	if (!error) {
-		void *buffer = malloc(bufferSize);
+		void *buffer = malloc(kBufferSize);
 		error = buffer ? B_OK : B_NO_MEMORY;
 		if (!error) {
 			// Fudge the buffer size from here out if the requested
 			// number of bytes to write is smaller than the buffer
-			bufferSize = (size < bufferSize ? size : bufferSize);
+			size_t bufferSize = (size < kBufferSize ? size : kBufferSize);
 			// Zero
 			memset(buffer, 0, bufferSize);
 			// Write
 			while (bytes < size) {
-				ssize_t bytesWritten = BFile::WriteAt(pos, buffer, bufferSize);
+				ssize_t bytesWritten = fFile.WriteAt(pos, buffer, bufferSize);
 				if (bytesWritten >= 0) {
 					bytes += bytesWritten;
 					pos += bytesWritten;
@@ -185,3 +193,21 @@ OutputFile::ZeroAt(off_t pos, size_t size, size_t bufferSize)
 	}	
 	return error ? ssize_t(error) : ssize_t(bytes);	
 }	
+
+off_t
+FileStream::Seek(off_t position, uint32 seek_mode)
+{
+	return fFile.Seek(position, seek_mode);
+}
+
+off_t
+FileStream::Position() const
+{
+	return fFile.Position();
+}
+
+status_t
+FileStream::SetSize(off_t size)
+{
+	return fFile.SetSize(size);
+}
--- src/apps/bin/makeudfimage/FileStream.h
+++ src/apps/bin/makeudfimage/FileStream.h
@@ -5,9 +5,7 @@
 //  Copyright (c) 2003 Tyler Dauwalder, tyler@dauwalder.net
 //----------------------------------------------------------------------
 
-/*! \file OutputFile.h
-
-	BFile subclass with some handy new output methods. (declarations)
+/*! \file FileStream.h
 */
 
 #ifndef _OUTPUT_FILE_H
@@ -15,21 +13,38 @@
 
 #include <File.h>
 
-class OutputFile : public BFile {
+#include "DataStream.h"
+
+/*! \brief 	DataStream implementation that writes directly to a file.
+
+	Basically, this the filestream interface of BFile with a few
+	new, handy output functions.
+*/
+class FileStream : public DataStream {
 public:
-	OutputFile(const char *path, uint32 open_mode);
+	FileStream(const char *path, uint32 open_mode);
+	status_t InitCheck() const;
 
-	static const size_t kDefaultBufferSize = 32 * 1024;
+	static const size_t kBufferSize = 32 * 1024;
 
-	virtual	ssize_t Write(const void *buffer, size_t size);
-	virtual	ssize_t WriteAt(off_t pos, const void *buffer, size_t size);
-	ssize_t Write(BDataIO &data, size_t size,
-	              size_t bufferSize = kDefaultBufferSize);
-	ssize_t	WriteAt(off_t pos, BDataIO &data, size_t size,
-	                size_t bufferSize = kDefaultBufferSize);
-	ssize_t Zero(size_t size, size_t bufferSize = kDefaultBufferSize);
-	ssize_t	ZeroAt(off_t pos, size_t size,
-	               size_t bufferSize = kDefaultBufferSize);	
+	virtual ssize_t Read(void *buffer, size_t size);
+	virtual	ssize_t ReadAt(off_t pos, void *buffer, size_t size);
+
+	virtual ssize_t Write(const void *buffer, size_t size);
+	virtual ssize_t WriteAt(off_t pos, const void *buffer, size_t size);
+
+	virtual ssize_t Write(BDataIO &data, size_t size);
+	virtual ssize_t	WriteAt(off_t pos, BDataIO &data, size_t size);
+
+	virtual ssize_t Zero(size_t size);
+	virtual ssize_t	ZeroAt(off_t pos, size_t size);	
+
+	virtual off_t Seek(off_t position, uint32 seek_mode);
+	virtual off_t Position() const;
+
+	virtual status_t SetSize(off_t size);
+private:
+	BFile fFile;	               
 };
 
 #endif	// _OUTPUT_FILE_H
