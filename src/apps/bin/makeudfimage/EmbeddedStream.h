2019a4454aa6310f2fa4c772964f8e4e2d51684e
Tyler Dauwalder <tylerdauwalder@nowhere.fake>
2003-12-18 09:29:28 +0000

    Initial checkin. SimulatedStream implementation that takes a single offset and length. Will be used for data streams embedded in icbs. Mostly complete, completely untested.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@5698 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/apps/bin/makeudfimage/EmbeddedStream.h
@@ -0,0 +1,32 @@
+//----------------------------------------------------------------------
+//  This software is part of the OpenBeOS distribution and is covered 
+//  by the OpenBeOS license.
+//
+//  Copyright (c) 2003 Tyler Dauwalder, tyler@dauwalder.net
+//----------------------------------------------------------------------
+
+/*! \file EmbeddedStream.h
+*/
+
+#ifndef _EMBEDDED_STREAM_H
+#define _EMBEDDED_STREAM_H
+
+#include "SimulatedStream.h"
+
+/*! \brief AbstractExtentStream implementation that takes a single, 
+	not-neccessarily-block-aligned extent.
+*/
+class EmbeddedStream : public SimulatedStream {
+public:
+	EmbeddedStream(DataStream &stream, off_t offset, size_t size);
+
+protected:
+	virtual status_t _GetExtent(off_t pos, size_t size, data_extent &extent);
+	virtual off_t _Size();
+
+private:
+	off_t fOffset;
+	size_t fSize;
+};
+
+#endif	// _EMBEDDED_STREAM_H
