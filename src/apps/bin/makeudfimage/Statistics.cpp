ef92dae19f8af5e71c1718ca3d1d433f57c38b15
Tyler Dauwalder <tylerdauwalder@nowhere.fake>
2004-01-02 03:49:16 +0000

    Added [Set]ImageSize() and {DirectoryBytes,FileBytes,ImageSize}String().
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@5868 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/apps/bin/makeudfimage/Statistics.cpp
+++ src/apps/bin/makeudfimage/Statistics.cpp
@@ -12,17 +12,64 @@
 
 #include "Statistics.h"
 
+#include <stdio.h>
+
+/*! \brief Returns a string describing the given number of bytes
+	in the most appropriate units (i.e. bytes, KB, MB, etc.).
+*/
+static
+std::string
+bytes_to_string(uint64 bytes)
+{
+	const uint64 kb = 1024;			// kilo
+	const uint64 mb = 1024 * kb;	// mega
+	const uint64 gb = 1024 * mb;	// giga
+	const uint64 tb = 1024 * gb;	// tera
+	const uint64 pb = 1024 * tb;	// peta
+	const uint64 eb = 1024 * pb;	// exa
+	std::string units;
+	uint64 divisor = 1;
+	if (bytes >= eb) {
+		units = "EB";
+		divisor = eb;
+	} else if (bytes >= pb) {
+		units = "PB";
+		divisor = pb;
+	} else if (bytes >= tb) {
+		units = "TB";
+		divisor = tb;
+	} else if (bytes >= gb) {
+		units = "GB";
+		divisor = gb;
+	} else if (bytes >= mb) {
+		units = "MB";
+		divisor = mb;
+	} else if (bytes >= kb) {
+		units = "KB";
+		divisor = kb;
+	} else {	
+		units = "bytes";
+		divisor = 1;
+	}
+	double scaledValue = double(bytes) / double(divisor);
+	char scaledString[10];
+		// Should really only need 7 chars + NULL...
+	sprintf(scaledString, "%.1f ", scaledValue);
+	return std::string(scaledString) + units;	
+}
+
 /*! \brief Creates a new statistics object and sets the start time
 	for the duration timer.
 */
 Statistics::Statistics()
-	: fDirectories(0)
+	: fStartTime(real_time_clock())
+	, fDirectories(0)
 	, fFiles(0)
 	, fSymlinks(0)
 	, fAttributes(0)
 	, fDirectoryBytes(0)
 	, fFileBytes(0)
-	, fStartTime(real_time_clock())
+	, fImageSize(0)
 {
 }
 
@@ -35,3 +82,34 @@ Statistics::Reset()
 	Statistics null;
 	*this = null;
 }
+
+
+/*! \brief Returns a string describing the number of bytes
+	allocated to directory data and metadata, displayed in the
+	appropriate units (i.e. bytes, KB, MB, etc.).
+*/
+std::string
+Statistics::DirectoryBytesString() const
+{
+	return bytes_to_string(DirectoryBytes());
+}
+
+/*! \brief Returns a string describing the number of bytes
+	allocated to file data and metadata, displayed in the
+	appropriate units (i.e. bytes, KB, MB, etc.).
+*/
+std::string
+Statistics::FileBytesString() const
+{
+	return bytes_to_string(FileBytes());
+}
+
+/*! \brief Returns a string describing the total image size,
+	displayed in the appropriate units (i.e. bytes, KB, MB, etc.).
+*/
+std::string
+Statistics::ImageSizeString() const
+{
+	return bytes_to_string(ImageSize());
+}
+
--- src/apps/bin/makeudfimage/Statistics.h
+++ src/apps/bin/makeudfimage/Statistics.h
@@ -14,6 +14,7 @@
 #define _STATISTICS_H
 
 #include <OS.h>
+#include <string>
 #include <SupportDefs.h>
 
 class Statistics {
@@ -32,6 +33,8 @@ public:
 	void AddDirectoryBytes(uint32 count) { fDirectoryBytes += count; }
 	void AddFileBytes(uint32 count) { fFileBytes += count; }
 
+	void SetImageSize(uint64 size) { fImageSize = size; }
+
 	uint64 Directories() const { return fDirectories; }
 	uint64 Files() const { return fFiles; }
 	uint64 Symlinks() const { return fSymlinks; }
@@ -39,14 +42,20 @@ public:
 	
 	uint64 DirectoryBytes() const { return fDirectoryBytes; }
 	uint64 FileBytes() const { return fFileBytes; }
+	std::string DirectoryBytesString() const;
+	std::string FileBytesString() const;
+	
+	uint64 ImageSize() const { return fImageSize; }
+	std::string ImageSizeString() const;
 private:
+	time_t fStartTime;
 	uint64 fDirectories;
 	uint64 fFiles;
 	uint64 fSymlinks;
 	uint64 fAttributes;
 	uint64 fDirectoryBytes;
 	uint64 fFileBytes;
-	time_t fStartTime;
+	uint64 fImageSize;
 };
 
 #endif	// _STATISTICS_H
