755b5518b73a18e7189a4d3a1ab8c180c4571c93
Tyler Dauwalder <tylerdauwalder@nowhere.fake>
2004-01-02 02:56:18 +0000

    Converted to be a subclass of PositionIOStream.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@5864 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/apps/bin/makeudfimage/FileStream.h
+++ src/apps/bin/makeudfimage/FileStream.h
@@ -8,43 +8,23 @@
 /*! \file FileStream.h
 */
 
-#ifndef _OUTPUT_FILE_H
-#define _OUTPUT_FILE_H
+#ifndef _FILE_STREAM_H
+#define _FILE_STREAM_H
 
 #include <File.h>
 
-#include "DataStream.h"
+#include "PositionIOStream.h"
 
 /*! \brief 	DataStream implementation that writes directly to a file.
-
-	Basically, this the filestream interface of BFile with a few
-	new, handy output functions.
 */
-class FileStream : public DataStream {
+class FileStream : public PositionIOStream {
 public:
 	FileStream(const char *path, uint32 open_mode);
-	status_t InitCheck() const;
+	virtual status_t InitCheck() const;
+	void Flush() { fFile.Sync(); }
 
-	static const size_t kBufferSize = 32 * 1024;
-
-	virtual ssize_t Read(void *buffer, size_t size);
-	virtual	ssize_t ReadAt(off_t pos, void *buffer, size_t size);
-
-	virtual ssize_t Write(const void *buffer, size_t size);
-	virtual ssize_t WriteAt(off_t pos, const void *buffer, size_t size);
-
-	virtual ssize_t Write(BDataIO &data, size_t size);
-	virtual ssize_t	WriteAt(off_t pos, BDataIO &data, size_t size);
-
-	virtual ssize_t Zero(size_t size);
-	virtual ssize_t	ZeroAt(off_t pos, size_t size);	
-
-	virtual off_t Seek(off_t position, uint32 seek_mode);
-	virtual off_t Position() const;
-
-	virtual status_t SetSize(off_t size);
 private:
 	BFile fFile;	               
 };
 
-#endif	// _OUTPUT_FILE_H
+#endif	// _FILE_STREAM_H
