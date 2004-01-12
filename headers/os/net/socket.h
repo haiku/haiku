0a574d33165cc497424c2303f341df7a986db997
Philippe Houdoin <philippe.houdoin@gmail.com>
2004-01-12 23:52:40 +0000

    Introduce an OBOS_NET_STACK_VERSION symbol to help checking which installed network stack a networking app is built on.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@6037 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/net/socket.h
+++ headers/os/net/socket.h
@@ -2,9 +2,12 @@
 #define _NET_SOCKET_H
 
 #include <BeBuild.h>
+
 #include <netinet/in.h> /* in_addr, sockaddr_in */
 #include <sys/socket.h> /* sockaddr */
 #include <sys/select.h>
+
+#define OBOS_NET_STACK_VERSION	0x0100      /* 1.0 */
 
 /* 
  * Be extension
