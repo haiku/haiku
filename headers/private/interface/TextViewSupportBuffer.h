f1569dbb9e1cbb1c71c0d574c051d219d03ffe0c
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2004-01-15 07:15:37 +0000

    Moved some headers here so they can be included by Globals.cpp in the interface kit folder
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@6092 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/interface/TextViewSupportBuffer.h
@@ -0,0 +1,149 @@
+//------------------------------------------------------------------------------
+//	Copyright (c) 2001-2003, OpenBeOS
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a
+//	copy of this software and associated documentation files (the "Software"),
+//	to deal in the Software without restriction, including without limitation
+//	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//	and/or sell copies of the Software, and to permit persons to whom the
+//	Software is furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//	DEALINGS IN THE SOFTWARE.
+//
+//	File Name:		TextViewSupportBuffer.h
+//	Author:			Marc Flerackers (mflerackers@androme.be)
+//	Description:	Template class used to implement the various BTextView
+//					buffer classes.
+//------------------------------------------------------------------------------
+
+#ifndef __TEXT_VIEW_SUPPORT_BUFFER__H__
+#define __TEXT_VIEW_SUPPORT_BUFFER__H__
+
+// Standard Includes -----------------------------------------------------------
+#include <cstdlib>
+#include <cstring>
+
+// System Includes -------------------------------------------------------------
+#include <SupportDefs.h>
+
+// Project Includes ------------------------------------------------------------
+
+// Local Includes --------------------------------------------------------------
+
+// Local Defines ---------------------------------------------------------------
+
+// Globals ---------------------------------------------------------------------
+
+// _BTextViewSupportBuffer_ class ----------------------------------------------
+template <class T>
+class _BTextViewSupportBuffer_ {
+
+public:
+				_BTextViewSupportBuffer_(int32 inExtraCount = 0, int32 inCount = 0);
+virtual			~_BTextViewSupportBuffer_();
+
+		void	InsertItemsAt(int32 inNumItems, int32 inAtIndex, const T *inItem);
+		void	RemoveItemsAt(int32 inNumItems, int32 inAtIndex);
+
+		int32	ItemCount() const;
+
+protected:
+		int32	fExtraCount;	
+		int32	fItemCount;				
+		int32	fBufferCount;
+		T*		fBuffer;
+};
+//------------------------------------------------------------------------------
+template <class T>
+_BTextViewSupportBuffer_<T>::_BTextViewSupportBuffer_(int32 inExtraCount,
+													  int32 inCount)
+	:	fExtraCount(inExtraCount),
+		fItemCount(inCount),
+		fBufferCount(fExtraCount + fItemCount),
+		fBuffer(NULL)
+{
+	fBuffer = (T *)calloc(fExtraCount + fItemCount, sizeof(T));
+}
+//------------------------------------------------------------------------------
+template <class T>
+_BTextViewSupportBuffer_<T>::~_BTextViewSupportBuffer_()
+{
+	free(fBuffer);
+}
+//------------------------------------------------------------------------------
+template <class T>
+void _BTextViewSupportBuffer_<T>::InsertItemsAt(int32 inNumItems,
+												int32 inAtIndex,
+												const T *inItem)
+{
+	if (inNumItems < 1)
+		return;
+	
+	inAtIndex = (inAtIndex > fItemCount) ? fItemCount : inAtIndex;
+	inAtIndex = (inAtIndex < 0) ? 0 : inAtIndex;
+
+	int32 delta = inNumItems * sizeof(T);
+	int32 logSize = fItemCount * sizeof(T);
+	if ((logSize + delta) >= fBufferCount) {
+		fBufferCount = logSize + delta + (fExtraCount * sizeof(T));
+		fBuffer = (T *)realloc(fBuffer, fBufferCount);
+	}
+	
+	T *loc = fBuffer + inAtIndex;
+	memmove(loc + inNumItems, loc, (fItemCount - inAtIndex) * sizeof(T));
+	memcpy(loc, inItem, delta);
+	
+	fItemCount += inNumItems;
+}
+//------------------------------------------------------------------------------
+template <class T>
+void
+_BTextViewSupportBuffer_<T>::RemoveItemsAt(int32 inNumItems,
+												int32 inAtIndex)
+{
+	if (inNumItems < 1)
+		return;
+	
+	inAtIndex = (inAtIndex > fItemCount - 1) ? (fItemCount - 1) : inAtIndex;
+	inAtIndex = (inAtIndex < 0) ? 0 : inAtIndex;
+	
+	T *loc = fBuffer + inAtIndex;
+	memmove(loc, loc + inNumItems, 
+			(fItemCount - (inNumItems + inAtIndex)) * sizeof(T));
+	
+	int32 delta = inNumItems * sizeof(T);
+	int32 logSize = fItemCount * sizeof(T);
+	uint32 extraSize = fBufferCount - (logSize - delta);
+	if (extraSize > (fExtraCount * sizeof(T))) {
+		fBufferCount = (logSize - delta) + (fExtraCount * sizeof(T));
+		fBuffer = (T *)realloc(fBuffer, fBufferCount);
+	}
+	
+	fItemCount -= inNumItems;
+}
+//------------------------------------------------------------------------------
+template<class T>
+inline int32
+_BTextViewSupportBuffer_<T>::ItemCount() const
+{
+	return fItemCount;
+}
+//------------------------------------------------------------------------------
+
+#endif // __TEXT_VIEW_SUPPORT_BUFFER__H__
+
+/*
+ * $Log $
+ *
+ * $Id  $
+ *
+ */
