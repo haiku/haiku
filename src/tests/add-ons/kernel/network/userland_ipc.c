338e8e55bcd251a4b7960c834faba9fe4a754a55
Philippe Houdoin <philippe.houdoin@gmail.com>
2004-01-20 00:04:33 +0000

    Add OS.h header too, as we should not assert it comes with Drivers.h...
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@6167 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/tests/add-ons/kernel/network/userland_ipc.c
+++ src/tests/add-ons/kernel/network/userland_ipc.c
@@ -18,8 +18,8 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <Drivers.h>
-	// for B_SET_(NON)BLOCKING_IO
+#include <OS.h>
+#include <Drivers.h>	// for B_SET_(NON)BLOCKING_IO
 
 
 typedef struct commands_info {
