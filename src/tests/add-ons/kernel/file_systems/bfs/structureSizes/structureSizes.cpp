0207f7189499be332224e39bccc6f954de3c9784
Axel Dörfler <axeld@pinc-software.de>
2004-01-21 15:54:53 +0000

    Added a simple test program to check if the disk structure sizes change with different compiler options - couldn't find anything, though.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@6213 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/tests/add-ons/kernel/file_systems/bfs/structureSizes/structureSizes.cpp
@@ -0,0 +1,37 @@
+/*
+** Copyright 2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+** Distributed under the terms of the OpenBeOS License.
+*/
+
+
+#include <SupportDefs.h>
+
+#undef _PACKED
+#define _PACKED
+
+#include "bfs.h"
+
+#include <stdio.h>
+#include <stddef.h>
+
+
+#define SIZEOF(x) "%20s = %lu\n", #x, sizeof(x)
+#define OFFSETOF(x, y) #x "." #y " at offset %lu\n", offsetof(x, y)
+
+
+int 
+main(int argc, char **argv)
+{
+	printf(SIZEOF(disk_super_block));
+	printf(SIZEOF(small_data));
+	printf(SIZEOF(data_stream));
+	printf(SIZEOF(bfs_inode));
+
+	puts("");
+
+	printf(OFFSETOF(bfs_inode, inode_size));
+	printf(OFFSETOF(bfs_inode, etc));
+	printf(OFFSETOF(bfs_inode, data));
+
+	return 0;
+}
