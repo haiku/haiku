a898baf0cc190dc54e8532524bad2053e15cf14b
Axel Dörfler <axeld@pinc-software.de>
2004-01-23 07:31:57 +0000

    Added missing AddOnManager and FormatManager.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@6243 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/servers/media/media_server.h
+++ src/servers/media/media_server.h
@@ -4,9 +4,13 @@ class NodeManager;
 class BufferManager;
 class NotificationManager;
 class MMediaFilesManager;
+class AddOnManager;
+class FormatManager;
 
 extern AppManager *				gAppManager;
 extern NodeManager *			gNodeManager;
 extern BufferManager *			gBufferManager;
 extern NotificationManager *	gNotificationManager;
 extern MMediaFilesManager *		gMMediaFilesManager;
+extern AddOnManager *gAddOnManager;
+extern FormatManager *gFormatManager;
