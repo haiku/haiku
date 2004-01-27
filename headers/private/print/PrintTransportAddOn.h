6df73d220c95c01a7d4b66bf709cd0af6f21b166
Michael Pfeiffer <michael.w.pfeiffer@gmail.com>
2004-01-27 21:23:40 +0000

    Added copyright text.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@6369 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/print/PrintTransportAddOn.h
+++ headers/private/print/PrintTransportAddOn.h
@@ -1,10 +1,41 @@
+/*
+
+PrintTransportAddOn
+
+Copyright (c) 2003, 04 OpenBeOS. 
+
+Author:
+	Michael Pfeiffer
+	
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
 #ifndef PRINT_TRANSPORT_ADD_ON_H
 #define PRINT_TRANSPORT_ADD_ON_H
 
+#include <image.h>
 #include <DataIO.h>
 #include <Directory.h>
 #include <Message.h>
 
+// to be implemented by the transport add-on
 extern "C" BDataIO* instanciate_transport(BDirectory* printer, BMessage* msg);
 
 #endif PRINT_TRANSPORT_ADD_ON_H
