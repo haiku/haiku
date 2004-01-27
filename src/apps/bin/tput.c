d6c6bb2c2192662572c778fd02650932def2bc51
Phil Greenway <sikosis@gmail.com>
2004-01-27 20:55:49 +0000

    Factor by Chris Simmons Tput by Jan-Rixt Van Hoye (fixed doesnt require ncurses now) Cut by Jan-Rixt Van Hoye
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@6362 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/apps/bin/tput.c
+++ src/apps/bin/tput.c
@@ -1,20 +1,5 @@
-/*	$OpenBSD: tput.c,v 1.15 2003/06/17 21:56:26 millert Exp $	*/
+/*	$NetBSD: tput.c,v 1.15 2003/08/07 11:16:46 agc Exp $	*/
 
-/*
- * Copyright (c) 1999 Todd C. Miller <Todd.Miller@courtesan.com>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
 /*-
  * Copyright (c) 1980, 1988, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -44,8 +29,9 @@
  * SUCH DAMAGE.
  */
 
+//#include <sys/cdefs.h>
 #ifndef lint
-static char copyright[] = "@(#) Copyright (c) 1980, 1988, 1993\n\
+static char copyright[] ="@(#) Copyright (c) 1980, 1988, 1993\n\
 	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
@@ -53,51 +39,38 @@ static char copyright[] = "@(#) Copyright (c) 1980, 1988, 1993\n\
 #if 0
 static char sccsid[] = "@(#)tput.c	8.3 (Berkeley) 4/28/95";
 #endif
-static char rcsid[] = "$OpenBSD: tput.c,v 1.15 2003/06/17 21:56:26 millert Exp $";
+static char rcsid[] = "$NetBSD: tput.c,v 1.15 2003/08/07 11:16:46 agc Exp $";
 #endif /* not lint */
 
-#include <sys/param.h>
+#include <termios.h>
 
-#include <ctype.h>
-#include <curses.h>
-#include <term.h>
+//#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <termios.h>
-#include <unistd.h>
 #include <string.h>
+#include <termcap.h>
+#include <unistd.h>
 
-#include <sys/wait.h>
-
-static void   init(void);
-static char **process(char *, char *, char **);
-static void   reset(void);
-static void   set_margins(void);
-static void   usage(void);
-
-extern char  *__progname;
+	int   main __P((int, char **));
+static int    outc __P((int));
+static void   prlongname __P((char *));
+static void   setospeed __P((void));
+static void   usage __P((void));
+static char **process __P((char *, char *, char **));
 
 int
-main(int argc, char *argv[])
+main(argc, argv)
+	int argc;
+	char **argv;
 {
-	int ch, exitval, n, Sflag, Vflag;
-	char *p, *term, *str;
-	char **oargv;
+	int ch, exitval, n;
+	char *cptr, *p, *term, buf[1024], tbuf[1024];
 
-	oargv = argv;
 	term = NULL;
-	Sflag = Vflag = exitval = 0;
-	while ((ch = getopt(argc, argv, "VST:")) != -1)
+	while ((ch = getopt(argc, argv, "T:")) != -1)
 		switch(ch) {
 		case 'T':
 			term = optarg;
-			break;
-		case 'S':
-			Sflag = 1;
-			break;
-		/*<Jan-Rixt> 25/09/2003 version of ncursus*/	
-		case 'V':
-			Vflag = 1;
 			break;
 		case '?':
 		default:
@@ -106,300 +79,156 @@ main(int argc, char *argv[])
 	argc -= optind;
 	argv += optind;
 
-	if (Sflag && argc > 0)
-		usage();
-
 	if (!term && !(term = getenv("TERM")))
-		fprintf(stderr, "No value for $TERM and no -T specified");
-
-	/*
-	 * NOTE: tgetent() will call setupterm() and set ospeed for us
-	 * (this is ncurses-specific behavior)
-	 */
-	if (tgetent(NULL, term) != 1)
-		fprintf(stderr, "Unknown terminal type `%s'", term);
-
-	if (strcmp(__progname, "clear") == 0) {
-		if (Sflag)
-			usage();
-		argv = oargv;
-		*argv = __progname;
-		*(argv+1) = NULL;
-	}
-	if (Sflag) {
-		char **av;
-		/* Build new argv based on stdin */
-		argc = n = 0;
-		av = NULL;
-		str = malloc(1024);
-		while (fgets(str,1024,stdin) && (str[0] != '!')) {
-			if (str[strlen(str)-1] != '\n')
-				fprintf(stderr, "premature EOF");
-			str[strlen(str)-1] = '\0';
-			/* grow av as needed */
-			if (argc + 1 >= n) {
-				n += 64;
-				av = (char **)realloc(av, sizeof(char *) * n);
-				if (av == NULL)
-					fprintf(stderr, "out of memory 1");
-			}
-			/*<Jan-Rixt> 25/09/2003 adaption needed for OpenBeOs*/
-			p = strtok(str," ");
-			while (p != NULL) {
-				if (*p != '\0' && (av[argc++] = strdup(p)) == NULL)
-					fprintf(stderr, "out of memory 2");
-				p = strtok(NULL, " \t");
-			}
-		}
-		if (argc > 0) {
-		av[argc] = NULL;
-			argv = av;
-		}
-	}
-	if(Vflag)
-	{
-		printf("%s\n",curses_version());
-	}
-	while ((p = *argv++)) {
+		fprintf(stderr, "no terminal type specified and no TERM environmental variable.");
+	if (tgetent(tbuf, term) != 1)
+		fprintf(stderr, "tgetent failure");
+	setospeed();
+	for (exitval = 0; (p = *argv) != NULL; ++argv) {
 		switch (*p) {
+		case 'c':
+			if (!strcmp(p, "clear"))
+				p = "cl";
+			break;
 		case 'i':
-			if (!strcmp(p, "init")) {
-				init();
-				continue;
-			}
+			if (!strcmp(p, "init"))
+				p = "is";
 			break;
 		case 'l':
 			if (!strcmp(p, "longname")) {
-				puts(longname());
+				prlongname(tbuf);
 				continue;
 			}
 			break;
 		case 'r':
-			if (!strcmp(p, "reset")) {
-				reset();
-				continue;
-			}
+			if (!strcmp(p, "reset"))
+				p = "rs";
 			break;
 		}
-
-		/* First try as terminfo */
-		if ((str = tigetstr(p)) && str != (char *)-1)
-			argv = process(p, str, argv);
-		else if ((n = tigetnum(p)) != -2)
-			(void)printf("%d\n", n);
-		else if ((n = tigetflag(p)) != -1)
-			exitval = !n;
-		/* Then fall back on termcap */
-		else if ((str = tgetstr(p, NULL)))
-			argv = process(p, str, argv);
+		cptr = buf;
+		if (tgetstr(p, &cptr))
+			argv = process(p, buf, argv);
 		else if ((n = tgetnum(p)) != -1)
 			(void)printf("%d\n", n);
-		else if ((exitval = tgetflag(p)) != 0)
-			exitval = !exitval;
-		else {
-			(void)printf("Unknown terminfo capability `%s'\n", p);
-			exitval = 4;
-		}
+		else
+			exitval = !tgetflag(p);
+
+		if (argv == NULL)
+			break;
 	}
-	exit(exitval);
+	exit(argv ? exitval : 2);
+}
+
+static void
+prlongname(buf)
+	char *buf;
+{
+	int savech;
+	char *p, *savep;
+
+	for (p = buf; *p && *p != ':'; ++p)
+		continue;
+	savech = *(savep = p);
+	for (*p = '\0'; p >= buf && *p != '|'; --p)
+		continue;
+	(void)printf("%s\n", p + 1);
+	*savep = savech;
 }
 
 static char **
-process(char *cap, char *str, char **argv)
+process(cap, str, argv)
+	char *cap, *str, **argv;
 {
-	char *cp, *s, *nargv[9];
-	int arg_need, popcount, i;
+	static const char errfew[] =
+	    "not enough arguments (%d) for capability `%s'";
+	static const char errmany[] =
+	    "too many arguments (%d) for capability `%s'";
+	static const char erresc[] =
+	    "unknown %% escape `%c' for capability `%s'";
+	char *cp;
+	int arg_need, arg_rows, arg_cols;
 
 	/* Count how many values we need for this capability. */
-	for (cp = str, arg_need = popcount = 0; *cp != '\0'; cp++) {
-		if (*cp == '%') {
-			switch (*++cp) {
-			case '%':
-			   	cp++;
-				break;
-			case 'i':
-				if (popcount < 2)
-					popcount = 2;
-				break;
-			case 'p':
-				cp++;
-				if (isdigit(cp[1]) && popcount < cp[1] - '0')
-					popcount = cp[1] - '0';
-				break;
-			case 'd':
-			case 's':
-			case '0':
-			case '1':
-			case '2':
-			case '3':
-			case '4':
-			case '5':
-			case '6':
-			case '7':
-			case '8':
-			case '9':
-			case '.':
-			case '+':
-				arg_need++;
-				break;
-			default:
-				break;
-			}
-		}
+	for (cp = str, arg_need = 0; *cp != '\0'; cp++)
+		if (*cp == '%')
+			    switch (*++cp) {
+			    case 'd':
+			    case '2':
+			    case '3':
+			    case '.':
+			    case '+':
+				    arg_need++;
+				    break;
+			    case '%':
+			    case '>':
+			    case 'i':
+			    case 'r':
+			    case 'n':
+			    case 'B':
+			    case 'D':
+				    break;
+			    default:
+				/*
+				 * hpux has lot's of them, but we complain
+				 */
+				 fprintf(stderr, erresc, *cp, cap);
+			    }
+
+	/* And print them. */
+	switch (arg_need) {
+	case 0:
+		(void)tputs(str, 1, outc);
+		break;
+	case 1:
+		arg_cols = 0;
+
+		if (*++argv == NULL || *argv[0] == '\0')
+			fprintf(stderr, errfew);
+		arg_rows = atoi(*argv);
+
+		(void)tputs(tgoto(str, arg_cols, arg_rows), 1, outc);
+		break;
+	case 2:
+		if (*++argv == NULL || *argv[0] == '\0')
+			fprintf(stderr, errfew);
+		arg_rows = atoi(*argv);
+
+		if (*++argv == NULL || *argv[0] == '\0')
+			fprintf(stderr, errfew);
+		arg_cols = atoi(*argv);
+
+		(void) tputs(tgoto(str, arg_cols, arg_rows), arg_rows, outc);
+		break;
+
+	default:
+		fprintf(stderr, errmany);
 	}
-	arg_need = MAX(arg_need, popcount);
-	if (arg_need > 9)
-		fprintf(stderr, "too many arguments (%d) for capability `%s'",
-		    arg_need, cap);
-	
-	for (i = 0; i < arg_need; i++) {
-		long l;
-
-		if (argv[i] == NULL)
-			fprintf(stderr, "not enough arguments (%d) for capability `%s'",
-			    arg_need, cap);
-
-		/* convert ascii representation of numbers to longs */
-		if (isdigit(argv[i][0]) && (l = strtol(argv[i], &cp, 10)) >= 0
-		    && l < LONG_MAX && *cp == '\0')
-			nargv[i] = (char *)l;
-		else
-			nargv[i] = argv[i];
-	}
-
-	s = tparm(str, nargv[0], nargv[1], nargv[2], nargv[3],
-	    nargv[4], nargv[5], nargv[6], nargv[7], nargv[8]);
-	putp(s);
-	fflush(stdout);
-
-	return (argv + arg_need);
+	return (argv);
 }
 
 static void
-init(void)
+setospeed()
 {
-	FILE *ifile;
-	char *buf;
-	int wstatus;
+#undef ospeed
+	extern short ospeed;
+	struct termios t;
 
-	if (init_prog /*&& !issetugid()*/) {
-		switch (fork()) {
-		case -1:
-			fprintf(stderr, "fork");
-			break;
-		case 0:
-			/* child */
-			execl(init_prog, init_prog, (char *)NULL);
-			_exit(127);
-			break;
-		default:
-			wait(&wstatus);
-			/* parent */
-			break;
-		}
-	}
-	if (init_1string)
-		putp(init_1string);
-	if (init_2string)
-		putp(init_2string);
-	set_margins();
-	/* always use 8 space tabs */
-	if (init_tabs != 8 && clear_all_tabs && set_tab) {
-		int i;
-
-		putp(clear_all_tabs);
-		for (i = 0; i < (columns - 1) / 8; i++) {
-			if (parm_right_cursor)
-				putp(tparm(parm_right_cursor, 8));
-			else
-				fputs("        ", stdout);
-			putp(set_tab);
-		}
-	}
-	buf = malloc(1024);
-	if (init_file /*&& !issetugid()*/ && (ifile = fopen(init_file, "r"))) {
-		while (fgets(buf,1024,ifile) != NULL) {
-			if (buf[strlen(buf)-1] != '\n')
-				fprintf(stderr, "premature EOF reading %s", init_file);
-			buf[strlen(buf)-1] = '\0';
-			putp(buf);
-		}
-		fclose(ifile);
-	}
-	if (init_3string)
-		putp(init_3string);
-	fflush(stdout);
-}
-
-static void
-reset(void)
-{
-	FILE *rfile;
-	char *buf;
-
-	if (reset_1string)
-		putp(reset_1string);
-	if (reset_2string)
-		putp(reset_2string);
-	set_margins();
-	buf = malloc(1024);
-	if (reset_file /*&& !issetugid()*/ && (rfile = fopen(reset_file, "r"))) {
-		while (fgets(buf,1024,rfile) != NULL) {
-			if (buf[strlen(buf)-1] != '\n')
-				fprintf(stderr, "premature EOF reading %s", reset_file);
-			buf[strlen(buf)-1] = '\0';
-			putp(buf);
-		}
-		fclose(rfile);
-	}
-	if (reset_3string)
-		putp(reset_3string);
-	fflush(stdout);
-}
-
-static void
-set_margins(void)
-{
-
-	/*
-	 * Four possibilities:
-	 *	1) we have set_lr_margin and can set things with one call
-	 *	2) we have set_{left,right}_margin_parm, use two calls
-	 *	3) we have set_{left,right}_margin, set based on position
-	 *	4) none of the above, leave things the way they are
-	 */
-	if (set_lr_margin) {
-		putp(tparm(set_lr_margin, 0, columns - 1));
-	} else if (set_left_margin_parm && set_right_margin_parm) {
-		putp(tparm(set_left_margin_parm, 0));
-		putp(tparm(set_right_margin_parm, columns - 1));
-	} else if (set_left_margin && set_right_margin && clear_margins) {
-		putp(clear_margins);
-
-		/* go to column 0 and set the left margin */
-		putp(carriage_return ? carriage_return : "\r");
-		putp(set_left_margin);
-
-		/* go to last column and set the right margin */
-		if (parm_right_cursor)
-			putp(tparm(parm_right_cursor, columns - 1));
-		else
-			printf("%*s", columns - 1, " ");
-		putp(set_right_margin);
-		putp(carriage_return ? carriage_return : "\r");
-	}
-	fflush(stdout);
-}
-
-static void
-usage(void)
-{
-
-	if (strcmp(__progname, "clear") == 0)
-		(void)fprintf(stderr, "usage: %s [-T term]\n", __progname);
+	if (tcgetattr(STDOUT_FILENO, &t) != -1)
+		ospeed = 0;
 	else
-		(void)fprintf(stderr,
-		    "usage: %s [-T term] attribute [attribute-args] ...\n"
-		    "       %s [-T term] -S\n", __progname, __progname);
+		ospeed = cfgetospeed(&t);
+}
+
+static int
+outc(c)
+	int c;
+{
+	return (putchar(c));
+}
+
+static void
+usage()
+{
+	(void)fprintf(stderr, "usage: tput [-T term] attribute ...\n");
 	exit(1);
 }
