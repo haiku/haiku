0fc073341c8074816b8e060bbf6b3b25e8b76332
Axel Dörfler <axeld@pinc-software.de>
2004-02-23 02:18:32 +0000

    Renamed CHECK_USER_ADDRESS to IS_USER_ADDRESS.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@6675 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/util/syscall_args.h
+++ headers/private/kernel/util/syscall_args.h
@@ -5,7 +5,7 @@
 
 #include <vm.h>
 
-// Hack to be able to use the CHECK_USER_ADDRESS macro when compiling for R5.
+// Hack to be able to use the IS_USER_ADDRESS macro when compiling for R5.
 #ifdef R5_MEMORY_LAYOUT
 #	undef KERNEL_BASE
 #	define KERNEL_BASE 0x0
@@ -17,7 +17,7 @@ inline
 status_t
 copy_ref_var_from_user(T *user, T &kernel)
 {
-	if (!CHECK_USER_ADDRESS(user))
+	if (!IS_USER_ADDRESS(user))
 		return B_BAD_ADDRESS;
 	return user_memcpy(&kernel, user, sizeof(T));
 }
@@ -28,7 +28,7 @@ inline
 status_t
 copy_ref_var_to_user(T &kernel, T *user)
 {
-	if (!CHECK_USER_ADDRESS(user))
+	if (!IS_USER_ADDRESS(user))
 		return B_BAD_ADDRESS;
 	return user_memcpy(user, &kernel, sizeof(T));
 }
