d5b989f7ffbaf2d209a5f1ce5c6e355ff5213f15
Matthew Wilber <mwilber@nowhere.fake>
2004-02-28 19:17:11 +0000

    Changed to use version macros
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@6795 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kits/translation/TranslatorRoster.cpp
+++ src/kits/translation/TranslatorRoster.cpp
@@ -329,9 +329,9 @@ BTranslatorRoster::Version(int32 *outCurVersion, int32 *outMinVersion,
 	static char vDate[] = __DATE__;
 	if (!vString[0]) {
 		sprintf(vString, "Translation Kit v%d.%d.%d %s\n",
-			static_cast<int>(B_TRANSLATION_CURRENT_VERSION >> 8),
-			static_cast<int>((B_TRANSLATION_CURRENT_VERSION >> 4) & 0xf),
-			static_cast<int>(B_TRANSLATION_CURRENT_VERSION & 0xf),
+			static_cast<int>(TK_MAJOR_VERSION(B_TRANSLATION_CURRENT_VERSION)),
+			static_cast<int>(TK_MINOR_VERSION(B_TRANSLATION_CURRENT_VERSION)),
+			static_cast<int>(TK_REVISION_VERSION(B_TRANSLATION_CURRENT_VERSION)),
 			vDate);
 	}
 	*outCurVersion = B_TRANSLATION_CURRENT_VERSION;
