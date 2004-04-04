0029e79802e07adb1863a2cde1c69275c038039e
Adi Oanca <adioanca@nowhere.fake>
2004-04-04 20:32:23 +0000

    added a small but important comment
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@7163 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/servers/app/server/ServerBitmap.cpp
+++ src/servers/app/server/ServerBitmap.cpp
@@ -42,6 +42,8 @@ ServerBitmap::ServerBitmap(BRect rect,color_space space, int32 flags,
 	_initialized=false;
 
 	_area=B_ERROR;
+//!!! WARNING !!! '1' is added to the width and height. Same is done in FBBitmap
+// subclass, so if you modify here make sure to do the same under FBBitmap::SetSize(...)
 	_width=rect.IntegerWidth()+1;
 	_height=rect.IntegerHeight()+1;
 	_space=space;
