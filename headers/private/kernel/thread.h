54327b01b3eacb0dbe14defebaa947b5ebfc6ade
Axel Dörfler <axeld@pinc-software.de>
2004-04-06 21:30:06 +0000

    Added prototype for _user_rename_thread().
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@7178 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/thread.h
+++ headers/private/kernel/thread.h
@@ -54,6 +54,7 @@ thread_id spawn_kernel_thread_etc(thread_func, const char *name, int32 priority,
 
 // used in syscalls.c
 status_t _user_set_thread_priority(thread_id thread, int32 newPriority);
+status_t _user_rename_thread(thread_id thread, const char *name);
 status_t _user_suspend_thread(thread_id thread);
 status_t _user_resume_thread(thread_id thread);
 thread_id _user_spawn_thread(thread_func func, const char *name, int32 priority, void *arg1, void *arg2);
