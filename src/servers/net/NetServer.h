64002c3e10080809184953380d08a2bdb96fd8f0
Waldemar Kornewald <wkornew@nowhere.fake>
2004-04-08 11:13:14 +0000

    Thank you, Jack Burton, for telling me. Sorry. Did not checkin this file. BTW, this is the first checkin with our own netstack and ppp implementation!
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@7183 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/servers/net/NetServer.h
@@ -0,0 +1,15 @@
+//-----------------------------------------------------------------------
+//  This software is part of the OpenBeOS distribution and is covered 
+//  by the OpenBeOS license.
+//
+//  Copyright (c) 2004 Waldemar Kornewald, Waldemar.Kornewald@web.de
+//-----------------------------------------------------------------------
+
+#ifndef _NET_SERVER__H
+#define _NET_SERVER__H
+
+
+#define NET_SERVER_SIGNATURE	"application/x-obos.net_server"
+
+
+#endif
