46df41d103f319d154279f084f89ab983d57a2a0
Axel Dörfler <axeld@pinc-software.de>
2004-04-18 23:58:26 +0000

    Implemented call_bios() function that is called from protected mode. It switches to real mode for execution of the BIOS interrupt; it's really only intended for the boot loader.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@7233 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/kernel/boot/platform/bios_ia32/bios.h
@@ -0,0 +1,32 @@
+/*
+** Copyright 2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+** Distributed under the terms of the OpenBeOS License.
+*/
+#ifndef BIOS_H
+#define BIOS_H
+
+
+#include <SupportDefs.h>
+
+
+// The values in this structure are passed to the BIOS call, and
+// are updated to the register contents after that call.
+
+struct bios_regs {
+	uint32	eax;
+	uint32	ebx;
+	uint32	ecx;
+	uint32	edx;
+	uint32	esi;
+	uint32	edi;
+	uint16	es;
+	uint16	flags;
+};
+
+extern
+#ifdef __cplusplus
+"C"
+#endif
+void call_bios(uint8 num, struct bios_regs *regs);
+
+#endif	/* BIOS_H */
