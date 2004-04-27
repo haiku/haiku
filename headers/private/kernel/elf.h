eaa9132647e70f179aa8489d23f039fe72c0d016
Axel Dörfler <axeld@pinc-software.de>
2004-04-27 11:54:00 +0000

    Changed some function return codes to return a status_t where appropriate.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@7330 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/elf.h
+++ headers/private/kernel/elf.h
@@ -14,12 +14,12 @@
 extern "C" {
 #endif
 
-int elf_load_uspace(const char *path, struct team *t, int flags, addr *entry);
+status_t elf_load_uspace(const char *path, struct team *t, int flags, addr *entry);
 image_id elf_load_kspace(const char *path, const char *sym_prepend);
-int elf_unload_kspace(const char *path);
-addr elf_lookup_symbol(image_id id, const char *symbol);
-int elf_lookup_symbol_address(addr address, addr *baseAddress, char *text, size_t length);
-int elf_init(kernel_args *ka);
+status_t elf_unload_kspace(const char *path);
+addr_t elf_lookup_symbol(image_id id, const char *symbol);
+status_t elf_lookup_symbol_address(addr address, addr *baseAddress, char *text, size_t length);
+status_t elf_init(kernel_args *ka);
 
 #ifdef __cplusplus
 }
