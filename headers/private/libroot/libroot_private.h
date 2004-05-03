d6f96fbff910e0d315a42cbc839b3a7928e9c978
Axel Dörfler <axeld@pinc-software.de>
2004-05-03 12:42:43 +0000

    Added a header that contains private libroot definitions.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@7371 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/libroot/libroot_private.h
@@ -0,0 +1,13 @@
+#ifndef LIBROOT_PRIVATE_H
+#define LIBROOT_PRIVATE_H
+/*
+** Copyright 2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+** Distributed under the terms of the OpenBeOS License.
+*/
+
+
+extern char _single_threaded;
+	/* This determines if a process runs single threaded or not */
+
+
+#endif	/* LIBROOT_PRIVATE_H */
