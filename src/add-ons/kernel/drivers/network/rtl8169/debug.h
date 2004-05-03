45f9110d5c1645f9de2c9b8fe59dbfeb76fdcc24
beveloper <beveloper@nowhere.fake>
2004-05-03 21:42:05 +0000

    added TRACE and ERROR macro, removed LOG macro
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@7381 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/kernel/drivers/network/rtl8169/debug.h
+++ src/add-ons/kernel/drivers/network/rtl8169/debug.h
@@ -1,14 +1,17 @@
+/* Realtek RTL8169 Family Driver
+ * Copyright (C) 2004 Marcus Overhagen <marcus@overhagen.de>. All rights reserved.
+ */
 #ifndef __DEBUG_H
 #define __DEBUG_H
 
 #include <KernelExport.h>
 
-#define DEBUG
-
 #ifdef DEBUG
-	#define LOG dprintf
+	#define TRACE(a...) dprintf("rtl8169: " a)
 #else
-	#define LOG(a...)
+	#define TRACE(a...)
 #endif
+
+#define ERROR(a...) dprintf("rtl8169: " a)
 
 #endif
