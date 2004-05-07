6e5cac2da6bce0c54a2d31080864112dd47ce27b
Axel Dörfler <axeld@pinc-software.de>
2004-05-07 00:36:44 +0000

    Private exported kernel functions of the device manager.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@7439 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/kernel/kdevice_manager.h
@@ -0,0 +1,14 @@
+/*
+** Copyright 2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+** Distributed under the terms of the OpenBeOS License.
+*/
+#ifndef _KERNEL_DEVICE_MANAGER_H
+#define _KERNEL_DEVICE_MANAGER_H
+
+
+#include <SupportDefs.h>
+
+
+extern status_t device_manager_init(struct kernel_args *args);
+
+#endif	/* _KRENEL_DEVICE_MANAGER_H */
