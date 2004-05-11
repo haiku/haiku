d81a053b902bfb7292384c4da88a8e87e7accaa2
Axel Dörfler <axeld@pinc-software.de>
2004-05-11 19:54:10 +0000

    Bye, bye elf_lookup_symbol(), hello get_image_symbol(). Also renamed elf_load_kspace() to load_kernel_add_on(), and friends. The new unload_kernel_add_on() now has an image_id as parameter, no longer a path (like the former elf_unload_kspace()).
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@7529 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/elf.h
+++ headers/private/kernel/elf.h
@@ -14,12 +14,14 @@
 extern "C" {
 #endif
 
-status_t elf_load_uspace(const char *path, struct team *t, int flags, addr *entry);
-image_id elf_load_kspace(const char *path, const char *sym_prepend);
-status_t elf_unload_kspace(const char *path);
-addr_t elf_lookup_symbol(image_id id, const char *symbol);
-status_t elf_lookup_symbol_address(addr address, addr *baseAddress, char *text, size_t length);
-status_t elf_init(kernel_args *ka);
+status_t elf_load_user_image(const char *path, struct team *team, int flags, addr_t *_entry);
+
+// these two might get public one day:
+image_id load_kernel_add_on(const char *path);
+status_t unload_kernel_add_on(image_id id);
+
+status_t elf_lookup_symbol_address(addr_t address, addr_t *baseAddress, char *text, size_t length);
+status_t elf_init(kernel_args *args);
 
 #ifdef __cplusplus
 }
