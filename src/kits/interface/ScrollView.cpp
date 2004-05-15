8c15126aec6b80f50b38b665b40c167dcba5e28a
DarkWyrm <darkwyrm@gmail.com>
2004-05-15 17:19:30 +0000

    Replaced header with standard one Investigated Axel's TODO concerning SetBorderHighlighted and converted the #if 0 to a regular C-style comment.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@7590 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kits/interface/ScrollView.cpp
+++ src/kits/interface/ScrollView.cpp
@@ -1,8 +1,28 @@
-/* 
-** Copyright 2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
-** Distributed under the terms of the OpenBeOS License.
-*/
-
+//------------------------------------------------------------------------------
+//	Copyright (c) 2001-2002, OpenBeOS
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a
+//	copy of this software and associated documentation files (the "Software"),
+//	to deal in the Software without restriction, including without limitation
+//	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//	and/or sell copies of the Software, and to permit persons to whom the
+//	Software is furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//	DEALINGS IN THE SOFTWARE.
+//
+//	File Name:		ScrollView.cpp
+//	Author:			Axel Dörfler <axeld@pinc-software.de>
+//	Description:	BView with two scrollbars
+//------------------------------------------------------------------------------
 
 #include <ScrollView.h>
 #include <Message.h>
@@ -300,14 +320,14 @@ BScrollView::SetBorderHighlighted(bool state)
 
 	fHighlighted = state;
 
-	// ToDo: The BeBook describes something like this:
-#if 0
+	/* The BeBook describes something like this:
+	
 	if (LockLooper()) {
 		Draw(Bounds());
 		UnlockLooper();
 	}
-#endif
-
+	*/
+	
 	// but this is much cleaner, I think:
 	BRect bounds = Bounds();
 	Invalidate(BRect(bounds.left, bounds.top, bounds.right, bounds.top));
