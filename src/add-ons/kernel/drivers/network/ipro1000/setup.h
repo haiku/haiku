bf3a0dce489fea0b70f6b4ec49ddcffdd755028e
beveloper <beveloper@nowhere.fake>
2004-05-29 23:20:15 +0000

    incremented version number to 0.2, the driver is working, even though not SMP save
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@7685 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/kernel/drivers/network/ipro1000/setup.h
+++ src/add-ons/kernel/drivers/network/ipro1000/setup.h
@@ -19,7 +19,9 @@
 #ifndef __SETUP_H
 #define __SETUP_H
 
-#define INFO1 "ipro1000: Intel PRO/1000 Family Driver. Version 0.1 Build " __DATE__ " "__TIME__
+#define VERSION "0.2"
+
+#define INFO1 "ipro1000: Intel PRO/1000 Family Driver. Version " VERSION " Build " __DATE__ " "__TIME__
 #define INFO2 "ipro1000: Copyright (c) 2001-2003, Intel Corporation. All rights reserved."
 #define INFO3 "ipro1000: Copyright (c) 2004 Marcus Overhagen. All rights reserved."
 
