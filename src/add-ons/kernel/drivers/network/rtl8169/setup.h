06cfaa56e109ab7f13f4ee061f17c745252eb4a9
beveloper <beveloper@nowhere.fake>
2004-05-29 23:29:48 +0000

    Incremented Version number to 1.2. Added the RTL8110 chipset the the info banner.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@7687 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/kernel/drivers/network/rtl8169/setup.h
+++ src/add-ons/kernel/drivers/network/rtl8169/setup.h
@@ -19,7 +19,9 @@
 #ifndef __SETUP_H
 #define __SETUP_H
 
-#define INFO1 "rtl8169: Realtek RTL8169 Family Driver. Version 1.0"
+#define VERSION "1.2"
+
+#define INFO1 "rtl8169: Realtek RTL8169 and RTL8110 Family Driver. Version " VERSION
 #define INFO2 "rtl8169: Copyright (C) 2004 Marcus Overhagen <marcus@overhagen.de>"
 #define INFO3 "rtl8169: All rights reserved. Build " __DATE__ " "__TIME__
 
