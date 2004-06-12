5b4e9da884e134905f8e46f35efdf636ca007302
shatty <shatty@nowhere.fake>
2004-06-12 23:10:22 +0000

    libogg-1.1
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@7945 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/media/plugins/ogg/libogg/config_types.h
+++ src/add-ons/media/plugins/ogg/libogg/config_types.h
@@ -3,6 +3,7 @@
 
 /* these are filled in by configure */
 typedef int16_t ogg_int16_t;
+typedef uint16_t ogg_uint16_t;
 typedef int32_t ogg_int32_t;
 typedef uint32_t ogg_uint32_t;
 typedef int64_t ogg_int64_t;
