746bb367c0fea0b869c7b6c5a48c724f5774fa25
Axel Dörfler <axeld@pinc-software.de>
2004-06-17 13:22:05 +0000

    Replaced "addr" with "addr_t", improved other types.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@8028 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/vm_translation_map.h
+++ headers/private/kernel/vm_translation_map.h
@@ -25,17 +25,17 @@ typedef struct vm_translation_map_struct {
 // table of operations the vm may want to do to this mapping
 typedef struct vm_translation_map_ops_struct {
 	void (*destroy)(vm_translation_map *);
-	int (*lock)(vm_translation_map*);
-	int (*unlock)(vm_translation_map*);
-	int (*map)(vm_translation_map *map, addr va, addr pa, unsigned int attributes);
-	int (*unmap)(vm_translation_map *map, addr start, addr end);
-	int (*query)(vm_translation_map *map, addr va, addr *out_physical, unsigned int *out_flags);
-	addr (*get_mapped_size)(vm_translation_map*);
-	int (*protect)(vm_translation_map *map, addr base, addr top, unsigned int attributes);
-	int (*clear_flags)(vm_translation_map *map, addr va, unsigned int flags);
+	status_t (*lock)(vm_translation_map*);
+	status_t (*unlock)(vm_translation_map*);
+	status_t (*map)(vm_translation_map *map, addr_t va, addr_t pa, uint32 attributes);
+	status_t (*unmap)(vm_translation_map *map, addr_t start, addr_t end);
+	status_t (*query)(vm_translation_map *map, addr_t va, addr_t *_outPhysical, uint32 *_outFlags);
+	addr_t (*get_mapped_size)(vm_translation_map*);
+	status_t (*protect)(vm_translation_map *map, addr_t base, addr_t top, uint32 attributes);
+	status_t (*clear_flags)(vm_translation_map *map, addr_t va, uint32 flags);
 	void (*flush)(vm_translation_map *map);
-	int (*get_physical_page)(addr physical_address, addr *out_virtual_address, int flags);
-	int (*put_physical_page)(addr virtual_address);
+	status_t (*get_physical_page)(addr_t physical_address, addr_t *out_virtual_address, uint32 flags);
+	status_t (*put_physical_page)(addr_t virtual_address);
 } vm_translation_map_ops;
 
 // ToDo: fix this
