3ae898da8b0d5deb0bfc83381103df507a8793fa
Axel Dörfler <axeld@pinc-software.de>
2004-06-17 12:22:10 +0000

    Fixed the PPC boot loader build and added empty stub routines.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@8022 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/kernel/boot/platform/openfirmware/video.cpp
@@ -0,0 +1,22 @@
+/*
+** Copyright 2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+** Distributed under the terms of the OpenBeOS License.
+*/
+
+
+#include <boot/platform.h>
+
+
+extern "C" void
+platform_switch_to_logo(void)
+{
+	// ToDo: implement me
+}
+
+
+extern "C" void
+platform_switch_to_text_mode(void)
+{
+	// ToDo: implement me
+}
+
