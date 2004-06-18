fc17fe17eb5bb93f10cb6d480dcfe970f2df7b95
Axel Dörfler <axeld@pinc-software.de>
2004-06-18 02:48:53 +0000

    Implemented the BIOS functions to access the keyboard buffer.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@8035 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/kernel/boot/platform/bios_ia32/keyboard.cpp
@@ -0,0 +1,76 @@
+/*
+** Copyright 2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+** Distributed under the terms of the OpenBeOS License.
+*/
+
+
+#include "keyboard.h"
+#include "bios.h"
+
+#include <boot/platform.h>
+
+
+/** Note, checking for keys doesn't seem to work in graphics
+ *	mode, at least in Bochs.
+ */
+
+static uint16
+check_for_key(void)
+{
+	bios_regs regs;
+	regs.eax = 0x0100;
+	call_bios(0x16, &regs);
+
+	// the zero flag is set when there is no key stroke waiting for us
+	if (regs.flags & ZERO_FLAG)
+		return 0;
+
+	// remove the key from the buffer
+	regs.eax = 0;
+	call_bios(0x16, &regs);
+
+	return regs.eax & 0xffff;
+}
+
+
+extern "C" union key
+wait_for_key(void)
+{
+	bios_regs regs;
+	regs.eax = 0;
+	call_bios(0x16, &regs);
+
+	union key key;
+	key.ax = regs.eax & 0xffff;
+
+	return key;
+}
+
+
+extern "C" uint32
+check_for_boot_keys(void)
+{
+	union key key;
+	uint32 options = 0;
+
+	while ((key.ax = check_for_key()) != 0) {
+		dprintf("pressed %lx\n", key.ax);
+		switch (key.code.ascii) {
+			case ' ':
+				options |= BOOT_OPTION_MENU;
+				break;
+			case 0x1b:	// escape
+				options |= BOOT_OPTION_DEBUG_OUTPUT;
+				break;
+			case 0:
+				// evaluate BIOS scan codes
+				// ...
+				break;
+		}
+	}
+
+	dprintf("options = %ld\n", options);
+	panic("\n");
+	return options;
+}
+
