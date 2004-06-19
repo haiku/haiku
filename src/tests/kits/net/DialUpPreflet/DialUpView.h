49c6a79a591c940c352ed387019fa4500309c5e3
Waldemar Kornewald <wkornew@nowhere.fake>
2004-06-19 15:48:39 +0000

    Changed license header to include the full MIT license instead of referring to the OpenBeOS license (there is no OpenBeOS anymore). Added resource definition. Exported dial-up view as static library for reuse by ppp_up.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@8063 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/tests/kits/net/DialUpPreflet/DialUpView.h
+++ src/tests/kits/net/DialUpPreflet/DialUpView.h
@@ -1,9 +1,24 @@
-//-----------------------------------------------------------------------
-//  This software is part of the OpenBeOS distribution and is covered 
-//  by the OpenBeOS license.
-//
-//  Copyright (c) 2003-2004 Waldemar Kornewald, Waldemar.Kornewald@web.de
-//-----------------------------------------------------------------------
+/* -----------------------------------------------------------------------
+ * Copyright (c) 2003-2004 Waldemar Kornewald, Waldemar.Kornewald@web.de
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ * ----------------------------------------------------------------------- */
 
 #ifndef _DIAL_UP_VIEW__H
 #define _DIAL_UP_VIEW__H
@@ -28,9 +43,12 @@ class DialUpView : public BView {
 		
 		// used by ppp_up application
 		bool SelectInterfaceNamed(const char *name);
+		bool NeedsRequest() const;
 		BView *AuthenticationView() const;
 		BView *StatusView() const;
 		BView *ConnectButton() const;
+		bool SaveSettings(BMessage *settings, BMessage *profile, bool saveTemporary);
+		bool SaveSettingsToFile();
 
 	private:
 		void GetPPPDirectories(BDirectory *settingsDirectory,
@@ -44,8 +62,6 @@ class DialUpView : public BView {
 		
 		bool LoadSettings(bool isNew);
 		void IsModified(bool *settings, bool *profile);
-		bool SaveSettings(BMessage *settings, BMessage *profile, bool saveTemporary);
-		bool SaveSettingsToFile();
 		
 		void LoadInterfaces();
 		void LoadAddons();
