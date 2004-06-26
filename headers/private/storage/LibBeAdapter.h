5c6dbc9b5e522eece94f3333ca22f898a1009d9e
Axel Dörfler <axeld@pinc-software.de>
2004-06-26 14:43:35 +0000

    Removed no longer needed adapter functions.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@8179 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/storage/LibBeAdapter.h
+++ headers/private/storage/LibBeAdapter.h
@@ -11,11 +11,6 @@ extern status_t entry_ref_to_path_adapter(dev_t device, ino_t directory,
 										  const char *name,
 										  char *buffer, size_t size);
 
-extern status_t swap_data_adapter(type_code type, void *data, size_t length,
-								  swap_action action);
-
-extern bool is_type_swapped_adapter(type_code type);
-
 #endif	// _LIB_BE_ADAPTER_H
 
 
