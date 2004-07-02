cfa218268b7a2bb720ef5e888c009f7f4839c840
Axel Dörfler <axeld@pinc-software.de>
2004-07-02 02:38:03 +0000

    Moved VFS syscall prototypes from vfs.h to syscalls.h.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@8266 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kernel/core/fs/vfs_select.c
+++ src/kernel/core/fs/vfs_select.c
@@ -1,11 +1,12 @@
 /* 
-** Copyright 2002, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
-** Distributed under the terms of the OpenBeOS License.
+** Copyright 2002-2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+** Distributed under the terms of the Haiku License.
 */
 
 
 #include <vfs.h>
 #include <fd.h>
+#include <syscalls.h>
 #include "vfs_select.h"
 
 #include <sys/select.h>
