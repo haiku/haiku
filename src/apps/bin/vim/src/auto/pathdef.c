c7bbbf672999f1e457078052d2a47719135042be
Jérôme Duval <korli@users.berlios.de>
2004-07-11 20:32:12 +0000

    change path to /etc/vim
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@8377 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/apps/bin/vim/src/auto/pathdef.c
+++ src/apps/bin/vim/src/auto/pathdef.c
@@ -2,7 +2,7 @@
 /* This file is automatically created by Makefile
  * DO NOT EDIT!  Change Makefile only. */
 #include "vim.h"
-char_u *default_vim_dir = (char_u *)"/boot/home/config/share/vim";
+char_u *default_vim_dir = (char_u *)"/etc/vim";
 char_u *default_vimruntime_dir = (char_u *)"";
 char_u *all_cflags = (char_u *)"gcc -c -I. -Iproto -DHAVE_CONFIG_H -DFEAT_GUI_BEOS    -g -O2       ";
 char_u *all_lflags = (char_u *)"gcc    -o vim -lbe -lroot      -ltermcap ";
