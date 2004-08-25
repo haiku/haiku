e13bd0e1a86918d0ea0950d305f564be130557c7
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2004-08-25 07:36:57 +0000

    Reverted to the original comment, as I had worked on an older version of this file. Took the chance to add the license header.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@8639 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/interface/clipping.h
+++ headers/private/interface/clipping.h
@@ -1,3 +1,28 @@
+//------------------------------------------------------------------------------
+//	Copyright (c) 2001-2004, Haiku, Inc.
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a
+//	copy of this software and associated documentation files (the "Software"),
+//	to deal in the Software without restriction, including without limitation
+//	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//	and/or sell copies of the Software, and to permit persons to whom the
+//	Software is furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//	DEALINGS IN THE SOFTWARE.
+//
+//	File Name:		clipping.h
+//	Author:			Stefano Ceccherini (burton666@libero.it)
+//	Description:	Helper methods to manipulate clipping_rects
+//------------------------------------------------------------------------------
 #ifndef __CLIPPING_H
 #define __CLIPPING_H
 
@@ -115,8 +140,8 @@ valid_rect(const clipping_rect &rect)
 static inline bool
 rects_intersect(const clipping_rect &rectA, const clipping_rect &rectB)
 {
-	// TODO: should we skip that check and let the caller do this
-	// kind of work ?
+	// We behave like BRect::Intersects() does:
+	// we return false if one of the two rects is not valid
 	if (!valid_rect(rectA) || !valid_rect(rectB))
 		return false;
 
