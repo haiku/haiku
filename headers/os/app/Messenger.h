5090f23182d783094a26df1413ac7ce7df047d60
Ingo Weinhold <ingo_weinhold@gmx.de>
2004-08-28 20:06:15 +0000

    Got rid of one of BMessengers friends. Poor BMessenger. ;-)
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@8692 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/app/Messenger.h
+++ headers/os/app/Messenger.h
@@ -96,7 +96,6 @@ private:
 	friend class BRoster;
 	friend class BPrivate::TRoster;
 	friend class BMessage;
-	friend class BQuery;
 	friend class Private;
 	friend inline void _set_message_reply_(BMessage *, BMessenger);
 	friend status_t swap_data(type_code, void *, size_t, swap_action);
