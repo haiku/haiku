f283d6c99e86b1b0c7ec5ce61dfccb8c9bb787bb
Axel Dörfler <axeld@pinc-software.de>
2004-09-01 14:37:38 +0000

    Added dummy strcoll() function.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@8773 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/kernel/libroot/posix/string/strcoll.c
@@ -0,0 +1,16 @@
+/*
+** Copyright 2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+** Distributed under the terms of the Haiku License.
+*/
+
+
+#include <string.h>
+
+
+int
+strcoll(const char *a, const char *b)
+{
+	// ToDo: implement me properly!
+	return strcmp(a, b);
+}
+
