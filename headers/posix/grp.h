77a6c2446ac6c56bee4df5d023e60c3f05fa5801
Axel Dörfler <axeld@pinc-software.de>
2004-09-02 01:04:55 +0000

    We now have a POSIX and BeOS compatible grp.h header.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@8789 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/posix/grp.h
@@ -0,0 +1,38 @@
+#ifndef _GRP_H_
+#define _GRP_H_
+/* 
+** Distributed under the terms of the Haiku License.
+*/
+
+
+#include <sys/types.h>
+
+
+struct group {
+	char	*gr_name;
+	char	*gr_passwd;
+	int		gr_gid;
+	char	**gr_mem;
+};
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern struct group *getgrgid(gid_t gid);
+extern struct group *getgrnam(const char *name);
+extern int getgrgid_r(gid_t gid, struct group *group, char *buffer,
+				size_t bufferSize, struct group **_result);
+extern int getgrnam_r(const char *name, struct group *group, char *buffer,
+				size_t bufferSize, struct group **_result);
+
+extern struct group *getgrent(void);
+extern void setgrent(void);
+extern void endgrent(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _GRP_H_ */
