9f67399eea050cdf8c13f4c4cb1ccbf50331aa55
Axel Dörfler <axeld@pinc-software.de>
2004-09-06 21:49:38 +0000

    Made header C++ safe. Also replaced the old "addr" type with "addr_t".
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@8866 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/vm_page.h
+++ headers/private/kernel/vm_page.h
@@ -5,23 +5,32 @@
 #ifndef _KERNEL_VM_PAGE_H
 #define _KERNEL_VM_PAGE_H
 
-#include <kernel.h>
+
 #include <vm.h>
+
 
 struct kernel_args;
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 int vm_page_init(struct kernel_args *ka);
 int vm_page_init2(struct kernel_args *ka);
 int vm_page_init_postthread(struct kernel_args *ka);
 
-int vm_mark_page_inuse(addr page);
-int vm_mark_page_range_inuse(addr start_page, addr len);
+int vm_mark_page_inuse(addr_t page);
+int vm_mark_page_range_inuse(addr_t startPage, addr_t len);
 int vm_page_set_state(vm_page *page, int state);
 
 vm_page *vm_page_allocate_page(int state);
-vm_page *vm_page_allocate_page_run(int state, addr len);
-vm_page *vm_page_allocate_specific_page(addr page_num, int state);
-vm_page *vm_lookup_page(addr page_num);
+vm_page *vm_page_allocate_page_run(int state, addr_t len);
+vm_page *vm_page_allocate_specific_page(addr_t page_num, int state);
+vm_page *vm_lookup_page(addr_t page_num);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif	/* _KERNEL_VM_PAGE_H */
