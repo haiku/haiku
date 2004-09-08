496d0bc32fe2f6aff4e9218cebafb59e3e12635d
Waldemar Kornewald <wkornew@nowhere.fake>
2004-09-08 08:45:32 +0000

    Moved uio struct to core_private.h.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@8893 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/posix/sys/uio.h
+++ headers/posix/sys/uio.h
@@ -14,25 +14,6 @@ typedef struct iovec {
 } iovec;
 
 
-enum    uio_rw { UIO_READ, UIO_WRITE };
-
-/* Segment flag values. */
-enum uio_seg {
-	UIO_USERSPACE,		/* from user data space */
-	UIO_SYSSPACE		/* from system space */
-};
-
-struct uio {
-	struct  iovec *uio_iov;		/* pointer to array of iovecs */
-	int     uio_iovcnt;			/* number of iovecs in array */
-	off_t   uio_offset;			/* offset into file this uio corresponds to */
-	size_t  uio_resid;			/* residual i/o count */
-	enum    uio_seg uio_segflg;	/* see above */
-	enum    uio_rw uio_rw;		/* see above */
-//	struct  proc *uio_procp;	/* process if UIO_USERSPACE */
-};
-
-
 #ifdef __cplusplus
 extern "C" {
 #endif
--- src/add-ons/kernel/network/core/core_private.h
+++ src/add-ons/kernel/network/core/core_private.h
@@ -2,8 +2,28 @@
 #define _CORE_PRIVATE__H
 
 
+struct iovec;
+
+enum    uio_rw { UIO_READ, UIO_WRITE };
+
+/* Segment flag values. */
+enum uio_seg {
+	UIO_USERSPACE,		/* from user data space */
+	UIO_SYSSPACE		/* from system space */
+};
+
+struct uio {
+	struct  iovec *uio_iov;		/* pointer to array of iovecs */
+	int     uio_iovcnt;			/* number of iovecs in array */
+	off_t   uio_offset;			/* offset into file this uio corresponds to */
+	size_t  uio_resid;			/* residual i/o count */
+	enum    uio_seg uio_segflg;	/* see above */
+	enum    uio_rw uio_rw;		/* see above */
+//	struct  proc *uio_procp;	/* process if UIO_USERSPACE */
+};
+
 struct ifnet;
-struct uio;
+
 
 int uiomove(char *cp, int n, struct uio *uio);
 
