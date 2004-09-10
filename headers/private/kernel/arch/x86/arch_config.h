1aaaa3689a7d0b4d7a9cee20454ce6900c0b6517
Axel Dörfler <axeld@pinc-software.de>
2004-09-10 13:08:06 +0000

    Moved some arch dependent config information to this new file (from arch_cpu.h). There is probably more that could be moved in here.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@8899 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/kernel/arch/x86/arch_config.h
@@ -0,0 +1,13 @@
+/*
+** Copyright 2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+** Distributed under the terms of the Haiku License.
+*/
+#ifndef _KERNEL_ARCH_x86_CONFIG_H
+#define _KERNEL_ARCH_x86_CONFIG_H
+
+#define FUNCTION_CALL_PARAMETER_ALIGNMENT_TYPE	uint32
+
+//#define ATOMIC_FUNCS_ARE_SYSCALLS
+//#define ATOMIC64_FUNCS_ARE_SYSCALLS
+
+#endif /* _KERNEL_ARCH_x86_CONFIG_H */
