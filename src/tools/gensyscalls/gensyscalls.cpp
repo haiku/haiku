91a479693905981194510c2bf8a6e166a62ce4c9
Ingo Weinhold <ingo_weinhold@gmx.de>
2004-09-10 23:14:18 +0000

    No need to define the types here, since arch_config.h now uses C types.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@8908 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/tools/gensyscalls/gensyscalls.cpp
+++ src/tools/gensyscalls/gensyscalls.cpp
@@ -11,13 +11,6 @@
 #include "gensyscalls.h"
 #include "gensyscalls_common.h"
 
-// We can't use SupportDefs.h to maintain portability
-typedef uint8_t uint8;
-typedef uint16_t uint16;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
-
-
 extern "C" gensyscall_syscall_info *gensyscall_get_infos(int *count);
 
 // usage
