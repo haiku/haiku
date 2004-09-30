43c1e268094e8e359b4c2acbbbf922efdd4637dd
Nathan Whitehorn <nwhitehorn@nowhere.fake>
2004-09-30 03:49:35 +0000

    Made the packaging system better, with readmes and a better install script.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@9115 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/servers/mail/install.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+base=`dirname "$0"`
+cd "$base"
+
+RETURN=`alert "There can only be ONE version of mail_daemon on the system at one time (see the enclosed README file for why).
+
+Choose 'Backup' if you wish to keep your old mail_daemon, E-mail preferences app, and BeMail, and this is the first time you're installing the Mail Daemon Replacement.  Otherwise, you should choose 'Purge' to clear the other versions from your system." "Purge" "Backup" "Abort - Don't do anything!"`
+
+if [[ $RETURN = Purge ]]
+then
+	# note: we don't remove libmail.so, because it doesn't matter here, and there may be symlinks and things
+	query -a 'BEOS:APP_SIG == "application/x-vnd.Be-POST" || BEOS:APP_SIG == "application/x-vnd.Be-mprf" || BEOS:APP_SIG == "application/x-vnd.Be-MAIL" || BEOS:APP_SIG == "application/x-vnd.agmsmith.AGMSBayesianSpamServer"' | grep -v "`/bin/pwd`" | xargs rm -f
+elif [[ $RETURN = Backup ]]
+then
+	query -a 'BEOS:APP_SIG == "application/x-vnd.Be-POST" || BEOS:APP_SIG == "application/x-vnd.Be-mprf" || BEOS:APP_SIG == "application/x-vnd.Be-MAIL" || BEOS:APP_SIG == "application/x-vnd.agmsmith.AGMSBayesianSpamServer" || name == libmail.so' | grep -v "`/bin/pwd`" | xargs zip -ym /boot/home/maildaemon.zip
+else
+	alert "No backup will be done.  That means it's up to YOU to purge all of your old mail_daemons and ensure that the new version is the only version."
+	exit -1
+fi
+
+if [[ `uname -m` == BePC ]] && test ! -e ~/config/lib/libssl.so; then
+
+RETURN=`alert "You don't seem to have OpenSSL installed, which is required for IMAP." "Get OpenSSL" "I Don't Care"`
+
+if [[ $RETURN = "Get OpenSSL" ]]
+then
+	NetPositive http://www.bebits.com/app/1020 &
+fi
+
+fi
+
+quit "application/x-vnd.Be-POST"
+quit "application/x-vnd.Be-TSKB"
+
+if [ -n "$TTY" ]
+then
+    unzip -d / install.zip
+else
+    response=`alert "Would you like to automatically overwrite existing files, or receive a prompt?" "Overwrite" "Prompt"`
+    if [ $response == "Overwrite" ]
+    then
+        unzip -od / install.zip
+        alert "Finished installing" "Thanks"
+    else
+        if [ -e /boot/beos/apps/Terminal ]
+        then
+            terminal=/boot/beos/apps/Terminal
+        else
+            terminal=`query Terminal | head -1`
+        fi
+        $terminal -t "installer" /bin/sh "$0"
+    fi
+fi
