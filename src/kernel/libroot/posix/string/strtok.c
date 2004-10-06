3451283511f4f60327720a3495c9dc918dea09e0
Axel Dörfler <axeld@pinc-software.de>
2004-10-06 00:14:41 +0000

    Minor cleanup.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@9221 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kernel/libroot/posix/string/strtok.c
+++ src/kernel/libroot/posix/string/strtok.c
@@ -3,10 +3,13 @@
 ** Distributed under the terms of the NewOS License.
 */
 
+
 #include <sys/types.h>
 #include <string.h>
 
+
 static char *___strtok = NULL;
+
 
 char *
 strtok_r(char *s, char const *ct, char **save_ptr)
@@ -17,15 +20,16 @@ strtok_r(char *s, char const *ct, char **save_ptr)
 		return NULL;
 
 	sbegin  = s ? s : *save_ptr;
-	if (!sbegin) {
+	if (!sbegin)
 		return NULL;
-	}
-	sbegin += strspn(sbegin,ct);
+
+	sbegin += strspn(sbegin, ct);
 	if (*sbegin == '\0') {
 		if (save_ptr)
 			*save_ptr = NULL;
 		return NULL;
 	}
+
 	send = strpbrk(sbegin, ct);
 	if (send && *send != '\0')
 		*send++ = '\0';
@@ -34,6 +38,7 @@ strtok_r(char *s, char const *ct, char **save_ptr)
 
 	return sbegin;
 }
+
 
 char *
 strtok(char *s, char const *ct)
