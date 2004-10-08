1d60efac7259498a3f2a1b0d0cb373863d2265eb
Axel Dörfler <axeld@pinc-software.de>
2004-10-08 15:07:57 +0000

    Added a prototype for vm_cache_resize().
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@9258 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/vm_cache.h
+++ headers/private/kernel/vm_cache.h
@@ -16,16 +16,17 @@ struct kernel_args;
 extern "C" {
 #endif
 
-int vm_cache_init(struct kernel_args *ka);
+status_t vm_cache_init(struct kernel_args *ka);
 vm_cache *vm_cache_create(vm_store *store);
 vm_cache_ref *vm_cache_ref_create(vm_cache *cache);
 void vm_cache_acquire_ref(vm_cache_ref *cache_ref, bool acquire_store_ref);
 void vm_cache_release_ref(vm_cache_ref *cache_ref);
-vm_page *vm_cache_lookup_page(vm_cache_ref *cache_ref, off_t page);
-void vm_cache_insert_page(vm_cache_ref *cache_ref, vm_page *page, off_t offset);
-void vm_cache_remove_page(vm_cache_ref *cache_ref, vm_page *page);
-int vm_cache_insert_region(vm_cache_ref *cache_ref, vm_region *region);
-int vm_cache_remove_region(vm_cache_ref *cache_ref, vm_region *region);
+vm_page *vm_cache_lookup_page(vm_cache_ref *cacheRef, off_t page);
+void vm_cache_insert_page(vm_cache_ref *cacheRef, vm_page *page, off_t offset);
+void vm_cache_remove_page(vm_cache_ref *cacheRef, vm_page *page);
+status_t vm_cache_resize(vm_cache_ref *cacheRef, size_t newSize);
+status_t vm_cache_insert_region(vm_cache_ref *cacheRef, vm_region *region);
+status_t vm_cache_remove_region(vm_cache_ref *cacheRef, vm_region *region);
 
 #ifdef __cplusplus
 }
