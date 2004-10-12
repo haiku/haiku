709d033403632d2fd171c62abd80b230572e5b88
Axel Dörfler <axeld@pinc-software.de>
2004-10-12 16:26:44 +0000

    Added prototypes for __init_image() and __init_dlfcn() so that they don't have to be defined in the source files anymore.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@9302 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/libroot/libroot_private.h
+++ headers/private/libroot/libroot_private.h
@@ -2,8 +2,14 @@
 #define LIBROOT_PRIVATE_H
 /*
 ** Copyright 2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
-** Distributed under the terms of the OpenBeOS License.
+** Distributed under the terms of the Haiku License.
 */
+
+
+struct uspace_program_args;
+
+void __init_image(const struct uspace_program_args *);
+void __init_dlfcn(const struct uspace_program_args *);
 
 
 extern char _single_threaded;
