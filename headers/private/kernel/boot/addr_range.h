53ac60d4ea9dcc99198ad5a7fdd839c4a1afa0ed
Axel Dörfler <axeld@pinc-software.de>
2004-10-19 16:57:19 +0000

    Added the prototype for insert_address_range() that can be used for the various addr_range arrays in the kernel_args structure.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@9412 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/boot/addr_range.h
+++ headers/private/kernel/boot/addr_range.h
@@ -1,6 +1,6 @@
-/* 
+/*
 ** Copyright 2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
-** Distributed under the terms of the OpenBeOS License.
+** Distributed under the terms of the Haiku License.
 */
 #ifndef KERNEL_BOOT_ADDR_RANGE_H
 #define KERNEL_BOOT_ADDR_RANGE_H
@@ -13,5 +13,11 @@ typedef struct addr_range {
 	addr_t start;
 	addr_t size;
 } addr_range;
+
+#ifdef __cplusplus
+extern "C"
+#endif
+status_t insert_address_range(addr_range *ranges, uint32 *_numRanges, uint32 maxRanges,
+	addr_t start, uint32 size);
 
 #endif	/* KERNEL_BOOT_ADDR_RANGE_H */
