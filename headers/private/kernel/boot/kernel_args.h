107f1a6a3f5ae5b71f6c4dac2dd84e6f3d023a89
Axel Dörfler <axeld@pinc-software.de>
2004-10-19 17:02:11 +0000

    Added an addr_range array for kernel_args. Removed the "str" field I have no idea what it could have been useful for. Changed the frame buffer part of the kernel args - "already_mapped" is no longer there, as it doesn't make a lot of sense.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@9413 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/boot/kernel_args.h
+++ headers/private/kernel/boot/kernel_args.h
@@ -1,4 +1,7 @@
 /* 
+** Copyright 2002-2004, The Haiku Team. All rights reserved.
+** Distributed under the terms of the Haiku License.
+**
 ** Copyright 2001-2002, Travis Geiselbrecht. All rights reserved.
 ** Distributed under the terms of the NewOS License.
 */
@@ -13,14 +16,14 @@
 #include <arch_kernel_args.h>
 
 
-#define CURRENT_KERNEL_ARGS_VERSION 1
+#define CURRENT_KERNEL_ARGS_VERSION	1
+#define MAX_KERNEL_ARGS_RANGE		8
 
 typedef struct kernel_args {
 	uint32		kernel_args_size;
 	uint32		version;
 
 	uint32		cons_line;
-	char		*str;
 	addr_range	bootdir_addr;
 
 	struct preloaded_image kernel_image;
@@ -32,21 +35,22 @@ typedef struct kernel_args {
 	addr_range	physical_allocated_range[MAX_PHYSICAL_ALLOCATED_RANGE];
 	uint32		num_virtual_allocated_ranges;
 	addr_range	virtual_allocated_range[MAX_VIRTUAL_ALLOCATED_RANGE];
+	uint32		num_kernel_args_ranges;
+	addr_range	kernel_args_range[MAX_KERNEL_ARGS_RANGE];
 
 	uint32		num_cpus;
 	addr_range	cpu_kstack[MAX_BOOT_CPUS];
 
+	struct {
+		bool	enabled;
+		int32	width;
+		int32	height;
+		int32	depth;
+		addr_range physical_buffer;
+	} frame_buffer;
+
 	platform_kernel_args platform_args;
 	arch_kernel_args arch_args;
-
-	struct framebuffer {
-		int		enabled;
-		int		x_size;
-		int		y_size;
-		int		bit_depth;
-		int		already_mapped;
-		addr_range mapping;
-	} fb;
 } kernel_args;
 
 #endif	/* KERNEL_BOOT_KERNEL_ARGS_H */
