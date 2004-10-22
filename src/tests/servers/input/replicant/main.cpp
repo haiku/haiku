3f2b5033eec07763ca6fec6e4516b52abb277ee6
Jérôme Duval <korli@users.berlios.de>
2004-10-22 13:09:06 +0000

    added replicant test
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@9446 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/tests/servers/input/replicant/main.cpp
@@ -0,0 +1,49 @@
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+//
+//	Copyright (c) 2004, OpenBeOS
+//
+//  This software is part of the OpenBeOS distribution and is covered 
+//  by the OpenBeOS license.
+//
+//
+//  Program:	 desklink
+//  Author:      Jérôme DUVAL
+//  Description: MethodReplicant Test
+//  Created :    October 20, 2004
+// 
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+#include <Application.h>
+#include <Deskbar.h>
+#include <Roster.h>
+#include <stdio.h>
+#include "MethodReplicant.h"
+
+const char *app_signature = "application/x-vnd.obos.testmethodreplicant";
+// the application signature used by the replicant to find the supporting
+// code
+
+extern "C" _EXPORT BView* instantiate_deskbar_item();
+
+BView *
+instantiate_deskbar_item()
+{
+	return new MethodReplicant(app_signature);
+}
+
+int main(int argc, char **argv) 
+{	
+	BApplication app(app_signature);
+			
+	app_info info;
+	app.GetAppInfo(&info);
+		
+	status_t err = BDeskbar().AddItem(&info.ref);
+	
+	if(err!=B_OK) {
+		printf("desklink: Deskbar refuses link\n");	
+		return 1;
+	}
+		
+	return 0;
+}
