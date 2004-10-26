cc67a158ee62123f41a4ea120c2a59abde0006d9
Axel Dörfler <axeld@pinc-software.de>
2004-10-26 23:29:00 +0000

    Added function for an early boot message before anything else (because of an error, there is supposed to be one of these at maximum right now).
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@9517 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/arch/dbg_console.h
+++ headers/private/kernel/arch/dbg_console.h
@@ -1,15 +1,24 @@
 /* 
+** Copyright 2002-2004, The Haiku Team. All rights reserved.
+** Distributed under the terms of the Haiku License.
+**
 ** Copyright 2001-2002, Travis Geiselbrecht. All rights reserved.
 ** Distributed under the terms of the NewOS License.
 */
 #ifndef KERNEL_DBG_CONSOLE_H
 #define KERNEL_DBG_CONSOLE_H
 
+
+#include <SupportDefs.h>
+
+
 struct kernel_args;
 
 char arch_dbg_con_read(void);
 char arch_dbg_con_putch(char c);
 void arch_dbg_con_puts(const char *s);
-int arch_dbg_con_init(struct kernel_args *ka);
+
+void arch_dbg_con_early_boot_message(const char *string);
+status_t arch_dbg_con_init(struct kernel_args *args);
 
 #endif	/* KERNEL_DBG_CONSOLE_H */
