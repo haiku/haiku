60bbe6afcea490ffd571d797aae4a6fd4dccad10
Axel Dörfler <axeld@pinc-software.de>
2004-11-08 18:32:43 +0000

    Our file system are different, and old mount stuff does not work anymore; we do not support mount() and unmount() anymore. I've not removed that file in case we decide that we have to export those functions for whatever reason.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@9868 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kernel/libroot/posix/unistd/mount.c
+++ src/kernel/libroot/posix/unistd/mount.c
@@ -1,39 +1,30 @@
 /* 
 ** Copyright 2002-2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
-** Distributed under the terms of the OpenBeOS License.
+** Distributed under the terms of the Haiku License.
 */
 
 
-#include <unistd.h>
-#include <syscalls.h>
+#if 0
+
 #include <errno.h>
+#include <sys/types.h>
 
-
-#define RETURN_AND_SET_ERRNO(err) \
-	if (err < 0) { \
-		errno = err; \
-		return -1; \
-	} \
-	return err;
-
+int mount(const char *filesystem, const char *where, const char *device, ulong flags, void *parms, int len);
+int unmount(const char *path);
 
 int 
 mount(const char *filesystem, const char *where, const char *device, ulong flags, void *parms, int len)
 {
-	// ToDo: consider parsing "parms" string in userland
-	int status = _kern_mount(where, device, filesystem, flags, parms);
-
-	(void)len;
-
-	RETURN_AND_SET_ERRNO(status);
+	errno = EOPNOTSUPP;
+	return -1;
 }
 
 
 int 
 unmount(const char *path)
 {
-	int status = _kern_unmount(path);
-
-	RETURN_AND_SET_ERRNO(status);
+	errno = EOPNOTSUPP;
+	return -1;
 }
 
+#endif
