8a3a4e98dc8d7bcbe46f381477825756d394b5bd
Nathan Whitehorn <nwhitehorn@nowhere.fake>
2004-11-12 18:53:45 +0000

    Added sanity to the C++ ABI hack. If we aren't using GCC 2 or mwcc, we clearly don't care about binary compatibility, so there is no point in using the BC shim.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@9943 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kits/mail/cpp_abi_base64.c
+++ src/kits/mail/cpp_abi_base64.c
@@ -3,13 +3,14 @@
 #if __MWERKS__
 	#define encode_base64__local_abi encode_base64__FPcPcx
 	#define decode_base64__local_abi decode_base64__FPcPcxb
+	#define USETHISFILEATALL=1
 #elif __GNUC__ <= 2
 	#define encode_base64__local_abi encode_base64__FPcT0x
 	#define decode_base64__local_abi decode_base64__FPcT0xb
-#else
-	#error "We don't seem to have a C++ ABI hack for your compiler. Please add one."
+	#define USETHISFILEATALL=1	
 #endif
 
+#if USETHISFILEATALL /* If we are using GCC >= 3 or something else, we clearly have given up on binary compat anyway */
 
 ssize_t encode_base64__local_abi(char *out, char *in, off_t length);
 ssize_t decode_base64__local_abi(char *out, char *in, off_t length, char);
@@ -22,4 +23,6 @@ _EXPORT ssize_t decode_base64__local_abi(char *out, char *in, off_t length, char
 	nothing = '\0';
 	return decode_base64(out,in,length);
 }
+
+#endif
 
