b5027c0e2b4d18123ed1a14ae382f84e8b6f5cff
Jérôme Duval <korli@users.berlios.de>
2004-11-16 11:24:35 +0000

    added ctime_r
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@9975 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kernel/libroot/posix/time/ctime.c
+++ src/kernel/libroot/posix/time/ctime.c
@@ -12,3 +12,13 @@ ctime(const time_t *_timer)
 {
 	return asctime(localtime(_timer));
 }
+
+
+char *
+ctime_r(const time_t *_timer, char *buf)
+{
+	struct tm	tm;
+	
+	return asctime_r(localtime_r(_timer, &tm), buf);
+}
+
