b3c1f2c654cd046b48e5d950f21653a78a70e060
Jérôme Duval <korli@users.berlios.de>
2004-11-16 00:37:26 +0000

    removed comments
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@9972 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kernel/libroot/os/time.c
+++ src/kernel/libroot/os/time.c
@@ -31,12 +31,8 @@ setup_rtc_boottime()
 		printf("setup_rtc_boottime: error getting real time data info\n");
 		return err;
 	}
-	printf("setup_rtc_boottime : %d, %p\n", dataArea, info.address);
-/*	sBootTime = &((struct real_time_data *)info.address)->boot_time;
-	printf("setup_rtc_time : %p\n", sBootTime);
+	sBootTime = &((struct real_time_data *)info.address)->boot_time;
 	return B_OK;
-*/
-	return B_ERROR;
 }
 
 
