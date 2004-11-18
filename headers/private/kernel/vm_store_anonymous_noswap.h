704d6e100b42acd4ecf709cd092584ef8c5cd939
Axel Dörfler <axeld@pinc-software.de>
2004-11-18 17:46:39 +0000

    Added stack special case to the anonymous store.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@10012 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/vm_store_anonymous_noswap.h
+++ headers/private/kernel/vm_store_anonymous_noswap.h
@@ -1,14 +1,15 @@
 /* 
-** Copyright 2001-2002, Travis Geiselbrecht. All rights reserved.
-** Distributed under the terms of the NewOS License.
-*/
+ * Copyright 2004, Axel Dörfler, axeld@pinc-software.de.
+ * Distributed under the terms of the MIT License.
+ *
+ * Copyright 2001-2002, Travis Geiselbrecht. All rights reserved.
+ * Distributed under the terms of the NewOS License.
+ */
 #ifndef _KERNEL_VM_STORE_ANONYMOUS_H
 #define _KERNEL_VM_STORE_ANONYMOUS_H
 
-#include <kernel.h>
-#include <vm.h>
+#include <vm_types.h>
 
-vm_store *vm_store_create_anonymous_noswap(void);
+vm_store *vm_store_create_anonymous_noswap(bool stack, int32 numGuardPages);
 
-#endif
-
+#endif	/* _KERNEL_VM_STORE_ANONYMOUS_H */
