04169a304b9f8c3c87eaff1a57b82cac257688c3
Axel Dörfler <axeld@pinc-software.de>
2004-11-26 16:23:49 +0000

    Added header for additional safemode definitions.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@10256 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/kernel/safemode.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _KERNEL_SAFEMODE_H
+#define _KERNEL_SAFEMODE_H
+
+
+#include <driver_settings.h>
+
+
+// these are BeOS compatible additions to the safemode
+// constants in the driver_settings.h header
+
+#define B_SAFEMODE_DISABLE_USER_ADD_ONS		"disableuseraddons"
+#define B_SAFEMODE_DISABLE_IDE_DMA			"disableidedma"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+status_t get_safemode_option(const char *parameter, char *buffer, size_t *_bufferSize);
+status_t _user_get_safemode_option(const char *parameter, char *buffer, size_t *_bufferSize);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* _KERNEL_SAFEMODE_H */
