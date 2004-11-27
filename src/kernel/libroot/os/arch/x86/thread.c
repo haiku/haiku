f05c4043796d2ce96954a945604503bec3bdcbd7
Axel Dörfler <axeld@pinc-software.de>
2004-11-27 12:54:58 +0000

    Updated to the changes made to OS.h.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@10267 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kernel/libroot/os/arch/x86/thread.c
+++ src/kernel/libroot/os/arch/x86/thread.c
@@ -1,15 +1,17 @@
 /* 
-** Copyright 2003-2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
-** Distributed under the terms of the OpenBeOS License.
-*/
+ * Copyright 2003-2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
 
 
 #include <OS.h>
 #include "syscalls.h"
 
 
-// see OS.h for the reason why we need this
-thread_id _kfind_thread_(const char *name);
+// see OS.h from BeOS R5 for the reason why we need this
+// (it's referenced there in the same way we reference _kern_find_thread())
+extern thread_id _kfind_thread_(const char *name);
+
 
 thread_id
 _kfind_thread_(const char *name)
