7028b4da5d158173d38515f143d1ef081a255eef
Axel Dörfler <axeld@pinc-software.de>
2004-11-30 18:17:59 +0000

    Cleanup, made C++ safe, updated license.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@10306 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/sem.h
+++ headers/private/kernel/sem.h
@@ -1,9 +1,13 @@
 /*
-** Copyright 2001-2002, Travis Geiselbrecht. All rights reserved.
-** Distributed under the terms of the NewOS License.
-*/
+ * Copyright 2002-2004, Axel Dörfler, axeld@pinc-software.de.
+ * Distributed under the terms of the MIT License.
+ *
+ * Copyright 2001-2002, Travis Geiselbrecht. All rights reserved.
+ * Distributed under the terms of the NewOS License.
+ */
 #ifndef KERNEL_SEM_H
 #define KERNEL_SEM_H
+
 
 #include <OS.h>
 #include <thread.h>
@@ -11,6 +15,18 @@
 
 struct kernel_args;
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern status_t sem_init(struct kernel_args *args);
+extern int sem_delete_owned_sems(team_id owner);
+extern status_t	sem_interrupt_thread(struct thread *t);
+extern int32 sem_used_sems(void);
+extern int32 sem_max_sems(void);
+
+extern sem_id create_sem_etc(int32 count, const char *name, team_id owner);
 
 /* user calls */
 sem_id _user_create_sem(int32 count, const char *name);
@@ -24,12 +40,8 @@ status_t _user_get_sem_info(sem_id, struct sem_info *, size_t);
 status_t _user_get_next_sem_info(team_id, int32 *, struct sem_info *, size_t);
 status_t _user_set_sem_owner(sem_id id, team_id team);
 
-/* kernel calls */
-extern sem_id	create_sem_etc(int32 count, const char *name, team_id owner);
-extern status_t	sem_init(struct kernel_args *ka);
-extern int		sem_delete_owned_sems(team_id owner);
-extern status_t	sem_interrupt_thread(struct thread *t);
-extern int32	sem_used_sems(void);
-extern int32	sem_max_sems(void);
+#ifdef __cplusplus
+}
+#endif
 
 #endif	/* KERNEL_SEM_H */
