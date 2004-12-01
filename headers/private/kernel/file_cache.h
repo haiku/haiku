a16bf97a7d9187877106a2939427ad611a01332f
Axel Dörfler <axeld@pinc-software.de>
2004-12-01 07:35:20 +0000

    Added preliminary support for cache modules.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@10334 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/file_cache.h
+++ headers/private/kernel/file_cache.h
@@ -1,17 +1,31 @@
 /*
-** Copyright 2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
-** Distributed under the terms of the Haiku License.
-*/
+ * Copyright 2004, Axel Dörfler, axeld@pinc-software.de.
+ * Distributed under the terms of the MIT License.
+ */
 #ifndef _KERNEL_FILE_CACHE_H
 #define _KERNEL_FILE_CACHE_H
 
 
+#include <vfs.h>
 #include <vm_types.h>
+#include <module.h>
 
+
+struct cache_module_info {
+	module_info	info;
+
+	void (*node_opened)(mount_id mountID, vnode_id vnodeID, off_t size);
+	void (*node_closed)(mount_id mountID, vnode_id vnodeID);
+};
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+extern void cache_node_opened(void *ref, mount_id mountID, vnode_id vnodeID);
+extern void cache_node_closed(void *ref, mount_id mountID, vnode_id vnodeID);
+extern void cache_prefetch(mount_id mountID, vnode_id vnodeID);
+extern status_t file_cache_init(void);
 
 extern vm_store *vm_create_vnode_store(void *vnode);
 
