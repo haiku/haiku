a32c8f261f8ffb26c0a8413e3ace8852ce80be8e
Axel Dörfler <axeld@pinc-software.de>
2004-12-01 04:14:55 +0000

    Renamed _kern_exit() to _kern_exit_team(), just to avoid any confusion. Added real _user_exit_team() syscall (it was previously incorrectly mapped to _user_exit_thread()).
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@10325 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/team.h
+++ headers/private/kernel/team.h
@@ -35,6 +35,7 @@ int32 team_used_teams(void);
 // used in syscalls.c
 thread_id _user_load_image(int32 argCount, const char **args, int32 envCount, const char **envp, int32 priority);
 status_t _user_wait_for_team(team_id id, status_t *_returnCode);
+void _user_exit_team(status_t returnValue);
 status_t _user_kill_team(thread_id thread);
 thread_id _user_wait_for_child(thread_id child, uint32 flags, int32 *_reason, status_t *_returnCode);
 status_t _user_exec(const char *path, int32 argc, char * const *argv, int32 envCount, char * const *environment);
