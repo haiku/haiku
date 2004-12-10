68b54c0db9d30c13d517a3732e3cc0fa531a1a53
Axel Dörfler <axeld@pinc-software.de>
2004-12-10 18:48:31 +0000

    Introduced new arch_vm.h header.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@10385 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/arch/vm.h
+++ headers/private/kernel/arch/vm.h
@@ -1,15 +1,16 @@
 /*
-** Copyright 2002-2004, The Haiku Team. All rights reserved.
-** Distributed under the terms of the Haiku License.
-**
-** Copyright 2001-2002, Travis Geiselbrecht. All rights reserved.
-** Distributed under the terms of the NewOS License.
-*/
+ * Copyright 2002-2004, Axel Dörfler, axeld@pinc-software.de.
+ * Distributed under the terms of the MIT License.
+ *
+ * Copyright 2001-2002, Travis Geiselbrecht. All rights reserved.
+ * Distributed under the terms of the NewOS License.
+ */
 #ifndef KERNEL_ARCH_VM_H
 #define KERNEL_ARCH_VM_H
 
 
 #include <vm.h>
+#include <arch_vm.h>
 
 
 #ifdef __cplusplus
