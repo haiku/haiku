f4160aedf922f8ab3f3720adfd19ccb844612d6a
Axel Dörfler <axeld@pinc-software.de>
2004-12-14 16:07:23 +0000

    Renamed semaphore syscalls to the new scheme.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@10454 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kernel/libroot/os/sem.c
+++ src/kernel/libroot/os/sem.c
@@ -1,79 +1,79 @@
-/* 
-** Copyright 2002, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
-** Distributed under the terms of the OpenBeOS License.
-*/
+/*
+ * Copyright 2002-2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
 
 
 #include <OS.h>
-#include "syscalls.h"
+#include <syscalls.h>
 
 
 sem_id
 create_sem(int32 count, const char *name)
 {
-	return sys_create_sem(count, name);
+	return _kern_create_sem(count, name);
 }
 
 
 status_t
 delete_sem(sem_id id)
 {
-	return sys_delete_sem(id);
+	return _kern_delete_sem(id);
 }
 
 
 status_t
 acquire_sem(sem_id id)
 {
-	return sys_acquire_sem(id);
+	return _kern_acquire_sem(id);
 }
 
 
 status_t
 acquire_sem_etc(sem_id id, int32 count, uint32 flags, bigtime_t timeout)
 {
-	return sys_acquire_sem_etc(id, count, flags, timeout);
+	return _kern_acquire_sem_etc(id, count, flags, timeout);
 }
 
 
 status_t
 release_sem(sem_id id)
 {
-	return sys_release_sem(id);
+	return _kern_release_sem(id);
 }
 
 
 status_t
 release_sem_etc(sem_id id, int32 count, uint32 flags)
 {
-	return sys_release_sem_etc(id, count, flags);
+	return _kern_release_sem_etc(id, count, flags);
 }
 
 
 status_t
 get_sem_count(sem_id sem, int32 *count)
 {
-	return sys_get_sem_count(sem, count);
+	return _kern_get_sem_count(sem, count);
 }
 
 
 status_t
 set_sem_owner(sem_id sem, team_id team)
 {
-	return sys_set_sem_owner(sem, team);
+	return _kern_set_sem_owner(sem, team);
 }
 
 
 status_t
 _get_sem_info(sem_id sem, sem_info *info, size_t size)
 {
-	return sys_get_sem_info(sem, info, size);
+	return _kern_get_sem_info(sem, info, size);
 }
 
 
 status_t
 _get_next_sem_info(team_id team, int32 *cookie, sem_info *info, size_t size)
 {
-	return sys_get_next_sem_info(team, cookie, info, size);
+	return _kern_get_next_sem_info(team, cookie, info, size);
 }
 
