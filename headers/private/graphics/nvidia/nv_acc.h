bd877ea210f7727f2719c1eb0e2781225e9657af
Rudolf Cornelissen <rudolf.cornelissen@gmail.com>
2004-12-18 13:30:33 +0000

    added file which will contain acc cmd struct defines
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@10485 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/graphics/nvidia/nv_acc.h
@@ -0,0 +1,28 @@
+/* 
+	definitions for used nVidia acceleration engine commands.
+
+	Written by Rudolf Cornelissen 12/2004
+*/
+
+#ifndef NV_ACC_H
+#define NV_ACC_H
+
+typedef struct {
+} cmd_nv_rop5_solid;
+
+typedef struct {
+} cmd_nv_image_black_rectangle;
+
+typedef struct {
+} cmd_nv_image_pattern;
+
+typedef struct {
+} cmd_nv_image_blit;
+
+typedef struct {
+} cmd_nv3_gdi_rectangle_text;
+
+typedef struct {
+} cmd_nv_dx5_texture_triangle;
+
+#endif
