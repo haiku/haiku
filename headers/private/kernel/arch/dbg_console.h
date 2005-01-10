3278ab847589fc8720ebc97c36d527894bbf089d
Axel Dörfler <axeld@pinc-software.de>
2005-01-10 07:43:48 +0000

    Made header C++ safe. Added new function prototype for arch_dbg_con_init_settings().
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@10638 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/arch/dbg_console.h
+++ headers/private/kernel/arch/dbg_console.h
@@ -1,10 +1,10 @@
 /* 
-** Copyright 2002-2004, The Haiku Team. All rights reserved.
-** Distributed under the terms of the Haiku License.
-**
-** Copyright 2001-2002, Travis Geiselbrecht. All rights reserved.
-** Distributed under the terms of the NewOS License.
-*/
+ * Copyright 2002-2005, Axel Dörfler, axeld@pinc-software.de
+ * Distributed under the terms of the MIT License.
+ *
+ * Copyright 2001-2002, Travis Geiselbrecht. All rights reserved.
+ * Distributed under the terms of the NewOS License.
+ */
 #ifndef KERNEL_DBG_CONSOLE_H
 #define KERNEL_DBG_CONSOLE_H
 
@@ -14,11 +14,20 @@
 
 struct kernel_args;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 char arch_dbg_con_read(void);
 char arch_dbg_con_putch(char c);
 void arch_dbg_con_puts(const char *s);
 
 void arch_dbg_con_early_boot_message(const char *string);
 status_t arch_dbg_con_init(struct kernel_args *args);
+status_t arch_dbg_con_init_settings(struct kernel_args *args);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif	/* KERNEL_DBG_CONSOLE_H */
