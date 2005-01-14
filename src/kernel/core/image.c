2397da28a0ad5530360bf58bc5b4c389a139ed25
Ingo Weinhold <ingo_weinhold@gmx.de>
2005-01-14 15:32:14 +0000

    Added support for get_next_image_info() with team ID == 0, which means the current team.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@10727 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kernel/core/image.c
+++ src/kernel/core/image.c
@@ -161,7 +161,10 @@ _get_next_image_info(team_id teamID, int32 *cookie, image_info *info, size_t siz
 	state = disable_interrupts();
 	GRAB_TEAM_LOCK();
 
-	team = team_get_team_struct_locked(teamID);
+	if (teamID == 0)
+		team = thread_get_current_thread()->team;
+	else
+		team = team_get_team_struct_locked(teamID);
 	if (team) {
 		struct image *image = NULL;
 		int32 count = 0;
