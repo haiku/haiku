ceba105306d70cbfa5b59bf77cd0500119ceae0d
Axel Dörfler <axeld@pinc-software.de>
2005-01-18 02:34:00 +0000

    Added function prototypes for the kernel private load_module() and unload_module() calls. Made the header C++ safe.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@10820 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/kmodule.h
+++ headers/private/kernel/kmodule.h
@@ -1,17 +1,29 @@
 /*
-** Copyright 2001-2002, Thomas Kurschel. All rights reserved.
-** Distributed under the terms of the NewOS License.
-*/
-
+ * Copyright 2005, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+ *
+ * Distributed under the terms of the MIT License.
+ */
 #ifndef _KERNEL_MODULE_H
 #define _KERNEL_MODULE_H
+
 
 #include <drivers/module.h>
 #include <kernel.h>
 
 struct kernel_args;
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern status_t unload_module(const char *path);
+extern status_t load_module(const char *path, module_info ***_modules);
+
 extern status_t module_init(struct kernel_args *args);
-extern void module_test(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif	/* _KRENEL_MODULE_H */
