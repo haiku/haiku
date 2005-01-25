dd5857c2833916e4007859b40f4cd0c41924cf16
Axel Dörfler <axeld@pinc-software.de>
2005-01-25 02:37:08 +0000

    Added devfs_publish_directory() prototype.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@11016 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/devfs.h
+++ headers/private/kernel/devfs.h
@@ -1,10 +1,10 @@
 /*
-** Copyright 2002-2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
-** Distributed under the terms of the Haiku License.
-**
-** Copyright 2001-2002, Travis Geiselbrecht. All rights reserved.
-** Distributed under the terms of the NewOS License.
-*/
+ * Copyright 2002-2005, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Copyright 2001-2002, Travis Geiselbrecht. All rights reserved.
+ * Distributed under the terms of the NewOS License.
+ */
 #ifndef _DEVFS_H
 #define _DEVFS_H
 
@@ -23,6 +23,7 @@ status_t devfs_unpublish_partition(const char *path);
 status_t devfs_publish_partition(const char *path, const partition_info *info);
 
 status_t devfs_publish_device(const char *path, void *ident, device_hooks *calls);
+status_t devfs_publish_directory(const char *path);
 
 #ifdef __cplusplus
 }
