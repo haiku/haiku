4edff48607c4afe7a2f6ef387e3fcf773aa8957b
Axel Dörfler <axeld@pinc-software.de>
2005-01-25 21:23:29 +0000

    We still have to be binary compatible when we build for Haiku. Cleanup.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@11041 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/posix/sys/utsname.h
+++ headers/posix/sys/utsname.h
@@ -1,29 +1,25 @@
 /*
- *
- * Copyright 2004, Haiku Inc. All Rights Reserved.
+ * Copyright 2004-2005, Haiku Inc. All Rights Reserved.
  * Distributed under the terms of the MIT License.
  */
-
 #ifndef _SYS_UTSNAME_H
 #define _SYS_UTSNAME_H
 
-#ifdef __HAIKU__
-#define _SYS_NAMELEN 256
-#else
-#define _SYS_NAMELEN 32		// R5 compatibility
-#endif
+
+#define _SYS_NAMELEN 32
+
+struct utsname {
+	char sysname[_SYS_NAMELEN];		/* Name of the OS */
+	char nodename[_SYS_NAMELEN];	/* Name of this node (network related) */
+	char release[_SYS_NAMELEN];		/* Current release level */
+	char version[_SYS_NAMELEN];		/* Current version level */
+	char machine[_SYS_NAMELEN];		/* Name of the hardware type */
+};
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-struct utsname {
-	char sysname[_SYS_NAMELEN];	/* Name of the OS */
-	char nodename[_SYS_NAMELEN];	/* Name of this node (network related) */
-	char release[_SYS_NAMELEN];	/* Current release level */
-	char version[_SYS_NAMELEN];	/* Current version level */
-	char machine[_SYS_NAMELEN];	/* Name of the hardware type */
-};
 
 int uname(struct utsname *);
 
@@ -31,5 +27,4 @@ int uname(struct utsname *);
 }
 #endif
 
-#endif
-
+#endif	/* _SYS_UTSNAME_H */
