7f2a4198291f295a7e3013f646e125c9b61bbad3
Jérôme Duval <korli@users.berlios.de>
2005-01-29 12:36:20 +0000

    Removed -v flag for tar expanding as noticed by Fredrik Ekdahl
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@11118 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/apps/expander/ExpanderRules.cpp
+++ src/apps/expander/ExpanderRules.cpp
@@ -58,10 +58,10 @@ ExpanderRule::ExpanderRule(const char*  mimetype, const char*  filenameExtension
 
 ExpanderRules::ExpanderRules()
 {
-	fList.AddItem(new ExpanderRule("", 								".tar.gz", 	"zcat %s | tar -tvf -", 		"zcat %s | tar -xvf -"));
-	fList.AddItem(new ExpanderRule("", 								".tar.bz2", "bzcat %s | tar -tvf -", 		"bzcat %s | tar -xvf -"));
-	fList.AddItem(new ExpanderRule("", 								".tar.Z", 	"zcat %s | tar -tvf -", 		"zcat %s | tar -xvf -"));
-	fList.AddItem(new ExpanderRule("", 								".tgz", 	"zcat %s | tar -tvf -", 		"zcat %s | tar -xvf -"));
+	fList.AddItem(new ExpanderRule("", 								".tar.gz", 	"zcat %s | tar -tvf -", 		"zcat %s | tar -xf -"));
+	fList.AddItem(new ExpanderRule("", 								".tar.bz2", "bzcat %s | tar -tvf -", 		"bzcat %s | tar -xf -"));
+	fList.AddItem(new ExpanderRule("", 								".tar.Z", 	"zcat %s | tar -tvf -", 		"zcat %s | tar -xf -"));
+	fList.AddItem(new ExpanderRule("", 								".tgz", 	"zcat %s | tar -tvf -", 		"zcat %s | tar -xf -"));
 	fList.AddItem(new ExpanderRule("application/x-tar", 			".tar", 	"tar -tvf %s", 					"tar -xf %s"));
 	fList.AddItem(new ExpanderRule("application/x-gzip",			".gz", 		"echo %s | sed 's/.gz$//g'", 	"gunzip %s"));
 	fList.AddItem(new ExpanderRule("application/x-bzip2",				".bz2", 	"echo %s | sed 's/.bz2$//g'", 	"bunzip2 %s"));
