1b6e740f1ead54afb9c9a68cb852c52c67b0acd1
Jérôme Duval <korli@users.berlios.de>
2005-02-02 13:54:30 +0000

    kMenuWindowLook is actually a kMenuWindowFeel Added private flags
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@11215 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/interface/WindowPrivate.h
+++ headers/private/interface/WindowPrivate.h
@@ -11,15 +11,20 @@
 
 const window_look kDesktopWindowLook = window_look(4);
 const window_look kLeftTitledWindowLook = window_look(25);
-const window_look kMenuWindowLook = window_look(1025);
 
 /* Private window feels */
 
 const window_feel kDesktopWindowFeel = window_feel(1024);
+const window_feel kMenuWindowFeel = window_feel(1025);
 
 /* Private window types */
 
 const window_type kWindowScreenWindow = window_type(1026);
+
+/* Private window flags */
+
+const uint32 kWorkspacesWindowFlag = 0x8000;
+const uint32 kWindowScreenFlag = 0x10000;
 
 #endif //_WINDOW_PRIVATE_H
 
