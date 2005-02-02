31d6118e452e02abf274cce102a9f5a7660f06f7
Jérôme Duval <korli@users.berlios.de>
2005-02-02 11:05:21 +0000

    Added a private header for window looks, types, feels
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@11212 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/interface/WindowPrivate.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2005, Jérôme Duval, jerome.duval@free.fr.
+
+ * Distributed under the terms of the MIT License.
+ */
+
+#ifndef _WINDOW_PRIVATE_H
+#define _WINDOW_PRIVATE_H
+
+/* Private window looks */
+
+const window_look kDesktopWindowLook = window_look(4);
+const window_look kLeftTitledWindowLook = window_look(25);
+const window_look kMenuWindowLook = window_look(1025);
+
+/* Private window feels */
+
+const window_feel kDesktopWindowFeel = window_feel(1024);
+
+/* Private window types */
+
+const window_type kWindowScreenWindow = window_type(1026);
+
+#endif //_WINDOW_PRIVATE_H
+
