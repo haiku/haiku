28362fed3342eec0b3a5255b2d9e583964e81a68
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2005-02-21 13:47:12 +0000

    I just noticed this file had a weird license header...
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@11430 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kits/interface/Screen.cpp
+++ src/kits/interface/Screen.cpp
@@ -1,15 +1,31 @@
-//	File name:		Screen.cpp
-
-//	Description:	BScreen let you retrieve and change the display settings.
-
+//------------------------------------------------------------------------------
+//	Copyright (c) 2003-2005, Haiku, Inc.
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a
+//	copy of this software and associated documentation files (the "Software"),
+//	to deal in the Software without restriction, including without limitation
+//	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//	and/or sell copies of the Software, and to permit persons to whom the
+//	Software is furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//	DEALINGS IN THE SOFTWARE.
+//
+//	File Name:		Screen.cpp
 //	Author:			Stefano Ceccherini (burton666@libero.it)
-
-
-// System includes
+//	Description:	BScreen lets you retrieve and change the display settings.
+//------------------------------------------------------------------------------
 #include <Screen.h>
 #include <Window.h>
 
-// Private includes
 #include <PrivateScreen.h>
 
 
@@ -457,7 +473,6 @@ BScreen::private_screen()
 
 
 /*----- Private or reserved -----------------------------------------*/
-
 /*!	\brief Deprecated, use ProposeMode() instead.
 */
 status_t
