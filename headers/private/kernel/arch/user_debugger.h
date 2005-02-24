c7eab67fe12f2bf0f9fe97ef91468ab3b075725c
Ingo Weinhold <ingo_weinhold@gmx.de>
2005-02-24 15:44:48 +0000

    Architecture specific debugger interface.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@11469 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/kernel/arch/user_debugger.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2005, Ingo Weinhold, bonefish@users.sf.net.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef KERNEL_ARCH_USER_DEBUGGER_H
+#define KERNEL_ARCH_USER_DEBUGGER_H
+
+void arch_get_debug_cpu_state(struct debug_cpu_state *cpuState);
+
+#endif	// KERNEL_ARCH_USER_DEBUGGER_H
