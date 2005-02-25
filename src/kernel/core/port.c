8c37e2348e3cad71a05b5edd9ed661cae8704c96
Ingo Weinhold <ingo_weinhold@gmx.de>
2005-02-25 14:13:00 +0000

    Added support for new flag B_KILL_CAN_INTERRUPT. (Do ports really not support absolute timeouts?)
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@11486 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kernel/core/port.c
+++ src/kernel/core/port.c
@@ -878,7 +878,8 @@ read_port_etc(port_id id, int32 *_msgCode, void *msgBuffer, size_t bufferSize,
 		|| timeout < 0)
 		return B_BAD_VALUE;
 
-	flags = flags & (B_CAN_INTERRUPT | B_TIMEOUT);
+	flags = flags & (B_CAN_INTERRUPT | B_KILL_CAN_INTERRUPT | B_TIMEOUT);
+		// ToDo: We don't support absolute timeouts here?
 	slot = id % sMaxPorts;
 
 	state = disable_interrupts();
@@ -980,7 +981,7 @@ write_port_etc(port_id id, int32 msgCode, const void *msgBuffer,
 		return B_BAD_PORT_ID;
 
 	// mask irrelevant flags (for acquire_sem() usage)
-	flags = flags & (B_CAN_INTERRUPT | B_TIMEOUT);
+	flags = flags & (B_CAN_INTERRUPT | B_KILL_CAN_INTERRUPT | B_TIMEOUT);
 	slot = id % sMaxPorts;
 
 	if (bufferSize > PORT_MAX_MESSAGE_SIZE)
