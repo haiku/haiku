782c98afd9b856369fe39dae8b1abf8c7a1ea4f3
Ingo Weinhold <ingo_weinhold@gmx.de>
2005-02-25 14:16:07 +0000

    * Added KILL_SIGNALS, a signal mask for SIGKILL + SIGKILLTHR. * Removed SIGKILLTHR from BLOCKABLE_SIGS. * Made header C++ safe. * Added function is_kill_signal_pending(), returning true, if a   SIGKILL[THR] is pending for the current thread.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@11487 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/ksignal.h
+++ headers/private/kernel/ksignal.h
@@ -9,14 +9,25 @@
 #include <KernelExport.h>
 #include <signal.h>
 
-#define BLOCKABLE_SIGS	(~((1L << (SIGKILL - 1)) | (1L << (SIGSTOP - 1))))
+#define KILL_SIGNALS	((1L << (SIGKILL - 1)) | (1L << (SIGKILLTHR - 1)))
+#define BLOCKABLE_SIGS	(~(KILL_SIGNALS | (1L << (SIGSTOP - 1))))
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 extern int handle_signals(struct thread *t, cpu_status *state);
+extern bool is_kill_signal_pending();
 
 extern int _user_send_signal(pid_t tid, uint sig);
 extern int _user_sigprocmask(int how, const sigset_t *set, sigset_t *oldSet);
 extern int _user_sigaction(int sig, const struct sigaction *action, struct sigaction *oldAction);
 extern bigtime_t _user_set_alarm(bigtime_t time, uint32 mode);
+
+#ifdef __cplusplus
+}	// extern "C"
+#endif
+
 
 #endif	/* _KERNEL_SIGNAL_H */
