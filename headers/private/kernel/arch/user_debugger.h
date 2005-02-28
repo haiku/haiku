dd07f6001dd30f52250b1c96163abeb095ee7c24
Ingo Weinhold <ingo_weinhold@gmx.de>
2005-02-28 00:43:43 +0000

    * Added functions for clearing/destroying the architecture specific team   debug infos. * Added function for setting the CPU state. * Added functions for setting break-/watchpoints.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@11507 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/arch/user_debugger.h
+++ headers/private/kernel/arch/user_debugger.h
@@ -5,6 +5,29 @@
 #ifndef KERNEL_ARCH_USER_DEBUGGER_H
 #define KERNEL_ARCH_USER_DEBUGGER_H
 
+#include <debugger.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct arch_team_debug_info;
+
+void arch_clear_team_debug_info(struct arch_team_debug_info *info);
+void arch_destroy_team_debug_info(struct arch_team_debug_info *info);
+
+void arch_set_debug_cpu_state(const struct debug_cpu_state *cpuState);
 void arch_get_debug_cpu_state(struct debug_cpu_state *cpuState);
+
+status_t arch_set_breakpoint(void *address);
+status_t arch_clear_breakpoint(void *address);
+status_t arch_set_watchpoint(void *address, uint32 type, int32 length);
+status_t arch_clear_watchpoint(void *address);
+
+#ifdef __cplusplus
+}
+#endif
+
+#include <arch_user_debugger.h>
 
 #endif	// KERNEL_ARCH_USER_DEBUGGER_H
