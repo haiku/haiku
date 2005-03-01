00a2b607d8489e913024570526ac92141aacac3f
Ingo Weinhold <ingo_weinhold@gmx.de>
2005-03-01 23:49:20 +0000

    Added functions for clearing/destroying the architecture specific thread debug info.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@11521 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/arch/user_debugger.h
+++ headers/private/kernel/arch/user_debugger.h
@@ -12,9 +12,12 @@ extern "C" {
 #endif
 
 struct arch_team_debug_info;
+struct arch_thread_debug_info;
 
 void arch_clear_team_debug_info(struct arch_team_debug_info *info);
 void arch_destroy_team_debug_info(struct arch_team_debug_info *info);
+void arch_clear_thread_debug_info(struct arch_thread_debug_info *info);
+void arch_destroy_thread_debug_info(struct arch_thread_debug_info *info);
 
 void arch_set_debug_cpu_state(const struct debug_cpu_state *cpuState);
 void arch_get_debug_cpu_state(struct debug_cpu_state *cpuState);
