ea8e6dbaab936272fbd73c1b0ce638408e8bbf24
Ingo Weinhold <ingo_weinhold@gmx.de>
2005-03-03 00:40:03 +0000

    Made C++ safe.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/current@11548 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/tests/add-ons/kernel/file_systems/fs_shell/argv.h
+++ src/tests/add-ons/kernel/file_systems/fs_shell/argv.h
@@ -1,2 +1,17 @@
+#ifndef FS_SHELL_ARGV_H
+#define FS_SHELL_ARGV_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 /* this function takes a string and chops it into individual "words" */
 char **build_argv(char *str, int *argc);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	// FS_SHELL_ARGV_H
