1929784f0c0077479e30db09e745df7b9e9862c9
Jérôme Duval <korli@users.berlios.de>
2005-03-18 10:43:58 +0000

    Removed unused files
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@11902 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/apps/bin/gnu/cp-hash.h
+++
@@ -1,8 +0,0 @@
-/* For created inodes, a pointer in the search structure to this
-   character identifies the inode as being new.  */
-extern char new_file;
-
-void hash_init PARAMS ((unsigned int modulus, unsigned int entry_tab_size));
-void forget_all PARAMS ((void));
-char *remember_copied PARAMS ((const char *node, ino_t ino, dev_t dev));
-int remember_created PARAMS ((const char *path));
