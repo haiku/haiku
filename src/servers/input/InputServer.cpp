688204180d4390895edf1a57d5542b77f50df96a
Adi Oanca <adioanca@nowhere.fake>
2005-03-26 08:40:33 +0000

    No need to send string length on B_KEY_DOWN/UP
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@12015 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/servers/input/InputServer.cpp
+++ src/servers/input/InputServer.cpp
@@ -1285,8 +1285,6 @@ InputServer::DispatchEvent(BMessage *message)
 			appsvrlink->Attach(repeatcount);
 			appsvrlink->Attach(modifiers);
 			appsvrlink->Attach(utf8data,sizeof(int8)*3);
-			int8 strlength = string.Length()+1;
-			appsvrlink->Attach(strlength);
 			appsvrlink->AttachString(string.String());
 			appsvrlink->Attach(keyarray,sizeof(int8)*16);
 			appsvrlink->Flush();
@@ -1315,8 +1313,6 @@ InputServer::DispatchEvent(BMessage *message)
 			appsvrlink->Attach(asciicode);
 			appsvrlink->Attach(modifiers);
 			appsvrlink->Attach(utf8data,sizeof(int8)*3);
-			int8 strlength = string.Length() + 1;
-			appsvrlink->Attach(strlength);
 			appsvrlink->AttachString(string.String());
 			appsvrlink->Attach(keyarray,sizeof(int8)*16);
 			appsvrlink->Flush();
