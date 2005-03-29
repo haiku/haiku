7ad3536bb5585a798201720106fd181844b155f1
Stephan Aßmus <superstippi@gmx.de>
2005-03-29 21:27:35 +0000

    remove some views, the more we use, the more problems...
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@12136 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/tests/servers/app/windows/main.cpp
+++ src/tests/servers/app/windows/main.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 #include "Application.h"
+#include "TextControl.h"
 #include "View.h"
 #include "Window.h"
 
@@ -15,9 +16,13 @@ class HelloView : public BView {
 
 	virtual	void	Draw(BRect updateRect)
 					{
+//				printf("HelloView::Draw()\n");
+//				updateRect.PrintToStream();
+
 						SetHighColor(ui_color(B_PANEL_BACKGROUND_COLOR));
 						FillRect(updateRect);
 						BRect r(Bounds());
+//				r.PrintToStream();
 
 						const char* message = "Hello World!";
 						float width = StringWidth(message);
@@ -39,9 +44,14 @@ show_window(BRect frame, const char* name)
 								  B_ASYNCHRONOUS_CONTROLS | B_QUIT_ON_WINDOW_CLOSE);
 
 	BView* view = new HelloView(window->Bounds(), "test", B_FOLLOW_ALL,
-								B_WILL_DRAW | B_FULL_UPDATE_ON_RESIZE);
+								B_WILL_DRAW/* | B_FULL_UPDATE_ON_RESIZE*/);
 
 	window->AddChild(view);
+
+/*	BRect b(view->Bounds());
+	b.InsetBy(10.0, 20.0);
+	BTextControl* control = new BTextControl(b, "tc", "Text", "Enter Text Here", NULL);
+	view->AddChild(control);*/
 
 	window->Show();
 }
