8e6163ac4dee65df4d0aed807704d3b515731794
Axel Dörfler <axeld@pinc-software.de>
2005-03-31 11:11:17 +0000

    Now uses the B_SAFEMODE_SAFE_MODE definition instead of the string directly.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@12184 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/bin/safemode.c
+++ src/bin/safemode.c
@@ -1,11 +1,10 @@
 /*
  * (c) 2004, Jérôme DUVAL for Haiku
  * released under the MIT licence.
- *
- * safemode
  */
 
 
+#include <drivers/driver_settings.h>
 #include <syscalls.h>
 
 #include <stdio.h>
@@ -18,7 +17,7 @@ main(int argc, char **argv)
 	char buffer[B_FILE_NAME_LENGTH];
 	size_t size = sizeof(buffer);
 
-	status_t status = _kern_get_safemode_option("safemode", buffer, &size);
+	status_t status = _kern_get_safemode_option(B_SAFEMODE_SAFE_MODE, buffer, &size);
 	if (status == B_OK) {
 		if (!strncasecmp(buffer, "true", size)
 			|| !strncasecmp(buffer, "yes", size)
