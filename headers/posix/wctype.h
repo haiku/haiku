c1fc0493cb5bc9c86a7defdccdcf2ef6eaabc659
Axel Dörfler <axeld@pinc-software.de>
2005-05-18 15:02:09 +0000

    Added missing wctype.h header - R5 does not include this header, but its libroot.so exports the functions declared therein.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@12713 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/posix/wctype.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2005, Haiku Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _WCTYPE_H_
+#define _WCTYPE_H_
+
+
+#include <wchar.h>
+
+
+typedef const int *wctrans_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int		iswalnum(wint_t wc);
+extern int		iswalpha(wint_t wc);
+extern int		iswblank(wint_t wc);
+extern int		iswcntrl(wint_t wc);
+extern int		iswdigit(wint_t wc);
+extern int		iswgraph(wint_t wc);
+extern int		iswlower(wint_t wc);
+extern int		iswprint(wint_t wc);
+extern int		iswpunct(wint_t wc);
+extern int		iswspace(wint_t wc);
+extern int		iswupper(wint_t wc);
+extern int		iswxdigit(wint_t wc);
+
+extern int		iswctype(wint_t wc, wctype_t charClass);
+extern wint_t	towctrans(wint_t wc, wctrans_t charClass);
+extern wint_t	towlower(wint_t wc);
+extern wint_t	towupper(wint_t wc);
+
+extern wctrans_t wctrans(const char *charClass);
+extern wctype_t	wctype(const char *property);
+
+#ifndef __cplusplus
+}
+#endif
+
+#endif	/* _WCTYPE_H_ */
