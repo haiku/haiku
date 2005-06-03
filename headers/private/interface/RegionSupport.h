59cbc87ad805d7605dc09f52ad70169a380cb4d5
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2005-06-03 08:34:08 +0000

    RegionSupport accepts, as parameters, references to BRegions (consts when possible) instead of pointers. Note: this is not a functional change
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@12937 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/interface/RegionSupport.h
+++ headers/private/interface/RegionSupport.h
@@ -3,40 +3,41 @@
 #ifndef __REGION_SUPPORT_H
 #define __REGION_SUPPORT_H
 
-#include <Region.h>
-
-class BRegion::Support 
-{
+struct clipping_rect;
+class BRegion;
+class BRegion::Support {
 public:
-	static void ZeroRegion(BRegion *a_region);
-	static void ClearRegion(BRegion *a_region);
-	static void CopyRegion(BRegion *src_region, BRegion *dst_region);
-	static void AndRegion(BRegion *first, BRegion *second, BRegion *dest);
-	static void OrRegion(BRegion *first, BRegion *second, BRegion *dest);
-	static void SubRegion(BRegion *first, BRegion *second, BRegion *dest);
+	static void ZeroRegion(BRegion &region);
+	static void ClearRegion(BRegion &region);
+	static void CopyRegion(const BRegion &source, BRegion &dest);
+	static void AndRegion(const BRegion &first, const BRegion &second, BRegion &dest);
+	static void OrRegion(const BRegion &first, const BRegion &second, BRegion &dest);
+	static void SubRegion(const BRegion &first, const BRegion &second, BRegion &dest);
 
 private:
-	static void CleanupRegion(BRegion *region_in);
-	static void CleanupRegionVertical(BRegion *region_in);
-	static void CleanupRegionHorizontal(BRegion *region_in);
+	static void CleanupRegion(BRegion &region);
+	static void CleanupRegionVertical(BRegion &region);
+	static void CleanupRegionHorizontal(BRegion &region);
 	
 	static void SortRects(clipping_rect *rects, long count);
 	static void SortTrans(long *lptr1, long *lptr2, long count);	
 	
-	static void CopyRegionMore(BRegion*, BRegion*, long);
+	static void CopyRegionMore(const BRegion &, BRegion &, long);
 	
-	static void AndRegionComplex(BRegion*, BRegion*, BRegion*);
-	static void AndRegion1ToN(BRegion*, BRegion*, BRegion*);
+	static void AndRegionComplex(const BRegion &, const BRegion &, BRegion &);
+	static void AndRegion1ToN(const BRegion &, const BRegion &, BRegion &);
 
-	static void AppendRegion(BRegion*, BRegion*, BRegion*);
+	static void AppendRegion(const BRegion &, const BRegion &, BRegion &);
 	
-	static void OrRegionComplex(BRegion*, BRegion*, BRegion*);
-	static void OrRegion1ToN(BRegion*, BRegion*, BRegion*);
-	static void OrRegionNoX(BRegion*, BRegion*, BRegion*);
-	static void ROr(long, long, BRegion*, BRegion*, BRegion*, long*, long *);
+	static void OrRegionComplex(const BRegion &, const BRegion &, BRegion &);
+	static void OrRegion1ToN(const BRegion &, const BRegion &, BRegion &);
+	static void OrRegionNoX(const BRegion &, const BRegion &, BRegion &);
+	static void ROr(long, long, const BRegion &, const BRegion &,
+					BRegion &, long *, long *);
 
-	static void SubRegionComplex(BRegion*, BRegion*, BRegion*);
-	static void RSub(long , long, BRegion*, BRegion*, BRegion*, long*, long*);
+	static void SubRegionComplex(const BRegion &, const BRegion &, BRegion &);
+	static void RSub(long, long, const BRegion &, const BRegion &,
+					BRegion &, long *, long *);
 };
 
 #endif // __REGION_SUPPORT_H
