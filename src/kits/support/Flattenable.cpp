383050e5a648b9361aa276da41e11ed8a35c0e22
DarkWyrm <darkwyrm@gmail.com>
2005-06-06 19:29:04 +0000

    Minor tweaks to be able to declare this one done. :) Removal of the obsolete OpenBeOS namespace Added header
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@12963 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kits/support/Flattenable.cpp
+++ src/kits/support/Flattenable.cpp
@@ -1,14 +1,32 @@
-// Quick and dirty implementation of the BFlattenable class.
-// Just here to be able to compile and test BPath.
-// To be replaced by the OpenBeOS version to be provided by the IK Team.
-
+//------------------------------------------------------------------------------
+//	Copyright (c) 2001-2005, Haiku, Inc.
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a
+//	copy of this software and associated documentation files (the "Software"),
+//	to deal in the Software without restriction, including without limitation
+//	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//	and/or sell copies of the Software, and to permit persons to whom the
+//	Software is furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//	DEALINGS IN THE SOFTWARE.
+//
+//	File Name:		Flattenable.cpp
+//	Author:			unknown
+//	Description:	Provides functionality for subclasses to convert to
+//					a run of bytes
+//------------------------------------------------------------------------------
 #include <stdio.h>
 
 #include <Flattenable.h>
-
-#ifdef USE_OPENBEOS_NAMESPACE
-namespace OpenBeOS {
-#endif
 
 // AllowsTypeCode
 bool
@@ -26,8 +44,3 @@ BFlattenable::~BFlattenable()
 void BFlattenable::_ReservedFlattenable1() {}
 void BFlattenable::_ReservedFlattenable2() {}
 void BFlattenable::_ReservedFlattenable3() {}
-
-
-#ifdef USE_OPENBEOS_NAMESPACE
-}
-#endif
