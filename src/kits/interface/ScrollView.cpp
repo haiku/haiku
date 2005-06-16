1ae95ac8724efaf5fce9058e4d2f1c16a303ffea
Axel Dörfler <axeld@pinc-software.de>
2005-06-16 01:01:23 +0000

    Mixed up argument order - dunno why this didn't have any effect on R5, but the frame wasn't visible in Haiku, and rightly so :-)
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@13166 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kits/interface/ScrollView.cpp
+++ src/kits/interface/ScrollView.cpp
@@ -1,28 +1,8 @@
-//------------------------------------------------------------------------------
-//	Copyright (c) 2001-2002, OpenBeOS
-//
-//	Permission is hereby granted, free of charge, to any person obtaining a
-//	copy of this software and associated documentation files (the "Software"),
-//	to deal in the Software without restriction, including without limitation
-//	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-//	and/or sell copies of the Software, and to permit persons to whom the
-//	Software is furnished to do so, subject to the following conditions:
-//
-//	The above copyright notice and this permission notice shall be included in
-//	all copies or substantial portions of the Software.
-//
-//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-//	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-//	DEALINGS IN THE SOFTWARE.
-//
-//	File Name:		ScrollView.cpp
-//	Author:			Axel Dörfler <axeld@pinc-software.de>
-//	Description:	BView with two scrollbars
-//------------------------------------------------------------------------------
+/*
+ * Copyright 2004-2005, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+
 
 #include <ScrollView.h>
 #include <Message.h>
@@ -36,7 +16,7 @@ static const float kPlainBorderSize = 1;
 BScrollView::BScrollView(const char *name, BView *target, uint32 resizingMode,
 	uint32 flags, bool horizontal, bool vertical, border_style border)
 	: BView(CalcFrame(target, horizontal, vertical, border), name,
-		ModifyFlags(flags, border), resizingMode),
+		resizingMode, ModifyFlags(flags, border)),
 	fTarget(target),
 	fHorizontalScrollBar(NULL),
 	fVerticalScrollBar(NULL),
