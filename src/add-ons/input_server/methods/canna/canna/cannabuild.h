b7df9a98dfc8ec94de77c6c14fa567520d3e2132
Jérôme Duval <korli@users.berlios.de>
2005-07-07 17:17:04 +0000

    avoids warnings
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@13547 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/input_server/methods/canna/canna/cannabuild.h
+++ src/add-ons/input_server/methods/canna/canna/cannabuild.h
@@ -1,9 +1,11 @@
-#pragma once
 //////////////////////////////////////////////////////
 // Header file for emport / import gloval symbols.
 // Valid for BeOS Releas 3 or later.
 // 1998.5.4 by T.Murai
 //
+
+#ifndef CANNABUILD_H
+#define CANNABUILD_H
 
 //#define _BUILDING_CANNALIB 
 
@@ -12,3 +14,5 @@
 #else 
 #define canna_export __declspec(dllimport) 
 #endif
+
+#endif // CANNABUILD_H
