29f38965b42e22b2aa7b60511090e89ab4d6a1eb
Rudolf Cornelissen <rudolf.cornelissen@gmail.com>
2005-07-10 17:31:31 +0000

    removed it again
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@13575 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/accelerants/via/skeleton/engine/std.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <sys/ioctl.h>
-#include <math.h>
-#include <OS.h>
-#include "DriverInterface.h"
-#include "globals.h"
-//apsed #include "extern.h"
-#include "proto.h"
-#include "macros.h"
