22365ebc4046a3ef6f3aaaa7dd131c084e3ca90f
Axel Dörfler <axeld@pinc-software.de>
2005-08-03 17:26:02 +0000

    Added a command line application that officially ends the boot process (for the "launch_speedup" module).
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@13895 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/bin/boot_process_done.cpp
@@ -0,0 +1,32 @@
+/* 
+ * Copyright 2005, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+
+
+#include "launch_speedup.h"
+
+#include <OS.h>
+#include <syscalls.h>
+#include <generic_syscall.h>
+
+#include <stdio.h>
+
+
+int
+main(int argc, char **argv)
+{
+	uint32 version = 0;
+	status_t status = _kern_generic_syscall(LAUNCH_SPEEDUP_SYSCALLS, B_SYSCALL_INFO,
+		&version, sizeof(version));
+	if (status != B_OK) {
+		// the launch speedup module is not available
+		fprintf(stderr, "\"launch_speedup\" module not available.\n");
+		return 1;
+	}
+
+	_kern_generic_syscall(LAUNCH_SPEEDUP_SYSCALLS, LAUNCH_SPEEDUP_STOP_SESSION,
+		(void *)"system boot", strlen("system boot"));
+	return 0;
+}
+
