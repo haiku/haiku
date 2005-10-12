a26988784adabc85fcaaa92da730a00a786154f5
Jérôme Duval <korli@users.berlios.de>
2005-10-12 15:52:35 +0000

    added _fseek for R5 compatibility
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@14365 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/system/libroot/posix/stdio/_fseek.c
@@ -0,0 +1,12 @@
+/*
+** Copyright 2005, Jérôme Duval. All rights reserved.
+** Distributed under the terms of the Haiku License.
+*/
+
+#include <stdio.h>
+
+int
+_fseek(FILE *stream, fpos_t offset, int seekType)
+{
+	return fseeko(stream, offset, seekType);
+}
