0bf62ebeceb89b1d5c611ff91ba06c2756cc8ca7
Axel Dörfler <axeld@pinc-software.de>
2005-10-18 20:02:16 +0000

    The BlockArray class is now longer used, and strlcpy() is part of the Haiku kernel anyway.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@14416 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/kernel/file_systems/bfs/Utility.h
+++ src/add-ons/kernel/file_systems/bfs/Utility.h
@@ -34,33 +34,4 @@ sorted_array::Find(off_t value) const
 	return FindInternal(value,i) ? i : -1;
 }
 
-
-// The BlockArray reserves a multiple of "blockSize" and
-// maintain array size for new entries.
-// This is used for the in-memory log entries before they
-// are written to disk.
-
-class BlockArray {
-	public:
-		BlockArray(int32 blockSize);
-		~BlockArray();
-
-		int32 Find(off_t value);
-		status_t Insert(off_t value);
-		status_t Remove(off_t value);
-
-		void MakeEmpty();
-
-		int32 CountItems() const { return fArray != NULL ? fArray->count : 0; }
-		int32 BlocksUsed() const { return fArray != NULL ? ((fArray->count + 1) * sizeof(off_t) + fBlockSize - 1) / fBlockSize : 0; }
-		sorted_array *Array() const { return fArray; }
-		int32 Size() const { return fSize; }
-
-	private:
-		sorted_array *fArray;
-		int32	fBlockSize;
-		int32	fSize;
-		int32	fMaxBlocks;
-};
-
 #endif	/* UTILITY_H */
