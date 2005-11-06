c713cfe2bb874b9c70f44235744259c8ddb48d87
Axel Dörfler <axeld@pinc-software.de>
2005-11-06 23:36:59 +0000

    Fixed __strerror_r() - this closes bug #66.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@14723 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/libroot/posix/glibc/include/string.h
+++ src/system/libroot/posix/glibc/include/string.h
@@ -7,7 +7,15 @@
 #define __memcpy(to, from, size)	memcpy(to, from, size)
 #define __strchrnul(string, character)	strchrnul(string, character)
 #define __strnlen			strnlen
-#define __strerror_r			strerror_r
+
+static inline char *
+__strerror_r(int error, char *buffer, size_t bufferSize)
+{
+	if (strerror_r(error, buffer, bufferSize) != 0)
+		strlcpy(buffer, "Unknown Error", bufferSize);
+
+	return buffer;
+}
 
 static inline void *
 __mempcpy(void *to, const void *from, size_t size)
