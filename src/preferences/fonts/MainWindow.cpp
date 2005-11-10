0867bda58fd9c6befdbcd7d73329522b5c4651bd
Axel Dörfler <axeld@pinc-software.de>
2005-11-10 16:22:10 +0000

    Offscreen is also in the other direction...
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@14829 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/preferences/fonts/MainWindow.cpp
+++ src/preferences/fonts/MainWindow.cpp
@@ -92,7 +92,9 @@ MainWindow::MainWindow()
 		// make sure window is on screen
 		BScreen screen(this);
 		if (Frame().left > screen.Frame().right - 10
-			|| Frame().top > screen.Frame().bottom - 10)
+			|| Frame().top > screen.Frame().bottom - 10
+			|| Frame().right < screen.Frame().left + 10
+			|| Frame().bottom < screen.Frame().top + 10)
 			_Center();
 	}
 
