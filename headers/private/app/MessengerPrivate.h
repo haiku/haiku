740ccdc27ecb6571ba2f6d123776fe250c634b30
Axel Dörfler <axeld@pinc-software.de>
2005-11-17 20:19:27 +0000

    Cleanup, added license.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@15008 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/app/MessengerPrivate.h
+++ headers/private/app/MessengerPrivate.h
@@ -1,21 +1,26 @@
+/*
+ * Copyright 2003-2005, Haiku.
+ * Distributed under the terms of the MIT License.
+ */
 #ifndef MESSENGER_PRIVATE_H
 #define MESSENGER_PRIVATE_H
 
+
 #include <Messenger.h>
 
-class BMessenger::Private
-{
+
+class BMessenger::Private {
 	public:
-		Private(BMessenger* msnger) : fMessenger(msnger) {;}
-		Private(BMessenger& msnger) : fMessenger(&msnger) {;}
+		Private(BMessenger* messenger) : fMessenger(messenger) {}
+		Private(BMessenger& messenger) : fMessenger(&messenger) {}
 
 		port_id	Port()
 			{ return fMessenger->fPort; }
-		int32	Token()
+		int32 Token()
 			 { return fMessenger->fHandlerToken; }
 		team_id	Team()
 			{ return fMessenger->fTeam; }
-		bool	IsPreferredTarget()
+		bool IsPreferredTarget()
 			{ return fMessenger->fPreferredTarget; }
 
 		void SetTo(team_id team, port_id port, int32 token, bool preferred)
