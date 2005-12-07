8b150d6067ffabe5ec1e6c36e49d36c64d6b1e47
Ingo Weinhold <ingo_weinhold@gmx.de>
2005-12-07 22:41:39 +0000

    Added PPC specific and some generic glibc headers. Taken from glibc 2.3.2, which seems to be the version used in other places, though I also found older files.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@15402 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/libroot/posix/glibc/include/arch/ppc/sysdep.h
+++ src/system/libroot/posix/glibc/include/arch/ppc/sysdep.h
@@ -16,6 +16,8 @@
    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
    02111-1307 USA.  */
 
+#include_next <sysdep.h>
+
 #ifdef __ASSEMBLER__
 
 /* Symbolic names for the registers.  The only portable way to write asm
