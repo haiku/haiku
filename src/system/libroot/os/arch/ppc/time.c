079900209735f7a11cf5b669b759ee0f5ccfe508
Ingo Weinhold <ingo_weinhold@gmx.de>
2005-12-09 14:09:42 +0000

    Added an unimplemented __arch_init_time() for PPC.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@15443 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/system/libroot/os/arch/ppc/time.c
@@ -0,0 +1,17 @@
+/* 
+ * Copyright 2004, Axel Dörfler, axeld@pinc-software.de.
+ * Distributed under the terms of the MIT License.
+ */
+
+
+#include <libroot_private.h>
+#include <real_time_data.h>
+#include <arch_cpu.h>
+
+
+void
+__arch_init_time(struct real_time_data *data)
+{
+	// TODO: Implement!
+}
+
