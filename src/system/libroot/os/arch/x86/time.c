8f9f676b46b212cef5591fe5e4f0dd2934ce2c16
Jérôme Duval <korli@users.berlios.de>
2006-01-05 15:09:50 +0000

    we don't use atomic_get64 to read a read-only area, not authorized and throw a VM fault this fix #83 partially
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@15845 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/libroot/os/arch/x86/time.c
+++ src/system/libroot/os/arch/x86/time.c
@@ -25,6 +25,7 @@ __arch_init_time(struct real_time_data *data, bool setDefaults)
 bigtime_t
 __arch_get_system_time_offset(struct real_time_data *data)
 {
-	return atomic_get64(&data->arch_data.system_time_offset);
+	//we don't use atomic_get64 because memory is read-only, maybe find another way to lock
+	return data->arch_data.system_time_offset;
 }
 
