e2491d6b8a16ca83b1a8a5580c12c70a861be71a
Jérôme Duval <korli@users.berlios.de>
2006-01-08 16:01:05 +0000

    reverted to previous version
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@15873 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kits/tracker/FSUtils.cpp
+++ src/kits/tracker/FSUtils.cpp
@@ -1449,18 +1449,20 @@ FSDuplicate(BObjectList<entry_ref> *srcList, BList *pointList)
 }
 
 
+#if 0
 status_t
 FSCopyFolder(BEntry *srcEntry, BDirectory *destDir, CopyLoopControl *loopControl,
-	BPoint *loc, bool makeOriginalName, Undo &undo)
+	BPoint *loc, bool makeOriginalName)
 {
 	try {
-		CopyFolder(srcEntry, destDir, loopControl, loc, makeOriginalName, undo);
+		CopyFolder(srcEntry, destDir, loopControl, loc, makeOriginalName);
 	} catch (status_t error) {
 		return error;
 	}
 
 	return B_OK;
 }
+#endif
 
 
 status_t
