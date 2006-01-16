079a1529f7e6f2fb6f6247c126e63b2ecd6296c2
Marcus Overhagen <marcusoverhagen@gmail.com>
2006-01-16 21:06:34 +0000

    simplified ps2_hid driver, only used to load the ps2 bus manager right now
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@15986 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/add-ons/kernel/drivers/input/ps2_hid_new/ps2_hid.c
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2005 Haiku, Inc.
+ * Distributed under the terms of the MIT License.
+ *
+ * PS/2 hid device driver
+ *
+ * Authors (in chronological order):
+ *		Marcus Overhagen (marcus@overhagen.de)
+ */
+
+#include <KernelExport.h>
+#include <Drivers.h>
+#include "../../../bus_managers/ps2/PS2.h"
+
+#define TRACE(x) dprintf x
+
+int32 api_version = B_CUR_DRIVER_API_VERSION;
+
+ps2_module_info *gPs2 = NULL;
+
+status_t
+init_hardware(void)
+{
+	TRACE(("ps2_hid: init_hardware\n"));
+	return B_OK;
+}
+
+
+const char **
+publish_devices(void)
+{
+	TRACE(("ps2_hid: publish_devices\n"));
+	return NULL;
+}
+
+
+device_hooks *
+find_device(const char *name)
+{
+	TRACE(("ps2_hid: find_device\n"));
+	return NULL;
+}
+
+
+status_t
+init_driver(void)
+{
+	TRACE(("ps2_hid: init_driver\n"));
+	return get_module(B_PS2_MODULE_NAME, (module_info **)&gPs2);
+}
+
+
+void
+uninit_driver(void)
+{
+	TRACE(("ps2_hid: uninit_driver\n"));
+	put_module(B_PS2_MODULE_NAME);
+}
