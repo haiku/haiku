906b28a9acbc46452027a76b24ca8a00ca70ef64
Ingo Weinhold <ingo_weinhold@gmx.de>
2006-01-26 15:21:31 +0000

    Added some PPC support to the PCI bus manager. We scan the Open Firmware device tree for PCI controllers and make them known to the bus manager, if we know how to talk with them. ATM we support only the UniNorth chip, which can be found in G4 Macs (code ported from FreeBSD). As far as I can judge it, all attached devices are identified correctly on all three host bridges of my Mac mini.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@16102 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/kernel/bus_managers/pci/arch/ppc/pci_controller.cpp
+++ src/add-ons/kernel/bus_managers/pci/arch/ppc/pci_controller.cpp
@@ -1,10 +1,20 @@
 #include "pci_controller.h"
+
+#include <arch_platform.h>
+
 #include "pci_priv.h"
+
+#include "openfirmware/pci_openfirmware.h"
 
 
 status_t
 pci_controller_init(void)
 {
+	switch (PPCPlatform::Default()->PlatformType()) {
+		case PPC_PLATFORM_OPEN_FIRMWARE:
+			return ppc_openfirmware_pci_controller_init();
+			break;
+	}
 	return B_OK;
 }
 
--- src/add-ons/kernel/bus_managers/pci/arch/ppc/pci_io.c
+++ src/add-ons/kernel/bus_managers/pci/arch/ppc/pci_io.c
@@ -1,3 +1,4 @@
+#include "pci_io.h"
 #include "pci_priv.h"
 
 status_t
@@ -10,37 +11,40 @@ pci_io_init()
 uint8
 pci_read_io_8(int mapped_io_addr)
 {
-	return 0;
+	return ppc_in8((vuint8*)mapped_io_addr);
 }
 
 
 void
 pci_write_io_8(int mapped_io_addr, uint8 value)
 {
+	ppc_out8((vuint8*)mapped_io_addr, value);
 }
 
 
 uint16
 pci_read_io_16(int mapped_io_addr)
 {
-	return 0;
+	return ppc_in16((vuint16*)mapped_io_addr);
 }
 
 
 void
 pci_write_io_16(int mapped_io_addr, uint16 value)
 {
+	ppc_out16((vuint16*)mapped_io_addr, value);
 }
 
 
 uint32
 pci_read_io_32(int mapped_io_addr)
 {
-	return 0;
+	return ppc_in32((vuint32*)mapped_io_addr);
 }
 
 
 void
 pci_write_io_32(int mapped_io_addr, uint32 value)
 {
+	ppc_out32((vuint32*)mapped_io_addr, value);
 }
