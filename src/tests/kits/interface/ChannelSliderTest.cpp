43f7b9297798e1c696b303edd2af81e19ed5d413
Jérôme Duval <korli@users.berlios.de>
2006-02-10 17:12:06 +0000

    made a slider vertical, reduced initial rects, and applied ResizeToPreferred()
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@16337 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/tests/kits/interface/ChannelSliderTest.cpp
+++ src/tests/kits/interface/ChannelSliderTest.cpp
@@ -7,12 +7,15 @@ public:
 	MainWindow()
 		:BWindow(BRect(50, 50, 250, 360), "window", B_TITLED_WINDOW, B_ASYNCHRONOUS_CONTROLS)
 	{
-		BChannelSlider *slider = new BChannelSlider(BRect(20, 10, 180, 190),
+		BChannelSlider *slider = new BChannelSlider(BRect(10, 10, 20, 20),
 			"vertical slider", "vertical slider", new BMessage('test'), 4);
+		slider->SetOrientation(B_VERTICAL);
 		AddChild(slider);
-		BChannelSlider *horizontal = new BChannelSlider(BRect(20, 200, 180, 300),
+		slider->ResizeToPreferred();
+		BChannelSlider *horizontal = new BChannelSlider(BRect(100, 20, 136, 30),
 			 "horizontal slider", "horizontal slider", new BMessage('test'), 3);
 		AddChild(horizontal);
+		horizontal->ResizeToPreferred();
 	}
 	
 	virtual bool QuitRequested() { be_app->PostMessage(B_QUIT_REQUESTED); return BWindow::QuitRequested() ; }
