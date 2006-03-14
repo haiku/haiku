e0f9a4e8156e1ae05ec416428505d18eced96a5e
Jérôme Duval <korli@users.berlios.de>
2006-03-14 23:18:32 +0000

    added some wchar.h definitions
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@16803 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/posix/wchar.h
+++ headers/posix/wchar.h
@@ -19,7 +19,18 @@ typedef struct {
 
 #include <size_t.h>
 
+size_t 	mbrlen(const char *s, size_t n, mbstate_t *ps);
+size_t 	mbrtowc(wchar_t *pwc, const char *s, size_t n, mbstate_t *ps);
+size_t	mbsrtowcs(wchar_t *dst, const char **src, size_t len, 
+			mbstate_t *ps);
 int		mbsinit(const mbstate_t *);
 size_t	wcrtomb(char *, wchar_t, mbstate_t *);
+wchar_t	*wcschr(const wchar_t *ws, wchar_t wc);
+int 	wcscmp(const wchar_t *ws1, const wchar_t *ws2);
+int 	wcscoll(const wchar_t *ws1, const wchar_t *ws2);
+size_t	wcsrtombs(char *dst, const wchar_t **src, size_t len,
+			mbstate_t *ps);
+
+#define WEOF ((wint_t)(-1))
 
 #endif /* _WCHAR_H */
