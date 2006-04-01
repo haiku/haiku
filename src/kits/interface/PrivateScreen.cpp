0ef40c5e17ad18066faa78f61981550c9502f3ff
Michael Lotz <mmlr@mlotz.ch>
2006-04-01 14:31:21 +0000

    This should fix the wrong colors for selected Tracker icons. Fixes bug #205.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@16951 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kits/interface/PrivateScreen.cpp
+++ src/kits/interface/PrivateScreen.cpp
@@ -237,7 +237,7 @@ BPrivateScreen::IndexForColor(uint8 red, uint8 green, uint8 blue, uint8 alpha)
 		&& alpha == B_TRANSPARENT_COLOR.alpha)
 		return B_TRANSPARENT_8_BIT;
 
-	uint16 index = ((blue & 0xf8) << 7) | ((green & 0xf8) << 2) | (red >> 3);
+	uint16 index = ((red & 0xf8) << 7) | ((green & 0xf8) << 2) | (blue >> 3);
 	if (ColorMap())
 		return fColorMap->index_map[index];
 
