99f695c9a575f1dc3e0737a8cf16091c8a990d68
Axel Dörfler <axeld@pinc-software.de>
2006-04-19 10:24:14 +0000

    Fixed warning.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@17171 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kits/interface/StatusBar.cpp
+++ src/kits/interface/StatusBar.cpp
@@ -1,34 +1,19 @@
-//------------------------------------------------------------------------------
-//	Copyright (c) 2001-2002, OpenBeOS
-//
-//	Permission is hereby granted, free of charge, to any person obtaining a
-//	copy of this software and associated documentation files (the "Software"),
-//	to deal in the Software without restriction, including without limitation
-//	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-//	and/or sell copies of the Software, and to permit persons to whom the
-//	Software is furnished to do so, subject to the following conditions:
-//
-//	The above copyright notice and this permission notice shall be included in
-//	all copies or substantial portions of the Software.
-//
-//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-//	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-//	DEALINGS IN THE SOFTWARE.
-//
-//	File Name:		StatusBar.cpp
-//	Author:			Marc Flerackers (mflerackers@androme.be)
-//	Description:	BStatusBar displays a "percentage-of-completion" gauge.
-//------------------------------------------------------------------------------
-#include <string.h>
-#include <stdlib.h>
+/*
+ * Copyright 2001-2006, Haiku.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Marc Flerackers (mflerackers@androme.be)
+ */
+
+/*! BStatusBar displays a "percentage-of-completion" gauge. */
 
 #include <Debug.h>
 #include <Message.h>
 #include <StatusBar.h>
+
+#include <stdlib.h>
+#include <string.h>
 
 
 BStatusBar::BStatusBar(BRect frame, const char *name, const char *label,
@@ -621,7 +606,7 @@ BStatusBar::SetTextData(char **dest, const char *source)
 		*dest = NULL;
 	}
 
-	if (source != NULL && source[0] != NULL)
+	if (source != NULL && source[0] != '\0')
 		*dest = strdup(source);
 }
 
