12f6a76d9a7eb887bdd722978162474dfcecc2bd
Rudolf Cornelissen <rudolf.cornelissen@gmail.com>
2006-04-21 20:51:23 +0000

    removed two ID's from VIA kerneldriver: those where no graphics adapters after all.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@17195 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/kernel/drivers/graphics/via/driver.c
+++ src/add-ons/kernel/drivers/graphics/via/driver.c
@@ -101,9 +101,7 @@ static uint16 via_device_list[] = {
 	0x3022, /* CLE266 Unichrome Pro (CLE3022) */
 	0x3108, /* K8M800 Unichrome Pro (unknown chiptype) */
 	0x3122, /* CLE266 Unichrome Pro (CLE3122) */
-	0x3204, /* K8M800 Unichrome Pro (VT3204) */
 	0x3205, /* KM400 Unichrome (VT3205) */
-	0x7204, /* K8M800 Unichrome Pro (VT7204) */
 	0x7205, /* KM400 Unichrome (VT7205) */
 	0
 };
