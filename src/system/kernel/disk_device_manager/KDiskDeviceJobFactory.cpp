b02b72c4489f179726ea6c91e12bdbd888c23ccc
Axel Dörfler <axeld@pinc-software.de>
2006-05-05 10:44:07 +0000

    Minor cleanup.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@17333 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/kernel/disk_device_manager/KDiskDeviceJobFactory.cpp
+++ src/system/kernel/disk_device_manager/KDiskDeviceJobFactory.cpp
@@ -1,4 +1,10 @@
-// KDiskDeviceJobFactory.cpp
+/*
+ * Copyright 2004-2006, Haiku, Inc. All rights reserved.
+ * Copyright 2003-2004, Ingo Weinhold, bonefish@cs.tu-berlin.de. All rights reserved.
+ *
+ * Distributed under the terms of the MIT License.
+ */
+
 
 #include <util/kernel_cpp.h>
 
@@ -10,17 +16,17 @@
 
 using namespace std;
 
-// constructor
+
 KDiskDeviceJobFactory::KDiskDeviceJobFactory()
 {
 }
 
-// destructor
+
 KDiskDeviceJobFactory::~KDiskDeviceJobFactory()
 {
 }
 
-// CreateDefragmentJob
+
 KDiskDeviceJob *
 KDiskDeviceJobFactory::CreateDefragmentJob(partition_id partitionID)
 {
@@ -28,125 +34,116 @@ KDiskDeviceJobFactory::CreateDefragmentJob(partition_id partitionID)
 	return NULL;
 }
 
-// CreateRepairJob
+
 KDiskDeviceJob *
 KDiskDeviceJobFactory::CreateRepairJob(partition_id partitionID,
-									   bool checkOnly)
+	bool checkOnly)
 {
 	// not implemented
 	return NULL;
 }
 
-// CreateResizeJob
+
 KDiskDeviceJob *
 KDiskDeviceJobFactory::CreateResizeJob(partition_id parentID,
-									   partition_id partitionID, off_t size)
+	partition_id partitionID, off_t size)
 {
 	return new(nothrow) KResizeJob(parentID, partitionID, size);
 }
 
-// CreateMoveJob
+
 KDiskDeviceJob *
-KDiskDeviceJobFactory::CreateMoveJob(partition_id parentID,
-									 partition_id partitionID, off_t offset,
-									 const partition_id *contentsToMove,
-									 int32 contentsToMoveCount)
+KDiskDeviceJobFactory::CreateMoveJob(partition_id parentID, partition_id partitionID,
+	off_t offset, const partition_id *contentsToMove, int32 contentsToMoveCount)
 {
 	// not implemented
 	return NULL;
 }
 
-// CreateSetNameJob
+
 KDiskDeviceJob *
 KDiskDeviceJobFactory::CreateSetNameJob(partition_id parentID,
-										partition_id partitionID,
-										const char *name)
+	partition_id partitionID, const char *name)
 {
 	// not implemented
 	return NULL;
 }
 
-// CreateSetContentNameJob
+
 KDiskDeviceJob *
 KDiskDeviceJobFactory::CreateSetContentNameJob(partition_id partitionID,
-											   const char *name)
+	const char *name)
 {
 	// not implemented
 	return NULL;
 }
 
-// CreateSetTypeJob
+
 KDiskDeviceJob *
 KDiskDeviceJobFactory::CreateSetTypeJob(partition_id parentID,
-										partition_id partitionID,
-										const char *type)
+	partition_id partitionID, const char *type)
 {
 	// not implemented
 	return NULL;
 }
 
-// CreateSetParametersJob
+
 KDiskDeviceJob *
 KDiskDeviceJobFactory::CreateSetParametersJob(partition_id parentID,
-											  partition_id partitionID,
-											  const char *parameters)
+	partition_id partitionID, const char *parameters)
 {
 	// not implemented
 	return NULL;
 }
 
-// CreateSetContentParametersJob
+
 KDiskDeviceJob *
 KDiskDeviceJobFactory::CreateSetContentParametersJob(partition_id partitionID,
-													 const char *parameters)
+	const char *parameters)
 {
 	// not implemented
 	return NULL;
 }
 
-// CreateInitializeJob
+
 KDiskDeviceJob *
 KDiskDeviceJobFactory::CreateInitializeJob(partition_id partitionID,
-										   disk_system_id diskSystemID,
-										   const char *name,
-										   const char *parameters)
+	disk_system_id diskSystemID, const char *name, const char *parameters)
 {
 	// not implemented
 	return NULL;
 }
 
-// CreateUninitializeJob
+
 KDiskDeviceJob *
 KDiskDeviceJobFactory::CreateUninitializeJob(partition_id partitionID)
 {
 	return new(nothrow) KUninitializeJob(partitionID);
 }
 
-// CreateCreateChildJob
+
 KDiskDeviceJob *
 KDiskDeviceJobFactory::CreateCreateChildJob(partition_id partitionID,
-											partition_id childID, off_t offset,
-											off_t size, const char *type,
-											const char *parameters)
+	partition_id childID, off_t offset, off_t size, const char *type,
+	const char *parameters)
 {
 	// not implemented
 	return NULL;
 }
 
-// CreateDeleteChildJob
+
 KDiskDeviceJob *
 KDiskDeviceJobFactory::CreateDeleteChildJob(partition_id parentID,
-											partition_id partitionID)
+	partition_id partitionID)
 {
 	// not implemented
 	return NULL;
 }
 
-// CreateScanPartitionJob
+
 KDiskDeviceJob *
 KDiskDeviceJobFactory::CreateScanPartitionJob(partition_id partitionID)
 {
-	// not implemented
 	return new(nothrow) KScanPartitionJob(partitionID);
 }
 
