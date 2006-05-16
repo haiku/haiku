a2762cadacf267eecf349acef5999d13923f8aa4
Jérôme Duval <korli@users.berlios.de>
2006-05-16 12:11:26 +0000

    hopefully handles bug #587
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@17476 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/libroot/posix/glibc/include/allocalim.h
+++ src/system/libroot/posix/glibc/include/allocalim.h
@@ -1,4 +1,7 @@
-extern inline int __libc_use_alloca (size_t size)
+#ifndef _EXTERN_INLINE
+#define _EXTERN_INLINE extern inline
+#endif
+_EXTERN_INLINE int __libc_use_alloca (size_t size)
 {
   return size <= __MAX_ALLOCA_CUTOFF;
 }
---
+++ src/system/libroot/posix/glibc/stdlib/inlines.c
@@ -0,0 +1,4 @@
+#define _FORCE_INLINES
+#define _EXTERN_INLINE /* empty */
+#include "gmp.h"
+#include "include/alloca.h"
