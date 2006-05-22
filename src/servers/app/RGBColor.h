8dc6e23650e34044a52e094f7bf3e51d5279a98e
Stephan Aßmus <superstippi@gmx.de>
2006-05-22 10:03:24 +0000

    cleanup
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@17526 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/servers/app/RGBColor.h
+++ src/servers/app/RGBColor.h
@@ -13,48 +13,61 @@
 
 
 class RGBColor {
-	public:
-		RGBColor(uint8 r, uint8 g, uint8 b, uint8 a = 255);
-		RGBColor(int r, int g, int b, int a = 255);
-		RGBColor(const rgb_color &col);
-		RGBColor(uint16 col);
-		RGBColor(uint8 col);
-		RGBColor(const RGBColor &col);
-		RGBColor();
+ public:
+								RGBColor(uint8 r,
+										 uint8 g,
+										 uint8 b,
+										 uint8 a = 255);
+								RGBColor(int r,
+										 int g,
+										 int b,
+										 int a = 255);
+								RGBColor(const rgb_color& color);
+								RGBColor(uint16 color);
+								RGBColor(uint8 color);
+								RGBColor(const RGBColor& color);
+								RGBColor();
 
-		uint8 GetColor8() const;
-		uint16 GetColor15() const;
-		uint16 GetColor16() const;
-		rgb_color GetColor32() const;
+			uint8				GetColor8() const;
+			uint16				GetColor15() const;
+			uint16				GetColor16() const;
+			rgb_color			GetColor32() const;
 
-		void SetColor(uint8 r, uint8 g, uint8 b, uint8 a = 255);
-		void SetColor(int r, int g, int b, int a = 255);
-		void SetColor(uint16 color16);
-		void SetColor(uint8 color8);
-		void SetColor(const rgb_color &color);
-		void SetColor(const RGBColor &col);
+			void				SetColor(uint8 r,
+										 uint8 g,
+										 uint8 b,
+										 uint8 a = 255);
+			void				SetColor(int r,
+										 int g,
+										 int b,
+										 int a = 255);
+			void				SetColor(uint16 color16);
+			void				SetColor(uint8 color8);
+			void				SetColor(const rgb_color& color);
+			void				SetColor(const RGBColor& color);
 
-		const RGBColor & operator=(const RGBColor &color);
-		const RGBColor & operator=(const rgb_color &color);
-		bool operator==(const rgb_color &color) const;
-		bool operator==(const RGBColor &color) const;
-		bool operator!=(const rgb_color &color) const;
-		bool operator!=(const RGBColor &color) const;
+			const RGBColor&		operator=(const RGBColor& color);
+			const RGBColor&		operator=(const rgb_color& color);
 
-		bool IsTransparentMagic() const;
-
-		void PrintToStream() const;
+			bool				operator==(const rgb_color& color) const;
+			bool				operator==(const RGBColor& color) const;
+			bool				operator!=(const rgb_color& color) const;
+			bool				operator!=(const RGBColor& color) const;
+	
+			bool				IsTransparentMagic() const;
+	
+			void				PrintToStream() const;
 
 	protected:
-		rgb_color fColor32;
+			rgb_color			fColor32;
 
-		// caching
-		mutable uint16 fColor16;
-		mutable uint16 fColor15;
-		mutable uint8 fColor8;
-		mutable bool fUpdate8;
-		mutable bool fUpdate15;
-		mutable bool fUpdate16;
+	// caching
+	mutable	uint16				fColor16;
+	mutable	uint16				fColor15;
+	mutable	uint8				fColor8;
+	mutable	bool				fUpdate8;
+	mutable	bool				fUpdate15;
+	mutable	bool				fUpdate16;
 };
 
 #endif	// RGB_COLOR_H
