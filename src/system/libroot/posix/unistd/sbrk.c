506c8fbbbff63d14f464e580ab1a6d64b48c86fc
Axel Dörfler <axeld@pinc-software.de>
2006-05-25 18:17:55 +0000

    Disabled sbrk() since it doesn't seem to work yet (makes GDB crash).
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@17590 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/libroot/posix/unistd/sbrk.c
+++ src/system/libroot/posix/unistd/sbrk.c
@@ -1,19 +1,22 @@
 /*
-** Copyright 2004, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
-** Distributed under the terms of the Haiku License.
-*/
+ * Copyright 2004-2006, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
 
 
 #include <unistd.h>
 #include <errno.h>
 
+
 /* in hoard wrapper */
 extern void *(*sbrk_hook)(long);
+
 
 void *
 sbrk(long increment)
 {
-	if (sbrk_hook)
-		return (*sbrk_hook)(increment);
+// TODO: disabled for now - let's GDB crash, so it obviously doesn't work yet
+//	if (sbrk_hook)
+//		return (*sbrk_hook)(increment);
 	return NULL;
 }
