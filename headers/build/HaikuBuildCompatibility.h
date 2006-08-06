340ac5d007353be5f8bb9bd794f21822bb2e0739
Axel Dörfler <axeld@pinc-software.de>
2006-08-06 12:51:10 +0000

    After Ingo's changes, fixed build for target Dano and probably more.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@18415 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/build/HaikuBuildCompatibility.h
+++ headers/build/HaikuBuildCompatibility.h
@@ -3,9 +3,12 @@
 
 #include <sys/types.h>
 
-#define _BE_ERRNO_H_
-	// this is what Dano/Zeta is using
-#include <Errors.h>
+#ifdef HAIKU_TARGET_PLATFORM_LIBBE_TEST
+#	define _BE_ERRNO_H_
+		// this is what Dano/Zeta is using
+#	include <Errors.h>
+#endif
+
 #include <string.h>
 
 // no addr_t under standard BeOS
@@ -13,17 +16,15 @@ typedef unsigned long haiku_build_addr_t;
 #define addr_t haiku_build_addr_t
 
 #ifndef DEFFILEMODE
-#define	DEFFILEMODE	(S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH)
+#	define	DEFFILEMODE	(S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH)
 #endif
 
 #ifndef FS_WRITE_FSINFO_NAME
-#define	FS_WRITE_FSINFO_NAME	0x0001
+#	define	FS_WRITE_FSINFO_NAME	0x0001
 #endif
 
 #ifndef B_CURRENT_TEAM
-#define B_CURRENT_TEAM	0
-
-
+#	define B_CURRENT_TEAM	0
 #endif
 
 
@@ -41,19 +42,19 @@ extern char		*strcasestr(const char *string, const char *searchString);
 extern float	roundf(float value);
 
 #ifdef __cplusplus
-} // extern "C"
+}
 #endif
 
 // These are R1-specific extensions
-#ifndef HAIKU_TARGET_PLATFORM_HAIKU
+#ifndef HAIKU_TARGET_PLATFORM_LIBBE_TEST
+#	define B_TRANSLATION_MAKE_VERSION(major,minor,revision) ((major << 8) | ((minor << 4) & 0xf0) | (revision & 0x0f))
+#	define B_TRANSLATION_MAJOR_VERSION(v) (v >> 8)
+#	define B_TRANSLATION_MINOR_VERSION(v) ((v >> 4) & 0xf)
+#	define B_TRANSLATION_REVISION_VERSION(v) (v & 0xf)
+#endif	// HAIKU_TARGET_PLATFORM_HAIKU
 
-#define B_TRANSLATION_MAKE_VERSION(major,minor,revision) ((major << 8) | ((minor << 4) & 0xf0) | (revision & 0x0f))
-#define B_TRANSLATION_MAJOR_VERSION(v) (v >> 8)
-#define B_TRANSLATION_MINOR_VERSION(v) ((v >> 4) & 0xf)
-#define B_TRANSLATION_REVISION_VERSION(v) (v & 0xf)
-
-#define B_BAD_DATA (B_NOT_ALLOWED+1)
-
+#ifdef HAIKU_TARGET_PLATFORM_BEOS
+#	define B_BAD_DATA (B_NOT_ALLOWED+1)
 #endif
 
 #endif	// HAIKU_BUILD_COMPATIBILITY_H
