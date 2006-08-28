096e7f3b926aa844041ba56a686299c95a0aa490
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2006-08-28 10:10:27 +0000

    Adjusted license headers
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@18658 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kits/interface/BTextView/LineBuffer.h
+++ src/kits/interface/BTextView/LineBuffer.h
@@ -1,38 +1,20 @@
-//------------------------------------------------------------------------------
-//	Copyright (c) 2001-2004, Haiku, Inc.
-//
-//	Permission is hereby granted, free of charge, to any person obtaining a
-//	copy of this software and associated documentation files (the "Software"),
-//	to deal in the Software without restriction, including without limitation
-//	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-//	and/or sell copies of the Software, and to permit persons to whom the
-//	Software is furnished to do so, subject to the following conditions:
-//
-//	The above copyright notice and this permission notice shall be included in
-//	all copies or substantial portions of the Software.
-//
-//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-//	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-//	DEALINGS IN THE SOFTWARE.
-//
-//	File Name:		LineBuffer.h
-//	Author:			Marc Flerackers (mflerackers@androme.be)
-//	Description:	Line storage used by BTextView
-//------------------------------------------------------------------------------
+/*
+ * Copyright 2001-2006, Haiku, Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Marc Flerackers (mflerackers@androme.be)
+ */
 
 #include <SupportDefs.h>
 #include "TextViewSupportBuffer.h"
 
-typedef struct STELine {
-	long			offset;		// offset of first character of line
-	float			origin;		// pixel position of top of line
-	float			ascent;		// maximum ascent for line
-	float			width;		// not used for now, but could be
-} STELine;
+struct STELine {
+	long		offset;		// offset of first character of line
+	float		origin;		// pixel position of top of line
+	float		ascent;		// maximum ascent for line
+	float		width;		// not used for now, but could be
+};
 
 
 // _BLineBuffer_ class ---------------------------------------------------------
