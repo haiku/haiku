4ab9bd97788dbdd49bb257aba4104f65c57318e1
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2006-08-29 07:30:30 +0000

    Fix build on R5. Apparantly HaikuBuildCompatability.h (or something like that) is automatically included, so this define is useless.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@18685 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/kernel/drivers/network/sis900/sis900.h
+++ src/add-ons/kernel/drivers/network/sis900/sis900.h
@@ -29,10 +29,6 @@ enum SiS_revisions {
 #define DEVICE_NAME "sis900"
 #define DEVICE_DRIVERNAME "net/" DEVICE_NAME
 
-#ifndef __HAIKU__
-	typedef unsigned long addr_t;
-#endif
-
 
 /***************************** Buffer & Buffer Descriptors *****************************/
 
