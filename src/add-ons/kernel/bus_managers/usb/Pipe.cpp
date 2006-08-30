haiku/haiku

commit ffe3f107c4ae65ec52128bb36115779fe07a01c0

* Redisigned some of the USB Stack. Unified constructors, removed redundant functions and moved some other functions around * Implemented the EHCIRootHub * Reworked UHCIRootHub to be more of a utility class and moved the actual work into the UHCI BusManager * Implemented port reset in EHCI. Ports with low and fullspeed devices are now correctly handed over to the companion host controller.
Attachs and detachs as well as port resetting / enabling works now in EHCI. Only the actual data transfers are missing thereafter ;-)

git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@18656 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/add-ons/kernel/bus_managers/usb/Pipe.cpp
@@ -10,30 +10,13 @@
 #include "usb_p.h"
 
 
-Pipe::Pipe(Device *device, pipeDirection direction, pipeSpeed speed,
-	uint8 endpointAddress, size_t maxPacketSize)
-	:	Object(device->Manager()),
-		fDevice(device),
-		fDeviceAddress(device->Address()),
-		fBus(device->Manager()),
+Pipe::Pipe(BusManager *bus, int8 deviceAddress, pipeDirection direction,
+	pipeSpeed speed, uint8 endpointAddress, size_t maxPacketSize)
+	:	Object(bus),
+		fDeviceAddress(deviceAddress),
+		fEndpointAddress(endpointAddress),
 		fDirection(direction),
 		fSpeed(speed),
-		fEndpoint(endpointAddress),
-		fMaxPacketSize(maxPacketSize),
-		fDataToggle(false)
-{
-}
-
-
-Pipe::Pipe(BusManager *bus, int8 deviceAddress, pipeSpeed speed,
-	size_t maxPacketSize)
-	:	Object(bus),
-		fDevice(NULL),
-		fDeviceAddress(deviceAddress),
-		fBus(bus),
-		fDirection(Default),
-		fSpeed(speed),
-		fEndpoint(0),
 		fMaxPacketSize(maxPacketSize),
 		fDataToggle(true)
 {
@@ -45,21 +28,11 @@ Pipe::~Pipe()
 }
 
 
-int8
-Pipe::DeviceAddress()
-{
-	if (!fDevice)
-		return fDeviceAddress;
-
-	return fDevice->Address();
-}
-
-
 status_t
 Pipe::SubmitTransfer(Transfer *transfer)
 {
 	// ToDo: keep track of all submited transfers to be able to cancel them
-	return fBus->SubmitTransfer(transfer);
+	return GetBusManager()->SubmitTransfer(transfer);
 }
 
 
@@ -70,68 +43,16 @@ Pipe::CancelQueuedTransfers()
 }
 
 
-status_t
-Pipe::SetFeature(uint16 selector)
-{
-	if (!fDevice)
-		return B_ERROR;
-
-	return fDevice->SendRequest(
-		USB_REQTYPE_STANDARD | USB_REQTYPE_ENDPOINT_OUT,
-		USB_REQUEST_SET_FEATURE,
-		selector,
-		0,
-		0,
-		NULL,
-		0,
-		NULL);
-}
-
-
-status_t
-Pipe::ClearFeature(uint16 selector)
-{
-	if (!fDevice)
-		return B_ERROR;
-
-	return fDevice->SendRequest(
-		USB_REQTYPE_STANDARD | USB_REQTYPE_ENDPOINT_OUT,
-		USB_REQUEST_CLEAR_FEATURE,
-		selector,
-		0,
-		0,
-		NULL,
-		0,
-		NULL);
-}
-
-
-status_t
-Pipe::GetStatus(uint16 *status)
-{
-	if (!fDevice)
-		return B_ERROR;
-
-	return fDevice->SendRequest(
-		USB_REQTYPE_STANDARD | USB_REQTYPE_ENDPOINT_IN,
-		USB_REQUEST_GET_STATUS,
-		0,
-		0,
-		2,
-		(void *)status,
-		2,
-		NULL);
-}
-
-
 //
 // #pragma mark -
 //
 
 
-InterruptPipe::InterruptPipe(Device *device, pipeDirection direction,
-	pipeSpeed speed, uint8 endpointAddress, size_t maxPacketSize)
-	:	Pipe(device, direction, speed, endpointAddress, maxPacketSize)
+InterruptPipe::InterruptPipe(BusManager *bus, int8 deviceAddress,
+	pipeDirection direction, pipeSpeed speed, uint8 endpointAddress,
+	size_t maxPacketSize)
+	:	Pipe(bus, deviceAddress, direction, speed, endpointAddress,
+			maxPacketSize)
 {
 }
 
@@ -159,9 +80,11 @@ InterruptPipe::QueueInterrupt(void *data, size_t dataLength,
 //
 
 
-BulkPipe::BulkPipe(Device *device, pipeDirection direction,
-	pipeSpeed speed, uint8 endpointAddress, size_t maxPacketSize)
-	:	Pipe(device, direction, speed, endpointAddress, maxPacketSize)
+BulkPipe::BulkPipe(BusManager *bus, int8 deviceAddress,
+	pipeDirection direction, pipeSpeed speed, uint8 endpointAddress,
+	size_t maxPacketSize)
+	:	Pipe(bus, deviceAddress, direction, speed, endpointAddress,
+			maxPacketSize)
 {
 }
 
@@ -207,9 +130,11 @@ BulkPipe::QueueBulkV(iovec *vector, size_t vectorCount,
 //
 
 
-IsochronousPipe::IsochronousPipe(Device *device, pipeDirection direction,
-	pipeSpeed speed, uint8 endpointAddress, size_t maxPacketSize)
-	:	Pipe(device, direction, speed, endpointAddress, maxPacketSize)
+IsochronousPipe::IsochronousPipe(BusManager *bus, int8 deviceAddress,
+	pipeDirection direction, pipeSpeed speed, uint8 endpointAddress,
+	size_t maxPacketSize)
+	:	Pipe(bus, deviceAddress, direction, speed, endpointAddress,
+			maxPacketSize)
 {
 }
 
@@ -235,16 +160,9 @@ struct transfer_result_data {
 };
 
 
-ControlPipe::ControlPipe(Device *device, pipeSpeed speed,
-	uint8 endpointAddress, size_t maxPacketSize)
-	:	Pipe(device, Pipe::Default, speed, endpointAddress, maxPacketSize)
-{
-}
-
-
 ControlPipe::ControlPipe(BusManager *bus, int8 deviceAddress, pipeSpeed speed,
-	size_t maxPacketSize)
-	:	Pipe(bus, deviceAddress, speed, maxPacketSize)
+	uint8 endpointAddress, size_t maxPacketSize)
+	:	Pipe(bus, deviceAddress, Default, speed, endpointAddress, maxPacketSize)
 {
 }
 
@@ -321,4 +239,49 @@ ControlPipe::QueueRequest(uint8 requestType, uint8 request, uint16 value,
 	if (result < B_OK)
 		delete transfer;
 	return result;
+}
+
+
+status_t
+ControlPipe::SetFeature(uint16 selector)
+{
+	return SendRequest(
+		USB_REQTYPE_STANDARD | USB_REQTYPE_ENDPOINT_OUT,
+		USB_REQUEST_SET_FEATURE,
+		selector,
+		0,
+		0,
+		NULL,
+		0,
+		NULL);
+}
+
+
+status_t
+ControlPipe::ClearFeature(uint16 selector)
+{
+	return SendRequest(
+		USB_REQTYPE_STANDARD | USB_REQTYPE_ENDPOINT_OUT,
+		USB_REQUEST_CLEAR_FEATURE,
+		selector,
+		0,
+		0,
+		NULL,
+		0,
+		NULL);
+}
+
+
+status_t
+ControlPipe::GetStatus(uint16 *status)
+{
+	return SendRequest(
+		USB_REQTYPE_STANDARD | USB_REQTYPE_ENDPOINT_IN,
+		USB_REQUEST_GET_STATUS,
+		0,
+		0,
+		2,
+		(void *)status,
+		2,
+		NULL);
 }

src/add-ons/kernel/bus_managers/usb/BusManager.cpp
@@ -28,11 +28,11 @@ BusManager::BusManager(Stack *stack)
 		fDeviceMap[i] = false;
 
 	// Set up the default pipes
-	fDefaultPipe = new(std::nothrow) ControlPipe(this, 0, Pipe::NormalSpeed, 8);
+	fDefaultPipe = new(std::nothrow) ControlPipe(this, 0, Pipe::FullSpeed, 0, 8);
 	if (!fDefaultPipe)
 		return;
 
-	fDefaultPipeLowSpeed = new(std::nothrow) ControlPipe(this, 0, Pipe::LowSpeed, 8);
+	fDefaultPipeLowSpeed = new(std::nothrow) ControlPipe(this, 0, Pipe::LowSpeed, 0, 8);
 	if (!fDefaultPipeLowSpeed)
 		return;
 
@@ -135,7 +135,7 @@ BusManager::AllocateNewDevice(Device *parent, bool lowSpeed)
 
 	// Create a temporary pipe with the new address
 	ControlPipe pipe(this, deviceAddress,
-		lowSpeed ? Pipe::LowSpeed : Pipe::NormalSpeed, 8);
+		lowSpeed ? Pipe::LowSpeed : Pipe::FullSpeed, 0, 8);
 
 	// Get the device descriptor
 	// Just retrieve the first 8 bytes of the descriptor -> minimum supported
@@ -257,14 +257,6 @@ BusManager::Stop()
 
 status_t
 BusManager::SubmitTransfer(Transfer *transfer)
-{
-	// virtual function to be overridden
-	return B_ERROR;
-}
-
-
-status_t
-BusManager::SubmitRequest(Transfer *transfer)
 {
 	// virtual function to be overridden
 	return B_ERROR;

src/add-ons/kernel/bus_managers/usb/Device.cpp
@@ -13,14 +13,13 @@
 Device::Device(BusManager *bus, Device *parent, usb_device_descriptor &desc,
 	int8 deviceAddress, bool lowSpeed)
 	:	ControlPipe(bus, deviceAddress,
-			lowSpeed ? Pipe::LowSpeed : Pipe::NormalSpeed,
+			lowSpeed ? Pipe::LowSpeed : Pipe::FullSpeed, 0,
 			desc.max_packet_size_0),
 		fDeviceDescriptor(desc),
 		fConfigurations(NULL),
 		fCurrentConfiguration(NULL),
 		fInitOK(false),
 		fLowSpeed(lowSpeed),
-		fBus(bus),
 		fParent(parent),
 		fDeviceAddress(deviceAddress),
 		fLock(-1),
@@ -60,7 +59,7 @@ Device::Device(BusManager *bus, Device *parent, usb_device_descriptor &desc,
 	TRACE(("\tvendor_id:...........0x%04x\n", fDeviceDescriptor.vendor_id));
 	TRACE(("\tproduct_id:..........0x%04x\n", fDeviceDescriptor.product_id));
 	TRACE(("\tdevice_version:......0x%04x\n", fDeviceDescriptor.device_version));
-	TRACE(("\tmanufacturer:........0x%04x\n", fDeviceDescriptor.manufacturer));
+	TRACE(("\tmanufacturer:........0x%02x\n", fDeviceDescriptor.manufacturer));
 	TRACE(("\tproduct:.............0x%02x\n", fDeviceDescriptor.product));
 	TRACE(("\tserial_number:.......0x%02x\n", fDeviceDescriptor.serial_number));
 	TRACE(("\tnum_configurations:..%d\n", fDeviceDescriptor.num_configurations));
@@ -147,7 +146,8 @@ Device::Device(BusManager *bus, Device *parent, usb_device_descriptor &desc,
 					interfaceInfo->generic_count = 0;
 					interfaceInfo->generic = NULL;
 
-					Interface *interface = new(std::nothrow) Interface(this);
+					Interface *interface = new(std::nothrow) Interface(GetBusManager(),
+						fDeviceAddress, Speed());
 					interfaceInfo->handle = interface->USBID();
 
 					currentInterface = interfaceInfo;
@@ -182,14 +182,15 @@ Device::Device(BusManager *bus, Device *parent, usb_device_descriptor &desc,
 					Pipe *endpoint = NULL;
 					switch (endpointDescriptor->attributes & 0x03) {
 						case 0x00: /* Control Endpoint */
-							endpoint = new(std::nothrow) ControlPipe(this,
-								Speed(),
+							endpoint = new(std::nothrow) ControlPipe(GetBusManager(),
+								fDeviceAddress, Speed(),
 								endpointDescriptor->endpoint_address & 0x0f,
 								endpointDescriptor->max_packet_size);
 							break;
 
 						case 0x01: /* Isochronous Endpoint */
-							endpoint = new(std::nothrow) IsochronousPipe(this,
+							endpoint = new(std::nothrow) IsochronousPipe(GetBusManager(),
+								fDeviceAddress,
 								endpointDescriptor->endpoint_address & 0x80 > 0 ? Pipe::In : Pipe::Out,
 								Speed(),
 								endpointDescriptor->endpoint_address & 0x0f,
@@ -197,7 +198,8 @@ Device::Device(BusManager *bus, Device *parent, usb_device_descriptor &desc,
 							break;
 
 						case 0x02: /* Bulk Endpoint */
-							endpoint = new(std::nothrow) BulkPipe(this,
+							endpoint = new(std::nothrow) BulkPipe(GetBusManager(),
+								fDeviceAddress,
 								endpointDescriptor->endpoint_address & 0x80 > 0 ? Pipe::In : Pipe::Out,
 								Speed(),
 								endpointDescriptor->endpoint_address & 0x0f,
@@ -205,7 +207,8 @@ Device::Device(BusManager *bus, Device *parent, usb_device_descriptor &desc,
 							break;
 
 						case 0x03: /* Interrupt Endpoint */
-							endpoint = new(std::nothrow) InterruptPipe(this,
+							endpoint = new(std::nothrow) InterruptPipe(GetBusManager(),
+								fDeviceAddress,
 								endpointDescriptor->endpoint_address & 0x80 > 0 ? Pipe::In : Pipe::Out,
 								Speed(),
 								endpointDescriptor->endpoint_address & 0x0f,

src/add-ons/kernel/bus_managers/usb/Hub.cpp
@@ -180,7 +180,7 @@ Hub::Explore()
 					continue;
 				}
 
-				Device *newDevice = fBus->AllocateNewDevice(this,
+				Device *newDevice = GetBusManager()->AllocateNewDevice(this,
 					(fPortStatus[i].status & PORT_STATUS_LOW_SPEED) > 0);
 
 				if (newDevice) {
@@ -273,7 +273,7 @@ Hub::BuildDeviceName(char *string, uint32 *index, size_t bufferSize,
 	status_t result = Device::BuildDeviceName(string, index, bufferSize, device);
 	if (result < B_OK) {
 		// recursion to parent failed, we're at the root(hub)
-		int32 managerIndex = GetStack()->IndexOfBusManager(Manager());
+		int32 managerIndex = GetStack()->IndexOfBusManager(GetBusManager());
 		*index += snprintf(string + *index, bufferSize - *index, "%ld", managerIndex);
 	}
 

src/add-ons/kernel/bus_managers/usb/Interface.cpp
@@ -9,8 +9,8 @@
 #include "usb_p.h"
 
 
-Interface::Interface(Device *device)
-	:	ControlPipe(device, device->Speed(), 0, 8)
+Interface::Interface(BusManager *bus, int8 deviceAddress, pipeSpeed speed)
+	:	ControlPipe(bus, deviceAddress, speed, 0, 8)
 {
 }
 

src/add-ons/kernel/bus_managers/usb/Object.cpp
@@ -10,7 +10,8 @@
 
 
 Object::Object(BusManager *bus)
+	:	fBusManager(bus),
+		fStack(bus->GetStack()),
+		fUSBID(fStack->GetUSBID(this))
 {
-	fStack = bus->GetStack();
-	fUSBID = fStack->GetUSBID(this);
 }

src/add-ons/kernel/bus_managers/usb/Transfer.cpp
@@ -26,6 +26,9 @@ Transfer::~Transfer()
 	// we take ownership of the request data
 	if (fRequestData)
 		delete fRequestData;
+
+	if (fVector && fVector != &fData)
+		delete[] fVector;
 }
 
 
@@ -50,7 +53,8 @@ Transfer::SetData(uint8 *data, size_t dataLength)
 void
 Transfer::SetVector(iovec *vector, size_t vectorCount)
 {
-	fVector = vector;
+	fVector = new(std::nothrow) iovec[vectorCount];
+	memcpy(fVector, vector, vectorCount * sizeof(iovec));
 	fVectorCount = vectorCount;
 }
 

src/add-ons/kernel/bus_managers/usb/usb.cpp
@@ -145,10 +145,10 @@ set_feature(usb_id handle, uint16 selector)
 {
 	TRACE(("usb_module: set_feature(0x%08x, %d)\n", handle, selector));
 	Object *object = gUSBStack->GetObject(handle);
-	if (!object || (object->Type() & USB_OBJECT_PIPE) == 0)
+	if (!object || (object->Type() & USB_OBJECT_CONTROL_PIPE) == 0)
 		return B_BAD_VALUE;
 
-	return ((Pipe *)object)->SetFeature(selector);
+	return ((ControlPipe *)object)->SetFeature(selector);
 }
 
 
@@ -157,10 +157,10 @@ clear_feature(usb_id handle, uint16 selector)
 {
 	TRACE(("usb_module: clear_feature(0x%08x, %d)\n", handle, selector));
 	Object *object = gUSBStack->GetObject(handle);
-	if (!object || (object->Type() & USB_OBJECT_PIPE) == 0)
+	if (!object || (object->Type() & USB_OBJECT_CONTROL_PIPE) == 0)
 		return B_BAD_VALUE;
 
-	return ((Pipe *)object)->ClearFeature(selector);
+	return ((ControlPipe *)object)->ClearFeature(selector);
 }
 
 
@@ -172,10 +172,10 @@ get_status(usb_id handle, uint16 *status)
 		return B_BAD_VALUE;
 
 	Object *object = gUSBStack->GetObject(handle);
-	if (!object || (object->Type() & USB_OBJECT_PIPE) == 0)
+	if (!object || (object->Type() & USB_OBJECT_CONTROL_PIPE) == 0)
 		return B_BAD_VALUE;
 
-	return ((Pipe *)object)->GetStatus(status);
+	return ((ControlPipe *)object)->GetStatus(status);
 }
 
 

commit 8001b0c61402c8e977de173ce25a81e1a0718b5a

This subtle error (introduced in r18656) caused bulk pipes to stall.
git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@18713 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/add-ons/kernel/bus_managers/usb/Pipe.cpp
@@ -18,7 +18,7 @@ Pipe::Pipe(Object *parent, int8 deviceAddress, uint8 endpointAddress,
 		fDirection(direction),
 		fSpeed(speed),
 		fMaxPacketSize(maxPacketSize),
-		fDataToggle(true)
+		fDataToggle(false)
 {
 }
 