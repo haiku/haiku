80f3aa2d2b709f2e78fac1cc9431d32384748435
Jérôme Duval <korli@users.berlios.de>
2006-09-21 13:04:59 +0000

    style cleanup
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@18904 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/midi2/MidiEndpoint.h
+++ headers/os/midi2/MidiEndpoint.h
@@ -17,8 +17,8 @@ class BMidiEndpoint
 {
 public:
 
-	const char* Name() const;
-	void SetName(const char* name);
+	const char *Name() const;
+	void SetName(const char *name);
 	
 	int32 ID() const;
 
@@ -32,8 +32,8 @@ public:
 	status_t Release();
 	status_t Acquire();
 
-	status_t SetProperties(const BMessage* properties);
-	status_t GetProperties(BMessage* properties) const;
+	status_t SetProperties(const BMessage *properties);
+	status_t GetProperties(BMessage *properties) const;
 
 	status_t Register();
 	status_t Unregister();
@@ -47,7 +47,7 @@ private:
 	friend class BMidiRoster;
 	friend class BPrivate::BMidiRosterLooper;
 	
-	BMidiEndpoint(const char* name);
+	BMidiEndpoint(const char *name);
 	virtual	~BMidiEndpoint();
 
 	virtual void _Reserved1();
@@ -60,7 +60,7 @@ private:
 	virtual void _Reserved8();
 	
 	status_t SendRegisterRequest(bool);
-	status_t SendChangeRequest(BMessage*);
+	status_t SendChangeRequest(BMessage *);
 
 	bool IsRegistered() const;
 	bool LockLooper() const;
