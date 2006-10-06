0d7ec13a8220cee31866cfd5f4aec8af9941912f
Axel Dörfler <axeld@pinc-software.de>
2006-10-06 11:46:23 +0000

    Simplified a bit, removed unneeded function stubs.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@19009 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/runtime_loader/export.c
+++ src/system/runtime_loader/export.c
@@ -27,43 +27,14 @@ export_unload_add_on(image_id id)
 }
 
 
-static status_t
-export_get_image_symbol(image_id id, char const *symbolName, int32 symbolType, void **_location)
-{
-	return get_symbol(id, symbolName, symbolType, _location);
-}
-
-
-static status_t
-export_get_nth_image_symbol(image_id id, int32 num, char *nameBuffer, int32 *_nameLength,
-	int32 *_symbolType, void **_location)
-{
-	return get_nth_symbol(id, num, nameBuffer, _nameLength, _symbolType, _location);
-}
-
-
-static status_t
-export_test_executable(const char *path, uid_t user, gid_t group, char *starter)
-{
-	return test_executable(path, user, group, starter);
-}
-
-
-static status_t
-export_get_next_image_dependency(image_id id, uint32 *cookie, const char **_name)
-{
-	return get_next_image_dependency(id, cookie, _name);
-}
-
-
 struct rld_export gRuntimeLoader = {
 	// dynamic loading support API
 	export_load_add_on,
 	export_unload_add_on,
-	export_get_image_symbol,
-	export_get_nth_image_symbol,
-	export_test_executable,
-	export_get_next_image_dependency
+	get_symbol,
+	get_nth_symbol,
+	test_executable,
+	get_next_image_dependency
 };
 
 
