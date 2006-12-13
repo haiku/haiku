84aa7549bd19a4aa9900acdfc6aca600f6bd9867
Jérôme Duval <korli@users.berlios.de>
2006-12-13 20:00:19 +0000

    this seems to fail on ppc
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@19499 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/libs/fluidsynth/src/fluidsynth_priv.h
+++ src/libs/fluidsynth/src/fluidsynth_priv.h
@@ -26,9 +26,9 @@
 #include "config.h"
 #endif
 
-#if defined(__POWERPC__) && !(defined(__APPLE__) && defined(__MACH__))
-#include "config_maxmsp43.h"
-#endif
+//#if defined(__POWERPC__) && !(defined(__APPLE__) && defined(__MACH__))
+//#include "config_maxmsp43.h"
+//#endif
 
 #if defined(WIN32) && !defined(MINGW32)
 #include "config_win32.h"
