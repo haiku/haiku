2ca64e48861582e4abcb85af81a069c89e1e3ec6
Axel Dörfler <axeld@pinc-software.de>
2007-01-05 19:53:45 +0000

    Minor cleanup.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@19712 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/servers/input/InputServer.cpp
+++ src/servers/input/InputServer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006, Haiku, Inc. All Rights Reserved.
+ * Copyright 2002-2007, Haiku, Inc. All Rights Reserved.
  * Distributed under the terms of the MIT License.
  */
 
@@ -1764,7 +1764,7 @@ extern "C" void
 RegisterDevices(input_device_ref** devices)
 {
 	CALLED();
-};
+}
 
 
 BView *
@@ -1774,8 +1774,11 @@ instantiate_deskbar_item()
 }
 
 
+//	#pragma mark -
+
+
 int
-main()
+main(int /*argc*/, char** /*argv*/)
 {
 	InputServer	*inputServer = new InputServer;
 
