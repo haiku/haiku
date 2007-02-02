a83c8bbffaac52830987276a7353bb0041d3b901
Axel Dörfler <axeld@pinc-software.de>
2007-02-02 10:53:57 +0000

    Moved libroot/posix/kerrno.c to kernel/lib/ where it belongs, and renamed it to kernel_errno.c.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@20044 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/system/kernel/lib/kernel_errno.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2003-2007, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+
+
+#include "thread.h"
+
+#include <errno.h>
+
+
+/*!
+	Kernel space storage for "errno", located in the thread structure
+	(user "errno" can't be changed from kernel internal POSIX calls)
+*/
+int *
+_errnop(void)
+{
+	struct thread *thread = thread_get_current_thread();
+
+	return &thread->kernel_errno;
+}
+
