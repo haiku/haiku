4467a7025088175fdbaa8be9c2903f922284daec
Stephan Aßmus <superstippi@gmx.de>
2007-04-03 10:39:38 +0000

    * created icons to inform about network connectivity status
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@20528 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/servers/net/NetworkStatusIcons.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2007, Haiku. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef NETWORK_STATUS_ICONS_H
+#define NETWORK_STATUS_ICONS_H
+
+enum {
+	kNetworkStatusNoDevice		= 1000,
+	kNetworkStatusNoConnection	= 1001,
+	kNetworkStatusNoSettings	= 1002,
+	kNetworkStatusReady			= 1003
+};
+
+#endif // NETWORK_STATUS_ICONS_H
