d444ac5b51fc3cc7b8f4d75430f9a0234f398903
Jérôme Duval <korli@users.berlios.de>
2007-04-17 18:07:37 +0000

    updated license header
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@20741 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/servers/media_addon/MediaFilePlayer.cpp
+++ src/servers/media_addon/MediaFilePlayer.cpp
@@ -1,28 +1,10 @@
 /*
- * Copyright (c) 2004, Marcus Overhagen <marcus@overhagen.de>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2004-2007, Marcus Overhagen <marcus@overhagen.de>. All rights reserved.
+ * Distributed under the terms of the MIT License.
  */
 
+#include <Entry.h>
+#include <MediaFiles.h>
 #include "MediaFilePlayer.h"
 #include "debug.h"
 
@@ -30,5 +12,8 @@ void
 PlayMediaFile(const char *media_type, const char *media_name)
 {
 	TRACE("PlayMediaFile: type '%s', name '%s'\n", media_type, media_name);
+
+	entry_ref ref;
+	BMediaFiles().GetRefFor(media_type, media_name, &ref);
 }
 
