2006a80079aa065d6043b4081f651dca3e8098cd
Jérôme Duval <korli@users.berlios.de>
2007-04-17 23:14:13 +0000

    now plays the file found for the selected event reclaim and reuse of sounds ressources still to be implemented
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@20742 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/servers/media_addon/MediaFilePlayer.cpp
+++ src/servers/media_addon/MediaFilePlayer.cpp
@@ -1,9 +1,11 @@
 /*
- * Copyright (c) 2004-2007, Marcus Overhagen <marcus@overhagen.de>. All rights reserved.
+ * Copyright (c) 2004, Marcus Overhagen <marcus@overhagen.de>. All rights reserved.
+ * Copyright (c) 2007, Jérôme Duval. All rights reserved.
  * Distributed under the terms of the MIT License.
  */
 
 #include <Entry.h>
+#include <FileGameSound.h>
 #include <MediaFiles.h>
 #include "MediaFilePlayer.h"
 #include "debug.h"
@@ -14,6 +16,15 @@ PlayMediaFile(const char *media_type, const char *media_name)
 	TRACE("PlayMediaFile: type '%s', name '%s'\n", media_type, media_name);
 
 	entry_ref ref;
-	BMediaFiles().GetRefFor(media_type, media_name, &ref);
+	if (BMediaFiles().GetRefFor(media_type, media_name, &ref)!=B_OK
+		|| !BEntry(&ref).Exists())
+		return;
+
+	BFileGameSound *sound = new BFileGameSound(&ref, false);
+	if (sound->InitCheck() == B_OK) {
+		sound->Preload();
+		sound->StartPlaying();
+	}
+	// TODO reclaim sound
 }
 
