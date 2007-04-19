375dc591e670786ef8f90fe337050ff07de5e2ac
Stephan Aßmus <superstippi@gmx.de>
2007-04-19 19:28:57 +0000

    * thought of an extension for the icon editor protocol
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@20755 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/libs/icon/IconEditorProtocol.h
+++ headers/libs/icon/IconEditorProtocol.h
@@ -8,7 +8,7 @@
 #define ICON_EDITOR_PROTOCOL_H
 
 enum {
-	B_EDIT_ICON_DATA	= 'EICN',
+	B_EDIT_ICON_DATA			= 'EICN',
 	// the message needs to contain these fields:
 	// B_MESSENGER_TYPE		"reply to"		the messenger to which
 	//										B_ICON_DATA_EDITED messages should
@@ -16,13 +16,17 @@ enum {
 	// B_VECTOR_ICON_TYPE	"icon data"		OPTIONAL - the original vector icon data
 	//										- if missing, a new icon is created
 
-	B_ICON_DATA_EDITED	= 'ICNE',
+	B_ICON_DATA_EDITED			= 'ICNE',
 	// the message needs to contain these fields:
 	// B_VECTOR_ICON_TYPE	"icon data"		the changed icon data, do with it
 	//										as you please, the message is sent
 	//										to the specified messenger and is
 	//										the result of the user having
 	//										decided to "save" the edited icon
+	B_CANCEL_EDITING_ICON_DATA	= 'CEIC',
+	// the application requesting an icon to be edited is no longer interested
+	// TODO: think of a way to tell the original application if the icon was
+	// still changed, and if it wants to do anything with the changed icon
 };
 
 #endif // ICON_EDITOR_PROTOCOL_H
