81149059e22560693f3fc35c98035e4dacf4c365
Ingo Weinhold <ingo_weinhold@gmx.de>
2007-05-04 07:59:47 +0000

    Removed kludge that was only necessary for building the old bfs_shell.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@21011 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/posix/size_t.h
+++ headers/posix/size_t.h
@@ -1,20 +1,7 @@
 #ifndef _SIZE_T_H_
 #define _SIZE_T_H_
 
-/*
- *    XXX serious hack that doesn't really solve the problem.
- *       As of right now, some versions of the toolchain expect size_t to
- *       be unsigned long (newer ones than 2.95.2 and beos), and the older
- *       ones need it to be unsigned int. It's an actual failure when 
- *       operator new is declared. This will have to be resolved in the future. 
- */
-
-#ifdef __BEOS__
 typedef unsigned long       size_t;
 typedef signed long         ssize_t;
-#else
-typedef unsigned int        size_t;
-typedef signed int          ssize_t;
-#endif
 
 #endif /* _SIZE_T_H_ */
