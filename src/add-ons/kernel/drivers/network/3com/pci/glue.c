18c80ff05451a646eab14d18aed7f9be7c44e804
Hugo Santos <hugosantos@nowhere.fake>
2007-05-10 06:33:49 +0000

    3com driver, added required MII modules.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@21101 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/kernel/drivers/network/3com/pci/glue.c
+++ src/add-ons/kernel/drivers/network/3com/pci/glue.c
@@ -1,7 +1,22 @@
 #include <sys/bus.h>
 
-HAIKU_FBSD_DRIVER_GLUE(3com, xl, pci)
+HAIKU_FBSD_DRIVER_GLUE(3com, xl, pci);
 
-NO_HAIKU_CHECK_DISABLE_INTERRUPTS()
+extern driver_t *DRIVER_MODULE_NAME(bmtphy, miibus);
+extern driver_t *DRIVER_MODULE_NAME(xlphy, miibus);
 
-HAIKU_DRIVER_REQUIREMENTS(FBSD_TASKQUEUES);
+driver_t *__haiku_select_miibus_driver(device_t dev)
+{
+	driver_t *drivers[] = {
+		DRIVER_MODULE_NAME(bmtphy, miibus),
+		DRIVER_MODULE_NAME(xlphy, miibus)
+	};
+
+	return __haiku_probe_miibus(dev, drivers, 2);
+}
+
+/* TODO interrupt handling */
+NO_HAIKU_CHECK_DISABLE_INTERRUPTS();
+NO_HAIKU_REENABLE_INTERRUPTS();
+
+HAIKU_DRIVER_REQUIREMENTS(FBSD_TASKQUEUES | FBSD_SWI_TASKQUEUE);
