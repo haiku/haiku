7de9382792997bffa21a826e3ba1284e219de4a3
Ithamar R. Adema <ithamar.adema@team-embedded.nl>
2007-05-13 22:41:03 +0000

    * Set clock values based on card type.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@21134 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/kernel/drivers/graphics/intel_extreme/intel_extreme.cpp
+++ src/add-ons/kernel/drivers/graphics/intel_extreme/intel_extreme.cpp
@@ -417,9 +417,17 @@ intel_extreme_init(intel_info &info)
 	info.shared_info->graphics_memory_size = totalSize;
 	info.shared_info->frame_buffer_offset = 0;
 	info.shared_info->dpms_mode = B_DPMS_ON;
-	info.shared_info->pll_info.reference_frequency = 48000;	// 48 kHz
-	info.shared_info->pll_info.min_frequency = 25000;		// 25 MHz (not tested)
-	info.shared_info->pll_info.max_frequency = 350000;		// 350 MHz RAM DAC speed
+
+	if (info.device_type == INTEL_TYPE_9xx) {
+		info.shared_info->pll_info.reference_frequency = 96000;	// 96 kHz
+		info.shared_info->pll_info.max_frequency = 400000;		// 400 MHz RAM DAC speed
+		info.shared_info->pll_info.min_frequency = 20000;		// 20 MHz (not tested)
+	} else {
+		info.shared_info->pll_info.reference_frequency = 48000;	// 48 kHz
+		info.shared_info->pll_info.max_frequency = 350000;		// 350 MHz RAM DAC speed
+		info.shared_info->pll_info.min_frequency = 25000;		// 25 MHz (not tested)
+	}
+
 	info.shared_info->pll_info.divisor_register = INTEL_DISPLAY_A_PLL_DIVISOR_0;
 
 	info.shared_info->device_type = info.device_type;
