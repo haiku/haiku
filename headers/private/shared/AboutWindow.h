b2a409507e761c0a588b85369a9c91b7df6abfd5
Ryan Leavengood <leavengood@gmail.com>
2007-06-13 02:36:18 +0000

    Adding the header.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@21403 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/shared/AboutWindow.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2007 Haiku, Inc.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Ryan Leavengood, leavengood@gmail.com
+ */
+#ifndef B_ABOUT_WINDOW_H
+#define B_ABOUT_WINDOW_H
+
+
+#include <String.h>
+
+
+class BAboutWindow {
+	public:
+		BAboutWindow(char *appName, int32 firstCopyrightYear,
+			const char **authors, char *extraInfo = NULL);
+		virtual ~BAboutWindow();
+
+		void Show();
+
+	private:
+		BString* 		fAppName;
+		BString*		fText;
+};
+
+#endif	// B_ABOUT_WINDOW_H
