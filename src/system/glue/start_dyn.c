ee76fc8f163747b64c64793a695698f0a65f4b73
Axel Dörfler <axeld@pinc-software.de>
2007-06-20 01:06:48 +0000

    The SimpleTest rule is now using the Haiku glue code - so it should better be 100% compatible; turns out _init_c_library_() gets the arguments and environment as parameters (in case you wondered why __libc_argv and __libc_argc wouldn't work anymore for these apps all of a sudden).
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@21469 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/glue/start_dyn.c
+++ src/system/glue/start_dyn.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2006, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+ * Copyright 2003-2007, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
  * Distributed under the terms of the MIT License.
  *
  * Copyright 2001, Travis Geiselbrecht. All rights reserved.
@@ -14,7 +14,7 @@
 
 
 extern int main(int argc, char **argv, char **env);
-extern void _init_c_library_(void);
+extern void _init_c_library_(int argc, char **argv, char **env);
 extern void _call_init_routines_(void);
 
 int _start(int argc, char **argv, char **env);
@@ -37,7 +37,7 @@ _start(int argc, char **argv, char **environment)
 
 	// These two are called to make our glue code usable under BeOS R5
 	// - in Haiku, they are both empty.
-	_init_c_library_();
+	_init_c_library_(argc, argv, environment);
 	_call_init_routines_();
 
 	returnCode = main(argc, argv, environment);
