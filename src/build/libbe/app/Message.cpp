ccaa095f9c7260a3dc8980b6612c4bb8c80cc1e8
Ingo Weinhold <ingo_weinhold@gmx.de>
2007-07-03 14:17:03 +0000

    <malloc.h> ain't a standard header. Should fix the build on platforms that don't have it.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@21546 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/build/libbe/app/Message.cpp
+++ src/build/libbe/app/Message.cpp
@@ -25,8 +25,8 @@
 
 #include <assert.h>
 #include <ctype.h>
-#include <malloc.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 
--- src/kits/app/Message.cpp
+++ src/kits/app/Message.cpp
@@ -28,8 +28,8 @@
 
 #include <assert.h>
 #include <ctype.h>
-#include <malloc.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 
