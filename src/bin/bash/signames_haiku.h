d348ab947fdc4a0adb384457184d6c4655a8a847
Ingo Weinhold <ingo_weinhold@gmx.de>
2007-08-28 12:07:49 +0000

    mksignames is not at all cross-compilation-friendly, since it includes the host platform's <signal.h> to build the signal name array from. We use a hand-edited signames.h when cross-compiling for Haiku, now. The shell does now show the correct signal names when having been compiled under Linux. Moreover we avoid bug #1428 (mksignames being broken under FreeBSD).
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@22092 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/bin/bash/signames_haiku.h
@@ -0,0 +1,39 @@
+/* This file was automatically created by /home/bonefish/develop/haiku/haiku/generated-gcc2/objects/linux/x86/release/bin/bash/mksignames.
+   Do not edit.  Edit support/mksignames.c instead. */
+
+/* A translation list so we can be polite to our users. */
+char *signal_names[NSIG + 3] = {
+    "EXIT",
+    "SIGHUP",
+    "SIGINT",
+    "SIGQUIT",
+    "SIGILL",
+    "SIGCHLD",
+    "SIGABRT",
+    "SIGPIPE",
+    "SIGFPE",
+    "SIGKILL",
+    "SIGSTOP",
+    "SIGSEGV",
+    "SIGCONT",
+    "SIGTSTP",
+    "SIGALRM",
+    "SIGTERM",
+    "SIGTTIN",
+    "SIGTTOU",
+    "SIGUSR1",
+    "SIGUSR2",
+    "SIGWINCH",
+    "SIGKILLTHR",
+    "SIGTRAP",
+    "SIGPOLL",
+    "SIGPROF",
+    "SIGSYS",
+    "SIGURG",
+    "SIGVTALRM",
+    "SIGXCPU",
+    "SIGXFSZ",
+    "DEBUG",
+    "ERR",
+    (char *)0x0
+};
