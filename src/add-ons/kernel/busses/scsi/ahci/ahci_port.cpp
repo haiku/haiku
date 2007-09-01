9815383e54ccce94caaf8b92874211cec907405c
Marcus Overhagen <marcusoverhagen@gmail.com>
2007-09-01 18:51:03 +0000

    a class for the SATA ports
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@22143 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/add-ons/kernel/busses/scsi/ahci/ahci_port.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2007, Marcus Overhagen. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+
+#include "ahci_port.h"
+
+#include <KernelExport.h>
+#include <stdio.h>
+
+#define TRACE(a...) dprintf("\33[34mahci:\33[0m " a)
+#define FLOW(a...)	dprintf("ahci: " a)
+
+
+AHCIPort::AHCIPort(AHCIController *controller, int index)
+	: fIndex(index)
+{
+}
+				
+
+AHCIPort::~AHCIPort()
+{
+}
+
+	
+status_t
+AHCIPort::Init()
+{
+	TRACE("AHCIPort::Init port %d\n", fIndex);
+	return B_OK;
+}
+
+
+void		
+AHCIPort::Uninit()
+{
+	TRACE("AHCIPort::Uninit port %d\n", fIndex);
+}
---
+++ src/add-ons/kernel/busses/scsi/ahci/ahci_port.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2007, Marcus Overhagen. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _AHCI_PORT_H
+#define _AHCI_PORT_H
+
+#include "ahci_defs.h"
+
+class AHCIController;
+
+class AHCIPort {
+public:
+				AHCIPort(AHCIController *controller, int index);
+				~AHCIPort();
+
+	status_t	Init();
+	void		Uninit();
+
+private:
+	int			fIndex;
+};
+
+#endif	// _AHCI_PORT_H
