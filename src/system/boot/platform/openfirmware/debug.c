1ea618c556f1becd00729f1184861d5d23a948b0
Jérôme Duval <korli@users.berlios.de>
2007-10-07 19:52:24 +0000

    added BOOT_MODE define instead of adding dprintf_no_syslog()
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@22478 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/boot/platform/openfirmware/debug.c
+++ src/system/boot/platform/openfirmware/debug.c
@@ -39,14 +39,3 @@ dprintf(const char *format, ...)
 	va_end(list);
 }
 
-
-void
-dprintf_no_syslog(const char *format, ...)
-{
-	va_list list;
-
-	va_start(list, format);
-	vprintf(format, list);
-	va_end(list);
-}
-
