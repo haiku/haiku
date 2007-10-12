36b89f8f1272ca940cad5f683f65700f16e4a3be
Ingo Weinhold <ingo_weinhold@gmx.de>
2007-10-12 16:49:19 +0000

    Patch by Vasilis Kaoutsis: Style cleanup.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@22518 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/libroot/posix/string/strchr.c
+++ src/system/libroot/posix/string/strchr.c
@@ -1,25 +1,25 @@
 /* 
-** Copyright 2001, Travis Geiselbrecht. All rights reserved.
-** Distributed under the terms of the NewOS License.
-*/
+ * Copyright 2001, Travis Geiselbrecht. All rights reserved.
+ * Distributed under the terms of the NewOS License.
+ */
+
 
 #include <sys/types.h>
 #include <string.h>
 
 
-char *
-strchr(const char *s, int c)
+char*
+strchr(const char* s, int c)
 {
-	for(; *s != (char) c; ++s)
+	for (; *s != (char)c; ++s)
 		if (*s == '\0')
 			return NULL;
-	return (char *)s;
+	return (char*)s;
 }
 
 
-char *
-index(const char *s, int c)
+char*
+index(const char* s, int c)
 {
 	return strchr(s, c);
 }
-
