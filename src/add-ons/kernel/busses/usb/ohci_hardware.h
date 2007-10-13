1b442f19197e315955fc4948a1e9109501abac79
Salvatore Benedetto <salvatore.benedetto@gmail.com>
2007-10-13 09:44:23 +0000

    * Revert Copyright
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@22527 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/kernel/busses/usb/ohci_hardware.h
+++ src/add-ons/kernel/busses/usb/ohci_hardware.h
@@ -1,11 +1,28 @@
-/*
- * Copyright 2005-2008, Haiku Inc. All rights reserved.
- * Distributed under the terms of the MIT License.
- *
- * Authors:
- *		Jan-Rixt Van Hoye
- *		Salvatore Benedetto <salvatore.benedetto@gmail.com>
- */
+//------------------------------------------------------------------------------
+//     Copyright (c) 2005, Jan-Rixt Van Hoye
+//
+//     Permission is hereby granted, free of charge, to any person obtaining a
+//     copy of this software and associated documentation files (the "Software"),
+//     to deal in the Software without restriction, including without limitation
+//     the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//     and/or sell copies of the Software, and to permit persons to whom the
+//     Software is furnished to do so, subject to the following conditions:
+//
+//     The above copyright notice and this permission notice shall be included in
+//     all copies or substantial portions of the Software.
+//
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//     DEALINGS IN THE SOFTWARE.
+//
+//     ----------------------------------------------------------------------------
+//     Authors:
+//     		Salvatore Benedetto <salvatore.benedetto@gmail.com>
+
 
 #ifndef OHCI_HARD_H
 #define OHCI_HARD_H
