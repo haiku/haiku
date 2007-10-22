8c5602829ec1eb6dfce71c1ce87215da37d048bc
François Revol <revol@free.fr>
2007-10-22 23:55:19 +0000

    arch-specific stubs for config manager. Will allow hardcoding ISA devices for platform faking an ISA bus, and the PC floppy and PS/2 ports.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@22672 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/add-ons/kernel/bus_managers/config_manager/arch/x86/config_manager_arch.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2007 Haiku, Inc.
+ * Distributed under the terms of the MIT License.
+ *
+ * arch-specific config manager
+ *
+ * Authors (in chronological order):
+ *              François Revol (revol@free.fr)
+ */
+
+int config_manager_scan_hardcoded(void *)
+{
+	return B_OK;
+}
+
