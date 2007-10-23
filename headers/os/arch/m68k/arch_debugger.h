1bd446cc4476ced6a761d47fc7dd9fb882d807a5
François Revol <revol@free.fr>
2007-10-23 23:44:47 +0000

    Debugger support. Unfinished.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@22695 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/os/arch/m68k/arch_debugger.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2005, Haiku Inc.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _ARCH_M68K_DEBUGGER_H
+#define _ARCH_M68K_DEBUGGER_H
+
+struct debug_cpu_state {
+	uint32	d0;
+	uint32	d1;
+	uint32	d2;
+	uint32	d3;
+	uint32	d4;
+	uint32	d5;
+	uint32	d6;
+	uint32	d7;
+	uint32	a0;
+	uint32	a1;
+	uint32	a2;
+	uint32	a3;
+	uint32	a4;
+	uint32	a5;
+	uint32	a6;
+	uint32	a7;
+	uint32	pc;
+	uint16	sr;
+#warning XXX: missing members!
+	uint32	dummy;
+} __attribute__((aligned(8)));
+
+#endif	// _ARCH_M68K_DEBUGGER_H
