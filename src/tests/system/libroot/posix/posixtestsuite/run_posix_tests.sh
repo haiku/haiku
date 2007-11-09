83274b71159cb8c16a1a0c18ad2adc9b21154a7b
Ingo Weinhold <ingo_weinhold@gmx.de>
2007-11-09 23:36:37 +0000

    Patch by Vasilis Kaoutsis: Shell script to run the POSIX conformance tests.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@22867 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/tests/system/libroot/posix/posixtestsuite/run_posix_tests.sh
@@ -0,0 +1,124 @@
+#!/bin/sh
+
+usage()
+{
+    cat <<EOF 
+Usage: $0 [AIO|MEM|MSG|SEM|SIG|THR|TMR|TPS]
+	Run the tests for POSIX area specified by the 3 letter tag
+	in the POSIX spec
+
+	or
+	$0 all
+	to run all the available tests.
+
+EOF
+}
+
+
+targets()
+{
+	$1
+}
+
+
+asynchronous_input_output_tests()
+{
+	echo "asynchronous_input_output_tests: Not yet implemented."
+	#aio_* lio_listio
+}
+
+
+signals_tests()
+{
+	echo "kill()"
+	conformance/interfaces/kill/kill_2-1
+	echo ""
+	echo "sighold()"
+	conformance/interfaces/sighold/sighold_1-1
+	conformance/interfaces/sighold/sighold_2-1
+	conformance/interfaces/sighold/sighold_3-core-buildonly 1
+	conformance/interfaces/sighold/sighold_3-core-buildonly 2
+	conformance/interfaces/sighold/sighold_3-core-buildonly 3
+	conformance/interfaces/sighold/sighold_3-core-buildonly 4
+	echo ""
+	echo "sigignore()"
+	conformance/interfaces/sigignore/sigignore_1-1
+	conformance/interfaces/sigignore/sigignore_4-1
+	conformance/interfaces/sigignore/sigignore_5-core-buildonly 1
+	conformance/interfaces/sigignore/sigignore_5-core-buildonly 2
+	conformance/interfaces/sigignore/sigignore_5-core-buildonly 3
+	conformance/interfaces/sigignore/sigignore_5-core-buildonly 4
+	conformance/interfaces/sigignore/sigignore_6-1
+	conformance/interfaces/sigignore/sigignore_6-2
+	echo ""
+	echo "sigprocmask()"
+	conformance/interfaces/sigprocmask/sigprocmask_8-1
+	conformance/interfaces/sigprocmask/sigprocmask_8-2
+	conformance/interfaces/sigprocmask/sigprocmask_8-3
+	conformance/interfaces/sigprocmask/sigprocmask_12-1
+	echo ""
+	echo "sigrelse()"
+	conformance/interfaces/sigrelse/sigrelse_1-1
+	conformance/interfaces/sigrelse/sigrelse_2-1
+	conformance/interfaces/sigrelse/sigrelse_3-core-buildonly 1
+	conformance/interfaces/sigrelse/sigrelse_3-core-buildonly 2
+	conformance/interfaces/sigrelse/sigrelse_3-core-buildonly 3
+	conformance/interfaces/sigrelse/sigrelse_3-core-buildonly 4
+	echo ""
+	echo "signal()"
+	conformance/interfaces/signal/signal_1-1
+	conformance/interfaces/signal/signal_2-1
+	conformance/interfaces/signal/signal_3-1
+	conformance/interfaces/signal/signal_5-1
+	conformance/interfaces/signal/signal_6-1
+	conformance/interfaces/signal/signal_7-1
+	echo ""
+	echo "sigsuspend()"
+	echo "FIXME: haiku' sigsuspend can not 'wake up' yet."
+}
+
+all_tests()
+{
+	asynchronous_input_output_tests
+	signals_tests
+}
+
+
+echo "posixtestsuit-1.5.2"
+sleep 1
+
+	case $1 in
+  all) echo "Executing all tests:"
+  	all_tests
+  	;;
+  AIO) echo "Executing asynchronous I/O tests"
+	asynchronous_input_output_tests
+	;;
+  SIG) echo "Executing signals tests"
+	signals_tests
+	;;
+  SEM) echo "Executing semaphores tests"
+	#TODO sem*
+	;;
+  THR) echo "Executing threads tests"
+	#TODO pthread_*
+	;;
+  TMR) echo "Executing timers and clocks tests"
+	#TODO time* *time clock* nanosleep
+	;;
+  MSG) echo "Executing message queues tests"
+	#TODO mq_*
+	;;
+  TPS) echo "Executing process and thread scheduling tests"
+	#TODO *sched*
+	;;
+  MEM) echo "Executing mapped, process and shared memory tests"
+	#TODO m*lock* m*map shm_*
+	;;
+  *)	usage
+	exit 1
+	;;
+esac
+
+
+echo "**** Tests Complete ****"
