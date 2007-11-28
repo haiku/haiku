1ab7a518e9f44495e05e9ca1eae740c7858d20e2
Axel Dörfler <axeld@pinc-software.de>
2007-11-28 16:50:48 +0000

    Added fallback MII to the marvell_yukon driver.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@23012 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/kernel/drivers/network/marvell_yukon/dev/msk/glue.c
+++ src/add-ons/kernel/drivers/network/marvell_yukon/dev/msk/glue.c
@@ -1,10 +1,30 @@
+/*
+ * Copyright 2007, Hugo Santos. All Rights Reserved.
+ * Copyright 2007, Axel Dörfler, axeld@pinc-software.de. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
+
+
 #include <sys/bus.h>
+
 
 HAIKU_FBSD_DRIVER_GLUE(marvell_yukon, mskc, pci)
 
+extern driver_t *DRIVER_MODULE_NAME(ukphy, miibus);
+
+
+driver_t *
+__haiku_select_miibus_driver(device_t dev)
+{
+	driver_t *drivers[] = {
+		DRIVER_MODULE_NAME(ukphy, miibus)
+	};
+
+	return __haiku_probe_miibus(dev, drivers, 1);
+}
+
 NO_HAIKU_CHECK_DISABLE_INTERRUPTS();
 NO_HAIKU_REENABLE_INTERRUPTS();
-NO_HAIKU_FBSD_MII_DRIVER();
 
 #ifdef EM_FAST_INTR
 	HAIKU_DRIVER_REQUIREMENTS(FBSD_TASKQUEUES | FBSD_FAST_TASKQUEUE);
