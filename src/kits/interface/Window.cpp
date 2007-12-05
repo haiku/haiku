ebe41f1a762536773439faaa3ff9f4549c867476
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2007-12-05 12:13:43 +0000

    added a TODO item
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@23063 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kits/interface/Window.cpp
+++ src/kits/interface/Window.cpp
@@ -1683,6 +1683,7 @@ BWindow::UpdateIfNeeded()
 
 	BMessage *msg;
 	for (int32 i = 0; (msg = queue->FindMessage(i)) != NULL; i++) {
+		// TODO: Dispatch more messages ?!?!? Check what beos does.
 		if (msg->what == _UPDATE_ || msg->what == B_WINDOW_ACTIVATED) {
 			BWindow::DispatchMessage(msg, this);
 				// we need to make sure that no overridden method is called 
