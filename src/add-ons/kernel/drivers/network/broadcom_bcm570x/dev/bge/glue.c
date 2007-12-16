1607b8e50319953c52fa9404b0d5c9989289881f
Axel Dörfler <axeld@pinc-software.de>
2007-12-16 10:56:30 +0000

    * Added Broadcom BCM570x driver from FreeBSD (bge). Does not compile yet, needs   a bit more work on the compatibility layer. * Added nVidia nForce MCP driver (nfe) from FreeBSD. Compiles and might even   work - please test. It would also be nice to know if your PHY is detected   correctly (see syslog output).
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@23124 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/add-ons/kernel/drivers/network/broadcom_bcm570x/dev/bge/glue.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2007, Axel Dörfler, axeld@pinc-software.de. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
+
+
+#include <sys/bus.h>
+
+
+HAIKU_FBSD_DRIVER_GLUE(broadcom_bcm570x, bge, pci);
+HAIKU_DRIVER_REQUIREMENTS(FBSD_TASKQUEUES | FBSD_SWI_TASKQUEUE);
+
+
+extern driver_t *DRIVER_MODULE_NAME(ukphy, miibus);
+
+
+driver_t *
+__haiku_select_miibus_driver(device_t dev)
+{
+	driver_t *drivers[] = {
+		DRIVER_MODULE_NAME(ukphy, miibus),
+		NULL
+	};
+
+	return __haiku_probe_miibus(dev, drivers);
+}
+
---
+++ src/add-ons/kernel/drivers/network/nforce/dev/nfe/glue.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2007, Axel Dörfler, axeld@pinc-software.de. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
+
+
+#include <sys/bus.h>
+
+
+HAIKU_FBSD_DRIVER_GLUE(nforce, nfe, pci)
+
+HAIKU_DRIVER_REQUIREMENTS(FBSD_TASKQUEUES | FBSD_FAST_TASKQUEUE);
+NO_HAIKU_CHECK_DISABLE_INTERRUPTS();
+NO_HAIKU_REENABLE_INTERRUPTS();
+
+extern driver_t *DRIVER_MODULE_NAME(ciphy, miibus);
+extern driver_t *DRIVER_MODULE_NAME(ukphy, miibus);
+
+
+driver_t *
+__haiku_select_miibus_driver(device_t dev)
+{
+	driver_t *drivers[] = {
+		DRIVER_MODULE_NAME(ciphy, miibus),
+		DRIVER_MODULE_NAME(ukphy, miibus),
+		NULL
+	};
+
+	return __haiku_probe_miibus(dev, drivers);
+}
+
