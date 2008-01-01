a24251b49bd54a8b1fc328266e94b2a9bc78ec71
François Revol <revol@free.fr>
2008-01-01 19:56:53 +0000

    A single header should be enough for all TOS calls.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@23206 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/system/boot/platform/atari_m68k/toscalls.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2007, Haiku, Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT license.
+ *
+ * Author:
+ *		François Revol, revol@free.fr.
+ */
+
+#ifndef _TOSCALLS_H
+#define _TOSCALLS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* 
+ * Atari BIOS calls
+ */
+
+extern uint32 bios(uint16 nr, ...);
+
+#define Bconin(p0) bios(2, p0)
+//XXX nparams ?
+#define Kbshift(p0) bios(11, p0)
+
+/* 
+ * Atari XBIOS calls
+ */
+
+extern uint32 bios(uint16 nr, ...);
+
+
+/* 
+ * Atari GEMDOS calls
+ */
+
+extern uint32 gemdos(uint16 nr, ...);
+
+// official names
+#define Cconin() gemdos(1)
+#define Super(a) gemdos(0x20, (uint32)a)
+
+// check for names
+#define terminate() GEMDOS(0)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _TOSCALLS_H */
