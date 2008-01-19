907f26dbb5afc96316614fd29fc176f8633d02d5
Jérôme Duval <korli@users.berlios.de>
2008-01-19 15:43:45 +0000

    added a arch_commpage.h header for ppc, dunno if it needs to be changed
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@23637 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/kernel/arch/ppc/arch_commpage.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2007, Travis Geiselbrecht. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _KERNEL_ARCH_PPC_COMMPAGE_H
+#define _KERNEL_ARCH_PPC_COMMPAGE_H
+
+#ifndef _KERNEL_COMMPAGE_H
+#	error Must not be included directly. Include <commpage.h> instead!
+#endif
+
+#define COMMPAGE_ENTRY_PPC_SYSCALL	(COMMPAGE_ENTRY_FIRST_ARCH_SPECIFIC + 0)
+#define COMMPAGE_ENTRY_PPC_MEMCPY	(COMMPAGE_ENTRY_FIRST_ARCH_SPECIFIC + 1)
+
+#define ARCH_USER_COMMPAGE_ADDR (0xffff0000)
+
+#endif	/* _KERNEL_ARCH_PPC_COMMPAGE_H */
