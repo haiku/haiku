5cea25bfaffcff3349f878ed46d07423ed1d19a6
Ryan Leavengood <leavengood@gmail.com>
2008-02-17 01:08:28 +0000

    Adding patch from Juan Sebastian Muñoz Arango to fix bug #1571:
    This creates a new tab Info to contain the information for the media
    mixer instead of placing it next to the setup controls.
    
    
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@23967 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/media/media-add-ons/mixer/AudioMixer.cpp
+++ src/add-ons/media/media-add-ons/mixer/AudioMixer.cpp
@@ -1698,7 +1698,9 @@ AudioMixer::UpdateParameterWeb()
 	group->MakeDiscreteParameter(PARAM_ETC(80), B_MEDIA_RAW_AUDIO, "Refuse output format changes", B_ENABLE);
 	group->MakeDiscreteParameter(PARAM_ETC(90), B_MEDIA_RAW_AUDIO, "Refuse input format changes", B_ENABLE);
 
+	top = web->MakeGroup("Info"); // top level group
 	group = top->MakeGroup("");
+	
 	group->MakeNullParameter(PARAM_ETC(1001), B_MEDIA_RAW_AUDIO, "Info:", B_GENERIC);
 	group->MakeNullParameter(PARAM_ETC(1002), B_MEDIA_RAW_AUDIO, "Haiku audio mixer", B_GENERIC);
 	group->MakeNullParameter(PARAM_ETC(1003), B_MEDIA_RAW_AUDIO, "Version: " VERSION_STRING , B_GENERIC);
