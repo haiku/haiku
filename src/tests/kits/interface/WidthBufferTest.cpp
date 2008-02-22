8dd5c3f974dbad5cd672c44d2c746126d8e09ebb
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2008-02-22 22:30:34 +0000

    Refactored BWidthBuffer test into something more useful. Looks like the class has some problems (hangs while calculating width of lines inside termcap, cf bug #1690
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@24070 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/tests/kits/interface/WidthBufferTest.cpp
@@ -0,0 +1,89 @@
+/* 
+** Copyright 2008, Stefano Ceccherini (stefano.ceccherini@gmail.com).
+** All rights reserved.
+** Distributed under the terms of the Haiku License.
+*/
+
+#include <Application.h>
+#include <Font.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <WidthBuffer.h>
+
+
+class App : public BApplication {
+public:
+	App();
+	~App();
+	virtual void ReadyToRun();
+	
+private:
+	_BWidthBuffer_ *fWidthBuffer;
+	thread_id fThread;
+	
+	int32 TesterFunc();
+	static int32 _thread(void *data);
+	
+};
+
+
+int main()
+{
+	App app;
+	app.Run();
+	return 0;
+}
+
+
+App::App()
+	:BApplication("application/x-vnd-WidthBufferTest")
+{
+	fWidthBuffer = new _BWidthBuffer_;
+	fThread = spawn_thread(App::_thread, "widthbuffer tester", 
+				B_NORMAL_PRIORITY, this);
+}
+
+ 
+void
+App::ReadyToRun()
+{
+	resume_thread(fThread);
+}
+
+
+App::~App()
+{
+	status_t status;
+	wait_for_thread(fThread, &status);
+	delete fWidthBuffer;
+}
+
+
+int32
+App::TesterFunc()
+{
+	FILE *file = fopen("/boot/beos/etc/termcap", "r");
+	if (file != NULL) {
+		char buffer[512];
+		while (fgets(buffer, 512, file)) {
+			float width = fWidthBuffer->StringWidth(buffer, 0,
+							strlen(buffer), be_plain_font);
+			printf("string: %s, width: %f\n", buffer, width);
+		}
+		fclose(file);
+	}
+
+	PostMessage(B_QUIT_REQUESTED);
+	
+	return 0;
+}
+
+
+int32
+App::_thread(void *data)
+{
+	return static_cast<App *>(data)->TesterFunc();
+}
+
--- src/tests/kits/interface/bwidthbuffer/WidthBufferTest.h
+++
@@ -1,8 +0,0 @@
-#ifndef __WIDTHBUFFER_TEST_H
-#define __WIDTHBUFFER_TEST_H
-
-class CppUnit::Test;
-
-CppUnit::Test *WidthBufferTestSuite();
-
-#endif	__WIDTHBUFFER_TEST_H
