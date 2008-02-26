10da43ea2e53061b8cc4648bd9a36d4576069ad7
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2008-02-26 15:59:17 +0000

    Use DejaVu as default fonts, instead of Bitstream Vera. DejaVu is just like Vera, but it has some more glyphs. Actually we could get rid of Vera completely.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@24143 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/servers/app/ServerConfig.h
+++ src/servers/app/ServerConfig.h
@@ -23,17 +23,17 @@
 
 // These definitions provide the server something to use for default
 // system fonts.
-# define DEFAULT_PLAIN_FONT_FAMILY "Bitstream Vera Sans"
+# define DEFAULT_PLAIN_FONT_FAMILY "DejaVu Sans"
 # define FALLBACK_PLAIN_FONT_FAMILY "Swis721 BT"
-# define DEFAULT_PLAIN_FONT_STYLE "Roman"
+# define DEFAULT_PLAIN_FONT_STYLE "Book"
 # define DEFAULT_PLAIN_FONT_SIZE 12.0f
-# define DEFAULT_BOLD_FONT_FAMILY "Bitstream Vera Sans"
+# define DEFAULT_BOLD_FONT_FAMILY "DejaVu Sans"
 # define FALLBACK_BOLD_FONT_FAMILY "Swis721 BT"
 # define DEFAULT_BOLD_FONT_STYLE "Bold"
 # define DEFAULT_BOLD_FONT_SIZE 12.0f
-# define DEFAULT_FIXED_FONT_FAMILY "Bitstream Vera Sans Mono"
+# define DEFAULT_FIXED_FONT_FAMILY "DejaVu Sans Mono"
 # define FALLBACK_FIXED_FONT_FAMILY "Courier10 BT"
-# define DEFAULT_FIXED_FONT_STYLE "Roman"
+# define DEFAULT_FIXED_FONT_STYLE "Book"
 # define DEFAULT_FIXED_FONT_SIZE 12.0f
 // This is the port capacity for all monitoring objects - ServerApps
 // and ServerWindows
