18c2aaaea27d0643b9a470d930355b0410cfbb30
Marcus Overhagen <marcusoverhagen@gmail.com>
2008-03-01 22:33:37 +0000

    Changed the license from GPL to MIT on request by Maurice. I'm the sole author of this code.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@24195 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/media/media-add-ons/videowindow/VideoView.h
+++ src/add-ons/media/media-add-ons/videowindow/VideoView.h
@@ -1,21 +1,7 @@
 /*
- * VideoView.h - "Video Window" media add-on.
+ * Copyright (C) 2006 Marcus Overhagen <marcus@overhagen.de>. All rights reserved.
  *
- * Copyright (C) 2006 Marcus Overhagen <marcus@overhagen.de>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
+ * Distributed under the terms of the MIT License.
  */
 #ifndef __VIDEO_VIEW_H
 #define __VIDEO_VIEW_H
