haiku/haiku

commit 6800e4f8ba370bc79df266a086200a1474ecb581

Reverted r24720, and at least fixed the problematic return code after the first loop - the vnode passed in would still need to be checked, though. Eventually, it would be nice to move the check to the top, though, so that we don't need to search for the patch delimiter twice.
git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@24721 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/system/kernel/fs/vfs.cpp
@@ -1821,15 +1821,6 @@ vnode_path_to_vnode(struct vnode *vnode, char *path, bool traverseLeafLink,
 
 		if (status < B_OK) {
 			put_vnode(vnode);
-                        if (status == B_NOT_ALLOWED) {
-                          // We are only concerned about directories here, so
-                          // B_NOT_ALLOWED is actually B_NOT_A_DIRECTORY. This
-                          // happens because fs_access() can be called on files
-                          // to and this would be valid outside of the context
-                          // of vnode_path_to_vnode(). This is also what POSIX
-                          // programs expect as return code in this case.
-                          return B_NOT_A_DIRECTORY;
-                        }
 			return status;
 		}
 
@@ -1841,15 +1832,16 @@ vnode_path_to_vnode(struct vnode *vnode, char *path, bool traverseLeafLink,
 		mutex_unlock(&sVnodeMutex);
 
 		if (!nextVnode) {
-			// pretty screwed up here - the file system found the vnode, but the hash
-			// lookup failed, so our internal structures are messed up
+			// pretty screwed up here - the file system found the vnode, but the
+			// hash lookup failed, so our internal structures are messed up
 			panic("vnode_path_to_vnode: could not lookup vnode (mountid 0x%lx vnid 0x%Lx)\n",
 				vnode->device, vnodeID);
 			put_vnode(vnode);
 			return B_ENTRY_NOT_FOUND;
 		}
 
-		// If the new node is a symbolic link, resolve it (if we've been told to do it)
+		// If the new node is a symbolic link, resolve it (if we've been told
+		// to do it)
 		if (S_ISLNK(type) && !(!traverseLeafLink && nextPath[0] == '\0')) {
 			size_t bufferSize;
 			char *buffer;
@@ -1936,6 +1928,13 @@ vnode_path_to_vnode(struct vnode *vnode, char *path, bool traverseLeafLink,
 
 		path = nextPath;
 		vnode = nextVnode;
+
+		if (strchr(path, '/') && !S_ISDIR(type)) {
+			// We need to continue to traverse, but our next vnode is not
+			// a directory.
+			put_vnode(vnode);
+			return B_NOT_A_DIRECTORY;
+		}
 
 		// see if we hit a mount point
 		struct vnode *mountPoint = resolve_mount_point_to_volume_root(vnode);

commit 0cc0b6fc8f3451e7e793980e2a3035675805ade8

* Fixed resolution of symlinks path components, broken since   r24721/r24722. After resolving a symlink we weren't updating the local   type variable to the type of the link target, so that the directory   check in the next iteration would always fail. * Added TODO.
git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@24735 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/system/kernel/fs/vfs.cpp
@@ -1756,6 +1756,10 @@ vnode_path_to_vnode(struct vnode *vnode, char *path, bool traverseLeafLink,
 	status_t status = B_OK;
 	ino_t lastParentID = vnode->id;
 	int type = 0;
+		// TODO: Actually the caller could provide the type of the start node,
+		// if known, which it is in most cases (context root, mount root, cwd).
+		// We'd save a now always required read_stat().
+		// Doing that via _type wouldn't even require an interface change.
 
 	FUNCTION(("vnode_path_to_vnode(vnode = %p, path = %s)\n", vnode, path));
 
@@ -1923,9 +1927,10 @@ vnode_path_to_vnode(struct vnode *vnode, char *path, bool traverseLeafLink,
 			if (absoluteSymlink && *path == '\0') {
 				// symlink was just "/"
 				nextVnode = vnode;
+				type = S_IFDIR;
 			} else {
 				status = vnode_path_to_vnode(vnode, path, traverseLeafLink,
-					count + 1, ioContext, &nextVnode, &lastParentID, _type);
+					count + 1, ioContext, &nextVnode, &lastParentID, &type);
 			}
 
 			free(buffer);