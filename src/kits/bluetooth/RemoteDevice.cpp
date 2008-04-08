2154bbe8d665e6e47bc7eb3ab31fedde741cd63c
Oliver Ruiz Dorantes <oliver.ruiz.dorantes@gmail.com>
2008-04-08 20:39:04 +0000

    Added copyright
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@24867 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/kits/bluetooth/RemoteDevice.cpp
+++ src/kits/bluetooth/RemoteDevice.cpp
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2008 Oliver Ruiz Dorantes, oliver.ruiz.dorantes_at_gmail.com
+ *
+ * All rights reserved. Distributed under the terms of the MIT License.
+ *
+ */
+
+
 #include <bluetooth/DiscoveryAgent.h>
 #include <bluetooth/DiscoveryListener.h>
 #include <bluetooth/bdaddrUtils.h>
