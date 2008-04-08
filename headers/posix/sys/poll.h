3d7e508f7b3404a9fd10cc4000436c25e42aa6e8
Axel Dörfler <axeld@pinc-software.de>
2008-04-08 09:17:07 +0000

    Added sys/poll.h header by popular request (see ticket #2037).
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@24864 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/posix/sys/poll.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2008, Haiku Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _SYS_POLL_H
+#define _SYS_POLL_H
+
+
+#include <poll.h>
+	// for compatibility with legacy applications
+
+
+#endif	/* _SYS_POLL_H */
