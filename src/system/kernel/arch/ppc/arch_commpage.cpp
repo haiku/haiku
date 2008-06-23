826e403de59cf7e7431535689447a51df4626381
François Revol <revol@free.fr>
2008-06-23 01:53:36 +0000

    Stubbed commpage stuff for ppc. Someone writeme please...
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@26093 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/system/kernel/arch/ppc/arch_commpage.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2007, Travis Geiselbrecht. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+
+
+#include <commpage.h>
+
+#include <string.h>
+
+#include <KernelExport.h>
+
+#include <cpu.h>
+#include <smp.h>
+
+
+// user syscall assembly stubs
+extern "C" void _user_syscall_int(void);
+extern unsigned int _user_syscall_int_end;
+extern "C" void _user_syscall_sysenter(void);
+extern unsigned int _user_syscall_sysenter_end;
+
+
+status_t
+arch_commpage_init(void)
+{
+#error WRITEME
+	return B_OK;
+}
+
