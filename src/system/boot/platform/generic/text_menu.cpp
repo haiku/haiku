42d85ff725b8ff517aeab4727f437fd8f032b65d
François Revol <revol@free.fr>
2008-06-23 23:50:23 +0000

    Extend copyright years to 2008 for the boot menu.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@26118 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/boot/platform/generic/text_menu.cpp
+++ src/system/boot/platform/generic/text_menu.cpp
@@ -203,7 +203,7 @@ draw_menu(Menu *menu)
 	print_centered(2, "Haiku Boot Loader");
 
 	console_set_color(kCopyrightColor, kBackgroundColor);
-	print_centered(4, "Copyright 2004-2007 Haiku Inc.");
+	print_centered(4, "Copyright 2004-2008 Haiku Inc.");
 
 	if (menu->Title()) {
 		console_set_cursor(kOffsetX, kFirstLine - 2);
