9c557206dbcb571d1e20576e503b7780078f762f
Axel Dörfler <axeld@pinc-software.de>
2008-06-24 09:03:44 +0000

    * Rewrote NetKit.h, and added the libnetapi headers. * This closes bug #2444.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@26120 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/NetKit.h
+++ headers/os/NetKit.h
@@ -1,8 +1,16 @@
 /*
- * Master include file for the Net Kit.
- *
+ * Copyright 2008, Haiku Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
  */
-#include <net_settings.h>
+#ifndef _NET_KIT_H_
+#define _NET_KIT_H_
+
 #include <netdb.h>
-#include <socket.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
+
+#include <NetAddress.h>
+#include <NetBuffer.h>
+#include <NetEndpoint.h>
+
+#endif	// _NET_KIT_H_
