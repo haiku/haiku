ed7c4a3e9a9fbb65a5096b567c70d381ac421750
Axel Dörfler <axeld@pinc-software.de>
2008-08-07 14:25:06 +0000

    * Added simple test app for readdir() to see how it fills out the dirent   and the private DIR parts.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@26860 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/tests/system/libroot/posix/dirent_test.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2008, Axel Dörfler, axeld@pinc-software.de.
+ * Distributed under the terms of the MIT License.
+ */
+
+#include <dirent.h>
+#include <stdio.h>
+
+#include <dirent_private.h>
+
+
+int
+main(int argc, char** argv)
+{
+	DIR* dir = opendir(".");
+
+	while (true) {
+		dirent* dirent = readdir(dir);
+		if (dirent == NULL)
+			break;
+
+		printf("Entry: dev %ld, ino %Ld, name \"%s\"\n", dirent->d_dev,
+			dirent->d_ino, dirent->d_name);
+		printf("  left: %u, next: %d\n", dir->entries_left, dir->next_entry);
+	}
+
+	closedir(dir);
+	return 0;
+}
