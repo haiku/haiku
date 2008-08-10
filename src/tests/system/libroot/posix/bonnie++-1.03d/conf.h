24bd90083335f8550ed80fab0bcb15aa3b893354
Salvatore Benedetto <salvatore.benedetto@gmail.com>
2008-08-10 15:36:25 +0000

    * Adding bonnie++ suite version 1.03d. It includes the binary zcat and bonnie++ itself
    I'm adding it to the same directory of where bonnie was, but I think
    src/test/apps would be a better place.
    
    
    
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@26919 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/tests/system/libroot/posix/bonnie++-1.03d/conf.h
@@ -0,0 +1,10 @@
+/* conf.h.  Generated from conf.h.in by configure.  */
+#ifndef CONF_H
+#define CONF_H
+
+#define HAVE_ALGO_H 1
+/* #undef HAVE_ALGO */
+#define HAVE_ALGORITHM 1
+#define HAVE_VECTOR 1
+
+#endif
