3cdf783e2b2b56c04a415a14db42f23c7caba657
François Revol <revol@free.fr>
2008-08-21 15:03:14 +0000

    Start of a PC-style serial port driver. For now it's mostly just copied parts of usb_serial, not yet usable.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@27111 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/add-ons/kernel/drivers/ports/pc_serial/Tracing.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2007-2008 by Michael Lotz
+ * Heavily based on the original usb_serial driver which is:
+ *
+ * Copyright (c) 2003 by Siarzhuk Zharski <imker@gmx.li>
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _USB_SERIAL_TRACING_H_
+#define _USB_SERIAL_TRACING_H_
+
+void load_settings();
+void create_log_file();
+void usb_serial_trace(bool force, char *format, ...);
+
+#define TRACE_ALWAYS(x...) usb_serial_trace(true, x);
+#define TRACE(x...) usb_serial_trace(false, x);
+
+extern bool gLogFunctionCalls;
+#define TRACE_FUNCALLS(x...) \
+	if (gLogFunctionCalls) \
+		usb_serial_trace(false, x);
+
+extern bool gLogFunctionReturns;
+#define TRACE_FUNCRET(x...) \
+	if (gLogFunctionReturns) \
+		usb_serial_trace(false, x);
+
+extern bool gLogFunctionResults;
+#define TRACE_FUNCRES(func, param) \
+	if (gLogFunctionResults) \
+		func(param);
+
+void trace_ddomain(struct ddomain *dd);
+void trace_termios(struct termios *tios);
+void trace_str(struct str *str);
+void trace_winsize(struct winsize *ws);
+void trace_tty(struct tty *tty);
+
+#endif //_USB_SERIAL_TRACING_H_
