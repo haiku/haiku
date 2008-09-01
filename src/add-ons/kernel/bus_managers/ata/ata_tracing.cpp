24bc53c351b1a1f24f8a7d52c3c45c6bf63c6f01
Marcus Overhagen <marcusoverhagen@gmail.com>
2008-09-01 21:25:44 +0000

    ATA kernel tracing functions.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@27281 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/add-ons/kernel/bus_managers/ata/ata_tracing.cpp
@@ -0,0 +1,61 @@
+#include <tracing.h>
+#include <string.h>
+
+#include "ata_tracing.h"
+#include "ide_internal.h"
+
+#if ATA_TRACING
+
+class ATATraceEntry : public AbstractTraceEntry {
+	public:
+		ATATraceEntry(int bus, int device, const char *info)
+		: fBus(bus)
+		, fDevice(device)
+		{
+			strlcpy(fInfo, info, sizeof(fInfo));
+			Initialized();
+		}
+
+		void AddDump(TraceOutput& out, const char* name)
+		{
+			out.Print("ata");
+			out.Print(" - %s - ", name);
+			out.Print("bus %d", fBus);
+			out.Print("device %d", fDevice);			
+			out.Print("%s", fInfo);			
+		}
+
+		int fBus;
+		int fDevice;
+ 		char fInfo[200];
+};
+
+
+extern "C" void 
+__ata_trace_device(ide_device_info *dev, const char *fmt, ...)
+{
+	char info[200];
+	va_list ap; 
+
+	va_start(ap, fmt); 
+	vsnprintf(info, sizeof(info), fmt, ap); 
+	va_end(ap);
+
+	new(std::nothrow) ATATraceEntry(dev->bus->path_id, dev->is_device1, info);
+}
+
+
+extern "C" void 
+__ata_trace_bus_device(ide_bus_info *bus, int dev, const char *fmt, ...)
+{
+	char info[200];
+	va_list ap; 
+
+	va_start(ap, fmt); 
+	vsnprintf(info, sizeof(info), fmt, ap); 
+	va_end(ap);
+
+	new(std::nothrow) ATATraceEntry(bus->path_id, dev, info);
+}
+
+#endif
