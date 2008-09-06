af18effb0e90fd1f128a83b732f0ec40bd3046ef
François Revol <revol@free.fr>
2008-09-06 00:57:24 +0000

    No need to report an error on modules that aren't modules...
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@27341 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/kernel/module.cpp
+++ src/system/kernel/module.cpp
@@ -1806,7 +1806,7 @@ module_init(kernel_args* args)
 
 	for (image = args->preloaded_images; image != NULL; image = image->next) {
 		status_t status = register_preloaded_module_image(image);
-		if (status != B_OK) {
+		if (status != B_OK && image->is_module) {
 			dprintf("Could not register image \"%s\": %s\n", image->name,
 				strerror(status));
 		}
