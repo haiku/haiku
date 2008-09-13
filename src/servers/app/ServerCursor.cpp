bbe2597fb6cb26270842f57b6cdf5c8bd4c82354
Michael Lotz <mmlr@mlotz.ch>
2008-09-13 16:39:11 +0000

    CID 18 and CID 19: Fix leaking the cursor data.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@27487 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/servers/app/ServerCursor.cpp
+++ src/servers/app/ServerCursor.cpp
@@ -173,6 +173,7 @@ ServerCursor::ServerCursor(const ServerCursor* cursor)
 //!	Frees the heap space allocated for the cursor's image data
 ServerCursor::~ServerCursor()
 {
+	delete fCursorData;
 }
 
 
