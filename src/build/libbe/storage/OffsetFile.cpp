6dcce65687bc9f6dd7581514497de3d3f8ddd663
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2008-09-17 09:15:48 +0000

    port the changes made to the libbe counterpart (r27297 and r27485)
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@27594 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/build/libbe/storage/OffsetFile.cpp
+++ src/build/libbe/storage/OffsetFile.cpp
@@ -16,7 +16,7 @@ namespace Storage {
 
 // constructor
 OffsetFile::OffsetFile()
-	: fFile(),
+	: fFile(NULL),
 	  fOffset(0),
 	  fCurrentPosition(0)
 {
@@ -145,7 +145,7 @@ OffsetFile::SetSize(off_t size)
 
 // GetSize
 status_t
-OffsetFile::GetSize(off_t *size)
+OffsetFile::GetSize(off_t *size) const
 {
 	status_t error = (size ? B_OK : B_BAD_VALUE );
 	if (error == B_OK)
