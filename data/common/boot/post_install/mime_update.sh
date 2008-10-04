7ef5a940472ca89129b307baf996557da010a0e1
Stephan Aßmus <superstippi@gmx.de>
2008-10-04 18:28:15 +0000

    Added optional package "Welcome". The welcome package documation is copied to /boot/beos/documetation/welcome and a link to welcome.html is placed on the Desktop. The mime_update.sh script makes sure that the mime type of the html files is set. However, Firefox has a problem when it is launched for the first time via double clicking an HTML file. Apparently, it initializes some settings and relaunches itself, which in turn makes it forget to open the file. A second problem is annoying as well: When launching Firefox to display a local file, it will always open a second window with the default web location.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@27868 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- data/common/boot/post_install/mime_update.sh
+++ data/common/boot/post_install/mime_update.sh
@@ -3,6 +3,7 @@
 # Make sure all apps have a MIME DB entry.
 
 mimeset -apps -f /boot/beos/apps
+mimeset -f /boot/beos/documentation
 mimeset -apps -f /boot/beos/preferences
 mimeset -apps -f /boot/beos/system/servers
 mimeset -apps -f /boot/apps
