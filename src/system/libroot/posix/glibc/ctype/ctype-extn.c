026c75e91c59932afd99006c90bdd53397469c4f
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2008-10-09 14:58:45 +0000

    add ctype-extn.c to the build. It contains the function versions of the is*** defines. Without this, bash 3 refuses to compile on haiku
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@27946 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/libroot/posix/glibc/ctype/ctype-extn.c
+++ src/system/libroot/posix/glibc/ctype/ctype-extn.c
@@ -18,12 +18,12 @@
 
 #define	__NO_CTYPE
 #include <ctype.h>
-
+/*
 #define __ctype_tolower \
   ((uint32_t *) _NL_CURRENT (LC_CTYPE, _NL_CTYPE_TOLOWER) + 128)
 #define __ctype_toupper \
   ((uint32_t *) _NL_CURRENT (LC_CTYPE, _NL_CTYPE_TOUPPER) + 128)
-
+*/
 /* Real function versions of the non-ANSI ctype functions.  isblank is
    now in ISO C99 but we leave it here.  */
 
@@ -58,10 +58,11 @@ isascii (int c)
 }
 weak_alias (isascii, __isascii_l)
 
-
+/*
 int
 __isblank_l (int c, __locale_t l)
 {
   return __isctype_l (c, _ISblank, l);
 }
 weak_alias (__isblank_l, isblank_l)
+*/
