1f7f1654fbf063e0c7340e66519102c832cbc429
Rene Gollent <anevilyak@gmail.com>
2009-01-11 04:52:18 +0000

    The problem appears to be at the level of _PointerList_ itself. Also note, BList does not exhibit the issue.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@28874 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/tests/kits/support/pointerlist/PointerListSortTest.cpp
+++ src/tests/kits/support/pointerlist/PointerListSortTest.cpp
@@ -1,24 +1,24 @@
 #include <ObjectList.h>
 #include <String.h>
 
-static int SortItemTestPositive(const BString *item1, const BString *item2)
+static int SortItemTestPositive(const void *item1, const void *item2)
 {
 	return 1;
 }
 
-static int SortItemTestNegative(const BString *item1, const BString *item2)
+static int SortItemTestNegative(const void *item1, const void *item2)
 {
 	return -1;
 }
 
-static int SortItemTestEqual(const BString *item1, const BString *item2)
+static int SortItemTestEqual(const void *item1, const void *item2)
 {
 	return 0;
 }
 
 int main(int, char **) 
 {
-	BObjectList<BString> list;
+	_PointerList_ list;
 	for (int i = 0; i < 20; i++) {
 		list.AddItem(new BString("test"));
 		printf("List contains %d items, attempting sorts\n", i);
