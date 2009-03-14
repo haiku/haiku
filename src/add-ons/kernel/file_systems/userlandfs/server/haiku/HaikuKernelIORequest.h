d6c6d091ef64ab3b0e9ca85d1f924e6b4579a84e
Ingo Weinhold <ingo_weinhold@gmx.de>
2009-03-14 02:32:59 +0000

    Forgot to add.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@29506 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/add-ons/kernel/file_systems/userlandfs/server/haiku/HaikuKernelIORequest.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2009, Ingo Weinhold, ingo_weinhold@gmx.de.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef USERLAND_FS_HAIKU_KERNEL_IO_REQUEST_H
+#define USERLAND_FS_HAIKU_KERNEL_IO_REQUEST_H
+
+#include <fs_interface.h>
+
+#include <util/OpenHashTable.h>
+
+#include "../IORequestInfo.h"
+
+
+namespace UserlandFS {
+
+struct HaikuKernelIORequest : HashTableLink<HaikuKernelIORequest>,
+	IORequestInfo {
+
+	int32 refCount;
+
+	HaikuKernelIORequest(const IORequestInfo& info)
+		:
+		IORequestInfo(info),
+		refCount(1)
+	{
+	}
+};
+
+struct HaikuKernelIterativeFDIOCookie {
+	int						fd;
+	HaikuKernelIORequest*	request;
+	iterative_io_get_vecs	getVecs;
+	iterative_io_finished	finished;
+	void*					cookie;
+
+	HaikuKernelIterativeFDIOCookie(int fd, HaikuKernelIORequest* request,
+		iterative_io_get_vecs getVecs, iterative_io_finished finished,
+		void* cookie)
+		:
+		fd(fd),
+		request(request),
+		getVecs(getVecs),
+		finished(finished),
+		cookie(cookie)
+	{
+	}
+};
+
+}	// namespace UserlandFS
+
+
+using UserlandFS::HaikuKernelIORequest;
+using UserlandFS::HaikuKernelIterativeFDIOCookie;
+
+
+#endif	// USERLAND_FS_HAIKU_KERNEL_IO_REQUEST_H
