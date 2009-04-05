haiku/haiku

commit df8b89cbf7e103487d466b76539b9e46c407d643

* Return B_ERROR if the window couldn't be locked, thanks stippi
git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@29728 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/kits/interface/Dragger.cpp
@@ -123,7 +123,8 @@ BDragger::Archive(BMessage *data, bool deep) const
 			if (ret == B_OK)
 				ret = data->AddMessage("_popup", &popupMsg);
 			fPopUp->Window()->Unlock();
-		}
+		} else
+			ret = B_ERROR;
 	}
 
 	if (ret == B_OK)

commit 642cc1315d43a965abb1b5d7a0738c9056ea5c87

* Fixes a small regression introduced in r29728, don't return B_ERROR if we couldn't lock the popup's window, in this case the window just closed, so we go on with archiving then. There's still a (rarely occuring) problem with the unlocking though, probably due to some BMenu funkyness, will investigate, left a TODO.
git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@29947 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/kits/interface/Dragger.cpp
@@ -118,13 +118,17 @@ BDragger::Archive(BMessage *data, bool deep) const
 	BMessage popupMsg;
 	
 	if (fPopUp) {
-		if (fPopUp->Window()->Lock()) {
-			ret = fPopUp->Archive(&popupMsg, deep);
-			if (ret == B_OK)
-				ret = data->AddMessage("_popup", &popupMsg);
+		bool windowLocked = fPopUp->Window()->Lock();
+
+		ret = fPopUp->Archive(&popupMsg, deep);
+
+		if (windowLocked)
 			fPopUp->Window()->Unlock();
-		} else
-			ret = B_ERROR;
+				// TODO: Investigate, in some (rare) occasions the menu window
+				//		 has already been unlocked
+
+		if (ret == B_OK)
+			ret = data->AddMessage("_popup", &popupMsg);
 	}
 
 	if (ret == B_OK)