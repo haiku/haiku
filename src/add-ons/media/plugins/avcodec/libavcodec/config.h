370ed3de610ffb52171e90797a8b17a07a2b487d
Philippe Houdoin <philippe.houdoin@gmail.com>
2009-04-15 23:11:01 +0000

    Updated to support other $(TARGET_ARCH) specific optimization more easily: moved out of config.h some definitions that we could do dynamically per target architecture.
    NOTE: ppc, m68k and sparc optimizations are currently missing and should 
    be imported from ffmpeg 0.5 before we can build avcodec plugin for these 
    targets. Hence why it's still X86_ONLY qualified.
    
    
    
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@30185 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/media/plugins/avcodec/libavcodec/config.h
+++ src/add-ons/media/plugins/avcodec/libavcodec/config.h
@@ -10,30 +10,30 @@
 #define ARCH_M68K 0
 #define ARCH_MIPS 0
 #define ARCH_PARISC 0
-#define ARCH_PPC 0
+// #define ARCH_PPC 0
 #define ARCH_S390 0
 #define ARCH_SH4 0
-#define ARCH_SPARC 0
+// #define ARCH_SPARC 0
 #define ARCH_SPARC64 0
-#define ARCH_X86 1
-#define ARCH_X86_32 1
+// #define ARCH_X86 1
+// #define ARCH_X86_32 1
 #define ARCH_X86_64 0
-#define HAVE_ALTIVEC 0
-#define HAVE_AMD3DNOW 0
-#define HAVE_AMD3DNOWEXT 0
+// #define HAVE_ALTIVEC 0
+// #define HAVE_AMD3DNOW 0
+// #define HAVE_AMD3DNOWEXT 0
 #define HAVE_ARMV5TE 0
 #define HAVE_ARMV6 0
 #define HAVE_ARMV6T2 0
 #define HAVE_ARMVFP 0
 #define HAVE_IWMMXT 0
 #define HAVE_MMI 0
-#define HAVE_MMX 1
-#define HAVE_MMX2 1
+// #define HAVE_MMX 1
+// #define HAVE_MMX2 1
 #define HAVE_NEON 0
 #define HAVE_PPC4XX 0
-#define HAVE_SSE 0
-#define HAVE_SSSE3 1
-#define HAVE_VIS 0
+// #define HAVE_SSE 0
+// #define HAVE_SSSE3 1
+// #define HAVE_VIS 0
 #define HAVE_BEOSTHREADS 0
 #define HAVE_OS2THREADS 0
 #define HAVE_PTHREADS 0
