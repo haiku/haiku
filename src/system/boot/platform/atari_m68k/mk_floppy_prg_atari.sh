4fd790129219be2ef0a42ad21e779018bc0282c3
François Revol <revol@free.fr>
2009-04-27 02:37:31 +0000

    Finally managed to get floppy boot working for Atari \o/ - loop on Floprd() alternating sides every sect/track, currently hardcoded from the FAT bootblock as probing like the NetBSD loader asserts ARAnyM, - print a dot / sector read in the great tradition of bootloaders, - set gBootedFromImage correctly, - fix the script, make it jam later on.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@30455 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/boot/platform/atari_m68k/mk_floppy_prg_atari.sh
+++ src/system/boot/platform/atari_m68k/mk_floppy_prg_atari.sh
@@ -1,17 +1,26 @@
 #!/bin/sh -x
 IMG=$HOME/floppy.img
-jam -q zbeos && \
-dd if=generated/objects/haiku/m68k/release/system/boot/zbeos of=$IMG bs=512 count=9 conv=notrunc && \
-dd if=generated/objects/haiku/m68k/release/system/boot/zbeos of=$IMG bs=512 count=9 conv=notrunc skip=9 seek=$((9*180)) && \
-dd if=generated/objects/haiku/m68k/release/system/boot/zbeos of=$IMG bs=512 count=9 conv=notrunc skip=9 seek=$((2*9)) && \
-dd if=generated/objects/haiku/m68k/release/system/boot/zbeos of=$IMG bs=512 count=9 conv=notrunc skip=$((2*9)) seek=$((2*2*9)) && \
-dd if=generated/objects/haiku/m68k/release/system/boot/zbeos of=$IMG bs=512 count=9 conv=notrunc skip=$((3*9)) seek=$((3*2*9)) && \
+ZBEOS=haiku_loader
+jam -q haiku.prg $ZBEOS && \
+dd if=/dev/zero of=$IMG bs=1k count=1440 && \
+dd if=generated/objects/haiku/m68k/release/system/boot/$ZBEOS of=$IMG bs=512 conv=notrunc && \
 src/system/boot/platform/atari_m68k/fixup_tos_floppy_chksum $IMG && \
-generated.m68k/cross-tools/bin/m68k-unknown-haiku-ld -o haiku.prg -T src/system/ldscripts/m68k/boot_prg_atari_m68k.ld generated/objects/haiku/m68k/release/system/boot/boot_loader_atari_m68k && \
-zip haiku.prg.zip haiku.prg
+true
+
+exit 0
+
+dd if=generated/objects/haiku/m68k/release/system/boot/$ZBEOS of=$IMG bs=512 count=9 conv=notrunc && \
+dd if=generated/objects/haiku/m68k/release/system/boot/$ZBEOS of=$IMG bs=512 count=9 conv=notrunc skip=9 seek=$((9*180)) && \
+dd if=generated/objects/haiku/m68k/release/system/boot/$ZBEOS of=$IMG bs=512 count=9 conv=notrunc skip=9 seek=$((2*9)) && \
+dd if=generated/objects/haiku/m68k/release/system/boot/$ZBEOS of=$IMG bs=512 count=9 conv=notrunc skip=$((2*9)) seek=$((2*2*9)) && \
+dd if=generated/objects/haiku/m68k/release/system/boot/$ZBEOS of=$IMG bs=512 count=9 conv=notrunc skip=$((3*9)) seek=$((3*2*9)) && \
+src/system/boot/platform/atari_m68k/fixup_tos_floppy_chksum $IMG && \
+true 
+#generated.m68k/cross-tools/bin/m68k-unknown-haiku-ld -o haiku.prg -T src/system/ldscripts/m68k/boot_prg_atari_m68k.ld generated/objects/haiku/m68k/release/system/boot/boot_loader_atari_m68k && \
+#zip haiku.prg.zip haiku.prg
 
 
 
-#dd if=generated/objects/haiku/m68k/release/system/boot/zbeos of=$IMG bs=512 count=500 conv=notrunc && \
+#dd if=generated/objects/haiku/m68k/release/system/boot/$ZBEOS of=$IMG bs=512 count=500 conv=notrunc && \
 
 
