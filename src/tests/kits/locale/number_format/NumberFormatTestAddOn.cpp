bd3cb5f1b0b75afe0de51a8292042c40ce091bc8
Oliver Tappe <zooey@hirschkaefer.de>
2009-05-02 18:59:35 +0000

    * added number format tests to build of locale kit
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@30582 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/tests/kits/locale/number_format/NumberFormatTestAddOn.cpp
+++ src/tests/kits/locale/number_format/NumberFormatTestAddOn.cpp
@@ -4,13 +4,12 @@
 #include "generic_number_format/GenericNumberFormatTests.h"
 
 // getTestSuite
-_EXPORT
 BTestSuite*
 getTestSuite()
 {
 	BTestSuite *suite = new BTestSuite("NumberFormat");
 
 	suite->addTest("BGenericNumberFormat", GenericNumberFormatTestSuite());
-	
+
 	return suite;
 }
