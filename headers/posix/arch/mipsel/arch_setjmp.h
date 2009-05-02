41a9e149b783bb9baf0d5fbf51749d634eac291c
Jonas Sundström <jonas@kirilla.com>
2009-05-02 18:30:28 +0000

    Adding a few directories for mipsel.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@30581 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/posix/arch/mipsel/arch_setjmp.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2008, Haiku, Inc.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _ARCH_SETJMP_H_
+#define _ARCH_SETJMP_H_
+
+typedef int __jmp_buf[42];
+#warning MIPSEL: fix jmpbuf size
+
+#endif	/* _ARCH_SETJMP_H_ */
---
+++ headers/posix/arch/mipsel/signal.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2008, Haiku, Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _ARCH_SIGNAL_H_
+#define _ARCH_SIGNAL_H_
+
+/*
+ * Architecture-specific structure passed to signal handlers
+ */
+
+#if __MIPSEL__
+struct vregs
+{
+	ulong r0;
+	/* r1, r2, ... */
+
+#warning MIPSEL: fixme
+};
+#endif /* __MIPSEL__ */ 
+
+
+#endif /* _ARCH_SIGNAL_H_ */
