a4e151e06dbcd5da5980e2c58b19849cb21d3520
Ingo Weinhold <ingo_weinhold@gmx.de>
2009-05-12 20:55:49 +0000

    * Removed the non-GCC macro definitions. * Added missing macro B_HAIKU_VERSION. Also added a version macro for alpha 1   -- it doesn't seem unlikely that the API will change between alpha 1 and R1. * Added new macro B_HAIKU_ABI indicating which ABI is used (gcc 2/4).
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@30727 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/BeBuild.h
+++ headers/os/BeBuild.h
@@ -13,21 +13,30 @@
 #define B_BEOS_VERSION		B_BEOS_VERSION_5
 #define B_BEOS_VERSION_MAUI	B_BEOS_VERSION_5
 
-#define B_HAIKU_VERSION_1	0x0100
+// Haiku (API) version
+#define B_HAIKU_VERSION_1_ALPHA_1	0x0010
+#define B_HAIKU_VERSION_1			0x0100
 
-#if __GNUC__
-#	define _UNUSED(argument) argument
-#	define _PACKED __attribute__((packed))
-#	define _PRINTFLIKE(_format_, _args_) \
-		__attribute__((format(__printf__, _format_, _args_)))
-#	define _EXPORT
-#	define _IMPORT
+#define B_HAIKU_VERSION				B_HAIKU_VERSION_1_ALPHA_1
+
+// Haiku ABI
+#define B_HAIKU_ABI_GCC_2			0x01
+#define B_HAIKU_ABI_GCC_4			0x02
+
+#if __GNUC__ == 2
+#	define B_HAIKU_ABI				B_HAIKU_ABI_GCC_2
+#elif __GNUC__ == 4
+#	define B_HAIKU_ABI				B_HAIKU_ABI_GCC_4
 #else
-#	define _UNUSED(argument) argument
-#	error Define _PACKED for your compiler
-#	define _PRINTFLIKE(format, args)
-#	define _EXPORT
-#	define _IMPORT
+#	error Unsupported gcc version!
 #endif
+
+
+#define _UNUSED(argument) argument
+#define _PACKED __attribute__((packed))
+#define _PRINTFLIKE(_format_, _args_) \
+	__attribute__((format(__printf__, _format_, _args_)))
+#define _EXPORT
+#define _IMPORT
 
 #endif	/* _BE_BUILD_H */
