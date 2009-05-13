0509bd723141dba91a22927a42ca90ea049c5e2f
Ingo Weinhold <ingo_weinhold@gmx.de>
2009-05-13 14:37:41 +0000

    * Changed the layout of the Haiku version and ABI constants to allow for   more inbetween versions. * Added constants for legacy versions.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@30746 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/BeBuild.h
+++ headers/os/BeBuild.h
@@ -1,32 +1,41 @@
 /*
- * Copyright 2007-2008, Haiku, Inc. All Rights Reserved.
+ * Copyright 2007-2009, Haiku, Inc. All Rights Reserved.
  * Distributed under the terms of the MIT License.
  */
 #ifndef _BE_BUILD_H
 #define _BE_BUILD_H
 
 
-#define B_BEOS_VERSION_4	0x0400
-#define B_BEOS_VERSION_4_5	0x0450
-#define B_BEOS_VERSION_5	0x0500
+#define B_BEOS_VERSION_4				0x0400
+#define B_BEOS_VERSION_4_5				0x0450
+#define B_BEOS_VERSION_5				0x0500
 
-#define B_BEOS_VERSION		B_BEOS_VERSION_5
-#define B_BEOS_VERSION_MAUI	B_BEOS_VERSION_5
+#define B_BEOS_VERSION					B_BEOS_VERSION_5
+#define B_BEOS_VERSION_MAUI				B_BEOS_VERSION_5
 
 // Haiku (API) version
-#define B_HAIKU_VERSION_1_ALPHA_1	0x0010
-#define B_HAIKU_VERSION_1			0x0100
+#define B_HAIKU_VERSION_BEOS			0x00000001
+#define B_HAIKU_VERSION_BONE			0x00000002
+#define B_HAIKU_VERSION_DANO			0x00000003
+#define B_HAIKU_VERSION_1_PRE_ALPHA_1	0x00000011
+#define B_HAIKU_VERSION_1_ALPHA_1		0x00000100
+#define B_HAIKU_VERSION_1				0x00010000
 
-#define B_HAIKU_VERSION				B_HAIKU_VERSION_1_ALPHA_1
+#define B_HAIKU_VERSION					B_HAIKU_VERSION_1_PRE_ALPHA_1
 
 // Haiku ABI
-#define B_HAIKU_ABI_GCC_2			0x01
-#define B_HAIKU_ABI_GCC_4			0x02
+#define B_HAIKU_ABI_MAJOR				0xffff0000
+#define B_HAIKU_ABI_GCC_2				0x00020000
+#define B_HAIKU_ABI_GCC_4				0x00040000
+
+#define B_HAIKU_ABI_GCC_2_ANCIENT		0x00020000
+#define B_HAIKU_ABI_GCC_2_BEOS			0x00020001
+#define B_HAIKU_ABI_GCC_2_HAIKU			0x00020002
 
 #if __GNUC__ == 2
-#	define B_HAIKU_ABI				B_HAIKU_ABI_GCC_2
+#	define B_HAIKU_ABI					B_HAIKU_ABI_GCC_2
 #elif __GNUC__ == 4
-#	define B_HAIKU_ABI				B_HAIKU_ABI_GCC_4
+#	define B_HAIKU_ABI					B_HAIKU_ABI_GCC_4
 #else
 #	error Unsupported gcc version!
 #endif
