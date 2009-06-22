ebe1a956f0b684048e6f881eabb7e6e232a98ae2
Oliver Tappe <zooey@hirschkaefer.de>
2009-06-22 19:13:47 +0000

    * fixed a warning
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@31181 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/tests/system/libroot/posix/mbtest.c
+++ src/tests/system/libroot/posix/mbtest.c
@@ -1,12 +1,13 @@
 #include <locale.h>
 #include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-	
+
 	setlocale(LC_CTYPE, "");
-	
+
 	printf("Len: %i\n", mblen(argv[1], SSIZE_MAX));
-	
+
 	return 0;
 }
