0ca532347962a09dd9ccf068016de9b6e6d9053b
Axel Dörfler <axeld@pinc-software.de>
2009-07-15 15:28:49 +0000

    * Changed the enum into defines, so that apps can check whether or not they   exist (as mDNSResponder does, for example).
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@31588 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/posix/sys/sockio.h
+++ headers/posix/sys/sockio.h
@@ -6,52 +6,50 @@
 #define _SYS_SOCKIO_H
 
 
-enum {
-	SIOCADDRT = 8900,	/* add route */
-	SIOCDELRT,			/* delete route */
-	SIOCSIFADDR,		/* set interface address */
-	SIOCGIFADDR,		/* get interface address */
-	SIOCSIFDSTADDR,		/* set point-to-point address */
-	SIOCGIFDSTADDR,		/* get point-to-point address */
-	SIOCSIFFLAGS,		/* set interface flags */
-	SIOCGIFFLAGS,		/* get interface flags */
-	SIOCGIFBRDADDR,		/* get broadcast address */
-	SIOCSIFBRDADDR,		/* set broadcast address */
-	SIOCGIFCOUNT,		/* count interfaces */
-	SIOCGIFCONF,		/* get interface list */
-	SIOCGIFINDEX,		/* interface name -> index */
-	SIOCGIFNAME,		/* interface index -> name */
-	SIOCGIFNETMASK,		/* get net address mask */
-	SIOCSIFNETMASK,		/* set net address mask */
-	SIOCGIFMETRIC,		/* get interface metric */
-	SIOCSIFMETRIC,		/* set interface metric */
-	SIOCDIFADDR,		/* delete interface address */
-	SIOCAIFADDR,		/* configure interface alias */
-	SIOCADDMULTI,		/* add multicast address */
-	SIOCDELMULTI,		/* delete multicast address */
-	SIOCGIFMTU,			/* get interface MTU */
-	SIOCSIFMTU,			/* set interface MTU */
-	SIOCSIFMEDIA,		/* set net media */
-	SIOCGIFMEDIA,		/* get net media */
+#define SIOCADDRT			8900	/* add route */
+#define SIOCDELRT			8901	/* delete route */
+#define SIOCSIFADDR			8902	/* set interface address */
+#define SIOCGIFADDR			8903	/* get interface address */
+#define SIOCSIFDSTADDR		8904	/* set point-to-point address */
+#define SIOCGIFDSTADDR		8905	/* get point-to-point address */
+#define SIOCSIFFLAGS		8906	/* set interface flags */
+#define SIOCGIFFLAGS		8907	/* get interface flags */
+#define SIOCGIFBRDADDR		8908	/* get broadcast address */
+#define SIOCSIFBRDADDR		8909	/* set broadcast address */
+#define SIOCGIFCOUNT		8910	/* count interfaces */
+#define SIOCGIFCONF			8911	/* get interface list */
+#define SIOCGIFINDEX		8912	/* interface name -> index */
+#define SIOCGIFNAME			8913	/* interface index -> name */
+#define SIOCGIFNETMASK		8914	/* get net address mask */
+#define SIOCSIFNETMASK		8915	/* set net address mask */
+#define SIOCGIFMETRIC		8916	/* get interface metric */
+#define SIOCSIFMETRIC		8917	/* set interface metric */
+#define SIOCDIFADDR			8918	/* delete interface address */
+#define SIOCAIFADDR			8919	/* configure interface alias */
+#define SIOCADDMULTI		8920	/* add multicast address */
+#define SIOCDELMULTI		8921	/* delete multicast address */
+#define SIOCGIFMTU			8922	/* get interface MTU */
+#define SIOCSIFMTU			8923	/* set interface MTU */
+#define SIOCSIFMEDIA		8924	/* set net media */
+#define SIOCGIFMEDIA		8925	/* get net media */
 
-	SIOCGRTSIZE,		/* get route table size */
-	SIOCGRTTABLE,		/* get route table */
-	SIOCGETRT,			/* get route information for destination */
+#define SIOCGRTSIZE			8926	/* get route table size */
+#define SIOCGRTTABLE		8927	/* get route table */
+#define SIOCGETRT			8928	/* get route information for destination */
 
-	SIOCGIFSTATS,		/* get interface stats */
-	SIOCGIFPARAM,		/* get interface parameter */
-	SIOCGIFTYPE,		/* get interface type */
+#define SIOCGIFSTATS		8929	/* get interface stats */
+#define SIOCGIFPARAM		8930	/* get interface parameter */
+#define SIOCGIFTYPE			8931	/* get interface type */
 
-	SIOCSPACKETCAP,		/* Start capturing packets on an interface */
-	SIOCCPACKETCAP,		/* Stop capturing packets on an interface */
+#define SIOCSPACKETCAP		8932	/* Start capturing packets on an interface */
+#define SIOCCPACKETCAP		8933	/* Stop capturing packets on an interface */
 
-	SIOCSHIWAT,			/* set high watermark */
-	SIOCGHIWAT,			/* get high watermark */
-	SIOCSLOWAT,			/* set low watermark */
-	SIOCGLOWAT,			/* get low watermark */
-	SIOCATMARK,			/* at out-of-band mark? */
-	SIOCSPGRP,			/* set process group */
-	SIOCGPGRP			/* get process group */
-};
+#define SIOCSHIWAT			8934	/* set high watermark */
+#define SIOCGHIWAT			8935	/* get high watermark */
+#define SIOCSLOWAT			8936	/* set low watermark */
+#define SIOCGLOWAT			8937	/* get low watermark */
+#define SIOCATMARK			8938	/* at out-of-band mark? */
+#define SIOCSPGRP			8939	/* set process group */
+#define SIOCGPGRP			8940	/* get process group */
 
 #endif	/* _SYS_SOCKIO_H */
