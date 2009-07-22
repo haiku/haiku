8666a725ac5953443ff2890be2f5567992c64588
François Revol <revol@free.fr>
2009-07-22 18:03:42 +0000

    [GSoC] [ARM] Patch by Johannes Wischert. Add ARM header for debugger.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@31697 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/os/arch/arm/arch_debugger.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2005, Haiku Inc.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _ARCH_ARM_DEBUGGER_H
+#define _ARCH_ARM_DEBUGGER_H
+
+struct arm_debug_cpu_state {
+        ulong r0;
+        ulong r1;
+        ulong r2;
+        ulong r3;
+        ulong r4;
+        ulong r5;
+        ulong r6;
+        ulong r7;
+        ulong r8;
+        ulong r9;
+        ulong r10;
+        ulong r11;
+        ulong r12;
+        ulong r13;      /* stack pointer */
+        ulong r14;      /* link register */
+        ulong r15;      /* program counter */
+        ulong cpsr;
+#warning ARM: missing members!
+	uint32	dummy;
+} __attribute__((aligned(8)));
+
+#endif	// _ARCH_ARM_DEBUGGER_H
