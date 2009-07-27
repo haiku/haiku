6f14ecb4259868f79bcf9f4af89f055749b9e31f
François Revol <revol@free.fr>
2009-07-27 16:27:13 +0000

    [GSoC] [ARM] Patch by Johannes Wischert. Misc. Assembler functions (atomic ops, byte swapping, longjmp and libc).
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@31822 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/system/libroot/os/arch/arm/system_time.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2006, Ingo Weinhold <bonefish@cs.tu-berlin.de>.
+ * All rights reserved. Distributed under the terms of the MIT License.
+ */
+
+#include <OS.h>
+
+#include <arch_cpu.h>
+#include <libroot_private.h>
+#include <real_time_data.h>
+
+
+bigtime_t
+system_time(void)
+{
+#warning ARM:WRITEME
+	return 0;
+}
