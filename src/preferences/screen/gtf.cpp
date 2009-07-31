55c3b5f1d53baa4d15da4581785e457aeb96bd67
Axel Dörfler <axeld@pinc-software.de>
2009-07-31 13:15:06 +0000

    * If the copyright says to include the full list, we better do, even if our   license is basically the same thing.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@32017 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/preferences/screen/gtf.cpp
+++ src/preferences/screen/gtf.cpp
@@ -6,11 +6,41 @@
  *		Gerald Zajac
  */
 
-/*!	This file contains function(s) to generate video mode timings using the
-	GTF Timing Standard.  The code in this file was adapted from the X.org
-	source file gtf.c which has the following copyright:
+/* Copyright (c) 2001, Andy Ritger  aritger@nvidia.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * o Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * o Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer
+ *   in the documentation and/or other materials provided with the
+ *   distribution.
+ * o Neither the name of NVIDIA nor the names of its contributors
+ *   may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
-	Copyright (c) 2001, Andy Ritger  aritger@nvidia.com  All rights reserved.
+
+/*!	This file contains function(s) to generate video mode timings using the
+	GTF Timing Standard, based on VESA.org's GTF_V1R1.xls.
 */
 
 
