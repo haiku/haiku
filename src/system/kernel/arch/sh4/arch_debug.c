885b7b4a8594b00c7f8ed6ac1aa28d0b2db82566
Ingo Weinhold <ingo_weinhold@gmx.de>
2009-08-03 11:32:08 +0000

    Removed unsupported architectures.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@32071 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/kernel/arch/sh4/arch_debug.c
+++
@@ -1,8 +0,0 @@
-/* 
-** Copyright 2001, Travis Geiselbrecht. All rights reserved.
-** Distributed under the terms of the NewOS License.
-*/
-#include <kernel/kernel.h>
-#include <kernel/debug.h>
-#include <kernel/arch/debug.h>
-
