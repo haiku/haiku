15317caafee2313921df7b0ab8e2a5366a50eca5
François Revol <revol@free.fr>
2009-08-08 00:35:48 +0000

    [ARM] - moved board/ folder around again, it probably belongs only to kernel stuff, - added board_config.h templates for gumstix boards.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@32197 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/kernel/arch/arm/board/overo/board_config.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2009
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _BOARD_OVERO_BOARD_CONFIG_H
+#define _BOARD_OVERO_BOARD_CONFIG_H
+
+#define BOARD_NAME_PRETTY "Gumstix Overo"
+
+#include <arch/arm/omap3.h>
+
+#endif /* _BOARD_OVERO_BOARD_CONFIG_H */
---
+++ headers/private/kernel/arch/arm/board/verdex/board_config.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2009
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _BOARD_VERDEX_BOARD_CONFIG_H
+#define _BOARD_VERDEX_BOARD_CONFIG_H
+
+#define BOARD_NAME_PRETTY "Gumstix Verdex"
+
+#include <arch/arm/pxa270.h>
+
+#endif /* _BOARD_VERDEX_BOARD_CONFIG_H */
