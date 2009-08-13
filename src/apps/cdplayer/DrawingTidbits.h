570830b85b2a1803c8bec6abf7434874a126aee2
Jonas Sundström <jonas@kirilla.com>
2009-08-13 22:21:56 +0000

    Removing unused code to reveal what's left to replace, so DrawingBits.h and .cpp can be removed as per the TODO comment in these files. Mentioned GPL code is here:  http://vlc.sourcearchive.com/documentation/0.9.8a/DrawingTidbits_8cpp-source.html
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@32333 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/apps/cdplayer/DrawingTidbits.h
+++ src/apps/cdplayer/DrawingTidbits.h
@@ -19,44 +19,6 @@
 class BBitmap;
 
 const rgb_color kBlack			= {   0,   0,   0, 255 };
-const rgb_color kWhite			= { 255, 255, 255, 255 };
-
-rgb_color ShiftColor(rgb_color , float );
-
-inline rgb_color
-Color(int32 r, int32 g, int32 b, int32 alpha = 255)
-{
-	rgb_color result;
-	result.red = r;
-	result.green = g;
-	result.blue = b;
-	result.alpha = alpha;
-
-	return result;
-}
-
-const float kDarkness = 1.06;
-const float kDimLevel = 0.6;
-
-void ReplaceColor(BBitmap *bitmap, rgb_color from, rgb_color to);
-void ReplaceTransparentColor(BBitmap *bitmap, rgb_color with);
-
-// function can be used to scale the upper left part of
-// a bitmap to fill the entire bitmap, ie fromWidth
-// and fromHeight must be smaller or equal to the bitmaps size!
-// only supported colorspaces are B_RGB32 and B_RGBA32
-status_t scale_bitmap( BBitmap* bitmap,
-					   uint32 fromWidth, uint32 fromHeight );
-
-// bitmaps need to be the same size, or this function will fail
-// currently supported conversions:
-//   B_YCbCr422	-> B_RGB32
-//   B_RGB32	-> B_RGB32
-//   B_RGB16	-> B_RGB32
-// not yet implemented conversions:
-//   B_YCbCr420	-> B_RGB32
-//   B_YUV422	-> B_RGB32
-status_t convert_bitmap(BBitmap* inBitmap, BBitmap* outBitmap);
 
 // dims bitmap (in place) by finding the distance of
 // the color at each pixel to the provided "center" color
