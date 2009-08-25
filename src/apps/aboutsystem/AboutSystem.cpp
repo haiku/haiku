619926647605b809f6443a271d0c65c3ae8ba48d
Jonas Sundström <jonas@kirilla.com>
2009-08-25 22:05:47 +0000

    Patch by Fredrik Ekdahl, slightly modified. Added Fredrik to list of Contributors. +alphabranch
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@32677 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/apps/aboutsystem/AboutSystem.cpp
+++ src/apps/aboutsystem/AboutSystem.cpp
@@ -790,6 +790,7 @@ AboutView::_CreateCreditsView()
 		"David Dengg\n"
 		"John Drinkwater\n"
 		"Cian Duffy\n"
+		"Fredrik Ekdahl\n"
 		"Mark Erben\n"
 		"Christian Fasshauer\n"
 		"Andreas Färber\n"
