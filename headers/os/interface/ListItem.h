7e4350229931b088c8be0d4239deebbbc075de64
Stephan Aßmus <superstippi@gmx.de>
2009-08-26 11:27:06 +0000

    * Updated indentation. * Better grouping. +alphabranch
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@32699 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/interface/ListItem.h
+++ headers/os/interface/ListItem.h
@@ -1,6 +1,6 @@
 /*
- * Copyright 2006-2009, Haiku, Inc. All Rights Reserved.
- * Distributed under the terms of the MIT License.
+ * Copyright 2006-2009, Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT license.
  */
 #ifndef _LIST_ITEM_H
 #define _LIST_ITEM_H
@@ -18,63 +18,67 @@ class BView;
 
 class BListItem : public BArchivable {
 public:
-							BListItem(uint32 outlineLevel = 0,
-								bool expanded = true);
-							BListItem(BMessage* archive);
-	virtual					~BListItem();
+								BListItem(uint32 outlineLevel = 0,
+									bool expanded = true);
+								BListItem(BMessage* archive);
+	virtual						~BListItem();
 
-	virtual	status_t		Archive(BMessage* archive, bool deep = true) const;
+	virtual	status_t			Archive(BMessage* archive,
+									bool deep = true) const;
 
-			float			Height() const;
-			float			Width() const;
-			bool			IsSelected() const;
-			void			Select();
-			void			Deselect();
+			float				Height() const;
+			float				Width() const;
+			bool				IsSelected() const;
+			void				Select();
+			void				Deselect();
 
-	virtual	void			SetEnabled(bool enabled);
-			bool			IsEnabled() const;
+	virtual	void				SetEnabled(bool enabled);
+			bool				IsEnabled() const;
 
-			void			SetHeight(float height);
-			void			SetWidth(float width);
-	virtual	void			DrawItem(BView* owner, BRect frame,
-								bool complete = false) = 0;
-	virtual	void			Update(BView* owner, const BFont* font);
+			void				SetHeight(float height);
+			void				SetWidth(float width);
+	virtual	void				DrawItem(BView* owner, BRect frame,
+									bool complete = false) = 0;
+	virtual	void				Update(BView* owner, const BFont* font);
 
-	virtual	status_t		Perform(perform_code code, void* arg);
+			bool				IsExpanded() const;
+			void				SetExpanded(bool expanded);
+			uint32				OutlineLevel() const;
 
-			bool			IsExpanded() const;
-			void			SetExpanded(bool expanded);
-			uint32			OutlineLevel() const;
+	virtual	status_t			Perform(perform_code code, void* arg);
+
+private:
+	// FBC padding
+	virtual	void				_ReservedListItem1();
+	virtual	void				_ReservedListItem2();
+
+	// Forbidden
+								BListItem(const BListItem& item);
+			BListItem&			operator=(const BListItem& item);
 
 private:
 	friend class BOutlineListView;
 	friend class BListView;
 
-			bool			HasSubitems() const;
+			bool				HasSubitems() const;
 
-	virtual	void			_ReservedListItem1();
-	virtual	void			_ReservedListItem2();
-
-							BListItem(const BListItem& item);
-			BListItem&		operator=(const BListItem& item);
-
-			bool			IsItemVisible() const;
-			void			SetItemVisible(bool visible);
-	inline	float			Top() const;
-	inline	float			Bottom() const;
-			void			SetTop(float top);
+			bool				IsItemVisible() const;
+			void				SetItemVisible(bool visible);
+	inline	float				Top() const;
+	inline	float				Bottom() const;
+			void				SetTop(float top);
 
 private:
-			float			fTop;
-			BList*			fTemporaryList;
-			float			fWidth;
-			float			fHeight;
-			uint32			fLevel;
-			bool			fSelected;
-			bool			fEnabled;
-			bool			fExpanded;
-			bool			fHasSubitems : 1;
-			bool			fVisible : 1;
+			float				fTop;
+			BList*				fTemporaryList;
+			float				fWidth;
+			float				fHeight;
+			uint32				fLevel;
+			bool				fSelected;
+			bool				fEnabled;
+			bool				fExpanded;
+			bool				fHasSubitems : 1;
+			bool				fVisible : 1;
 };
 
 
@@ -95,4 +99,4 @@ BListItem::Bottom(void) const
 #include <StringItem.h>
 	// to maintain source compatibility
 
-#endif	// _LIST_ITEM_H
+#endif // _LIST_ITEM_H
