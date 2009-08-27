04c2fe0b76828a8c76f76c6e2bded84837c6e337
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2009-08-27 08:20:46 +0000

    We don't need these either for now.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@32738 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/build/os/NetKit.h
+++
@@ -1,8 +0,0 @@
-/*
- * Master include file for the Net Kit.
- *
- */
-#include <net_settings.h>
-#include <netdb.h>
-#include <socket.h>
-#include <netinet/in.h>
--- headers/build/os/net/netdb.h
+++
@@ -1,6 +0,0 @@
-#ifndef _NET_NETDB_H
-#define _NET_NETDB_H
-
-#include <netdb.h> /* should include the posix netdb.h */
-
-#endif /* _NET_NETDB_H */
