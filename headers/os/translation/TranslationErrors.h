d7571ebcf7b31d7fa5d55a500910ee288a22bd00
Axel Dörfler <axeld@pinc-software.de>
2009-08-27 14:20:56 +0000

    * Rewrote headers as needed. * Cleaned up our own headers. * Moved translator errors into Errors.h. * Removed extra TranslationKit.h.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@32755 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/translation/TranslationErrors.h
+++ headers/os/translation/TranslationErrors.h
@@ -1,27 +1,12 @@
-/********************************************************************************
-/
-/      File:           TranslationErrors.h
-/
-/      Description:    Error codes for the Translation Kit
-/
-/      Copyright 1998, Be Incorporated, All Rights Reserved.
-/      Copyright 1995-1997, Jon Watte
-/
-********************************************************************************/
-
-#if !defined(_TRANSLATION_ERRORS_H)
+/*
+ * Copyright 2009, Haiku Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _TRANSLATION_ERRORS_H
 #define _TRANSLATION_ERRORS_H
+
 
 #include <Errors.h>
 
 
-enum B_TRANSLATION_ERROR {
-	B_TRANSLATION_BASE_ERROR = B_TRANSLATION_ERROR_BASE,
-	B_NO_TRANSLATOR = B_TRANSLATION_BASE_ERROR,	/*	no translator exists for data	*/
-	B_ILLEGAL_DATA,	/*	data is not what it said it was	*/
-/* aliases */
-	B_NOT_INITIALIZED = B_NO_INIT
-};
-
-
-#endif /* _TRANSLATION_ERRORS_H */
+#endif	// _TRANSLATION_ERRORS_H
--- headers/os/translation/TranslationUtils.h
+++ headers/os/translation/TranslationUtils.h
@@ -1,13 +1,14 @@
 /*
- * Copyright 2002-2007, Haiku, Inc. All Rights Reserved.
+ * Copyright 2002-2009, Haiku Inc. All Rights Reserved.
  * Distributed under the terms of the MIT License.
  */
 #ifndef _TRANSLATION_UTILS_H
 #define _TRANSLATION_UTILS_H
 
 
-#include <TranslationDefs.h>
 #include <SupportDefs.h>
+#include <TranslationDefs.h>
+
 
 class BBitmap;
 class BFile;
@@ -20,52 +21,63 @@ struct entry_ref;
 
 
 class BTranslationUtils {
-	private:
-		BTranslationUtils();
-		~BTranslationUtils();
-		BTranslationUtils(const BTranslationUtils &);
-		BTranslationUtils & operator=(const BTranslationUtils &);
+								BTranslationUtils();
+								BTranslationUtils(
+									const BTranslationUtils& other);
+								~BTranslationUtils();
 
-	public:
-		// bitmap functions
-		static BBitmap *GetBitmap(const char *name, BTranslatorRoster *roster = NULL);
-		static BBitmap *GetBitmap(uint32 type, int32 id,
-			BTranslatorRoster *roster = NULL);
-		static BBitmap *GetBitmap(uint32 type, const char *name,
-			BTranslatorRoster *roster = NULL);
-		static BBitmap *GetBitmapFile(const char *name,
-			BTranslatorRoster *roster = NULL);
-		static BBitmap *GetBitmap(const entry_ref *ref,
-			BTranslatorRoster *roster = NULL);	
-		static BBitmap *GetBitmap(BPositionIO *stream,
-			BTranslatorRoster *roster = NULL);
+			BTranslationUtils&	operator=(const BTranslationUtils& other);
 
-		// text functions
-		static status_t GetStyledText(BPositionIO* fromStream, BTextView* intoView,
-			BTranslatorRoster *roster = NULL);
-		static status_t GetStyledText(BPositionIO* fromStream, BTextView* intoView,
-			const char* encoding, BTranslatorRoster* roster = NULL);
-		static status_t PutStyledText(BTextView *fromView, BPositionIO *intoStream,
-			BTranslatorRoster *roster = NULL);
-		static status_t WriteStyledEditFile(BTextView *fromView, BFile *intoFile);
-		static status_t WriteStyledEditFile(BTextView *fromView, BFile *intoFile,
-			const char* encoding);
+public:
+	enum {
+		B_TRANSLATION_MENU = 'BTMN'
+	};
 
-		// misc. functions
-		static BMessage *GetDefaultSettings(translator_id forTranslator,
-			BTranslatorRoster *roster = NULL);
-		static BMessage *GetDefaultSettings(const char *translatorName,
-			int32 translatorVersion);
+	static	BBitmap*			GetBitmap(const char* name,
+									BTranslatorRoster* roster = NULL);
+	static	BBitmap*			GetBitmap(uint32 type, int32 id,
+									BTranslatorRoster* roster = NULL);
+	static	BBitmap*			GetBitmap(uint32 type, const char* name,
+									BTranslatorRoster* roster = NULL);
+	static	BBitmap*			GetBitmapFile(const char* name,
+									BTranslatorRoster* roster = NULL);
+	static	BBitmap*			GetBitmap(const entry_ref* ref,
+									BTranslatorRoster* roster = NULL);
+	static	BBitmap*			GetBitmap(BPositionIO* stream,
+									BTranslatorRoster* roster = NULL);
 
-		enum {
-			B_TRANSLATION_MENU = 'BTMN'
-		};
-		static status_t AddTranslationItems(BMenu *intoMenu, uint32 fromType,
-			const BMessage *model = NULL,			// default B_TRANSLATION_MENU
-			const char *translatorIdName = NULL,	// default "be:translator"
-			const char *translatorTypeName = NULL,	// default "be:type"
-			BTranslatorRoster *roster = NULL);
+	static	void				SetBitmapColorSpace(color_space space);
+	static	color_space			BitmapColorSpace();
+
+	static	status_t			GetStyledText(BPositionIO* fromStream,
+									BTextView* intoView,
+									BTranslatorRoster* roster = NULL);
+	static	status_t			GetStyledText(BPositionIO* fromStream,
+									BTextView* intoView, const char* encoding,
+									BTranslatorRoster* roster = NULL);
+	static	status_t			PutStyledText(BTextView* fromView,
+									BPositionIO* intoStream,
+									BTranslatorRoster* roster = NULL);
+	static	status_t			WriteStyledEditFile(BTextView* fromView,
+									BFile* intoFile);
+	static	status_t			WriteStyledEditFile(BTextView* fromView,
+									BFile* intoFile, const char* encoding);
+
+	static	BMessage*			GetDefaultSettings(translator_id translator,
+									BTranslatorRoster* roster = NULL);
+	static	BMessage*			GetDefaultSettings(const char* name,
+									int32 version);
+
+	static	status_t			AddTranslationItems(BMenu* intoMenu,
+									uint32 fromType,
+									const BMessage* model = NULL,
+									const char* idName = NULL,
+									const char* typeName = NULL,
+									BTranslatorRoster* roster = NULL);
+
+private:
+	static	color_space			sBitmapSpace;
 };
 
-#endif /* _TRANSLATION_UTILS_H */
 
+#endif	// _TRANSLATION_UTILS_H
