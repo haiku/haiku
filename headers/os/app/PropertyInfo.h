4a76f8198b5ed27c00f069437dc7fb1876f03ccc
Axel Dörfler <axeld@pinc-software.de>
2009-08-27 08:01:57 +0000

    * Fixed style and indentation issues; Stefano, you might want to check your   editor's tab settings before doing more of those.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@32737 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/app/PropertyInfo.h
+++ headers/os/app/PropertyInfo.h
@@ -2,9 +2,9 @@
  * Copyright 2009, Haiku Inc. All rights reserved.
  * Distributed under the terms of the MIT license.
  */
-
 #ifndef _PROPERTY_INFO_H
 #define _PROPERTY_INFO_H
+
 
 #include <BeBuild.h>
 #include <Flattenable.h>
@@ -28,7 +28,7 @@ struct property_info {
 	const char*		usage;
 	uint32			extra_data;
 	uint32			types[10];
-	compound_type		ctypes[3];
+	compound_type	ctypes[3];
 	uint32			_reserved[10];
 };
 
@@ -56,45 +56,46 @@ public:
 									bool freeOnDelete = false);
 	virtual						~BPropertyInfo();
 
-	virtual	int32					FindMatch(BMessage* msg, int32 index,
-								BMessage* spec, int32 form,
-								const char* prop, void* data = NULL) const;
+	virtual	int32				FindMatch(BMessage* msg, int32 index,
+									BMessage* specifier, int32 form,
+									const char* prop, void* data = NULL) const;
 
-	virtual	bool					IsFixedSize() const;
-	virtual	type_code				TypeCode() const;
-	virtual	ssize_t					FlattenedSize() const;
-	virtual	status_t				Flatten(void* buffer, ssize_t size) const;
-	virtual	bool					AllowsTypeCode(type_code code) const;
-	virtual	status_t				Unflatten(type_code code, const void* buf,
-								ssize_t size);
+	virtual	bool				IsFixedSize() const;
+	virtual	type_code			TypeCode() const;
+	virtual	ssize_t				FlattenedSize() const;
+	virtual	status_t			Flatten(void* buffer, ssize_t size) const;
+	virtual	bool				AllowsTypeCode(type_code code) const;
+	virtual	status_t			Unflatten(type_code code, const void* buffer,
+									ssize_t size);
 
-		const property_info*			Properties() const;
-		const value_info*			Values() const;
+		const property_info*	Properties() const;
+		const value_info*		Values() const;
 		int32					CountProperties() const;
 		int32					CountValues() const;
 
 		void					PrintToStream() const;
 
 protected:
-	static	bool					FindCommand(uint32, int32, property_info*);
-	static	bool					FindSpecifier(uint32, property_info*);
+	static	bool				FindCommand(uint32 what, int32 index,
+									property_info* info);
+	static	bool				FindSpecifier(uint32 form, property_info* info);
 
 private:
-	virtual	void					_ReservedPropertyInfo1();
-	virtual	void					_ReservedPropertyInfo2();
-	virtual	void					_ReservedPropertyInfo3();
-	virtual	void					_ReservedPropertyInfo4();
+	virtual	void				_ReservedPropertyInfo1();
+	virtual	void				_ReservedPropertyInfo2();
+	virtual	void				_ReservedPropertyInfo3();
+	virtual	void				_ReservedPropertyInfo4();
 
-								BPropertyInfo(const BPropertyInfo&);
-		BPropertyInfo&				operator=(const BPropertyInfo &);
-		void					FreeMem();
+								BPropertyInfo(const BPropertyInfo& other);
+			BPropertyInfo&		operator=(const BPropertyInfo& other);
+			void				FreeMem();
 
-		property_info*			fPropInfo;
-		value_info*			fValueInfo;
-		int32				fPropCount;
-		bool				fInHeap;
-		uint16				fValueCount;
-		uint32				_reserved[4];
+			property_info*		fPropInfo;
+			value_info*			fValueInfo;
+			int32				fPropCount;
+			bool				fInHeap;
+			uint16				fValueCount;
+			uint32				_reserved[4];
 };
 
 #endif /* _PROPERTY_INFO_H */
