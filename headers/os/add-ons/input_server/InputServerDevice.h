a69c16fec5c2d9ec184ec41f7738a3dc52c36863
Stephan Aßmus <superstippi@gmx.de>
2009-08-27 12:35:43 +0000

    Rewrote headers.
    +alphabranch
    
    
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@32747 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/add-ons/input_server/InputServerDevice.h
+++ headers/os/add-ons/input_server/InputServerDevice.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008 Haiku, Inc. All rights reserved.
+ * Copyright 2008, Haiku, Inc. All rights reserved.
  * Distributed under the terms of the MIT License.
  */
 #ifndef _INPUTSERVERDEVICE_H
--- headers/os/add-ons/input_server/InputServerMethod.h
+++ headers/os/add-ons/input_server/InputServerMethod.h
@@ -1,19 +1,12 @@
-/******************************************************************************
-/
-/	File:			InputServerMethod.h
-/
-/	Description:	Add-on class for input_server methods.
-/
-/	Copyright 1998, Be Incorporated, All Rights Reserved.
-/
-******************************************************************************/
-
+/*
+ * Copyright 2009, Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
 #ifndef _INPUTSERVERMETHOD_H
 #define _INPUTSERVERMETHOD_H
 
-#include <BeBuild.h>
+
 #include <InputServerFilter.h>
-#include <SupportDefs.h>
 
 
 class _BMethodAddOn_;
@@ -21,32 +14,37 @@ class AddOnManager;
 class BMenu;
 class InputServer;
 
+
 class BInputServerMethod : public BInputServerFilter {
 public:
-							BInputServerMethod(const char		*name,
-											   const uchar		*icon);
-	virtual					~BInputServerMethod();
+								BInputServerMethod(const char* name,
+									const uchar* icon);
+	virtual						~BInputServerMethod();
 
-	virtual status_t		MethodActivated(bool active);
+	virtual	status_t			MethodActivated(bool active);
 
-	status_t				EnqueueMessage(BMessage *message);
+			status_t			EnqueueMessage(BMessage* message);
 
-	status_t				SetName(const char *name);
-	status_t				SetIcon(const uchar *icon);
-	status_t				SetMenu(const BMenu *menu, const BMessenger target);
+			status_t			SetName(const char* name);
+			status_t			SetIcon(const uchar* icon);
+			status_t			SetMenu(const BMenu* menu,
+									const BMessenger target);
 
 private:
-	_BMethodAddOn_*			fOwner;
-	
-	friend class AddOnManager;
-	friend class InputServer;
-	
-	virtual void			_ReservedInputServerMethod1();
-	virtual void			_ReservedInputServerMethod2();
-	virtual void			_ReservedInputServerMethod3();
-	virtual void			_ReservedInputServerMethod4();
-	uint32					_reserved[4];
+	// FBC padding
+	virtual	void				_ReservedInputServerMethod1();
+	virtual	void				_ReservedInputServerMethod2();
+	virtual	void				_ReservedInputServerMethod3();
+	virtual	void				_ReservedInputServerMethod4();
+
+			uint32				_reserved[4];
+
+private:
+			friend class AddOnManager;
+			friend class InputServer;
+
+private:
+			_BMethodAddOn_*		fOwner;
 };
 
-
-#endif
+#endif // _INPUTSERVERMETHOD_H
