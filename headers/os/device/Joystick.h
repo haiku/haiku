ab83d3e93214227a4bfb368c96c69141518943de
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2009-08-28 19:08:52 +0000

    Rewrote Joystick.h and DigitalPort.h
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@32791 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/device/Joystick.h
+++ headers/os/device/Joystick.h
@@ -1,97 +1,91 @@
-/********************************************************************************
-/
-/	File:		Joystick.h
-/
-/	Description:	Joystick port class header.
-/
-/	Copyright 1996-98, Be Incorporated, All Rights Reserved.
-/
-********************************************************************************/
-
-
+/*
+ * Copyright 2009, Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
 #ifndef	_JOYSTICK_H
 #define	_JOYSTICK_H
 
 #include <BeBuild.h>
-#include <SupportDefs.h>
 #include <OS.h>
+#include <SupportDefs.h>
 
 class BList;
 class BString;
+class _BJoystickTweaker;
 struct entry_ref;
 struct _extended_joystick;
-class _BJoystickTweaker;
 struct _joystick_info;
 
-/* -----------------------------------------------------------------------*/
 class BJoystick {
 public:
-					BJoystick();
-virtual				~BJoystick();
+							BJoystick();
+	virtual					~BJoystick();
 
-		status_t	Open(const char *portName);
-		status_t	Open(const char *portName, bool enter_enhanced);
-		void		Close(void);
+			status_t		Open(const char* portName);
+			status_t		Open(const char* portName, bool enhanced);
+			void			Close();
 
-		status_t	Update(void);
-		status_t	SetMaxLatency(bigtime_t max_latency);
+			status_t		Update();
+			status_t		SetMaxLatency(bigtime_t maxLatency);
 
-		bigtime_t	timestamp;
-		int16		horizontal;
-		int16		vertical;
+			bigtime_t		timestamp;
+			int16			horizontal;
+			int16			vertical;
 		
-		/* NOTE: true == off */
-		bool		button1;
-		bool		button2;
+			bool			button1;
+			bool			button2;
 
-		int32		CountDevices();
-		status_t	GetDeviceName(int32 n, char * name,
-						size_t bufSize = B_OS_NAME_LENGTH);
+			int32			CountDevices();
+			status_t		GetDeviceName(int32 index, char* name,
+								size_t bufSize = B_OS_NAME_LENGTH);
 
-		bool		EnterEnhancedMode(const entry_ref * ref = NULL);
+			bool			EnterEnhancedMode(const entry_ref* ref = NULL);
 
-		int32		CountSticks();
+			int32			CountSticks();
 
-		status_t	GetControllerModule(BString * out_name);
-		status_t	GetControllerName(BString * out_name);
+			status_t		GetControllerModule(BString* outName);
+			status_t		GetControllerName(BString* outName);
 
-		bool		IsCalibrationEnabled();
-		status_t	EnableCalibration(bool calibrates = true);
+			bool			IsCalibrationEnabled();
+			status_t		EnableCalibration(bool calibrates = true);
 
-		int32		CountAxes();
-		status_t	GetAxisValues(int16 * out_values, int32 for_stick = 0);
-		status_t	GetAxisNameAt(int32 index, BString * out_name);
+			int32			CountAxes();
+			status_t		GetAxisValues(int16* outValues,
+								int32 forStick = 0);
+			status_t		GetAxisNameAt(int32 index,
+								BString* outName);
 		
-		int32		CountHats();
-		status_t	GetHatValues(uint8 * out_hats, int32 for_stick = 0);
-		status_t	GetHatNameAt(int32 index, BString * out_name);
+			int32			CountHats();
+			status_t		GetHatValues(uint8* outHats,
+								int32 forStick = 0);
+			status_t		GetHatNameAt(int32 index, BString* outName);
 		
-		int32		CountButtons();
-		/* NOTE: Buttons() are 1 == on */
-		uint32		ButtonValues(int32 for_stick = 0);
-		status_t	GetButtonNameAt(int32 index, BString * out_name);
+			int32			CountButtons();
+
+			uint32			ButtonValues(int32 forStick = 0);
+			status_t		GetButtonNameAt(int32 index,
+								BString* outName);
 
 protected:
-
-virtual	void		Calibrate(struct _extended_joystick * reading);
+	virtual	void			Calibrate(struct _extended_joystick*);
 
 private:
 friend class _BJoystickTweaker;
 
-		void		ScanDevices(bool use_disabled = false);
-		status_t	GatherEnhanced_info(const entry_ref * ref = NULL);
-		status_t	SaveConfig(const entry_ref * ref = NULL);
-		
-		bool			fBeBoxMode;
-		bool			fReservedBool;
-		int				ffd;
-		BList*			fDevices;
-		_joystick_info* fJoystickInfo;
-		char* 			fDevName;
+			void			ScanDevices(bool useDisabled = false);
+			status_t		GatherEnhanced_info(const entry_ref* ref = NULL);
+			status_t		SaveConfig(const entry_ref* ref = NULL);
+
+			bool			fBeBoxMode;
+			bool			fReservedBool;
+			int				ffd;
+			BList*			fDevices;
+			_joystick_info*	fJoystickInfo;
+			char*			fDevName;
 #if DEBUG
 public:
-	static FILE *sLogFile;
+	static	FILE*			sLogFile;
 #endif
 };
 
-#endif
+#endif // _JOYSTICK_H
