cb3f273dd2ec921b8134f9378a91adb0b2af165b
Stephan Aßmus <superstippi@gmx.de>
2009-08-28 14:39:55 +0000

    Rewrote more headers, style updates in some
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@32785 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/media/PlaySound.h
+++ headers/os/media/PlaySound.h
@@ -1,31 +1,23 @@
-/******************************************************************************
-
-	File:			PlaySound.h
-
-	Description:	Interface for a simple beep sound.
-
-	Copyright 1995-97, Be Incorporated
-
-******************************************************************************/
+/*
+ * Copyright 2009, Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
 #ifndef _PLAY_SOUND_H
 #define _PLAY_SOUND_H
 
-#ifndef _BE_BUILD_H
-#include <BeBuild.h>
-#endif
 #include <OS.h>
 #include <Entry.h>
 
+
 typedef sem_id sound_handle;
 
-sound_handle play_sound(const entry_ref *soundRef,
-						bool mix,
-						bool queue,
-						bool background
-						);
+
+sound_handle play_sound(const entry_ref* soundRef, bool mix, bool queue,
+	bool background);
 
 status_t stop_sound(sound_handle handle);
 
 status_t wait_for_sound(sound_handle handle);
 
-#endif			/* #ifndef _PLAY_SOUND_H*/
+
+#endif // _PLAY_SOUND_H
