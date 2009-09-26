04aa141beb3f9e17ec7698f15c72ae17720370f7
Michael Lotz <mmlr@mlotz.ch>
2009-09-26 00:09:13 +0000

    Adding a VMWare mouse input server filter add-on. It supports the same io port based protocol as the X vmmouse driver does. It yields proper absolute pointer positions under both the normal VMWare console and using the builtin VNC server. Therefore "fixes" the issue seen in #4630.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@33298 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/add-ons/input_server/filters/vmware_mouse/VMWareTypes.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2009, Haiku Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Michael Lotz <mmlr@mlotz.ch>
+ */
+#ifndef VMWARE_TYPES_H
+#define VMWARE_TYPES_H
+
+#define VMWWARE_PORT_MAGIC					0x564d5868
+#define VMWWARE_PORT_NUMBER					0x5658
+
+#define VMWWARE_VERSION_ID					0x3442554a
+#define VMWWARE_ERROR						0xffff
+
+#define VMWWARE_VALUE_DISABLE				0x000000f5
+#define VMWWARE_VALUE_READ_ID				0x45414552
+#define VMWWARE_VALUE_REQUEST_ABSOLUTE		0x53424152
+
+#define VMWWARE_COMMAND_POINTER_DATA		39
+#define VMWWARE_COMMAND_POINTER_STATUS		40
+#define VMWWARE_COMMAND_POINTER_COMMAND		41
+
+
+struct command_s {
+	uint32	magic;
+	uint32	value;
+	uint32	command;
+	uint32	port;
+} _PACKED;
+
+
+struct data_s {
+	uint16	buttons;
+	uint16	flags;
+	int32	x; // signed when relative
+	int32	y; // signed when relative
+	int32	z; // always signed
+} _PACKED;
+
+
+struct status_s {
+	uint16	num_words;
+	uint16	status;
+	uint32	unused[2];
+} _PACKED;
+
+
+struct version_s {
+	uint32	version;
+	uint32	unused[3];
+} _PACKED;
+
+
+union packet_u {
+	struct command_s	command;
+	struct data_s		data;
+	struct status_s		status;
+	struct version_s	version;
+};
+
+#endif // VMWARE_TYPES_H
