35e2c7ab62580807b1783f1b4b557e35beeeb18b
Jérôme Duval <korli@users.berlios.de>
2009-11-08 23:30:28 +0000

    updated libpng to 1.2.40
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@33956 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/libs/png/png.c
+++ src/libs/png/png.c
@@ -16,7 +16,7 @@
 #include "png.h"
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_2_39 Your_png_h_is_not_version_1_2_39;
+typedef version_1_2_40 Your_png_h_is_not_version_1_2_40;
 
 /* Version information for C files.  This had better match the version
  * string defined in png.h.  */
@@ -705,7 +705,7 @@ png_charp PNGAPI
 png_get_copyright(png_structp png_ptr)
 {
    png_ptr = png_ptr;  /* Silence compiler warning about unused png_ptr */
-   return ((png_charp) "\n libpng version 1.2.39 - August 13, 2009\n\
+   return ((png_charp) "\n libpng version 1.2.40 - September 10, 2009\n\
    Copyright (c) 1998-2009 Glenn Randers-Pehrson\n\
    Copyright (c) 1996-1997 Andreas Dilger\n\
    Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.\n");
