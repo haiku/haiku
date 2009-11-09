66570f5d3b3d089cc565cc92de253993d10267f7
Axel Dörfler <axeld@pinc-software.de>
2009-11-09 13:17:47 +0000

    * Replaced "Mark As New" with a "Mark Asâ¦" Tracker add-on that let you choose   among all defined status. * Note, until bug #4851 is solved, the list Mail shows might differ from the   one "Mark Asâ¦" shows.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@33960 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/tracker/mark_as/MarkAs.cpp
+++ src/add-ons/tracker/mark_as/MarkAs.cpp
@@ -1,33 +1,121 @@
 /*
- * Copyright 2006, Ryan Leavengood, leavengood@gmail.com. All rights reserved.
+ * Copyright 2009, Axel Dörfler, axeld@pinc-software.de.
  * Distributed under the terms of the MIT License.
  */
 
 
+#include <Directory.h>
 #include <Entry.h>
+#include <FindDirectory.h>
+#include <MenuItem.h>
 #include <Message.h>
 #include <Node.h>
+#include <Path.h>
+#include <PopUpMenu.h>
 #include <String.h>
-#include <TrackerAddOn.h>
+#include <View.h>
+#include <Window.h>
+
+
+/*!	Returns the current mouse position in screen coordinates.
+	Since there is no method to retrieve this in the Be API without a view,
+	this looks a bit more complicated.
+*/
+static BPoint
+mouse_position()
+{
+	BWindow* window = new BWindow(BRect(-1000, -1000, -900, -900), "mouse",
+		B_NO_BORDER_WINDOW_LOOK, B_NORMAL_WINDOW_FEEL,
+		B_AVOID_FRONT | B_AVOID_FOCUS);
+	BView* view = new BView(window->Bounds(), "mouse", B_FOLLOW_ALL, 0);
+	window->AddChild(view);
+	window->Run();
+
+	window->Lock();
+
+	BPoint position;
+	uint32 buttons;
+	view->GetMouse(&position, &buttons);
+	view->ConvertToScreen(&position);
+
+	window->Quit();
+
+	return position;
+}
+
+
+static void
+add_status_item(BMenu* menu, const char* name)
+{
+	if (menu->FindItem(name) != NULL)
+		return;
+
+	// Sort items alphabetically
+	int32 index;
+	for (index = 0; index < menu->CountItems(); index++) {
+		if (strcmp(menu->ItemAt(index)->Label(), name) > 0)
+			break;
+	}
+
+	menu->AddItem(new BMenuItem(name, NULL), index);
+}
+
+
+static void
+retrieve_status_items(BMenu* menu)
+{
+	BPath path;
+	if (find_directory(B_USER_SETTINGS_DIRECTORY, &path) != B_OK)
+		return;
+
+	path.Append("Mail/status");
+
+	BDirectory directory(path.Path());
+
+	entry_ref ref;
+	while (directory.GetNextRef(&ref) == B_OK) {
+		if (!strcmp(ref.name, ".") || !strcmp(ref.name, ".."))
+			continue;
+
+		add_status_item(menu, ref.name);
+	}
+
+	add_status_item(menu, "New");
+	add_status_item(menu, "Read");
+	add_status_item(menu, "Replied");
+}
+
+
+// #pragma mark -
 
 
 extern "C" void
-process_refs(entry_ref dir, BMessage* msg, void* /*reserved*/)
+process_refs(entry_ref dir, BMessage* message, void* /*reserved*/)
 {
-	int32 refs;
-	entry_ref ref;
-	BString status(STATUS_HERE);
-		// The above will be substituted with a real value (such as "New" or 
-		// "Read") by a #define and #include in another file. This is done 
-		// to avoid duplicate code.                                                                
-	BString type;
+	BPopUpMenu* menu = new BPopUpMenu("status");
+	retrieve_status_items(menu);
 
-	for (refs = 0; msg->FindRef("refs", refs, &ref) == B_NO_ERROR; refs++) {
+	BMenuItem* item = menu->Go(mouse_position() - BPoint(5, 5), false, true);
+	if (item == NULL)
+		return;
+
+	BString status = item->Label();
+
+	entry_ref ref;
+	for (int i = 0; message->FindRef("refs", i, &ref) == B_OK; i++) {
 		BNode node(&ref);
-		if ((node.InitCheck() == B_NO_ERROR)
-			&& (node.ReadAttrString("BEOS:TYPE", &type) == B_NO_ERROR)
-			&& (type == "text/x-email"))
-			node.WriteAttrString("MAIL:status", &status);
+		BString type;
+
+		if (node.InitCheck() == B_OK
+			&& node.ReadAttrString("BEOS:TYPE", &type) == B_OK
+			&& type == "text/x-email") {
+			BString previousStatus;
+
+			// Only update the attribute if there is an actual change
+			if (node.ReadAttrString("MAIL:status", &previousStatus) != B_OK
+				|| previousStatus != status)
+				node.WriteAttrString("MAIL:status", &status);
+		}
 	}
-}                                                                                     
+}
 
--- src/add-ons/tracker/mark_as/MarkAsRead.cpp
+++ src/add-ons/tracker/mark_as/MarkAsRead.cpp
@@ -1,2 +1,35 @@
-#define STATUS_HERE "Read"
-#include "MarkAs.cpp"
+/*
+ * Copyright 2009, Axel Dörfler, axeld@pinc-software.de.
+ * Copyright 2006, Ryan Leavengood, leavengood@gmail.com. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+
+
+#include <Entry.h>
+#include <Message.h>
+#include <Node.h>
+#include <String.h>
+
+
+extern "C" void
+process_refs(entry_ref dir, BMessage* message, void* /*reserved*/)
+{
+	entry_ref ref;
+	for (int i = 0; message->FindRef("refs", i, &ref) == B_OK; i++) {
+		BNode node(&ref);
+		BString type;
+
+		if (node.InitCheck() == B_OK
+			&& node.ReadAttrString("BEOS:TYPE", &type) == B_OK
+			&& type == "text/x-email") {
+			BString previousStatus;
+			BString status("Read");
+
+			// Only update the attribute if there is an actual change
+			if (node.ReadAttrString("MAIL:status", &previousStatus) != B_OK
+				|| previousStatus != status)
+				node.WriteAttrString("MAIL:status", &status);
+		}
+	}
+}
+
