045852f9524fcd5152fa65cc0dd6150b8ac775a9
Ingo Weinhold <ingo_weinhold@gmx.de>
2009-11-24 23:01:22 +0000

    Forgot the search and replace step after the copy and paste.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@34226 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/build/os/support/SupportDefs.h
+++ headers/build/os/support/SupportDefs.h
@@ -59,83 +59,83 @@ typedef uint32					perform_code;
 
 
 /* printf()/scanf() format strings for [u]int* types */
-#define FSSH_B_PRId8		PRId8
-#define FSSH_B_PRIi8		PRIi8
-#define FSSH_B_PRId16		PRId16
-#define FSSH_B_PRIi16		PRIi16
-#define FSSH_B_PRId32		PRId32
-#define FSSH_B_PRIi32		PRIi32
-#define FSSH_B_PRId64		PRId64
-#define FSSH_B_PRIi64		PRIi64
-#define FSSH_B_PRIu8		PRIu8
-#define FSSH_B_PRIo8		PRIo8
-#define FSSH_B_PRIx8		PRIx8
-#define FSSH_B_PRIX8		PRIX8
-#define FSSH_B_PRIu16		PRIu16
-#define FSSH_B_PRIo16		PRIo16
-#define FSSH_B_PRIx16		PRIx16
-#define FSSH_B_PRIX16		PRIX16
-#define FSSH_B_PRIu32		PRIu32
-#define FSSH_B_PRIo32		PRIo32
-#define FSSH_B_PRIx32		PRIx32
-#define FSSH_B_PRIX32		PRIX32
-#define FSSH_B_PRIu64		PRIu64
-#define FSSH_B_PRIo64		PRIo64
-#define FSSH_B_PRIx64		PRIx64
-#define FSSH_B_PRIX64		PRIX64
+#define B_PRId8			PRId8
+#define B_PRIi8			PRIi8
+#define B_PRId16		PRId16
+#define B_PRIi16		PRIi16
+#define B_PRId32		PRId32
+#define B_PRIi32		PRIi32
+#define B_PRId64		PRId64
+#define B_PRIi64		PRIi64
+#define B_PRIu8			PRIu8
+#define B_PRIo8			PRIo8
+#define B_PRIx8			PRIx8
+#define B_PRIX8			PRIX8
+#define B_PRIu16		PRIu16
+#define B_PRIo16		PRIo16
+#define B_PRIx16		PRIx16
+#define B_PRIX16		PRIX16
+#define B_PRIu32		PRIu32
+#define B_PRIo32		PRIo32
+#define B_PRIx32		PRIx32
+#define B_PRIX32		PRIX32
+#define B_PRIu64		PRIu64
+#define B_PRIo64		PRIo64
+#define B_PRIx64		PRIx64
+#define B_PRIX64		PRIX64
 
-#define FSSH_B_SCNd8 		SCNd8
-#define FSSH_B_SCNi8 		SCNi8
-#define FSSH_B_SCNd16		SCNd16
-#define FSSH_B_SCNi16 		SCNi16
-#define FSSH_B_SCNd32 		SCNd32
-#define FSSH_B_SCNi32 		SCNi32
-#define FSSH_B_SCNd64		SCNd64
-#define FSSH_B_SCNi64 		SCNi64
-#define FSSH_B_SCNu8 		SCNu8
-#define FSSH_B_SCNo8 		SCNo8
-#define FSSH_B_SCNx8 		SCNx8
-#define FSSH_B_SCNu16		SCNu16
-#define FSSH_B_SCNu16		SCNu16
-#define FSSH_B_SCNx16		SCNx16
-#define FSSH_B_SCNu32 		SCNu32
-#define FSSH_B_SCNo32 		SCNo32
-#define FSSH_B_SCNx32 		SCNx32
-#define FSSH_B_SCNu64		SCNu64
-#define FSSH_B_SCNo64		SCNo64
-#define FSSH_B_SCNx64		SCNx64
+#define B_SCNd8 		SCNd8
+#define B_SCNi8 		SCNi8
+#define B_SCNd16		SCNd16
+#define B_SCNi16 		SCNi16
+#define B_SCNd32 		SCNd32
+#define B_SCNi32 		SCNi32
+#define B_SCNd64		SCNd64
+#define B_SCNi64 		SCNi64
+#define B_SCNu8 		SCNu8
+#define B_SCNo8 		SCNo8
+#define B_SCNx8 		SCNx8
+#define B_SCNu16		SCNu16
+#define B_SCNu16		SCNu16
+#define B_SCNx16		SCNx16
+#define B_SCNu32 		SCNu32
+#define B_SCNo32 		SCNo32
+#define B_SCNx32 		SCNx32
+#define B_SCNu64		SCNu64
+#define B_SCNo64		SCNo64
+#define B_SCNx64		SCNx64
 
 
 /* printf() format strings for some standard types */
 /* size_t */
-#define FSSH_B_PRIuSIZE		FSSH_B_PRIu32
-#define FSSH_B_PRIoSIZE		FSSH_B_PRIo32
-#define FSSH_B_PRIxSIZE		FSSH_B_PRIx32
-#define FSSH_B_PRIXSIZE		FSSH_B_PRIX32
+#define B_PRIuSIZE		B_PRIu32
+#define B_PRIoSIZE		B_PRIo32
+#define B_PRIxSIZE		B_PRIx32
+#define B_PRIXSIZE		B_PRIX32
 /* ssize_t */
-#define FSSH_B_PRIdSSIZE	FSSH_B_PRId32
-#define FSSH_B_PRIiSSIZE	FSSH_B_PRIi32
+#define B_PRIdSSIZE		B_PRId32
+#define B_PRIiSSIZE		B_PRIi32
 /* addr_t */
 #ifdef HAIKU_HOST_PLATFORM_64_BIT
-#	define FSSH_B_PRIuADDR	FSSH_B_PRIu64
-#	define FSSH_B_PRIoADDR	FSSH_B_PRIo64
-#	define FSSH_B_PRIxADDR	FSSH_B_PRIx64
-#	define FSSH_B_PRIXADDR	FSSH_B_PRIX64
+#	define B_PRIuADDR	B_PRIu64
+#	define B_PRIoADDR	B_PRIo64
+#	define B_PRIxADDR	B_PRIx64
+#	define B_PRIXADDR	B_PRIX64
 #else
-#	define FSSH_B_PRIuADDR	FSSH_B_PRIu32
-#	define FSSH_B_PRIoADDR	FSSH_B_PRIo32
-#	define FSSH_B_PRIxADDR	FSSH_B_PRIx32
-#	define FSSH_B_PRIXADDR	FSSH_B_PRIX32
+#	define B_PRIuADDR	B_PRIu32
+#	define B_PRIoADDR	B_PRIo32
+#	define B_PRIxADDR	B_PRIx32
+#	define B_PRIXADDR	B_PRIX32
 #endif
 /* off_t */
-#define FSSH_B_PRIdOFF		FSSH_B_PRId64
-#define FSSH_B_PRIiOFF		FSSH_B_PRIi64
+#define B_PRIdOFF		B_PRId64
+#define B_PRIiOFF		B_PRIi64
 /* dev_t */
-#define FSSH_B_PRIdDEV		FSSH_B_PRId32
-#define FSSH_B_PRIiDEV		FSSH_B_PRIi32
+#define B_PRIdDEV		B_PRId32
+#define B_PRIiDEV		B_PRIi32
 /* ino_t */
-#define FSSH_B_PRIdINO		FSSH_B_PRId64
-#define FSSH_B_PRIiINO		FSSH_B_PRIi64
+#define B_PRIdINO		B_PRId64
+#define B_PRIiINO		B_PRIi64
 
 
 /* Empty string ("") */
