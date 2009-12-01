6bb7a8908c9dd32a8de114cf7f6ffa31b760c7a9
Axel Dörfler <axeld@pinc-software.de>
2009-12-01 07:58:45 +0000

    * Cleanup, removed author information.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@34398 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/app/Application.h
+++ headers/os/app/Application.h
@@ -1,16 +1,12 @@
 /*
- * Copyright 2001-2007, Haiku.
+ * Copyright 2001-2009, Haiku Inc.
  * Distributed under the terms of the MIT License.
- *
- * Authors:
- *		Erik Jaesler (erik@cgsoftware.com)
  */
 #ifndef _APPLICATION_H
 #define _APPLICATION_H
 
 
-#include <BeBuild.h>
-#include <AppDefs.h>		// For convenience
+#include <AppDefs.h>
 #include <InterfaceDefs.h>
 #include <Looper.h>
 #include <Messenger.h>
@@ -35,61 +31,63 @@ namespace BPrivate {
 
 class BApplication : public BLooper {
 public:
-							BApplication(const char* signature);
-							BApplication(const char* signature, status_t* error);
-	virtual					~BApplication();
+								BApplication(const char* signature);
+								BApplication(const char* signature,
+									status_t* error);
+	virtual						~BApplication();
 
 	// Archiving
-							BApplication(BMessage* data);
-	static	BArchivable*	Instantiate(BMessage* data);
-	virtual	status_t		Archive(BMessage* data, bool deep = true) const;
+								BApplication(BMessage* data);
+	static	BArchivable*		Instantiate(BMessage* data);
+	virtual	status_t			Archive(BMessage* data, bool deep = true) const;
 
-			status_t		InitCheck() const;
+			status_t			InitCheck() const;
 
 	// App control and System Message handling
-	virtual	thread_id		Run();
-	virtual	void			Quit();
-	virtual bool			QuitRequested();
-	virtual	void			Pulse();
-	virtual	void			ReadyToRun();
-	virtual	void			MessageReceived(BMessage* msg);
-	virtual	void			ArgvReceived(int32 argc, char** argv);
-	virtual	void			AppActivated(bool active);
-	virtual	void			RefsReceived(BMessage* message);
-	virtual	void			AboutRequested();
+	virtual	thread_id			Run();
+	virtual	void				Quit();
+	virtual bool				QuitRequested();
+	virtual	void				Pulse();
+	virtual	void				ReadyToRun();
+	virtual	void				MessageReceived(BMessage* message);
+	virtual	void				ArgvReceived(int32 argc, char** argv);
+	virtual	void				AppActivated(bool active);
+	virtual	void				RefsReceived(BMessage* message);
+	virtual	void				AboutRequested();
 
 	// Scripting
-	virtual BHandler*		ResolveSpecifier(BMessage* msg,
-											 int32 index,
-											 BMessage* specifier,
-											 int32 form,
-											 const char* property);
+	virtual BHandler*			ResolveSpecifier(BMessage* message, int32 index,
+									BMessage* specifier, int32 form,
+									const char* property);
 
 	// Cursor control, window/looper list, and app info
-			void			ShowCursor();
-			void			HideCursor();
-			void			ObscureCursor();
-			bool			IsCursorHidden() const;
-			void			SetCursor(const void* cursor);
-			void			SetCursor(const BCursor* cursor, bool sync = true);
-			int32			CountWindows() const;
-			BWindow*		WindowAt(int32 index) const;
-			int32			CountLoopers() const;
-			BLooper*		LooperAt(int32 index) const;
-			bool			IsLaunching() const;
-			status_t		GetAppInfo(app_info* info) const;
-	static	BResources*		AppResources();
+			void				ShowCursor();
+			void				HideCursor();
+			void				ObscureCursor();
+			bool				IsCursorHidden() const;
+			void				SetCursor(const void* cursor);
+			void				SetCursor(const BCursor* cursor,
+									bool sync = true);
 
-	virtual	void			DispatchMessage(BMessage* message,
-											BHandler* handler);
-			void			SetPulseRate(bigtime_t rate);
+			int32				CountWindows() const;
+			BWindow*			WindowAt(int32 index) const;
+
+			int32				CountLoopers() const;
+			BLooper*			LooperAt(int32 index) const;
+			bool				IsLaunching() const;
+			status_t			GetAppInfo(app_info* info) const;
+	static	BResources*			AppResources();
+
+	virtual	void				DispatchMessage(BMessage* message,
+									BHandler* handler);
+			void				SetPulseRate(bigtime_t rate);
 
 	// More scripting
-	virtual status_t		GetSupportedSuites(BMessage* data);
+	virtual status_t			GetSupportedSuites(BMessage* data);
 
 
 	// Private or reserved
-	virtual status_t		Perform(perform_code d, void* arg);
+	virtual status_t			Perform(perform_code d, void* arg);
 
 	class Private;
 
@@ -99,70 +97,57 @@ private:
 	friend class Private;
 	friend class BServer;
 
-							BApplication(const char* signature, bool initGUI,
-										 status_t* error);
-							BApplication(uint32 signature);
-							BApplication(const BApplication&);
-			BApplication&	operator=(const BApplication&);
+								BApplication(const char* signature, bool initGUI,
+									status_t* error);
+								BApplication(uint32 signature);
+								BApplication(const BApplication&);
+			BApplication&		operator=(const BApplication&);
 
-	virtual	void			_ReservedApplication1();
-	virtual	void			_ReservedApplication2();
-	virtual	void			_ReservedApplication3();
-	virtual	void			_ReservedApplication4();
-	virtual	void			_ReservedApplication5();
-	virtual	void			_ReservedApplication6();
-	virtual	void			_ReservedApplication7();
-	virtual	void			_ReservedApplication8();
+	virtual	void				_ReservedApplication1();
+	virtual	void				_ReservedApplication2();
+	virtual	void				_ReservedApplication3();
+	virtual	void				_ReservedApplication4();
+	virtual	void				_ReservedApplication5();
+	virtual	void				_ReservedApplication6();
+	virtual	void				_ReservedApplication7();
+	virtual	void				_ReservedApplication8();
 
-	virtual	bool			ScriptReceived(BMessage* msg, int32 index,
-								BMessage* specifier, int32 form,
-								const char* property);
-			void			_InitData(const char* signature, bool initGUI,
-								status_t* error);
-			void			BeginRectTracking(BRect r, bool trackWhole);
-			void			EndRectTracking();
-			status_t		_SetupServerAllocator();
-			status_t		_InitGUIContext();
-			status_t		_ConnectToServer();
-/*			void			send_drag(	BMessage* msg,
-										int32 vs_token,
-										BPoint offset,
-										BRect drag_rect,
-										BHandler* reply_to);
-			void			send_drag(	BMessage* msg,
-										int32 vs_token,
-										BPoint offset,
-										int32 bitmap_token,
-										drawing_mode dragMode,
-										BHandler* reply_to);
-			void			write_drag(_BSession_* session, BMessage* a_message);
-*/			bool			_QuitAllWindows(bool force);
-			bool			_WindowQuitLoop(bool quitFilePanels, bool force);
-			void			_ArgvReceived(BMessage* message);
+	virtual	bool				ScriptReceived(BMessage* msg, int32 index,
+									BMessage* specifier, int32 form,
+									const char* property);
+			void				_InitData(const char* signature, bool initGUI,
+									status_t* error);
+			void				BeginRectTracking(BRect r, bool trackWhole);
+			void				EndRectTracking();
+			status_t			_SetupServerAllocator();
+			status_t			_InitGUIContext();
+			status_t			_ConnectToServer();
+			bool				_QuitAllWindows(bool force);
+			bool				_WindowQuitLoop(bool quitFilePanels, bool force);
+			void				_ArgvReceived(BMessage* message);
 
-			uint32			InitialWorkspace();
-			int32			_CountWindows(bool includeMenus) const;
-			BWindow*		_WindowAt(uint32 index, bool includeMenus) const;
+			uint32				InitialWorkspace();
+			int32				_CountWindows(bool includeMenus) const;
+			BWindow*			_WindowAt(uint32 index, bool includeMenus) const;
 
-	static	void			_InitAppResources();
+	static	void				_InitAppResources();
 
 private:
-	static	BResources*		sAppResources;
+	static	BResources*			sAppResources;
 
-			const char*		fAppName;
-			BPrivate::PortLink* fServerLink;
+			const char*			fAppName;
+			BPrivate::PortLink*	fServerLink;
 			BPrivate::ServerMemoryAllocator* fServerAllocator;
 
-			void*			fCursorData;
-			bigtime_t		fPulseRate;
-			uint32			fInitialWorkspace;
-			//_drag_data_*	fDraggedMessage;
-			BMessageRunner*	fPulseRunner;
-			status_t		fInitError;
-			void*			fServerReadOnlyMemory;
-			uint32			_reserved[12];
+			void*				fCursorData;
+			bigtime_t			fPulseRate;
+			uint32				fInitialWorkspace;
+			BMessageRunner*		fPulseRunner;
+			status_t			fInitError;
+			void*				fServerReadOnlyMemory;
+			uint32				_reserved[12];
 
-			bool			fReadyToRunCalled;
+			bool				fReadyToRunCalled;
 };
 
 // Global Objects
