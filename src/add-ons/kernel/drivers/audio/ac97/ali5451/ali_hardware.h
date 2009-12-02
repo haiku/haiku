88e5c1ac3b3d40331dc20def20c8810f4ab9b5bd
Jérôme Duval <korli@users.berlios.de>
2009-12-02 20:49:37 +0000

    ALI 5451 audio driver for Haiku multi audio by Krzysiek Cwiertnia  * playback, tested with 16bit format, with sample rate range from 8 to 48kHz  * recording, fixed at 48kHz 16 bit (read below)  * controlling some mixers, input selector, etc.
    I placed the driver in the ac97 directory as it fits better.
    Also a few coding style fixes by me.
    This driver collides at least with one pci id of the sis7018 driver.
    
    
    
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@34452 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/add-ons/kernel/drivers/audio/ac97/ali5451/ali_hardware.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2009, Krzysztof Ćwiertnia (krzysiek.bmkx_gmail_com).
+ *
+ * All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
+
+
+#ifndef _ALI_HARDWARE_H
+#define _ALI_HARDWARE_H
+
+// AC97 codec registers
+
+#define AC97_REG_RESET 0x00
+#define AC97_MIX_MASTER 0x02
+#define AC97_MIX_HEADPHONE 0x04
+#define AC97_MIX_MONO 0x06
+#define AC97_MIX_PHONE 0x0c
+#define AC97_MIX_MIC 0x0e
+#define AC97_MIX_LINEIN 0x10
+#define AC97_MIX_CD 0x12
+#define AC97_MIX_AUX 0x16
+#define AC97_MIX_PCM 0x18
+#define AC97_MIX_RECORDSELECT 0x1a
+#define AC97_MIX_RECORDGAIN 0x1c
+#define AC97_REG_GEN 0x20
+#define AC97_REG_POWER 0x26
+#define AC97_FRONT_DAC_SR 0x2c
+#define AC97_ADC_RATE 0x32
+#define AC97_JACK_SENSE 0x72
+#define AC97_REG_ID1 0x7c
+#define AC97_REG_ID2 0x7e
+
+
+uint16 ac97_read(ali_dev *card, uint8 reg);
+void ac97_write(ali_dev *card, uint8 reg, uint16 val);
+
+status_t hardware_init(ali_dev *card);
+void hardware_terminate(ali_dev *card);
+
+uint32 ali_read_int(ali_dev *card);
+
+bool ali_voice_start(ali_stream *stream);
+void ali_voice_stop(ali_stream *stream);
+void ali_voice_clr_int(ali_stream *stream);
+
+#endif // _ALI_HARDWARE_H
