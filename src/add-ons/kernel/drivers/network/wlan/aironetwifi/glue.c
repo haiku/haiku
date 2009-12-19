eb4ede2859a70492462118fc8491cbea61cb2f53
Colin Günther <coling@gmx.de>
2009-12-19 20:34:50 +0000

    * Adding the glue.c file needed to make the aironetwifi linking. The driver   is not tested, though, as I don't own the hardware. * Note: This wifi driver is special, as it doesn't require the FBSD_WLAN flag   set in the glue.c. This is due to the driver making little use of the   wlan stack. Thus no initialization of wlan stuff is needed.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@34710 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/add-ons/kernel/drivers/network/wlan/aironetwifi/glue.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2009, Colin Günther, coling@gmx.de.
+ * All Rights Reserved. Distributed under the terms of the MIT License.
+ */
+
+
+#include <sys/bus.h>
+#include <sys/kernel.h>
+
+#include <net/if.h>
+#include <net/ethernet.h>
+#include <net/if_media.h>
+
+#include <net80211/ieee80211_var.h>
+
+#include <machine/bus.h>
+
+#include <dev/an/if_aironet_ieee.h>
+#include <dev/an/if_anreg.h>
+
+
+// Netgraph function variables.
+void (*ng_ether_attach_p)(struct ifnet *ifp) = NULL;
+void (*ng_ether_detach_p)(struct ifnet *ifp) = NULL;
+
+
+HAIKU_FBSD_WLAN_DRIVER_GLUE(aironetwifi, an, pci)
+NO_HAIKU_FBSD_MII_DRIVER();
+NO_HAIKU_REENABLE_INTERRUPTS();
+HAIKU_DRIVER_REQUIREMENTS(FBSD_TASKQUEUES);
+
+
+int
+HAIKU_CHECK_DISABLE_INTERRUPTS(device_t dev)
+{
+	struct an_softc* sc = (struct an_softc*)device_get_softc(dev);
+
+	if (sc->an_gone)
+		return 0;
+
+	/* Disable interrupts. */
+	CSR_WRITE_2(sc, AN_INT_EN(sc->mpi350), 0);
+
+	return 1;
+}
