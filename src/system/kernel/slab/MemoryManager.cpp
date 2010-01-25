30f2c6c8599407d744e7979d7fe69db725ec343e
Ingo Weinhold <ingo_weinhold@gmx.de>
2010-01-25 15:42:35 +0000

    When we create areas for large raw allocations there's no need to allocate clear pages.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@35286 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/kernel/slab/MemoryManager.cpp
+++ src/system/kernel/slab/MemoryManager.cpp
@@ -603,8 +603,9 @@ MemoryManager::AllocateRaw(size_t size, uint32 flags, void*& _pages)
 			(flags & CACHE_ALIGN_ON_SIZE) != 0
 				? B_ANY_KERNEL_BLOCK_ADDRESS : B_ANY_KERNEL_ADDRESS,
 			size, B_FULL_LOCK, B_KERNEL_READ_AREA | B_KERNEL_WRITE_AREA, 0,
-			(flags & CACHE_DONT_WAIT_FOR_MEMORY) != 0
-				? CREATE_AREA_DONT_WAIT : 0);
+			((flags & CACHE_DONT_WAIT_FOR_MEMORY) != 0
+					? CREATE_AREA_DONT_WAIT : 0)
+				| CREATE_AREA_DONT_CLEAR);
 		return area >= 0 ? B_OK : area;
 	}
 
