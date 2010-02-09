38ecc1a76420fb483f5f42dbf29e48046ef9da8b
Artur Wyszynski <aljen-mlists@o2.pl>
2010-02-09 06:09:28 +0000

    Added FreeBSD fenv implementation - tested with dmd port, seems that it works
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@35437 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/posix/fenv.h
@@ -0,0 +1,15 @@
+#ifndef _FENV_H
+#define _FENV_H
+
+#if defined(_X86_)
+#  include <arch/x86/fenv.h>
+#elif defined(__ARM__)
+#  include <arch/arm/fenv.h>
+#elif defined(__POWERPC__)
+#  include <arch/ppc/fenv.h>
+#else
+#  error There is no fenv.h for this architecture!
+#endif
+
+#endif /* _FENV_H */
+
