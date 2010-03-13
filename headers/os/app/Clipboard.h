2318b47802d763f671cad12088c69246200e0f1e
Stephan Aßmus <superstippi@gmx.de>
2010-03-13 17:55:17 +0000

    Indentation update.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@35841 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/app/Clipboard.h
+++ headers/os/app/Clipboard.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007, Haiku Inc. All Rights Reserved.
+ * Copyright 2007-2009, Haiku Inc. All Rights Reserved.
  * Distributed under the terms of the MIT License.
  *
  * Author:
@@ -21,49 +21,50 @@ enum {
 };
 
 class BClipboard {
-	public:
-		BClipboard(const char* name, bool transient = false);
-		virtual ~BClipboard();
+public:
+								BClipboard(const char* name,
+									bool transient = false);
+	virtual						~BClipboard();
 
-		const char*	Name() const;
+			const char*			Name() const;
 
-		uint32		LocalCount() const;
-		uint32		SystemCount() const;
-		status_t	StartWatching(BMessenger target);
-		status_t	StopWatching(BMessenger target);
+			uint32				LocalCount() const;
+			uint32				SystemCount() const;
+			status_t			StartWatching(BMessenger target);
+			status_t			StopWatching(BMessenger target);
 
-		bool		Lock();
-		void		Unlock();
-		bool		IsLocked() const;
+			bool				Lock();
+			void				Unlock();
+			bool				IsLocked() const;
 
-		status_t	Clear();
-		status_t	Commit();
-		status_t	Commit(bool failIfChanged);
-		status_t	Revert();
+			status_t			Clear();
+			status_t			Commit();
+			status_t			Commit(bool failIfChanged);
+			status_t			Revert();
 
-		BMessenger	DataSource() const;
-		BMessage*	Data() const;
+			BMessenger			DataSource() const;
+			BMessage*			Data() const;
 
-	private:
-		BClipboard(const BClipboard &);
-		BClipboard	&operator=(const BClipboard &);
+private:
+								BClipboard(const BClipboard&);
+			BClipboard&			operator=(const BClipboard&);
 
-		virtual	void _ReservedClipboard1();
-		virtual	void _ReservedClipboard2();
-		virtual	void _ReservedClipboard3();
+	virtual	void				_ReservedClipboard1();
+	virtual	void				_ReservedClipboard2();
+	virtual	void				_ReservedClipboard3();
 
-		bool		_AssertLocked() const;
-		status_t	_DownloadFromSystem(bool force = false);
-		status_t	_UploadToSystem();
-
-		uint32		_reserved0;
-		BMessage*	fData;
-		BLocker		fLock;
-		BMessenger	fClipHandler;
-		BMessenger	fDataSource;
-		uint32		fCount;
-		char*		fName;
-		uint32		_reserved[4];
+			bool				_AssertLocked() const;
+			status_t			_DownloadFromSystem(bool force = false);
+			status_t			_UploadToSystem();
+	
+			uint32				_reserved0;
+			BMessage*			fData;
+			BLocker				fLock;
+			BMessenger			fClipHandler;
+			BMessenger			fDataSource;
+			uint32				fCount;
+			char*				fName;
+			uint32				_reserved[4];
 };
 
 extern BClipboard* be_clipboard;
