545259ff2e3e145d84dda9e51bce1061188c5b17
Michael Lotz <mmlr@mlotz.ch>
2010-04-14 12:07:09 +0000

    This fixes the debug build of libroot.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@36253 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/libroot/posix/glibc/stdio-common/printf_size.c
+++ src/system/libroot/posix/glibc/stdio-common/printf_size.c
@@ -19,6 +19,8 @@
    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
    02111-1307 USA.  */
 
+#include <libioP.h>
+
 #include <stdio_private.h>
 #include <ctype.h>
 #include <ieee754.h>
