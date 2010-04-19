45b01eb84189a623981270a02d21c7a8cb93a9e1
Ingo Weinhold <ingo_weinhold@gmx.de>
2010-04-19 14:16:48 +0000

    Added private system arch specific arch_cpu_defs.h header, currently defining only a SPINLOCK_PAUSE() macro.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@36351 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/system/arch/m68k/arch_cpu_defs.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2010, Ingo Weinhold, ingo_weinhold@gmx.de.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _SYSTEM_ARCH_M68K_DEFS_H
+#define _SYSTEM_ARCH_M68K_DEFS_H
+
+
+#define SPINLOCK_PAUSE()	do {} while (false)
+
+
+#endif	/* _SYSTEM_ARCH_M68K_DEFS_H */
