e590fb24c657e81bf9388c0303dceaad02c9de51
Oliver Tappe <zooey@hirschkaefer.de>
2010-04-26 17:11:11 +0000

    * style adjustments * commented out a useless block of code (and added a respective TODO)
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@36496 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/preferences/locale/LanguageListView.h
+++ src/preferences/locale/LanguageListView.h
@@ -1,12 +1,11 @@
 /*
- * Copyright 2006-2010, Haiku.
- * All rights reserved. Distributed under the terms of the MIT License.
+ * Copyright 2006-2010, Haiku Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
  * Authors:
- *		Adrien Destugues <pulkomandy@gmail.com>
  *		Stephan Aßmus <superstippi@gmx.de>
+ *		Adrien Destugues <pulkomandy@gmail.com>
 */
-
-
 #ifndef __LANGUAGE_LIST_VIEW_H
 #define __LANGUAGE_LIST_VIEW_H
 
@@ -16,40 +15,42 @@
 #include <String.h>
 
 
-class LanguageListItem: public BStringItem {
-	public:
-		LanguageListItem(const char* text, const char* code);
-		LanguageListItem(const LanguageListItem& other);
-		~LanguageListItem();
+class LanguageListItem : public BStringItem {
+public:
+							LanguageListItem(const char* text,
+								const char* code);
+							LanguageListItem(const LanguageListItem& other);
+	virtual					~LanguageListItem();
 
-		const BString& LanguageCode() { return fLanguageCode; }
-		void DrawItem(BView *owner, BRect frame, bool complete = false);
+			const BString&	LanguageCode() { return fLanguageCode; }
+			void			DrawItem(BView* owner, BRect frame,
+								bool complete = false);
 
-	private:
-		BString fLanguageCode;
-		BBitmap* fIcon;
+private:
+			BString			fLanguageCode;
+			BBitmap*		fIcon;
 };
 
 
-class LanguageListView: public BOutlineListView {
-	public:
-		LanguageListView(const char* name, list_view_type type);
+class LanguageListView : public BOutlineListView {
+public:
+							LanguageListView(const char* name,
+								list_view_type type);
+	virtual					~LanguageListView();
 
-		~LanguageListView() { delete fMsgPrefLanguagesChanged; }
-
-		bool InitiateDrag(BPoint point, int32 index, bool wasSelected);
-		void MouseMoved(BPoint where, uint32 transit, const BMessage* msg);
-		void MoveItems(BList& items, int32 index);
-		void MoveItemFrom(BOutlineListView* origin, int32 index, int32 dropSpot = 0);
-		void AttachedToWindow()
-		{
-			BOutlineListView::AttachedToWindow();
-			ScrollToSelection();
-		}
-		void MessageReceived (BMessage* message);
-	private:
-		int32		fDropIndex;
-		BMessage*	fMsgPrefLanguagesChanged;
+			bool 			InitiateDrag(BPoint point, int32 index,
+								bool wasSelected);
+			void 			MouseMoved(BPoint where, uint32 transit,
+								const BMessage* msg);
+			void 			MoveItems(BList& items, int32 index);
+			void 			MoveItemFrom(BOutlineListView* origin, int32 index,
+								int32 dropSpot = 0);
+			void 			AttachedToWindow();
+			void 			MessageReceived (BMessage* message);
+private:
+			int32			fDropIndex;
+			BMessage*		fMsgPrefLanguagesChanged;
 };
+
 
 #endif
