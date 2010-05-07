626bfa871bc42067dff8fbb3c1b018ea7518aad0
Ingo Weinhold <ingo_weinhold@gmx.de>
2010-05-07 14:40:23 +0000

    Disabled serial debug output by default. Interestingly the kernel still produces some output.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/branches/releases/r1alpha2@36721 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/boot/platform/bios_ia32/start.c
+++ src/system/boot/platform/bios_ia32/start.c
@@ -127,7 +127,7 @@ _start(void)
 	args.arguments = NULL;
 
 	serial_init();
-	serial_enable();
+//	serial_enable();
 	console_init();
 	cpu_init();
 	mmu_init();
@@ -137,8 +137,8 @@ _start(void)
 	// reading the keyboard doesn't seem to work in graphics mode
 	// (maybe a bochs problem)
 	sBootOptions = check_for_boot_keys();
-//	if (sBootOptions & BOOT_OPTION_DEBUG_OUTPUT)
-//		serial_enable();
+	if (sBootOptions & BOOT_OPTION_DEBUG_OUTPUT)
+		serial_enable();
 
 	apm_init();
 	acpi_init();
