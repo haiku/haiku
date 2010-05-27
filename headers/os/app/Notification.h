0cf96104499a321c3767b7ccf1c7c853361a2459
Axel Dörfler <axeld@pinc-software.de>
2010-05-27 15:53:35 +0000

    * Fixed incorrect spacing and blank lines.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@36950 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/app/Notification.h
+++ headers/os/app/Notification.h
@@ -5,7 +5,13 @@
 #ifndef _NOTIFICATION_H
 #define _NOTIFICATION_H
 
+
 #include <Entry.h>
+
+
+class BBitmap;
+class BList;
+
 
 // notification types
 enum notification_type {
@@ -15,58 +21,57 @@ enum notification_type {
 	B_PROGRESS_NOTIFICATION
 };
 
-class BBitmap;
-class BList;
 
 class BNotification {
 public:
-						BNotification(notification_type type);
-						~BNotification();
+								BNotification(notification_type type);
+								~BNotification();
 
-	notification_type	Type() const;
+			notification_type	Type() const;
 
-	const char*			Application() const;
-	void				SetApplication(const char* app);
+			const char*			Application() const;
+			void				SetApplication(const char* app);
 
-	const char*			Title() const;
-	void				SetTitle(const char* title);
+			const char*			Title() const;
+			void				SetTitle(const char* title);
 
-	const char*			Content() const;
-	void				SetContent(const char* content);
+			const char*			Content() const;
+			void				SetContent(const char* content);
 
-	const char*			MessageID() const;
-	void				SetMessageID(const char* id);
+			const char*			MessageID() const;
+			void				SetMessageID(const char* id);
 
-	float				Progress() const;
-	void				SetProgress(float progress);
+			float				Progress() const;
+			void				SetProgress(float progress);
 
-	const char*			OnClickApp() const;
-	void				SetOnClickApp(const char* app);
+			const char*			OnClickApp() const;
+			void				SetOnClickApp(const char* app);
 
-	entry_ref*			OnClickFile() const;
-	void				SetOnClickFile(const entry_ref* file);
+			entry_ref*			OnClickFile() const;
+			void				SetOnClickFile(const entry_ref* file);
 
-	BList*				OnClickRefs() const;
-	void				AddOnClickRef(const entry_ref* ref);
+			BList*				OnClickRefs() const;
+			void				AddOnClickRef(const entry_ref* ref);
 
-	BList*				OnClickArgv() const;
-	void				AddOnClickArg(const char* arg);
+			BList*				OnClickArgv() const;
+			void				AddOnClickArg(const char* arg);
 
-	BBitmap*			Icon() const;
-	void				SetIcon(BBitmap* icon);
+			BBitmap*			Icon() const;
+			void				SetIcon(BBitmap* icon);
 
 private:
-	notification_type	fType;
-	char*				fAppName;
-	char*				fTitle;
-	char*				fContent;
-	char*				fID;
-	float				fProgress;
-	char*				fApp;
-	entry_ref*			fFile;
-	BList*				fRefs;
-	BList*				fArgv;
-	BBitmap*			fBitmap;
+			notification_type	fType;
+			char*				fAppName;
+			char*				fTitle;
+			char*				fContent;
+			char*				fID;
+			float				fProgress;
+			char*				fApp;
+			entry_ref*			fFile;
+			BList*				fRefs;
+			BList*				fArgv;
+			BBitmap*			fBitmap;
 };
+
 
 #endif	// _NOTIFICATION_H
