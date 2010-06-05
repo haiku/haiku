haiku/haiku

commit 19a0767e35d1d28d859122744ab2a68bdc334fe9

Quick and dirty work-around for broken interface.
git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@36999 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/add-ons/kernel/drivers/graphics/intel_extreme/intel_extreme.cpp
@@ -166,8 +166,12 @@ status_t
 intel_allocate_memory(intel_info &info, size_t size, size_t alignment,
 	uint32 flags, addr_t *_base, addr_t *_physicalBase)
 {
-	return gGART->allocate_memory(info.aperture, size, alignment,
-		flags, _base, _physicalBase);
+	// TODO: _physicalBase should be phys_addr_t!
+	phys_addr_t physicalBase;
+	status_t error = gGART->allocate_memory(info.aperture, size, alignment,
+		flags, _base, &physicalBase);
+	*_physicalBase = physicalBase;
+	return error;
 }
 
 
@@ -255,7 +259,7 @@ intel_extreme_init(intel_info &info)
 			dprintf("G4x mobile clock gating\n");
 		    dspclk_gate_val |= 1L << 18;
 		}
-		write32(info.registers + 0x6200, dspclk_gate_val)	;	
+		write32(info.registers + 0x6200, dspclk_gate_val)	;
 
 	} else {
 		dprintf("i965 quirk\n");

commit 5003f66a4c18cc3e86be170fdf604a45c02bf6df

The physical base pointer can be NULL since it's only allocated if certain flags are passed. Fixes regression introduced in r36999 and hopefully ticket #6137. Axel, please review.
git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@37016 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/add-ons/kernel/drivers/graphics/intel_extreme/intel_extreme.cpp
@@ -170,7 +170,8 @@ intel_allocate_memory(intel_info &info, size_t size, size_t alignment,
 	phys_addr_t physicalBase;
 	status_t error = gGART->allocate_memory(info.aperture, size, alignment,
 		flags, _base, &physicalBase);
-	*_physicalBase = physicalBase;
+	if (_physicalBase != NULL)
+		*_physicalBase = physicalBase;
 	return error;
 }
 