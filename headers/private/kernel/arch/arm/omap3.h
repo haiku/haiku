dae84707ac5d9278df29cf3787b788bb08e62085
François Revol <revol@free.fr>
2010-06-30 17:39:36 +0000

    Patch by "notzed" (Ticket #5380): Add support for setting video mode for OMAP3.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@37321 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/arch/arm/omap3.h
+++ headers/private/kernel/arch/arm/omap3.h
@@ -263,5 +263,19 @@
 #define I2C_ACTOA			(0x50)
 #define I2C_SBLOCK			(0x54)
 
+/* GPIO */
+#define GPIO1_BASE 0x48310000
+#define GPIO2_BASE 0x49050000
+#define GPIO3_BASE 0x49052000
+#define GPIO4_BASE 0x49054000
+#define GPIO5_BASE 0x49056000
+#define GPIO6_BASE 0x49058000
+
+/* (incomplete) */
+#define GPIO_OE 0x034
+#define GPIO_DATAIN 0x038
+#define GPIO_DATAOUT 0x03C
+#define GPIO_SETDATAOUT 0x094
+
 #endif
 
