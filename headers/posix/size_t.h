818786a83af8b2d275629472aa4497f77678fc49
Jérôme Duval <korli@users.berlios.de>
2010-07-03 16:39:29 +0000

    Define ssize_t in Haiku headers when building with GCC4 (see also #5719). This could imply a conflict with old gcc4 Haiku builds.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@37380 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/posix/size_t.h
+++ headers/posix/size_t.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008, Haiku Inc. All Rights Reserved.
+ * Copyright 2008-2010, Haiku Inc. All Rights Reserved.
  * Distributed under the terms of the MIT License.
  */
 #ifndef _SIZE_T_H_
@@ -7,7 +7,12 @@
 
 #include <stddef.h>
 
-/* TODO: ATM gcc's stddef.h defines ssize_t for BeOS.h. We should fix this.
- * typedef long signed int		ssize_t; */
+/* ATM we don't define ssize_t for GCC2 */
+#if __GNUC__ > 2 
+#ifndef __ssize_t__
+#define __ssize_t__
+typedef long signed int	ssize_t;
+#endif /* __ssize_t__ */
+#endif /* __GNUC__ > 2 */
 
 #endif /* _SIZE_T_H_ */
