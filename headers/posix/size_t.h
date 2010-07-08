816d81b51b8572a8b7d9151854474cb102ac6807
Axel Dörfler <axeld@pinc-software.de>
2010-07-08 06:52:16 +0000

    * Minor cleanup.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@37424 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/posix/size_t.h
+++ headers/posix/size_t.h
@@ -5,14 +5,15 @@
 #ifndef _SIZE_T_H_
 #define _SIZE_T_H_
 
+
 #include <stddef.h>
 
-/* ATM we don't define ssize_t for GCC2 */
-#if __GNUC__ > 2 
-#ifndef __ssize_t__
-#define __ssize_t__
-typedef long signed int	ssize_t;
-#endif /* __ssize_t__ */
-#endif /* __GNUC__ > 2 */
 
-#endif /* _SIZE_T_H_ */
+/* ATM we don't define ssize_t for GCC2 */
+#if __GNUC__ > 2 && !defined(__ssize_t__)
+#	define __ssize_t__
+typedef long signed int	ssize_t;
+#endif
+
+
+#endif	/* _SIZE_T_H_ */
