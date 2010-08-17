7640bbb6cd62422a0c68cb687e16030be186d5fe
Clemens Zeidler <clemens.zeidler@googlemail.com>
2010-08-17 21:35:16 +0000

    Fix coding style.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@38212 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/interface/WindowStack.h
+++ headers/private/interface/WindowStack.h
@@ -5,39 +5,41 @@
 #ifndef WINDOW_STACK_H
 #define WINDOW_STACK_H
 
+
 #include <Window.h>
 
 
-class BWindowStack
-{
+class BWindowStack {
 public:
-							BWindowStack(BWindow* window);
-							~BWindowStack();
+								BWindowStack(BWindow* window);
+								~BWindowStack();
 
-		status_t			InitCheck();
+		status_t				InitCheck();
 
-		status_t			AddWindow(BWindow* window);
-		status_t			AddWindow(BMessenger& window);
-		status_t			AddWindowAt(BWindow* window, int32 position);
-		status_t			AddWindowAt(BMessenger& window, int32 position);
+		status_t				AddWindow(BWindow* window);
+		status_t				AddWindow(BMessenger& window);
+		status_t				AddWindowAt(BWindow* window, int32 position);
+		status_t				AddWindowAt(BMessenger& window,
+									int32 position);
 
-		status_t			RemoveWindow(BWindow* window);
-		status_t			RemoveWindow(BMessenger& window);
-		status_t			RemoveWindowAt(int32 position,
-								BMessenger* window = NULL);
+		status_t				RemoveWindow(BWindow* window);
+		status_t				RemoveWindow(BMessenger& window);
+		status_t				RemoveWindowAt(int32 position,
+									BMessenger* window = NULL);
 
-		int32				CountWindows();
+		int32					CountWindows();
 
-		status_t			WindowAt(int32 position, BMessenger& messenger);
-		bool				HasWindow(BWindow* window);
-		bool				HasWindow(BMessenger& window);
+		status_t				WindowAt(int32 position,
+									BMessenger& messenger);
+		bool					HasWindow(BWindow* window);
+		bool					HasWindow(BMessenger& window);
 
 private:
-		status_t			_AttachMessenger(BMessenger& window);
-		status_t			_ReadMessenger(BMessenger& window);
-		status_t			_StartMessage(int32 what);
+		status_t				_AttachMessenger(BMessenger& window);
+		status_t				_ReadMessenger(BMessenger& window);
+		status_t				_StartMessage(int32 what);
 
-		BPrivate::PortLink*	fLink;
+		BPrivate::PortLink*		fLink;
 };
 
 
