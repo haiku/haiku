haiku/haiku

commit 1c9ed76e337c592e93710ee9b6691711ce439644

Fix CID 879. We leaked some stuff in case of error. Removed useless check.
git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@38247 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/kits/mail/MailChain.cpp
@@ -103,14 +103,20 @@ BMailChain::Load(BMessage* settings)
 			break;
 		}
 		
-		if (!filter_settings.AddItem(filter) || !filter_addons.AddItem(ref))
-			break;
+		if (!filter_settings.AddItem(filter)) {
+			delete filter;
+			delete ref;
+			return B_NO_MEMORY;
+		}
+
+		if (!filter_addons.AddItem(ref)) {
+			filter_settings.RemoveItem(filter);
+			delete filter;
+			delete ref;
+			return B_NO_MEMORY;
+		}
 	}
-	
-	if (filter_settings.CountItems() != settings_ct
-		||  filter_addons.CountItems() != addons_ct)
-		return B_NO_MEMORY;
-	
+
 	return B_OK;
 }
 

commit 2d05ca4e7a89921ba3417ec9e45bde8dfe94c682

Actually return B_NO_MEMORY in case of error (was a regression introduced in r38247. Small style fixes (more to come)
git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@38250 a95241bf-73f2-0310-859d-f6bbb57e9c96

headers/os/mail/MailSettings.h
@@ -40,16 +40,17 @@ class BMailChain;
 BMailChain* NewMailChain();
 BMailChain* GetMailChain(uint32 id);
 
-status_t GetOutboundMailChains(BList *list);
-status_t GetInboundMailChains(BList *list);
+status_t GetOutboundMailChains(BList* list);
+status_t GetInboundMailChains(BList* list);
+
 
 class BMailChain : public BArchivable {
-  public:
+public:
 	BMailChain(uint32 id);
 	BMailChain(BMessage*);
 	virtual ~BMailChain();
 	
-	virtual status_t Archive(BMessage*,bool) const;
+	virtual status_t Archive(BMessage*, bool) const;
 	static BArchivable* Instantiate(BMessage*);
 	
 	status_t Save(bigtime_t timeout = B_INFINITE_TIMEOUT);
@@ -62,32 +63,32 @@ class BMailChain : public BArchivable {
 	b_mail_chain_direction ChainDirection() const;
 	void SetChainDirection(b_mail_chain_direction);
 	
-	const char *Name() const;
+	const char* Name() const;
 	status_t SetName(const char*);
 	
-	BMessage *MetaData() const;
+	BMessage* MetaData() const;
 	
 	// "Filter" below refers to the settings message for a MailFilter
 	int32 CountFilters() const;
-	status_t GetFilter(int32 index, BMessage* out_settings, entry_ref *addon = NULL) const;
+	status_t GetFilter(int32 index, BMessage* out_settings, entry_ref* addon = NULL) const;
 	status_t SetFilter(int32 index, const BMessage&, const entry_ref&);
 	
 	status_t AddFilter(const BMessage&, const entry_ref&); // at end
 	status_t AddFilter(int32 index, const BMessage&, const entry_ref&);
 	status_t RemoveFilter(int32 index);
 	
-	void RunChain(BMailStatusWindow *window,
+	void RunChain(BMailStatusWindow* window,
 		bool async = true,
 		bool save_when_done = true,
 		bool delete_when_done = false);
 	
   private:
-	status_t Path(BPath *path) const;
+	status_t Path(BPath* path) const;
 	status_t Load(BMessage*);
 	
 	int32 fId;
 	char fName[B_FILE_NAME_LENGTH];
-	BMessage *fMetaData;
+	BMessage* fMetaData;
 	
 	status_t fStatus;
 
@@ -146,7 +147,7 @@ class BMailSettings {
 	void SetDefaultOutboundChainID(uint32 to);
 
   private:
-	BMessage data;
+	BMessage fData;
 	uint32 _reserved[4];
 };
 

src/kits/mail/MailChain.cpp
@@ -100,7 +100,7 @@ BMailChain::Load(BMessage* settings)
 			&& settings->FindRef("filter_addons", i, ref) < B_OK)) {
 			delete filter;
 			delete ref;
-			break;
+			return B_NO_MEMORY;
 		}
 		
 		if (!fFilterSettings.AddItem(filter)) {