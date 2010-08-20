6acb83bf616513770b3cd70c51929917967b7a8e
Clemens Zeidler <clemens.zeidler@googlemail.com>
2010-08-20 04:34:18 +0000

    Fix coding style.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@38286 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/interface/WindowStack.h
+++ headers/private/interface/WindowStack.h
@@ -14,31 +14,31 @@ public:
 								BWindowStack(BWindow* window);
 								~BWindowStack();
 
-		status_t				AddWindow(const BWindow* window);
-		status_t				AddWindow(const BMessenger& window);
-		status_t				AddWindowAt(const BWindow* window,
+			status_t			AddWindow(const BWindow* window);
+			status_t			AddWindow(const BMessenger& window);
+			status_t			AddWindowAt(const BWindow* window,
 									int32 position);
-		status_t				AddWindowAt(const BMessenger& window,
+			status_t			AddWindowAt(const BMessenger& window,
 									int32 position);
 
-		status_t				RemoveWindow(const BWindow* window);
-		status_t				RemoveWindow(const BMessenger& window);
-		status_t				RemoveWindowAt(int32 position,
+			status_t			RemoveWindow(const BWindow* window);
+			status_t			RemoveWindow(const BMessenger& window);
+			status_t			RemoveWindowAt(int32 position,
 									BMessenger* window = NULL);
 
-		int32					CountWindows();
+			int32				CountWindows();
 
-		status_t				WindowAt(int32 position,
-									BMessenger& messenger);
-		bool					HasWindow(const BWindow* window);
-		bool					HasWindow(const BMessenger& window);
+			status_t			WindowAt(int32 position,
+								BMessenger& messenger);
+			bool				HasWindow(const BWindow* window);
+			bool				HasWindow(const BMessenger& window);
 
 private:
-		status_t				_AttachMessenger(const BMessenger& window);
-		status_t				_ReadMessenger(BMessenger& window);
-		status_t				_StartMessage(int32 what);
+			status_t			_AttachMessenger(const BMessenger& window);
+			status_t			_ReadMessenger(BMessenger& window);
+			status_t			_StartMessage(int32 what);
 
-		BPrivate::PortLink*		fLink;
+			BPrivate::PortLink*	fLink;
 };
 
 
