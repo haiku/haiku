0dfe97ade17f42901a3c669b415fe0ead27b5963
Andreas Färber <andreas.faerber@web.de>
2010-08-21 17:42:28 +0000

    boot_loader_openfirmware: Preparations for frame buffer
    Initialize the frame buffer as not enabled. Add checks for this condition
    and for the debug boot option to the video functions.
    
    Code is adapted from bios_ia32 platform. Part of ticket #6105.
    
    
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@38299 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/boot/platform/openfirmware/video.cpp
+++ src/system/boot/platform/openfirmware/video.cpp
@@ -1,15 +1,21 @@
 /*
  * Copyright 2004, Axel Dörfler, axeld@pinc-software.de.
+ * Copyright 2010 Andreas Färber <andreas.faerber@web.de>
  * All rights reserved. Distributed under the terms of the MIT License.
  */
 
 
 #include <boot/platform.h>
+#include <boot/stage2.h>
 
 
 extern "C" void
 platform_switch_to_logo(void)
 {
+	// in debug mode, we'll never show the logo
+	if ((platform_boot_options() & BOOT_OPTION_DEBUG_OUTPUT) != 0)
+		return;
+
 	// ToDo: implement me
 }
 
@@ -17,13 +23,21 @@ platform_switch_to_logo(void)
 extern "C" void
 platform_switch_to_text_mode(void)
 {
+	// nothing to do if we're in text mode
+	if (!gKernelArgs.frame_buffer.enabled)
+		return;
+
 	// ToDo: implement me
+
+	gKernelArgs.frame_buffer.enabled = false;
 }
 
 
 extern "C" status_t
 platform_init_video(void)
 {
+	gKernelArgs.frame_buffer.enabled = false;
+
 	// ToDo: implement me
 	return B_OK;
 }
