b3ac89d6a1ebca9e00e4947e993fc7aff5f3523b
Philippe Houdoin <philippe.houdoin@gmail.com>
2010-09-07 08:04:00 +0000

    Fix application signature.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@38558 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/apps/3dmov/Application.cpp
+++ src/apps/3dmov/Application.cpp
@@ -2,7 +2,7 @@
 	AUTHORS:		Zenja Solaja
 	COPYRIGHT:		2009 Haiku Inc
 	DESCRIPTION:	Haiku version of the famous BeInc demo 3Dmov
-					
+
 					There is an issue in Haiku when creating a 2nd BGLView which doesn't exist in Zeta
 					(see MainWindow.cpp for details).  Until that issue is resolved,
 					I've allowed specifying of shape type (0-2) as a command line argument.
@@ -19,18 +19,18 @@ class MainApp : public BApplication
 {
 public:
 				MainApp(MainWindow::SHAPE shape);
-	
+
 private:
 	MainWindow	*fWindow;
 };
-	
+
 /*	FUNCTION:		MainApp :: MainApp
 	ARGS:			none
 	RETURN:			n/a
 	DESCRIPTION:	Application constructor
 */
 MainApp :: MainApp(MainWindow::SHAPE shape)
-	: BApplication("application/x_vnd.Haiku")
+	: BApplication("application/x-vnd.Haiku-3DMov")
 {
 	BRect frame(50, 50, 50+400, 50+300);
 	fWindow = new MainWindow(frame, shape);
@@ -56,7 +56,7 @@ int main(int argc, char **argv)
 		if (value >= 0 && value < MainWindow::NUMBER_OF_SHAPES)
 			shape = (MainWindow::SHAPE) value;
 	}
-	
+
 	MainApp *app = new MainApp(shape);
 	app->Run();
 	delete app;
