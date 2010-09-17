4f9892e0f82f8cc84a39d40df6956c04f6521a31
Rene Gollent <anevilyak@gmail.com>
2010-09-17 18:21:54 +0000

    Build fix.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@38692 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/decorators/SATDecorator/StackAndTile.h
+++ src/add-ons/decorators/SATDecorator/StackAndTile.h
@@ -73,7 +73,7 @@ public:
 
 	virtual void				WindowTabLocationChanged(Window* window,
 									float location);
-	virtual void				SizeLimitsChanged(Window* window,
+	virtual void				SizeLimitChanged(Window* window,
 									int32 minWidth, int32 maxWidth,
 									int32 minHeight, int32 maxHeight);
 
