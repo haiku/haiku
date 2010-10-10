91d95c16eb5beaf9a06be4d1785e8501561326d5
François Revol <revol@free.fr>
2010-10-10 18:16:16 +0000

    Clone atari platform args headers. This gets the Amiga loader to link.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@38937 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ headers/private/kernel/boot/platform/amiga_m68k/platform_stage2_args.h
@@ -0,0 +1,16 @@
+/*
+** Copyright 2003, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
+** Distributed under the terms of the OpenBeOS License.
+*/
+#ifndef KERNEL_BOOT_PLATFORM_AMIGA_M68K_STAGE2_ARGS_H
+#define KERNEL_BOOT_PLATFORM_AMIGA_M68K_STAGE2_ARGS_H
+
+#ifndef KERNEL_BOOT_STAGE2_ARGS_H
+#	error This file is included from <boot/stage2_args.h> only
+#endif
+
+struct platform_stage2_args {
+	int dummy;
+};
+
+#endif	/* KERNEL_BOOT_PLATFORM_AMIGA_M68K_STAGE2_ARGS_H */
