a009356a78f3f517b9c31f59b4e3d46a2061073c
Philippe Houdoin <philippe.houdoin@gmail.com>
2010-10-16 19:21:56 +0000

    Update the comment after the timeout change in r38982. Spotted by axeld, thanks!
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@38985 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/kernel/bus_managers/usb/Pipe.cpp
+++ src/add-ons/kernel/bus_managers/usb/Pipe.cpp
@@ -338,7 +338,7 @@ ControlPipe::SendRequest(uint8 requestType, uint8 request, uint16 value,
 	}
 
 	// The sem will be released unconditionally in the callback after the
-	// result data was filled in. Use a 1 second timeout for control transfers.
+	// result data was filled in. Use a 2 seconds timeout for control transfers.
 	if (acquire_sem_etc(fNotifySem, 1, B_RELATIVE_TIMEOUT, 2000000) < B_OK) {
 		TRACE_ERROR("timeout waiting for queued request to complete\n");
 
