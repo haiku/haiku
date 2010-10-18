1f0b0045c23624ad5f6ddb8f2fdabdb7d669e535
Rene Gollent <anevilyak@gmail.com>
2010-10-18 20:26:33 +0000

    Fix build.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@39007 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/kernel/fs/rootfs.cpp
+++ src/system/kernel/fs/rootfs.cpp
@@ -1023,6 +1023,13 @@ rootfs_read_stat(fs_volume* _volume, fs_vnode* _v, struct stat* stat)
 
 
 static status_t
+rootfs_preallocate(fs_volume*, fs_vnode*, off_t, off_t)
+{
+	return B_NOT_SUPPORTED;
+}
+
+
+static status_t
 rootfs_write_stat(fs_volume* _volume, fs_vnode* _vnode, const struct stat* stat,
 	uint32 statMask)
 {
@@ -1174,6 +1181,7 @@ fs_vnode_ops sVnodeOps = {
 	NULL,	// fs_access()
 	&rootfs_read_stat,
 	&rootfs_write_stat,
+	&rootfs_preallocate,
 
 	/* file */
 	&rootfs_create,
