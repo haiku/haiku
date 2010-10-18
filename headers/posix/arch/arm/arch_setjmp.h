a85c91d6f0f5845a2269b2431f70d55c9d7e98b6
Ithamar R. Adema <ithamar.adema@team-embedded.nl>
2010-10-18 00:50:59 +0000

    * Fix setjmp/jmpbuf_t size so we can finally get to the KDL prompt
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@38993 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/posix/arch/arm/arch_setjmp.h
+++ headers/posix/arch/arm/arch_setjmp.h
@@ -5,8 +5,7 @@
 #ifndef _ARCH_SETJMP_H_
 #define _ARCH_SETJMP_H_
 
-#define _SETJMP_BUF_SZ (7+6+2+8*((96/8)/4))
+#define _SETJMP_BUF_SZ ((15+2)*4)
 typedef int __jmp_buf[_SETJMP_BUF_SZ];
-#warning ARM: fix jmpbuf size
 
 #endif	/* _ARCH_SETJMP_H_ */
