2609a6bc98f1d890caa6a3370d5aed4332c11ac2
Ithamar R. Adema <ithamar.adema@team-embedded.nl>
2010-10-24 22:06:22 +0000

    * Fix header guard.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@39133 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/kernel/arch/arm/arch_vm_translation_map.h
+++ headers/private/kernel/arch/arm/arch_vm_translation_map.h
@@ -2,25 +2,23 @@
 ** Copyright 2003, Axel Dörfler, axeld@pinc-software.de. All rights reserved.
 ** Distributed under the terms of the OpenBeOS License.
 */
-#ifndef _KERNEL_ARCH_M68K_VM_TRANSLATION_MAP_H
-#define _KERNEL_ARCH_M68K_VM_TRANSLATION_MAP_H
+#ifndef _KERNEL_ARCH_ARM_VM_TRANSLATION_MAP_H
+#define _KERNEL_ARCH_ARM_VM_TRANSLATION_MAP_H
 
 #include <arch/vm_translation_map.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-/*
-void m68k_translation_map_change_asid(vm_translation_map *map);
 
-status_t m68k_map_address_range(addr_t virtualAddress, addr_t physicalAddress,
+status_t arm_map_address_range(addr_t virtualAddress, addr_t physicalAddress,
 	size_t size);
-void m68k_unmap_address_range(addr_t virtualAddress, size_t size);
-status_t m68k_remap_address_range(addr_t *virtualAddress, size_t size,
+void arm_unmap_address_range(addr_t virtualAddress, size_t size);
+status_t arm_remap_address_range(addr_t *virtualAddress, size_t size,
 	bool unmap);
-*/
+
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* _KERNEL_ARCH_M68K_VM_TRANSLATION_MAP_H */
+#endif /* _KERNEL_ARCH_ARM_VM_TRANSLATION_MAP_H */
