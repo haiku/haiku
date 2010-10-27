eb3504793b7e806576fc9fe14d74328798729c91
Stephan Aßmus <superstippi@gmx.de>
2010-10-27 14:16:19 +0000

    Added the new headers to the NetworkKit.h all-in-one header.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@39162 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/NetworkKit.h
+++ headers/os/NetworkKit.h
@@ -5,5 +5,9 @@
 #include <sys/socket.h>
 #include <NetAddress.h>
 #include <NetBuffer.h>
+#include <NetworkCookie.h>
+#include <NetworkCookieJar.h>
 #include <NetEndpoint.h>
 #include <NetDebug.h>
+#include <Url.h>
+#include <UrlContext.h>
