0d16ad4437b8279a504c4d5200397b928c05214e
Jérôme Duval <korli@users.berlios.de>
2010-11-01 20:52:29 +0000

    style fix (pointed out by Axel)
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@39261 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/kernel/generic/scsi_periph/block.cpp
+++ src/add-ons/kernel/generic/scsi_periph/block.cpp
@@ -59,8 +59,8 @@ periph_check_capacity(scsi_periph_device_info *device, scsi_ccb *request)
 		if (capacity == UINT_MAX) {
 			RELEASE_BEN(&device->mutex);
 			
-			scsi_cmd_read_capacity_long *cmd =
-				(scsi_cmd_read_capacity_long *)request->cdb;
+			scsi_cmd_read_capacity_long *cmd
+				= (scsi_cmd_read_capacity_long *)request->cdb;
 			
 			scsi_res_read_capacity_long capacityLongResult;
 			request->data = (uint8*)&capacityLongResult;
