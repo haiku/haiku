ddcb5a2cdaf84f49f57c6450dc7ca186547fb45a
Axel Dörfler <axeld@pinc-software.de>
2010-11-03 20:00:49 +0000

    * Reverted Rene's fix for the broken build - it's no longer necessary. Thank   you for cleaning up behind me! * Fixed bug #6771 by adding the shift key to the page shortcuts.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@39282 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/apps/showimage/ShowImageWindow.cpp
+++ src/apps/showimage/ShowImageWindow.cpp
@@ -126,10 +126,6 @@ bs_printf(BString* string, const char* format, ...)
 //	#pragma mark -- ShowImageWindow
 
 
-#undef B_TRANSLATE_CONTEXT
-#define B_TRANSLATE_CONTEXT "Menus"
-
-
 ShowImageWindow::ShowImageWindow(const entry_ref* ref,
 	const BMessenger& trackerMessenger)
 	:
@@ -205,7 +201,7 @@ ShowImageWindow::ShowImageWindow(const entry_ref* ref,
 	}
 
 	// add View menu here so it can access ShowImageView methods
-	BMenu* menu = new BMenu(B_TRANSLATE("View"));
+	BMenu* menu = new BMenu(B_TRANSLATE_WITH_CONTEXT("View", "Menus"));
 	_BuildViewMenu(menu, false);
 	fBar->AddItem(menu);
 
@@ -636,7 +632,7 @@ ShowImageWindow::MessageReceived(BMessage* message)
 					strCaption << i;
 
 					BMenuItem* item = new BMenuItem(strCaption.String(), goTo,
-						shortcut);
+						B_SHIFT_KEY, shortcut);
 					if (currentPage == i)
 						item->SetMarked(true);
 					fGoToPageMenu->AddItem(item);
@@ -895,9 +891,10 @@ ShowImageWindow::_LoadError(const entry_ref& ref)
 {
 	// TODO: give a better error message!
 	BAlert* alert = new BAlert(B_TRANSLATE("ShowImage"),
-		B_TRANSLATE("Could not load image! Either the "
-			"file or an image translator for it does not exist."),
-		B_TRANSLATE("OK"), NULL, NULL,
+		B_TRANSLATE_WITH_CONTEXT("Could not load image! Either the "
+			"file or an image translator for it does not exist.",
+			"LoadAlerts"),
+		B_TRANSLATE_WITH_CONTEXT("OK", "Alerts"), NULL, NULL,
 		B_WIDTH_AS_USUAL, B_INFO_ALERT);
 	alert->Go();
 }
