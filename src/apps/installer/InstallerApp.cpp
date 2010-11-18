a3e6f7e1df1e7c4fe0c9f5f2b24ba96964af8c4f
Joachim Seemer <humdingerb@googlemail.com>
2010-11-18 08:06:31 +0000

    Split the massive EULA text into paragraphs to ease translations. Does the HTA site update automatically?
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@39474 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/apps/installer/InstallerApp.cpp
+++ src/apps/installer/InstallerApp.cpp
@@ -99,108 +99,124 @@ InstallerApp::AboutRequested()
 void
 InstallerApp::ReadyToRun()
 {
-	const char* infoText = B_TRANSLATE(
-		"Welcome to the Haiku Installer!\n\n"
-
-		"IMPORTANT INFORMATION BEFORE INSTALLING HAIKU\n\n"
-
+	BString infoText;
+	infoText << B_TRANSLATE(
+		"Welcome to the Haiku Installer!\n\n");
+	infoText << B_TRANSLATE(
+		"IMPORTANT INFORMATION BEFORE INSTALLING HAIKU\n\n");
+	infoText << B_TRANSLATE(
 		"This is alpha-quality software! It means there is a high risk of "
 		"losing important data. Make frequent backups! You have been "
-		"warned.\n\n\n"
-
+		"warned.\n\n\n");
+	infoText << B_TRANSLATE(
 		"1)   If you are installing Haiku onto real hardware (not inside an "
 		"emulator) it is recommended that you have already prepared a hard "
 		"disk partition. The Installer and the DriveSetup tool offer to "
 		"initialize existing partitions with the Haiku native file system, "
 		"but the options to change the actual partition layout may not have "
 		"been tested on a sufficiently great variety of computer "
-		"configurations so we do not recommend using it.\n"
+		"configurations so we do not recommend using it.\n");
+	infoText << B_TRANSLATE(
 		"If you have not created a partition yet, simply reboot, create the "
 		"partition using whatever tool you feel most comfortable with, and "
 		"reboot into Haiku to continue with the installation. You could for "
 		"example use the GParted Live-CD, it can also resize existing "
-		"partitions to make room.\n\n\n"
-
+		"partitions to make room.\n\n\n");
+	infoText << B_TRANSLATE(
 		"2)   The Installer will make the Haiku partition itself bootable, "
 		"but takes no steps to integrate Haiku into an existing boot menu. "
 		"If you have GRUB already installed, you can add Haiku to its boot "
 		"menu. Depending on what version of GRUB you use, this is done "
-		"differently.\n\n\n"
-
-		"2.1) GRUB 1\n"
+		"differently.\n\n\n");
+	infoText << B_TRANSLATE(
+		"2.1) GRUB 1\n");
+	infoText << B_TRANSLATE(
 		"Configure your /boot/grub/menu.lst by launching your favorite "
-		"editor from a Terminal like this:\n\n"
-
-		"\tsudo <your favorite text editor> /boot/grub/menu.lst\n\n"
-
+		"editor from a Terminal like this:\n\n");
+	infoText << B_TRANSLATE(
+		"\tsudo <your favorite text editor> /boot/grub/menu.lst\n\n");
+	infoText << B_TRANSLATE(
 		"You'll note that GRUB uses a different naming strategy for hard "
-		"drives than Linux.\n\n"
-
-		"With GRUB it's: (hdN,n)\n\n"
-
-		"All hard disks start with \"hd\".\n"
-		"\"N\" is the hard disk number, starting with \"0\".\n"
-		"\"n\" is the partition number, also starting with \"0\".\n"
+		"drives than Linux.\n\n");
+	infoText << B_TRANSLATE(
+		"With GRUB it's: (hdN,n)\n\n");
+	infoText << B_TRANSLATE(
+		"All hard disks start with \"hd\".\n");
+	infoText << B_TRANSLATE(
+		"\"N\" is the hard disk number, starting with \"0\".\n");
+	infoText << B_TRANSLATE(
+		"\"n\" is the partition number, also starting with \"0\".\n");
+	infoText << B_TRANSLATE(
 		"The first logical partition always has the number \"4\", regardless "
-		"of the number of primary partitions.\n\n"
-
+		"of the number of primary partitions.\n\n");
+	infoText << B_TRANSLATE(
 		"So behind the other menu entries towards the bottom of the file, add "
-		"something similar to these lines:\n\n"
-
-		"\t# Haiku on /dev/sda7\n"
-		"\ttitle\t\t\t\tHaiku\n"
-		"\trootnoverify\t\t(hd0,6)\n"
-		"\tchainloader\t\t+1\n\n"
-
-		"You can see the correct partition in GParted for example.\n\n\n"
-
-		"2.2) GRUB 2\n"
+		"something similar to these lines:\n\n");
+	infoText << B_TRANSLATE(
+		"\t# Haiku on /dev/sda7\n");
+	infoText << B_TRANSLATE(
+		"\ttitle\t\t\t\tHaiku\n");
+	infoText << B_TRANSLATE(
+		"\trootnoverify\t\t(hd0,6)\n");
+	infoText << B_TRANSLATE(
+		"\tchainloader\t\t+1\n\n");
+	infoText << B_TRANSLATE(
+		"You can see the correct partition in GParted for example.\n\n\n");
+	infoText << B_TRANSLATE(
+		"2.2) GRUB 2\n");
+	infoText << B_TRANSLATE(
 		"Newer versions of GRUB use an extra configuration file to add "
 		"custom entries to the boot menu. To add them to the top, you have "
 		"to create/edit a file by launching your favorite editor from a "
-		"Terminal like this:\n\n"
-
-		"\tsudo <your favorite text editor> /etc/grub.d/40_custom\n\n"
-
+		"Terminal like this:\n\n");
+	infoText << B_TRANSLATE(
+		"\tsudo <your favorite text editor> /etc/grub.d/40_custom\n\n");
+	infoText << B_TRANSLATE(
 		"NOTE: While the naming strategy for hard disks is still as described "
-		"under 2.1) the naming scheme for partitions has changed.\n\n"
-		
-		"GRUB's naming scheme is still: (hdN,n)\n\n"
-
-		"All hard disks start with \"hd\".\n"
-		"\"N\" is the hard disk number, starting with \"0\".\n"
-		"\"n\" is the partition number, which for GRUB 2 starts with \"1\"\n"
+		"under 2.1) the naming scheme for partitions has changed.\n\n");
+	infoText << B_TRANSLATE(		
+		"GRUB's naming scheme is still: (hdN,n)\n\n");
+	infoText << B_TRANSLATE(
+		"All hard disks start with \"hd\".\n");
+	infoText << B_TRANSLATE(
+		"\"N\" is the hard disk number, starting with \"0\".\n");
+	infoText << B_TRANSLATE(
+		"\"n\" is the partition number, which for GRUB 2 starts with \"1\"\n");
+	infoText << B_TRANSLATE(
 		"With GRUB 2 the first logical partition always has the number \"5\", "
-		"regardless of the number of primary partitions.\n\n"
-
+		"regardless of the number of primary partitions.\n\n");
+	infoText << B_TRANSLATE(
 		"So below the heading that must not be edited, add something similar "
-		"to these lines:\n\n"
-
-		"\t# Haiku on /dev/sda7\n"
-		"\tmenuentry \"Haiku Alpha\" {\n"
-		"\t\tset root=(hd0,7)\n"
-		"\t\tchainloader +1\n"
-		"\t}\n\n"
-
+		"to these lines:\n\n");
+	infoText << B_TRANSLATE(
+		"\t# Haiku on /dev/sda7\n");
+	infoText << B_TRANSLATE(
+		"\tmenuentry \"Haiku Alpha\" {\n");
+	infoText << B_TRANSLATE(
+		"\t\tset root=(hd0,7)\n");
+	infoText << B_TRANSLATE(
+		"\t\tchainloader +1\n");
+	infoText << B_TRANSLATE(
+		"\t}\n\n");
+	infoText << B_TRANSLATE(
 		"Additionally you have to edit another file to actually display the "
-		"boot menu:\n\n"
-
-		"\tsudo <your favorite text editor> /etc/default/grub\n\n"
-
+		"boot menu:\n\n");
+	infoText << B_TRANSLATE(
+		"\tsudo <your favorite text editor> /etc/default/grub\n\n");
+	infoText << B_TRANSLATE(
 		"Here you have to comment out the line \"GRUB_HIDDEN_TIMEOUT=0\" by "
 		"putting a \"#\" in front of it in order to actually display the "
-		"boot menu.\n\n"
-
-		"Finally, you have to update the boot menu by entering:\n\n"
-
-		"\tsudo update-grub\n\n\n"
-
+		"boot menu.\n\n");
+	infoText << B_TRANSLATE(
+		"Finally, you have to update the boot menu by entering:\n\n");
+	infoText << B_TRANSLATE(
+		"\tsudo update-grub\n\n\n");
+	infoText << B_TRANSLATE(
 		"3)   When you successfully boot into Haiku for the first time, make "
 		"sure to read our \"Welcome\" documentation, there is a link on the "
-		"Desktop.\n\n"
-
-		"Have fun and thanks a lot for trying out Haiku! We hope you like it!"
-	);
+		"Desktop.\n\n");
+	infoText << B_TRANSLATE(
+		"Have fun and thanks a lot for trying out Haiku! We hope you like it!");
 
 #if 1
 	// Show the EULA first.
