haiku/haiku

commit 89d652d5e0defd9d095c778709cef82f5f10c357

* Decorator: Replaced the MouseAction() method with a new RegionAt().   RegionAt() just gets a point and returns which region was hit. This is in   order to move behavioral code to [Default]WindowBehaviour. I'm not happy   with this solution either, but to do it properly one would have to break the   Decorator interface into separate look and feel interfaces and reorganize the   interaction with WindowBehaviour. A task for the so-inclined reader. :-) * Adjusted the Decorators implementations, but really tested only the default   one. * DefaultWindowBehaviour:   - Replaced _ActionFor() method by a _RegionFor() which interprets the region     returned by Decorator::RegionAt() and converts it to a "functional" region,     i.e. combines cases we handle the same way.   - MouseDown():     - Handle the click region cases more in detail, disentangling the mouse       button cases. With the following effects:       - The middle mouse button has no effect anymore.       - Left and right mouse buttons no longer share common behavior. A right         click on a decorator button will send the window to the back.     - The window key window management modifier combo does now have precedence,       i.e. Cmd-Ctrl-click on the decorator buttons will have the same effect as       clicking anywhere in the window.     - When modifiers change between the clicks, reset the click count. Prevents       a standard click in the window followed by a Cmd-Ctrl-click from being       recognized as a double-click.   - Mouse*(): Introduced a fMinimizeCheckOnMouseUp which works similar to     fActivateOnMouseUp, just for double-clicks. The decision whether a     double-click minimizes the window is postponed until releasing the mouse     button. After moving the mouse sufficiently far or waiting half a second     without moving the mouse the window will no longer be minimized. Fixes     #6868.   - MouseUp(): Moved the primary mouse button check without the     "decorator != NULL" block. I suppose this fixes issues with the Cmd-Ctrl     actions and decoratorless windows (if those actually exist).

src/servers/app/DefaultWindowBehaviour.cpp
@@ -9,10 +9,13 @@
  *		Axel Dörfler <axeld@pinc-software.de>
  *		Brecht Machiels <brecht@mos6581.org>
  *		Clemens Zeidler <haiku@clemens-zeidler.de>
+ *		Ingo Weinhold <ingo_weinhold@gmx.de>
  */
 
 
 #include "DefaultWindowBehaviour.h"
+
+#include <WindowPrivate.h>
 
 #include "Desktop.h"
 #include "DrawingEngine.h"
@@ -27,6 +30,7 @@
 #endif
 
 
+// The span between mouse down
 static const bigtime_t kWindowActivationTimeout = 500000LL;
 
 
@@ -39,11 +43,14 @@ DefaultWindowBehaviour::DefaultWindowBehaviour(Window* window)
 	fIsZooming(false),
 	fIsSlidingTab(false),
 	fActivateOnMouseUp(false),
+	fMinimizeCheckOnMouseUp(false),
 
 	fLastMousePosition(0.0f, 0.0f),
 	fMouseMoveDistance(0.0f),
 	fLastMoveTime(0),
-	fLastSnapTime(0)
+	fLastSnapTime(0),
+	fLastModifiers(0),
+	fResetClickCount(0)
 {
 	fDesktop = fWindow->Desktop();
 }
@@ -65,33 +72,106 @@ DefaultWindowBehaviour::MouseDown(BMessage* message, BPoint where)
 
 	int32 modifiers = message->FindInt32("modifiers");
 	bool windowModifier = _IsWindowModifier(modifiers);
+
+	// Get the click count and reset it, if the modifiers changed in the
+	// meantime.
+	// TODO: This should be done in a better place (e.g. the input server). It
+	// should also reset clicks after mouse movement (which we don't do here
+	// either -- though that's probably acceptable).
+	int32 clickCount = message->FindInt32("clicks");
+	if (clickCount <= 1) {
+		fResetClickCount = 0;
+	} else if (modifiers != fLastModifiers
+		|| clickCount - fResetClickCount < 1) {
+		fResetClickCount = clickCount - 1;
+		clickCount = 1;
+	} else
+		clickCount -= fResetClickCount;
+	fLastModifiers = modifiers;
+
+	Region hitRegion = REGION_NONE;
 	click_type action = CLICK_NONE;
 
 	if (windowModifier || inBorderRegion) {
-		// Click on the window border or we have the window modifier keys held
-		int32 buttons = message->FindInt32("buttons");
+		// click on the window decorator or we have the window modifier keys
+		// held
 
-		if (inBorderRegion)
-			action = _ActionFor(message, buttons, modifiers);
-		else {
-			if ((buttons & B_SECONDARY_MOUSE_BUTTON) != 0)
-				action = CLICK_MOVE_TO_BACK;
-			else if ((fWindow->Flags() & B_NOT_MINIMIZABLE) == 0
-				&& message->FindInt32("clicks") == 2)
-				action = CLICK_MINIMIZE;
-			else if ((fWindow->Flags() & B_NOT_MOVABLE) == 0
-				&& decorator != NULL)
-				action = CLICK_DRAG;
-			else {
-				// pass click on to the application
-				windowModifier = false;
-			}
+		// get the functional hit region
+		if (windowModifier) {
+			// click with window modifier keys -- let the whole window behave
+			// like the border
+			hitRegion = REGION_BORDER;
+		} else {
+			// click on the decorator -- get the exact region
+			hitRegion = _RegionFor(message);
+		}
+
+		// translate the region into an action
+		int32 buttons = message->FindInt32("buttons");
+		bool leftButton = (buttons & B_PRIMARY_MOUSE_BUTTON) != 0;
+		bool rightButton = (buttons & B_SECONDARY_MOUSE_BUTTON) != 0;
+		uint32 flags = fWindow->Flags();
+
+		switch (hitRegion) {
+			case REGION_NONE:
+				break;
+
+			case REGION_TAB:
+				// tab sliding in any case if either shift key is held down
+				// except sliding up-down by moving mouse left-right would look
+				// strange
+				if (leftButton && (modifiers & B_SHIFT_KEY) != 0
+					&& fWindow->Look() != kLeftTitledWindowLook) {
+					action = CLICK_SLIDE_TAB;
+					break;
+				}
+				// otherwise fall through -- same handling as for the border...
+
+			case REGION_BORDER:
+				if (leftButton)
+					action = CLICK_DRAG;
+				else if (rightButton)
+					action = CLICK_MOVE_TO_BACK;
+				break;
+
+			case REGION_CLOSE_BUTTON:
+				if (leftButton) {
+					action = (flags & B_NOT_CLOSABLE) == 0
+						? CLICK_CLOSE : CLICK_DRAG;
+				} else if (rightButton)
+					action = CLICK_MOVE_TO_BACK;
+				break;
+
+			case REGION_ZOOM_BUTTON:
+				if (leftButton) {
+					action = (flags & B_NOT_ZOOMABLE) == 0
+						? CLICK_ZOOM : CLICK_DRAG;
+				} else if (rightButton)
+					action = CLICK_MOVE_TO_BACK;
+				break;
+
+			case REGION_MINIMIZE_BUTTON:
+				if (leftButton) {
+					action = (flags & B_NOT_MINIMIZABLE) == 0
+						? CLICK_MINIMIZE : CLICK_DRAG;
+				} else if (rightButton)
+					action = CLICK_MOVE_TO_BACK;
+				break;
+
+			case REGION_RESIZE_CORNER:
+				if (leftButton) {
+					action = (flags & B_NOT_RESIZABLE) == 0
+						? CLICK_RESIZE : CLICK_DRAG;
+				} else if (rightButton)
+					action = CLICK_MOVE_TO_BACK;
+				break;
 		}
 	}
 
-	if (!windowModifier && !inBorderRegion) {
-		// This is a click inside the window's contents
-		return false;
+	if (action == CLICK_NONE) {
+		// No action -- if this is a click inside the window's contents,
+		// let it be forwarded to the window.
+		return inBorderRegion;
 	}
 
 	DesktopSettings desktopSettings(fDesktop);
@@ -165,9 +245,6 @@ DefaultWindowBehaviour::MouseDown(BMessage* message, BPoint where)
 		engine->UnlockParallelAccess();
 
 		fWindow->RegionPool()->Recycle(visibleBorder);
-	} else if (fIsMinimizing) {
-		fWindow->ServerWindow()->NotifyQuitRequested();
-		return true;
 	}
 
 	if (action == CLICK_MOVE_TO_BACK) {
@@ -184,12 +261,15 @@ DefaultWindowBehaviour::MouseDown(BMessage* message, BPoint where)
 		else {
 			fDesktop->SetFocusWindow(fWindow);
 
-			if (action == CLICK_DRAG || action == CLICK_RESIZE) {
+			if (action == CLICK_DRAG || action == CLICK_RESIZE)
 				fActivateOnMouseUp = true;
-				fMouseMoveDistance = 0.0f;
-				fLastMoveTime = system_time();
-			}
 		}
+
+		if (fIsDragging && clickCount == 2)
+			fMinimizeCheckOnMouseUp = true;
+
+		fMouseMoveDistance = 0.0f;
+		fLastMoveTime = system_time();
 	}
 
 	return true;
@@ -201,11 +281,9 @@ DefaultWindowBehaviour::MouseUp(BMessage* message, BPoint where)
 {
 	Decorator* decorator = fWindow->Decorator();
 
-	if (decorator != NULL) {
-		int32 modifiers = message->FindInt32("modifiers");
-		int32 buttons = message->FindInt32("buttons");
-		click_type action = _ActionFor(message, buttons, modifiers);
+	int32 buttons = message->FindInt32("buttons");
 
+	if (decorator != NULL) {
 		// redraw decorator
 		BRegion* visibleBorder = fWindow->RegionPool()->GetRegion();
 		fWindow->GetBorderRegion(visibleBorder);
@@ -218,44 +296,54 @@ DefaultWindowBehaviour::MouseUp(BMessage* message, BPoint where)
 		if (fIsZooming) {
 			fIsZooming = false;
 			decorator->SetZoom(false);
-			if (action == CLICK_ZOOM)
+			if (_RegionFor(message) == REGION_ZOOM_BUTTON)
 				fWindow->ServerWindow()->NotifyZoom();
 		}
 		if (fIsClosing) {
 			fIsClosing = false;
 			decorator->SetClose(false);
-			if (action == CLICK_CLOSE)
+			if (_RegionFor(message) == REGION_CLOSE_BUTTON)
 				fWindow->ServerWindow()->NotifyQuitRequested();
 		}
 		if (fIsMinimizing) {
 			fIsMinimizing = false;
 			decorator->SetMinimize(false);
-			if (action == CLICK_MINIMIZE || _IsWindowModifier(modifiers))
+			if (_RegionFor(message) == REGION_MINIMIZE_BUTTON)
 				fWindow->ServerWindow()->NotifyMinimize(true);
 		}
 
 		engine->UnlockParallelAccess();
 
 		fWindow->RegionPool()->Recycle(visibleBorder);
-
-		// if the primary mouse button is released, stop
-		// dragging/resizing/sliding
-		if ((buttons & B_PRIMARY_MOUSE_BUTTON) == 0) {
-			fIsDragging = false;
-			fIsResizing = false;
-			fIsSlidingTab = false;
-		}
 	}
 
-	// in FFM mode, activate the window and bring it
-	// to front in case this was a drag click but the
-	// mouse was not moved
-	if (fActivateOnMouseUp) {
-		fActivateOnMouseUp = false;
-		// on R5, there is a time window for this feature
-		// ie, click and press too long, nothing will happen
-		if (system_time() - fLastMoveTime < kWindowActivationTimeout)
-			fDesktop->ActivateWindow(fWindow);
+	// if the primary mouse button is released, stop
+	// dragging/resizing/sliding
+	if ((buttons & B_PRIMARY_MOUSE_BUTTON) == 0) {
+		if (fMinimizeCheckOnMouseUp) {
+			// If the modifiers haven't changed in the meantime and not too
+			// much time has elapsed, we're supposed to minimize the window.
+			fMinimizeCheckOnMouseUp = false;
+			if (message->FindInt32("modifiers") == fLastModifiers
+				&& (fWindow->Flags() & B_NOT_MINIMIZABLE) == 0
+				&& system_time() - fLastMoveTime < kWindowActivationTimeout) {
+				fWindow->ServerWindow()->NotifyMinimize(true);
+			}
+		}
+
+		// In FFM mode, activate the window and bring it to front in case this
+		// was a drag click but the mouse was not moved.
+		if (fActivateOnMouseUp) {
+			fActivateOnMouseUp = false;
+			// on R5, there is a time window for this feature
+			// ie, click and press too long, nothing will happen
+			if (system_time() - fLastMoveTime < kWindowActivationTimeout)
+				fDesktop->ActivateWindow(fWindow);
+		}
+
+		fIsDragging = false;
+		fIsResizing = false;
+		fIsSlidingTab = false;
 	}
 }
 
@@ -285,10 +373,12 @@ DefaultWindowBehaviour::MouseMoved(BMessage *message, BPoint where, bool isFake)
 			// the then current mouse position
 			return;
 		}
-		if (fActivateOnMouseUp) {
+		if (fActivateOnMouseUp || fMinimizeCheckOnMouseUp) {
 			if (now - fLastMoveTime >= kWindowActivationTimeout) {
-				// This click is too long already for window activation.
+				// This click is too long already for window activation/
+				// minimizing.
 				fActivateOnMouseUp = false;
+				fMinimizeCheckOnMouseUp = false;
 			}
 		} else
 			fLastMoveTime = now;
@@ -303,16 +393,14 @@ DefaultWindowBehaviour::MouseMoved(BMessage *message, BPoint where, bool isFake)
 		engine->LockParallelAccess();
 		engine->ConstrainClippingRegion(visibleBorder);
 
-		int32 buttons = message->FindInt32("buttons");
-		int32 modifiers = message->FindInt32("modifiers");
-		click_type type = _ActionFor(message, buttons, modifiers);
+		Region hitRegion = _RegionFor(message);
 
 		if (fIsZooming)
-			decorator->SetZoom(type == CLICK_ZOOM);
+			decorator->SetZoom(hitRegion == REGION_ZOOM_BUTTON);
 		else if (fIsClosing)
-			decorator->SetClose(type == CLICK_CLOSE);
+			decorator->SetClose(hitRegion == REGION_CLOSE_BUTTON);
 		else if (fIsMinimizing)
-			decorator->SetMinimize(type == CLICK_MINIMIZE);
+			decorator->SetMinimize(hitRegion == REGION_MINIMIZE_BUTTON);
 
 		engine->UnlockParallelAccess();
 		fWindow->RegionPool()->Recycle(visibleBorder);
@@ -329,11 +417,12 @@ DefaultWindowBehaviour::MouseMoved(BMessage *message, BPoint where, bool isFake)
 
 	// If the window was moved enough, it doesn't come to
 	// the front in FFM mode when the mouse is released.
-	if (fActivateOnMouseUp) {
+	if (fActivateOnMouseUp || fMinimizeCheckOnMouseUp) {
 		fMouseMoveDistance += delta.x * delta.x + delta.y * delta.y;
-		if (fMouseMoveDistance > 16.0f)
+		if (fMouseMoveDistance > 16.0f) {
 			fActivateOnMouseUp = false;
-		else
+			fMinimizeCheckOnMouseUp = false;
+		} else
 			delta = B_ORIGIN;
 	}
 
@@ -403,19 +492,52 @@ DefaultWindowBehaviour::_IsWindowModifier(int32 modifiers) const
 }
 
 
-click_type
-DefaultWindowBehaviour::_ActionFor(const BMessage* message, int32 buttons,
-	int32 modifiers) const
+DefaultWindowBehaviour::Region
+DefaultWindowBehaviour::_RegionFor(const BMessage* message) const
 {
 	Decorator* decorator = fWindow->Decorator();
 	if (decorator == NULL)
-		return CLICK_NONE;
+		return REGION_NONE;
 
 	BPoint where;
 	if (message->FindPoint("where", &where) != B_OK)
-		return CLICK_NONE;
+		return REGION_NONE;
 
-	return decorator->MouseAction(message, where, buttons, modifiers);
+	// translate the tab region into a functional region
+	switch (decorator->RegionAt(where)) {
+		case Decorator::REGION_NONE:
+			return REGION_NONE;
+
+		case Decorator::REGION_TAB:
+			return REGION_TAB;
+
+		case Decorator::REGION_CLOSE_BUTTON:
+			return REGION_CLOSE_BUTTON;
+
+		case Decorator::REGION_ZOOM_BUTTON:
+			return REGION_ZOOM_BUTTON;
+
+		case Decorator::REGION_MINIMIZE_BUTTON:
+			return REGION_MINIMIZE_BUTTON;
+
+		case Decorator::REGION_LEFT_BORDER:
+		case Decorator::REGION_RIGHT_BORDER:
+		case Decorator::REGION_TOP_BORDER:
+		case Decorator::REGION_BOTTOM_BORDER:
+			return REGION_BORDER;
+
+		case Decorator::REGION_LEFT_TOP_CORNER:
+		case Decorator::REGION_LEFT_BOTTOM_CORNER:
+		case Decorator::REGION_RIGHT_TOP_CORNER:
+			// not supported yet
+			return REGION_BORDER;
+
+		case Decorator::REGION_RIGHT_BOTTOM_CORNER:
+			return REGION_RESIZE_CORNER;
+
+		default:
+			return REGION_NONE;
+	}
 }
 
 

src/servers/app/DefaultWindowBehaviour.h
@@ -9,6 +9,7 @@
  *		Axel Dörfler <axeld@pinc-software.de>
  *		Brecht Machiels <brecht@mos6581.org>
  *		Clemens Zeidler <haiku@clemens-zeidler.de>
+ *		Ingo Weinhold <ingo_weinhold@gmx.de>
  */
 #ifndef DEFAULT_WINDOW_BEHAVIOUR_H
 #define DEFAULT_WINDOW_BEHAVIOUR_H
@@ -32,28 +33,44 @@ public:
 	virtual	void				MouseUp(BMessage* message, BPoint where);
 	virtual	void				MouseMoved(BMessage *message, BPoint where,
 									bool isFake);
-		
+
+private:
+			enum Region {
+				REGION_NONE,
+
+				REGION_TAB,
+				REGION_BORDER,
+
+				REGION_CLOSE_BUTTON,
+				REGION_ZOOM_BUTTON,
+				REGION_MINIMIZE_BUTTON,
+
+				REGION_RESIZE_CORNER
+			};
+
 private:
 			bool				_IsWindowModifier(int32 modifiers) const;
-			click_type			_ActionFor(const BMessage* message,
-									int32 buttons, int32 modifiers) const;
+			Region				_RegionFor(const BMessage* message) const;
 			void				_AlterDeltaForSnap(BPoint& delta,
 									bigtime_t now);
 
 protected:
 			Window*				fWindow;
 			Desktop*			fDesktop;
-	
+
 			bool				fIsClosing : 1;
 			bool				fIsMinimizing : 1;
 			bool				fIsZooming : 1;
 			bool				fIsSlidingTab : 1;
 			bool				fActivateOnMouseUp : 1;
-	
+			bool				fMinimizeCheckOnMouseUp : 1;
+
 			BPoint				fLastMousePosition;
 			float				fMouseMoveDistance;
 			bigtime_t			fLastMoveTime;
 			bigtime_t			fLastSnapTime;
+			int32				fLastModifiers;
+			int32				fResetClickCount;
 };
 
 

src/add-ons/decorators/BeDecorator/BeDecorator.cpp
@@ -89,7 +89,7 @@ BeDecorAddOn::BeDecorAddOn(image_id id, const char* name)
 	:
 	DecorAddOn(id, name)
 {
-	
+
 }
 
 
@@ -214,76 +214,47 @@ BeDecorator::GetSizeLimits(int32* minWidth, int32* minHeight,
 }
 
 
-click_type
-BeDecorator::MouseAction(const BMessage* message, BPoint point, int32 buttons,
-	int32 modifiers)
+Decorator::Region
+BeDecorator::RegionAt(BPoint where) const
 {
-#ifdef DEBUG_DECORATOR
-	printf("BeDecorator: Clicked\n");
-	printf("\tPoint: (%.1f,%.1f)\n", point.x, point.y);
-	printf("\tButtons: %ld, Modifiers: 0x%lx\n", buttons, modifiers);
-#endif // DEBUG_DECORATOR
+	// Let the base class version identify hits of the buttons and the tab.
+	Region region = Decorator::RegionAt(where);
+	if (region != REGION_NONE)
+		return region;
 
-	if (buttons != 0)
-		fWasDoubleClick = message->FindInt32("clicks") == 2;
+	// check the resize corner
+	if (fLook == B_DOCUMENT_WINDOW_LOOK && fResizeRect.Contains(where))
+		return REGION_RIGHT_BOTTOM_CORNER;
 
-	// In checking for hit test stuff, we start with the smallest rectangles
-	// the user might be clicking on and gradually work our way out into larger
-	// rectangles.
-	if (!(fFlags & B_NOT_CLOSABLE) && fCloseRect.Contains(point))
-		return CLICK_CLOSE;
+	// hit-test the borders
+	if (fLeftBorder.Contains(where))
+		return REGION_LEFT_BORDER;
+	if (fTopBorder.Contains(where))
+		return REGION_TOP_BORDER;
 
-	if (!(fFlags & B_NOT_ZOOMABLE) && fZoomRect.Contains(point))
-		return CLICK_ZOOM;
+	// Part of the bottom and right borders may be a resize-region, so we have
+	// to check explicitly, if it has been it.
+	if (fRightBorder.Contains(where))
+		region = REGION_RIGHT_BORDER;
+	else if (fBottomBorder.Contains(where))
+		region = REGION_BOTTOM_BORDER;
+	else
+		return REGION_NONE;
 
-	if (fLook == B_DOCUMENT_WINDOW_LOOK && fResizeRect.Contains(point))
-		return CLICK_RESIZE;
-
-	bool clicked = false;
-
-	// Clicking in the tab?
-	if (fTabRect.Contains(point)) {
-		// tab sliding in any case if either shift key is held down
-		// except sliding up-down by moving mouse left-right would look strange
-		if ((modifiers & B_SHIFT_KEY) && (fLook != kLeftTitledWindowLook))
-			return CLICK_SLIDE_TAB;
-
-		clicked = true;
-	} else if (fLeftBorder.Contains(point) || fRightBorder.Contains(point)
-		|| fTopBorder.Contains(point) || fBottomBorder.Contains(point)) {
-		// Clicked on border
-
-		// check resize area
-		if (!(fFlags & B_NOT_RESIZABLE)
-			&& (fLook == B_TITLED_WINDOW_LOOK
-				|| fLook == B_FLOATING_WINDOW_LOOK
-				|| fLook == B_MODAL_WINDOW_LOOK
-				|| fLook == kLeftTitledWindowLook)) {
-			BRect resizeRect(BPoint(fBottomBorder.right - kBorderResizeLength,
-				fBottomBorder.bottom - kBorderResizeLength),
-				fBottomBorder.RightBottom());
-			if (resizeRect.Contains(point))
-				return CLICK_RESIZE;
-		}
-
-		clicked = true;
+	// check resize area
+	if ((fFlags & B_NOT_RESIZABLE) == 0
+		&& (fLook == B_TITLED_WINDOW_LOOK
+			|| fLook == B_FLOATING_WINDOW_LOOK
+			|| fLook == B_MODAL_WINDOW_LOOK
+			|| fLook == kLeftTitledWindowLook)) {
+		BRect resizeRect(BPoint(fBottomBorder.right - kBorderResizeLength,
+			fBottomBorder.bottom - kBorderResizeLength),
+			fBottomBorder.RightBottom());
+		if (resizeRect.Contains(where))
+			return REGION_RIGHT_BOTTOM_CORNER;
 	}
 
-	if (clicked) {
-		// NOTE: On R5, windows are not moved to back if clicked inside the
-		// resize area with the second mouse button. So we check this after
-		// the check above
-		if ((buttons & B_SECONDARY_MOUSE_BUTTON) != 0)
-			return CLICK_MOVE_TO_BACK;
-
-		if (fWasDoubleClick && !(fFlags & B_NOT_MINIMIZABLE))
-			return CLICK_MINIMIZE;
-
-		return CLICK_DRAG;
-	}
-
-	// Guess user didn't click anything
-	return CLICK_NONE;
+	return region;
 }
 
 

src/add-ons/decorators/MacDecorator/MacDecorator.cpp
@@ -34,7 +34,7 @@ MacDecorAddOn::MacDecorAddOn(image_id id, const char* name)
 	:
 	DecorAddOn(id, name)
 {
-	
+
 }
 
 
@@ -64,7 +64,7 @@ MacDecorator::MacDecorator(DesktopSettings& settings, BRect rect,
 	frame_midcol = (rgb_color){ 216, 216, 216, 255 };
 	frame_lowcol = (rgb_color){ 110, 110, 110, 255 };
 	frame_lowercol = (rgb_color){ 0, 0, 0, 255 };
-	
+
 	fButtonHighColor = (rgb_color){ 232, 232, 232, 255 };
 	fButtonLowColor = (rgb_color){ 128, 128, 128, 255 };
 
@@ -72,7 +72,7 @@ MacDecorator::MacDecorator(DesktopSettings& settings, BRect rect,
 	fNonFocusTextColor = settings.UIColor(B_WINDOW_INACTIVE_TEXT_COLOR);
 
 	_DoLayout();
-	
+
 	textoffset=5;
 
 	STRACE(("MacDecorator()\n"));
@@ -117,43 +117,29 @@ MacDecorator::Draw()
 // TODO : add GetSizeLimits
 
 
-click_type
-MacDecorator::MouseAction(const BMessage* message, BPoint point, int32 buttons,
-	int32 modifiers)
+Decorator::Region
+MacDecorator::RegionAt(BPoint where) const
 {
-	if (!(fFlags & B_NOT_CLOSABLE) && fCloseRect.Contains(point)) {
-		STRACE(("MacDecorator():Clicked() - Close\n"));
-		return CLICK_CLOSE;
-	}
+	// Let the base class version identify hits of the buttons and the tab.
+	Region region = Decorator::RegionAt(where);
+	if (region != REGION_NONE)
+		return region;
 
-	if (!(fFlags & B_NOT_ZOOMABLE) && fZoomRect.Contains(point)) {
-		STRACE(("MacDecorator():Clicked() - Zoom\n"));
-		return CLICK_ZOOM;
-	}
-	
-	// Clicking in the tab?
-	if (fTabRect.Contains(point)) {
-		// Here's part of our window management stuff
-		/* TODO: This is missing CLICK_MOVETOFRONT
-		if(buttons == B_PRIMARY_MOUSE_BUTTON && !IsFocus())
-			return CLICK_MOVETOFRONT;
-		*/
-		return CLICK_DRAG;
-	}
+	// check the resize corner
+	if (fLook == B_DOCUMENT_WINDOW_LOOK && fResizeRect.Contains(where))
+		return REGION_RIGHT_BOTTOM_CORNER;
 
-	// We got this far, so user is clicking on the border?
+	// hit-test the borders
 	if (!(fFlags & B_NOT_RESIZABLE)
 		&& (fLook == B_TITLED_WINDOW_LOOK
 			|| fLook == B_FLOATING_WINDOW_LOOK
 			|| fLook == B_MODAL_WINDOW_LOOK)
-		&& fBorderRect.Contains(point) && !fFrame.Contains(point)) {
-		STRACE(("MacDecorator():Clicked() - Resize\n"));
-		return CLICK_RESIZE;
+		&& fBorderRect.Contains(where) && !fFrame.Contains(where)) {
+		return REGION_BOTTOM_BORDER;
+			// TODO: Determine the actual border!
 	}
 
-	// Guess user didn't click anything
-	STRACE(("MacDecorator():Clicked()\n"));
-	return CLICK_NONE;
+	return REGION_NONE;
 }
 
 
@@ -215,7 +201,7 @@ MacDecorator::_DoLayout()
 		fMinimizeRect=fZoomRect;
 
 		fCloseRect.OffsetTo(fTabRect.left+4,fTabRect.top+4);
-	
+
 		fZoomRect.OffsetBy(0-(fZoomRect.Width()+4),0);
 		if (Title() && fDrawingEngine) {
 			titlepixelwidth=fDrawingEngine->StringWidth(Title(),strlen(Title()));
@@ -468,7 +454,7 @@ MacDecorator::_DrawTab(BRect invalid)
 			}
 		}
 
-		// Draw the buttons if we're supposed to	
+		// Draw the buttons if we're supposed to
 		if (!(fFlags & B_NOT_CLOSABLE))
 			_DrawClose(fCloseRect);
 		if (!(fFlags & B_NOT_ZOOMABLE))
@@ -776,7 +762,7 @@ MacDecorator::_GetFootprint(BRegion* region)
 
 	if (fLook == B_NO_BORDER_WINDOW_LOOK)
 		return;
-	
+
 	region->Set(fBorderRect);
 	region->Exclude(fFrame);
 

src/add-ons/decorators/MacDecorator/MacDecorator.h
@@ -32,9 +32,7 @@ public:
 			void				Draw(BRect updateRect);
 			void				Draw();
 
-	click_type					MouseAction(const BMessage* message,
-									BPoint point, int32 buttons,
-									int32 modifiers);
+	virtual	Region				RegionAt(BPoint where) const;
 
 protected:
 			void				_DoLayout();
@@ -57,7 +55,7 @@ protected:
 									BRegion* updateRegion = NULL);
 			void				_SetFlags(uint32 flags,
 									BRegion* updateRegion = NULL);
-	
+
 			void				_SetColors();
 
 			void				_MoveBy(BPoint offset);

src/add-ons/decorators/SATDecorator/StackAndTile.cpp
@@ -140,12 +140,22 @@ StackAndTile::MouseDown(Window* window, BMessage* message, const BPoint& where)
 	// we are only interested in single clicks
 	if (message->FindInt32("clicks") == 2)
 		return;
-	int32 modifiers = message->FindInt32("modifiers");
-	int32 buttons = message->FindInt32("buttons");
-	click_type clickArea = satWindow->GetDecorator()->MouseAction(message,
-		where, buttons, modifiers);
-	if (clickArea != CLICK_DRAG && clickArea < CLICK_RESIZE)
-		return;
+
+	switch (satWindow->GetDecorator()->RegionAt(where)) {
+		case Decorator::REGION_TAB:
+		case Decorator::REGION_LEFT_BORDER:
+		case Decorator::REGION_RIGHT_BORDER:
+		case Decorator::REGION_TOP_BORDER:
+		case Decorator::REGION_BOTTOM_BORDER:
+		case Decorator::REGION_LEFT_TOP_CORNER:
+		case Decorator::REGION_LEFT_BOTTOM_CORNER:
+		case Decorator::REGION_RIGHT_TOP_CORNER:
+		case Decorator::REGION_RIGHT_BOTTOM_CORNER:
+			break;
+
+		default:
+			return;
+	}
 
 	ASSERT(fCurrentSATWindow == NULL);
 	fCurrentSATWindow = satWindow;
@@ -330,7 +340,7 @@ StackAndTile::WindowLookChanged(Window* window, window_look look)
 	SATGroup* group = satWindow->GetGroup();
 	if (!group)
 		return;
-	group->RemoveWindow(satWindow);	
+	group->RemoveWindow(satWindow);
 }
 
 
@@ -617,5 +627,5 @@ WindowIterator::_ReverseRewind()
 
 SATSnappingBehaviour::~SATSnappingBehaviour()
 {
-	
+
 }

commit 5116393fb92dee2915c2c9f624466843757c1dfa

MouseDown(): Also check whether the buttons pressed have changed since the last click and reset the click count, if so. Fixes a regression introduced in r39602 (left-click after right-click was recognized as double-click).
git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@39609 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/servers/app/DefaultWindowBehaviour.cpp
@@ -491,6 +491,7 @@ DefaultWindowBehaviour::DefaultWindowBehaviour(Window* window)
 	fDesktop(window->Desktop()),
 	fState(NULL),
 	fLastModifiers(0),
+	fLastMouseButtons(0),
 	fResetClickCount(0)
 {
 }
@@ -506,21 +507,26 @@ bool
 DefaultWindowBehaviour::MouseDown(BMessage* message, BPoint where)
 {
 	// Get the click count and reset it, if the modifiers changed in the
-	// meantime.
-	// TODO: This should be done in a better place (e.g. the input server). It
-	// should also reset clicks after mouse movement (which we don't do here
-	// either -- though that's probably acceptable).
+	// meantime. Do the same when this is not the button we've seen before.
+	// TODO: At least the modifier check should be done in a better place
+	// (e.g. the input server). It should also reset clicks after mouse
+	// movement (which we don't do here either -- though that's probably
+	// acceptable).
 	int32 clickCount = message->FindInt32("clicks");
 	int32 modifiers = message->FindInt32("modifiers");
+	int32 buttons = message->FindInt32("buttons");
+
 	if (clickCount <= 1) {
 		fResetClickCount = 0;
-	} else if (modifiers != fLastModifiers
+	} else if (modifiers != fLastModifiers || buttons != fLastMouseButtons
 		|| clickCount - fResetClickCount < 1) {
 		fResetClickCount = clickCount - 1;
 		clickCount = 1;
 	} else
 		clickCount -= fResetClickCount;
+
 	fLastModifiers = modifiers;
+	fLastMouseButtons = buttons;
 
 	// if a state is active, let it do the job
 	if (fState != NULL)
@@ -554,7 +560,6 @@ DefaultWindowBehaviour::MouseDown(BMessage* message, BPoint where)
 		}
 
 		// translate the region into an action
-		int32 buttons = message->FindInt32("buttons");
 		bool leftButton = (buttons & B_PRIMARY_MOUSE_BUTTON) != 0;
 		bool rightButton = (buttons & B_SECONDARY_MOUSE_BUTTON) != 0;
 		uint32 flags = fWindow->Flags();

src/servers/app/DefaultWindowBehaviour.h
@@ -74,6 +74,7 @@ protected:
 			Desktop*			fDesktop;
 			State*				fState;
 			int32				fLastModifiers;
+			int32				fLastMouseButtons;
 			int32				fResetClickCount;
 };
 