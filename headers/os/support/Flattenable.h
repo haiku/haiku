8bbce28359bcebd3bdf75dbc8001c4c232ac9a38
Axel Dörfler <axeld@pinc-software.de>
2010-11-29 19:39:13 +0000

    * Cleanup.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@39675 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/os/support/Flattenable.h
+++ headers/os/support/Flattenable.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007, Haiku, Inc. All Rights Reserved.
+ * Copyright 2007-2010, Haiku, Inc. All Rights Reserved.
  * Distributed under the terms of the MIT License.
  */
 #ifndef	_FLATTENABLE_H
@@ -10,20 +10,22 @@
 
 
 class BFlattenable {
-	public:
-		virtual	bool IsFixedSize() const = 0;
-		virtual	type_code TypeCode() const = 0;
-		virtual	ssize_t FlattenedSize() const = 0;
-		virtual	status_t Flatten(void* buffer, ssize_t size) const = 0;
-		virtual	bool AllowsTypeCode(type_code code) const;
-		virtual	status_t Unflatten(type_code code, const void* buffer, ssize_t size) = 0;
+public:
+	virtual	bool				IsFixedSize() const = 0;
+	virtual	type_code			TypeCode() const = 0;
+	virtual	ssize_t				FlattenedSize() const = 0;
+	virtual	status_t			Flatten(void* buffer, ssize_t size) const = 0;
+	virtual	bool				AllowsTypeCode(type_code code) const;
+	virtual	status_t			Unflatten(type_code code, const void* buffer,
+									ssize_t size) = 0;
 
-		virtual	~BFlattenable();
+	virtual						~BFlattenable();
 
-	private:
-		void _ReservedFlattenable1();
-		virtual	void _ReservedFlattenable2();
-		virtual	void _ReservedFlattenable3();
+private:
+			void				_ReservedFlattenable1();
+	virtual	void				_ReservedFlattenable2();
+	virtual	void				_ReservedFlattenable3();
 };
+
 
 #endif	// _FLATTENABLE_H
