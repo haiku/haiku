d222eb79427e5b924e7f5d8d1dc79218e35852b3
Michael Pfeiffer <michael.w.pfeiffer@gmail.com>
2010-12-03 10:46:20 +0000

    Changed wording and fixed typo (ticket #6945).
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@39715 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/servers/print/PrintServerApp.cpp
+++ src/servers/print/PrintServerApp.cpp
@@ -431,13 +431,13 @@ PrintServerApp::CreatePrinter(const char* printerName, const char* driverName,
 				return B_OK;
 			} else {
 				info.SetTo(B_TRANSLATE(
-					"There already exists a printer you are going to "
-					"create, but it's driver could not be found! Replace?"));
+					"A printer with that name already exists, "
+					"but its driver could not be found! Replace?"));
 			}
 		} else {
 			info.SetTo(B_TRANSLATE(
-				"There already exists a printer you are going to "
-				"create, but it's not usable at all! Replace?"));
+				"A printer with that name already exists, "
+				"but its not usable at all! Replace?"));
 		}
 
 		if (info.Length() != 0) {
