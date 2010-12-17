8c71173a30c45c7dd432ecfa4abaaf87aebed741
Philippe Houdoin <philippe.houdoin@gmail.com>
2010-12-17 15:52:52 +0000

    Switched ActivityMonitor to use the now localized BAboutWindow.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@39880 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/apps/activitymonitor/ActivityMonitor.cpp
+++ src/apps/activitymonitor/ActivityMonitor.cpp
@@ -8,15 +8,10 @@
 
 #include <stdlib.h>
 
-#include <Alert.h>
+#include <AboutWindow.h>
 #include <Application.h>
-#include <Catalog.h>
-#include <TextView.h>
 
 #include "ActivityWindow.h"
-
-#undef B_TRANSLATE_CONTEXT
-#define B_TRANSLATE_CONTEXT "ActivityMonitor"
 
 const char* kSignature = "application/x-vnd.Haiku-ActivityMonitor";
 
@@ -64,24 +59,13 @@ ActivityMonitor::AboutRequested()
 /*static*/ void
 ActivityMonitor::ShowAbout()
 {
-	BString	text;
-	text << kAppName << "\n"
-		<< B_TRANSLATE("\twritten by Axel Dörfler\n"
-		"\tCopyright 2008, Haiku Inc.\n");
+	const char* kAuthors[] = {
+		"Axel Dörfler",
+		NULL
+	};
 
-	BAlert *alert = new BAlert(B_TRANSLATE("About"),
-		text.String(), B_TRANSLATE("OK"));
-	BTextView *view = alert->TextView();
-	BFont font;
-
-	view->SetStylable(true);
-
-	view->GetFont(&font);
-	font.SetSize(18);
-	font.SetFace(B_BOLD_FACE);
-	view->SetFontAndColor(0, strlen(kAppName), &font);
-
-	alert->Go();
+	BAboutWindow aboutWindow(kAppName, 2008, kAuthors);
+	aboutWindow.Show();
 }
 
 
