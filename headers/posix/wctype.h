b27cf34dc1a615b7042aee80bafad880640bf165
Oliver Tappe <zooey@hirschkaefer.de>
2010-12-21 23:13:47 +0000

    Close #6969 (warning about redundant declarations): * drop function declarations from wctype.h which are already in wchar.h
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@39917 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/posix/wctype.h
+++ headers/posix/wctype.h
@@ -14,20 +14,8 @@ typedef int wctrans_t;
 extern "C" {
 #endif
 
-extern int		iswalnum(wint_t wc);
-extern int		iswalpha(wint_t wc);
 extern int		iswblank(wint_t wc);
-extern int		iswcntrl(wint_t wc);
-extern int		iswdigit(wint_t wc);
-extern int		iswgraph(wint_t wc);
-extern int		iswlower(wint_t wc);
-extern int		iswprint(wint_t wc);
-extern int		iswpunct(wint_t wc);
-extern int		iswspace(wint_t wc);
-extern int		iswupper(wint_t wc);
-extern int		iswxdigit(wint_t wc);
 
-extern int		iswctype(wint_t wc, wctype_t charClass);
 extern wint_t	towctrans(wint_t wc, wctrans_t transition);
 extern wint_t	towlower(wint_t wc);
 extern wint_t	towupper(wint_t wc);
