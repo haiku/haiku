haiku/haiku

commit 523ce58a8f775840e214534f41019070425e4ee2

Applied patch from engleek with fix hints from devheart (bug #4720): Scale to fit now keeps the aspect ratio by cutting horizontally or vertically.
git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@40144 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/kits/tracker/BackgroundImage.cpp
@@ -199,7 +199,18 @@ BackgroundImage::Show(BackgroundImageInfo *info, BView *view)
 			// else fall thru
 		case kScaledToFit:
 			if (fIsDesktop) {
-				destinationBitmapBounds = viewBounds;
+				if (BRectRatio(destinationBitmapBounds)
+					>= BRectRatio(viewBounds)) {
+					float overlap = BRectHorizontalOverlap(viewBounds,
+						destinationBitmapBounds);
+					destinationBitmapBounds.Set(-overlap, 0,
+						viewBounds.Width() + overlap, viewBounds.Height());
+				} else {
+					float overlap = BRectVerticalOverlap(viewBounds,
+						destinationBitmapBounds);
+					destinationBitmapBounds.Set(0, -overlap,
+						viewBounds.Width(), viewBounds.Height() + overlap);
+				}
 				followFlags = B_FOLLOW_ALL;
 				break;
 			}
@@ -223,6 +234,30 @@ BackgroundImage::Show(BackgroundImageInfo *info, BView *view)
 	view->Invalidate();
 	fShowingBitmap = info;
 }
+
+
+float
+BackgroundImage::BRectRatio(BRect rect)
+{
+	return rect.Width() / rect.Height();
+}
+
+
+float
+BackgroundImage::BRectHorizontalOverlap(BRect hostRect, BRect resizedRect)
+{
+	return ((hostRect.Height() / resizedRect.Height() * resizedRect.Width())
+		- hostRect.Width()) / 2;
+}
+
+
+float
+BackgroundImage::BRectVerticalOverlap(BRect hostRect, BRect resizedRect)
+{
+	return ((hostRect.Width() / resizedRect.Width() * resizedRect.Height())
+		- hostRect.Height()) / 2;
+}
+
 
 void 
 BackgroundImage::Remove()

src/preferences/backgrounds/BackgroundImage.cpp
@@ -299,7 +299,18 @@ BackgroundImage::Show(BackgroundImageInfo* info, BView* view)
 			// else fall thru
 		case kScaledToFit:
 			if (fIsDesktop) {
-				destinationBitmapBounds = viewBounds;
+				if (BRectRatio(destinationBitmapBounds)
+					>= BRectRatio(viewBounds)) {
+					float overlap = BRectHorizontalOverlap(viewBounds,
+						destinationBitmapBounds);
+					destinationBitmapBounds.Set(-overlap, 0,
+						viewBounds.Width() + overlap, viewBounds.Height());
+				} else {
+					float overlap = BRectVerticalOverlap(viewBounds,
+						destinationBitmapBounds);
+					destinationBitmapBounds.Set(0, -overlap,
+						viewBounds.Width(), viewBounds.Height() + overlap);
+				}
 				followFlags = B_FOLLOW_ALL;
 				break;
 			}
@@ -331,6 +342,29 @@ BackgroundImage::Show(BackgroundImageInfo* info, BView* view)
 			fShowingBitmap->UnloadBitmap(fCacheMode);
 		fShowingBitmap = info;
 	}*/
+}
+
+
+float
+BackgroundImage::BRectRatio(BRect rect)
+{
+	return rect.Width() / rect.Height();
+}
+
+
+float
+BackgroundImage::BRectHorizontalOverlap(BRect hostRect, BRect resizedRect)
+{
+	return ((hostRect.Height() / resizedRect.Height() * resizedRect.Width())
+		- hostRect.Width()) / 2;
+}
+
+
+float
+BackgroundImage::BRectVerticalOverlap(BRect hostRect, BRect resizedRect)
+{
+	return ((hostRect.Width() / resizedRect.Width() * resizedRect.Height())
+		- hostRect.Height()) / 2;
 }
 
 

src/kits/tracker/BackgroundImage.h
@@ -115,6 +115,10 @@ private:
 
 	void Add(BackgroundImageInfo *);
 
+	float BRectRatio(BRect rect);
+	float BRectHorizontalOverlap(BRect hostRect, BRect resizedRect);
+	float BRectVerticalOverlap(BRect hostRect, BRect resizedRect);
+
 	bool fIsDesktop;
 	BNode fDefinedByNode;
 	BView *fView;

src/preferences/backgrounds/BackgroundImage.h
@@ -139,6 +139,10 @@ private:
 		// no public constructor, GetBackgroundImage factory function is
 		// used instead
 
+	float BRectRatio(BRect rect);
+	float BRectHorizontalOverlap(BRect hostRect, BRect resizedRect);
+	float BRectVerticalOverlap(BRect hostRect, BRect resizedRect);
+
 	bool fIsDesktop;
 	BNode fDefinedByNode;
 	BView* fView;

commit 9f609ed7c64b3d484b5faadfadf2bba662828487

* Fixed wrong indentation of the if-clause introduced in r40144. * Automatic whitespace cleanup.
git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@40147 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/kits/tracker/BackgroundImage.cpp
@@ -66,7 +66,7 @@ BackgroundImage::GetBackgroundImage(const BNode *node, bool isDesktop)
 	attr_info info;
 	if (node->GetAttrInfo(kBackgroundImageInfo, &info) != B_OK)
 		return NULL;
-	
+
 	BMessage container;
 	char *buffer = new char [info.size];
 
@@ -88,7 +88,7 @@ BackgroundImage::GetBackgroundImage(const BNode *node, bool isDesktop)
 		BPoint offset;
 		BBitmap *bitmap = NULL;
 
-		if (container.FindString(kBackgroundImageInfoPath, index, &path) == B_OK) {	
+		if (container.FindString(kBackgroundImageInfoPath, index, &path) == B_OK) {
 			bitmap = BTranslationUtils::GetBitmap(path);
 			if (!bitmap) {
 				PRINT(("failed to load background bitmap from path\n"));
@@ -146,14 +146,14 @@ BackgroundImage::~BackgroundImage()
 }
 
 
-void 
+void
 BackgroundImage::Add(BackgroundImageInfo *info)
 {
 	fBitmapForWorkspaceList.AddItem(info);
 }
 
 
-void 
+void
 BackgroundImage::Show(BView *view, int32 workspace)
 {
 	fView = view;
@@ -167,7 +167,7 @@ BackgroundImage::Show(BView *view, int32 workspace)
 	}
 }
 
-void 
+void
 BackgroundImage::Show(BackgroundImageInfo *info, BView *view)
 {
 	BPoseView *poseView = dynamic_cast<BPoseView *>(view);
@@ -200,7 +200,7 @@ BackgroundImage::Show(BackgroundImageInfo *info, BView *view)
 		case kScaledToFit:
 			if (fIsDesktop) {
 				if (BRectRatio(destinationBitmapBounds)
-					>= BRectRatio(viewBounds)) {
+						>= BRectRatio(viewBounds)) {
 					float overlap = BRectHorizontalOverlap(viewBounds,
 						destinationBitmapBounds);
 					destinationBitmapBounds.Set(-overlap, 0,
@@ -227,7 +227,7 @@ BackgroundImage::Show(BackgroundImageInfo *info, BView *view)
 			tile = B_TILE_BITMAP;
 			break;
 	}
-	
+
 	// switch to the bitmap and force a redraw
 	view->SetViewBitmap(info->fBitmap, bitmapBounds, destinationBitmapBounds,
 		followFlags, tile);
@@ -259,7 +259,7 @@ BackgroundImage::BRectVerticalOverlap(BRect hostRect, BRect resizedRect)
 }
 
 
-void 
+void
 BackgroundImage::Remove()
 {
 	if (fShowingBitmap) {
@@ -280,7 +280,7 @@ BackgroundImage::ImageInfoForWorkspace(int32 workspace) const
 
 	for ( ; workspace; workspace--)
 		workspaceMask *= 2;
-	
+
 	int32 count = fBitmapForWorkspaceList.CountItems();
 
 	// do a simple lookup for the most likely candidate bitmap -
@@ -294,11 +294,11 @@ BackgroundImage::ImageInfoForWorkspace(int32 workspace) const
 		if (info->fWorkspace & workspaceMask)
 			result = info;
 	}
-	
+
 	return result;
 }
 
-void 
+void
 BackgroundImage::WorkspaceActivated(BView *view, int32 workspace, bool state)
 {
 	if (!fIsDesktop)
@@ -308,7 +308,7 @@ BackgroundImage::WorkspaceActivated(BView *view, int32 workspace, bool state)
 	if (!state)
 		// we only care comming into a new workspace, not leaving one
 		return;
-		
+
 	BackgroundImageInfo *info = ImageInfoForWorkspace(workspace);
 
 	if (info != fShowingBitmap) {
@@ -324,12 +324,12 @@ BackgroundImage::WorkspaceActivated(BView *view, int32 workspace, bool state)
 	}
 }
 
-void 
+void
 BackgroundImage::ScreenChanged(BRect, color_space)
 {
 	if (!fIsDesktop || !fShowingBitmap)
 		return;
-	
+
 	if (fShowingBitmap->fMode == kCentered) {
 		BRect viewBounds(fView->Bounds());
 		BRect bitmapBounds(fShowingBitmap->fBitmap->Bounds());
@@ -352,7 +352,7 @@ BackgroundImage::Refresh(BackgroundImage *oldBackgroundImage,
 		oldBackgroundImage->Remove();
 		delete oldBackgroundImage;
 	}
-	
+
 	BackgroundImage *result = GetBackgroundImage(fromNode, desktop);
 	if (result && poseView->ViewMode() != kListMode)
 		result->Show(poseView, current_workspace());