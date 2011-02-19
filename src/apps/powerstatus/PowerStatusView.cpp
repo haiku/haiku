haiku/haiku

commit dc15e113e46653abfda42ed15f6f05740e782093

Yet another localization patch made by Jorma Karvonen. Fixes #7237.
Notes: 
1) proposed in original patch localization of the 
Deskbar item name I found a bad idea so it is rejected;
2) I have additionally removed translation for console
messages visible only in terminal. They are purposed 
mainly for debugging and are not visible to the user
in usual usage conditions;
3) Translation of the name of settings file also was 
removed as common localization practice.



git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@40557 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/apps/powerstatus/PowerStatusView.cpp
@@ -42,6 +42,7 @@
 
 
 extern "C" _EXPORT BView *instantiate_deskbar_item(void);
+extern const char* kDeskbarItemName;
 
 const uint32 kMsgToggleLabel = 'tglb';
 const uint32 kMsgToggleTime = 'tgtm';
@@ -50,7 +51,6 @@ const uint32 kMsgToggleExtInfo = 'texi';
 
 const uint32 kMinIconWidth = 16;
 const uint32 kMinIconHeight = 16;
-
 
 PowerStatusView::PowerStatusView(PowerStatusDriverInterface* interface,
 		BRect frame, int32 resizingMode,  int batteryID, bool inDeskbar)
@@ -609,7 +609,7 @@ PowerStatusReplicant::MouseDown(BPoint point)
 void
 PowerStatusReplicant::_AboutRequested()
 {
-	BAlert* alert = new BAlert(B_TRANSLATE("about"),
+	BAlert* alert = new BAlert(B_TRANSLATE("About"),
 		B_TRANSLATE("PowerStatus\n"
 			"written by Axel Dörfler, Clemens Zeidler\n"
 			"Copyright 2006, Haiku, Inc.\n"), B_TRANSLATE("OK"));
@@ -621,7 +621,7 @@ PowerStatusReplicant::_AboutRequested()
 	view->GetFont(&font);
 	font.SetSize(18);
 	font.SetFace(B_BOLD_FACE);
-	view->SetFontAndColor(0, 11, &font);
+	view->SetFontAndColor(0, strlen(B_TRANSLATE("PowerStatus"))-1, &font);
 
 	alert->Go();
 }
@@ -635,7 +635,7 @@ PowerStatusReplicant::_Init()
 		delete fDriverInterface;
 		fDriverInterface = new APMDriverInterface;
 		if (fDriverInterface->Connect() != B_OK) {
-			fprintf(stderr, B_TRANSLATE("No power interface found.\n"));
+			fprintf(stderr, "No power interface found.\n");
 			_Quit();
 		}
 	}
@@ -668,7 +668,7 @@ PowerStatusReplicant::_GetSettings(BFile& file, int mode)
 	if (status != B_OK)
 		return status;
 
-	path.Append(B_TRANSLATE("PowerStatus settings"));
+	path.Append("PowerStatus settings");
 
 	return file.SetTo(path.Path(), mode);
 }

src/apps/powerstatus/ACPIDriverInterface.cpp
@@ -12,14 +12,9 @@
 #include <stdio.h>
 
 #include <Autolock.h>
-#include <Catalog.h>
 #include <Directory.h>
 #include <Entry.h>
 #include <Path.h>
-
-
-#undef B_TRANSLATE_CONTEXT
-#define B_TRANSLATE_CONTEXT "PowerStatus"
 
 
 RateBuffer::RateBuffer()
@@ -143,7 +138,7 @@ Battery::_Init()
 	if (fInitStatus != B_OK)
 		return;
 
-	printf(B_TRANSLATE("ACPI driver found\n"));
+	printf("ACPI driver found\n");
 
 }
 
@@ -162,7 +157,7 @@ const char* kDriverDir = "/dev/power";
 status_t
 ACPIDriverInterface::Connect()
 {
-	printf(B_TRANSLATE("ACPI connect\n"));
+	printf("ACPI connect\n");
 	return _FindDrivers(kDriverDir);
 }
 
@@ -245,7 +240,7 @@ ACPIDriverInterface::_FindDrivers(const char* dirpath)
 		else {
 			int32 handler = open(path.Path(), O_RDWR);
 			if (handler >= 0) {
-				printf(B_TRANSLATE("try %s\n"), path.Path());
+				printf("try %s\n", path.Path());
 				Battery* battery = new Battery(handler);
 				if (battery->InitCheck() == B_OK) {
 					fDriverList.AddItem(battery);

commit c39eaa9d2d01c659a4a5ed50a63a8c8be5b86711

Fix the font formatting issues introduced by r40557, r40558. Fixes #7238. Pointed by Diver. Thanks ... and sorry.
git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@40559 a95241bf-73f2-0310-859d-f6bbb57e9c96

src/apps/powerstatus/PowerStatusView.cpp
@@ -621,7 +621,7 @@ PowerStatusReplicant::_AboutRequested()
 	view->GetFont(&font);
 	font.SetSize(18);
 	font.SetFace(B_BOLD_FACE);
-	view->SetFontAndColor(0, strlen(B_TRANSLATE("PowerStatus"))-1, &font);
+	view->SetFontAndColor(0, strlen(B_TRANSLATE("PowerStatus")), &font);
 
 	alert->Go();
 }

src/apps/processcontroller/ProcessController.cpp
@@ -412,7 +412,7 @@ ProcessController::AboutRequested()
 	view->GetFont(&font);
 	font.SetSize(font.Size() * 1.5);
 	font.SetFace(B_BOLD_FACE);
-	view->SetFontAndColor(0, strlen(B_TRANSLATE("ProcessController"))-1, &font);
+	view->SetFontAndColor(0, strlen(B_TRANSLATE("ProcessController")), &font);
 
 	alert->Go();
 }