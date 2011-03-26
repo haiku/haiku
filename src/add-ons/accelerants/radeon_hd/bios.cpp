0ceb83958d08555379a75c78de5f2740aeb3d1e5
Alexander von Gluck IV <kallisti5@unixzen.com>
2011-03-26 16:19:25 +0000

    add first accelerant atombios wrapper sources, atombios is now accessible from accelerant; atombios calls PCI register read/write routines from the main accelerant that need implimented, thus bios.cpp is disabled atm... going to be tricky
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@41112 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ src/add-ons/accelerants/radeon_hd/bios.cpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2011, Haiku, Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Alexander von Gluck IV, kallisti5@unixzen.com
+ */
+
+
+#include <Debug.h>
+
+#include "bios.h"
+
+#include "accelerant.h"
+#include "accelerant_protos.h"
+
+
+#undef TRACE
+
+#define TRACE_ATOM
+#ifdef TRACE_ATOM
+#   define TRACE(x) _sPrintf x
+#else
+#   define TRACE(x) ;
+#endif
+
+
+status_t
+AtomBiosParser(void *pspace, uint8_t index, void *handle, void *BIOSBase)
+{
+	DEVICE_DATA deviceData;
+
+	deviceData.pParameterSpace = (UINT32*)pspace;
+	deviceData.CAIL = handle;
+	deviceData.pBIOS_Image = (UINT8*)BIOSBase;
+	deviceData.format = TABLE_FORMAT_BIOS;
+
+	switch (ParseTable(&deviceData, index)) {
+		case CD_SUCCESS:
+			TRACE(("%s: CD_SUCCESS : success\n", __FUNCTION__));
+			return B_OK;
+			break;
+		case CD_CALL_TABLE:
+			TRACE(("%s: CD_CALL_TABLE : success\n", __FUNCTION__));
+			return B_OK;
+			break;
+		case CD_COMPLETED:
+			TRACE(("%s: CD_COMPLETED : success\n", __FUNCTION__));
+			return B_OK;
+			break;
+		default:
+			TRACE(("%s: UNKNOWN ERROR\n", __FUNCTION__));
+	}
+	return B_ERROR;
+}
+
---
+++ src/add-ons/accelerants/radeon_hd/bios.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2011, Haiku, Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Alexander von Gluck IV, kallisti5@unixzen.com
+ */
+
+
+#include <stdint.h>
+
+// AtomBios includes
+extern "C" {
+#include "CD_Common_Types.h"
+#include "CD_Definitions.h"
+#include "atombios.h"
+}
