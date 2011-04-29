8235ff073b8ee758b7f1c4981bcf29ea0cdb7664
Joachim Seemer <humdingerb@googlemail.com>
2011-04-29 17:18:28 +0000

    * Added all yet missing flags (245 all in all) plus some more optimizations. * GS, KY and PN all share the same flag, because I'm too lazy to add their   specific code of arms, which one wouldn't recognize in the small flag   anyway... At least every country is now listed in CountryFlags.rdef, so   anyone can improve flags and simply replace the rdef data there. * Added newly generated CountryFlags.rdef * Added little script to create CountryFlags.rdef to build/scripts/ just in   case.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@41292 a95241bf-73f2-0310-859d-f6bbb57e9c96

---
+++ build/scripts/generate_CountryFlags.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Generate LocaleKit's CountryFlags.rdef
+# from all flag rdefs in the current folder
+
+destination=CountryFlags.rdef
+nr=0
+
+for file in *
+do 
+	id=`echo "$file" | cut -b -2`
+	name=`echo "${file%%.*}" | cut -b 4-`
+
+	echo "# Flag data for $name" >> $destination
+	echo "resource($nr,\"$id\") #'VICN' array {" >> $destination
+	tail -n +3 "$file" >> $destination
+	echo >> $destination
+	
+	nr=`expr $nr + 1`	
+	echo \ $nr, $name, $id... OK
+done
