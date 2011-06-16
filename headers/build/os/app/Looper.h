bd9d1df3a00fe5bb03cb1941d3b7cfdc659294e0
Ingo Weinhold <ingo_weinhold@gmx.de>
2011-06-16 19:12:46 +0200

    Added very simple BLooper class to libbe_build.

--- headers/build/os/app/Looper.h
+++ headers/build/os/app/Looper.h
@@ -1,1 +1,28 @@
-#include <../os/app/Looper.h>
+/*
+ * Copyright 2011, Haiku, Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _LOOPER_H
+#define _LOOPER_H
+
+
+#include <OS.h>
+
+
+// Port (Message Queue) Capacity
+#define B_LOOPER_PORT_DEFAULT_CAPACITY  200
+
+
+class BLooper {
+public:
+								BLooper(const char* name = NULL,
+                                	int32 priority = B_NORMAL_PRIORITY,
+	                                int32 port_capacity
+    	                                = B_LOOPER_PORT_DEFAULT_CAPACITY);
+
+			bool				Lock()		{ return true; }
+			void				Unlock()	{}
+};
+
+
+#endif	// _LOOPER_H
