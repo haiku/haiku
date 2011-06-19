fab5eddca9e54e7325fe452aed2c7a0bbd3dfe62
Ingo Weinhold <ingo_weinhold@gmx.de>
2011-06-19 13:46:51 +0200

    Split package kit DataReader.cpp
    Pull AttributeDataReader and FDDataReader implementations out of
    DataReader.cpp into own source files. Thus we can avoid dependencies
    (e.g. to fs_attr code) we don't need/want.

--- src/kits/package/hpkg/DataReader.cpp
+++ src/kits/package/hpkg/DataReader.cpp
@@ -1,16 +1,12 @@
 /*
- * Copyright 2009, Ingo Weinhold, ingo_weinhold@gmx.de.
+ * Copyright 2009-2011, Ingo Weinhold, ingo_weinhold@gmx.de.
  * Distributed under the terms of the MIT License.
  */
 
 
 #include <package/hpkg/DataReader.h>
 
-#include <errno.h>
 #include <string.h>
-#include <unistd.h>
-
-#include <fs_attr.h>
 
 
 namespace BPackageKit {
@@ -23,50 +19,6 @@ namespace BHPKG {
 
 BDataReader::~BDataReader()
 {
-}
-
-
-// #pragma mark - BFDDataReader
-
-
-BFDDataReader::BFDDataReader(int fd)
-	:
-	fFD(fd)
-{
-}
-
-
-status_t
-BFDDataReader::ReadData(off_t offset, void* buffer, size_t size)
-{
-	ssize_t bytesRead = pread(fFD, buffer, size, offset);
-	if (bytesRead < 0)
-		return errno;
-	return (size_t)bytesRead == size ? B_OK : B_ERROR;
-}
-
-
-// #pragma mark - BAttributeDataReader
-
-
-BAttributeDataReader::BAttributeDataReader(int fd, const char* attribute,
-	uint32 type)
-	:
-	fFD(fd),
-	fType(type),
-	fAttribute(attribute)
-{
-}
-
-
-status_t
-BAttributeDataReader::ReadData(off_t offset, void* buffer, size_t size)
-{
-	ssize_t bytesRead = fs_read_attr(fFD, fAttribute, fType, offset, buffer,
-		size);
-	if (bytesRead < 0)
-		return errno;
-	return (size_t)bytesRead == size ? B_OK : B_ERROR;
 }
 
 
