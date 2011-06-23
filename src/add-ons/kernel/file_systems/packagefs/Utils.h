3ad1037bcce52347c19b49ede50eeed8545e3cff
Ingo Weinhold <ingo_weinhold@gmx.de>
2011-06-23 21:28:35 +0200

    Move set_dirent_name() to Utils.h

--- src/add-ons/kernel/file_systems/packagefs/Utils.h
+++ src/add-ons/kernel/file_systems/packagefs/Utils.h
@@ -6,6 +6,10 @@
 #define UTILS_H
 
 
+#include <dirent.h>
+#include <string.h>
+
+
 inline bool operator<(const timespec& a, const timespec& b)
 {
 	return a.tv_sec < b.tv_sec
@@ -16,6 +20,21 @@ inline bool operator<(const timespec& a, const timespec& b)
 inline bool operator>(const timespec& a, const timespec& b)
 {
 	return b < a;
+}
+
+
+static inline bool
+set_dirent_name(struct dirent* buffer, size_t bufferSize, const char* name,
+	size_t nameLen)
+{
+	size_t length = (buffer->d_name + nameLen + 1) - (char*)buffer;
+	if (length > bufferSize)
+		return false;
+
+	memcpy(buffer->d_name, name, nameLen);
+	buffer->d_name[nameLen] = '\0';
+	buffer->d_reclen = length;
+	return true;
 }
 
 
