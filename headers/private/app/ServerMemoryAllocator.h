cefef1c3718548e2c2e8d62a658e0986d0c93219
Axel Dörfler <axeld@pinc-software.de>
2011-06-24 18:22:02 +0000

    * Header cleanup.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@42304 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- headers/private/app/ServerMemoryAllocator.h
+++ headers/private/app/ServerMemoryAllocator.h
@@ -12,25 +12,31 @@
 #include <OS.h>
 #include <List.h>
 
+
 namespace BPrivate {
 
+
 class ServerMemoryAllocator {
-	public:
-		ServerMemoryAllocator();
-		~ServerMemoryAllocator();
+public:
+								ServerMemoryAllocator();
+								~ServerMemoryAllocator();
 
-		status_t InitCheck();
+			status_t			InitCheck();
 
-		status_t AddArea(area_id serverArea, area_id& _localArea, uint8*& _base,
-					size_t size, bool readOnly = false);
-		void RemoveArea(area_id serverArea);
+			status_t			AddArea(area_id serverArea, area_id& _localArea,
+									uint8*& _base, size_t size,
+									bool readOnly = false);
+			void				RemoveArea(area_id serverArea);
 
-		status_t AreaAndBaseFor(area_id serverArea, area_id& area, uint8*& base);
+			status_t			AreaAndBaseFor(area_id serverArea,
+									area_id& area, uint8*& base);
 
-	private:
-		BList	fAreas;
+private:
+			BList				fAreas;
 };
+
 
 }	// namespace BPrivate
 
-#endif	/* SERVER_MEMORY_ALLOCATOR_H */
+
+#endif	// SERVER_MEMORY_ALLOCATOR_H
