c2f56ca020107fdff0c1148fb8429d3431e655c7
Ingo Weinhold <ingo_weinhold@gmx.de>
2011-06-27 03:48:00 +0200

    BPackageResolvable: Add compatibleVersion property

--- src/kits/package/PackageResolvable.cpp
+++ src/kits/package/PackageResolvable.cpp
@@ -33,17 +33,20 @@ BPackageResolvable::BPackageResolvable(const BPackageResolvableData& data)
 	:
 	fName(data.name),
 	fType(data.type),
-	fVersion(data.version)
+	fVersion(data.version),
+	fCompatibleVersion(data.compatibleVersion)
 {
 }
 
 
 BPackageResolvable::BPackageResolvable(const BString& name,
-	BPackageResolvableType type, const BPackageVersion& version)
+	BPackageResolvableType type, const BPackageVersion& version,
+	const BPackageVersion& compatibleVersion)
 	:
 	fName(name),
 	fType(type),
-	fVersion(version)
+	fVersion(version),
+	fCompatibleVersion(compatibleVersion)
 {
 }
 
@@ -76,6 +79,13 @@ BPackageResolvable::Version() const
 }
 
 
+const BPackageVersion&
+BPackageResolvable::CompatibleVersion() const
+{
+	return fCompatibleVersion;
+}
+
+
 BString
 BPackageResolvable::ToString() const
 {
@@ -85,17 +95,21 @@ BPackageResolvable::ToString() const
 	if (fVersion.InitCheck() == B_OK)
 		string << '=' << fVersion.ToString();
 
+	if (fCompatibleVersion.InitCheck() == B_OK)
+		string << " compat>=" << fCompatibleVersion.ToString();
+
 	return string;
 }
 
 
 void
 BPackageResolvable::SetTo(const BString& name, BPackageResolvableType type,
-	const BPackageVersion& version)
+	const BPackageVersion& version, const BPackageVersion& compatibleVersion)
 {
 	fName = name;
 	fType = type;
 	fVersion = version;
+	fCompatibleVersion = compatibleVersion;
 }
 
 
@@ -104,6 +118,7 @@ BPackageResolvable::Clear()
 {
 	fName.Truncate(0);
 	fVersion.Clear();
+	fCompatibleVersion.Clear();
 }
 
 
