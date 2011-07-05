331b89b3a5568ad9efcd8a10aa587366753bc6a4
Ingo Weinhold <ingo_weinhold@gmx.de>
2011-07-05 22:57:29 +0200

    Add NameIndex class and create instance in Volume

---
+++ src/add-ons/kernel/file_systems/packagefs/NameIndex.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2011, Ingo Weinhold, ingo_weinhold@gmx.de.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef NAME_INDEX_H
+#define NAME_INDEX_H
+
+
+#include "Index.h"
+#include "NodeListener.h"
+
+
+class NameIndexIterator;
+
+
+class NameIndex : public Index, private NodeListener {
+public:
+								NameIndex();
+	virtual						~NameIndex();
+
+			status_t			Init(Volume* volume);
+
+	virtual	int32				CountEntries() const;
+
+private:
+	virtual	void				NodeAdded(Node* node);
+	virtual	void				NodeRemoved(Node* node);
+	virtual	void				NodeChanged(Node* node, uint32 statFields);
+
+protected:
+	virtual	AbstractIndexIterator* InternalGetIterator();
+	virtual	AbstractIndexIterator* InternalFind(const void* key,
+									size_t length);
+
+private:
+			class EntryTree;
+			friend class NameIndexIterator;
+
+			void				_UpdateLiveQueries(Node* entry,
+									const char* oldName, const char* newName);
+
+private:
+			EntryTree*			fEntries;
+};
+
+
+#endif	// NAME_INDEX_H
