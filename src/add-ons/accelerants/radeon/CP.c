70d8929d63a2cb9ee2a5e97f411858d9450d7c99
Alexander von Gluck IV <kallisti5@unixzen.com>
2011-07-11 18:05:46 +0000

    * Wrap x86 asm in defines
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@42410 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/accelerants/radeon/CP.c
+++ src/add-ons/accelerants/radeon/CP.c
@@ -329,7 +329,10 @@ void Radeon_SendIndirectBuffer( accelerator_info *ai,
 	// flush writes to CP buffers
 	// (this code is a bit of a overkill - currently, only some WinChip/Cyrix 
 	//  CPU's support out-of-order writes, but we are prepared)
+	// TODO : Other Architectures? PowerPC?
+	#if defined(__x86__) || defined(__x86_64__)
 	__asm__ __volatile__ ("lock; addl $0,0(%%esp)": : :"memory");
+	#endif
 	// make sure the motherboard chipset has flushed its write buffer by
 	// reading some uncached memory
 	//(void)*(volatile int *)si->framebuffer;
