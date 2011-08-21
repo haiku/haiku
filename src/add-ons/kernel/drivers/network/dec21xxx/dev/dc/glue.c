283db26d5c13815ee18d3b4a9a51789db2fa98d2
Siarzhuk Zharski <zharik@gmx.li>
2011-08-21 12:45:12 +0000

    * FreeBSD compatibility layer for network drivers is modified to handle   NULL-terminated list of driver_t* entries instead of single entry.   That allows to combine multiple FreeBSD drivers into single Haiku   driver add-ons; * Support for DEC 21140 (Tulip) chipsets (provided by the 'de' driver)   incorporated into dec21xxx driver. That brings network connectivity to   Haiku systems running in MS Virtual PC VMs.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@42658 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/kernel/drivers/network/dec21xxx/dev/dc/glue.c
+++ src/add-ons/kernel/drivers/network/dec21xxx/dev/dc/glue.c
@@ -1,6 +1,10 @@
 /*
- * Copyright 2007, Axel Dörfler, axeld@pinc-software.de. All Rights Reserved.
+ * Copyright 2011, Haiku, Inc. All Rights Reserved.
  * Distributed under the terms of the MIT License.
+ *
+ * Author(s):
+ *		Axel Dörfler <axeld@pinc-software.de>
+ *		Siarzhuk Zharski <imker@gmx.li>
  */
 
 
@@ -8,19 +12,32 @@
 #include <sys/mutex.h>
 #include <sys/systm.h>
 #include <machine/bus.h>
+#include <shared.h>
 
 #include "if_dcreg.h"
 
-HAIKU_FBSD_DRIVER_GLUE(dec21xxx, dc, pci)
+HAIKU_FBSD_DRIVERS_GLUE(dec21xxx);
 
 HAIKU_DRIVER_REQUIREMENTS(FBSD_TASKQUEUES | FBSD_FAST_TASKQUEUE | FBSD_SWI_TASKQUEUE);
+
+extern driver_t *DRIVER_MODULE_NAME(dc, pci);
+extern driver_t *DRIVER_MODULE_NAME(de, pci);
+
+status_t __haiku_handle_fbsd_drivers_list(status_t (*handler)(driver_t *[]))
+{
+	driver_t *drivers[] = { 
+		DRIVER_MODULE_NAME(dc, pci),
+		DRIVER_MODULE_NAME(de, pci),
+		NULL
+	};
+	return (*handler)(drivers);
+}
 
 extern driver_t *DRIVER_MODULE_NAME(acphy, miibus);
 extern driver_t *DRIVER_MODULE_NAME(amphy, miibus);
 extern driver_t *DRIVER_MODULE_NAME(dcphy, miibus);
 extern driver_t *DRIVER_MODULE_NAME(pnphy, miibus);
 extern driver_t *DRIVER_MODULE_NAME(ukphy, miibus);
-
 
 driver_t *
 __haiku_select_miibus_driver(device_t dev)
@@ -38,8 +55,50 @@ __haiku_select_miibus_driver(device_t dev)
 }
 
 
+int check_disable_interrupts_dc(device_t dev);
+void reenable_interrupts_dc(device_t dev);
+
+extern int check_disable_interrupts_de(device_t dev);
+extern void reenable_interrupts_de(device_t dev);
+
+
 int
 HAIKU_CHECK_DISABLE_INTERRUPTS(device_t dev)
+{
+	uint16 name = *(uint16*)dev->device_name;
+	switch(name) {
+		case 'cd':
+			return check_disable_interrupts_dc(dev);
+		case 'ed':
+			return check_disable_interrupts_de(dev);
+		default:
+			break;
+	}
+
+	panic("Unsupported device: %#x (%s)!", name, dev->device_name);
+	return 0;
+}
+
+
+void
+HAIKU_REENABLE_INTERRUPTS(device_t dev)
+{
+	uint16 name = *(uint16*)dev->device_name;
+	switch(name) {
+		case 'cd':
+			reenable_interrupts_dc(dev);
+			break;
+		case 'ed':
+			reenable_interrupts_de(dev);
+			break;
+		default:
+			panic("Unsupported device: %#x (%s)!", name, dev->device_name);
+			break;
+	}
+}
+
+
+int check_disable_interrupts_dc(device_t dev)
 {
 	struct dc_softc *sc = device_get_softc(dev);
 	uint16_t status;
@@ -72,11 +131,11 @@ HAIKU_CHECK_DISABLE_INTERRUPTS(device_t dev)
 }
 
 
-void
-HAIKU_REENABLE_INTERRUPTS(device_t dev)
+void reenable_interrupts_dc(device_t dev)
 {
 	struct dc_softc *sc = device_get_softc(dev);
 	DC_LOCK(sc);
 	CSR_WRITE_4(sc, DC_IMR, DC_INTRS);
 	DC_UNLOCK(sc);
 }
+
