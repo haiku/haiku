d1e1709087f986fa437f3db30d0beff372fd6864
Siarzhuk Zharski <zharik@gmx.li>
2011-08-23 18:54:25 +0000

    * ipro1000 driver updated to FreeBSD 8.2 level:   it incorporate now "em" and "lem" freebsd drivers.   Note: if_igb is not used - too much "undeclared"   stuff required from the compatibility layer. :-(
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@42680 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/add-ons/kernel/drivers/network/ipro1000/dev/e1000/glue.c
+++ src/add-ons/kernel/drivers/network/ipro1000/dev/e1000/glue.c
@@ -1,10 +1,27 @@
 #include <sys/bus.h>
 
-HAIKU_FBSD_DRIVER_GLUE(ipro1000, em, pci)
+
+extern driver_t *DRIVER_MODULE_NAME(em, pci);
+extern driver_t *DRIVER_MODULE_NAME(lem, pci);
+
+HAIKU_FBSD_DRIVERS_GLUE(ipro1000);
 
 NO_HAIKU_CHECK_DISABLE_INTERRUPTS();
 NO_HAIKU_REENABLE_INTERRUPTS();
 NO_HAIKU_FBSD_MII_DRIVER();
+
+
+status_t
+__haiku_handle_fbsd_drivers_list(status_t (*handler)(driver_t *[]))
+{
+	driver_t *drivers[] = {
+		DRIVER_MODULE_NAME(em, pci),
+		DRIVER_MODULE_NAME(lem, pci),
+		NULL
+	};
+	return (*handler)(drivers);
+}
+
 
 #ifdef EM_FAST_INTR
 	HAIKU_DRIVER_REQUIREMENTS(FBSD_TASKQUEUES | FBSD_FAST_TASKQUEUE);
