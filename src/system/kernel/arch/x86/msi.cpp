20f094cc38adc63ae5b19b07bb90863734012f02
Michael Lotz <mmlr@mlotz.ch>
2011-10-12 21:06:21 +0000

    Fix and add copyright year spotted by Urias.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@42833 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/system/kernel/arch/x86/msi.cpp
+++ src/system/kernel/arch/x86/msi.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 20010, Michael Lotz, mmlr@mlotz.ch. All Rights Reserved.
+ * Copyright 2010-2011, Michael Lotz, mmlr@mlotz.ch. All Rights Reserved.
  * Distributed under the terms of the MIT license.
  */
 
