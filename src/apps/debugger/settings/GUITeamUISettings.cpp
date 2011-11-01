4b38291dbcc6c82709a2f470efa7e3194291c4ab
Rene Gollent <anevilyak@gmail.com>
2011-11-01 19:10:21 +0000

    Style fixes.
    git-svn-id: file:///srv/svn/repos/haiku/haiku/trunk@43075 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- src/apps/debugger/settings/GUITeamUISettings.cpp
+++ src/apps/debugger/settings/GUITeamUISettings.cpp
@@ -2,6 +2,8 @@
  * Copyright 2011, Rene Gollent, rene@gollent.com.
  * Distributed under the terms of the MIT License.
  */
+
+
 #include "GUITeamUISettings.h"
 
 #include <Message.h>
--- src/apps/debugger/settings/TeamUISettings.cpp
+++ src/apps/debugger/settings/TeamUISettings.cpp
@@ -2,6 +2,8 @@
  * Copyright 2011, Rene Gollent, rene@gollent.com.
  * Distributed under the terms of the MIT License.
  */
+
+
 #include "TeamUISettings.h"
 
 
