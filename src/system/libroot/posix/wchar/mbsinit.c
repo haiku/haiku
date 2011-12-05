cf13327bef173d8476f8ee22bb6534c2890cba4b
Oliver Tappe <zooey@hirschkaefer.de>
2011-12-06 00:06:50 +0100

    Whitespace cleanup.

--- src/system/libroot/posix/wchar/mbsinit.c
+++ src/system/libroot/posix/wchar/mbsinit.c
@@ -2,15 +2,15 @@
 ** Copyright 2011, Oliver Tappe, zooey@hirschkaefer.de. All rights reserved.
 ** Distributed under the terms of the Haiku License.
 */
- 
+
 #include <wchar_private.h>
- 
- 
- int
+
+
+int
 __mbsinit(const mbstate_t* ps)
- {
+{
 	return ps == NULL || ps->count == 0;
- }
+}
 
 
 B_DEFINE_WEAK_ALIAS(__mbsinit, mbsinit);
