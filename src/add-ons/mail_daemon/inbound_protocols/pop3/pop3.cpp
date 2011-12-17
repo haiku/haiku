f0389daa940ba14fa0828e8d6c1bb3bf8d5dc357
czeidler <haiku@clemens-zeidler.de>
2011-12-17 14:35:22 +1300

    Fix comment and add a TODO.

--- src/add-ons/mail_daemon/inbound_protocols/pop3/pop3.cpp
+++ src/add-ons/mail_daemon/inbound_protocols/pop3/pop3.cpp
@@ -608,10 +608,12 @@ POP3Protocol::CheckForDeletedMessages()
 		Delete(fUniqueIDs.IndexOf(toDelete.StringAt(i)));
 	}
 
-	//*(unique_ids) -= to_delete; --- This line causes bad things to
-	// happen (POP3 client uses the wrong indices to retrieve
-	// messages).  Without it, bad things don't happen.
+	// Don't remove ids from fUniqueIDs, the indices have to stay the same when
+	// retrieving new messages.
 	fManifest.Remove(toDelete);
+
+	// TODO: at some point the purged manifest should be written to disk
+	// otherwise it will grow forever
 }
 
 
