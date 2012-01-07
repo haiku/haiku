b7417fbec1977006971f93c91a288138a9462e5a
Oliver Tappe <zooey@hirschkaefer.de>
2012-01-07 22:02:48 +0100

    Switch wchar from glibc to our own implementations.

--- headers/posix/wchar.h
+++ headers/posix/wchar.h
@@ -51,76 +51,103 @@ extern "C" {
 
 extern wint_t	btowc(int);
 
-extern wint_t	fgetwc(FILE *);
-extern wchar_t	*fgetws(wchar_t *, int, FILE *);
-extern wint_t	fputwc(wchar_t, FILE *);
-extern int		fputws(const wchar_t *, FILE *);
-extern int		fwide(FILE *, int);
-extern int		fwprintf(FILE *, const wchar_t *, ...);
-extern int		fwscanf(FILE *, const wchar_t *, ...);
-extern wint_t	getwc(FILE *);
+extern wint_t	fgetwc(FILE *stream);
+extern wchar_t	*fgetws(wchar_t *dest, int destLength, FILE *stream);
+extern wint_t	fputwc(wchar_t wc, FILE *stream);
+extern int		fputws(const wchar_t *wcs, FILE *stream);
+extern int		fwide(FILE *stream, int mode);
+extern int		fwprintf(FILE *stream, const wchar_t *format, ...);
+extern int		fwscanf(FILE *stream, const wchar_t *format, ...);
+extern wint_t	getwc(FILE *stream);
 extern wint_t	getwchar(void);
 
-extern size_t 	mbrlen(const char *s, size_t n, mbstate_t *ps);
-extern size_t 	mbrtowc(wchar_t *pwc, const char *s, size_t n, mbstate_t *ps);
-extern int		mbsinit(const mbstate_t *);
-extern size_t	mbsrtowcs(wchar_t *dst, const char **src, size_t len,
-					mbstate_t *ps);
+extern size_t 	mbrlen(const char *s, size_t n, mbstate_t *mbState);
+extern size_t 	mbrtowc(wchar_t *dest, const char *src, size_t destLength,
+					mbstate_t *mbState);
+extern int		mbsinit(const mbstate_t *mbState);
+extern size_t	mbsnrtowcs(wchar_t *dest, const char **src, size_t srcLength,
+					size_t destLength, mbstate_t *mbState);
+extern size_t	mbsrtowcs(wchar_t *dest, const char **src, size_t destLength,
+					mbstate_t *mbState);
 
-extern wint_t	putwc(wchar_t, FILE *);
-extern wint_t	putwchar(wchar_t);
+extern wint_t	putwc(wchar_t wc, FILE *stream);
+extern wint_t	putwchar(wchar_t wc);
 
-extern int		swprintf(wchar_t *, size_t, const wchar_t *, ...);
-extern int		swscanf(const wchar_t *, const wchar_t *, ...);
+extern int		swprintf(wchar_t *dest, size_t destLength,
+					const wchar_t *format, ...);
+extern int		swscanf(const wchar_t *wcs, const wchar_t *format, ...);
 
-extern wint_t	ungetwc(wint_t, FILE *);
+extern wint_t	ungetwc(wint_t wc, FILE *stream);
 
-extern int		vfwprintf(FILE *, const wchar_t *, va_list);
-extern int		vfwscanf(FILE *, const wchar_t *, va_list);
-extern int		vswprintf(wchar_t *, size_t, const wchar_t *, va_list);
-extern int		vswscanf(const wchar_t *, const wchar_t *, va_list);
-extern int		vwprintf(const wchar_t *, va_list);
-extern int		vwscanf(const wchar_t *, va_list);
+extern int		vfwprintf(FILE *stream, const wchar_t *format, va_list arg);
+extern int		vfwscanf(FILE *stream, const wchar_t *format, va_list arg);
+extern int		vswprintf(wchar_t *dest, size_t destLength,
+					const wchar_t *format, va_list arg);
+extern int		vswscanf(const wchar_t *wcs, const wchar_t *format,
+					va_list arg);
+extern int		vwprintf(const wchar_t *format, va_list arg);
+extern int		vwscanf(const wchar_t *format, va_list arg);
 
-extern size_t   wcrtomb(char *, wchar_t, mbstate_t *);
-extern wchar_t	*wcscat(wchar_t *, const wchar_t *);
-extern wchar_t	*wcschr(const wchar_t *, wchar_t);
-extern int      wcscmp(const wchar_t *ws1, const wchar_t *ws2);
-extern int      wcscoll(const wchar_t *ws1, const wchar_t *ws2);
-extern wchar_t	*wcscpy(wchar_t *, const wchar_t *);
-extern size_t	wcscspn(const wchar_t *, const wchar_t *);
-extern wchar_t	*wcsdup(const wchar_t *);
-extern size_t	wcsftime(wchar_t *, size_t, const wchar_t *, const struct tm *);
-extern size_t	wcslen(const wchar_t *);
-extern wchar_t 	*wcsncat(wchar_t *, const wchar_t *, size_t);
-extern int		wcsncmp(const wchar_t *, const wchar_t *, size_t);
-extern wchar_t	*wcsncpy(wchar_t *, const wchar_t *, size_t);
-extern wchar_t	*wcspbrk(const wchar_t *, const wchar_t *);
-extern wchar_t	*wcsrchr(const wchar_t *, wchar_t);
-extern size_t   wcsrtombs(char *dst, const wchar_t **src, size_t len,
-					mbstate_t *ps);
-extern size_t	wcsspn(const wchar_t *, const wchar_t *);
-extern wchar_t	*wcsstr(const wchar_t *, const wchar_t *);
-extern double	wcstod(const wchar_t *, wchar_t **);
-extern float	wcstof(const wchar_t *, wchar_t **);
-extern wchar_t	*wcstok(wchar_t *, const wchar_t *, wchar_t **);
-extern long		wcstol(const wchar_t *, wchar_t **, int);
-extern long double			wcstold(const wchar_t *, wchar_t **);
-extern long long			wcstoll(const wchar_t *, wchar_t **, int);
-extern unsigned long		wcstoul(const wchar_t *, wchar_t **, int);
-extern unsigned long long	wcstoull(const wchar_t *, wchar_t **, int);
-extern wchar_t	*wcswcs(const wchar_t *, const wchar_t *);
-extern int		wcswidth(const wchar_t *, size_t);
-extern size_t	wcsxfrm(wchar_t *, const wchar_t *, size_t);
-extern int		wctob(wint_t);
-extern int		wcwidth(wchar_t);
-extern wchar_t	*wmemchr(const wchar_t *, wchar_t, size_t);
-extern int		wmemcmp(const wchar_t *, const wchar_t *, size_t);
-extern wchar_t	*wmemcpy(wchar_t *, const wchar_t *, size_t);
-extern wchar_t	*wmemmove(wchar_t *, const wchar_t *, size_t);
-extern wchar_t	*wmemset(wchar_t *, wchar_t, size_t);
-extern int		wprintf(const wchar_t *, ...);
-extern int		wscanf(const wchar_t *, ...);
+extern wchar_t	*wcpcpy(wchar_t *dest, const wchar_t *src);
+extern wchar_t	*wcpncpy(wchar_t *dest, const wchar_t *src, size_t srcLength);
+extern size_t   wcrtomb(char *dest, wchar_t wc, mbstate_t *mbState);
+extern int		wcscasecmp(const wchar_t *wcs1, const wchar_t *wcs2);
+extern wchar_t	*wcscat(wchar_t *dest, const wchar_t *src);
+extern wchar_t	*wcschr(const wchar_t *wcs, wchar_t wc);
+#ifdef __USE_GNU
+extern wchar_t	*wcschrnul(const wchar_t *wcs, wchar_t wc);
+#endif
+extern int      wcscmp(const wchar_t *wcs1, const wchar_t *wcs2);
+extern int      wcscoll(const wchar_t *wcs1, const wchar_t *wcs2);
+extern wchar_t	*wcscpy(wchar_t *dest, const wchar_t *src);
+extern size_t	wcscspn(const wchar_t *wcs, const wchar_t *reject);
+extern wchar_t	*wcsdup(const wchar_t *wcs);
+extern size_t	wcsftime(wchar_t *dest, size_t destLength,
+					const wchar_t *format, const struct tm *time);
+extern size_t	wcslcat(wchar_t *dest, const wchar_t *src, size_t maxLength);
+extern size_t	wcslcpy(wchar_t *dest, const wchar_t *src, size_t maxLength);
+extern size_t	wcslen(const wchar_t *wcs);
+extern int		wcsncasecmp(const wchar_t *wcs1, const wchar_t *wcs2,
+					size_t maxLength);
+extern wchar_t 	*wcsncat(wchar_t *dest, const wchar_t *src, size_t srcLength);
+extern int		wcsncmp(const wchar_t *wcs1, const wchar_t *wcs2,
+					size_t length);
+extern wchar_t	*wcsncpy(wchar_t *dest, const wchar_t *src, size_t srcLength);
+extern size_t	wcsnlen(const wchar_t *wcs, size_t maxLength);
+extern size_t   wcsnrtombs(char *dest, const wchar_t **src, size_t srcLength,
+					size_t destLength, mbstate_t *mbState);
+extern wchar_t	*wcspbrk(const wchar_t *wcs, const wchar_t *accept);
+extern wchar_t	*wcsrchr(const wchar_t *wcs, wchar_t wc);
+extern size_t   wcsrtombs(char *dest, const wchar_t **src, size_t destLength,
+					mbstate_t *mbState);
+extern size_t	wcsspn(const wchar_t *wcs, const wchar_t *accept);
+extern wchar_t	*wcsstr(const wchar_t *haystack, const wchar_t *needle);
+extern double	wcstod(const wchar_t *wcs, wchar_t **endPtr);
+extern float	wcstof(const wchar_t *wcs, wchar_t **endPtr);
+extern wchar_t	*wcstok(wchar_t *wcs1, const wchar_t *wcs2, wchar_t **ptr);
+extern long		wcstol(const wchar_t *wcs, wchar_t **endPtr, int base);
+extern long double	wcstold(const wchar_t *wcs, wchar_t **endPtr);
+extern long long	wcstoll(const wchar_t *wcs, wchar_t **endPtr, int base);
+extern unsigned long	wcstoul(const wchar_t *wcs, wchar_t **endPtr,
+							int base);
+extern unsigned long long	wcstoull(const wchar_t *wcs, wchar_t **endPtr,
+								int base);
+extern wchar_t	*wcswcs(const wchar_t *haystack, const wchar_t *needle);
+extern int		wcswidth(const wchar_t *wcs, size_t length);
+extern size_t	wcsxfrm(wchar_t *dest, const wchar_t *src, size_t destLength);
+extern int		wctob(wint_t wc);
+extern int		wcwidth(wchar_t wc);
+extern wchar_t	*wmemchr(const wchar_t *wcs, wchar_t wc, size_t n);
+extern int		wmemcmp(const wchar_t *wcs1, const wchar_t *wcs2, size_t n);
+extern wchar_t	*wmemcpy(wchar_t *dest, const wchar_t *src, size_t n);
+extern wchar_t	*wmemmove(wchar_t *dest, const wchar_t *src, size_t n);
+#ifdef __USE_GNU
+extern wchar_t	*wmempcpy(wchar_t *dest, const wchar_t *src, size_t n);
+#endif
+extern wchar_t	*wmemset(wchar_t *dest, wchar_t wc, size_t n);
+
+extern int		wprintf(const wchar_t *format, ...);
+extern int		wscanf(const wchar_t *format, ...);
 
 #ifdef __cplusplus
 }
--- headers/private/libroot/wchar_private.h
+++ headers/private/libroot/wchar_private.h
@@ -15,57 +15,75 @@
 __BEGIN_DECLS
 
 
-extern wint_t	__btowc(int);
+extern wint_t	__btowc(int c);
 
-extern int		__mblen(const char *string, size_t maxSize);
-extern size_t 	__mbrlen(const char *s, size_t n, mbstate_t *ps);
-extern size_t 	__mbrtowc(wchar_t *pwc, const char *s, size_t n, mbstate_t *ps);
-extern int		__mbsinit(const mbstate_t *);
-extern size_t	__mbsrtowcs(wchar_t *dst, const char **src, size_t len,
-					mbstate_t *ps);
-extern size_t	__mbstowcs(wchar_t *pwcs, const char *string, size_t maxSize);
-extern int		__mbtowc(wchar_t *pwc, const char *string, size_t maxSize);
+extern int		__mblen(const char *string, size_t length);
+extern size_t 	__mbrlen(const char *s, size_t n, mbstate_t *mbState);
+extern size_t 	__mbrtowc(wchar_t *dest, const char *src, size_t destLength,
+					mbstate_t *mbState);
+extern int		__mbsinit(const mbstate_t *mbState);
+extern size_t	__mbsnrtowcs(wchar_t *dest, const char **src, size_t srcLength,
+					size_t destLength, mbstate_t *mbState);
+extern size_t	__mbsrtowcs(wchar_t *dest, const char **src, size_t destLength,
+					mbstate_t *mbState);
+extern size_t	__mbstowcs(wchar_t *dest, const char *src, size_t destLength);
+extern int		__mbtowc(wchar_t *dest, const char *src, size_t destLength);
 
-extern size_t   __wcrtomb(char *, wchar_t, mbstate_t *);
-extern wchar_t	*__wcscat(wchar_t *, const wchar_t *);
-extern wchar_t	*__wcschr(const wchar_t *, wchar_t);
-extern int      __wcscmp(const wchar_t *ws1, const wchar_t *ws2);
-extern int      __wcscoll(const wchar_t *ws1, const wchar_t *ws2);
-extern wchar_t	*__wcscpy(wchar_t *, const wchar_t *);
-extern size_t	__wcscspn(const wchar_t *, const wchar_t *);
-extern wchar_t	*__wcsdup(const wchar_t *);
-extern size_t	__wcsftime(wchar_t *, size_t, const wchar_t *,
-					const struct tm *);
-extern size_t	__wcslen(const wchar_t *);
-extern wchar_t 	*__wcsncat(wchar_t *, const wchar_t *, size_t);
-extern int		__wcsncmp(const wchar_t *, const wchar_t *, size_t);
-extern wchar_t	*__wcsncpy(wchar_t *, const wchar_t *, size_t);
-extern wchar_t	*__wcspbrk(const wchar_t *, const wchar_t *);
-extern wchar_t	*__wcsrchr(const wchar_t *, wchar_t);
-extern size_t   __wcsrtombs(char *dst, const wchar_t **src, size_t len,
-					mbstate_t *ps);
-extern size_t	__wcsspn(const wchar_t *, const wchar_t *);
-extern wchar_t	*__wcsstr(const wchar_t *, const wchar_t *);
-extern double	__wcstod(const wchar_t *, wchar_t **);
-extern float	__wcstof(const wchar_t *, wchar_t **);
-extern wchar_t	*__wcstok(wchar_t *, const wchar_t *, wchar_t **);
-extern long		__wcstol(const wchar_t *, wchar_t **, int);
-extern long double			__wcstold(const wchar_t *, wchar_t **);
-extern long long			__wcstoll(const wchar_t *, wchar_t **, int);
-extern unsigned long		__wcstoul(const wchar_t *, wchar_t **, int);
-extern unsigned long long	__wcstoull(const wchar_t *, wchar_t **, int);
-extern size_t	__wcstombs(char *string, const wchar_t *pwcs, size_t maxSize);
-extern int		__wctomb(char *string, wchar_t wchar);
-extern wchar_t	*__wcswcs(const wchar_t *, const wchar_t *);
-extern int		__wcswidth(const wchar_t *, size_t);
-extern size_t	__wcsxfrm(wchar_t *, const wchar_t *, size_t);
-extern int		__wctob(wint_t);
-extern int		__wcwidth(wchar_t);
-extern wchar_t	*__wmemchr(const wchar_t *, wchar_t, size_t);
-extern int		__wmemcmp(const wchar_t *, const wchar_t *, size_t);
-extern wchar_t	*__wmemcpy(wchar_t *, const wchar_t *, size_t);
-extern wchar_t	*__wmemmove(wchar_t *, const wchar_t *, size_t);
-extern wchar_t	*__wmemset(wchar_t *, wchar_t, size_t);
+extern wchar_t	*__wcpcpy(wchar_t *dest, const wchar_t *src);
+extern wchar_t	*__wcpncpy(wchar_t *dest, const wchar_t *src, size_t srcLength);
+extern size_t   __wcrtomb(char *dest, wchar_t wc, mbstate_t *mbState);
+extern int		__wcscasecmp(const wchar_t *wcs1, const wchar_t *wcs2);
+extern wchar_t	*__wcscat(wchar_t *dest, const wchar_t *src);
+extern wchar_t	*__wcschr(const wchar_t *wcs, wchar_t wc);
+extern wchar_t	*__wcschrnul(const wchar_t *wcs, wchar_t wc);
+extern int      __wcscmp(const wchar_t *wcs1, const wchar_t *wcs2);
+extern int      __wcscoll(const wchar_t *wcs1, const wchar_t *wcs2);
+extern wchar_t	*__wcscpy(wchar_t *dest, const wchar_t *src);
+extern size_t	__wcscspn(const wchar_t *wcs, const wchar_t *reject);
+extern wchar_t	*__wcsdup(const wchar_t *wcs);
+extern size_t	__wcsftime(wchar_t *dest, size_t destLength,
+					const wchar_t *format, const struct tm *time);
+extern size_t	__wcslcat(wchar_t *dest, const wchar_t *src, size_t maxLength);
+extern size_t	__wcslcpy(wchar_t *dest, const wchar_t *src, size_t maxLength);
+extern size_t	__wcslen(const wchar_t *wcs);
+extern int		__wcsncasecmp(const wchar_t *wcs1, const wchar_t *wcs2,
+					size_t maxLength);
+extern wchar_t 	*__wcsncat(wchar_t *dest, const wchar_t *src, size_t srcLength);
+extern int		__wcsncmp(const wchar_t *wcs1, const wchar_t *wcs2,
+					size_t length);
+extern wchar_t	*__wcsncpy(wchar_t *dest, const wchar_t *src, size_t srcLength);
+extern size_t	__wcsnlen(const wchar_t *wcs, size_t maxLength);
+extern size_t   __wcsnrtombs(char *dest, const wchar_t **src, size_t srcLength,
+					size_t destLength, mbstate_t *mbState);
+extern wchar_t	*__wcspbrk(const wchar_t *wcs, const wchar_t *accept);
+extern wchar_t	*__wcsrchr(const wchar_t *wcs, wchar_t wc);
+extern size_t   __wcsrtombs(char *dest, const wchar_t **src, size_t destLength,
+					mbstate_t *mbState);
+extern size_t	__wcsspn(const wchar_t *wcs, const wchar_t *accept);
+extern wchar_t	*__wcsstr(const wchar_t *haystack, const wchar_t *needle);
+extern double	__wcstod(const wchar_t *wcs, wchar_t **endPtr);
+extern float	__wcstof(const wchar_t *wcs, wchar_t **endPtr);
+extern wchar_t	*__wcstok(wchar_t *wcs1, const wchar_t *wcs2, wchar_t **ptr);
+extern long		__wcstol(const wchar_t *wcs, wchar_t **endPtr, int base);
+extern long double	__wcstold(const wchar_t *wcs, wchar_t **endPtr);
+extern long long	__wcstoll(const wchar_t *wcs, wchar_t **endPtr, int base);
+extern unsigned long	__wcstoul(const wchar_t *wcs, wchar_t **endPtr,
+							int base);
+extern unsigned long long	__wcstoull(const wchar_t *wcs, wchar_t **endPtr,
+								int base);
+extern size_t	__wcstombs(char *string, const wchar_t *wcs, size_t maxSize);
+extern int		__wctomb(char *string, wchar_t wc);
+extern wchar_t	*__wcswcs(const wchar_t *haystack, const wchar_t *needle);
+extern int		__wcswidth(const wchar_t *wcs, size_t length);
+extern size_t	__wcsxfrm(wchar_t *dest, const wchar_t *src, size_t destLength);
+extern int		__wctob(wint_t wc);
+extern int		__wcwidth(wchar_t wc);
+extern wchar_t	*__wmemchr(const wchar_t *wcs, wchar_t wc, size_t n);
+extern int		__wmemcmp(const wchar_t *wcs1, const wchar_t *wcs2, size_t n);
+extern wchar_t	*__wmemcpy(wchar_t *dest, const wchar_t *src, size_t n);
+extern wchar_t	*__wmempcpy(wchar_t *dest, const wchar_t *src, size_t n);
+extern wchar_t	*__wmemmove(wchar_t *dest, const wchar_t *src, size_t n);
+extern wchar_t	*__wmemset(wchar_t *dest, wchar_t wc, size_t n);
 
 
 __END_DECLS
