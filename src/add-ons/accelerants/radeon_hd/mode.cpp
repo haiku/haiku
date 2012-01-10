haiku/haiku

commit 4e9e029551cb5459078b3097e8e2073b814f5e5f

Set encoder dpms on dpms, it just feels right

src/add-ons/accelerants/radeon_hd/mode.cpp
@@ -133,6 +133,7 @@ radeon_dpms_set(int mode)
 			for (uint8 id = 0; id < MAX_DISPLAY; id++) {
 				if (gDisplay[id]->active == false)
 					continue;
+				encoder_dpms_set(id, mode);
 				display_crtc_lock(id, ATOM_ENABLE);
 				display_crtc_power(id, ATOM_ENABLE);
 				if (info.dceMajor >= 3)
@@ -154,6 +155,7 @@ radeon_dpms_set(int mode)
 					display_crtc_memreq(id, ATOM_DISABLE);
 				display_crtc_power(id, ATOM_DISABLE);
 				display_crtc_lock(id, ATOM_DISABLE);
+				encoder_dpms_set(id, mode);
 			}
 			break;
 	}

commit 7dde214bc0f1f3ec2417b8e1ae6e07733d79829e

radeon_hd: Fix accelerant regression in 4e9e029
* After 4e9e029, radeon_hd functionality became erratic.
* I think failing to lock the encoder bios scratch on
  encoder changes was causing it.

src/add-ons/accelerants/radeon_hd/mode.cpp
@@ -133,7 +133,9 @@ radeon_dpms_set(int mode)
 			for (uint8 id = 0; id < MAX_DISPLAY; id++) {
 				if (gDisplay[id]->active == false)
 					continue;
+				encoder_output_lock(true);
 				encoder_dpms_set(id, mode);
+				encoder_output_lock(false);
 				display_crtc_lock(id, ATOM_ENABLE);
 				display_crtc_power(id, ATOM_ENABLE);
 				if (info.dceMajor >= 3)
@@ -155,7 +157,9 @@ radeon_dpms_set(int mode)
 					display_crtc_memreq(id, ATOM_DISABLE);
 				display_crtc_power(id, ATOM_DISABLE);
 				display_crtc_lock(id, ATOM_DISABLE);
+				encoder_output_lock(true);
 				encoder_dpms_set(id, mode);
+				encoder_output_lock(false);
 			}
 			break;
 	}