c72a1d4e6a1077b18592ff3ac7c26e9de868a641
John Scipione <jscipione@gmail.com>
2012-03-01 15:19:47 -0500

    Don't return 0 for the unimplemented *at() functions, set errno to ENOSYS (not implemented) and return -1 instead.

--- src/build/libroot/fs_darwin.cpp
+++ src/build/libroot/fs_darwin.cpp
@@ -18,89 +18,102 @@
 int
 faccessat(int fd, const char* path, int accessMode, int flag)
 {
-	return 0;
+	errno = ENOSYS;
+	return -1;
 }
 
 
 int
 fchmodat(int fd, const char* path, mode_t mode, int flag)
 {
-	return 0;
+	errno = ENOSYS;
+	return -1;
 }
 
 
 int
 fchownat(int fd, const char* path, uid_t owner, gid_t group, int flag)
 {
-	return 0;
+	errno = ENOSYS;
+	return -1;
 }
 
 
 int
 fstatat(int fd, const char *path, struct stat *st, int flag)
 {
-	return 0;
+	errno = ENOSYS;
+	return -1;
 }
 
 
 int
 mkdirat(int fd, const char *path, mode_t mode)
 {
-	return 0;
+	errno = ENOSYS;
+	return -1;
 }
 
 
 int
 mkfifoat(int fd, const char *path, mode_t mode)
 {
-	return 0;
+	errno = ENOSYS;
+	return -1;
 }
 
 
 int
 mknodat(int fd, const char *name, mode_t mode, dev_t dev)
 {
-	return 0;
+	errno = ENOSYS;
+	return -1;
 }
 
 
 int
 renameat(int fromFD, const char* from, int toFD, const char* to)
 {
-	return 0;
+	errno = ENOSYS;
+	return -1;
 }
 
 
 ssize_t
 readlinkat(int fd, const char *path, char *buffer, size_t bufferSize)
 {
-	return 0;
+	errno = ENOSYS;
+	return -1;
 }
 
 
 int
 symlinkat(const char *toPath, int fd, const char *symlinkPath)
 {
-	return 0;
+	errno = ENOSYS;
+	return -1;
 }
 
 
 int
 unlinkat(int fd, const char *path, int flag)
 {
-	return 0;
+	errno = ENOSYS;
+	return -1;
 }
 
 
 int
 linkat(int toFD, const char *toPath, int linkFD, const char *linkPath, int flag)
 {
-	return 0;
+	errno = ENOSYS;
+	return -1;
 }
 
 
 int
 futimesat(int fd, const char *path, const struct timeval times[2])
 {
-	return 0;
+	errno = ENOSYS;
+	return -1;
 }
