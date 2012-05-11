0fa5c3d11512e966045bea0e129da6679e8da7b0
François Revol <revol@free.fr>
2012-05-11 02:31:57 +0200

    Add board config header for ACube Sam460ex
    * the UART is hardcoded for now, ideally it should be set up from the address found in the FDT blob.

---
+++ headers/private/kernel/arch/ppc/board/sam460ex/board_config.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2012 Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		François Revol, revol@free.fr
+ */
+#ifndef _BOARD_SAM460EX_BOARD_CONFIG_H
+#define _BOARD_SAM460EX_BOARD_CONFIG_H
+
+
+#define BOARD_NAME_PRETTY "ACube Sam460ex"
+
+// UART Settings
+// TODO: use the FDT instead of hardcoding
+#define BOARD_UART1_BASE 0xef600300
+#define BOARD_UART2_BASE 0
+#define BOARD_UART3_BASE 0
+
+#define BOARD_UART_DEBUG BOARD_UART1_BASE
+
+#define BOARD_UART_CLOCK 0
+	/* ?Mhz */
+
+
+#endif /* _BOARD_SAM460EX_BOARD_CONFIG_H */
