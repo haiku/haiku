f50e7b8dc594beaaabf630f212852bb983882ddd
Stephan Aßmus <superstippi@gmx.de>
2012-05-12 13:48:54 +0200

    Cleanup & small fixes  * Style cleanup  * Don't delete message in case they happen to be the passed in pointers.

--- src/apps/icon-o-matic/generic/gui/SwatchView.h
+++ src/apps/icon-o-matic/generic/gui/SwatchView.h
@@ -1,63 +1,47 @@
 /*
- * Copyright 2006, Haiku.
+ * Copyright 2006-2012, Haiku.
  * Distributed under the terms of the MIT License.
  *
  * Authors:
  *		Stephan Aßmus <superstippi@gmx.de>
  */
-
 #ifndef SWATCH_VIEW_H
 #define SWATCH_VIEW_H
 
 #include <View.h>
 
-#if LIB_LAYOUT
-#include <layout.h>
-#endif
 
-class SwatchView :
-				   #if LIB_LAYOUT
-				   public MView,
-				   #endif
-				   public BView {
- public:
-								SwatchView(const char* name,
-										   BMessage* message,
-										   BHandler* target,
-										   rgb_color color,
-										   float width = 24.0,
-										   float height = 24.0);
+class SwatchView : public BView {
+public:
+								SwatchView(const char* name, BMessage* message,
+									BHandler* target, rgb_color color,
+									float width = 24.0, float height = 24.0);
 	virtual						~SwatchView();
 
-	#if LIB_LAYOUT
-								// MView
-	virtual	minimax				layoutprefs();
-	virtual	BRect				layout(BRect frame);
-	#endif
-
-								// BView
+	// BView
 	virtual	void				Draw(BRect updateRect);
 	virtual	void				MessageReceived(BMessage* message);
 
 	virtual	void				MouseDown(BPoint where);
 	virtual	void				MouseUp(BPoint where);
 	virtual	void				MouseMoved(BPoint where, uint32 transit,
-										   const BMessage* dragMessage);
+									const BMessage* dragMessage);
 
-								// SwatchView
+	// SwatchView
 			void				SetColor(rgb_color color);
-			rgb_color			Color() const
+	inline	rgb_color			Color() const
 									{ return fColor; }
 
 			void				SetClickedMessage(BMessage* message);
 			void				SetDroppedMessage(BMessage* message);
 
- private:
+private:
 			void				_Invoke(const BMessage* message);
 			void				_StrokeRect(BRect frame, rgb_color leftTop,
-										   rgb_color rightBottom);
+									rgb_color rightBottom);
 			void				_DragColor();
 
+private:
 			rgb_color			fColor;
 			BPoint				fTrackingStart;
 			bool				fActive;
