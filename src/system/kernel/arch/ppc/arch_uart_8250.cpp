3bd0ac4aa39e077d335e787dbe7a8dc0c04f247d
François Revol <revol@free.fr>
2012-05-24 21:18:24 +0200

    PPC: Add eioio as barrier for the UART class
    * probably unneeded but it shouldn't harm.

--- src/system/kernel/arch/ppc/arch_uart_8250.cpp
+++ src/system/kernel/arch/ppc/arch_uart_8250.cpp
@@ -5,15 +5,41 @@
 
 #include <debug.h>
 #include <arch/generic/debug_uart_8250.h>
+#include <arch/ppc/arch_cpu.h>
 #include <new>
 
 
-// we shouldn't need special setup, we use plain MMIO.
+class ArchUART8250 : public DebugUART8250 {
+public:
+							ArchUART8250(addr_t base, int64 clock);
+							~ArchUART8250();
+
+	virtual void			Barrier();
+};
+
+
+ArchUART8250::ArchUART8250(addr_t base, int64 clock)
+	: DebugUART8250(base, clock)
+{
+}
+
+
+ArchUART8250::~ArchUART8250()
+{
+}
+
+
+void
+ArchUART8250::Barrier()
+{
+	eieio();
+}
+
 
 DebugUART8250 *arch_get_uart_8250(addr_t base, int64 clock)
 {
-	static char buffer[sizeof(DebugUART8250)];
-	DebugUART8250 *uart = new(buffer) DebugUART8250(base, clock);
+	static char buffer[sizeof(ArchUART8250)];
+	ArchUART8250 *uart = new(buffer) ArchUART8250(base, clock);
 	return uart;
 }
 
