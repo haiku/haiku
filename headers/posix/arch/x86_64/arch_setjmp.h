73f27ecd3a30c16c2363de51891534c09601ea6b
Alex Smith <alex@alex-smith.me.uk>
2012-06-15 19:57:21 +0100

    Implementations of some libroot functions for x86_64.
    These are the functions required by the kernel. These are all full
    implementations except for system_time(), which will be implemented later.

--- headers/posix/arch/x86_64/arch_setjmp.h
+++ headers/posix/arch/x86_64/arch_setjmp.h
@@ -1,12 +1,11 @@
 /*
- * Copyright 2005-2010 Haiku Inc. All Rights Reserved.
+ * Copyright 2005-2012 Haiku Inc. All Rights Reserved.
  * Distributed under the terms of the MIT License.
  */
 #ifndef _ARCH_SETJMP_H_
 #define _ARCH_SETJMP_H_
 
 
-/* TODO: A jmp_buf size of 12 might not be large enough. Increase to a large size if needed. */
-typedef int __jmp_buf[12];
+typedef int __jmp_buf[8];
 
 #endif	/* _ARCH_SETJMP_H_ */
