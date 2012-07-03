c005e747ef9ea6ba091c72e3fa56f8318a4bf893
Alex Smith <alex@alex-smith.me.uk>
2012-07-03 11:17:56 +0100

    Removed the x86_64 headers/source directories, now all merged with x86.

--- headers/private/kernel/arch/x86_64/arch_user_debugger.h
+++
@@ -1,6 +0,0 @@
-#ifndef _KERNEL_ARCH_X86_64_USER_DEBUGGER_H
-#define _KERNEL_ARCH_X86_64_USER_DEBUGGER_H
-
-#include "../x86/arch_user_debugger.h"
-
-#endif	/* _KERNEL_ARCH_X86_64_USER_DEBUGGER_H */
--- headers/private/kernel/arch/x86_64/arch_vm_types.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2012, Alex Smith, alex@alex-smith.me.uk.
- * Distributed under the terms of the MIT License.
- */
-#ifndef _KERNEL_ARCH_X86_64_VM_TYPES_H
-#define _KERNEL_ARCH_X86_64_VM_TYPES_H
-
-
-#endif	/* _KERNEL_ARCH_X86_64_VM_TYPES_H */
