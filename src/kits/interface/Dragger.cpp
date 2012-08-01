57e436840b318a2f9ecd6a862ed41bcd6ff0cc23
John Scipione <jscipione@gmail.com>
2012-08-01 07:54:39 -0400

    Revert "Remove the ellipses for About App in the replicant menu."
    Apparently ellipses are used for all dialog boxes on Haiku.
    Sorry for the noise.
    
    Thanks AnEvilYak
    
    This reverts commit 147ef3a9e89c28d64387f4bf645f48453913c3f8.

--- src/kits/interface/Dragger.cpp
+++ src/kits/interface/Dragger.cpp
@@ -740,7 +740,7 @@ BDragger::_BuildDefaultPopUp()
 	if (name != NULL)
 		msg->AddString("target", name);
 
-	BString about(B_TRANSLATE("About %app"));
+	BString about(B_TRANSLATE("About %app" B_UTF8_ELLIPSIS));
 	about.ReplaceFirst("%app", name);
 
 	fPopUp->AddItem(new BMenuItem(about.String(), msg));
