403bd97afc49c077fedeaf1a261332bd1f33656a
Ryan Leavengood <leavengood@gmail.com>
2012-08-20 01:13:19 -0400

    Update the copyright year too.

--- headers/os/BeBuild.h
+++ headers/os/BeBuild.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-2010, Haiku, Inc. All Rights Reserved.
+ * Copyright 2007-2012, Haiku, Inc. All Rights Reserved.
  * Distributed under the terms of the MIT License.
  */
 #ifndef _BE_BUILD_H
