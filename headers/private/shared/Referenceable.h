8358b7807b4597c3afa71ac00d35b6bcb3ff8c09
czeidler <haiku@clemens-zeidler.de>
2012-10-07 10:48:17 +1300

    Fix coding style violations.

--- headers/private/shared/Referenceable.h
+++ headers/private/shared/Referenceable.h
@@ -140,22 +140,22 @@ public:
 
 	bool operator==(const BReference<Type>& other) const
 	{
-		return (fObject == other.fObject);
+		return fObject == other.fObject;
 	}
 
 	bool operator==(const Type* other) const
 	{
-		return (fObject == other);
+		return fObject == other;
 	}
 
 	bool operator!=(const BReference<Type>& other) const
 	{
-		return (fObject != other.fObject);
+		return fObject != other.fObject;
 	}
 
 	bool operator!=(const Type* other) const
 	{
-		return (fObject != other);
+		return fObject != other;
 	}
 
 private:
