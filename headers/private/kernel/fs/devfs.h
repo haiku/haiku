028717d705ae8ceb5d677a6bc74fe8561365ce1b
Axel Dörfler <axeld@pinc-software.de>
2012-10-08 13:58:09 +0200

    Style cleanup.

--- headers/private/kernel/fs/devfs.h
+++ headers/private/kernel/fs/devfs.h
@@ -16,18 +16,18 @@
 extern "C" {
 #endif
 
-status_t devfs_unpublish_file_device(const char *path);
-status_t devfs_publish_file_device(const char *path, const char *filePath);
+status_t devfs_unpublish_file_device(const char* path);
+status_t devfs_publish_file_device(const char* path, const char* filePath);
 
-status_t devfs_unpublish_partition(const char *path);
-status_t devfs_publish_partition(const char *name, const partition_info *info);
-status_t devfs_rename_partition(const char *devicePath, const char *oldName,
-	const char *newName);
+status_t devfs_unpublish_partition(const char* path);
+status_t devfs_publish_partition(const char* name, const partition_info* info);
+status_t devfs_rename_partition(const char* devicePath, const char* oldName,
+	const char* newName);
 
-status_t devfs_unpublish_device(const char *path, bool disconnect);
-status_t devfs_publish_device(const char *path, device_hooks *calls);
-status_t devfs_publish_directory(const char *path);
-status_t devfs_rescan_driver(const char *driverName);
+status_t devfs_unpublish_device(const char* path, bool disconnect);
+status_t devfs_publish_device(const char* path, device_hooks* calls);
+status_t devfs_publish_directory(const char* path);
+status_t devfs_rescan_driver(const char* driverName);
 
 void devfs_compute_geometry_size(device_geometry* geometry, uint64 blockCount,
 	uint32 blockSize);
