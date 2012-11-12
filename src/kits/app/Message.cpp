a2f6e5ac9c3f022f1c2afb29e6cc616e08d3adca
Axel Dörfler <axeld@pinc-software.de>
2012-11-12 23:48:45 +0100

    Added missing BMessage::{Get|Set}String() methods.
    * Forgot to add those before accidentally.

--- src/kits/app/Message.cpp
+++ src/kits/app/Message.cpp
@@ -2564,6 +2564,7 @@ DEFINE_SET_GET_FUNCTIONS(uint64, UInt64, B_UINT64_TYPE);
 DEFINE_SET_GET_FUNCTIONS(bool, Bool, B_BOOL_TYPE);
 DEFINE_SET_GET_FUNCTIONS(float, Float, B_FLOAT_TYPE);
 DEFINE_SET_GET_FUNCTIONS(double, Double, B_DOUBLE_TYPE);
+DEFINE_SET_GET_FUNCTIONS(const char *, String, B_STRING_TYPE);
 
 #undef DEFINE_SET_GET_FUNCTION
 
@@ -3132,6 +3133,13 @@ BMessage::HasFlat(const char *name, int32 index, const BFlattenable *object)
 	const
 {
 	return HasData(name, object->TypeCode(), index);
+}
+
+
+status_t
+BMessage::SetString(const char *name, const BString& value)
+{
+	return SetData(name, B_STRING_TYPE, value.String(), value.Length() + 1);
 }
 
 
