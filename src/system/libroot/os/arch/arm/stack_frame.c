9ffc3c3d8ae7b5f34cb1ee8dc58ec54edb04d7cf
Ithamar R. Adema <ithamar@upgrade-android.com>
2012-11-15 18:41:57 +0100

    ARM/libroot: stub out the missing functions for now
    This will get libroot.so to build, which should give us much more compilation
    coverage for ARM now too. Will have to go through all the libroot code with
    a tooth comb anyway once userland comes up...
    
    (Will consider replacing the glibc mess with bsd for all non gcc2 platforms)

---
+++ src/system/libroot/os/arch/arm/stack_frame.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2012, Haiku, Inc.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		François Revol <revol@free.fr>
+ */
+
+#include <SupportDefs.h>
+
+#include <libroot_private.h>
+
+
+void*
+get_stack_frame(void)
+{
+	// TODO: Implement!
+	return NULL;
+}
+
+
+void*
+__arch_get_caller(void)
+{
+	// TODO: Implement!
+	return NULL;
+}
