c59004fcc35d0fee1a744b26e92de5f71faa00a8
Alexander von Gluck IV <kallisti5@unixzen.com>
2012-11-28 05:00:26 +0000

    swpipe: Fix copyright text in header

--- src/add-ons/opengl/swpipe/SoftwareRenderer.cpp
+++ src/add-ons/opengl/swpipe/SoftwareRenderer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2009, Haiku. All rights reserved.
+ * Copyright 2006-2012, Haiku, Inc. All rights reserved.
  * Distributed under the terms of the MIT License.
  *
  * Authors:
