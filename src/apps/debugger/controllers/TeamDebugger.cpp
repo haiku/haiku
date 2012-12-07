e206ff5692fbe64225d6a4a48f1152462788016b
Rene Gollent <anevilyak@gmail.com>
2012-12-07 17:34:25 -0500

    Fix crash in TeamDebugger.
    - TeamDebugger's destructor was assuming the report generator object
      had always been created, which might not be the case. Fixes second
      part of #9273.

--- src/apps/debugger/controllers/TeamDebugger.cpp
+++ src/apps/debugger/controllers/TeamDebugger.cpp
@@ -277,13 +277,17 @@ TeamDebugger::~TeamDebugger()
 			thread = next;
 		}
 	}
+
+	if (fReportGenerator != NULL) {
+		fReportGenerator->Lock();
+		fReportGenerator->Quit();
+	}
+
 	delete fImageInfoPendingThreads;
 
 	delete fBreakpointManager;
 	delete fWatchpointManager;
 	delete fMemoryBlockManager;
-	fReportGenerator->Lock();
-	fReportGenerator->Quit();
 	delete fWorker;
 	delete fTeam;
 	delete fFileManager;
