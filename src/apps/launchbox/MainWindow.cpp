4d6bb87ea72eebfcda0c1733788503aa91f067f1
Philippe Saint-Pierre <stpere@gmail.com>
2012-12-12 18:03:31 -0500

    LaunchBox: finish saving settings before closing the window (#6347)

--- src/apps/launchbox/MainWindow.cpp
+++ src/apps/launchbox/MainWindow.cpp
@@ -90,19 +90,23 @@ MainWindow::QuitRequested()
 		if (dynamic_cast<MainWindow*>(window))
 			padWindowCount++;
 	}
-	if (padWindowCount == 1) {
-		be_app->PostMessage(B_QUIT_REQUESTED);
-		return false;
-	} else {
-		BAlert* alert = new BAlert(B_TRANSLATE("last chance"),
-			B_TRANSLATE("Really close this pad?\n"
-						"(The pad will not be remembered.)"),
-			B_TRANSLATE("Close"), B_TRANSLATE("Cancel"), NULL);
-		alert->SetShortcut(1, B_ESCAPE);
-		if (alert->Go() == 1)
-			return false;
+	bool canClose = true;
+	if (Lock()) {
+		if (padWindowCount == 1) {
+			be_app->PostMessage(B_QUIT_REQUESTED);
+			canClose = false;
+		} else {
+			BAlert* alert = new BAlert(B_TRANSLATE("last chance"),
+				B_TRANSLATE("Really close this pad?\n"
+								"(The pad will not be remembered.)"),
+				B_TRANSLATE("Close"), B_TRANSLATE("Cancel"), NULL);
+			alert->SetShortcut(1, B_ESCAPE);
+			if (alert->Go() == 1)
+				canClose = false;
+		}
+		Unlock();
 	}
-	return true;
+	return canClose;
 }
 
 
