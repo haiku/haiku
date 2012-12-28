00e7904406d61e17409e0d646c9a461102c19940
Jérôme Duval <jerome.duval@gmail.com>
2012-12-28 18:30:03 +0100

    include string.h for FD_ZERO in sys/select.h
    * sys/select.h was not self contained before, this fixes #9327
    * index is defined as a function in string.h, fixed resulting name
      collision in glut_shapes.c

--- src/libs/glut/glut_shapes.c
+++ src/libs/glut/glut_shapes.c
@@ -507,7 +507,7 @@ static GLfloat idata[12][3] =
   {-Z, -X, 0}
 };
 
-static int index[20][3] =
+static int iIndex[20][3] =
 {
   {0, 4, 1},
   {0, 9, 4},
@@ -537,7 +537,7 @@ icosahedron(GLenum shadeType)
   int i;
 
   for (i = 19; i >= 0; i--) {
-    drawtriangle(i, idata, index, shadeType);
+    drawtriangle(i, idata, iIndex, shadeType);
   }
 }
 
