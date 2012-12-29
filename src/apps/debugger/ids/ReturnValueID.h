9ede3c06e8b42ae5428b6580a0416555af4dad05
Rene Gollent <anevilyak@gmail.com>
2012-12-28 22:24:40 -0500

    Add ReturnValueID base type.

---
+++ src/apps/debugger/ids/ReturnValueID.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2012, Rene Gollent, rene@gollent.com.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef RETURN_VALUE_ID_H
+#define RETURN_VALUE_ID_H
+
+
+#include "ObjectID.h"
+
+
+class ReturnValueID : public ObjectID {
+public:
+	virtual						~ReturnValueID();
+};
+
+
+#endif	// RETURN_VALUE_ID_H
