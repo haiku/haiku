524bb6d3ec20b00a4d5c7035eb7ca97a09c9c94e
Landon Fuller <landonf@bikemonkey.org>
2013-01-10 18:14:11 -0500

    Restore FTS_WHITEOUT, guard it with __HAIKU__.
    As per Jérôme Duval's code review in issue #9337.

--- headers/posix/fts.h
+++ headers/posix/fts.h
@@ -54,6 +54,9 @@ typedef struct {
 #define	FTS_PHYSICAL	0x010		/* physical walk */
 #define	FTS_SEEDOT	0x020		/* return dot and dot-dot */
 #define	FTS_XDEV	0x040		/* don't cross devices */
+#if !defined(__HAIKU__)
+#define	FTS_WHITEOUT	0x080		/* return whiteout information */
+#endif
 #define	FTS_OPTIONMASK	0x0ff		/* valid user option mask */
 
 #define	FTS_NAMEONLY	0x100		/* (private) child names only */
