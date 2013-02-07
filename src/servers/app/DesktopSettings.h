5c1b6cc3d4dd5bfd2c8a73cccd1ed2d17ae66d89
Axel Dörfler <axeld@pinc-software.de>
2013-02-08 00:38:34 +0100

    Style cleanup.

--- src/servers/app/DesktopSettings.h
+++ src/servers/app/DesktopSettings.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2001-2009, Haiku.
+ * Copyright 2001-2013, Haiku.
  * Distributed under the terms of the MIT License.
  *
  * Authors:
@@ -13,6 +13,7 @@
 #include <InterfaceDefs.h>
 #include <Menu.h>
 #include <Message.h>
+
 
 class Desktop;
 class DesktopSettingsPrivate;
@@ -30,70 +31,75 @@ enum {
 	kDraggerSettings	= 0x10,
 };
 
+
 class DesktopSettings {
-	public:
-		DesktopSettings(Desktop* desktop);
+public:
+								DesktopSettings(Desktop* desktop);
 
-		status_t		Save(uint32 mask = kAllSettings);
+			status_t			Save(uint32 mask = kAllSettings);
 
-		void			GetDefaultPlainFont(ServerFont& font) const;
-		void			GetDefaultBoldFont(ServerFont& font) const;
-		void			GetDefaultFixedFont(ServerFont& font) const;
+			void				GetDefaultPlainFont(ServerFont& font) const;
+			void				GetDefaultBoldFont(ServerFont& font) const;
+			void				GetDefaultFixedFont(ServerFont& font) const;
 
-		void			GetScrollBarInfo(scroll_bar_info& info) const;
-		void			GetMenuInfo(menu_info& info) const;
+			void				GetScrollBarInfo(scroll_bar_info& info) const;
+			void				GetMenuInfo(menu_info& info) const;
 
-		mode_mouse		MouseMode() const;
-		mode_focus_follows_mouse FocusFollowsMouseMode() const;
-		bool			FocusFollowsMouse() const;
-		bool			AcceptFirstClick() const;
+			mode_mouse			MouseMode() const;
+			mode_focus_follows_mouse FocusFollowsMouseMode() const;
+			bool				FocusFollowsMouse() const;
+			bool				AcceptFirstClick() const;
 
-		bool			ShowAllDraggers() const;
+			bool				ShowAllDraggers() const;
 
-		int32			WorkspacesCount() const;
-		int32			WorkspacesColumns() const;
-		int32			WorkspacesRows() const;
-		const BMessage*	WorkspacesMessage(int32 index) const;
+			int32				WorkspacesCount() const;
+			int32				WorkspacesColumns() const;
+			int32				WorkspacesRows() const;
+			const BMessage*		WorkspacesMessage(int32 index) const;
 
-		rgb_color		UIColor(color_which which) const;
+			rgb_color			UIColor(color_which which) const;
 
-		bool			SubpixelAntialiasing() const;
-		uint8			Hinting() const;
-		uint8			SubpixelAverageWeight() const;
-		bool			IsSubpixelOrderingRegular() const;
+			bool				SubpixelAntialiasing() const;
+			uint8				Hinting() const;
+			uint8				SubpixelAverageWeight() const;
+			bool				IsSubpixelOrderingRegular() const;
 
-	protected:
-		DesktopSettingsPrivate*	fSettings;
+protected:
+			DesktopSettingsPrivate*	fSettings;
 };
+
 
 class LockedDesktopSettings : public DesktopSettings {
-	public:
-		LockedDesktopSettings(Desktop* desktop);
-		~LockedDesktopSettings();
+public:
+								LockedDesktopSettings(Desktop* desktop);
+								~LockedDesktopSettings();
 
-		void			SetDefaultPlainFont(const ServerFont& font);
-		void			SetDefaultBoldFont(const ServerFont& font);
-		void			SetDefaultFixedFont(const ServerFont& font);
+			void				SetDefaultPlainFont(const ServerFont& font);
+			void				SetDefaultBoldFont(const ServerFont& font);
+			void				SetDefaultFixedFont(const ServerFont& font);
 
-		void			SetScrollBarInfo(const scroll_bar_info& info);
-		void			SetMenuInfo(const menu_info& info);
+			void				SetScrollBarInfo(const scroll_bar_info& info);
+			void				SetMenuInfo(const menu_info& info);
 
-		void			SetMouseMode(mode_mouse mode);
-		void			SetFocusFollowsMouseMode(
-							mode_focus_follows_mouse mode);
-		void			SetAcceptFirstClick(bool acceptFirstClick);
+			void				SetMouseMode(mode_mouse mode);
+			void				SetFocusFollowsMouseMode(
+									mode_focus_follows_mouse mode);
+			void				SetAcceptFirstClick(bool acceptFirstClick);
 
-		void			SetShowAllDraggers(bool show);
+			void				SetShowAllDraggers(bool show);
 
-		void			SetUIColor(color_which which, const rgb_color color);
+			void				SetUIColor(color_which which,
+									const rgb_color color);
 
-		void			SetSubpixelAntialiasing(bool subpix);
-		void			SetHinting(uint8 hinting);
-		void			SetSubpixelAverageWeight(uint8 averageWeight);
-		void			SetSubpixelOrderingRegular(bool subpixelOrdering);
+			void				SetSubpixelAntialiasing(bool subpix);
+			void				SetHinting(uint8 hinting);
+			void				SetSubpixelAverageWeight(uint8 averageWeight);
+			void				SetSubpixelOrderingRegular(
+									bool subpixelOrdering);
 
-	private:
-		Desktop*		fDesktop;
+private:
+			Desktop*			fDesktop;
 };
+
 
 #endif	/* DESKTOP_SETTINGS_H */
