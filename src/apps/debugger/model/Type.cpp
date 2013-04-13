8db1d0fc064b25babb3979c26e828dca0adeaf2e
Rene Gollent <anevilyak@gmail.com>
2013-04-12 23:22:28 -0400

    Add function for creating derived array types.

--- src/apps/debugger/model/Type.cpp
+++ src/apps/debugger/model/Type.cpp
@@ -104,6 +104,15 @@ Type::CreateDerivedAddressType(address_type_kind kind,
 }
 
 
+status_t
+Type::CreateDerivedArrayType(uint64 elementCount,
+	bool extendExisting, ArrayType*& _resultType)
+{
+	_resultType = NULL;
+	return B_ERROR;
+}
+
+
 // #pragma mark - PrimitiveType
 
 
