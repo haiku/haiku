8683f5d4ee10b4d4850b9368634916eb91e564be
Philippe Houdoin <philippe.houdoin@gmail.com>
2013-05-24 10:11:53 +0200

    Quick style polishing...

--- src/tests/kits/net/if_nameindex.c
+++ src/tests/kits/net/if_nameindex.c
@@ -2,16 +2,21 @@
 #include <stdio.h>
 #include <net/if.h>
 
-int main(int argc, char *argv[])
+
+int main(int argc, char* argv[])
 {
-  struct if_nameindex *ifs;
-  int i;
+	struct if_nameindex* ifs;
+	int i;
 
-  ifs = if_nameindex();
-  if (ifs == NULL) { perror("if_nameindex"); exit(EXIT_FAILURE); }
+	ifs = if_nameindex();
+	if (ifs == NULL) { 
+  		perror("if_nameindex"); 
+  		exit(EXIT_FAILURE);
+	}
 
-  for (i = 0; ifs[i].if_index != 0 || ifs[i].if_name != NULL; i++) {
-    printf("%d %s\n", ifs[i].if_index, ifs[i].if_name);
-  }
-  return EXIT_SUCCESS;
+	for (i = 0; ifs[i].if_index != 0 || ifs[i].if_name != NULL; i++) {
+		printf("%d %s\n", ifs[i].if_index, ifs[i].if_name);
+	}
+
+	return EXIT_SUCCESS;
 }
