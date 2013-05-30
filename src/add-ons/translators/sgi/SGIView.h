dc674f5d961fe92d691e6bae4def80ff6d14da3b
John Scipione <jscipione@gmail.com>
2013-05-30 17:43:34 -0400

    SGITranslator: Resize menu field to preferred width
    ... when the window is first activated. Unfortunately the preferred
    width has not been set until after the window is shown so we can't
    do the work in AllAttached().

--- src/add-ons/translators/sgi/SGIView.h
+++ src/add-ons/translators/sgi/SGIView.h
@@ -50,6 +50,7 @@ public:
 
 	virtual	void				AllAttached();
 	virtual	void				MessageReceived(BMessage* message);
+	virtual	void				WindowActivated(bool active);
 
 	enum {
 		MSG_COMPRESSION_CHANGED	= 'cmch',
