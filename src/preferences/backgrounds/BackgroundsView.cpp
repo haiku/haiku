67a0c8e7ac39d6e998175d16a3fe961656c41b20
John Scipione <jscipione@gmail.com>
2013-06-02 17:33:04 -0400

    Revert "Backgrounds: Line up BBox top borders"
    This reverts commit 4fe4dcf947abde59b084345697c723fb82053bc1.
    
    Want to do a different way.

--- src/preferences/backgrounds/BackgroundsView.cpp
+++ src/preferences/backgrounds/BackgroundsView.cpp
@@ -29,7 +29,6 @@
 #include <MimeType.h>
 #include <Point.h>
 #include <PopUpMenu.h>
-#include <SpaceLayoutItem.h>
 
 #include <be_apps/Tracker/Background.h>
 
@@ -212,12 +211,7 @@ BackgroundsView::BackgroundsView()
 
 	AddChild(BLayoutBuilder::Group<>(B_VERTICAL)
 		.AddGroup(B_HORIZONTAL)
-			.AddGroup(B_VERTICAL, 0)
-				.Add(BSpaceLayoutItem::CreateVerticalStrut(
-					ceilf(rightbox->TopBorderOffset() / 2
-						- previewBox->TopBorderOffset() / 2) + 1))
-				.Add(previewBox)
-				.End()
+			.Add(previewBox)
 			.Add(rightbox)
 			.End()
 		.AddGroup(B_HORIZONTAL)
