haiku/haiku

commit 21d806359150aa1b069e9e2d6051da2ac92431ff

Slight tweak to located file handling.
- When the user helps locate a missing source file, verify that the
source and located file names match. If they don't, prompt the user to
verify that they did in fact intend to choose the file in question.
Helps avoid accidentally clicking the wrong file when performing
location.

src/apps/debugger/user_interface/gui/team_window/TeamWindow.cpp
@@ -349,7 +349,9 @@ TeamWindow::MessageReceived(BMessage* message)
 		case B_REFS_RECEIVED:
 		{
 			entry_ref locatedPath;
-			message->FindRef("refs", &locatedPath);
+			if (message->FindRef("refs", &locatedPath) != B_OK)
+				break;
+
 			_HandleResolveMissingSourceFile(locatedPath);
 			break;
 		}
@@ -1438,11 +1440,30 @@ TeamWindow::_HandleResolveMissingSourceFile(entry_ref& locatedPath)
 			->SourceFile();
 		if (sourceFile != NULL) {
 			BString sourcePath;
-			BString targetPath;
 			sourceFile->GetPath(sourcePath);
-			BPath path(&locatedPath);
-			targetPath = path.Path();
-			fListener->SourceEntryLocateRequested(sourcePath, targetPath);
+			BPath sourceFilePath(sourcePath);
+			BPath targetFilePath(&locatedPath);
+			if (sourceFilePath.InitCheck() != B_OK
+				|| targetFilePath.InitCheck() != B_OK) {
+				return;
+			}
+
+			if (strcmp(sourceFilePath.Leaf(), targetFilePath.Leaf()) != 0) {
+				BString message;
+				message.SetToFormat("The names of source file '%s' and located"
+					" file '%s' differ. Use file anyway?",
+					sourceFilePath.Leaf(), targetFilePath.Leaf());
+				BAlert* alert = new(std::nothrow) BAlert(
+					"Source path mismatch", message.String(), "Cancel", "Use");
+				if (alert == NULL)
+					return;
+
+				int32 choice = alert->Go();
+				if (choice <= 0)
+					return;
+			}
+			fListener->SourceEntryLocateRequested(sourcePath,
+				targetFilePath.Path());
 			fListener->FunctionSourceCodeRequested(fActiveFunction);
 		}
 	}

commit 2214cb57eeed6481d73b5ef887b3ce27d47eaf8f

Fix regression introduced in 21d8063.
We can't use BPath to get the filename for the source file path embedded
in the debug information, since it may be relative, which BPath will try to
normalize.

src/apps/debugger/user_interface/gui/team_window/TeamWindow.cpp
@@ -1459,18 +1459,20 @@ TeamWindow::_HandleResolveMissingSourceFile(entry_ref& locatedPath)
 		if (sourceFile != NULL) {
 			BString sourcePath;
 			sourceFile->GetPath(sourcePath);
-			BPath sourceFilePath(sourcePath);
-			BPath targetFilePath(&locatedPath);
-			if (sourceFilePath.InitCheck() != B_OK
-				|| targetFilePath.InitCheck() != B_OK) {
-				return;
-			}
+			BString sourceFileName(sourcePath);
+			int32 index = sourcePath.FindLast('/');
+			if (index >= 0)
+				sourceFileName.Remove(0, index + 1);
 
-			if (strcmp(sourceFilePath.Leaf(), targetFilePath.Leaf()) != 0) {
+			BPath targetFilePath(&locatedPath);
+			if (targetFilePath.InitCheck() != B_OK)
+				return;
+
+			if (strcmp(sourceFileName.String(), targetFilePath.Leaf()) != 0) {
 				BString message;
 				message.SetToFormat("The names of source file '%s' and located"
 					" file '%s' differ. Use file anyway?",
-					sourceFilePath.Leaf(), targetFilePath.Leaf());
+					sourceFileName.String(), targetFilePath.Leaf());
 				BAlert* alert = new(std::nothrow) BAlert(
 					"Source path mismatch", message.String(), "Cancel", "Use");
 				if (alert == NULL)