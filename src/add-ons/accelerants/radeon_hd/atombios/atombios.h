f901b5b7fb9ebae4d4908014c1a5364353032ae6
Alexander von Gluck IV <kallisti5@unixzen.com>
2013-06-29 13:36:26 -0500

    RadeonHD: Fix endian bugs in atombios header
    * Style is not correct, keep in mind
      that this header is 1:1 upstream code
    * Linux kernel commit:
      f4a2596cecfcfce1e0ac1df5a1603f7bf392c122
    * AMD does not license this stuff GPL

--- src/add-ons/accelerants/radeon_hd/atombios/atombios.h
+++ src/add-ons/accelerants/radeon_hd/atombios/atombios.h
@@ -459,6 +459,7 @@ typedef struct _COMPUTE_MEMORY_ENGINE_PLL_PARAMETERS_V3
   union
   {
     ATOM_COMPUTE_CLOCK_FREQ  ulClock;         //Input Parameter
+    ULONG ulClockParams;                      //ULONG access for BE
     ATOM_S_MPLL_FB_DIVIDER   ulFbDiv;         //Output Parameter
   };
   UCHAR   ucRefDiv;                           //Output Parameter      
@@ -491,6 +492,7 @@ typedef struct _COMPUTE_MEMORY_ENGINE_PLL_PARAMETERS_V5
   union
   {
     ATOM_COMPUTE_CLOCK_FREQ  ulClock;         //Input Parameter
+    ULONG ulClockParams;                      //ULONG access for BE
     ATOM_S_MPLL_FB_DIVIDER   ulFbDiv;         //Output Parameter
   };
   UCHAR   ucRefDiv;                           //Output Parameter      
