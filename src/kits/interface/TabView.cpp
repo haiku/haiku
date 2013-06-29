haiku/haiku

commit 16e486eb4dd2d39b9a3eb65171106fd17d7e3fc0

BTabView: minor optimization.
Short circuit if asked to select the same index that's currently visible, to
avoid unnecessary invalidation/flicker.

Should fix last part of #9841.

src/kits/interface/TabView.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2001-2009, Haiku, Inc. All rights reserved.
+ * Copyright 2001-2013, Haiku, Inc. All rights reserved.
  * Distributed under the terms of the MIT License.
  *
  * Authors:
@@ -7,6 +7,7 @@
  *		Jérôme Duval (korli@users.berlios.de)
  *		Stephan Aßmus <superstippi@gmx.de>
  *		Artur Wyszynski
+ *		Rene Gollent (rene@gollent.com)
  */
 
 
@@ -746,6 +747,9 @@ BTabView::Pulse()
 void
 BTabView::Select(int32 index)
 {
+	if (index == Selection())
+		return;
+
 	if (index < 0 || index >= CountTabs())
 		index = Selection();
 
@@ -1351,7 +1355,7 @@ BTabView::_InitObject(bool layouted, button_width width)
 	fTabList = new BList;
 
 	fTabWidthSetting = width;
-	fSelection = 0;
+	fSelection = -1;
 	fFocus = -1;
 	fTabOffset = 0.0f;
 	fBorderStyle = B_FANCY_BORDER;

commit eaedb2f8a47036540c14494bc4cb8a02218fd12b

Fix regression introduced in 16e486e.
Explicitly select the first tab if the caller hasn't done so by the time
we're attached to the window.

src/kits/interface/TabView.cpp
@@ -569,7 +569,8 @@ BTabView::AttachedToWindow()
 {
 	BView::AttachedToWindow();
 
-	Select(fSelection);
+	if (fSelection < 0)
+		Select(0);
 }
 
 