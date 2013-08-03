3e2a7aa5ce6fa70a47364902153f922a8415b83c
Stephan Aßmus <superstippi@gmx.de>
2013-08-03 22:32:42 +0200

    HaikuDepot: Classes to interface with the Package Kit
     * Non functional
     * Basically allows to retrieve applicable actions for
       a given package.

---
+++ src/apps/haiku-depot/PackageManager.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013, Stephan Aßmus <superstippi@gmx.de>.
+ * All rights reserved. Distributed under the terms of the MIT License.
+ */
+#ifndef PACKAGE_MANAGER_H
+#define PACKAGE_MANAGER_H
+
+
+#include <Referenceable.h>
+
+#include "List.h"
+#include "PackageInfo.h"
+
+
+class PackageAction : public BReferenceable {
+public:
+								PackageAction(const PackageInfo& package);
+	virtual						~PackageAction();
+
+	virtual const char*			Label() const = 0;
+	
+	// TODO: Perform() needs to be passed a progress listener
+	// and it needs a mechanism to report and react to errors. The
+	// Package Kit supports this stuff already.
+	virtual status_t			Perform() = 0;
+
+protected:
+			PackageInfo			fPackage;
+};
+
+
+typedef BReference<PackageAction> PackageActionRef;
+typedef List<PackageActionRef, false> PackageActionList;
+
+
+class PackageManager {
+public:
+								PackageManager();
+	virtual						~PackageManager();
+
+	virtual	PackageActionList	GetPackageActions(const PackageInfo& package);
+};
+
+
+#endif // PACKAGE_MANAGER_H
