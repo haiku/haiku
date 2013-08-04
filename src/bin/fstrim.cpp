a352b438960f14e070d0850080706f9930dcd30f
Axel Dörfler <axeld@pinc-software.de>
2013-08-05 00:14:52 +0200

    Added fstrim command.
    Later, there should be a service that runs this from time to time for all
    devices that support it.
    
    Conflicts:
    	build/jam/HaikuImage

---
+++ src/bin/fstrim.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2013, Axel Dörfler, axeld@pinc-software.de.
+ * Distributed under the terms of the MIT license.
+ */
+
+
+#include <errno.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <Drivers.h>
+
+#include <AutoDeleter.h>
+
+
+static struct option const kLongOptions[] = {
+	{"help", no_argument, 0, 'h'},
+	{NULL}
+};
+
+
+extern const char *__progname;
+static const char *kProgramName = __progname;
+
+
+void
+usage(int returnValue)
+{
+	fprintf(stderr, "Usage: %s <path-to-mounted-file-system>\n", kProgramName);
+	exit(returnValue);
+}
+
+
+int
+main(int argc, char** argv)
+{
+	int c;
+	while ((c = getopt_long(argc, argv, "h", kLongOptions, NULL)) != -1) {
+		switch (c) {
+			case 0:
+				break;
+			case 'h':
+				usage(0);
+				break;
+			default:
+				usage(1);
+				break;
+		}
+	}
+
+	if (argc - optind < 1)
+		usage(1);
+	const char* path = argv[optind++];
+
+	int fd = open(path, O_RDONLY);
+	if (fd < 0) {
+		fprintf(stderr, "%s: Could not access path: %s\n", kProgramName,
+			strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	FileDescriptorCloser closer(fd);
+
+	fs_trim_data trimData;
+	trimData.offset = 0;
+	trimData.size = OFF_MAX;
+	trimData.trimmed_size = 0;
+
+	if (ioctl(fd, B_TRIM_DEVICE, &trimData, sizeof(fs_trim_data)) != 0) {
+		fprintf(stderr, "%s: Trimming failed: %s\n", kProgramName,
+			strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	printf("Trimmed %lld bytes from device.\n", trimData.trimmed_size);
+	return EXIT_SUCCESS;
+}
