ac359925f51e7a7da060f3e42f9d67d7c3575dfb
John Scipione <jscipione@gmail.com>
2013-08-20 18:13:14 -0400

    Revert "don't open the menu if it's disabled."
    This reverts commit ce8253d97b588366540230d11fb534c4846d0a09.

--- src/kits/interface/MenuField.cpp
+++ src/kits/interface/MenuField.cpp
@@ -428,9 +428,6 @@ BMenuField::MouseDown(BPoint where)
 	if (!fMenuBar->Frame().Contains(where))
 		return;
 
-	if (!fMenuBar->IsEnabled())
-		return;
-
 	BRect bounds = fMenuBar->ConvertFromParent(Bounds());
 
 	fMenuBar->StartMenuBar(-1, false, true, &bounds);
