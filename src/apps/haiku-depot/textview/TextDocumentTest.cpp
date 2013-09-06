59b52a11630a70a1c5c536bd75f75f5a003c5790
Stephan Aßmus <superstippi@gmx.de>
2013-09-07 00:09:02 +0200

    HaikuDepot: Added very simplistic markup parser.
     * Follows MediaWiki or TracWiki style.
     * Supports only level 2 headings, bold, italic and top-level
       bullet paragraphs.
     * Line break handling would need to be changed.
     * Outputs a TextDocument.

--- src/apps/haiku-depot/textview/TextDocumentTest.cpp
+++ src/apps/haiku-depot/textview/TextDocumentTest.cpp
@@ -5,11 +5,13 @@
 
 #include "TextDocumentTest.h"
 
+#include <math.h>
 #include <stdio.h>
 
 #include <LayoutBuilder.h>
 #include <Window.h>
 
+#include "MarkupParser.h"
 #include "TextDocumentView.h"
 
 TextDocumentTest::TextDocumentTest()
@@ -38,64 +40,80 @@ TextDocumentTest::ReadyToRun()
 		.Add(documentView)
 	;
 
-	ParagraphStyle paragraphStyle;
-	paragraphStyle.SetJustify(true);
-	
 	CharacterStyle regularStyle;
 	
-	CharacterStyle boldStyle(regularStyle);
-	boldStyle.SetBold(true);
-
-	CharacterStyle italicStyle(regularStyle);
-	italicStyle.SetItalic(true);
-
-	CharacterStyle italicAndBoldStyle(boldStyle);
-	italicAndBoldStyle.SetItalic(true);
+	float fontSize = regularStyle.Font().Size();
 	
-	CharacterStyle bigStyle(regularStyle);
-	bigStyle.SetFontSize(24);
-	bigStyle.SetForegroundColor(255, 50, 50);
-
-	TextDocumentRef document(new TextDocument(), true);
-
-	Paragraph paragraph(paragraphStyle);
-	paragraph.Append(TextSpan("This is a", regularStyle));
-	paragraph.Append(TextSpan(" test ", bigStyle));
-	paragraph.Append(TextSpan("to see if ", regularStyle));
-	paragraph.Append(TextSpan("different", boldStyle));
-	paragraph.Append(TextSpan(" character styles already work.", regularStyle));
-	document->Append(paragraph);
-
-	paragraphStyle.SetSpacingTop(8.0f);
-	paragraphStyle.SetAlignment(ALIGN_CENTER);
-	paragraphStyle.SetJustify(false);
-
-	paragraph = Paragraph(paragraphStyle);
-	paragraph.Append(TextSpan("Different alignment styles ", regularStyle));
-	paragraph.Append(TextSpan("are", boldStyle));
-	paragraph.Append(TextSpan(" supported as of now!", regularStyle));
-	document->Append(paragraph);
-
-	// Test a bullet list
-	paragraphStyle.SetSpacingTop(8.0f);
-	paragraphStyle.SetAlignment(ALIGN_LEFT);
+	ParagraphStyle paragraphStyle;
 	paragraphStyle.SetJustify(true);
-	paragraphStyle.SetBullet(Bullet("•", 12.0f));
-	paragraphStyle.SetLineInset(10.0f);
-
-	paragraph = Paragraph(paragraphStyle);
-	paragraph.Append(TextSpan("Even bullet lists are supported.", regularStyle));
-	document->Append(paragraph);
-
-	paragraph = Paragraph(paragraphStyle);
-	paragraph.Append(TextSpan("The wrapping in ", regularStyle));
-	paragraph.Append(TextSpan("this", italicStyle));
+	paragraphStyle.SetSpacingTop(ceilf(fontSize * 0.6f));
+	paragraphStyle.SetLineSpacing(ceilf(fontSize * 0.2f));
 	
-	paragraph.Append(TextSpan(" bullet item should look visually "
-		"pleasing. And ", regularStyle));
-	paragraph.Append(TextSpan("why", italicAndBoldStyle));
-	paragraph.Append(TextSpan(" should it not?", regularStyle));
-	document->Append(paragraph);
+//	CharacterStyle boldStyle(regularStyle);
+//	boldStyle.SetBold(true);
+//
+//	CharacterStyle italicStyle(regularStyle);
+//	italicStyle.SetItalic(true);
+//
+//	CharacterStyle italicAndBoldStyle(boldStyle);
+//	italicAndBoldStyle.SetItalic(true);
+//	
+//	CharacterStyle bigStyle(regularStyle);
+//	bigStyle.SetFontSize(24);
+//	bigStyle.SetForegroundColor(255, 50, 50);
+//
+//	TextDocumentRef document(new TextDocument(), true);
+//
+//	Paragraph paragraph(paragraphStyle);
+//	paragraph.Append(TextSpan("This is a", regularStyle));
+//	paragraph.Append(TextSpan(" test ", bigStyle));
+//	paragraph.Append(TextSpan("to see if ", regularStyle));
+//	paragraph.Append(TextSpan("different", boldStyle));
+//	paragraph.Append(TextSpan(" character styles already work.", regularStyle));
+//	document->Append(paragraph);
+//
+//	paragraphStyle.SetSpacingTop(8.0f);
+//	paragraphStyle.SetAlignment(ALIGN_CENTER);
+//	paragraphStyle.SetJustify(false);
+//
+//	paragraph = Paragraph(paragraphStyle);
+//	paragraph.Append(TextSpan("Different alignment styles ", regularStyle));
+//	paragraph.Append(TextSpan("are", boldStyle));
+//	paragraph.Append(TextSpan(" supported as of now!", regularStyle));
+//	document->Append(paragraph);
+//
+//	// Test a bullet list
+//	paragraphStyle.SetSpacingTop(8.0f);
+//	paragraphStyle.SetAlignment(ALIGN_LEFT);
+//	paragraphStyle.SetJustify(true);
+//	paragraphStyle.SetBullet(Bullet("•", 12.0f));
+//	paragraphStyle.SetLineInset(10.0f);
+//
+//	paragraph = Paragraph(paragraphStyle);
+//	paragraph.Append(TextSpan("Even bullet lists are supported.", regularStyle));
+//	document->Append(paragraph);
+//
+//	paragraph = Paragraph(paragraphStyle);
+//	paragraph.Append(TextSpan("The wrapping in ", regularStyle));
+//	paragraph.Append(TextSpan("this", italicStyle));
+//	
+//	paragraph.Append(TextSpan(" bullet item should look visually "
+//		"pleasing. And ", regularStyle));
+//	paragraph.Append(TextSpan("why", italicAndBoldStyle));
+//	paragraph.Append(TextSpan(" should it not?", regularStyle));
+//	document->Append(paragraph);
+
+	MarkupParser parser(regularStyle, paragraphStyle);
+	
+	TextDocumentRef document = parser.CreateDocumentFromMarkup(
+		"== Text document test ==\n"
+		"This is a test to see if '''different''' "
+		"character styles already work.\n"
+		"Different alignment styles '''are''' supported as of now!\n"
+		" * Even bullet lists are supported.\n"
+		" * The wrapping in ''this'' bullet item should look visually "
+		"pleasing. And ''why'' should it not?\n"
+	);
 
 	documentView->SetTextDocument(document);
 
