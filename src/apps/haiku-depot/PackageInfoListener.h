9202a719e143ce324d611990d2fb40939cf7e8c6
Stephan Aßmus <superstippi@gmx.de>
2013-09-15 17:24:15 +0200

    HaikuDepot: Add interface for PackageInfoListener.
     Also defines PackageInfoEvent.

---
+++ src/apps/haiku-depot/PackageInfoListener.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2013, Stephan Aßmus <superstippi@gmx.de>.
+ * All rights reserved. Distributed under the terms of the MIT License.
+ */
+#ifndef PACKAGE_INFO_LISTENER_H
+#define PACKAGE_INFO_LISTENER_H
+
+
+#include "PackageInfo.h"
+
+
+enum {
+	PKG_CHANGED_DESCRIPTION		= 1 << 0,
+	PKG_CHANGED_RATINGS			= 1 << 1,
+	PKG_CHANGED_SCREENSHOTS		= 1 << 2,
+	// ...
+};
+
+
+class PackageInfoEvent {
+public:
+								PackageInfoEvent();
+								PackageInfoEvent(const PackageInfoRef& package,
+									uint32 changes);
+								PackageInfoEvent(const PackageInfoEvent& other);
+	virtual						~PackageInfoEvent();
+
+			bool				operator==(const PackageInfoEvent& other);
+			bool				operator!=(const PackageInfoEvent& other);
+			PackageInfoEvent&	operator=(const PackageInfoEvent& other);
+
+	inline	const PackageInfoRef& Package() const
+									{ return fPackage; }
+
+	inline	uint32				Changes() const
+									{ return fChanges; }
+
+private:
+			PackageInfoRef		fPackage;
+			uint32				fChanges;
+};
+
+
+class PackageInfoListener {
+public:
+								PackageInfoListener();
+	virtual						~PackageInfoListener();
+
+	virtual	void				PackageChanged(
+									const PackageInfoEvent& event) = 0;
+};
+
+
+#endif // PACKAGE_INFO_LISTENER_H
