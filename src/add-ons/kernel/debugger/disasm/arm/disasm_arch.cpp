3d2c989bb58d9e2f68e2dc01ff279ec869d8eac5
François Revol <revol@free.fr>
2013-09-29 03:33:19 +0200

    disasm: Add stub for ARM

---
+++ src/add-ons/kernel/debugger/disasm/arm/disasm_arch.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2008, François Revol, revol@free.fr
+ * Distributed under the terms of the MIT License.
+ */
+
+#include <OS.h>
+
+#include "disasm_arch.h"
+
+status_t
+disasm_arch_dump_insns(addr_t where, int count, addr_t baseAddress,
+	int backCount)
+{
+	return ENOENT;
+}
+
+status_t
+disasm_arch_init()
+{
+	return ENOENT;
+}
+
+status_t
+disasm_arch_fini()
+{
+	return ENOENT;
+}
+
+
