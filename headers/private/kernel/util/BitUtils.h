149c82a8ec8b1faed2ae722852042ba9d603b552
Pawel Dziepak <pdziepak@quarnos.org>
2013-10-03 04:27:49 +0200

    kernel/util: Add bitmap implementation

--- headers/private/kernel/util/BitUtils.h
+++ headers/private/kernel/util/BitUtils.h
@@ -38,5 +38,23 @@ countSetBits(uint32 v)
 }
 
 
+static inline uint32
+log2(uint32 v)
+{
+	static const int MultiplyDeBruijnBitPosition[32] = {
+		0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
+		8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
+	};
+
+	v |= v >> 1;
+	v |= v >> 2;
+	v |= v >> 4;
+	v |= v >> 8;
+	v |= v >> 16;
+
+	return MultiplyDeBruijnBitPosition[(uint32)(v * 0x07C4ACDDU) >> 27];
+}
+
+
 #endif	// KERNEL_UTIL_RANDOM_H
 
