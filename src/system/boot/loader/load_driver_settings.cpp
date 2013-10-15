cf7f92c013715e81a44cf9756df6c87968334dda
François Revol <revol@free.fr>
2013-10-15 21:56:53 +0200

    bootloader: Use stage2_args:arguments_count
    The code probably doesn't work correctly yet though,
    I believe only the last line will be taken into account.

--- src/system/boot/loader/load_driver_settings.cpp
+++ src/system/boot/loader/load_driver_settings.cpp
@@ -116,7 +116,7 @@ add_stage2_driver_settings(stage2_args* args)
 {
 	// TODO: split more intelligently
 	for (const char** arg = args->arguments;
-			arg != NULL && arg[0] != NULL; arg++) {
+			arg != NULL && args->arguments_count-- && arg[0] != NULL; arg++) {
 		dprintf("adding args: '%s'\n", arg[0]);
 		add_safe_mode_settings((char*)arg[0]);
 	}
