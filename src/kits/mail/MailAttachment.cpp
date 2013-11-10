ade6fe2e939c483c68a51b22f786a363a0a5ddf1
Matt Madia <mattmadia@gmail.com>
2013-11-10 18:26:32 -0500

    Adjust array length.
    As per Axel: The "+1" is not needed; B_FILE_NAME_LENGTH already includes the
    terminating null byte.

--- src/kits/mail/MailAttachment.cpp
+++ src/kits/mail/MailAttachment.cpp
@@ -578,7 +578,7 @@ void
 BAttributedMailAttachment::SaveToDisk(BEntry *entry)
 {
 	BString path = "/tmp/";
-	char name[B_FILE_NAME_LENGTH + 1] = "";
+	char name[B_FILE_NAME_LENGTH] = "";
 	_data->FileName(name);
 	path << name;
 
