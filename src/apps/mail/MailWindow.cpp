9f7f92dd7f0f707f09569d94d5272906d0eca3cf
John Scipione <jscipione@gmail.com>
2014-01-11 18:19:11 -0500

    Mail: Make quote work with B_OPTION_KEY too.
    This is for AZERTY keymaps that don’t have ready access to
    [ and ] in the normal map.

--- src/apps/mail/MailWindow.cpp
+++ src/apps/mail/MailWindow.cpp
@@ -342,10 +342,16 @@ TMailWindow::TMailWindow(BRect rect, const char* title, TMailApp* app,
 		new BMessage(M_FIND_AGAIN), 'G'));
 	if (!fIncoming) {
 		menu->AddSeparatorItem();
-		menu->AddItem(fQuote = new BMenuItem(B_TRANSLATE("Quote"),
-			new BMessage(M_QUOTE), ']'));
-		menu->AddItem(fRemoveQuote = new BMenuItem(B_TRANSLATE("Remove quote"),
-			new BMessage(M_REMOVE_QUOTE), '['));
+		fQuote = new BMenuItem(B_TRANSLATE("Quote"),
+			new BMessage(M_QUOTE), ']');
+		menu->AddItem(fQuote);
+		AddShortcut(']', B_OPTION_KEY, new BMessage(M_QUOTE));
+
+		fRemoveQuote = new BMenuItem(B_TRANSLATE("Remove quote"),
+			new BMessage(M_REMOVE_QUOTE), '[');
+		menu->AddItem(fRemoveQuote);
+		AddShortcut('[', B_OPTION_KEY, new BMessage(M_REMOVE_QUOTE));
+
 		menu->AddSeparatorItem();
 		fSpelling = new BMenuItem(B_TRANSLATE("Check spelling"),
 			new BMessage(M_CHECK_SPELLING), ';');
