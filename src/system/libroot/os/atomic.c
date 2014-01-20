24f0b1e29e6c6abae8e1eae1f7100cc8c07a8083
Alexander von Gluck IV <kallisti5@unixzen.com>
2014-01-19 22:31:37 -0600

    libroot: Drop volatile in syscall atomic functions
    * Per 077c84eb27b, atomics are no longer volatile.
    * Was changed in SupportDefs.h, but no one told libroot
      that it was no-longer volatile.

--- src/system/libroot/os/atomic.c
+++ src/system/libroot/os/atomic.c
@@ -11,81 +11,93 @@
 
 #ifdef ATOMIC_FUNCS_ARE_SYSCALLS
 
-int32
-atomic_set(vint32 *value, int32 newValue)
+void
+atomic_set(int32 *value, int32 newValue)
 {
-	return _kern_atomic_set(value, newValue);
+	_kern_atomic_set(value, newValue);
 }
 
+
 int32
-atomic_test_and_set(vint32 *value, int32 newValue, int32 testAgainst)
+atomic_test_and_set(int32 *value, int32 newValue, int32 testAgainst)
 {
 	return _kern_atomic_test_and_set(value, newValue, testAgainst);
 }
 
+
 int32
-atomic_add(vint32 *value, int32 addValue)
+atomic_add(int32 *value, int32 addValue)
 {
 	return _kern_atomic_add(value, addValue);
 }
 
+
 int32
-atomic_and(vint32 *value, int32 andValue)
+atomic_and(int32 *value, int32 andValue)
 {
 	return _kern_atomic_and(value, andValue);
 }
 
+
 int32
-atomic_or(vint32 *value, int32 orValue)
+atomic_or(int32 *value, int32 orValue)
 {
 	return _kern_atomic_or(value, orValue);
 }
 
+
 int32
-atomic_get(vint32 *value)
+atomic_get(int32 *value)
 {
 	return _kern_atomic_get(value);
 }
+
 
 #endif	/* ATOMIC_FUNCS_ARE_SYSCALLS */
 
 #ifdef ATOMIC64_FUNCS_ARE_SYSCALLS
 
-int64
-atomic_set64(vint64 *value, int64 newValue)
+void
+atomic_set64(int64 *value, int64 newValue)
 {
-	return _kern_atomic_set64(value, newValue);
+	_kern_atomic_set64(value, newValue);
 }
 
+
 int64
-atomic_test_and_set64(vint64 *value, int64 newValue, int64 testAgainst)
+atomic_test_and_set64(int64 *value, int64 newValue, int64 testAgainst)
 {
 	return _kern_atomic_test_and_set64(value, newValue, testAgainst);
 }
 
+
 int64
-atomic_add64(vint64 *value, int64 addValue)
+atomic_add64(int64 *value, int64 addValue)
 {
 	return _kern_atomic_add64(value, addValue);
 }
 
+
 int64
-atomic_and64(vint64 *value, int64 andValue)
+atomic_and64(int64 *value, int64 andValue)
 {
 	return _kern_atomic_and64(value, andValue);
 }
 
+
 int64
-atomic_or64(vint64 *value, int64 orValue)
+atomic_or64(int64 *value, int64 orValue)
 {
 	return _kern_atomic_or64(value, orValue);
 }
 
+
 int64
-atomic_get64(vint64 *value)
+atomic_get64(int64 *value)
 {
 	return _kern_atomic_get64(value);
 }
+
 
 #endif	/* ATOMIC64_FUNCS_ARE_SYSCALLS */
 
@@ -99,7 +111,7 @@ extern int32_t __sync_fetch_and_add_4(int32_t *value, int32_t addValue);
 
 extern int32_t __sync_fetch_and_add_4(int32_t *value, int32_t addValue)
 {
-	return atomic_add((vint32 *)value, addValue);
+	return atomic_add((int32 *)value, addValue);
 }
 
 #endif
