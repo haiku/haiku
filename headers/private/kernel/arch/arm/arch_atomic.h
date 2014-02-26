8cfbbff4df895f36e749e45a28699c493be522e8
Alexander von Gluck IV <kallisti5@unixzen.com>
2014-02-26 13:22:18 -0600

    ARM: Fix dmb opcode 2 on ARMv6
    * Typo, also fix tabs
    * Sorry for the spam, this should be correct now

--- headers/private/kernel/arch/arm/arch_atomic.h
+++ headers/private/kernel/arch/arm/arch_atomic.h
@@ -12,15 +12,15 @@
 #if __ARM_ARCH__ <= 5
 #define isb() __asm__ __volatile__("" : : : "memory")
 #define dsb() __asm__ __volatile__("mcr p15, 0, %0, c7, c10, 4" \
-    : : "r" (0) : "memory")
+	: : "r" (0) : "memory")
 #define dmb() __asm__ __volatile__("" : : : "memory")
 #elif __ARM_ARCH__ == 6
 #define isb() __asm__ __volatile__("mcr p15, 0, %0, c7, c5, 4" \
 	: : "r" (0) : "memory")
 #define dsb() __asm__ __volatile__("mcr p15, 0, %0, c7, c10, 4" \
-    : : "r" (0) : "memory")
-#define dmb() __asm__ __volatile__("mcr p15, 0, %0, c7, c10, 4" \
-    : : "r" (0) : "memory")
+	: : "r" (0) : "memory")
+#define dmb() __asm__ __volatile__("mcr p15, 0, %0, c7, c10, 5" \
+	: : "r" (0) : "memory")
 #else /* ARMv7+ */
 #define isb() __asm__ __volatile__("isb" : : : "memory")
 #define dsb() __asm__ __volatile__("dsb" : : : "memory")
