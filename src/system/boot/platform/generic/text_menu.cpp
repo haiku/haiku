59881eaa166dc887b1c4a8f275e93e695b2da670
Ingo Weinhold <ingo_weinhold@gmx.de>
2014-04-14 08:18:35 +0200

    Boot loader: Update copyright right in boot menu

--- src/system/boot/platform/generic/text_menu.cpp
+++ src/system/boot/platform/generic/text_menu.cpp
@@ -209,7 +209,7 @@ draw_menu(Menu *menu)
 	print_centered(2, "Haiku Boot Loader");
 
 	console_set_color(kCopyrightColor, kBackgroundColor);
-	print_centered(4, "Copyright 2004-2013 Haiku, Inc.");
+	print_centered(4, "Copyright 2004-2014 Haiku, Inc.");
 
 	if (menu->Title()) {
 		console_set_cursor(kOffsetX, kFirstLine - 2);
