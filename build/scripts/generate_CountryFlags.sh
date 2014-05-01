8c54af563ce823c0e5b029716b2a020896c90ff4
Oliver Tappe <zooey@hirschkaefer.de>
2014-05-01 12:50:11 +0200

    Whitespace cleanup in root and 'build' folder.
    * Removed trailing whitespace and empty lines at top of files.

--- build/scripts/generate_CountryFlags.sh
+++ build/scripts/generate_CountryFlags.sh
@@ -7,7 +7,7 @@ destination=CountryFlags.rdef
 nr=0
 
 for file in *
-do 
+do
 	id=`echo "$file" | cut -b -2`
 	name=`echo "${file%%.*}" | cut -b 4-`
 
@@ -15,7 +15,7 @@ do
 	echo "resource($nr,\"$id\") #'VICN' array {" >> $destination
 	tail -n +3 "$file" >> $destination
 	echo >> $destination
-	
-	nr=`expr $nr + 1`	
+
+	nr=`expr $nr + 1`
 	echo \ $nr, $name, $id... OK
 done
