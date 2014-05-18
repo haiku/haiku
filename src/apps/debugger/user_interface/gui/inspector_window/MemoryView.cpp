4abae4aba8c49806a011b79aa70288d052fdc5ca
Rene Gollent <rene@gollent.com>
2014-05-17 21:41:35 -0400

    Debugger: Fix #10781.
    - Don't attempt to calculate a selection region if we don't currently have a
    valid target block. Should fix remaining part of #10781.

--- src/apps/debugger/user_interface/gui/inspector_window/MemoryView.cpp
+++ src/apps/debugger/user_interface/gui/inspector_window/MemoryView.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2013, Rene Gollent, rene@gollent.com. All rights reserved.
+ * Copyright 2011-2014, Rene Gollent, rene@gollent.com. All rights reserved.
  * Distributed under the terms of the MIT License.
  */
 
@@ -745,7 +745,7 @@ MemoryView::_GetAddressDisplayWidth() const
 void
 MemoryView::_GetSelectionRegion(BRegion& region)
 {
-	if (fHexMode == HexModeNone)
+	if (fHexMode == HexModeNone || fTargetBlock == NULL)
 		return;
 
 	region.MakeEmpty();
