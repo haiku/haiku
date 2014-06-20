086eaa586b162a2faa1b3e5901ef9e55c6a59bb9
John Scipione <jscipione@gmail.com>
2014-06-20 17:52:47 -0400

    Tracker: style fixes to FSUndoRedo
    Add Open Tracker License to source since this came from OpenTracker.

--- src/kits/tracker/FSUndoRedo.cpp
+++ src/kits/tracker/FSUndoRedo.cpp
@@ -1,3 +1,38 @@
+/*
+Open Tracker License
+
+Terms and Conditions
+
+Copyright (c) 1991-2000, Be Incorporated. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice applies to all licensees
+and shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF TITLE, MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+BE INCORPORATED BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Be Incorporated shall not be
+used in advertising or otherwise to promote the sale, use or other dealings in
+this Software without prior written authorization from Be Incorporated.
+
+Tracker(TM), Be(R), BeOS(R), and BeIA(TM) are trademarks or registered trademarks
+of Be Incorporated in the United States and other countries. Other brand product
+names are registered trademarks or trademarks of their respective holders.
+All rights reserved.
+*/
+
+
 #include "Commands.h"
 #include "FSUndoRedo.h"
 #include "FSUtils.h"
@@ -146,7 +181,7 @@ AddUndoItem(UndoItem* item)
 }
 
 
-//	#pragma mark -
+//	#pragma mark - Undo
 
 
 Undo::~Undo()
@@ -200,7 +235,7 @@ RenameVolumeUndo::RenameVolumeUndo(BVolume &volume, const char* newName)
 }
 
 
-//	#pragma mark -
+//	#pragma mark - UndoItemCopy
 
 
 UndoItemCopy::UndoItemCopy(BObjectList<entry_ref>* sourceList,
@@ -264,7 +299,7 @@ UndoItemCopy::UpdateEntry(BEntry* entry, const char* name)
 }
 
 
-//	#pragma mark -
+//	#pragma mark - UndoItemMove
 
 
 UndoItemMove::UndoItemMove(BObjectList<entry_ref>* sourceList,
@@ -385,11 +420,11 @@ UndoItemRename::Redo()
 }
 
 
-//	#pragma mark -
+//	#pragma mark - UndoItemRenameVolume
 
 
 UndoItemRenameVolume::UndoItemRenameVolume(BVolume &volume,
-		const char* newName)
+	const char* newName)
 	:
 	fVolume(volume),
 	fNewName(newName)
@@ -421,7 +456,7 @@ UndoItemRenameVolume::Redo()
 }
 
 
-//	#pragma mark -
+//	#pragma mark - FSUndo() and FSRedo() functions
 
 
 void
