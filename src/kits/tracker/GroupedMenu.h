a83fb6fed8946eb2f1c330c0239bc0f7e7d61a70
John Scipione <jscipione@gmail.com>
2014-06-20 17:59:19 -0400

    Tracker: style fixes to GroupedMenu
    Add Open Tracker License to source as this came from OpenTracker.

--- src/kits/tracker/GroupedMenu.h
+++ src/kits/tracker/GroupedMenu.h
@@ -1,5 +1,38 @@
-#ifndef GROUPED_MENU_H
-#define GROUPED_MENU_H
+/*
+Open Tracker License
+
+Terms and Conditions
+
+Copyright (c) 1991-2000, Be Incorporated. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice applies to all licensees
+and shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF TITLE, MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+BE INCORPORATED BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF, OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Be Incorporated shall not be
+used in advertising or otherwise to promote the sale, use or other dealings in
+this Software without prior written authorization from Be Incorporated.
+
+Tracker(TM), Be(R), BeOS(R), and BeIA(TM) are trademarks or registered
+trademarks of Be Incorporated in the United States and other countries. Other
+brand product names are registered trademarks or trademarks of their
+respective holders. All rights reserved.
+*/
+#ifndef _GROUPED_MENU_H
+#define _GROUPED_MENU_H
 
 
 #include <Menu.h>
@@ -68,4 +101,5 @@ class TGroupedMenu : public BMenu {
 
 }	// namespace BPrivate
 
-#endif	// GROUPED_MENU_H
+
+#endif	// _GROUPED_MENU_H
