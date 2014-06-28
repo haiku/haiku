ef2769426fe2a0bec2985377f99752d8a2cc2c3b
John Scipione <jscipione@gmail.com>
2014-06-27 22:32:57 -0400

    MidiPlayer: Update copyright headers
    Add Haiku, Inc. for all changes 2008-2014 based on commit logs.
    If people want to assign their own copyright for work they did they
    probably should add there name to the copyright list.

--- src/apps/midiplayer/MidiPlayerWindow.h
+++ src/apps/midiplayer/MidiPlayerWindow.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2004 Matthijs Hollemans
- * 
+ * Copyright (c) 2008-2014 Haiku, Inc. All rights reserved.
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a 
  * copy of this software and associated documentation files (the "Software"), 
  * to deal in the Software without restriction, including without limitation 
--- src/apps/midiplayer/SynthBridge.h
+++ src/apps/midiplayer/SynthBridge.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2004 Matthijs Hollemans
+ * Copyright (c) 2008-2014 Haiku, Inc. All rights reserved.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a 
  * copy of this software and associated documentation files (the "Software"), 
