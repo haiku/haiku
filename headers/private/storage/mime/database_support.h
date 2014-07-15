e8c8f8536cbe5870d2ab401c8b2e3cb553956b67
John Scipione <jscipione@gmail.com>
2014-07-14 20:30:29 -0400

    Storage Kit: Style fixes to database_support.h

--- headers/private/storage/mime/database_support.h
+++ headers/private/storage/mime/database_support.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013, Haiku, Inc. All Rights Reserved.
+ * Copyright 2002-2014 Haiku, Inc. All rights reserved.
  * Distributed under the terms of the MIT License.
  */
 #ifndef _MIME_DATABASE_SUPPORT_H
@@ -22,24 +22,24 @@ class DatabaseLocation;
 
 
 // Attribute Prefixes
-extern const char *kMiniIconAttrPrefix;
-extern const char *kLargeIconAttrPrefix;
-extern const char *kIconAttrPrefix;
+extern const char* kMiniIconAttrPrefix;
+extern const char* kLargeIconAttrPrefix;
+extern const char* kIconAttrPrefix;
 
 // Attribute names
-extern const char *kFileTypeAttr;
-extern const char *kTypeAttr;
-extern const char *kAttrInfoAttr;
-extern const char *kAppHintAttr;
-extern const char *kShortDescriptionAttr;
-extern const char *kLongDescriptionAttr;
-extern const char *kFileExtensionsAttr;
-extern const char *kMiniIconAttr;
-extern const char *kLargeIconAttr;
-extern const char *kIconAttr;
-extern const char *kPreferredAppAttr;
-extern const char *kSnifferRuleAttr;
-extern const char *kSupportedTypesAttr;
+extern const char* kFileTypeAttr;
+extern const char* kTypeAttr;
+extern const char* kAttrInfoAttr;
+extern const char* kAppHintAttr;
+extern const char* kShortDescriptionAttr;
+extern const char* kLongDescriptionAttr;
+extern const char* kFileExtensionsAttr;
+extern const char* kMiniIconAttr;
+extern const char* kLargeIconAttr;
+extern const char* kIconAttr;
+extern const char* kPreferredAppAttr;
+extern const char* kSnifferRuleAttr;
+extern const char* kSupportedTypesAttr;
 
 // Attribute Datatypes
 extern const int32 kFileTypeType;
@@ -57,18 +57,18 @@ extern const int32 kSnifferRuleType;
 extern const int32 kSupportedTypesType;
 
 // Message fields
-extern const char *kApplicationsField;
-extern const char *kExtensionsField;
-extern const char *kSupertypesField;
-extern const char *kSupportingAppsSubCountField;
-extern const char *kSupportingAppsSuperCountField;
-extern const char *kTypesField;
+extern const char* kApplicationsField;
+extern const char* kExtensionsField;
+extern const char* kSupertypesField;
+extern const char* kSupportingAppsSubCountField;
+extern const char* kSupportingAppsSuperCountField;
+extern const char* kTypesField;
 
 // Mime types
-extern const char *kGenericFileType;
-extern const char *kDirectoryType;
-extern const char *kSymlinkType;
-extern const char *kMetaMimeType;
+extern const char* kGenericFileType;
+extern const char* kDirectoryType;
+extern const char* kSymlinkType;
+extern const char* kMetaMimeType;
 
 // Error codes (to be used only by BPrivate::Storage::Mime members)
 extern const status_t kMimeGuessFailureError;
@@ -78,8 +78,8 @@ DatabaseLocation* default_database_location();
 
 // Called by BMimeType to get properly formatted icon data ready
 // to be shipped off to SetIcon*() and written to the database
-status_t get_icon_data(const BBitmap *icon, icon_size size, void **data,
-			int32 *dataSize);
+status_t get_icon_data(const BBitmap* icon, icon_size size, void** data,
+	int32* dataSize);
 
 
 } // namespace Mime
