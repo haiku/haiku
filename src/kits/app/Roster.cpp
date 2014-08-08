d53fe46d17d17c1f93f671be65f9ccb36d7f4cca
François Revol <revol@free.fr>
2014-08-09 01:20:25 +0200

    Add a non-const compatibility BRoster::Launch() call
    BartLAuncher needs this one...

--- src/kits/app/Roster.cpp
+++ src/kits/app/Roster.cpp
@@ -992,6 +992,15 @@ Launch__C7BRosterP9entry_refP8BMessagePl(BRoster* roster, entry_ref* ref,
 {
 	return roster->BRoster::Launch(ref, initialMessage, NULL);
 }
+/*!	Just here for providing binary compatibility
+	(for example "BartLauncher" needs this)
+*/
+extern "C" status_t
+Launch__C7BRosterP9entry_refiPPcPl(BRoster* roster, entry_ref* ref,
+	int argc, char * const *args, team_id *app_team)
+{
+	return roster->BRoster::Launch(ref, argc, args, app_team);
+}
 #endif	// __GNUC__ == 2
 
 
