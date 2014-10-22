5dbd0f48cc989ec2652740a4359d571b8f8a7884
Adrien Destugues <pulkomandy@gmail.com>
2014-10-22 18:58:37 +0200

    gcc4 fix.

--- src/tests/kits/media/FormatDescriptions.cpp
+++ src/tests/kits/media/FormatDescriptions.cpp
@@ -30,41 +30,41 @@ FormatDescriptionsTest::TestCompare()
 	a.u.avi.codec = 'DIVX';
 
 	media_format_description b;
-	CPPUNIT_ASSERT(a != b);
+	CPPUNIT_ASSERT(!(a == b));
 
 	b.family = a.family;
-	CPPUNIT_ASSERT(a != b);
+	CPPUNIT_ASSERT(!(a == b));
 
 	a.family = B_QUICKTIME_FORMAT_FAMILY;
 	a.u.quicktime.vendor = 5;
 	a.u.quicktime.codec = 5;
-	
+
 	b.family = B_QUICKTIME_FORMAT_FAMILY;
 	b.u.quicktime.vendor = 6;
 	b.u.quicktime.codec = 5;
 	CPPUNIT_ASSERT(a < b);
 
 	b.u.quicktime.vendor = 4;
-	CPPUNIT_ASSERT(a > b);
+	CPPUNIT_ASSERT(b < a);
 
 	b.u.quicktime.vendor = 5;
 	b.u.quicktime.codec = 6;
 	CPPUNIT_ASSERT(a < b);
 
 	b.u.quicktime.codec = 4;
-	CPPUNIT_ASSERT(a > b);
+	CPPUNIT_ASSERT(b < a);
 
 	a.family = B_MISC_FORMAT_FAMILY;
 	a.u.misc.file_format = 5;
 	a.u.misc.codec = 5;
-	
+
 	b.family = B_MISC_FORMAT_FAMILY;
 	b.u.misc.file_format = 6;
 	b.u.misc.codec = 5;
 	CPPUNIT_ASSERT(a < b);
 
 	b.u.misc.file_format = 4;
-	CPPUNIT_ASSERT(a > b);
+	CPPUNIT_ASSERT(b < a);
 
 
 	b.u.misc.file_format = 5;
@@ -72,7 +72,7 @@ FormatDescriptionsTest::TestCompare()
 	CPPUNIT_ASSERT(a < b);
 
 	b.u.misc.codec = 4;
-	CPPUNIT_ASSERT(a > b);
+	CPPUNIT_ASSERT(b < a);
 }
 
 
