b794d1f947dd2abadafcb3c960bd3a740c7955cb
Ithamar R. Adema <ithamar@upgrade-android.com>
2014-10-31 11:18:05 +0100

    ARM: platform: grab the FDT from the bootloader

--- src/system/kernel/arch/arm/arch_platform.cpp
+++ src/system/kernel/arch/arm/arch_platform.cpp
@@ -9,22 +9,28 @@
 #include <KernelExport.h>
 #include <arch/platform.h>
 #include <boot/kernel_args.h>
-#include <real_time_clock.h>
-#include <util/kernel_cpp.h>
-#include <kernel/debug.h>
+
+#include <drivers/bus/FDT.h>
+
+extern char *gFDT;
 
 status_t
 arch_platform_init(struct kernel_args *kernelArgs)
 {
 	// NOTE: dprintf() is off-limits here, too early...
+
+	// If we have an FDT blob, point the gFDT to it now
+	gFDT = (char *)kernelArgs->platform_args.fdt;
+
 	return B_OK;
 }
-
 
 status_t
 arch_platform_init_post_vm(struct kernel_args *kernelArgs)
 {
-	// now we can use the heap and create areas
+	// no area to create, since we pass the FDT in the kernel_args
+	// and the VM automagically creates the area's (with B_EXACT_ADDRESS)
+	// for them.
 	return B_OK;
 }
 
