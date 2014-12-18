9a539f9e807cafb07ea9c0821883c9cd4614c417
Adrien Destugues <pulkomandy@gmail.com>
2014-12-18 10:06:24 +0100

    Outsource ncurses.

--- headers/libs/ncurses/ncurses.h
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright 2006, Haiku, Inc. All Rights Reserved.
- * Distributed under the terms of the MIT License.
- */
-
-#include <curses.h>
--- src/libs/ncurses/misc/run_tic.sed
+++
@@ -1,4 +0,0 @@
-s%/usr/share/tabset%/usr/local/share/tabset%g
-/^# This is xterm for ncurses/,/^$/{
-	s/use=xterm-new,/use=xterm-new,/
-}
