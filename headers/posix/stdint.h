haiku/haiku

commit 1d13a6090186938a21aeb09c26c383f370c749eb

stdint.h: define [U]INT64[MAX|MIN] with [U]L on x86_64
instead of [U]LL

headers/posix/stdint.h
@@ -69,9 +69,14 @@ typedef uint64_t uintmax_t;
 #define INT32_MIN 	(-INT32_MAX-1)
 #define UINT32_MAX	(4294967295U)
 
-#define INT64_MAX	(9223372036854775807LL)
-#define INT64_MIN	(-INT64_MAX-1)
+#if defined(__SIZEOF_LONG__) && __SIZEOF_LONG__ > 4
+#define INT64_MAX	(9223372036854775807UL)
+#define UINT64_MAX	(18446744073709551615UL)
+#else
+#define INT64_MAX	(9223372036854775807ULL)
 #define UINT64_MAX	(18446744073709551615ULL)
+#endif
+#define INT64_MIN	(-INT64_MAX-1)
 
 /* Limits of minimum-width integer types */
 #define INT_LEAST8_MIN  	INT8_MIN

commit d1dc9cf655f283cf9a8b12f11b6cca7eb5d7a55e

stdint.h: use correct type for INT64_MAX (#11647)
int64_t is signed. Although it does not make a difference by itself, because
INT64_MAX is still a valid number for uint64_t (UL), the later INT64_MIN
declaration depends on INT64_MAX, and therefore got implicitly casted to
unsigned type.

This fixes the following program on a x86_64 system:

	#include <stdint.h>

	int main() {
		int64_t test = 5;
		if (test < INT64_MIN)
			return 1;
		return 0;
	}

This is a regression since commit 1d13a609 ("stdint.h: define [U]INT64[MAX|MIN]
with [U]L on x86_64").

Signed-off-by: Jerome Duval <jerome.duval@gmail.com>

headers/posix/stdint.h
@@ -70,10 +70,10 @@ typedef uint64_t uintmax_t;
 #define UINT32_MAX	(4294967295U)
 
 #if defined(__SIZEOF_LONG__) && __SIZEOF_LONG__ > 4
-#define INT64_MAX	(9223372036854775807UL)
+#define INT64_MAX	(9223372036854775807L)
 #define UINT64_MAX	(18446744073709551615UL)
 #else
-#define INT64_MAX	(9223372036854775807ULL)
+#define INT64_MAX	(9223372036854775807LL)
 #define UINT64_MAX	(18446744073709551615ULL)
 #endif
 #define INT64_MIN	(-INT64_MAX-1)