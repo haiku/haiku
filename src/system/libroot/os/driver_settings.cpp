haiku/haiku

commit 0687a01b53e495083e03aaa50bb223269575b11c

driver_settings: don't strdup(NULL)
* This is not allowed by strdup POSIX specs and GCC may use its builtin
strdup which doesn't check for it.
* also refactor parse_driver_settings_string to create the
settings_handle using settings_new, to reduce code duplication.

src/system/libroot/os/driver_settings.cpp
@@ -820,21 +820,16 @@ load_driver_settings_file(int fd)
 void *
 parse_driver_settings_string(const char *settingsString)
 {
+	if (settingsString == NULL)
+		return NULL;
+
 	// we simply copy the whole string to use it as our internal buffer
 	char *text = strdup(settingsString);
-	if (settingsString == NULL || text != NULL) {
-		settings_handle *handle
-			= (settings_handle*)malloc(sizeof(settings_handle));
-		if (handle != NULL) {
-			handle->magic = SETTINGS_MAGIC;
-			handle->text = text;
-
-			if (parse_settings(handle) == B_OK)
-				return handle;
-
-			free(handle);
-		}
-		free(text);
+	if (text != NULL) {
+		settings_handle *handle = new_settings(text, NULL);
+		if (handle == NULL)
+			free(text);
+		return handle;
 	}
 
 	return NULL;

commit 1736cb1d596076b3f727bbe501b15be4ada7b769

driver_settings: fix allocating an empty settings
I misread the condition and broke this in 0687a01. Thanks to Axel for
reviewing!
* Refactor the code again to move all the error checking at the top of
the function, to make it easier to read.

src/system/libroot/os/driver_settings.cpp
@@ -828,19 +828,18 @@ load_driver_settings_file(int fd)
 void *
 parse_driver_settings_string(const char *settingsString)
 {
-	if (settingsString == NULL)
-		return NULL;
-
-	// we simply copy the whole string to use it as our internal buffer
-	char *text = strdup(settingsString);
-	if (text != NULL) {
-		settings_handle *handle = new_settings(text, NULL);
-		if (handle == NULL)
-			free(text);
-		return handle;
+	char *text = NULL;
+	if (settingsString != NULL) {
+		// we simply copy the whole string to use it as our internal buffer
+		text = strdup(settingsString);
+		if (text == NULL)
+			return NULL;
 	}
 
-	return NULL;
+	settings_handle *handle = new_settings(text, NULL);
+	if (handle == NULL)
+		free(text);
+	return handle;
 }
 
 