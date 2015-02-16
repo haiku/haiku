6d8505a01e03725d3a8dd3c33d8ae8746534dcc2
Jérôme Duval <jerome.duval@gmail.com>
2015-02-16 19:58:26 +0100

    patch: use the outsourced packages.
    * remove patch Jamfile from the build and patch sources from the tree.
    * added patch to DevelopmentBase.
    * should a bootstrap package be added?

--- src/bin/patch/basename.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARAMS
-# if defined PROTOTYPES || (defined __STDC__ && __STDC__)
-#  define PARAMS(Args) Args
-# else
-#  define PARAMS(Args) ()
-# endif
-#endif
-
-char *base_name PARAMS ((char const *name));
--- src/bin/patch/version.h
+++
@@ -1,5 +0,0 @@
-/* Print the version number.  */
-
-/* $Id: version.h 8008 2004-06-16 21:22:10Z korli $ */
-
-void version PARAMS ((void));
