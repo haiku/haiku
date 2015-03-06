8a06abf132f8a6d880ac0aba8016d4a0de4be41b
Alexander von Gluck IV <kallisti5@unixzen.com>
2015-03-05 22:36:27 -0600

    bcm2708: Convert framebuffer driver over to new format
    * Remove from raspberry_pi bootloader (which should die soon)
    * Change to use new arm device layout
    * Create arch_mailbox to check arm mailboxes (WIP)

---
+++ src/system/boot/arch/arm/arch_mailbox.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2011-2015 Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Alexander von Gluck IV, kallisti5@unixzen.com
+ */
+#ifndef _ARCH_MAILBOX_H
+#define _ARCH_MAILBOX_H
+
+
+#include <boot/platform.h>
+#include <SupportDefs.h>
+
+
+#define TRACE_MAILBOX
+#ifdef TRACE_MAILBOX
+#   define TRACE(x...) dprintf(x)
+#	define CALLED() dprintf("%s()\n", __func__);
+#else
+#   define TRACE(x...) ;
+#	define CALLED() ;
+#endif
+#define ERROR(x...) dprintf(x)
+
+
+class ArchMailbox {
+public:
+							ArchMailbox(addr_t base)
+								:
+								fBase(base) {};
+							~ArchMailbox() {};
+
+	virtual status_t		Init() { return B_OK; };
+	virtual status_t		Probe() { return B_OK; };
+
+	virtual addr_t			Base() { return fBase; };
+			addr_t			PhysicalBase() { return fPhysicalBase; };
+
+	virtual	status_t		Write(uint8 channel, uint32 value);
+	virtual status_t		Read(uint8 channel, uint32& value);
+
+
+protected:
+			addr_t			fBase;
+			addr_t			fPhysicalBase;
+};
+
+
+#endif /* _ARCH_MAILBOX_H */
