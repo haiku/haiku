83941255ad61d9820334e8717c05fb7417b8994e
Axel Dörfler <axeld@pinc-software.de>
2015-03-11 09:33:09 +0000

    net_server: cleaned up Services.h, no functional change.

--- src/servers/net/Services.h
+++ src/servers/net/Services.h
@@ -24,38 +24,42 @@ typedef std::map<int, service_connection*> ServiceSocketMap;
 
 
 class Services : public BHandler {
-	public:
-		Services(const BMessage& services);
-		virtual ~Services();
+public:
+								Services(const BMessage& services);
+	virtual						~Services();
 
-		status_t InitCheck() const;
+			status_t			InitCheck() const;
 
-		virtual void MessageReceived(BMessage* message);
+	virtual void				MessageReceived(BMessage* message);
 
-	private:
-		void _NotifyListener(bool quit = false);
-		void _UpdateMinMaxSocket(int socket);
-		status_t _StartService(struct service& service);
-		status_t _StopService(struct service& service);
-		status_t _ToService(const BMessage& message, struct service*& service);
-		void _Update(const BMessage& services);
-		int32 _CompareServices(struct service& a, struct service& b);
-		status_t _LaunchService(struct service& service, int socket);
-		status_t _Listener();
-		static status_t _Listener(void* self);
+private:
+			void				_NotifyListener(bool quit = false);
+			void				_UpdateMinMaxSocket(int socket);
+			status_t			_StartService(struct service& service);
+			status_t			_StopService(struct service& service);
+			status_t			_ToService(const BMessage& message,
+									struct service*& service);
+			void				_Update(const BMessage& services);
+			status_t			_LaunchService(struct service& service,
+									int socket);
+			status_t			_Listener();
+	static	status_t			_Listener(void* self);
 
-		thread_id	fListener;
-		BLocker		fLock;
-		ServiceNameMap fNameMap;
-		ServiceSocketMap fSocketMap;
-		uint32		fUpdate;
-		int			fReadPipe;
-		int			fWritePipe;
-		int			fMinSocket;
-		int			fMaxSocket;
-		fd_set		fSet;
+private:
+			thread_id			fListener;
+			BLocker				fLock;
+			ServiceNameMap		fNameMap;
+			ServiceSocketMap	fSocketMap;
+			uint32				fUpdate;
+			int					fReadPipe;
+			int					fWritePipe;
+			int					fMinSocket;
+			int					fMaxSocket;
+			fd_set				fSet;
 };
 
+
 const static uint32 kMsgUpdateServices = 'srvU';
+
 
 #endif	// SERVICES_H
