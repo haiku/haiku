459e651fd518aaecda9ff6079ea0385456e0be5b
Michael Lotz <mmlr@mlotz.ch>
2015-04-11 11:13:33 +0200

    syscalls: Remove lookup_symbol syscall again.
    This partially reverts b959d46dbd2f9087ae860dbced40440c28596a6e.

--- headers/private/system/syscalls.h
+++ headers/private/system/syscalls.h
@@ -494,10 +494,6 @@ extern status_t		_kern_system_profiler_recorded(
 
 extern ssize_t		_kern_get_stack_trace(size_t addressCount,
 						addr_t* adresses);
-extern status_t		_kern_lookup_symbol(addr_t address, addr_t* baseAddress,
-						char* symbolNameBuffer, size_t symbolNameBufferSize,
-						char* imageNameBuffer, size_t imageNameBufferSize,
-						bool* exactMatch);
 
 /* atomic_* ops (needed for CPUs that don't support them directly) */
 #ifdef ATOMIC_FUNCS_ARE_SYSCALLS
