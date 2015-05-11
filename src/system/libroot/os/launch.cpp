ef462ab57f0ba739629194e6c071815d521c7955
Axel Dörfler <axeld@pinc-software.de>
2015-05-11 09:16:07 +0200

    libroot: ensure that the launch_daemon doesn't wait for itself.
    * The launch_daemon might indirectly call get_launch_daemon_port()
      which may cause a deadlock. Just fail in this case.

--- src/system/libroot/os/launch.cpp
+++ src/system/libroot/os/launch.cpp
@@ -16,8 +16,17 @@ static port_id sLaunchDaemonPort = -1;
 port_id
 BPrivate::get_launch_daemon_port()
 {
-	if (sLaunchDaemonPort < 0)
+	if (sLaunchDaemonPort < 0) {
 		sLaunchDaemonPort = find_port(B_LAUNCH_DAEMON_PORT_NAME);
+
+		port_info info;
+		if (get_port_info(sLaunchDaemonPort, &info) == B_OK
+			&& info.team == find_thread(NULL)) {
+			// Make sure that the launch_daemon doesn't wait on itself
+			sLaunchDaemonPort = -1;
+			return -1;
+		}
+	}
 
 	return sLaunchDaemonPort;
 }
