1f56c11b79c6465688164f0d37cf6a0093543a5a
Jérôme Duval <jerome.duval@gmail.com>
2015-06-10 11:26:51 +0200

    gzip: use the outsourced packages.
    * remove gzip Jamfile from the build and gzip sources from the tree.

--- src/bin/gzip/crypt.c
+++
@@ -1,6 +0,0 @@
-/* crypt.c (dummy version) -- do not perform encryption
- * Hardly worth copyrighting :-)
- */
-#ifdef RCSID
-static char rcsid[] = "$Id: crypt.c 3476 2003-06-11 15:56:10Z darkwyrm $";
-#endif
