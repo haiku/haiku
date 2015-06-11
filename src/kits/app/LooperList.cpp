9bc4bd38116bec4f319fc28f7c96be218b00bacb
Axel Dörfler <axeld@pinc-software.de>
2015-06-11 16:59:45 +0200

    BLooperList: reset list contents after fork.
    * No point in keeping those around; their threads are all gone.

--- src/kits/app/LooperList.cpp
+++ src/kits/app/LooperList.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2001-2011, Haiku.
+ * Copyright 2001-2015, Haiku.
  * Distributed under the terms of the MIT License.
  *
  * Authors:
@@ -190,6 +190,7 @@ BLooperList::InitAfterFork()
 {
 	// We need to reinitialize the locker to get a new semaphore
 	new (&fLock) BLocker("BLooperList lock");
+	fData.clear();
 }
 
 
