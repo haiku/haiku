a495c5e69a3c9185fd4a593a12388aa457e3af35
Philippe Saint-Pierre <stpere@gmail.com>
2015-07-03 14:31:57 -0400

    NetBuffer: Check result of FlattenedSize() for error
    CID 610923

--- src/kits/network/libnetapi/NetBuffer.cpp
+++ src/kits/network/libnetapi/NetBuffer.cpp
@@ -202,7 +202,11 @@ BNetBuffer::AppendMessage(const BMessage& data)
 {
 	char stackFlattenedData[STACK_BUFFER_SIZE];
 
-	size_t dataSize = data.FlattenedSize();
+	ssize_t dataSize = data.FlattenedSize();
+
+	if (dataSize < B_OK)
+		return dataSize;
+
 	if (dataSize == 0)
 		return B_ERROR;
 
