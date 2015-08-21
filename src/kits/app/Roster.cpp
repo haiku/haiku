haiku/haiku

commit 9bfc8331bb5e79bbf0506b89102d65c58feac77a

BRoster: find the registrar port via launch roster.

src/kits/app/Roster.cpp
@@ -42,6 +42,7 @@
 
 #include <AppMisc.h>
 #include <DesktopLink.h>
+#include <LaunchRoster.h>
 #include <MessengerPrivate.h>
 #include <PortLink.h>
 #include <RosterPrivate.h>
@@ -2585,13 +2586,17 @@ BRoster::_InitMessenger()
 	DBG(OUT("BRoster::InitMessengers()\n"));
 
 	// find the registrar port
-	port_id rosterPort = find_port(BPrivate::get_roster_port_name());
-	port_info info;
-	if (rosterPort >= 0 && get_port_info(rosterPort, &info) == B_OK) {
-		DBG(OUT("  found roster port\n"));
+	BMessage data;
+	if (BLaunchRoster().GetData("application/x-vnd.Haiku-registrar", data)
+			== B_OK) {
+		port_id port = data.GetInt32("port", -1);
+		team_id team = data.GetInt32("team", -1);
+		if (port >= 0) {
+			DBG(OUT("  found roster port\n"));
 
-		BMessenger::Private(fMessenger).SetTo(info.team, rosterPort,
-			B_PREFERRED_TOKEN);
+			BMessenger::Private(fMessenger).SetTo(team, port,
+				B_PREFERRED_TOKEN);
+		}
 	}
 
 	DBG(OUT("BRoster::InitMessengers() done\n"));

commit 811f0164a0edc6c7be1fa43f4e16234cfaa2f70a

BRoster: Make sure we aren't the registrar when initializing.
This fixes a regression introduced in 9bfc833, where the old way of
getting the roster port was replaced with its launch_daemon counterpart.

The previous method of finding the roster did always fail when running
from the registrar, as at the time of initialization (initialize_before
of libbe) the registrar looper doesn't exist yet. This commit restores
the previous behaviour by checking if the returned registrar team is the
current team and avoiding initialization in that case.

The regression caused a 5 second boot delay when later BApplication
initialization of the registrar tried to communicate with itself with a
reply timeout of that length.

Fixes #12258 at least partially and might affect #12237.

src/kits/app/Roster.cpp
@@ -2607,9 +2607,13 @@ BRoster::_InitMessenger()
 	BMessage data;
 	if (BLaunchRoster().GetData("application/x-vnd.Haiku-registrar", data)
 			== B_OK) {
+
 		port_id port = data.GetInt32("port", -1);
 		team_id team = data.GetInt32("team", -1);
-		if (port >= 0) {
+
+		if (port >= 0 && team != current_team()) {
+			// Make sure we aren't the registrar ourselves.
+
 			DBG(OUT("  found roster port\n"));
 
 			BMessenger::Private(fMessenger).SetTo(team, port,