1acc73c4374fe0e98448b1a8091af06dbe31075e
Dario Casalinuovo <b.vitruvio@gmail.com>
2015-12-06 15:03:49 +0100

    Deprecate media_realtime_init_* functions

--- src/kits/media/MediaDefs.cpp
+++ src/kits/media/MediaDefs.cpp
@@ -1450,36 +1450,6 @@ launch_media_server(bigtime_t timeout,
 }
 
 
-// #pragma mark -
-
-
-//	Given an image_id, prepare that image_id for realtime media
-//	If the kind of media indicated by "flags" is not enabled for real-time,
-//	B_MEDIA_REALTIME_DISABLED is returned.
-//	If there are not enough system resources to enable real-time performance,
-//	B_MEDIA_REALTIME_UNAVAILABLE is returned.
-status_t
-media_realtime_init_image(image_id image, uint32 flags)
-{
-	UNIMPLEMENTED();
-	return B_OK;
-}
-
-//	Given a thread ID, and an optional indication of what the thread is
-//	doing in "flags", prepare the thread for real-time media performance.
-//	Currently, this means locking the thread stack, up to size_used bytes,
-//	or all of it if 0 is passed. Typically, you will not be using all
-//	256 kB of the stack, so you should pass some smaller value you determine
-//	from profiling the thread; typically in the 32-64kB range.
-//	Return values are the same as for media_prepare_realtime_image().
-status_t
-media_realtime_init_thread(thread_id thread, size_t stack_used, uint32 flags)
-{
-	UNIMPLEMENTED();
-	return B_OK;
-}
-
-
 // #pragma mark - media_encode_info
 
 
