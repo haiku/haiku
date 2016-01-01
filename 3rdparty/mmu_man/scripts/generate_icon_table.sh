0fe022fd0d6d600863555f5a556ab5a5273d20e0
François Revol <revol@free.fr>
2016-01-01 05:53:38 +0100

    Add a little script to generate a montage of all icons
    This needs the 'montage' command from ImageMagick.
    (it will install it with pkgman if missing)
    
    It first uses 'translate' to convert Icon-O-Matic files to PNG,
    then makes a montage with the icon titles.
    
    Run it from the top source folder under Haiku, it might take a while.

---
+++ 3rdparty/mmu_man/scripts/generate_icon_table.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+which montage > /dev/null 2>&1 || pkgman install imagemagick
+
+tmpf=/tmp/$$_icon_
+
+for f in data/artwork/icons/*; do
+  [ -d "$f" ] && continue
+  bn="$(basename "$f")"
+  translate "$f" "${tmpf}${bn}.png" 'PNG '
+  echo "-label"
+  echo "${bn}"
+  echo "${tmpf}${bn}.png"
+done | xargs -d '\n' sh -c 'montage -frame 5 -background "#336699" -geometry +4+4 -font /system/data/fonts/ttfonts/DejaVuSansCondensed.ttf -pointsize 8 "$@" haiku_icons.png' --
+
+rm /tmp/$$_icon_*
