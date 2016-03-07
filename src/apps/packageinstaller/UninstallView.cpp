bb4cd92eae8ab7bf2b39110899cf8de33c592b98
Augustin Cavalier <waddlesplash@gmail.com>
2016-03-07 15:38:24 -0500

    PackageInstaller: Attempt to fix 64-bit build.

--- src/apps/packageinstaller/UninstallView.cpp
+++ src/apps/packageinstaller/UninstallView.cpp
@@ -377,7 +377,7 @@ UninstallView::_ReloadAppList()
 void
 UninstallView::_ClearAppList()
 {
-	while (BListItem* item = fAppList->RemoveItem(0L))
+	while (BListItem* item = fAppList->RemoveItem(0))
 		delete item;
 }
 
