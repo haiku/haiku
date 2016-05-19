e7a7cd4eda64b1c6c6695b2e7cea06235ac08b1b
Dario Casalinuovo <b.vitruvio@gmail.com>
2016-05-19 02:08:39 +0200

    Add streamer_sniff_test
    * This is a simple test that open an url for streaming using
    BMediaFile, the purpose is to test if streamers sniffing work
    well.

---
+++ src/tests/kits/media/streamer_sniff_test/main.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2016, Dario Casalinuovo
+ * Distributed under the terms of the MIT License.
+ */
+
+
+#include <MediaFile.h>
+#include <Url.h>
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+	if (argv[1] == NULL) {
+		printf("Specify an URL or a PATH\n");
+		return 0;
+	}
+
+	printf("Instantiating the BMediaFile\n");
+
+	BUrl* url = new BUrl(argv[1]);
+	if (!url->IsValid()) {
+		printf("Invalid URL\n");
+		return 0;
+	}
+
+	BMediaFile* mediaFile = new BMediaFile(url);
+	if (mediaFile->InitCheck() != B_OK) {
+		printf("Failed creation of BMediaFile!\n");
+		printf("Error: %s\n", strerror(mediaFile->InitCheck()));
+		return 0;
+	}
+
+	printf("Success!\n");
+	sleep(5);
+}
