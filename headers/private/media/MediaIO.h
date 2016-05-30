9217f5c5ed9d7e021f2c09d70963267ffcab74a5
Dario Casalinuovo <b.vitruvio@gmail.com>
2016-05-31 00:37:35 +0200

    BMediaIO: Add GetFlags method to define properties

--- headers/private/media/MediaIO.h
+++ headers/private/media/MediaIO.h
@@ -10,13 +10,23 @@
 #include <SupportDefs.h>
 
 
+enum media_io_flags {
+	B_MEDIA_STREAMING = 0x00000001,
+
+	B_MEDIA_MUTABLE_SIZE = 0x00000002,
+
+	B_MEDIA_SEEK_FORWARD = 0x00000004,
+	B_MEDIA_SEEK_BACKWARD = 0x00000008,
+	B_MEDIA_SEEKABLE = B_MEDIA_SEEK_BACKWARD | B_MEDIA_SEEK_FORWARD
+};
+
+
 class BMediaIO : public BPositionIO {
 public:
 								BMediaIO();
 	virtual						~BMediaIO();
 
-	virtual bool				IsSeekable() const = 0;
-	virtual	bool				IsEndless() const = 0;
+	virtual void				GetFlags(int32* flags) const = 0;
 
 private:
 								BMediaIO(const BMediaIO&);
@@ -28,7 +38,7 @@ private:
 	virtual void				_ReservedMediaIO4();
 	virtual void				_ReservedMediaIO5();
 
-			uint32				_reserved[1];
+			uint32				_reserved[5];
 };
 
 #endif	// _MEDIA_IO_H
