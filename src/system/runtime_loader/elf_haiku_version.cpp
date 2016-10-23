09c4bc5bd39a7908626265a9aa4e6b527784ac17
Adrien Destugues <pulkomandy@gmail.com>
2016-10-23 22:11:14 +0200

    runtime_loader: fix getting ABI from gcc .comment
    The format for the .comment string changed at some point, and this
    fallback code would always fail for files built only with "new" versions
    of gcc.
    
    Also consider gcc 5 to be ABI version 4, since it is still compatible
    with the old ABI (and provides a new one side by side).
    
    See https://gcc.gnu.org/onlinedocs/libstdc++/manual/using_dual_abi.html
    for infos on GCC ABI changes in GCC 5.

--- src/system/runtime_loader/elf_haiku_version.cpp
+++ src/system/runtime_loader/elf_haiku_version.cpp
@@ -87,7 +87,7 @@ analyze_object_gcc_version(int fd, image_t* image, elf_ehdr& eheader,
 	}
 
 	// the common prefix of the strings in the .comment section
-	static const char* kGCCVersionPrefix = "GCC: (GNU) ";
+	static const char* kGCCVersionPrefix = "GCC: (";
 	size_t gccVersionPrefixLen = strlen(kGCCVersionPrefix);
 
 	size_t index = 0;
@@ -116,13 +116,22 @@ analyze_object_gcc_version(int fd, image_t* image, elf_ehdr& eheader,
 		// GCC: (GNU) 2.9-beos-991026
 		// GCC: (GNU) 2.95.3-haiku-080322
 		// GCC: (GNU) 4.1.2
+		// GCC: (2016_02_29) 5.3.0
+
+		// FIXME this does not handle binaries generated with clang or other
+		// compilers.
 
 		// skip the common prefix
 		if (strncmp(stringStart, kGCCVersionPrefix, gccVersionPrefixLen) != 0)
 			continue;
 
+		// Skip the build identifier, the closing parenthesis, and the space
+		// that follows it.
+		// Hopefully no one is going to include nested parentheses in the
+		// version string, so we can save the need for a smarter parser.
+		char* gccVersion = strchr(stringStart + gccVersionPrefixLen, ')') + 2;
+
 		// the rest is the GCC version
-		char* gccVersion = stringStart + gccVersionPrefixLen;
 		char* gccPlatform = strchr(gccVersion, '-');
 		char* patchLevel = NULL;
 		if (gccPlatform != NULL) {
@@ -157,7 +166,7 @@ analyze_object_gcc_version(int fd, image_t* image, elf_ehdr& eheader,
 		// well as cases where e.g. in a gcc 2 program a single C file has
 		// been compiled with gcc 4.
 		if (gccMajor == 0 || gccMajor > version[0]
-		 	|| (gccMajor == version[0]
+			|| (gccMajor == version[0]
 				&& (gccMiddle < version[1]
 					|| (gccMiddle == version[1] && gccMinor < version[2])))) {
 			gccMajor = version[0];
@@ -181,8 +190,15 @@ analyze_object_gcc_version(int fd, image_t* image, elf_ehdr& eheader,
 			image->abi = B_HAIKU_ABI_GCC_2_HAIKU;
 		else
 			image->abi = B_HAIKU_ABI_GCC_2_BEOS;
-	} else
+	} else {
+		if (gccMajor == 5) {
+			// The ABI changes in GCC 5 are optional, and currently we are using
+			// it in backwards compatible mode. So, it is still generating ABI
+			// version 4.
+			gccMajor = 4;
+		}
 		image->abi = gccMajor << 16;
+	}
 
 	return true;
 }
