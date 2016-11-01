56f749a63f63f30bdc3ee15c5ce07717be9da7cc
Adrien Destugues <pulkomandy@pulkomandy.tk>
2016-11-01 11:17:31 +0100

    listusb: split out USB audio dumping to own file.
    The code to dump audio descriptors is now twice as long as the other
    parts of the listusb code. Move it to its own file for clarity.

---
+++ src/bin/listusb/listusb.h
@@ -0,0 +1,18 @@
+/*
+ * Originally released under the Be Sample Code License.
+ * Copyright 2000, Be Incorporated. All rights reserved.
+ *
+ * Modified for Haiku by François Revol and Michael Lotz.
+ * Copyright 2007-2016, Haiku Inc. All rights reserved.
+ */
+
+
+#ifndef LISTUSB_H
+#define LISTUSB_H
+
+
+void DumpDescriptorData(const usb_generic_descriptor* descriptor);
+void DumpAudioDescriptor(const usb_generic_descriptor* descriptor, int subclass);
+
+
+#endif /* !LISTUSB_H */
