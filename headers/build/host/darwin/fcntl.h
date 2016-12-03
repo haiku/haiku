129626b5938e5dafb4d4e93c7ec0c4ddaf46653d
John Scipione <jscipione@gmail.com>
2016-12-03 00:01:51 -0800

    Fix warnings building on newer OS X
    Apple has gotten around to updating their POSIX support so these
    constants are now defined. Add some #ifndef guards to prevent warnings
    during compile.

--- headers/build/host/darwin/fcntl.h
+++ headers/build/host/darwin/fcntl.h
@@ -11,16 +11,26 @@
  * to determine the target of relative file paths in the openat() and
  * similar syscalls.
  */
+#ifndef AT_FDCWD
 #define AT_FDCWD			-100	/* CWD FD for the *at() functions */
+#endif
 
 /*
  * Miscellaneous flags for the *at() syscalls.
  */
+#ifndef AT_EACCESS
 #define AT_EACCESS			0x100	/* faccessat() */
+#endif
+#ifndef AT_SYMLINK_NOFOLLOW
 #define AT_SYMLINK_NOFOLLOW	0x200	/* fstatat(), fchmodat(), fchownat(),
 									   utimensat() */
+#endif
+#ifndef AT_SYMLINK_FOLLOW
 #define AT_SYMLINK_FOLLOW	0x400	/* linkat() */
+#endif
+#ifndef AT_REMOVEDIR
 #define AT_REMOVEDIR		0x800	/* unlinkat() */
+#endif
 
 __BEGIN_DECLS
 
