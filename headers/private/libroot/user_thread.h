194afffd852cc76597f13132f103c534caa98e7e
Jérôme Duval <jerome.duval@gmail.com>
2016-12-17 20:00:47 +0100

    user_thread.h: use struct keyword to be usable in C.

--- headers/private/libroot/user_thread.h
+++ headers/private/libroot/user_thread.h
@@ -12,10 +12,10 @@
 #include <user_thread_defs.h>
 
 
-static inline user_thread*
+static inline struct user_thread*
 get_user_thread()
 {
-	return (user_thread*)tls_get(TLS_USER_THREAD_SLOT);
+	return (struct user_thread*)tls_get(TLS_USER_THREAD_SLOT);
 }
 
 
@@ -29,7 +29,7 @@ defer_signals()
 static void inline
 undefer_signals()
 {
-	user_thread* thread = get_user_thread();
+	struct user_thread* thread = get_user_thread();
 	if (--thread->defer_signals == 0 && thread->pending_signals != 0) {
 		// signals shall no longer be deferred -- call a dummy syscall to handle
 		// the pending ones
