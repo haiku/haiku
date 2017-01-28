3190c83b339fef76b6df775781b90ae8f884b836
Dario Casalinuovo <b.vitruvio@gmail.com>
2017-01-29 00:38:34 +0100

    media_addon_server: Don't set the gain at each start
    * Fixes the forgotten gain setting issue for startup
    sounds.

--- src/servers/media_addon/MediaFilePlayer.cpp
+++ src/servers/media_addon/MediaFilePlayer.cpp
@@ -100,7 +100,6 @@ MediaFilePlayer::MediaFilePlayer(const char* media_type,
 	if (fInitCheck != B_OK)
 		return;
 
-	fSoundPlayer->SetVolume(1.0f);
 	fSoundPlayer->SetHasData(true);
 	fSoundPlayer->Start();
 }
