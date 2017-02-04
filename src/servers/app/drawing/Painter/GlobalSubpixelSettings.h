ae0dad00b7d94696a5d3aa501b5d5e886f2d0a76
Augustin Cavalier <waddlesplash@gmail.com>
2017-02-04 16:06:53 -0500

    app_server: Remove erroenous comment as per Julian's review.

--- src/servers/app/drawing/Painter/GlobalSubpixelSettings.h
+++ src/servers/app/drawing/Painter/GlobalSubpixelSettings.h
@@ -17,10 +17,6 @@ enum {
 	HINTING_MODE_MONOSPACED_ONLY
 };
 
-// Uncommenting the following line will enable the average-based (grayscale)
-// subpixel filter, thus locking RGB subpixel font rendering out of the build.
-// Unless you have a subpixel-enabled FreeType, there shouldn't be a difference,
-// as FreeType returns the grayscale hinting unless compiled otherwise.
 //#define AVERAGE_BASED_SUBPIXEL_FILTERING
 
 extern bool gSubpixelAntialiasing;
