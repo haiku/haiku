100775b79017e688c7f04698f2a4448867068bff
PulkoMandy <pulkomandy@pulkomandy.tk>
2017-02-11 14:15:38 +0100

    Fix build on Linux
    Stub out some translation macros for host build, so the now localized
    package solver can be built there.

---
+++ headers/build/os/locale/Catalog.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2017, Haiku, Inc.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _CATALOG_H_
+#define _CATALOG_H_
+
+#ifndef B_AVOID_TRANSLATION_MACROS
+#define B_TRANSLATE_MARK(x) x
+#define B_TRANSLATE_NOCOLLECT(x) x
+#endif
+
+#endif
