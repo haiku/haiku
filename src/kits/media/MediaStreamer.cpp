dfecb37aeebb73181bcaec48e97ff04e3321cff0
Dario Casalinuovo <b.vitruvio@gmail.com>
2017-05-04 13:57:31 +0200

    Introduce new experimental class MediaStreamer

---
+++ src/kits/media/MediaStreamer.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017, Dario Casalinuovo
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+
+
+#include "MediaStreamer.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "debug.h"
+
+#include "PluginManager.h"
+
+
+MediaStreamer::MediaStreamer(BUrl url)
+	:
+	fStreamer(NULL)
+{
+	CALLED();
+
+	fUrl = url;
+}
+
+
+MediaStreamer::~MediaStreamer()
+{
+	CALLED();
+
+	if (fStreamer != NULL)
+		gPluginManager.DestroyStreamer(fStreamer);
+}
+
+
+status_t
+MediaStreamer::CreateAdapter(BDataIO** adapter)
+{
+	CALLED();
+
+	// NOTE: Consider splitting the streamer creation and
+	// sniff in PluginManager.
+	if (fStreamer != NULL)
+		gPluginManager.DestroyStreamer(fStreamer);
+
+	return gPluginManager.CreateStreamer(&fStreamer, fUrl, adapter);
+}
