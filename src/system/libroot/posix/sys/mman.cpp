0e9000201ff4d3a225490c6ae58547964c9e96ce
Jérôme Duval <jerome.duval@gmail.com>
2017-11-16 16:44:12 +0100

    POSIX: shm_open: set the FD_CLOEXEC file descriptor flag...
    on the new file descriptor.
    Fixes #13774.

--- src/system/libroot/posix/sys/mman.cpp
+++ src/system/libroot/posix/sys/mman.cpp
@@ -178,7 +178,7 @@ shm_open(const char* name, int openMode, mode_t permissions)
 	if (error != B_OK)
 		RETURN_AND_SET_ERRNO(error);
 
-	return open(path, openMode, permissions);
+	return open(path, openMode | FD_CLOEXEC, permissions);
 }
 
 
