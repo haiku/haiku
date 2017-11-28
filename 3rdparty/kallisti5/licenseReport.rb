ef854aff3a5d27191a6dfafc8c248d95fe67a33d
Alexander von Gluck IV <kallisti5@unixzen.com>
2017-11-28 10:25:54 -0600

    3rdparty/kallisti5: License Guess Tool
    * Pair it with a find, and pretty handy to
      highlight sources which have a non-standard,
      missing, or incompatible license.

---
+++ 3rdparty/kallisti5/licenseReport.rb
@@ -0,0 +1,38 @@
+#!/bin/env ruby
+#
+# Source Code License Guesser.
+# Copyright, 2017 Alexander von Gluck IV. All Rights Reserved
+# Released under the terms of the MIT license.
+#
+# Give a file, and I guess the license.
+#
+# Example Usage:
+# 	
+# 	haiku $ find src -name "*.cpp" -exec ./3rdparty/kallisti5/licenseReport.rb {} \;
+#
+
+@file = ARGV.first
+@licenses = [
+	{"MIT" => ["MIT License", "MIT Licence", "Haiku License", "X11 license"]},
+	{"BSD" => ["express or implied warranties", "provided by the author ``AS IS''", "the software is provided \"AS IS\"", "BSD license", "provided by the author \"as is\""]},
+	{"BeOS Sample Code" => ["be sample code license"]},
+	{"GPL" => ["terms of the GNU General Public License", "GNU L-GPL license", "GPL license"]},
+]
+
+def check_license(filename)
+	license = "unknown"
+	lines = File.foreach(filename).first(30).join("\n")
+	return "empty file" if lines == nil
+	@licenses.each do |entry|
+		entry.values.first.each do |pattern|
+			if lines.downcase.include?(pattern.downcase)
+				license = entry.keys.first
+				break
+			end
+		end
+		break if license != "unknown"
+	end
+	license
+end
+
+puts "#{@file}: #{check_license(@file)}"
