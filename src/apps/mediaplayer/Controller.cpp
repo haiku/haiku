3065fd47a0564e12b96cddf8d011befffe2294b6
Alexander von Gluck IV <kallisti5@unixzen.com>
2017-12-13 14:37:46 -0600

    MediaPlayer: Relicense to MIT
    * The GPL license here was a relic from the past to help
      control YellowTab's use of Haiku source code. The intent
      at the time was to relicense MIT.  Some GPL parts remained.
    * This is going to get harder and harder to correct over time.
    * This code makes Haiku *not* MIT. Lets correct that.
    * I reached out to everyone who made changes to these files and
      got personal approvals to relicense MIT.
    * If I missed somebody, *and* you feel as though the MIT license
      doesn't meet your needs, please contact Haiku, Inc. and we will
      work to revert your changes.
    
      Marcus Overhagen      EMAIL   - MIT OK 11/29/17
      Stephan Aßmus         EMAIL   - MIT OK 11/28/17
      Axel Dörfler          IRC     - MIT OK 11/29/17
      Ryan Leavengood       KEYBASE - MIT OK 11/28/17
      Michael Lotz          IRC     - MIT OK 11/29/17
      Adrien Destugues      EMAIL   - MIT OK 11/28/17
      Joachim Seemer        EMAIL   - MIT OK 11/28/17
      Jonas Sundström       EMAIL   - MIT OK 11/29/17
      François Revol        IRC     - MIT OK 11/29/17
      Jérôme Duval          EMAIL   - MIT OK 11/29/17
      Oliver Tappe          EMAIL   - MIT OK 11/29/17
      Dziadek               EMAIL   - MIT OK 11/29/17
      Philippe Saint-Pierre EMAIL   - MIT OK 12/6/17
      Philippe Houdoin      EMAIL   - MIT OK 12/8/17
      Dario Casalinuovo     IRC     - MIT OK 12/13/17

--- src/apps/mediaplayer/Controller.cpp
+++ src/apps/mediaplayer/Controller.cpp
@@ -2,22 +2,10 @@
  * Controller.cpp - Media Player for the Haiku Operating System
  *
  * Copyright (C) 2006 Marcus Overhagen <marcus@overhagen.de>
- * Copyright (C) 2007-2008 Stephan Aßmus <superstippi@gmx.de> (MIT Ok)
- * Copyright (C) 2007-2009 Fredrik Modéen <[FirstName]@[LastName].se> (MIT ok)
+ * Copyright (C) 2007-2008 Stephan Aßmus <superstippi@gmx.de>
+ * Copyright (C) 2007-2009 Fredrik Modéen <[FirstName]@[LastName].se>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
+ * Released under the terms of the MIT license.
  */
 
 
--- src/apps/mediaplayer/ControllerView.h
+++ src/apps/mediaplayer/ControllerView.h
@@ -5,19 +5,7 @@
  * Copyright (C) 2007 Stephan Aßmus <superstippi@gmx.de>
  * Copyright (C) 2008-2009 Fredrik Modéen 	<[FirstName]@[LastName].se> (MIT ok)
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
+ * Released under the terms of the MIT license.
  */
 #ifndef __CONTROLLER_VIEW_H
 #define __CONTROLLER_VIEW_H
