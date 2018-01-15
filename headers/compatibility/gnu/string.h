1a59e1d91c3e9a69cc0fae1314359cf5a5045c65
Leorize <alaviss@users.noreply.github.com>
2018-01-15 20:00:23 +0700

    gnu: introduce memmem(3)
    Implementation imported from OpenBSD
    
    Change-Id: If2620dc7ed373650bb3dabb0f22c2cd84b9e0ef9

---
+++ headers/compatibility/gnu/string.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2018 Haiku, Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _GNU_STRING_H_
+#define _GNU_STRING_H_
+
+
+#include_next <string.h>
+
+
+#ifdef _GNU_SOURCE
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void *memmem(const void *source, size_t sourceLength,
+	const void *search, size_t searchLength);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
+
+
+#endif	/* _GNU_STRING_H_ */
