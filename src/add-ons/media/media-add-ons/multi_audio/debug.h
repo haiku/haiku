401c837afce92beea2e767c9008d2b36b2289b91
Adrien Destugues <pulkomandy@pulkomandy.tk>
2018-04-28 08:44:13 +0200

    multi_audio: remove some unused debug macros
    We were #defining DEBUG here, which leads to some system-wide debugging
    macros being always enabled as well (PRINT_OBJECT and the like). This
    should be managed only using SetConfigVar in UserBuildConfig.
    
    Remove the unused UNIMPLEMENTED and BROKEN macros because everything is
    implemented, and nothing is currently known to be broken :)

--- src/add-ons/media/media-add-ons/multi_audio/debug.h
+++ src/add-ons/media/media-add-ons/multi_audio/debug.h
@@ -3,22 +3,6 @@
 
 #ifndef NDEBUG
 
-  #ifndef DEBUG
-  	#define DEBUG 2
-  #endif
-
-  #if DEBUG >= 1
-	#define UNIMPLEMENTED()		printf("UNIMPLEMENTED %s\n",__PRETTY_FUNCTION__)
-  #else
-  	#define UNIMPLEMENTED()		((void)0)
-  #endif
-
-  #if DEBUG >= 2
-	#define BROKEN()			printf("BROKEN %s\n",__PRETTY_FUNCTION__)
-  #else
-  	#define BROKEN()			((void)0)
-  #endif
-
   #if DEBUG >= 3
 	#define CALLED() 			printf("CALLED %s\n",__PRETTY_FUNCTION__)
   #else
@@ -27,8 +11,6 @@
 	
 #else
 
-	#define UNIMPLEMENTED() 	((void)0)
-	#define BROKEN()			((void)0)
 	#define CALLED()			((void)0)
 
 #endif
