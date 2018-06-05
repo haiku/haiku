4f059c1fc51d17a2a592c5675a2f188b4bea2f69
Augustin Cavalier <waddlesplash@gmail.com>
2018-06-05 17:27:45 +0000

    intel_extreme: Broadwell is really Gen7(.5), not 8.

--- src/add-ons/kernel/drivers/graphics/intel_extreme/driver.cpp
+++ src/add-ons/kernel/drivers/graphics/intel_extreme/driver.cpp
@@ -104,14 +104,14 @@ const struct supported_device {
 	{0x0d26, INTEL_MODEL_HASM, "Haswell Mobile"},
 	{0x0a16, INTEL_MODEL_HASM, "Haswell Mobile"},
 
+//	{0x1616, INTEL_MODEL_BDWM, "HD Graphics 5500 (Broadwell GT2)"},
+
 	{0x0155, INTEL_MODEL_VLV, "ValleyView Desktop"},
 	{0x0f30, INTEL_MODEL_VLVM, "ValleyView Mobile"},
 	{0x0f31, INTEL_MODEL_VLVM, "ValleyView Mobile"},
 	{0x0f32, INTEL_MODEL_VLVM, "ValleyView Mobile"},
 	{0x0f33, INTEL_MODEL_VLVM, "ValleyView Mobile"},
 	{0x0157, INTEL_MODEL_VLVM, "ValleyView Mobile"},
-
-//	{0x1616, INTEL_MODEL_BDWM, "HD Graphics 5500 (Broadwell GT2)"},
 
 	{0x1902, INTEL_MODEL_SKY,  "Skylake GT1"},
 	{0x1906, INTEL_MODEL_SKYM, "Skylake GT1"},
