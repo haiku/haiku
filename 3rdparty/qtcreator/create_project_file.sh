b31cb92f29fe89eaca84d173d0f70d38bf0c6a3d
Augustin Cavalier <waddlesplash@gmail.com>
2018-06-24 23:13:09 -0400

    3rdparty/qtcreator: Support for multiple directories in one project.

--- 3rdparty/qtcreator/create_project_file.sh
+++ 3rdparty/qtcreator/create_project_file.sh
@@ -1,27 +1,34 @@
 #!/bin/sh
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -lt 2 ]; then
 	echo "This script creates project files for Qt Creator to develop Haiku with."
 	echo "It should only be used on a per-project basis, as Qt Creator is too slow"
 	echo "when used on all of Haiku at once."
 	echo ""
 	echo "THIS SCRIPT *MUST* BE RUN FROM THE REPOSITORY ROOT."
 	echo ""
-	echo "Usage: <script> <project name> <path to project root>"
-	echo "e.g: create_project_file.sh Tracker src/kits/tracker/"
+	echo "Usage: <script> <project name> <path 1> <path 2> ..."
+	echo "e.g: create_project_file.sh Tracker src/kits/tracker/ src/apps/tracker/"
 	exit 1
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NAME=$1
-ROOTDIR=$2
+ROOTDIRS=${@:2}
 
 printf "// Add predefined macros for your project here. For example:\n// #define THE_ANSWER 42\n" \
 	>$DIR/$NAME.config
 printf "[General]\n" >$DIR/$NAME.creator
 
-# Build lists of files
-find $ROOTDIR -type f | sed "s@^@../../@" >$DIR/$NAME.files
-find $ROOTDIR -type d | sed "s@^@../../@" >$DIR/$NAME.includes
+# Clear the old file lists
+echo >$DIR/$NAME.files
+echo >$DIR/$NAME.includes
+
+# Build file lists
+for rootdir in $ROOTDIRS; do
+	find $rootdir -type f | sed "s@^@../../@" >>$DIR/$NAME.files
+	find $rootdir -type d | sed "s@^@../../@" >>$DIR/$NAME.includes
+done
 find headers -type d | sed "s@^@../../@" >>$DIR/$NAME.includes
+
 echo "Done. Project file: $DIR/$NAME.creator"
