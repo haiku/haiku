25cd28e8c17473039735b01fdd5e06a7c8bf7825
Augustin Cavalier <waddlesplash@gmail.com>
2018-08-02 19:59:30 -0400

    posix: Add initial ARM64 headers.
    Almost directly copied from jpelczar's work.

---
+++ headers/posix/arch/arm64/signal.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2017 Jaroslaw Pelczar <jarek@jpelczar.com>
+ * Distributed under the terms of the MIT License.
+ */
+
+#ifndef _ARCH_SIGNAL_H_
+#define _ARCH_SIGNAL_H_
+
+
+/*
+ * Architecture-specific structure passed to signal handlers
+ */
+
+#if defined(__arm64__)
+struct vregs
+{
+	ulong		x[30];
+	ulong		lr;
+	ulong		sp;
+	ulong		elr;
+	ulong		spsr;
+	__uint128_t	fp_q[32];
+	u_int		fpsr;
+	u_int		fpcr;
+};
+#endif /* defined(__arm64__) */
+
+
+#endif /* _ARCH_SIGNAL_H_ */
