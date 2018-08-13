0f36120d8dc438596a6040c8a1b636116fcfff67
Augustin Cavalier <waddlesplash@gmail.com>
2018-08-12 23:38:10 -0400

    "R5" fs_shell: Remove (mostly.)
    Two files remain: "argv.c" and "argv.h", as these are still used by some
    other, non-FS-related tests.
    
    This was one of the last significant chunks of "no commercial use" code
    in the tree, as it originally came from the "Filesystem Construction Kit,"
    the sample code distributed with "Practical Filesystem Design with the Be FS",
    and contained a sigifincant amount of BeOS kernel filesystem code in it
    (the original README, not preserved here, noted that "the cache code is
    the real Release 4 BeOS disk cache code".)
    
    It has long been replaced by Ingo's from-scratch, MIT-licensed fs_shell
    that lives in src/tools/fs_shell. Perhaps a note should be made somewhere
    of this code's prior existence, but I don't think we will have much use for
    it again, so confined to the dustbin of Git history it shall be.

--- src/tests/add-ons/kernel/file_systems/fs_shell/initfs.h
+++
@@ -1,2 +0,0 @@
-void *init_fs(char *disk_name);
-status_t initialize_fs(const char *deviceName, void *params, int paramLength);
--- src/tests/add-ons/kernel/file_systems/fs_shell/mount.h
+++
@@ -1,5 +0,0 @@
-myfs_info *myfs_create_fs(char *device, char *volname,
-                          int block_size, char *opts);
-int        myfs_mount(nspace_id nsid, const char *device, ulong flags,
-                      void *parms, size_t len, void **data, vnode_id *vnid);
-int        myfs_unmount(void *ns);
