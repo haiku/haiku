dc1991b5d5914fbd51292cf8e3c78eecbaf2b5d8
Humdinger <humdingerb@gmail.com>
2018-08-30 18:18:40 +0200

    Installer: update 'EULA' window
    * De-escalate the severety of data loss from "high risk" to "risk".
    
    * Remove the advice to not create partitions with DriveSetup. It's been tested
      and been working well since the last alpha. Do recommend making backups of
      all partitions.
    
    * Remove description how to add Haiku to Grub. It's too much detail and the
      user would have to memorize the info. Add a link to our Booting guide
      instead.
    
    * Mention BootManager as an alternative to boot Haiku, refering to the
      Userguide for more info.
    
    * Alpha -> Beta
    
    * Only give normal thanks instead of "a lot" and don't beg to like us.
      Sounded a bit desperate...
    
    Change-Id: I212513fde01287f5d1c9c5c9891827aaf0b7e1ea
    Reviewed-on: https://review.haiku-os.org/502
    Reviewed-by: waddlesplash <waddlesplash@gmail.com>
    (cherry picked from commit 1edc131debcf8ea1c9cd9eafd41c356f0432d9a5)
    Reviewed-on: https://review.haiku-os.org/508

--- src/apps/installer/EULAWindow.cpp
+++ src/apps/installer/EULAWindow.cpp
@@ -37,89 +37,34 @@ EULAWindow::EULAWindow()
 	infoText << B_TRANSLATE(
 		"IMPORTANT INFORMATION BEFORE INSTALLING HAIKU\n\n");
 	infoText << B_TRANSLATE(
-		"This is alpha-quality software! It means there is a high risk of "
+		"This is beta-quality software! It means there is a risk of "
 		"losing important data. Make frequent backups! You have been "
 		"warned.\n\n\n");
 	infoText << B_TRANSLATE(
 		"1)   If you are installing Haiku onto real hardware (not inside an "
-		"emulator) it is recommended that you have already prepared a hard "
-		"disk partition. The Installer and the DriveSetup tool offer to "
-		"initialize existing partitions with the Haiku native file system, "
-		"but the options to change the actual partition layout may not have "
-		"been tested on a sufficiently great variety of computer "
-		"configurations so we do not recommend using it.\n");
-	infoText << B_TRANSLATE(
-		"If you have not created a partition yet, simply reboot, create the "
-		"partition using whatever tool you feel most comfortable with, and "
-		"reboot into Haiku to continue with the installation. You could for "
-		"example use the GParted Live-CD, it can also resize existing "
-		"partitions to make room.\n\n\n");
+		"emulator), you may want to prepare a hard disk partition from "
+		"another OS (you could, for example, use a GParted Live-CD, which "
+		"can also resize existing partitions to make room).\n"
+		"You can also set up partitions by launching DriveSetup from "
+		"Installer, but you won't be able to resize existing partitions with "
+		"it. While DriveSetup has been quite thoroughly tested over the "
+		"years, it's recommended to have up-to-date backups of the other "
+		"partitions on your system. Just in case" B_UTF8_ELLIPSIS);
+	infoText << "\n\n\n";
 	infoText << B_TRANSLATE(
 		"2)   The Installer will make the Haiku partition itself bootable, "
 		"but takes no steps to integrate Haiku into an existing boot menu. "
-		"If you have GRUB already installed, you can add Haiku to its boot "
-		"menu. Depending on what version of GRUB you use, this is done "
-		"differently.\n\n\n");
-	infoText << B_TRANSLATE(
-		"2.1) GRUB (since os-prober v1.44)\n");
-	infoText << B_TRANSLATE(
-		"Starting with os-prober v1.44 (e.g. in Ubuntu 11.04 or later), Haiku "
-		"should be recognized out of the box. To add Haiku to the GRUB menu, "
-		"open a Terminal and enter:\n\n");
-	infoText << B_TRANSLATE(
-		"\tsudo update-grub\n\n\n");
-	infoText << B_TRANSLATE(
-		"2.2) GRUB 2\n");
-	infoText << B_TRANSLATE(
-		"If the os-prober approach doesn't work for you, GRUB 2 uses an extra "
-		"configuration file to add custom entries to the boot menu. To add "
-		"them to the top, you have to create/edit a file by launching your "
-		"favorite editor from a Terminal like this:\n\n");
-	infoText << B_TRANSLATE(
-		"\tsudo <your favorite text editor> /etc/grub.d/40_custom\n\n");
-	infoText << B_TRANSLATE(
-		"GRUB's naming scheme for partitions is: (hdN,n)\n\n");
-	infoText << B_TRANSLATE(
-		"All hard disks start with \"hd\".\n");
-	infoText << B_TRANSLATE(
-		"\"N\" is the hard disk number, starting with \"0\".\n");
-	infoText << B_TRANSLATE(
-		"\"n\" is the partition number, which for GRUB 2 starts with \"1\"\n");
-	infoText << B_TRANSLATE(
-		"With GRUB 2 the first logical partition always has the number \"5\", "
-		"regardless of the number of primary partitions.\n\n");
-	infoText << B_TRANSLATE(
-		"So below the heading that must not be edited, add something similar "
-		"to these lines:\n\n");
-	infoText << B_TRANSLATE(
-		"\t# Haiku on /dev/sda7\n");
-	infoText << B_TRANSLATE(
-		"\tmenuentry \"Haiku Alpha\" {\n");
-	infoText << B_TRANSLATE(
-		"\t\tset root=(hd0,7)\n");
-	infoText << B_TRANSLATE(
-		"\t\tchainloader +1\n");
-	infoText << B_TRANSLATE(
-		"\t}\n\n");
-	infoText << B_TRANSLATE(
-		"Additionally you have to edit another file to actually display the "
-		"boot menu:\n\n");
-	infoText << B_TRANSLATE(
-		"\tsudo <your favorite text editor> /etc/default/grub\n\n");
-	infoText << B_TRANSLATE(
-		"Here you have to comment out the line \"GRUB_HIDDEN_TIMEOUT=0\" by "
-		"putting a \"#\" in front of it in order to actually display the "
-		"boot menu.\n\n");
-	infoText << B_TRANSLATE(
-		"Finally, you have to update the boot menu by entering:\n\n");
-	infoText << B_TRANSLATE(
-		"\tsudo update-grub\n\n\n");
+		"If you have GRUB already installed, you can add Haiku to it.\n"
+		"For details, please consult the guide on booting Haiku on our "
+		"website at https://www.haiku-os.org/guides/booting.\n"
+		"Or you can set up a boot menu from Installer's \"Tools\" menu, see "
+		"the Userguide's topic on the application \"BootManager\".\n\n\n");
 	infoText << B_TRANSLATE(
 		"3)   When you successfully boot into Haiku for the first time, make "
 		"sure to read our \"Welcome\" and \"Userguide\" documentation. There "
 		"are links on the Desktop and in WebPositive's bookmarks.\n\n");
 	infoText << B_TRANSLATE(
-		"Have fun and thanks a lot for trying out Haiku! We hope you like it!");
+		"Have fun and thanks for trying out Haiku!");
 
 	BTextView* textView = new BTextView("eula", be_plain_font, NULL, B_WILL_DRAW);
 	textView->SetInsets(10, 10, 10, 10);
