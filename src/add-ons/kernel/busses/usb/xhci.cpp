7f644d106dccfa3780981e084523ac740e5b8bc0
Alexander von Gluck IV <kallisti5@unixzen.com>
2018-09-09 13:50:55 -0500

    xhci: gcc2 buildfix from hrev52319
    Change-Id: I5d82059bd002c26ba372d55f5a5538ce7d19ffb2

--- src/add-ons/kernel/busses/usb/xhci.cpp
+++ src/add-ons/kernel/busses/usb/xhci.cpp
@@ -2480,11 +2480,12 @@ XHCI::WaitOpBits(uint32 reg, uint32 mask, uint32 expected)
 		snooze(1000);
 		value = ReadOpReg(reg);
 		if (loops == 25) {
-			TRACE("delay waiting on reg 0x%x match 0x%x (0x%x)\n",
-				reg, expected, mask);
+			TRACE("delay waiting on reg 0x%" B_PRIX32 " match 0x%" B_PRIX32
+				" (0x%" B_PRIX32 ")\n",	reg, expected, mask);
 		} else if (loops > 100) {
-			TRACE_ERROR("timeout waiting on reg 0x%x match 0x%x (0x%x)\n",
-				reg, expected, mask);
+			TRACE_ERROR("timeout waiting on reg 0x%" B_PRIX32
+				" match 0x%" B_PRIX32 " (0x%" B_PRIX32 ")\n", reg, expected,
+				mask);
 			return B_ERROR;
 		}
 		loops++;
