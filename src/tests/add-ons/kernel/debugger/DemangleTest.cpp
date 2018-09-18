055a35e632c4f65a757047dc9ba3ad9a59f7187c
Augustin Cavalier <waddlesplash@gmail.com>
2018-09-17 23:49:17 -0400

    tests: Add some more things to DemangleTest.
    Change-Id: I6999aad2cc64650bddb7ba575536efc8878abcf2
    Reviewed-on: https://review.haiku-os.org/583
    Reviewed-by: waddlesplash <waddlesplash@gmail.com>

--- src/tests/add-ons/kernel/debugger/DemangleTest.cpp
+++ src/tests/add-ons/kernel/debugger/DemangleTest.cpp
@@ -28,17 +28,29 @@ DemangleTest::~DemangleTest()
 void
 DemangleTest::RunGCC2Tests()
 {
-	TEST("", "()");
+	// Long and complex things
 	TEST("BPrivate::IconCache::SyncDraw(BPrivate::Model*, BView*, BPoint, BPrivate::IconDrawMode, icon_size, void*, void*)",
 		"SyncDraw__Q28BPrivate9IconCachePQ28BPrivate5ModelP5BViewG6BPointQ28BPrivate12IconDrawMode9icon_sizePFP5BViewG6BPointP7BBitmapPv_vPv");
+	TEST("BPrivate::BContainerWindow::UpdateMenu(BMenu*, BPrivate::BContainerWindow::UpdateMenuContext)",
+		"UpdateMenu__Q28BPrivate16BContainerWindowP5BMenuQ38BPrivate16BContainerWindow17UpdateMenuContext");
+	TEST("icu_57::BreakIterator::registerInstance(icu_57::BreakIterator*, icu_57::Locale&, UBreakIteratorType, UErrorCode&)",
+		"registerInstance__Q26icu_5713BreakIteratorPQ26icu_5713BreakIteratorRCQ26icu_576Locale18UBreakIteratorTypeR10UErrorCode");
+
+	// Previously caused crashes
+	TEST("SetTo__Q282_GLOBAL_", "SetTo__Q282_GLOBAL_");
 }
 
 
 void
 DemangleTest::RunGCC3PTests()
 {
+	// Long and complex things
 	TEST("BPrivate::IconCache::SyncDraw(BPrivate::Model*, BView*, BPoint, BPrivate::IconDrawMode, icon_size, void (*)(BView*, BPoint, BBitmap*, void*), void*)",
 		"_ZN8BPrivate9IconCache8SyncDrawEPNS_5ModelEP5BView6BPointNS_12IconDrawModeE9icon_sizePFvS4_S5_P7BBitmapPvESA_");
+	TEST("BPrivate::BContainerWindow::UpdateMenu(BMenu*, BPrivate::BContainerWindow::UpdateMenuContext)",
+		"_ZN8BPrivate16BContainerWindow10UpdateMenuEP5BMenuNS0_17UpdateMenuContextE");
+	TEST("icu_57::BreakIterator::registerInstance(icu_57::BreakIterator*, icu_57::Locale const&, UBreakIteratorType, UErrorCode&)",
+		"_ZN6icu_5713BreakIterator16registerInstanceEPS0_RKNS_6LocaleE18UBreakIteratorTypeR10UErrorCode");
 }
 #undef TEST
 
