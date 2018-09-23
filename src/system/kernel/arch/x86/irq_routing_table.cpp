c609f04f86a93cc07d562cedb6e53bfee7d862f8
Augustin Cavalier <waddlesplash@gmail.com>
2018-09-23 14:19:25 -0400

    kernel: Print interrupt line when assuming no interrupt use on PCI device.

--- src/system/kernel/arch/x86/irq_routing_table.cpp
+++ src/system/kernel/arch/x86/irq_routing_table.cpp
@@ -584,8 +584,8 @@ ensure_all_functions_matched(pci_module_info* pci, uint8 bus,
 				// (PCI Local Bus spec 3.0, section 6.2.4 / page 223, footnote.)
 				if (interrupt_line == 0 || interrupt_line == 255) {
 					dprintf("assuming no interrupt use on PCI device"
-						" %u:%u:%u (bios irq 0, no routing information)\n",
-						bus, device, function);
+						" %u:%u:%u (bios irq 0, interrupt line %" B_PRId32 ")\n",
+						bus, device, function, interrupt_line);
 					continue;
 				}
 
