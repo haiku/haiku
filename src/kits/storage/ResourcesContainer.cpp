5475c081e25cedad10bd23c29f3a2415ebbf5830
Murai Takashi <tmurai01@gmail.com>
2018-10-15 20:15:44 +0900

    AddResource(): use index instead of count
    In AddResource(), parameter 'index' is not used.
    It seems AddItem() use index instead of count.
    
    Change-Id: I997ac96b7d32c5705606cdbf23c7fd71550c9aa6
    Reviewed-on: https://review.haiku-os.org/630
    Reviewed-by: Rene Gollent <rene@gollent.com>

--- src/kits/storage/ResourcesContainer.cpp
+++ src/kits/storage/ResourcesContainer.cpp
@@ -44,7 +44,7 @@ ResourcesContainer::AddResource(ResourceItem *item, int32 index,
 		int32 count = CountResources();
 		if (index < 0 || index > count)
 			index = count;
-		result = fResources.AddItem(item, count);
+		result = fResources.AddItem(item, index);
 		SetModified(true);
 	}
 	return result;
