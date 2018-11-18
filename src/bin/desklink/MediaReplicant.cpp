e980073d228b778967d68344b708a8ac65d80e2f
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2018-11-18 15:05:03 +0100

    Clarify desklink translatable
    Change-Id: Ia36e44ae4c6fec37694712dd1388cd1abb867f31
    Reviewed-on: https://review.haiku-os.org/712
    Reviewed-by: waddlesplash <waddlesplash@gmail.com>

--- src/bin/desklink/MediaReplicant.cpp
+++ src/bin/desklink/MediaReplicant.cpp
@@ -487,8 +487,9 @@ MediaReplicant::_Launch(const char* prettyName, const char* signature,
 		BString message = B_TRANSLATE("Couldn't launch ");
 		message << prettyName;
 
-		BAlert* alert = new BAlert(B_TRANSLATE("desklink"), message.String(),
-			B_TRANSLATE("OK"));
+		BAlert* alert = new BAlert(
+			B_TRANSLATE_COMMENT("desklink", "Title of an alert box"), 
+			message.String(), B_TRANSLATE("OK"));
 		alert->SetFlags(alert->Flags() | B_CLOSE_ON_ESCAPE);
 		alert->Go();
 	}
