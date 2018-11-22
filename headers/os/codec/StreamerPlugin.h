06b088bd627c1ab9125d0aa5115c39fea23bab31
Barrett17 <b.vitruvio@gmail.com>
2018-11-22 09:32:02 +0100

    Streamer: Make constructor private

--- headers/os/codec/StreamerPlugin.h
+++ headers/os/codec/StreamerPlugin.h
@@ -1,9 +1,12 @@
+/*
+ * Copyright 2017, Dario Casalinuovo. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
 #ifndef _STREAMER_PLUGIN_H
 #define _STREAMER_PLUGIN_H
 
 
 #include <MediaIO.h>
-#include <MediaTrack.h>
 #include <Url.h>
 
 #include "MediaPlugin.h"
@@ -16,15 +19,17 @@ class PluginManager;
 
 class Streamer {
 public:
+	virtual status_t			Sniff(const BUrl& url, BDataIO** source) = 0;
+
+protected:
 								Streamer();
 	virtual						~Streamer();
-
-	virtual status_t			Sniff(const BUrl& url, BDataIO** source) = 0;
 
 private:
 			MediaPlugin*		fMediaPlugin;
 
 	friend class PluginManager;
+	friend class MediaStreamer;
 
 	virtual void				_ReservedStreamer1();
 	virtual void				_ReservedStreamer2();
