6e48605b8feba5cb78f73437a2f34ac4c675b5da
Alexander von Gluck IV <kallisti5@unixzen.com>
2018-12-08 12:44:18 -0600

    kernel: Add riscv config_manager and kernel lib arch sources
    Change-Id: Idb40e98f4f664e6bfe0af6bc098bae5c871fb4e5

---
+++ src/add-ons/kernel/bus_managers/config_manager/arch/riscv32/config_manager_arch.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2009 Haiku, Inc.
+ * Distributed under the terms of the MIT License.
+ *
+ * arch-specific config manager
+ *
+ * Authors (in chronological order):
+ *              François Revol (revol@free.fr)
+ */
+
+#include <OS.h>
+#include <config_manager.h>
+
+int config_manager_scan_hardcoded(struct device_info **info, int32 *count)
+{
+	return B_OK;
+}
+
