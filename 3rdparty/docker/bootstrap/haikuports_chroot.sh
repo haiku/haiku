f4dca556adff9b54f53f9f82ca31e8f6add73477
Alexander von Gluck IV <kallisti5@unixzen.com>
2019-02-15 11:51:11 -0600

    bootstrap: Fix PS1 to solve entering chroot via haikuporter
    Change-Id: Ida03530aea9ab5075af3a951cf5804d7c5694490

--- 3rdparty/docker/bootstrap/haikuports_chroot.sh
+++ 3rdparty/docker/bootstrap/haikuports_chroot.sh
@@ -8,6 +8,9 @@ fi
 export GENERATED=$WORKPATH/generated.$TARGET_ARCH
 export LD_LIBRARY_PATH=$GENERATED/objects/linux/lib/:$LD_LIBRARY_PATH
 
+# A hack to fix haikuport chroot not liking Debian's fancy PS1
+export PS1='\u@\h:\w\$'
+
 $WORKPATH/src/haikuporter/haikuporter \
 	--config=$GENERATED/objects/haiku/$TARGET_ARCH/packaging/repositories/HaikuPortsCross-build/haikuports.conf \
 	--cross-devel-package $GENERATED/objects/haiku/$TARGET_ARCH/packaging/packages/haiku_cross_devel_sysroot_stage1_$TARGET_ARCH.hpkg \
