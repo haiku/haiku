40fcf16358a810cb8a1527a63dfb1cd34b2d6943
Alexander von Gluck IV <kallisti5@unixzen.com>
2019-04-04 19:20:14 +0000

    kernel/riscv64: Add missing kernel_arg ranges to get build going.
    Change-Id: Idf549a5e4d01e6dc8949ec1252296337e5ef970c
    Reviewed-on: https://review.haiku-os.org/c/1380
    Reviewed-by: Adrien Destugues <pulkomandy@gmail.com>

--- headers/private/kernel/arch/riscv64/arch_kernel_args.h
+++ headers/private/kernel/arch/riscv64/arch_kernel_args.h
@@ -9,12 +9,21 @@
 #	error This file is included from <boot/kernel_args.h> only
 #endif
 
+#define _PACKED __attribute__((packed))
+
+#define MAX_VIRTUAL_RANGES_TO_KEEP      32
+
+
 // kernel args
 typedef struct {
 	// architecture specific
 	uint64		phys_pgdir;
 	uint64  	vir_pgdir;
 	uint64		next_pagetable;
+
+	// The virtual ranges we want to keep in the kernel.
+	uint32		num_virtual_ranges_to_keep;
+	addr_range	virtual_ranges_to_keep[MAX_VIRTUAL_RANGES_TO_KEEP];
 } arch_kernel_args;
 
 #endif	/* KERNEL_ARCH_RISCV64_KERNEL_ARGS_H */
