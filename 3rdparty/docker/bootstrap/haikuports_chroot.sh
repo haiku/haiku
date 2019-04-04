e8b4366330ed6ca108a0f51c5b085fbcbf7af721
Alexander von Gluck IV <kallisti5@unixzen.com>
2019-04-03 20:43:17 -0500

    docker/bootstrap: Add sysroot stage flag to chroot tool
    Change-Id: I7a6303cb11756b66d6d3379063f2402b3b38b7c6

--- 3rdparty/docker/bootstrap/haikuports_chroot.sh
+++ 3rdparty/docker/bootstrap/haikuports_chroot.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-	echo "usage: $0 <recipe>"
+if [ $# -lt 1 ]; then
+	echo "usage: $0 <recipe> [sysroot-stage number]"
+	echo "Enters a haikuporter bootstrap chroot."
+	echo "Optionally, specify the sysroot-stage (default 1)"
 	exit 1
+fi
+
+STAGE="stage1"
+if [ $# -eq 2 ]; then
+	STAGE=stage$2
 fi
 
 export GENERATED=$WORKPATH/generated.$TARGET_ARCH
@@ -11,7 +18,9 @@ export LD_LIBRARY_PATH=$GENERATED/objects/linux/lib/:$LD_LIBRARY_PATH
 # A hack to fix haikuport chroot not liking Debian's fancy PS1
 export PS1='\u@\h:\w\$'
 
+echo "Entering chroot with $STAGE chroot..."
+
 $WORKPATH/src/haikuporter/haikuporter \
 	--config=$GENERATED/objects/haiku/$TARGET_ARCH/packaging/repositories/HaikuPortsCross-build/haikuports.conf \
-	--cross-devel-package $GENERATED/objects/haiku/$TARGET_ARCH/packaging/packages/haiku_cross_devel_sysroot_stage1_$TARGET_ARCH.hpkg \
+	--cross-devel-package $GENERATED/objects/haiku/$TARGET_ARCH/packaging/packages/haiku_cross_devel_sysroot_$STAGE_$TARGET_ARCH.hpkg \
 	--enter-chroot $1
