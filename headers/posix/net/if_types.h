db3b4a3caf398d395ebe72594235db57fc03ac06
Alexander von Gluck IV <kallisti5@unixzen.com>
2019-04-22 13:06:00 -0500

    posix/if_types.h: Fix tun id, add a few more
    * Oops, there's a standard for these. Stick to the standard.
    * Add a few that could be useful someday.
    * Mention iana spec.
    
    Change-Id: I4cf75e8c1e4b25f65d10921c7075fbd53f44e14e

--- headers/posix/net/if_types.h
+++ headers/posix/net/if_types.h
@@ -5,14 +5,20 @@
 #ifndef _NET_IF_TYPES_H
 #define _NET_IF_TYPES_H
 
+// Standard:
+// http://www.iana.org/assignments/smi-numbers/smi-numbers.xhtml
 
 #define IFT_OTHER		0x01
 #define IFT_ETHER		0x06
 #define IFT_PPP			0x17
 #define IFT_LOOP		0x18
 #define IFT_SLIP		0x1c
+#define IFT_LOCALTALK	0x2a
 #define IFT_MODEM		0x30
-#define IFT_TUN			0x40
+#define IFT_TUN			0x83
+#define IFT_L2VLAN		0x87
+#define IFT_IEEE1394	0x90
+#define IFT_BRIDGE		0xd1
 
 
 #endif	/* _NET_IF_TYPES_H */
