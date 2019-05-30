ef52a3d48fc3b96759cb4e2f9128166aaaffe2a2
brjhaiku <brjhaiku@gmail.com>
2019-05-31 04:26:54 +0530

    btrfs: document DirectoryIterator class (no functional change)
    Change-Id: Iffbd7112a9a266b65d6dd56dbd040f04e5ab58b2
    Reviewed-on: https://review.haiku-os.org/c/1493
    Reviewed-by: Jérôme Duval <jerome.duval@gmail.com>

--- src/add-ons/kernel/file_systems/btrfs/DirectoryIterator.h
+++ src/add-ons/kernel/file_systems/btrfs/DirectoryIterator.h
@@ -11,6 +11,7 @@
 #include "Inode.h"
 
 
+//! Class used to iterate through entries in a directory
 class DirectoryIterator {
 public:
 								DirectoryIterator(Inode* inode);
@@ -18,8 +19,19 @@ public:
 
 			status_t			InitCheck();
 
+			/*! Get details of next entry
+			 * \param name Location to copy name of next entry
+			 * \param _nameLength Location to copy length of next entry's name
+			 * \param _id Location to copy inode number of next entry
+			 */
 			status_t			GetNext(char* name, size_t* _nameLength, ino_t* _id);
+			/*! Search for item in current directory
+			 * \param name Name of entry to lookup
+			 * \param nameLength Length of name being searched
+			 * \param _id inode value of entry if found, ??? otherwise
+			 */
 			status_t			Lookup(const char* name, size_t nameLength, ino_t* _id);
+			//! Reset iterator to beginning
 			status_t			Rewind();
 private:
 			uint64				fOffset;
