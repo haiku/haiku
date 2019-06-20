b313d63eae110b2004cee5ef78df8f0b3f6cdb16
Alexander von Gluck IV <kallisti5@unixzen.com>
2019-06-20 12:04:47 -0500

    arm/atomics: Move cpu-specific memory barriers to arch_cpu
    * Now matches the rest of the architectures.
    
    Change-Id: I6699e0c8f729923770f136f2c9599185a685336a
    Reviewed-on: https://review.haiku-os.org/c/1527
    Reviewed-by: Alex von Gluck IV <kallisti5@unixzen.com>
    Reviewed-by: Adrien Destugues <pulkomandy@gmail.com>
    Reviewed-by: Axel Dörfler <axeld@pinc-software.de>

--- headers/private/kernel/arch/arm/arch_atomic.h
+++ headers/private/kernel/arch/arm/arch_atomic.h
@@ -9,23 +9,7 @@
 #define _KERNEL_ARCH_ARM_ATOMIC_H
 
 
-#if __ARM_ARCH__ <= 5
-#define isb() __asm__ __volatile__("" : : : "memory")
-#define dsb() __asm__ __volatile__("mcr p15, 0, %0, c7, c10, 4" \
-	: : "r" (0) : "memory")
-#define dmb() __asm__ __volatile__("" : : : "memory")
-#elif __ARM_ARCH__ == 6
-#define isb() __asm__ __volatile__("mcr p15, 0, %0, c7, c5, 4" \
-	: : "r" (0) : "memory")
-#define dsb() __asm__ __volatile__("mcr p15, 0, %0, c7, c10, 4" \
-	: : "r" (0) : "memory")
-#define dmb() __asm__ __volatile__("mcr p15, 0, %0, c7, c10, 5" \
-	: : "r" (0) : "memory")
-#else /* ARMv7+ */
-#define isb() __asm__ __volatile__("isb" : : : "memory")
-#define dsb() __asm__ __volatile__("dsb" : : : "memory")
-#define dmb() __asm__ __volatile__("dmb" : : : "memory")
-#endif
+#include <arch_cpu.h>
 
 
 static inline void
