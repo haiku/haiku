f1f452b3cf30baba2dfb14fa6fb2f270e2cab5b9
Alexander von Gluck IV <kallisti5@unixzen.com>
2019-07-01 14:10:32 -0500

    libroot/os: Fix missing time funcs on riscv64
    Change-Id: I291ebc949d63860e20eb609e4d3d2f600309d4e9
    Reviewed-on: https://review.haiku-os.org/c/1553
    Reviewed-by: Alex von Gluck IV <kallisti5@unixzen.com>
    Reviewed-by: Adrien Destugues <pulkomandy@gmail.com>

---
+++ src/system/libroot/os/arch/riscv64/time.c
@@ -0,0 +1,32 @@
+/* 
+ * Copyright 2006, Ingo Weinhold <bonefish@cs.tu-berlin.de>.
+ * Distributed under the terms of the MIT License.
+ */
+
+#include <OS.h>
+
+#include <arch_cpu.h>
+#include <libroot_private.h>
+#include <real_time_data.h>
+
+
+static struct arch_real_time_data *sRealTimeData;
+
+void
+__arch_init_time(struct real_time_data *data, bool setDefaults)
+{
+	sRealTimeData = &data->arch_data;
+
+	if (setDefaults) {
+		sRealTimeData->system_time_conversion_factor = 1000000000LL;
+	}
+
+	// __riscv64_setup_system_time(&sRealTimeData->system_time_conversion_factor);
+}
+
+
+bigtime_t
+__arch_get_system_time_offset(struct real_time_data *data)
+{
+	return 0;
+}
