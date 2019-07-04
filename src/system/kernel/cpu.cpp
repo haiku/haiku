05901d8963c69b695c9796289213a307cd714afb
Augustin Cavalier <waddlesplash@gmail.com>
2019-07-04 17:00:28 -0400

    kernel/cpu: Only root can enable or disable CPUs.

--- src/system/kernel/cpu.cpp
+++ src/system/kernel/cpu.cpp
@@ -367,6 +367,8 @@ _user_set_cpu_enabled(int32 cpu, bool enabled)
 {
 	int32 i, count;
 
+	if (geteuid() != 0)
+		return B_PERMISSION_DENIED;
 	if (cpu < 0 || cpu >= smp_get_num_cpus())
 		return B_BAD_VALUE;
 
