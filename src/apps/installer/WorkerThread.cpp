e06f64086224dda0e36704045448844f7376dafd
Augustin Cavalier <waddlesplash@gmail.com>
2019-07-13 15:54:47 -0400

    Installer: Ignore cache directories.
    Fixes #15145.

--- src/apps/installer/WorkerThread.cpp
+++ src/apps/installer/WorkerThread.cpp
@@ -97,6 +97,8 @@ public:
 			fIgnorePaths.insert("rr_moved");
 			fIgnorePaths.insert("boot.catalog");
 			fIgnorePaths.insert("haiku-boot-floppy.image");
+			fIgnorePaths.insert("system/cache");
+			fIgnorePaths.insert("home/config/cache");
 			fIgnorePaths.insert("system/var/swap");
 			fIgnorePaths.insert("system/var/shared_memory");
 			fIgnorePaths.insert("system/var/log/syslog");
