f60531661beee9c2bcde5df0e6b821dfc0bdfc24
François Revol <revol@free.fr>
2019-09-17 19:32:32 +0200

    Add a dump_windows script to help with automating screenshoting
    Oddly hey hangs when asking Pe, someone should investigate.
    
    Change-Id: Ic94d0a2600ceae5f73ce11d5666b6a7abd54c0c0

---
+++ 3rdparty/mmu_man/scripts/dump_windows.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# get running app signatures and filter out background apps
+roster | sed -n 's/)$//;s/^ *//;/[^64] (/s/ .*(/\t/p' | while read team app; do
+	echo "# team $team $app"
+	c="$(hey "$team" count Window | grep result | cut -d" " -f 7)"
+	for w in $(seq 1 "$c"); do
+		title="$(hey "$team" get Title of Window "$w" | sed -n 's/"$//;/"result"/s/.*TYPE) : "//p')"
+		frame="$(hey "$team" get Frame of Window "$w" | sed -n 's/)$//;/"result"/s/.*TYPE) : BRect(//p')"
+		echo "hey \"$app\" set Frame of Window \"$title\" to \"BRect($frame)\""
+	done
+done
