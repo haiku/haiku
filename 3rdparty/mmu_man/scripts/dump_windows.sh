96e0ae70d496e4c07e58d9c872ae11662e866b2a
François Revol <revol@free.fr>
2019-09-17 19:56:34 +0200

    dump_windows: off-by-1
    Change-Id: Iaa6bd350cbc8b07016f7a73a267107431e173e93

--- 3rdparty/mmu_man/scripts/dump_windows.sh
+++ 3rdparty/mmu_man/scripts/dump_windows.sh
@@ -4,7 +4,8 @@
 roster | sed -n 's/)$//;s/^ *//;/[^64] (/s/ .*(/\t/p' | while read team app; do
 	echo "# team $team $app"
 	c="$(hey "$team" count Window | grep result | cut -d" " -f 7)"
-	for w in $(seq 1 "$c"); do
+	let c="$c - 1"
+	for w in $(seq 0 "$c"); do
 		title="$(hey "$team" get Title of Window "$w" | sed -n 's/"$//;/"result"/s/.*TYPE) : "//p')"
 		frame="$(hey "$team" get Frame of Window "$w" | sed -n 's/)$//;/"result"/s/.*TYPE) : BRect(//p')"
 		echo "hey \"$app\" set Frame of Window \"$title\" to \"BRect($frame)\""
