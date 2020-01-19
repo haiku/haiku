aa1e42a3320764b8e688f0ab95e406941bf18896
Augustin Cavalier <waddlesplash@gmail.com>
2020-01-18 20:26:05 -0500

    libroot: Replace swab() with the musl version.
    This was the last function remaining in the glibc "string" directory,
    so now we can remove that directory and a bunch of related files
    that are no longer needed.

---
+++ src/system/libroot/posix/musl/string/swab.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+#if __GNUC__ < 4
+#define restrict
+#endif
+
+void swab(const void *restrict _src, void *restrict _dest, ssize_t n)
+{
+	const char *src = _src;
+	char *dest = _dest;
+	for (; n>1; n-=2) {
+		dest[0] = src[1];
+		dest[1] = src[0];
+		dest += 2;
+		src += 2;
+	}
+}
