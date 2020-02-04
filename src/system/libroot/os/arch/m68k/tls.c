66f88b473a3944fddbe06a5f82afc9501b55fcff
François Revol <revol@free.fr>
2020-02-04 16:43:52 +0100

    m68k: stub out more TLS
    Change-Id: Ia226dcf996556622faf66798647447f2311fbb03
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/2215
    Reviewed-by: waddlesplash <waddlesplash@gmail.com>

--- src/system/libroot/os/arch/m68k/tls.c
+++ src/system/libroot/os/arch/m68k/tls.c
@@ -7,10 +7,14 @@
 //	to decide how this should be done, so it's just broken now (okay for single
 //	threaded apps, though).
 
+#warning FIXME: M68K
+
 // we don't want to have the inline assembly included here
 #ifndef _NO_INLINE_ASM
 #	define _NO_INLINE_ASM 1
 #endif
+
+#include <runtime_loader/runtime_loader.h>
 
 #include "support/TLS.h"
 #include "tls.h"
@@ -18,6 +22,13 @@
 
 static int32 gNextSlot = TLS_FIRST_FREE_SLOT;
 static void *gSlots[TLS_MAX_KEYS];
+
+struct tls_index {
+	unsigned long int	module;
+	unsigned long int	offset;
+};
+
+void* __tls_get_addr(struct tls_index* ti);
 
 
 int32
@@ -49,5 +60,11 @@ void
 tls_set(int32 index, void *value)
 {
 	gSlots[index] = value;
+}
+
+void*
+__tls_get_addr(struct tls_index* ti)
+{
+	return __gRuntimeLoader->get_tls_address(ti->module, ti->offset);
 }
 
