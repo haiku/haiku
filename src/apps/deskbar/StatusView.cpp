03399b17b8e2415ef3731cb3c22c6e0cafaa7529
John Scipione <jscipione@gmail.com>
2020-02-19 18:44:13 -0500

    Deskbar: remove mistaken double assignment

--- src/apps/deskbar/StatusView.cpp
+++ src/apps/deskbar/StatusView.cpp
@@ -249,7 +249,7 @@ TReplicantTray::GetPreferredSize(float* preferredWidth, float* preferredHeight)
 
 		// this view has a fixed minimum width
 		width = std::max(kMinimumTrayWidth, width);
-		height = height = fBarView->TeamMenuItemHeight();
+		height = fBarView->TeamMenuItemHeight();
 	}
 
 	*preferredWidth = width;
