82a7c7395cb83b01af25931504f540bcb29e29fd
Alexander von Gluck IV <kallisti5@unixzen.com>
2020-02-22 16:49:27 -0600

    efi/arm: Begin stubbing out the essentials, add fdt library
    * We *might* no longer need FDT knowledge in the bootloader?
    * For now though, arm sources reference gFDT, so we need it.
    * Need to move away from lazy gFDT and store in arch_kernel_args.
      (which is next and will be a larger commit)
    
    Change-Id: I77cce0fc645143b78a7fd9f50ac8b96c97b5c862
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/2268
    Reviewed-by: Adrien Destugues <pulkomandy@gmail.com>

---
+++ src/system/boot/platform/efi/arch/arm/arch_smp.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2019-2020, Haiku, Inc. All rights reserved.
+ * Released under the terms of the MIT License.
+*/
+
+
+#include "arch_smp.h"
+
+#include <string.h>
+
+#include <KernelExport.h>
+
+#include <kernel.h>
+#include <safemode.h>
+#include <boot/platform.h>
+#include <boot/stage2.h>
+#include <boot/menu.h>
+
+
+//#define TRACE_SMP
+#ifdef TRACE_SMP
+#	define TRACE(x) dprintf x
+#else
+#	define TRACE(x) ;
+#endif
+
+
+int
+arch_smp_get_current_cpu(void)
+{
+	// One cpu for now.
+	return 0;
+}
+
+
+void
+arch_smp_init_other_cpus(void)
+{
+	// One cpu for now.
+	gKernelArgs.num_cpus = 1;
+	return;
+}
+
+
+void
+arch_smp_boot_other_cpus(uint32 pml4, uint64 kernel_entry)
+{
+	// One cpu for now.
+}
+
+
+void
+arch_smp_add_safemode_menus(Menu *menu)
+{
+	MenuItem *item;
+
+	if (gKernelArgs.num_cpus < 2)
+		return;
+
+	item = new(nothrow) MenuItem("Disable SMP");
+	menu->AddItem(item);
+	item->SetData(B_SAFEMODE_DISABLE_SMP);
+	item->SetType(MENU_ITEM_MARKABLE);
+	item->SetHelpText("Disables all but one CPU core.");
+}
+
+
+void
+arch_smp_init(void)
+{
+	// One cpu for now.
+}
---
+++ src/system/boot/platform/efi/arch/arm/arch_start.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2019-2020 Haiku, Inc. All rights reserved.
+ * Released under the terms of the MIT License.
+ */
+
+
+#include <boot/platform.h>
+#include <boot/stage2.h>
+#include <boot/stdio.h>
+
+
+void
+arch_start_kernel(addr_t kernelEntry)
+{
+	// Kernel Entry!
+}
