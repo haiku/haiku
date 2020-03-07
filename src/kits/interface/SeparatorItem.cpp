db3a94141ac2e2d7a9155279fdb95a05a105888d
X512 <danger_mail@list.ru>
2020-03-07 13:21:18 +0900

    SeparatorItem: support horizontal layout
    This is required for BMenuBar based toolbars.
    
    Fixes #15785.
    
    Change-Id: I8d108694b481e408e5c56e23a697c8e7829343dc
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/2316
    Reviewed-by: Adrien Destugues <pulkomandy@gmail.com>

--- src/kits/interface/SeparatorItem.cpp
+++ src/kits/interface/SeparatorItem.cpp
@@ -62,16 +62,24 @@ BSeparatorItem::SetEnabled(bool enable)
 void
 BSeparatorItem::GetContentSize(float* _width, float* _height)
 {
-	if (_width != NULL)
-		*_width = 2.0;
+	if (Menu() != NULL && Menu()->Layout() == B_ITEMS_IN_ROW) {
+		if (_width != NULL)
+			*_width = 2.0;
 
-	if (_height != NULL) {
-		BFont font(be_plain_font);
-		if (Menu() != NULL)
-			Menu()->GetFont(&font);
+		if (_height != NULL)
+			*_height = 2.0;
+	} else {
+		if (_width != NULL)
+			*_width = 2.0;
 
-		const float height = floorf((font.Size() * 0.8) / 2) * 2;
-		*_height = max_c(4, height);
+		if (_height != NULL) {
+			BFont font(be_plain_font);
+			if (Menu() != NULL)
+				Menu()->GetFont(&font);
+
+			const float height = floorf((font.Size() * 0.8) / 2) * 2;
+			*_height = max_c(4, height);
+		}
 	}
 }
 
@@ -87,14 +95,23 @@ BSeparatorItem::Draw()
 	rgb_color oldColor = menu->HighColor();
 	rgb_color lowColor = menu->LowColor();
 
-	const float startTop = bounds.top + (floor(bounds.Height())) / 2;
-	menu->SetHighColor(tint_color(lowColor, B_DARKEN_1_TINT));
-	menu->StrokeLine(BPoint(bounds.left + 1.0f, startTop),
-		BPoint(bounds.right - 1.0f, startTop));
-	menu->SetHighColor(tint_color(lowColor, B_LIGHTEN_2_TINT));
-	menu->StrokeLine(BPoint(bounds.left + 1.0f, startTop + 1.0f),
-		BPoint(bounds.right - 1.0f, startTop + 1.0f));
-
+	if (menu->Layout() == B_ITEMS_IN_ROW) {
+		const float startLeft = bounds.left + (floor(bounds.Width())) / 2;
+		menu->SetHighColor(tint_color(lowColor, B_DARKEN_1_TINT));
+		menu->StrokeLine(BPoint(startLeft, bounds.top + 1.0f),
+			BPoint(startLeft, bounds.bottom - 1.0f));
+		menu->SetHighColor(tint_color(lowColor, B_LIGHTEN_2_TINT));
+		menu->StrokeLine(BPoint(startLeft + 1.0f, bounds.top + 1.0f),
+			BPoint(startLeft + 1.0f, bounds.bottom - 1.0f));
+	} else {
+		const float startTop = bounds.top + (floor(bounds.Height())) / 2;
+		menu->SetHighColor(tint_color(lowColor, B_DARKEN_1_TINT));
+		menu->StrokeLine(BPoint(bounds.left + 1.0f, startTop),
+			BPoint(bounds.right - 1.0f, startTop));
+		menu->SetHighColor(tint_color(lowColor, B_LIGHTEN_2_TINT));
+		menu->StrokeLine(BPoint(bounds.left + 1.0f, startTop + 1.0f),
+			BPoint(bounds.right - 1.0f, startTop + 1.0f));
+	}
 	menu->SetHighColor(oldColor);
 }
 
