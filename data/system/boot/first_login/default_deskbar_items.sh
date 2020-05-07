7bf88c5132992fc499fd2cd23a5471575d2edc2f
Sergei Reznikov <diver@gelios.net>
2020-05-07 17:23:27 +0200

    Enable KeymapSwitcher automatically for some locales.
    Change-Id: Ie3b49be07dd02227850cb0d7d2a7befca7992668
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/2597
    Reviewed-by: Sergei Reznikov <diver@gelios.net>

--- data/system/boot/first_login/default_deskbar_items.sh
+++ data/system/boot/first_login/default_deskbar_items.sh
@@ -4,3 +4,8 @@
 /boot/system/apps/ProcessController -deskbar
 /boot/system/apps/NetworkStatus --deskbar
 /boot/system/bin/desklink --add-volume
+
+# install KeymapSwitcher for certain locales
+if [[ `locale -l` =~ ^(ru|uk|be)$ ]]; then
+   /boot/system/preferences/KeymapSwitcher --deskbar
+fi
