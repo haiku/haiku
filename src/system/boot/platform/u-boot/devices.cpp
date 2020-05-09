088faf4fb92e915297830ddeaebba985305d349f
Augustin Cavalier <waddlesplash@gmail.com>
2020-05-08 23:49:42 -0400

    Revert "Revert "loader: fetch all potential boot partitions for boot device.""
    This reverts commit 0d932a49ada8af6314cd3f20c3f597143f1f555d.

--- src/system/boot/platform/u-boot/devices.cpp
+++ src/system/boot/platform/u-boot/devices.cpp
@@ -48,8 +48,8 @@ platform_add_boot_device(struct stage2_args *args, NodeList *devicesList)
 
 
 status_t
-platform_get_boot_partition(struct stage2_args *args, Node *device,
-	NodeList *list, boot::Partition **_partition)
+platform_get_boot_partitions(struct stage2_args *args, Node *device,
+	NodeList *list, NodeList *partitionList)
 {
 	TRACE("platform_get_boot_partition\n");
 
@@ -57,7 +57,7 @@ platform_get_boot_partition(struct stage2_args *args, Node *device,
 	boot::Partition *partition = NULL;
 	while ((partition = (boot::Partition *)iterator.Next()) != NULL) {
 		// ToDo: just take the first partition for now
-		*_partition = partition;
+		partitionList->Insert(partition);
 		return B_OK;
 	}
 
