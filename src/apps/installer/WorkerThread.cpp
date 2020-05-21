9e9b996ae9e3d34e4e2f793ab2108b39a3ea3677
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2020-05-21 07:19:09 +0100

    Beta2: block using the Installer to upgrade an existing system.
    See #15808. Installer does not do what it promises to do, that is, that is
    clean the system folder and essentially perform a clean install. Instead all
    packages are copied to /system/packages, but they are not activated. To make it
    worse, the system is then unable to upgrade through pkgman later on, as it
    bails with the error that the files already exist.
    
    This change at least stops Installer from pretending to do a clean install.
    
    Change-Id: I5e2b1f571a99f9f8c020f77c314974719346f750
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/2760
    Reviewed-by: Adrien Destugues <pulkomandy@gmail.com>

--- src/apps/installer/WorkerThread.cpp
+++ src/apps/installer/WorkerThread.cpp
@@ -466,20 +466,13 @@ WorkerThread::_PerformInstall(partition_id sourcePartitionID,
 
 	if (entries != 0) {
 		BAlert* alert = new BAlert("", B_TRANSLATE("The target volume is not "
-			"empty. Are you sure you want to install anyway?\n\nNote: The "
-			"'system' folder will be a clean copy from the source volume but "
-			"will retain its settings folder, all other folders will be "
-			"merged, whereas files and links that exist on both the source "
-			"and target volume will be overwritten with the source volume "
-			"version."),
-			B_TRANSLATE("Install anyway"), B_TRANSLATE("Cancel"), 0,
-			B_WIDTH_AS_USUAL, B_STOP_ALERT);
-		alert->SetShortcut(1, B_ESCAPE);
-		if (alert->Go() != 0) {
-		// TODO: Would be cool to offer the option here to clean additional
-		// folders at the user's choice.
-			return _InstallationError(B_CANCELED);
-		}
+			"empty. The installer cannot upgrade your system. Please visit "
+			"the guide on the Haiku website on how to upgrade an existing "
+			"installation to R1 beta 2."),
+			B_TRANSLATE("Cancel"), 0, 0, B_WIDTH_AS_USUAL, B_STOP_ALERT);
+		alert->SetShortcut(0, B_ESCAPE);
+		alert->Go();
+		return _InstallationError(B_CANCELED);
 	}
 
 	// Begin actual installation
