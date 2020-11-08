f566cf181a44063cd93abc2d50fed19c69e177b0
Alexander von Gluck IV <kallisti5@unixzen.com>
2020-11-07 18:00:34 -0600

    file_systems/nfs: Fix SMAP violations
    * Try to return relevant errors when we can.
    * The style on these needs cleaned up badly.
    * I don't want the fix to be lost in style changes.
    
    Change-Id: I8a1661385fbeb8eec86a2c15828b449980050a78
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/3386
    Reviewed-by: Jérôme Duval <jerome.duval@gmail.com>

--- src/add-ons/kernel/file_systems/nfs/XDRInPacket.h
+++ src/add-ons/kernel/file_systems/nfs/XDRInPacket.h
@@ -13,7 +13,8 @@ struct XDRInPacket
 void XDRInPacketInit (struct XDRInPacket *packet);
 void XDRInPacketDestroy (struct XDRInPacket *packet);
 int32 XDRInPacketGetInt32 (struct XDRInPacket *packet);
-void XDRInPacketGetFixed (struct XDRInPacket *packet, void *buffer, size_t len);
+int32 XDRInPacketGetFixed (struct XDRInPacket *packet, void *buffer,
+	size_t len);
 size_t XDRInPacketGetDynamic (struct XDRInPacket *packet, void *buffer);
 char *XDRInPacketGetString (struct XDRInPacket *packet);
 void XDRInPacketSetTo (struct XDRInPacket *packet, uint8 *buffer, size_t offset);
