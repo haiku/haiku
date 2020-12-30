e5feb3a7ae31b61a985065085342286f74892700
Pascal Abresch <nep@packageloss.eu>
2020-12-30 17:17:32 +0100

    Webpositive: Change default search engine to duckduckgo
    Change-Id: I4cb519453b0697260778fb34449ba5ac36972dcd
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/3757
    Reviewed-by: Adrien Destugues <pulkomandy@gmail.com>

--- src/apps/webpositive/SettingsKeys.cpp
+++ src/apps/webpositive/SettingsKeys.cpp
@@ -30,7 +30,7 @@ const char* kSettingsKeySearchPageURL = "search page url";
 const char* kDefaultDownloadPath = "/boot/home/Desktop/";
 const char* kDefaultStartPageURL
 	= "file:///boot/home/config/settings/WebPositive/LoaderPages/Welcome";
-const char* kDefaultSearchPageURL = "https://www.google.com/search?q=%s";
+const char* kDefaultSearchPageURL = "https://duckduckgo.com/?q=%s";
 
 const char* kSettingsKeyUseProxy = "use http proxy";
 const char* kSettingsKeyProxyAddress = "http proxy address";
