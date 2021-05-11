a65f6a2308b75ed6e9f178e13442f00fa81f4098
Murai Takashi <tmurai01@gmail.com>
2021-05-11 19:48:22 +0900

    Accelerants: Fix multiple definitions
    Since nv_globals.h and globals.h are included from several files,
    it cause multiple definitions error when linking.
    Pointed out by gcc11.
    
    Change-Id: I10e27af062c6f4b49fdb9ef6bb3ba77809ebcba0
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/3904
    Reviewed-by: Adrien Destugues <pulkomandy@gmail.com>

--- src/add-ons/accelerants/nvidia/engine/nv_globals.c
+++ src/add-ons/accelerants/nvidia/engine/nv_globals.c
@@ -19,6 +19,48 @@ display_mode *my_mode_list;
 area_id	my_mode_list_area;
 int accelerantIsClone;
 
+crtc_interrupt_enable	head1_interrupt_enable;
+crtc_update_fifo		head1_update_fifo;
+crtc_validate_timing 	head1_validate_timing;
+crtc_set_timing 		head1_set_timing;
+crtc_depth				head1_depth;
+crtc_dpms				head1_dpms;
+crtc_set_display_pitch	head1_set_display_pitch;
+crtc_set_display_start	head1_set_display_start;
+crtc_cursor_init		head1_cursor_init;
+crtc_cursor_show		head1_cursor_show;
+crtc_cursor_hide		head1_cursor_hide;
+crtc_cursor_define		head1_cursor_define;
+crtc_cursor_position	head1_cursor_position;
+crtc_stop_tvout			head1_stop_tvout;
+crtc_start_tvout		head1_start_tvout;
+
+crtc_interrupt_enable	head2_interrupt_enable;
+crtc_update_fifo		head2_update_fifo;
+crtc_validate_timing	head2_validate_timing;
+crtc_set_timing			head2_set_timing;
+crtc_depth				head2_depth;
+crtc_dpms				head2_dpms;
+crtc_set_display_pitch	head2_set_display_pitch;
+crtc_set_display_start	head2_set_display_start;
+crtc_cursor_init		head2_cursor_init;
+crtc_cursor_show		head2_cursor_show;
+crtc_cursor_hide		head2_cursor_hide;
+crtc_cursor_define		head2_cursor_define;
+crtc_cursor_position	head2_cursor_position;
+crtc_stop_tvout			head2_stop_tvout;
+crtc_start_tvout		head2_start_tvout;
+
+dac_mode				head1_mode;
+dac_palette				head1_palette;
+dac_set_pix_pll			head1_set_pix_pll;
+dac_pix_pll_find		head1_pix_pll_find;
+
+dac_mode				head2_mode;
+dac_palette				head2_palette;
+dac_set_pix_pll			head2_set_pix_pll;
+dac_pix_pll_find		head2_pix_pll_find;
+
 nv_get_set_pci nv_pci_access=
 	{
 		NV_PRIVATE_DATA_MAGIC,
--- src/add-ons/accelerants/via/engine/globals.c
+++ src/add-ons/accelerants/via/engine/globals.c
@@ -18,6 +18,42 @@ display_mode *my_mode_list;
 area_id	my_mode_list_area;
 int accelerantIsClone;
 
+crtc_validate_timing 	head1_validate_timing;
+crtc_set_timing 		head1_set_timing;
+crtc_depth				head1_depth;
+crtc_dpms				head1_dpms;
+crtc_dpms_fetch			head1_dpms_fetch;
+crtc_set_display_pitch	head1_set_display_pitch;
+crtc_set_display_start	head1_set_display_start;
+crtc_cursor_init		head1_cursor_init;
+crtc_cursor_show		head1_cursor_show;
+crtc_cursor_hide		head1_cursor_hide;
+crtc_cursor_define		head1_cursor_define;
+crtc_cursor_position	head1_cursor_position;
+
+crtc_validate_timing	head2_validate_timing;
+crtc_set_timing			head2_set_timing;
+crtc_depth				head2_depth;
+crtc_dpms				head2_dpms;
+crtc_dpms_fetch			head2_dpms_fetch;
+crtc_set_display_pitch	head2_set_display_pitch;
+crtc_set_display_start	head2_set_display_start;
+crtc_cursor_init		head2_cursor_init;
+crtc_cursor_show		head2_cursor_show;
+crtc_cursor_hide		head2_cursor_hide;
+crtc_cursor_define		head2_cursor_define;
+crtc_cursor_position	head2_cursor_position;
+
+dac_mode				head1_mode;
+dac_palette				head1_palette;
+dac_set_pix_pll			head1_set_pix_pll;
+dac_pix_pll_find		head1_pix_pll_find;
+
+dac_mode				head2_mode;
+dac_palette				head2_palette;
+dac_set_pix_pll			head2_set_pix_pll;
+dac_pix_pll_find		head2_pix_pll_find;
+
 eng_get_set_pci eng_pci_access=
 	{
 		VIA_PRIVATE_DATA_MAGIC,
