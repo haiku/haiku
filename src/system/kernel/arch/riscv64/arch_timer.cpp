c15bf9b7929d77a4658fd53aca275421e0cf5702
X512 <danger_mail@list.ru>
2021-06-06 23:02:03 +0900

    kernel/arch/timer: implement for riscv64
    Change-Id: Ia854b0048a86b1427e975f482325ed4d4df1a12a
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/4061
    Reviewed-by: Alex von Gluck IV <kallisti5@unixzen.com>

--- src/system/kernel/arch/riscv64/arch_timer.cpp
+++ src/system/kernel/arch/riscv64/arch_timer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019, Haiku Inc. All rights reserved.
+ * Copyright 2019-2021, Haiku Inc. All rights reserved.
  * Distributed under the terms of the MIT License.
  *
  * Authors:
@@ -7,21 +7,54 @@
  */
 
 
-#include <kernel.h>
-#include <debug.h>
-#include <timer.h>
+#include <arch_cpu_defs.h>
+#include <arch_int.h>
 #include <arch/timer.h>
+#include <boot/kernel_args.h>
+#include <debug.h>
+#include <kernel.h>
+#include <platform/sbi/sbi_syscalls.h>
+#include <timer.h>
+#include <Clint.h>
+
+
+extern uint32 gPlatform1;
+extern uint32 gPlatform2;
 
 
 void
 arch_timer_set_hardware_timer(bigtime_t timeout)
 {
+	// TODO: Read timer frequency from FDT
+	switch (gPlatform1) {
+		case kPlatform1Riscv:
+			MSyscall(kMSyscallSetTimer, true,
+				gClintRegs->mtime + timeout * 10);
+			break;
+		case kPlatform1Sbi: {
+			sbi_set_timer(CpuTime() + timeout);
+			break;
+		}
+		default:
+			;
+	}
 }
 
 
 void
 arch_timer_clear_hardware_timer()
 {
+	switch (gPlatform1) {
+		case kPlatform1Riscv:
+			MSyscall(kMSyscallSetTimer, false);
+			break;
+		case kPlatform1Sbi: {
+			sbi_set_timer(CpuTime() + (uint64)10000000 * 3600);
+			break;
+		}
+		default:
+			;
+	}
 }
 
 
@@ -29,12 +62,4 @@ int
 arch_init_timer(kernel_args *args)
 {
 	return B_OK;
-}
-
-
-bigtime_t
-system_time(void)
-{
-	// TODO
-	return 0;
 }
