ddffdd25068a0fcbe09aeb55cb673f1f00ab78a1
Jérôme Duval <jerome.duval@gmail.com>
2021-06-15 22:03:23 +0200

    C11: define __STDC_NO_THREADS__ as threads.h is not provided
    Theorically we could define __STDC_NO_THREADS__ internally in GCC, but
    threads.h probably will be added in the future, and the compiler won't
    notice.
    Using this stdc-predef.h header and including it in GCC would solve this
    nicely, and saves us from hardcoding.
    The header can eventually be removed when obsolete.
    
    Change-Id: I29aa58686e3c45449dc63e02e5a9e13a960b9090
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/4097
    Tested-by: Commit checker robot <no-reply+buildbot@haiku-os.org>
    Reviewed-by: Adrien Destugues <pulkomandy@gmail.com>

---
+++ headers/posix/stdc-predef.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2021 Haiku, Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _STDC_PREDEF_H
+#define _STDC_PREDEF_H
+
+
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+#  define __STDC_NO_THREADS__	1
+#endif
+
+
+#endif /* _STDC_PREDEF_H */
