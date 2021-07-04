ba22ebb34431a1768e61076b07eba728b6fefb6c
Panagiotis Vasilopoulos <hello@alwayslivid.com>
2021-07-04 14:13:05 +0000

    midi_server: Update copyright notice
    This change aims to clean up the code a little. With explicit permission from Matthijs Hollemans.
    
    Change-Id: Idd6c46b8059ec4674653a0d844502b4c76990726
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/4146
    Reviewed-by: Adrien Destugues <pulkomandy@gmail.com>

--- src/servers/midi/MidiServerApp.cpp
+++ src/servers/midi/MidiServerApp.cpp
@@ -1,32 +1,15 @@
 /*
  * Copyright 2002-2015, Haiku, Inc. All rights reserved.
+ * Copyright 2002-2004, Matthijs Hollemans
+ * Copyright 2021, Panagiotis Vasilopoulos <hello@alwayslivid.com>
  * Distributed under the terms of the MIT License.
  *
  * Authors:
+ *		Humdinger
  *		Matthijs Hollemans
- */
-
-/*
- * Copyright (c) 2002-2004 Matthijs Hollemans
- * Copyright (c) 2021 Panagiotis Vasilopoulos
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
+ *		Oliver Tappe
+ *		Panagiotis Vasilopoulos
+ *		Philippe Houdoin
  */
 
 
