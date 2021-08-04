6211ae257544d7a1453bc34297c8eac3e2b0435d
John Scipione <jscipione@gmail.com>
2021-08-04 18:28:27 -0400

    BBitmap: Remove BeOS tweak to fix Spirograph 1.5
    This is a BeOS app compatibility fix.
    
    The comment in SetBits implementation said that B_RGB32 imports only
    24 bits of the data, but that doesn't seem to be correct.
    
    Fixes #12931
    
    Change-Id: I826a7e358ea379b8bf1550c899e82b623e5c21b4
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/4319
    Tested-by: Commit checker robot <no-reply+buildbot@haiku-os.org>
    Reviewed-by: waddlesplash <waddlesplash@gmail.com>

--- src/kits/interface/Bitmap.cpp
+++ src/kits/interface/Bitmap.cpp
@@ -637,11 +637,7 @@ BBitmap::SetBits(const void* data, int32 length, int32 offset,
 	int32 inBPR = -1;
 	// tweaks to mimic R5 behavior
 	if (error == B_OK) {
-		if (colorSpace == B_RGB32) {
-			// B_RGB32 means actually unpadded B_RGB24_BIG
-			colorSpace = B_RGB24_BIG;
-			inBPR = width * 3;
-		} else if (colorSpace == B_CMAP8 && fColorSpace != B_CMAP8) {
+		if (colorSpace == B_CMAP8 && fColorSpace != B_CMAP8) {
 			// If in color space is B_CMAP8, but the bitmap's is another one,
 			// ignore source data row padding.
 			inBPR = width;
