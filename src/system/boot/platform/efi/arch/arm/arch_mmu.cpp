b594d76c81522870f97c5bb7ec6f48e3d6374ab7
David Karoly <karolyd577@gmail.com>
2021-10-24 10:57:12 +0200

    boot/efi: use ROUNDUP for aligning the page directory
    Change-Id: Ib52ca83cbc390f2e450c174741ed24e745fb7eab
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/4655
    Reviewed-by: Adrien Destugues <pulkomandy@gmail.com>

--- src/system/boot/platform/efi/arch/arm/arch_mmu.cpp
+++ src/system/boot/platform/efi/arch/arm/arch_mmu.cpp
@@ -240,7 +240,7 @@ arch_mmu_allocate_page_tables(void)
 	if (platform_allocate_region((void **)&sPageDirectory,
 		ARM_MMU_L1_TABLE_SIZE + ALIGN_PAGEDIR + PAGE_TABLE_AREA_SIZE, 0, false) != B_OK)
 		panic("Failed to allocate page directory.");
-	sPageDirectory = (uint32*)(((uint32)sPageDirectory + ALIGN_PAGEDIR - 1) & ~(ALIGN_PAGEDIR-1));
+	sPageDirectory = (uint32 *)ROUNDUP((uint32)sPageDirectory, ALIGN_PAGEDIR);
 	memset(sPageDirectory, 0, ARM_MMU_L1_TABLE_SIZE);
 
 	sFirstPageTable = (uint32*)((uint32)sPageDirectory + ARM_MMU_L1_TABLE_SIZE);
