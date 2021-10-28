c8e72ef924f79cdfa8592de5113a1b59c2a64de0
David Karoly <karolyd577@gmail.com>
2021-10-28 09:57:49 +0200

    kernel/arch/arm/paging: ARMVMTranslationMap32Bit::QueryInterrupt shall return page start address
    Change-Id: I8949518c563b367b8b84f3728ae1e923e49044ba
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/4666
    Reviewed-by: Adrien Destugues <pulkomandy@gmail.com>

--- src/system/kernel/arch/arm/paging/32bit/ARMVMTranslationMap32Bit.cpp
+++ src/system/kernel/arch/arm/paging/32bit/ARMVMTranslationMap32Bit.cpp
@@ -680,7 +680,7 @@ ARMVMTranslationMap32Bit::QueryInterrupt(addr_t va, phys_addr_t *_physical,
 	page_table_entry entry = pt[VADDR_TO_PTENT(va)];
 
 	if ((entry & ARM_PTE_TYPE_MASK) != 0)
-		*_physical = (entry & ARM_PTE_ADDRESS_MASK) | VADDR_TO_PGOFF(va);
+		*_physical = (entry & ARM_PTE_ADDRESS_MASK);
 
 #if 0
 	// read in the page state flags
