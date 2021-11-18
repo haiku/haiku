ec21dc91b1f8fac253453915ee0eeb1adc48cc52
Augustin Cavalier <waddlesplash@gmail.com>
2021-11-18 12:10:27 -0500

    libbe_build: Synchronize Directory.cpp with the main one.
    The set_dir_fd code appears to be libbe_build specific, so that
    and accompanying logic is preserved.

--- headers/build/os/storage/Directory.h
+++ headers/build/os/storage/Directory.h
@@ -1,14 +1,10 @@
-//----------------------------------------------------------------------
-//  This software is part of the Haiku distribution and is covered
-//  by the MIT License.
-//---------------------------------------------------------------------
-/*!
-	\file Directory.h
-	BDirectory interface declaration.
-*/
-
+/*
+ * Copyright 2002-2006, Haiku, Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
 #ifndef _DIRECTORY_H
 #define _DIRECTORY_H
+
 
 #include <Node.h>
 #include <EntryList.h>
@@ -17,90 +13,78 @@
 
 class BFile;
 class BSymLink;
+struct stat_beos;
 
-/*!
-	\class BDirectory
-	\brief A directory in the filesystem
 
-	Provides an interface for manipulating directories and their contents.
-
-	\author <a href='mailto:bonefish@users.sf.net'>Ingo Weinhold</a>
-	\author <a href="mailto:tylerdauwalder@users.sf.net">Tyler Dauwalder</a>
-
-	\version 0.0.0
-*/
 class BDirectory : public BNode, public BEntryList {
-public:
-	BDirectory();
-	BDirectory(const BDirectory &dir);
-	BDirectory(const entry_ref *ref);
-	BDirectory(const node_ref *nref);
-	BDirectory(const BEntry *entry);
-	BDirectory(const char *path);
-	BDirectory(const BDirectory *dir, const char *path);
+	public:
+		BDirectory();
+		BDirectory(const BDirectory &dir);
+		BDirectory(const entry_ref *ref);
+		BDirectory(const node_ref *nref);
+		BDirectory(const BEntry *entry);
+		BDirectory(const char *path);
+		BDirectory(const BDirectory *dir, const char *path);
 
-	virtual ~BDirectory();
+		virtual ~BDirectory();
 
-	status_t SetTo(const entry_ref *ref);
-	status_t SetTo(const node_ref *nref);
-	status_t SetTo(const BEntry *entry);
-	status_t SetTo(const char *path);
-	status_t SetTo(const BDirectory *dir, const char *path);
+		status_t SetTo(const entry_ref *ref);
+		status_t SetTo(const node_ref *nref);
+		status_t SetTo(const BEntry *entry);
+		status_t SetTo(const char *path);
+		status_t SetTo(const BDirectory *dir, const char *path);
 
-	status_t GetEntry(BEntry *entry) const;
+		status_t GetEntry(BEntry *entry) const;
 
-	status_t FindEntry(const char *path, BEntry *entry,
-					   bool traverse = false) const;
+		bool IsRootDirectory() const;
 
-	bool Contains(const char *path, int32 nodeFlags = B_ANY_NODE) const;
-	bool Contains(const BEntry *entry, int32 nodeFlags = B_ANY_NODE) const;
+		status_t FindEntry(const char *path, BEntry *entry,
+			bool traverse = false) const;
 
-	status_t GetStatFor(const char *path, struct stat *st) const;
+		bool Contains(const char *path, int32 nodeFlags = B_ANY_NODE) const;
+		bool Contains(const BEntry *entry, int32 nodeFlags = B_ANY_NODE) const;
 
-	virtual status_t GetNextEntry(BEntry *entry, bool traverse = false);
-	virtual status_t GetNextRef(entry_ref *ref);
-	virtual int32 GetNextDirents(dirent *buf, size_t bufSize,
-								 int32 count = INT_MAX);
-	virtual status_t Rewind();
-	virtual int32 CountEntries();
+		status_t GetStatFor(const char *path, struct stat *st) const;
 
-	status_t CreateDirectory(const char *path, BDirectory *dir);
-	status_t CreateFile(const char *path, BFile *file,
-						bool failIfExists = false);
-	status_t CreateSymLink(const char *path, const char *linkToPath,
-						   BSymLink *link);
+		virtual status_t GetNextEntry(BEntry *entry, bool traverse = false);
+		virtual status_t GetNextRef(entry_ref *ref);
+		virtual int32 GetNextDirents(dirent *buf, size_t bufSize,
+			int32 count = INT_MAX);
+		virtual status_t Rewind();
+		virtual int32 CountEntries();
 
-	BDirectory &operator=(const BDirectory &dir);
+		status_t CreateDirectory(const char *path, BDirectory *dir);
+		status_t CreateFile(const char *path, BFile *file,
+			bool failIfExists = false);
+		status_t CreateSymLink(const char *path, const char *linkToPath,
+			BSymLink *link);
 
-private:
-	friend class BNode;
+		BDirectory &operator=(const BDirectory &dir);
 
-	virtual void _ErectorDirectory1();
-	virtual void _ErectorDirectory2();
-	virtual void _ErectorDirectory3();
-	virtual void _ErectorDirectory4();
-	virtual void _ErectorDirectory5();
-	virtual void _ErectorDirectory6();
+	private:
+		friend class BNode;
+		friend class BEntry;
+		friend class BFile;
 
-private:
-	virtual void close_fd();
-	int get_fd() const;
+		virtual void _ErectorDirectory1();
+		virtual void _ErectorDirectory2();
+		virtual void _ErectorDirectory3();
+		virtual void _ErectorDirectory4();
+		virtual void _ErectorDirectory5();
+		virtual void _ErectorDirectory6();
 
-	status_t set_dir_fd(int fd);
+	private:
+		virtual void close_fd();
+		int get_fd() const;
 
-private:
-	uint32 _reservedData[7];
-	int fDirFd;
-	node_ref	fDirNodeRef;
+		status_t set_dir_fd(int fd);
 
-	friend class BEntry;
-	friend class BFile;
+	private:
+		uint32 _reservedData[7];
+		int fDirFd;
+		node_ref fDirNodeRef;
 };
 
-
-// C functions
-
 status_t create_directory(const char *path, mode_t mode);
-
 
 #endif	// _DIRECTORY_H
