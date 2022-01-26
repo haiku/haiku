49ddb1ffd4bc521ad92d564f46f1158c1562ad93
Augustin Cavalier <waddlesplash@gmail.com>
2022-01-26 15:45:06 -0500

    kernel/condition_variable: Downgrade one of the panics into a dprintf.
    On some virtualized systems, this seems to still occur,
    and at this point may not be preventable.
    
    Fixes #17455.

--- src/system/kernel/condition_variable.cpp
+++ src/system/kernel/condition_variable.cpp
@@ -158,7 +158,7 @@ ConditionVariableEntry::_RemoveFromVariable()
 		while (atomic_pointer_get(&fVariable) != NULL) {
 			tries++;
 			if ((tries % 10000) == 0)
-				panic("variable pointer was not unset for a long time!");
+				dprintf("variable pointer was not unset for a long time!");
 			cpu_pause();
 		}
 
