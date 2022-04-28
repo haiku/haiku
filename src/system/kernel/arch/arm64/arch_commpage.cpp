e9d04b25e6b1e8b4d84f6354f3f737e3e7b74446
milek7 <me@milek7.pl>
2022-04-28 22:01:39 +0200

    arm64: Add thread exit syscall in commpage.
    Change-Id: Ia8e7e4626add623735fefefa1af151b7338adc35
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/5265
    Reviewed-by: Adrien Destugues <pulkomandy@gmail.com>
    Reviewed-by: waddlesplash <waddlesplash@gmail.com>
    Tested-by: Commit checker robot <no-reply+buildbot@haiku-os.org>

--- src/system/kernel/arch/arm64/arch_commpage.cpp
+++ src/system/kernel/arch/arm64/arch_commpage.cpp
@@ -13,6 +13,33 @@
 #include <smp.h>
 
 
+extern "C" void _thread_exit_syscall();
+
+
+static void
+register_commpage_function(const char* functionName, int32 commpageIndex,
+	const char* commpageSymbolName, addr_t expectedAddress)
+{
+	// get address and size of function
+	elf_symbol_info symbolInfo;
+	if (elf_lookup_kernel_symbol(functionName, &symbolInfo) != B_OK) {
+		panic("register_commpage_function(): Failed to find signal frame function \"%s\"!",
+			functionName);
+	}
+
+	ASSERT(expectedAddress == symbolInfo.address);
+
+	// fill in the commpage table entry
+	addr_t position = fill_commpage_entry(commpageIndex, (void*)symbolInfo.address,
+		symbolInfo.size);
+
+	// add symbol to the commpage image
+	image_id image = get_commpage_image();
+	elf_add_memory_image_symbol(image, commpageSymbolName, position, symbolInfo.size,
+		B_SYMBOL_TYPE_TEXT);
+}
+
+
 status_t
 arch_commpage_init(void)
 {
@@ -23,5 +50,8 @@ arch_commpage_init(void)
 status_t
 arch_commpage_init_post_cpus(void)
 {
+	register_commpage_function("_thread_exit_syscall", COMMPAGE_ENTRY_ARM64_THREAD_EXIT,
+		"commpage_thread_exit", (addr_t)&_thread_exit_syscall);
+
 	return B_OK;
 }
