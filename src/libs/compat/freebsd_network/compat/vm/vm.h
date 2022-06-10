30763b1e79a0f31aadf100b38e6c4782dda92b0f
Augustin Cavalier <waddlesplash@gmail.com>
2022-06-10 13:38:08 -0400

    freebsd_network: Rewrite bus/vm headers to use Haiku phys_addr types directly.
    Instead of redeclaring them, which it did so incorrectly on systems
    with PAE in some cases. May fix problems on some 32-bit systems.

--- src/libs/compat/freebsd_network/compat/vm/vm.h
+++ src/libs/compat/freebsd_network/compat/vm/vm.h
@@ -1,36 +1,25 @@
 /*
- * Copyright 2007, Hugo Santos. All Rights Reserved.
+ * Copyright 2022, Haiku, Inc. All rights reserved.
  * Distributed under the terms of the MIT License.
  */
 #ifndef _FBSD_COMPAT_VM_VM_H_
 #define _FBSD_COMPAT_VM_VM_H_
 
 
-#include <stdint.h>
 #include <KernelExport.h>
 
 
-#ifdef B_HAIKU_64_BIT
+typedef phys_addr_t vm_paddr_t;
+typedef addr_t vm_offset_t;
 
-typedef uint64_t vm_offset_t;
-typedef uint64_t vm_paddr_t;
-
-#else
-
-typedef uint32_t vm_offset_t;
-typedef uint32_t vm_paddr_t;
-
-#endif
-
-typedef void *pmap_t;
-
-
-#define vmspace_pmap(...)	NULL
-#define pmap_extract(...)	NULL
+typedef void* pmap_t;
 
 
 vm_paddr_t pmap_kextract(vm_offset_t virtualAddress);
 
 #define vtophys(virtualAddress) pmap_kextract((vm_offset_t)(virtualAddress))
+#define vmspace_pmap(...)	NULL
+#define pmap_extract(...)	NULL
+
 
 #endif	/* _FBSD_COMPAT_VM_VM_H_ */
