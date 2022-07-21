46fdf97dea86cc469ea1653ac287489ffbe29b75
Pascal Abresch <nep@packageloss.eu>
2022-07-21 19:56:46 +0200

    Interface Kit: add default dark appearence color constants
    Change-Id: I8added6ada702eb09e95f5f2d6e34b748067c6bc
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/4571
    Tested-by: Commit checker robot <no-reply+buildbot@haiku-os.org>
    Reviewed-by: Adrien Destugues <pulkomandy@pulkomandy.tk>

--- headers/private/interface/DefaultColors.h
+++ headers/private/interface/DefaultColors.h
@@ -1,18 +1,19 @@
 /*
- * Copyright 2006, Haiku, Inc.
+ * Copyright 2006-2022, Haiku, Inc.
  * Distributed under the terms of the MIT License.
  */
 #ifndef _DEFAULT_COLORS_H
 #define _DEFAULT_COLORS_H
 
 
-#include <GraphicsDefs.h>
-
+#include <InterfaceDefs.h>
 
 namespace BPrivate {
 
 extern const rgb_color* kDefaultColors;
 
-}	// namespace BPrivate
+rgb_color GetSystemColor(color_which, bool darkVariant);
+
+} // namespace BPrivate
 
 #endif // _DEFAULT_COLORS_H
