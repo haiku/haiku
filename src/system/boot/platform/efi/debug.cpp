9ae2ef5de65f46e090ebd4ac4e1740bf1d020c05
David Karoly <karolyd577@gmail.com>
2022-10-07 17:45:54 +0200

    boot/efi: implement ring buffer
    Change-Id: I7c384eb859e28ce824bd5cc5e85039d80462eb67
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/5722
    Tested-by: Commit checker robot <no-reply+buildbot@haiku-os.org>
    Reviewed-by: Adrien Destugues <pulkomandy@pulkomandy.tk>

--- src/system/boot/platform/efi/debug.cpp
+++ src/system/boot/platform/efi/debug.cpp
@@ -1,16 +1,18 @@
 /*
- * Copyright 2016 Haiku, Inc. All rights reserved.
+ * Copyright 2016-2022 Haiku, Inc. All rights reserved.
  * Distributed under the terms of the MIT License.
  */
 
+
+#include "debug.h"
 
 #include <string.h>
 
 #include <boot/platform.h>
 #include <boot/stage2.h>
 #include <boot/stdio.h>
+#include <util/ring_buffer.h>
 
-#include "debug.h"
 #include "efi_platform.h"
 #include "serial.h"
 
@@ -18,14 +20,18 @@
 static char sBuffer[16384];
 static uint32 sBufferPosition;
 
+static ring_buffer* sDebugSyslogBuffer = NULL;
+
 
 static void
 syslog_write(const char* buffer, size_t length)
 {
-	if (sBufferPosition + length > sizeof(sBuffer))
-		return;
-	memcpy(sBuffer + sBufferPosition, buffer, length);
-	sBufferPosition += length;
+	if (sDebugSyslogBuffer != NULL) {
+		ring_buffer_write(sDebugSyslogBuffer, (const uint8*)buffer, length);
+	} else if (sBufferPosition + length < sizeof(sBuffer)) {
+		memcpy(sBuffer + sBufferPosition, buffer, length);
+		sBufferPosition += length;
+	}
 }
 
 
@@ -36,6 +42,9 @@ dprintf_args(const char *format, va_list args)
 	int length = vsnprintf(buffer, sizeof(buffer), format, args);
 	if (length == 0)
 		return;
+
+	if (length >= (int)sizeof(buffer))
+		length = sizeof(buffer) - 1;
 
 	syslog_write(buffer, length);
 	serial_puts(buffer, length);
@@ -71,14 +80,45 @@ panic(const char *format, ...)
 }
 
 
+static void
+allocate_ring_buffer(void)
+{
+	// allocate 1 MB memory
+	void* buffer = NULL;
+	size_t size = 1024 * 1024;
+
+	if (platform_allocate_region(&buffer, size, 0, false) != B_OK)
+		return;
+
+	sDebugSyslogBuffer = create_ring_buffer_etc(buffer, size, 0);
+
+	gKernelArgs.debug_output = sDebugSyslogBuffer;
+	gKernelArgs.debug_size = sDebugSyslogBuffer->size;
+}
+
+
 void
 debug_cleanup(void)
 {
-	gKernelArgs.keep_debug_output_buffer = false;
-	gKernelArgs.debug_output = kernel_args_malloc(sBufferPosition);
-	if (gKernelArgs.debug_output != NULL) {
-		memcpy(gKernelArgs.debug_output, sBuffer, sBufferPosition);
-		gKernelArgs.debug_size = sBufferPosition;
+	allocate_ring_buffer();
+
+	if (sDebugSyslogBuffer != NULL) {
+		if (gKernelArgs.keep_debug_output_buffer) {
+			// copy the output gathered so far into the ring buffer
+			ring_buffer_clear(sDebugSyslogBuffer);
+			ring_buffer_write(sDebugSyslogBuffer, (uint8*)sBuffer,
+				sBufferPosition);
+		}
+	} else {
+		gKernelArgs.keep_debug_output_buffer = false;
+	}
+
+	if (!gKernelArgs.keep_debug_output_buffer) {
+		gKernelArgs.debug_output = kernel_args_malloc(sBufferPosition);
+		if (gKernelArgs.debug_output != NULL) {
+			memcpy(gKernelArgs.debug_output, sBuffer, sBufferPosition);
+			gKernelArgs.debug_size = sBufferPosition;
+		}
 	}
 }
 
