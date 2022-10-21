74622d8051323f8374c8736524fde4ce9e1f61df
Jérôme Duval <jerome.duval@gmail.com>
2022-10-21 16:28:38 +0200

    Devices: add vendor names for ACPI devices when available
    files can be updated so:
    curl --fail -L -o acpi_id_registry.html 'https://uefi.org/uefi-acpi-export'
    curl --fail -L -o pnp_id_registry.html 'https://uefi.org/uefi-pnp-export'
    
    Change-Id: Id0456f925729c635c5e2d43f80e9e016483b2611
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/5753
    Tested-by: Commit checker robot <no-reply+buildbot@haiku-os.org>
    Reviewed-by: Jérôme Duval <jerome.duval@gmail.com>

--- src/apps/devices/DeviceACPI.cpp
+++ src/apps/devices/DeviceACPI.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2019 Haiku Inc. All rights reserved.
+ * Copyright 2008-2022 Haiku Inc. All rights reserved.
  * Distributed under the terms of the MIT License.
  *
  * Authors:
@@ -15,8 +15,25 @@
 
 #include <Catalog.h>
 
+extern "C" {
+#include "acpipnpids.h"
+}
+
 #undef B_TRANSLATION_CONTEXT
 #define B_TRANSLATION_CONTEXT "DeviceACPI"
+
+
+void
+acpi_get_vendor_info(const char* vendorID, const char **vendorName)
+{
+	for (size_t i = 0; i < ACPIPNP_DEVTABLE_LEN; i++) {
+		if (strncmp(acpipnp_devids[i].VenId, vendorID, strlen(acpipnp_devids[i].VenId)) == 0) {
+			*vendorName = acpipnp_devids[i].VenName;
+			return;
+		}
+	}
+	*vendorName = NULL;
+}
 
 
 DeviceACPI::DeviceACPI(Device* parent)
@@ -34,10 +51,10 @@ DeviceACPI::~DeviceACPI()
 void
 DeviceACPI::InitFromAttributes()
 {
-	BString outlineName;
 	BString nodeACPIPath;
 	BString rootACPIPath;
 	BString nodeACPIHid;
+	BString deviceName;
 
 	rootACPIPath = nodeACPIPath = GetAttribute("acpi/path").fValue;
 	nodeACPIHid = GetAttribute("acpi/hid").fValue;
@@ -52,39 +69,50 @@ DeviceACPI::InitFromAttributes()
 
 	// Identify Predefined root namespaces (ACPI Spec 4.0a, p162)
 	if (rootACPIPath == "\\_SB_") {
-		outlineName = B_TRANSLATE("ACPI System Bus");
+		deviceName = B_TRANSLATE("ACPI System Bus");
 	} else if (rootACPIPath == "\\_TZ_") {
-		outlineName = B_TRANSLATE("ACPI Thermal Zone");
+		deviceName = B_TRANSLATE("ACPI Thermal Zone");
 	} else if (rootACPIPath == "\\_PR_.") {
 		// This allows to localize apostrophes, too
 		BString string(B_TRANSLATE("ACPI Processor Namespace '%2'"));
 		string.ReplaceFirst("%2", nodeACPIPath);
 		// each CPU node is considered a root node
-		outlineName << string.String();
+		deviceName << string.String();
 	} else if (rootACPIPath == "\\_SI_") {
-		outlineName = B_TRANSLATE("ACPI System Indicator");
+		deviceName = B_TRANSLATE("ACPI System Indicator");
 	} else if (nodeACPIPath != "") {
 		// This allows to localize apostrophes, too
 		BString string(B_TRANSLATE("ACPI node '%1'"));
 		string.ReplaceFirst("%1", nodeACPIPath);
-		outlineName << string.String();
+		deviceName << string.String();
 	} else if (nodeACPIPath == "" && nodeACPIHid != "") {
 		// Handle ACPI HID entries that do not return a path
 		nodeACPIHid.Remove(0, nodeACPIHid.FindLast("_") + 1);
 		BString string(B_TRANSLATE("ACPI Button '%1'")); 
 		string.ReplaceFirst("%1", nodeACPIHid); 
-		outlineName << string.String();
+		deviceName << string.String();
 	} else {
 		BString string(B_TRANSLATE("ACPI <unknown>"));
-		outlineName << string.String();
-	} 
+		deviceName << string.String();
+	}
 
-	SetAttribute(B_TRANSLATE("Device name"), outlineName.String());
-#if 0
-	// These are a source of confusion for users.
-	// Until we can display something useful, let's not show the lines at all.
-	SetAttribute(B_TRANSLATE("Manufacturer"), B_TRANSLATE("Not implemented"));
-#endif
+	// Fetch ManufacturerName
+	const char* vendorName = NULL;
+	BString manufacturerLabel;
+	if (nodeACPIHid != "")
+		acpi_get_vendor_info(nodeACPIHid, &vendorName);
+	if (vendorName == NULL) {
+		manufacturerLabel << B_TRANSLATE("Unknown");
+	} else {
+		manufacturerLabel << vendorName;
+	};
+
+	SetAttribute(B_TRANSLATE("Device name"), deviceName.String());
+	SetAttribute(B_TRANSLATE("Manufacturer"), manufacturerLabel);
+	BString outlineName;
+	if (vendorName != NULL)
+		outlineName << vendorName << " ";
+	outlineName << deviceName;
 	SetText(outlineName.String());
 }
 
