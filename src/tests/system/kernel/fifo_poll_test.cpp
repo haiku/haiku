a6957d9ff4765863cac1422a5f58c5a8a874d03c
PulkoMandy <pulkomandy@pulkomandy.tk>
2023-07-04 20:17:36 +0200

    fifo_poll_test: add handling of EINTR signal
    This is the test for #7859, the problem now is just that the SIGCHLD
    signal is delivered before the file descriptor close event wakes up the
    poll().
    
    Change-Id: Iddf96e2830d5c7496ec3c6bd27cc235c5ce9d7d4

--- src/tests/system/kernel/fifo_poll_test.cpp
+++ src/tests/system/kernel/fifo_poll_test.cpp
@@ -1,7 +1,17 @@
+#include <errno.h>
 #include <stdio.h>
 #include <poll.h>
 
 int main() {
+	/* Test for #7859.
+	 * Start a process with popen and watch the pipe using poll().
+	 * We should get these events (preferrably in this exact order):
+	 * - 3 reads of the values 1, 2 and 3 from the bash script
+	 * - 1 end of file event (that should make the pipe readable)
+	 * - 1 EINTR return from poll because of the SIGCHLD signal when the child process terminates
+	 *
+	 * Currently, the EINTR return happens before the end of file event notification.
+	 */
 	FILE* f = popen("/bin/bash -c 'for i in 1 2 3; do { echo $i; sleep 1; }; done'", "r");
 	printf("f=%p\n", f);
 	int fd = fileno(f);
@@ -18,8 +28,15 @@ int main() {
 		printf("rv=%d\n", rv);
 		if (rv == 0)
 			continue;
-		if (rv < 0)
-			break;
+		if (rv < 0) {
+			if (errno == EINTR) {
+				puts("warning: received SIGCHLD before stream close event");
+				continue;
+			} else {
+				printf("poll returns with error %s\n", strerror(errno));
+				break;
+			}
+		}
 		printf("events=%08x revents=%08x\n", pfd.events, pfd.revents);
 		if ((pfd.events & pfd.revents) == 0)
 			break;
