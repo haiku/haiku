39fa47a536dc4a7746eccce824ed094152afe506
David Karoly <karolyd577@gmail.com>
2023-09-19 18:10:36 +0200

    boot/next_m68k/mmu: implement missing functions
    Change-Id: I78b51ab394c9083b081f12ca4e02f50904f67674
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/6938
    Tested-by: Commit checker robot <no-reply+buildbot@haiku-os.org>
    Reviewed-by: waddlesplash <waddlesplash@gmail.com>
    Reviewed-by: Fredrik Holmqvist <fredrik.holmqvist@gmail.com>

--- src/system/boot/platform/next_m68k/mmu.cpp
+++ src/system/boot/platform/next_m68k/mmu.cpp
@@ -166,3 +166,21 @@ platform_init_heap(struct stage2_args *args, void **_base, void **_top)
 }
 
 
+extern "C" status_t
+platform_bootloader_address_to_kernel_address(void *address, addr_t *_result)
+{
+	TRACE(("%s: called\n", __func__));
+	// next_m68k really doesn't need an address conversion
+	*_result = (addr_t)address;
+	return B_OK;
+}
+
+
+extern "C" status_t
+platform_kernel_address_to_bootloader_address(addr_t address, void **_result)
+{
+	TRACE(("%s: called\n", __func__));
+	// next_m68k really doesn't need an address conversion
+	*_result = (void*)address;
+	return B_OK;
+}
