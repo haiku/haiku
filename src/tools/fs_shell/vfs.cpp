4b3e80f6b592e59f2a17fac411ea2317f05ebb02
JackBurton79 <stefano.ceccherini@gmail.com>
2023-10-25 00:06:46 +0200

    Fix build on arm64 (and other 64 bit non x86 platforms, probably)
    Change-Id: I0b09ab8925244746eded66de924d9dbf36693c22
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/7064
    Reviewed-by: waddlesplash <waddlesplash@gmail.com>

--- src/tools/fs_shell/vfs.cpp
+++ src/tools/fs_shell/vfs.cpp
@@ -3678,7 +3678,7 @@ common_unlock_node(int fd, bool kernel)
 
 	// We need to set the locking atomically - someone
 	// else might set one at the same time
-#ifdef __x86_64__
+#if LONG_MAX == INT_MAX
 	if (fssh_atomic_test_and_set64((int64_t *)&vnode->mandatory_locked_by,
 			0, (fssh_addr_t)descriptor) != (int64_t)descriptor)
 #else
