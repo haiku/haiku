088c695b175c23090e4a362e6fac4f9484952f60
Augustin Cavalier <waddlesplash@gmail.com>
2024-06-25 17:14:45 -0400

    freebsd_network: Remove fls/ffsl from generic/cpufunc.h

--- src/libs/compat/freebsd_network/compat/machine/generic/cpufunc.h
+++ src/libs/compat/freebsd_network/compat/machine/generic/cpufunc.h
@@ -6,31 +6,4 @@
 #define	_FBSD_MACHINE_GENERIC_CPUFUNC_H_
 
 
-#define	HAVE_INLINE_FLS
-static __inline int
-fls(int mask)
-{
-	int bit;
-	if (mask == 0)
-		return 0;
-	for (bit = 1; mask != 1; bit++)
-		mask = (unsigned int) mask >> 1;
-	return bit;
-}
-
-
-#define	HAVE_INLINE_FFSL
-static __inline int
-ffsl(long mask)
-{
-	int bit;
-
-	if (mask == 0)
-		return (0);
-	for (bit = 1; !(mask & 1); bit++)
-		mask = (unsigned long)mask >> 1;
-	return (bit);
-}
-
-
 #endif /* _FBSD_MACHINE_GENERIC_CPUFUNC_H_ */
