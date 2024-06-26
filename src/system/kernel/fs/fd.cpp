haiku/haiku

commit 34fcf3d9ead1371b57e46877071ced38235e789e

kernel/fs: Refactor file_descriptor seek-disabled behavior.
 * Default "pos" to "-1" instead of 0 and check for this value
   instead of ops->fd_seek directly in IO operations.

 * Set "pos" to "0" only for seekable vnodes.

 * Return ESPIPE in read() and write() if the specified pos is not
   -1, but the descriptor->pos is.

Fixes the VFS part of #18836.

Change-Id: Ib4da1652b06798588abedc98963aeb63511d3e41
Reviewed-on: https://review.haiku-os.org/c/haiku/+/7544
Reviewed-by: waddlesplash <waddlesplash@gmail.com>

src/system/kernel/fs/fd.cpp
@@ -84,7 +84,7 @@ alloc_fd(void)
 	descriptor->ref_count = 1;
 	descriptor->open_count = 0;
 	descriptor->open_mode = 0;
-	descriptor->pos = 0;
+	descriptor->pos = -1;
 
 	return descriptor;
 }
@@ -697,7 +697,7 @@ common_user_io(int fd, off_t pos, void* buffer, size_t length, bool write)
 	}
 
 	bool movePosition = false;
-	if (pos == -1 && descriptor->ops->fd_seek != NULL) {
+	if (pos == -1 && descriptor->pos != -1) {
 		pos = descriptor->pos;
 		movePosition = true;
 	}
@@ -755,7 +755,7 @@ common_user_vector_io(int fd, off_t pos, const iovec* userVecs, size_t count,
 	}
 
 	bool movePosition = false;
-	if (pos == -1 && descriptor->ops->fd_seek != NULL) {
+	if (pos == -1 && descriptor->pos != -1) {
 		pos = descriptor->pos;
 		movePosition = true;
 	}
@@ -1000,7 +1000,7 @@ _kern_read(int fd, off_t pos, void* buffer, size_t length)
 		return B_FILE_ERROR;
 
 	bool movePosition = false;
-	if (pos == -1) {
+	if (pos == -1 && descriptor->pos != -1) {
 		pos = descriptor->pos;
 		movePosition = true;
 	}
@@ -1029,7 +1029,6 @@ _kern_read(int fd, off_t pos, void* buffer, size_t length)
 ssize_t
 _kern_readv(int fd, off_t pos, const iovec* vecs, size_t count)
 {
-	bool movePosition = false;
 	status_t status;
 
 	if (pos < -1)
@@ -1042,7 +1041,8 @@ _kern_readv(int fd, off_t pos, const iovec* vecs, size_t count)
 	if ((descriptor->open_mode & O_RWMASK) == O_WRONLY)
 		return B_FILE_ERROR;
 
-	if (pos == -1) {
+	bool movePosition = false;
+	if (pos == -1 && descriptor->pos != -1) {
 		pos = descriptor->pos;
 		movePosition = true;
 	}
@@ -1092,7 +1092,7 @@ _kern_write(int fd, off_t pos, const void* buffer, size_t length)
 		return B_FILE_ERROR;
 
 	bool movePosition = false;
-	if (pos == -1) {
+	if (pos == -1 && descriptor->pos != -1) {
 		pos = descriptor->pos;
 		movePosition = true;
 	}
@@ -1121,7 +1121,6 @@ _kern_write(int fd, off_t pos, const void* buffer, size_t length)
 ssize_t
 _kern_writev(int fd, off_t pos, const iovec* vecs, size_t count)
 {
-	bool movePosition = false;
 	status_t status;
 
 	if (pos < -1)
@@ -1134,7 +1133,8 @@ _kern_writev(int fd, off_t pos, const iovec* vecs, size_t count)
 	if ((descriptor->open_mode & O_RWMASK) == O_RDONLY)
 		return B_FILE_ERROR;
 
-	if (pos == -1) {
+	bool movePosition = false;
+	if (pos == -1 && descriptor->pos != -1) {
 		pos = descriptor->pos;
 		movePosition = true;
 	}

src/system/kernel/fs/vfs.cpp
@@ -2841,6 +2841,22 @@ get_new_fd(int type, struct fs_mount* mount, struct vnode* vnode,
 	descriptor->type = type;
 	descriptor->open_mode = openMode;
 
+	if (descriptor->ops->fd_seek != NULL) {
+		// some kinds of files are not seekable
+		switch (vnode->Type() & S_IFMT) {
+			case S_IFIFO:
+			case S_IFSOCK:
+				ASSERT(descriptor->pos == -1);
+				break;
+
+			// The Open Group Base Specs don't mention any file types besides pipes,
+			// FIFOs, and sockets specially, so we allow seeking all others.
+			default:
+				descriptor->pos = 0;
+				break;
+		}
+	}
+
 	io_context* context = get_current_io_context(kernel);
 	fd = new_fd(context, descriptor);
 	if (fd < 0) {
@@ -5686,6 +5702,8 @@ file_read(struct file_descriptor* descriptor, off_t pos, void* buffer,
 
 	if (S_ISDIR(vnode->Type()))
 		return B_IS_A_DIRECTORY;
+	if (pos != -1 && descriptor->pos == -1)
+		return ESPIPE;
 
 	return FS_CALL(vnode, read, descriptor->cookie, pos, buffer, length);
 }
@@ -5701,6 +5719,9 @@ file_write(struct file_descriptor* descriptor, off_t pos, const void* buffer,
 
 	if (S_ISDIR(vnode->Type()))
 		return B_IS_A_DIRECTORY;
+	if (pos != -1 && descriptor->pos == -1)
+		return ESPIPE;
+
 	if (!HAS_FS_CALL(vnode, write))
 		return B_READ_ONLY_DEVICE;
 
@@ -5718,22 +5739,14 @@ file_seek(struct file_descriptor* descriptor, off_t pos, int seekType)
 	FUNCTION(("file_seek(pos = %" B_PRIdOFF ", seekType = %d)\n", pos,
 		seekType));
 
-	// some kinds of files are not seekable
-	switch (vnode->Type() & S_IFMT) {
-		case S_IFIFO:
-		case S_IFSOCK:
-			return ESPIPE;
+	if (descriptor->pos == -1)
+		return ESPIPE;
 
+	switch (vnode->Type() & S_IFMT) {
 		// drivers publish block devices as chr, so pick both
 		case S_IFBLK:
 		case S_IFCHR:
 			isDevice = true;
-			break;
-		// The Open Group Base Specs don't mention any file types besides pipes,
-		// fifos, and sockets specially, so we allow seeking them.
-		case S_IFREG:
-		case S_IFDIR:
-		case S_IFLNK:
 			break;
 	}
 

commit 20ac27def64f92a5003e04e2d40d4f9647fc183c

kernel/fs: Fix readv/writev after the seek-disabled refactor.
This fixes a regression introduced in 34fcf3d9ead1371b57e46877071ced38235e789e.

That commit correctly adjusted the "pos" checks at the top of these
functions, but missed that there was a place in the loop where pos
is incremented. After the first increment, we would have a non-zero
pos, and so the new checks in the actual read/write routines would
return an error, meaning that only the first iovec was ever processed.

This fixes WINE following the aforementioned change.

src/system/kernel/fs/fd.cpp
@@ -814,7 +814,8 @@ common_user_vector_io(int fd, off_t pos, const iovec* userVecs, size_t count,
 		else
 			bytesTransferred += (ssize_t)length;
 
-		pos += length;
+		if (movePosition)
+			pos += length;
 
 		if (length < vecs[i].iov_len)
 			break;
@@ -1089,7 +1090,8 @@ _kern_readv(int fd, off_t pos, const iovec* vecs, size_t count)
 		else
 			bytesRead += (ssize_t)length;
 
-		pos += vecs[i].iov_len;
+		if (movePosition)
+			pos += vecs[i].iov_len;
 	}
 
 	if (movePosition)
@@ -1181,7 +1183,8 @@ _kern_writev(int fd, off_t pos, const iovec* vecs, size_t count)
 		else
 			bytesWritten += (ssize_t)length;
 
-		pos += vecs[i].iov_len;
+		if (movePosition)
+			pos += vecs[i].iov_len;
 	}
 
 	if (movePosition)