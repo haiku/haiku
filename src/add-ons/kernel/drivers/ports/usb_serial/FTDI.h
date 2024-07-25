4c62171e8899e27beb8a785f9914c98b130b762a
Alexander von Gluck <alex@terarocket.io>
2024-07-25 15:57:07 -0500

    usb_serial: Add missing FTDI uarts USBID's
    * Witnessed at linux drivers/usb/serial/ftdi_sio_ids.h
    * I have a 0403:6010 in hand, validated functional
    * Looking at the linux code, there aren't any special
      cases for these alternate FTDI devices.
    * There are a bunch of clones we could add per the above
      reference, but lets just support the "official" ones for now.
    
    Change-Id: I4c1aa162b94753431d7497a65f646faec5cccfad
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/7902
    Tested-by: Commit checker robot <no-reply+buildbot@haiku-os.org>
    Reviewed-by: Adrien Destugues <pulkomandy@pulkomandy.tk>
    Reviewed-by: Alexander von Gluck <alex@terarocket.io>

--- src/add-ons/kernel/drivers/ports/usb_serial/FTDI.h
+++ src/add-ons/kernel/drivers/ports/usb_serial/FTDI.h
@@ -21,7 +21,18 @@
 const usb_serial_device kFTDIDevices[] = {
 	{VENDOR_FTDI, 0x8372, "FTDI 8U100AX serial converter"},
 	{VENDOR_FTDI, 0x6001, "FTDI 8U232AM serial converter"},
-	{VENDOR_FTDI, 0x6015, "FTDI FT231X serial converter"}
+	{VENDOR_FTDI, 0x6006, "FTDI 8U232AM serial converter"},
+	{VENDOR_FTDI, 0x6010, "FTDI 8U2232C serial converter"},
+	{VENDOR_FTDI, 0x6011, "FTDI 4232H serial converter"},
+	{VENDOR_FTDI, 0x6014, "FTDI 232H serial converter"},
+	{VENDOR_FTDI, 0x6015, "FTDI FT231X serial converter"},
+	{VENDOR_FTDI, 0x6040, "FTDI FT2233 serial converter"},
+	{VENDOR_FTDI, 0x6041, "FTDI FT4233 serial converter"},
+	{VENDOR_FTDI, 0x6042, "FTDI FT2232 serial converter"},
+	{VENDOR_FTDI, 0x6043, "FTDI FT4232 serial converter"},
+	{VENDOR_FTDI, 0x6044, "FTDI FT233 serial converter"},
+	{VENDOR_FTDI, 0x6045, "FTDI FT232 serial converter"},
+	{VENDOR_FTDI, 0x6048, "FTDI FT4232 serial converter"},
 };
 
 #define FTDI_BUFFER_SIZE		64
