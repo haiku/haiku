32edc757b010710685ea87fa873ca7d676258fba
Augustin Cavalier <waddlesplash@gmail.com>
2024-08-08 10:57:57 -0400

    Disable serial debug output by default.
    EFI doesn't have a boot keys mechanism so we just disable the
    routine that enables it entirely. Also disable it in early kernel
    output before the settings file has been read.

--- src/system/boot/platform/efi/serial.cpp
+++ src/system/boot/platform/efi/serial.cpp
@@ -97,9 +97,11 @@ serial_disable(void)
 extern "C" void
 serial_enable(void)
 {
+#if 0
 	sSerialEnabled = true;
 	if ((gUART != NULL) && !gUARTSkipInit)
 		gUART->InitPort(kSerialBaudRate);
+#endif
 }
 
 
