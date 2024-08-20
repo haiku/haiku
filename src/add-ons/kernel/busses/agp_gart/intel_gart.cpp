7f2dcab0e69204ed60237f5e9c4229d0424b896a
Fabio Tomat <f.t.public@gmail.com>
2024-08-20 20:26:28 +0200

    intel_extreme: enable support for GeminiLake and IceLake devices.
    Change-Id: I192db4329b92723a3ea988dabb85e595bcc0d686
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/8083
    Tested-by: Commit checker robot <no-reply+buildbot@haiku-os.org>
    Reviewed-by: Oscar Lesta <oscar.lesta@gmail.com>
    Reviewed-by: Jérôme Duval <jerome.duval@gmail.com>

--- src/add-ons/kernel/busses/agp_gart/intel_gart.cpp
+++ src/add-ons/kernel/busses/agp_gart/intel_gart.cpp
@@ -176,6 +176,9 @@ const struct supported_device {
 	{0x5904, 0x5926, INTEL_MODEL_KBY,  "Kabylake ULT GT3"},
 	{0x5904, 0x5927, INTEL_MODEL_KBY,  "Kabylake ULT GT3"},
 
+	{0x31f0, 0x3185, INTEL_MODEL_KBY,  "GeminiLake GT1"},
+	{0x31f0, 0x3184, INTEL_MODEL_KBY,  "GeminiLake GT1.5"},
+
 	{0x3e0f, 0x3e90, INTEL_MODEL_CFL,  "CoffeeLake GT1"},
 	{0x3e0f, 0x3e93, INTEL_MODEL_CFL,  "CoffeeLake GT1"},
 	{0x3e1f, 0x3e91, INTEL_MODEL_CFL,  "CoffeeLake GT2"},
@@ -188,6 +191,13 @@ const struct supported_device {
 	{0x3ec4, 0x3eab, INTEL_MODEL_CFLM, "CoffeeLake Halo GT2"},
 	{0x3ed0, 0x3ea5, INTEL_MODEL_CFL,  "CoffeeLake GT3"},
 	{0x3ed0, 0x3ea6, INTEL_MODEL_CFL,  "CoffeeLake GT3"},
+
+	{0x8a02, 0x8a56, INTEL_MODEL_CFLM, "IceLake GT1"},
+	{0x8a12, 0x8a5a, INTEL_MODEL_CFLM, "IceLake GT1.5"},
+	{0x8a06, 0x8a5c, INTEL_MODEL_CFLM, "IceLake GT1.5"},
+	{0x8a16, 0x8a51, INTEL_MODEL_CFLM, "IceLake GT2"},
+	{0x8a12, 0x8a52, INTEL_MODEL_CFLM, "IceLake GT2"},
+	{0x8a14, 0x8a53, INTEL_MODEL_CFLM, "IceLake GT2"},
 
 	{0x9b64, 0x9ba4, INTEL_MODEL_CML,	"CometLake GT1"},
 	{0x9b73, 0x9ba8, INTEL_MODEL_CML,	"CometLake GT1"},
