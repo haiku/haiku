fbe39bde6b62b43d30dc274850cc277b9cda78cb
Augustin Cavalier <waddlesplash@gmail.com>
2024-08-31 11:56:21 -0400

    mknod[at]: Implement the case where the node type is S_IFIFO.
    This is the only case the POSIX specification indicates is portable.
    We may be able to implement some of the others that Linux or the BSDs
    support if it turns out there are applications that want them, though.
    
    Fixes #19032.
    (cherry picked from commit 1c22c2608fc46aa8b2bf42a3160bf12cbd95a50f)
    
    Change-Id: I10f936e0f58f3beb518e46a3e93e748dddeb0754
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/8129
    Reviewed-by: waddlesplash <waddlesplash@gmail.com>

--- src/system/libroot/posix/sys/mknod.c
+++ src/system/libroot/posix/sys/mknod.c
@@ -1,27 +1,34 @@
 /*
  * Copyright 2004-2009, Axel Dörfler, axeld@pinc-software.de.
+ * Copyright 2024, Haiku, Inc. All rights reserved.
  * Distributed under the terms of the MIT License.
  */
 
 
 #include <errno.h>
+#include <fcntl.h>
 #include <sys/stat.h>
 
 #include <errno_private.h>
 
 
 int
-mknod(const char *name, mode_t mode, dev_t dev)
+mknod(const char *path, mode_t mode, dev_t dev)
 {
-	__set_errno(ENOTSUP);
-	return -1;
+	return mknodat(AT_FDCWD, path, mode, dev);
 }
 
 
 int
-mknodat(int fd, const char *name, mode_t mode, dev_t dev)
+mknodat(int fd, const char *path, mode_t mode, dev_t dev)
 {
+	if (dev == 0) {
+		mode_t type = mode & S_IFMT;
+		mode &= ~S_IFMT;
+		if (type == S_IFIFO)
+			return mkfifoat(fd, path, mode);
+	}
+
 	__set_errno(ENOTSUP);
 	return -1;
 }
-
