0b04cf2f5bac2b6572481ac6d8803085b5059a4c
Augustin Cavalier <waddlesplash@gmail.com>
2024-11-09 14:14:52 -0500

    libroot: Merge sigtimedwait.cpp into sigwaitinfo.cpp.
    No functional change intended.

--- src/system/libroot/posix/signal/sigwaitinfo.cpp
+++ src/system/libroot/posix/signal/sigwaitinfo.cpp
@@ -6,9 +6,52 @@
 
 #include <signal.h>
 
+#include <errno.h>
+#include <pthread.h>
+
+#include <syscall_utils.h>
+
+#include <errno_private.h>
+#include <time_private.h>
+#include <syscalls.h>
+
 
 int
 sigwaitinfo(const sigset_t* set, siginfo_t* info)
 {
 	return sigtimedwait(set, info, NULL);
+}
+
+
+int
+sigtimedwait(const sigset_t* set, siginfo_t* info,
+	const struct timespec* timeout)
+{
+	// make info non-NULL to simplify things
+	siginfo_t stackInfo;
+	if (info == NULL)
+		info = &stackInfo;
+
+	// translate the timeout
+	uint32 flags;
+	bigtime_t timeoutMicros = 0;
+	if (timeout != NULL) {
+		if (!timespec_to_bigtime(*timeout, timeoutMicros))
+			RETURN_AND_SET_ERRNO(EINVAL);
+
+		timeoutMicros += system_time();
+		flags = B_ABSOLUTE_TIMEOUT;
+	} else {
+		flags = 0;
+		timeoutMicros = 0;
+	}
+
+	status_t error = _kern_sigwait(set, info, flags, timeoutMicros);
+
+	pthread_testcancel();
+
+	if (error != B_OK)
+		RETURN_AND_SET_ERRNO(error);
+
+	return info->si_signo;
 }
