haiku/haiku

commit 12d6ecf0df54dc8752f7ccd1f430d74f45fcbd9b

kernel/fs: Allocate FD tables separately instead of in one malloc().
This way, we significantly increase the FD table sizes that can
be allocated without needing a "raw" allocation: previously
an FD table size of 512 would've been too large (on x86_64),
while now, tables of up to size 1024 will fit (so long as the
largest block allocator size is 8192, anyway.)

src/system/kernel/fs/vfs.cpp
@@ -5046,17 +5046,22 @@ vfs_resize_fd_table(struct io_context* context, uint32 newSize)
 	select_info** oldSelectInfos = context->select_infos;
 	uint8* oldCloseOnExecTable = context->fds_close_on_exec;
 
-	// allocate new tables
+	// allocate new tables (separately to reduce the chances of needing a raw allocation)
 	file_descriptor** newFDs = (file_descriptor**)malloc(
-		sizeof(struct file_descriptor*) * newSize
-		+ sizeof(struct select_infos**) * newSize
-		+ newCloseOnExitBitmapSize);
-	if (newFDs == NULL)
+		sizeof(struct file_descriptor*) * newSize);
+	select_info** newSelectInfos = (select_info**)malloc(
+		+ sizeof(select_info**) * newSize);
+	uint8* newCloseOnExecTable = (uint8*)malloc(newCloseOnExitBitmapSize);
+	if (newFDs == NULL || newSelectInfos == NULL || newCloseOnExecTable == NULL) {
+		free(newFDs);
+		free(newSelectInfos);
+		free(newCloseOnExecTable);
 		return B_NO_MEMORY;
+	}
 
 	context->fds = newFDs;
-	context->select_infos = (select_info**)(context->fds + newSize);
-	context->fds_close_on_exec = (uint8*)(context->select_infos + newSize);
+	context->select_infos = newSelectInfos;
+	context->fds_close_on_exec = newCloseOnExecTable;
 	context->table_size = newSize;
 
 	if (oldSize != 0) {
@@ -5079,6 +5084,8 @@ vfs_resize_fd_table(struct io_context* context, uint32 newSize)
 	}
 
 	free(oldFDs);
+	free(oldSelectInfos);
+	free(oldCloseOnExecTable);
 
 	return B_OK;
 }

commit 6d6a53f147d3d85db802efd20f8aac988a439805

kernel/fs: Add missing frees to I/O context destruction.
Fixes a memory leak regression from 12d6ecf0df54dc8752f7ccd1f430d74f45fcbd9b.

src/system/kernel/fs/vfs.cpp
@@ -3657,6 +3657,9 @@ free_io_context(io_context* context)
 	rw_lock_destroy(&context->lock);
 
 	remove_node_monitors(context);
+
+	free(context->fds_close_on_exec);
+	free(context->select_infos);
 	free(context->fds);
 	free(context);
 
@@ -5036,7 +5039,7 @@ vfs_resize_fd_table(struct io_context* context, uint32 newSize)
 	// If the tables shrink, make sure none of the fds being dropped are in use.
 	if (newSize < oldSize) {
 		for (uint32 i = oldSize; i-- > newSize;) {
-			if (context->fds[i])
+			if (context->fds[i] != NULL)
 				return B_BUSY;
 		}
 	}