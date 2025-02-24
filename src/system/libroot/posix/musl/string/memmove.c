ce7c098db258a0c4ba51c2df41732f37ca6dfe32
Augustin Cavalier <waddlesplash@gmail.com>
2025-02-24 14:12:28 -0500

    libroot: Replace memmove() with musl's.
    Includes an optimization to use memcpy where possible, and protection
    against aliasing undefined behavior.
    
    Also clean up the Jamrules for the kernel's usage of musl methods.

---
+++ src/system/libroot/posix/musl/string/memmove.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include <stdint.h>
+
+#if defined(__GNUC__) && __GNUC__ >= 4
+typedef __attribute__((__may_alias__)) size_t WT;
+#define WS (sizeof(WT))
+#endif
+
+void *memmove(void *dest, const void *src, size_t n)
+{
+	char *d = dest;
+	const char *s = src;
+
+	if (d==s) return d;
+	if ((uintptr_t)s-(uintptr_t)d-n <= -2*n) return memcpy(d, s, n);
+
+	if (d<s) {
+#if defined(__GNUC__) && __GNUC__ >= 4
+		if ((uintptr_t)s % WS == (uintptr_t)d % WS) {
+			while ((uintptr_t)d % WS) {
+				if (!n--) return dest;
+				*d++ = *s++;
+			}
+			for (; n>=WS; n-=WS, d+=WS, s+=WS) *(WT *)d = *(WT *)s;
+		}
+#endif
+		for (; n; n--) *d++ = *s++;
+	} else {
+#if defined(__GNUC__) && __GNUC__ >= 4
+		if ((uintptr_t)s % WS == (uintptr_t)d % WS) {
+			while ((uintptr_t)(d+n) % WS) {
+				if (!n--) return dest;
+				d[n] = s[n];
+			}
+			while (n>=WS) n-=WS, *(WT *)(d+n) = *(WT *)(s+n);
+		}
+#endif
+		while (n) n--, d[n] = s[n];
+	}
+
+	return dest;
+}
