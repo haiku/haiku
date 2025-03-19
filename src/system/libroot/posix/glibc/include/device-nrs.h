ac70aeab5bb5ab8a1c169e3a00c8e836814f4ffe
Augustin Cavalier <waddlesplash@gmail.com>
2025-03-19 13:29:41 -0400

    libroot/glibc: Remove some more unneeded stuff.

--- src/system/libroot/posix/glibc/include/device-nrs.h
+++ src/system/libroot/posix/glibc/include/device-nrs.h
@@ -1,7 +1,4 @@
 #ifndef _DEVICE_NRS_H_
 #define _DEVICE_NRS_H_
 
-// ToDo: do something nice here, perhaps reusing the new stat->st_type field
-#define DEV_TTY_P(stat) (0)
-
 #endif	/* _DEVICE_NRS_H_ */
