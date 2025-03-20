2ab23a1adeb50f4e9787505948c510f4c6a0931c
Augustin Cavalier <waddlesplash@gmail.com>
2025-03-20 13:50:47 -0400

    libroot/glibc: Fold needed declarations in stdlib/stdlib.h into include/stdlib.h.
    Most aren't needed. This fixes quite a lot of compiler warnings.

--- src/system/libroot/posix/glibc/include/stdlib.h
+++ src/system/libroot/posix/glibc/include/stdlib.h
@@ -1,13 +1,10 @@
-#ifndef _STDLIB_H
+#ifndef _GLIBC_STDLIB_H
+#define _GLIBC_STDLIB_H
 
-#ifdef __need_malloc_and_calloc
-#define __Need_M_And_C
-#endif
-
-#include <stdlib/stdlib.h>
+#include_next <stdlib.h>
+#include <stdint.h>
 
 /* Now define the internal interfaces.  */
-#ifndef __Need_M_And_C
 extern long int __random (void);
 extern void __srandom (unsigned int __seed);
 extern char *__initstate (unsigned int __seed, char *__statebuf,
@@ -15,10 +12,15 @@ extern char *__initstate (unsigned int __seed, char *__statebuf,
 extern char *__setstate (char *__statebuf);
 extern int __random_r (struct random_data *__buf, int32_t *__result);
 extern int __srandom_r (unsigned int __seed, struct random_data *__buf);
-extern int __initstate_r (unsigned int __seed, char *__statebuf,
-			  size_t __statelen, struct random_data *__buf);
-extern int __setstate_r (char *__statebuf, struct random_data *__buf);
 extern int __rand_r (unsigned int *__seed);
+
+struct drand48_data {
+	unsigned short int __x[3];
+	unsigned short int __old_x[3];
+	unsigned short int __c;
+	unsigned short int __init;
+	unsigned long long int __a;
+};
 extern int __erand48_r (unsigned short int __xsubi[3],
 			struct drand48_data *__buffer, double *__result);
 extern int __nrand48_r (unsigned short int __xsubi[3],
@@ -38,34 +40,51 @@ extern int __lcong48_r (unsigned short int __param[7],
 extern int __drand48_iterate (unsigned short int __xsubi[3],
 			      struct drand48_data *__buffer);
 
-extern int __setenv (__const char *__name, __const char *__value,
-		     int __replace);
-extern int __unsetenv (__const char *__name);
-extern int __clearenv (void);
-extern char *__canonicalize_file_name (__const char *__name);
-extern char *__realpath (__const char *__name, char *__resolved);
-extern int __ptsname_r (int __fd, char *__buf, size_t __buflen);
-extern int __getpt (void);
-extern int __posix_openpt (int __oflag);
-
-extern int __add_to_environ (const char *name, const char *value,
-			     const char *combines, int replace);
-
-extern void _quicksort (void *const pbase, size_t total_elems,
-			size_t size, __compar_fn_t cmp);
-
-extern int __on_exit (void (*__func) (int __status, void *__arg), void *__arg);
-
 extern int __cxa_atexit (void (*func) (void *), void *arg, void *d);
 
 extern void __cxa_finalize (void *d);
 
-extern int __posix_memalign (void **memptr, size_t alignment, size_t size)
-     __attribute_malloc__;
+/* The internal entry points for `strtoX' take an extra flag argument
+   saying whether or not to parse locale-dependent number grouping.  */
 
-extern int __libc_system (const char *line);
-
+extern double __strtod_internal (__const char *__restrict __nptr,
+				 char **__restrict __endptr, int __group)
+	 __THROW;
+extern float __strtof_internal (__const char *__restrict __nptr,
+				char **__restrict __endptr, int __group)
+	 __THROW;
+extern long double __strtold_internal (__const char *__restrict __nptr,
+					   char **__restrict __endptr,
+					   int __group) __THROW;
+#ifndef __strtol_internal_defined
+extern long int __strtol_internal (__const char *__restrict __nptr,
+				   char **__restrict __endptr,
+				   int __base, int __group) __THROW;
+# define __strtol_internal_defined	1
 #endif
-#undef __Need_M_And_C
+#ifndef __strtoul_internal_defined
+extern unsigned long int __strtoul_internal (__const char *__restrict __nptr,
+						 char **__restrict __endptr,
+						 int __base, int __group) __THROW;
+# define __strtoul_internal_defined	1
+#endif
+#if defined __GNUC__ || defined __USE_ISOC99
+# ifndef __strtoll_internal_defined
+__extension__
+extern long long int __strtoll_internal (__const char *__restrict __nptr,
+					 char **__restrict __endptr,
+					 int __base, int __group) __THROW;
+#  define __strtoll_internal_defined	1
+# endif
+# ifndef __strtoull_internal_defined
+__extension__
+extern unsigned long long int __strtoull_internal (__const char *
+						   __restrict __nptr,
+						   char **__restrict __endptr,
+						   int __base, int __group)
+	 __THROW;
+#  define __strtoull_internal_defined	1
+# endif
+#endif /* GCC */
 
 #endif  /* include/stdlib.h */
