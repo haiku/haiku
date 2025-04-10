d45f85d19fdcc929c6c02e166d67778793a346d0
Augustin Cavalier <waddlesplash@gmail.com>
2025-04-10 09:57:51 -0400

    libroot/musl: Use the correct toread and uflow for shgetc.
    Should fix crashes with Python after the merge of the new wcstol.

--- src/system/libroot/posix/musl/internal/shgetc.c
+++ src/system/libroot/posix/musl/internal/shgetc.c
@@ -1,5 +1,29 @@
 #include "shgetc.h"
 
+#ifdef __HAIKU__
+#define __toread toread
+static int __toread(FILE *f)
+{
+	f->mode |= f->mode-1;
+	if (f->wpos != f->wbase) f->write(f, 0, 0);
+	f->wpos = f->wbase = f->wend = 0;
+	if (f->flags & F_NORD) {
+		f->flags |= F_ERR;
+		return EOF;
+	}
+	f->rpos = f->rend = f->buf + f->buf_size;
+	return (f->flags & F_EOF) ? EOF : 0;
+}
+
+#define __uflow uflow
+static int __uflow(FILE *f)
+{
+	unsigned char c;
+	if (!__toread(f) && f->read(f, &c, 1)==1) return c;
+	return EOF;
+}
+#endif
+
 /* The shcnt field stores the number of bytes read so far, offset by
  * the value of buf-rpos at the last function call (__shlim or __shgetc),
  * so that between calls the inline shcnt macro can add rpos-buf to get
