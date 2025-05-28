5cfaa09f1f8d2cd4b67a6f50e01c16fe5d2f266d
Augustin Cavalier <waddlesplash@gmail.com>
2025-05-27 22:29:49 -0400

    iprowifi3945: Replace with the OpenBSD driver.
    According to the comments on #19588, this seems to be more reliable than
    the driver variants from FreeBSD or elsewhere.
    
    Change-Id: Icdc24683de3f588f51c4c08529aa6cab3a09c7fd
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/9311
    Reviewed-by: waddlesplash <waddlesplash@gmail.com>
    Tested-by: Commit checker robot <no-reply+buildbot@haiku-os.org>

--- src/add-ons/kernel/drivers/network/wlan/iprowifi3945/glue.c
+++ src/add-ons/kernel/drivers/network/wlan/iprowifi3945/glue.c
@@ -1,57 +1,24 @@
 /*
- * Copyright 2009, Colin Günther, coling@gmx.de.
- * Copyright 2018, Haiku, Inc.
- * All rights reserved. Distributed under the terms of the MIT license.
+ * Copyright 2025, Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
  */
 
-
-#include <sys/bus.h>
-#include <sys/kernel.h>
-
-#include <machine/bus.h>
-
-#include <net/if.h>
-#include <net/if_media.h>
-
-#include <net80211/ieee80211_var.h>
-#include <net80211/ieee80211_ratectl.h>
-
-#include <dev/wpi/if_wpireg.h>
-#include <dev/wpi/if_wpivar.h>
+#include <sys/haiku-module.h>
 
 
 HAIKU_FBSD_WLAN_DRIVER_GLUE(iprowifi3945, wpi, pci)
 NO_HAIKU_FBSD_MII_DRIVER();
 NO_HAIKU_REENABLE_INTERRUPTS();
-HAIKU_DRIVER_REQUIREMENTS(FBSD_WLAN);
-HAIKU_FIRMWARE_VERSION(2144);
+HAIKU_DRIVER_REQUIREMENTS(OBSD_WLAN);
+HAIKU_FIRMWARE_VERSION(1);
 HAIKU_FIRMWARE_NAME_MAP({
-	{"wpifw", "iwlwifi-3945-15.ucode"}
+	{"wpi-3945abg", "iwlwifi-3945-15.ucode"}
 });
 
 
 int
 HAIKU_CHECK_DISABLE_INTERRUPTS(device_t dev)
 {
-	struct wpi_softc* sc = (struct wpi_softc*)device_get_softc(dev);
-	uint32 r1, r2;
-
-	r1 = WPI_READ(sc, WPI_INT);
-
-	if (__predict_false(r1 == 0xffffffff ||
-			(r1 & 0xfffffff0) == 0xa5a5a5a0))
-		return 0;	/* Hardware gone! */
-
-	r2 = WPI_READ(sc, WPI_FH_INT);
-
-	if (r1 == 0 && r2 == 0)
-		return 0;	/* Interrupt not for us. */
-
-	/* Disable interrupts. */
-	WPI_WRITE(sc, WPI_INT_MASK, 0);
-
-	atomic_set((int32*)&sc->sc_intr_status_1, r1);
-	atomic_set((int32*)&sc->sc_intr_status_2, r2);
-
+	// We only support MSI, so we handle all interrupts.
 	return 1;
 }
