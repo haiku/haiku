7497dbc7ed75c8d399b74ec16896adab5bdf59f6
David  Ochoa Corrales <david.ochoa.corrales@gmail.com>
2025-07-07 10:55:27 -0700

    Documentation - FileMapDisk.cpp

--- src/system/boot/loader/FileMapDisk.cpp
+++ src/system/boot/loader/FileMapDisk.cpp
@@ -3,185 +3,175 @@
  * All rights reserved. Distributed under the terms of the MIT License.
  */
 
-
+// Include necessary headers for the FileMapDisk class
 #include <boot/FileMapDisk.h>
 #include <boot_item.h>
-
 #include <new>
-
 #include <endian.h>
 #include <stdio.h>
 #include <string.h>
-
 #include <OS.h>
 #include <SupportDefs.h>
 
-
+// Uncomment to enable tracing for debugging purposes
 //#define TRACE_FILEMAPDISK
+
+// Define a macro for tracing, which can be enabled or disabled
 #ifdef TRACE_FILEMAPDISK
 #	define TRACE(x) dprintf x
 #else
 #	define TRACE(x) ;
 #endif
 
-
+// Use the nothrow version of new to avoid exceptions on memory allocation failures
 using std::nothrow;
 
-
-// constructor
+// Constructor for the FileMapDisk class
 FileMapDisk::FileMapDisk()
 	:
-	fNode(NULL)
+	fNode(NULL) // Initialize the node pointer to NULL
 {
 }
 
-// destructor
+// Destructor for the FileMapDisk class
 FileMapDisk::~FileMapDisk()
 {
 }
 
-// Init
+// Initialize the FileMapDisk with a given node
 status_t
 FileMapDisk::Init(Node *node/*, Partition *partition, FileMap *map, off_t imageSize*/)
 {
-	TRACE(("FileMapDisk::FileMapDisk(%p)\n", node));
-	fNode = node;
+	TRACE(("FileMapDisk::FileMapDisk(%p)\n", node)); // Trace the initialization
+	fNode = node; // Assign the provided node to the member variable
+
 	/*
+	// Commented out code for socket initialization
 	fPartition = partition;
 	fMap = map;
 	fImageSize = imageSize;
-
-	// create and bind socket
+	// Create and bind a UDP socket
 	fSocket = new(nothrow) UDPSocket;
 	if (!fSocket)
-		return B_NO_MEMORY;
-
-	status_t error = fSocket->Bind(INADDR_ANY, 6666);
+		return B_NO_MEMORY; // Return error if socket creation fails
+	status_t error = fSocket->Bind(INADDR_ANY, 6666); // Bind to any address on port 6666
 	if (error != B_OK)
-		return error;
+		return error; // Return error if binding fails
 	*/
 
-	return B_OK;
+	return B_OK; // Return success
 }
 
-
+// Open the file map disk with the given mode
 status_t
 FileMapDisk::Open(void **_cookie, int mode)
 {
-	TRACE(("FileMapDisk::Open(, 0x%08x)\n", mode));
+	TRACE(("FileMapDisk::Open(, 0x%08x)\n", mode)); // Trace the open operation
 	if (fNode == NULL)
-		return B_NO_INIT;
-
-	return fNode->Open(_cookie, mode);
+		return B_NO_INIT; // Return error if not initialized
+	return fNode->Open(_cookie, mode); // Delegate the open operation to the node
 }
 
-
+// Close the file map disk
 status_t
 FileMapDisk::Close(void *cookie)
 {
-	TRACE(("FileMapDisk::Close(%p)\n", cookie));
+	TRACE(("FileMapDisk::Close(%p)\n", cookie)); // Trace the close operation
 	if (fNode == NULL)
-		return B_NO_INIT;
-
-	return fNode->Close(cookie);
+		return B_NO_INIT; // Return error if not initialized
+	return fNode->Close(cookie); // Delegate the close operation to the node
 }
 
-
-// ReadAt
+// Read data from the file map disk at a specific position
 ssize_t
-FileMapDisk::ReadAt(void *cookie, off_t pos, void *_buffer,
-	size_t bufferSize)
+FileMapDisk::ReadAt(void *cookie, off_t pos, void *_buffer, size_t bufferSize)
 {
-	TRACE(("FileMapDisk::ReadAt(%p, %lld, , %ld)\n", cookie, pos, bufferSize));
+	TRACE(("FileMapDisk::ReadAt(%p, %lld, , %ld)\n", cookie, pos, bufferSize)); // Trace the read operation
 	if (fNode == NULL)
-		return B_NO_INIT;
-
-	return fNode->ReadAt(cookie, pos, _buffer, bufferSize);
+		return B_NO_INIT; // Return error if not initialized
+	return fNode->ReadAt(cookie, pos, _buffer, bufferSize); // Delegate the read operation to the node
 }
 
-
-// WriteAt
+// Write data to the file map disk at a specific position
 ssize_t
-FileMapDisk::WriteAt(void */*cookie*/, off_t pos, const void *buffer,
-	size_t bufferSize)
+FileMapDisk::WriteAt(void */*cookie*/, off_t pos, const void *buffer, size_t bufferSize)
 {
-	// Not needed in the boot loader.
+	// Not needed in the boot loader, so return permission denied
 	return B_PERMISSION_DENIED;
 }
 
-// GetName
+// Get the name of the file map disk
 status_t
 FileMapDisk::GetName(char *nameBuffer, size_t bufferSize) const
 {
-	const char *prefix = "FileMapDisk:";
+	const char *prefix = "FileMapDisk:"; // Prefix for the name
 	if (nameBuffer == NULL)
-		return B_BAD_VALUE;
-
-	strlcpy(nameBuffer, prefix, bufferSize);
+		return B_BAD_VALUE; // Return error if buffer is NULL
+	strlcpy(nameBuffer, prefix, bufferSize); // Copy the prefix to the buffer
 	if (bufferSize > strlen(prefix) && fNode)
-		return fNode->GetName(nameBuffer + strlen(prefix),
-			bufferSize - strlen(prefix));
-
-	return B_OK;
+		return fNode->GetName(nameBuffer + strlen(prefix), bufferSize - strlen(prefix)); // Get the rest of the name from the node
+	return B_OK; // Return success
 }
 
-
+// Get the file map for the disk
 status_t
 FileMapDisk::GetFileMap(struct file_map_run *runs, int32 *count)
 {
-	return fNode->GetFileMap(runs, count);
+	return fNode->GetFileMap(runs, count); // Delegate the operation to the node
 }
 
-
+// Get the size of the file map disk
 off_t
 FileMapDisk::Size() const
 {
 	if (fNode == NULL)
-		return B_NO_INIT;
-	return fNode->Size();
+		return B_NO_INIT; // Return error if not initialized
+	return fNode->Size(); // Delegate the operation to the node
 }
 
-
+// Find any file map disk in the given volume
 FileMapDisk *
 FileMapDisk::FindAnyFileMapDisk(Directory *volume)
 {
-	TRACE(("FileMapDisk::FindAnyFileMapDisk(%p)\n", volume));
+	TRACE(("FileMapDisk::FindAnyFileMapDisk(%p)\n", volume)); // Trace the operation
 	Node *node;
 	status_t error;
-
 	if (volume == NULL)
-		return NULL;
+		return NULL; // Return NULL if volume is NULL
 
-	//XXX: check lower/mixed case as well
+	// Lookup the file map folder in the volume
+	// XXX: check lower/mixed case as well
 	Node *dirnode;
 	Directory *dir;
 	dirnode = volume->Lookup(FMAP_FOLDER_NAME, true);
 	if (dirnode == NULL || !S_ISDIR(dirnode->Type()))
-		return NULL;
+		return NULL; // Return NULL if the folder is not found or not a directory
+
 	dir = (Directory *)dirnode;
+	// Lookup the file map image in the directory
 	node = dir->Lookup(FMAP_IMAGE_NAME, true);
 	if (node == NULL)
-		return NULL;
+		return NULL; // Return NULL if the image is not found
 
-	// create a FileMapDisk object
+	// Create a FileMapDisk object
 	FileMapDisk *disk = new(nothrow) FileMapDisk;
 	if (disk != NULL) {
-		error = disk->Init(node);
+		error = disk->Init(node); // Initialize the disk with the node
 		if (error != B_OK) {
-			delete disk;
+			delete disk; // Delete the disk if initialization fails
 			disk = NULL;
 		}
 	}
-
-	return disk;
+	return disk; // Return the disk
 }
 
-
+// Register the file map boot item
 status_t
 FileMapDisk::RegisterFileMapBootItem()
 {
-	return B_ERROR;
+	return B_ERROR; // Return error
 #if 0
+	// Commented out code for registering the boot item
 	struct file_map_boot_item *item;
 	item = (struct file_map_boot_item *)malloc(sizeof(struct file_map_boot_item));
 	item->num_runs = FMAP_MAX_RUNS;
@@ -189,8 +179,9 @@ FileMapDisk::RegisterFileMapBootItem()
 	err = GetFileMap(item->runs, &item->num_runs);
 	if (err < B_OK)
 		return err;
-//	err = add_boot_item("file_map_disk", item, sizeof(struct file_map_boot_item));
+	// err = add_boot_item("file_map_disk", item, sizeof(struct file_map_boot_item));
 	err = B_ERROR;
 	return err;
 #endif
 }
+
