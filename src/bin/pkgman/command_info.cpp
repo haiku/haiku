35a3d6b739b9123f0cd37996b504a778a57fc21a
Oscar Lesta <oscar.lesta@gmail.com>
2025-08-11 16:27:30 -0300

    pkgman: add '--show <field>' to the 'info' command.
    This allows getting the requires and provides for uninstalled packages,
    in a format easy to parse for scripts.
    
    Change-Id: I11a138540f820a0ecca3dad2238085fdfb9d51d2
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/9589
    Reviewed-by: Adrien Destugues <pulkomandy@pulkomandy.tk>

--- src/bin/pkgman/command_info.cpp
+++ src/bin/pkgman/command_info.cpp
@@ -1,10 +1,11 @@
 /*
- * Copyright 2013-2023, Haiku, Inc. All Rights Reserved.
+ * Copyright 2013-2025, Haiku, Inc. All Rights Reserved.
  * Distributed under the terms of the MIT License.
  *
  * Authors:
  *		Ingo Weinhold <ingo_weinhold@gmx.de>
  *		Humdinger <humdingerb@gmail.com>
+ *		Adrien Destugues <pulkomandy@pulkomandy.tk>
  */
 
 
@@ -27,15 +28,28 @@ using namespace BPackageKit::BPrivate;
 
 
 static const char* const kShortUsage =
-	"  %command% <package>\n"
-	"    Shows summary and description of the specified package.\n";
+	"  %command% [--show <field>] [-v] <package>\n"
+	"    Shows information for the specified package.\n";
 
 static const char* const kLongUsage =
-	"Usage: %program% %command% <package>\n"
-	"Shows summary and description of the specified package.\n"
+	"Usage: %program% %command% [--show <field>] [-v] <package>\n"
+	"\n"
+	"Shows information for the specified package.\n"
+	"\n"
 	"The <package> argument is the name by which the package\n"
 	"is looked up in a remote repository.\n"
-	"\n";
+	"\n"
+	"Options:\n"
+	"    -s, --show field\n"
+	"        List a specific metadata field from the given <package>.\n"
+	"        The available fields are:\n"
+	"        * header: package name and description (this is the default)\n"
+	"        * provides: entities provided by the given package\n"
+	"        * requires: runtime dependencies of the given package\n"
+	"    -v, --versions\n"
+	"        Show version details for the provided/required entities.\n"
+	"        Otherwise only their names are printed.\n"
+	;
 
 
 DEFINE_COMMAND(InfoCommand, "info", kShortUsage, kLongUsage,
@@ -45,15 +59,20 @@ DEFINE_COMMAND(InfoCommand, "info", kShortUsage, kLongUsage,
 int
 InfoCommand::Execute(int argc, const char* const* argv)
 {
+	bool versions = false;
+	BString field = "header";
+
 	while (true) {
 		static struct option sLongOptions[] = {
 			{ "debug", required_argument, 0, OPTION_DEBUG },
 			{ "help", no_argument, 0, 'h' },
+			{ "show", required_argument, 0, 's' },
+			{ "versions", no_argument, 0, 'v' },
 			{ 0, 0, 0, 0 }
 		};
 
 		opterr = 0; // don't print errors
-		int c = getopt_long(argc, (char**)argv, "h", sLongOptions, NULL);
+		int c = getopt_long(argc, (char**)argv, "hs:v", sLongOptions, NULL);
 
 		if (c == -1)
 			break;
@@ -66,6 +85,14 @@ InfoCommand::Execute(int argc, const char* const* argv)
 				PrintUsageAndExit(false);
 				break;
 
+			case 's':
+				field = optarg;
+				break;
+
+			case 'v':
+				versions = true;
+				break;
+
 			default:
 				PrintUsageAndExit(true);
 				break;
@@ -75,6 +102,9 @@ InfoCommand::Execute(int argc, const char* const* argv)
 	// The remaining argument, if any, is the package.
 	const char* packageName = argv[optind++];
 	if (packageName == NULL)
+		PrintUsageAndExit(true);
+
+	if ((field != "header") && (field != "provides") && (field != "requires"))
 		PrintUsageAndExit(true);
 
 	// create the solver
@@ -98,17 +128,49 @@ InfoCommand::Execute(int argc, const char* const* argv)
 		return 0;
 	}
 
-	// print out summary and description of first exactly matching package
+	// select first exact match by package name
 	int32 packageCount = packages.CountItems();
+	BSolverPackage* package = NULL;
 	for (int32 i = 0; i < packageCount; i++) {
-		BSolverPackage* package = packages.ItemAt(i);
-		if (package->Name() == packageName) {
-			BString text("%name%: %summary%\n\n%description%\n");
-			text.ReplaceFirst("%name%", package->Name());
-			text.ReplaceFirst("%summary%", package->Info().Summary());
-			text.ReplaceFirst("%description%", package->Info().Description());
-			printf("%s\n", text.String());
+		if (packages.ItemAt(i)->Name() == packageName) {
+			package = packages.ItemAt(i);
 			break;
+		}
+	}
+
+	if (package == NULL) {
+		fprintf(stderr, "Could not find a package exactly named: \"%s\".\n", packageName);
+		return 1;
+	}
+
+	if (field == "header") {
+		// print out summary and description
+		BString text("%name%: %summary%\n\n%description%\n");
+		text.ReplaceFirst("%name%", package->Name());
+		text.ReplaceFirst("%summary%", package->Info().Summary());
+		text.ReplaceFirst("%description%", package->Info().Description());
+		printf("%s\n", text.String());
+		return 0;
+	}
+
+	if (field == "provides") {
+		BObjectList<BPackageResolvable, true> providesList = package->Info().ProvidesList();
+		for (int32 i = 0; i < providesList.CountItems(); i++) {
+			if (versions)
+				printf("%s\n", providesList.ItemAt(i)->ToString().String());
+			else
+				printf("%s\n", providesList.ItemAt(i)->Name().String());
+		}
+	}
+
+	if (field == "requires") {
+		BObjectList<BPackageResolvableExpression, true> requiresList
+			= package->Info().RequiresList();
+		for (int32 i = 0; i < requiresList.CountItems(); i++) {
+			if (versions)
+				printf("%s\n", requiresList.ItemAt(i)->ToString().String());
+			else
+				printf("%s\n", requiresList.ItemAt(i)->Name().String());
 		}
 	}
 
