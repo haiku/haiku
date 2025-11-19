haiku/haiku

commit fe8f88cff41e731db2bd6958331c6585a5feaa5a

app_server: Rework ClonedAreaMemory to use ServerMemoryAllocator.
This takes advantage of the new reference (use)-counting logic
inside ServerMemoryAllocator to not re-clone areas needlessly.

Fixes #8501.

src/servers/app/ClientMemoryAllocator.cpp
@@ -26,6 +26,7 @@
 #include <Autolock.h>
 
 #include "ServerApp.h"
+#include "ServerMemoryAllocator.h"
 
 
 typedef block_list::Iterator block_iterator;
@@ -364,6 +365,10 @@ ClientMemory::AreaOffset()
 // #pragma mark -
 
 
+static BLocker sLocker("ClonedAreaMemory allocator");
+static BPrivate::ServerMemoryAllocator sClonedAreaMemoryAllocator;
+
+
 ClonedAreaMemory::ClonedAreaMemory()
 	:
 	fClonedArea(-1),
@@ -375,18 +380,18 @@ ClonedAreaMemory::ClonedAreaMemory()
 
 ClonedAreaMemory::~ClonedAreaMemory()
 {
-	if (fClonedArea >= 0)
-		delete_area(fClonedArea);
+	BAutolock locker(sLocker);
+	sClonedAreaMemoryAllocator.RemoveArea(fClonedArea);
 }
 
 
 void*
 ClonedAreaMemory::Clone(area_id area, uint32 offset)
 {
-	fClonedArea = clone_area("server_memory", (void**)&fBase, B_ANY_ADDRESS,
-		B_READ_AREA | B_WRITE_AREA, area);
-	if (fBase == NULL)
+	BAutolock locker(sLocker);
+	if (sClonedAreaMemoryAllocator.AddArea(area, fClonedArea, fBase, 0, false) != B_OK)
 		return NULL;
+
 	fOffset = offset;
 	return Address();
 }

src/servers/app/ClientMemoryAllocator.h
@@ -89,7 +89,7 @@ private:
 
 
 /*! Just clones an existing area. */
-class ClonedAreaMemory  : public AreaMemory{
+class ClonedAreaMemory : public AreaMemory {
 public:
 								ClonedAreaMemory();
 	virtual						~ClonedAreaMemory();

commit d39ce117e02d3d8aac966b1a46bd955a2b59c069

app_server: ServerMemoryAllocator takes the original area as the key.
Should fix memory leaks caused by fe8f88cff41e.

src/servers/app/ClientMemoryAllocator.cpp
@@ -371,6 +371,7 @@ static BPrivate::ServerMemoryAllocator sClonedAreaMemoryAllocator;
 
 ClonedAreaMemory::ClonedAreaMemory()
 	:
+	fArea(-1),
 	fClonedArea(-1),
 	fOffset(0),
 	fBase(NULL)
@@ -381,7 +382,7 @@ ClonedAreaMemory::ClonedAreaMemory()
 ClonedAreaMemory::~ClonedAreaMemory()
 {
 	BAutolock locker(sLocker);
-	sClonedAreaMemoryAllocator.RemoveArea(fClonedArea);
+	sClonedAreaMemoryAllocator.RemoveArea(fArea);
 }
 
 
@@ -392,6 +393,7 @@ ClonedAreaMemory::Clone(area_id area, uint32 offset)
 	if (sClonedAreaMemoryAllocator.AddArea(area, fClonedArea, fBase, 0, false) != B_OK)
 		return NULL;
 
+	fArea = area;
 	fOffset = offset;
 	return Address();
 }

src/servers/app/ClientMemoryAllocator.h
@@ -101,6 +101,7 @@ public:
 	virtual uint32				AreaOffset();
 
 private:
+			area_id		fArea;
 			area_id		fClonedArea;
 			uint32		fOffset;
 			uint8*		fBase;