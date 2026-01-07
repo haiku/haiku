95b41d5986bfd5068c174d8b3b26b49a0237e02e
Augustin Cavalier <waddlesplash@gmail.com>
2026-01-07 16:04:18 -0500

    libroot/posix: Replace strtok_r with musl's.
    And rewrite/cleanup strtok.c also.

--- src/system/libroot/posix/string/strtok.c
+++ src/system/libroot/posix/string/strtok.c
@@ -1,48 +1,14 @@
 /* 
-** Copyright 2001, Travis Geiselbrecht. All rights reserved.
-** Distributed under the terms of the NewOS License.
-*/
+ * Copyright 2026, Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
 
-
-#include <sys/types.h>
 #include <string.h>
 
 
-static char *___strtok = NULL;
-
-
-char *
-strtok_r(char *s, char const *ct, char **save_ptr)
+char*
+strtok(char* str, char const* sep)
 {
-	char *sbegin, *send;
-	
-	if (!s && !save_ptr)
-		return NULL;
-
-	sbegin  = s ? s : *save_ptr;
-	if (!sbegin)
-		return NULL;
-
-	sbegin += strspn(sbegin, ct);
-	if (*sbegin == '\0') {
-		if (save_ptr)
-			*save_ptr = NULL;
-		return NULL;
-	}
-
-	send = strpbrk(sbegin, ct);
-	if (send && *send != '\0')
-		*send++ = '\0';
-	if (save_ptr)
-		*save_ptr = send;
-
-	return sbegin;
+	static char *p = NULL;
+	return strtok_r(str, sep, &p);
 }
-
-
-char *
-strtok(char *s, char const *ct)
-{
-	return strtok_r(s, ct, &___strtok);
-}
-
