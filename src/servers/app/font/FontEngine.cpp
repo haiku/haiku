1f3bc6f056475acd2fb0808028ae28e162556e01
Máximo Castañeda <antiswen@yahoo.es>
2026-01-31 14:38:21 +0100

    app_server: fix build for older FreeType
    5c254dff8c updated FreeType build packages for x86_64 and x86_gcc2,
    including a change in app_server to adapt to a different pointer type
    in a FT_Outline member. Other arches are still using older versions
    of FreeType, though, so they need the older type.
    
    Change-Id: I4ad934821a338e93e3827069618948e77eae66e1
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/10306
    Tested-by: Commit checker robot <no-reply+buildbot@haiku-os.org>
    Reviewed-by: Máximo Castañeda <antiswen@yahoo.es>

--- src/servers/app/font/FontEngine.cpp
+++ src/servers/app/font/FontEngine.cpp
@@ -69,7 +69,12 @@ decompose_ft_outline(const FT_Outline& outline, bool flip_y, PathStorage& path)
 
 	FT_Vector* point;
 	FT_Vector* limit;
+#if FREETYPE_MAJOR > 2 || (FREETYPE_MAJOR == 2 && (FREETYPE_MINOR > 13 \
+			|| (FREETYPE_MINOR == 13 && FREETYPE_PATCH >= 3)))
 	unsigned char* tags;
+#else
+	char* tags;
+#endif
 
 	int   n;		 // index of contour in outline
 	int   first;	 // index of first point in contour
