3c28d298f7848c94104b80fadf5352ba46c2de0c
Augustin Cavalier <waddlesplash@gmail.com>
2026-03-10 13:10:07 -0400

    kernel: Don't fail if the private string.h is included under GCC2.

--- headers/private/kernel/string.h
+++ headers/private/kernel/string.h
@@ -9,10 +9,12 @@
 #include_next <string.h>
 
 
+#if defined(__GNUC__) && __GNUC__ >= 4
 #define memset(DEST, V, LEN)	__builtin_memset((DEST), (V), (LEN))
 #define memcpy(DEST, SRC, LEN)	__builtin_memcpy((DEST), (SRC), (LEN))
 #define memmove(DEST, SRC, LEN)	__builtin_memmove((DEST), (SRC), (LEN))
 #define memcmp(B1, B2, LEN)		__builtin_memcmp((B1), (B2), (LEN))
+#endif
 
 
 #endif	/* KERNEL_STRING_H */
