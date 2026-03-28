634373e466adb6f2045bf4d0ecf7bfe33c997ade
SED4906 <sed4906birdie@gmail.com>
2026-03-28 09:37:16 -0700

    runtime_loader: define MAX_PAGE_SIZE correctly on arm64
    This fixes an issue where map_image returns B_BAD_DATA.
    
    Change-Id: I11efbb1c1d740474f62c37c0069a76b97aeb8bc4
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/10602
    Reviewed-by: Sam Roberts <sam@smrobtzz.dev>
    Tested-by: Commit checker robot <no-reply+buildbot@haiku-os.org>
    Reviewed-by: waddlesplash <waddlesplash@gmail.com>

--- src/system/runtime_loader/images.cpp
+++ src/system/runtime_loader/images.cpp
@@ -33,7 +33,7 @@
 	// page segment alignment.
 #	define RLD_PROGRAM_BASE	0x600000
 #	define MAX_PAGE_SIZE	0x200000
-#elif __arm__
+#elif defined(__arm__) || defined(__aarch64__)
 #	define RLD_PROGRAM_BASE	0x200000
 #	define MAX_PAGE_SIZE	0x10000
 #else
