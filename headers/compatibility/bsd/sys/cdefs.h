6e43f9a34a149c1e516db107a711936fcb7cb709
Augustin Cavalier <waddlesplash@gmail.com>
2026-06-03 17:10:42 -0400

    bsd/sys/cdefs.h: Additions for NVMM.

--- headers/compatibility/bsd/sys/cdefs.h
+++ headers/compatibility/bsd/sys/cdefs.h
@@ -14,7 +14,21 @@
 
 
 #define __FBSDID(x)
-#define __unused
+#define	__unused	__attribute__((__unused__))
+#define __packed	__attribute__((packed))
+#define	__aligned(x) __attribute__((__aligned__(x)))
+
+
+#if __GNUC__ > 4
+#define __predict_true(exp)     __builtin_expect((exp), 1)
+#define __predict_false(exp)    __builtin_expect((exp), 0)
+#define	__unreachable()			__builtin_unreachable()
+#else
+#define __predict_true(exp)     (exp)
+#define __predict_false(exp)    (exp)
+#define	__unreachable()			((void)0)
+#endif
+
 
 #define __printflike(a, b)	__attribute__ ((format (__printf__, (a), (b))))
 #define __printf0like(a, b)
