bb98e98305b0bbddeced0c01c9db167a90ab3889
Augustin Cavalier <waddlesplash@gmail.com>
2026-07-21 21:42:56 -0400

    safemode_defs: Sort and organize.
    No functional change. Reasonably we should have architecture-specific
    headers for some of these, but that can wait.
    (cherry picked from commit 450728b4b717b3ea09fb2145daf2e6a24874c6de)
    
    Change-Id: I887d37a54b44e127e4d1fe46675bf57bc2a392dc
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/11312
    Reviewed-by: waddlesplash <waddlesplash@gmail.com>

--- headers/private/system/safemode_defs.h
+++ headers/private/system/safemode_defs.h
@@ -6,20 +6,21 @@
 #define _SYSTEM_SAFEMODE_DEFS_H
 
 
-#define B_SAFEMODE_DISABLE_USER_ADD_ONS		"disable_user_addons"
-#define B_SAFEMODE_DISABLE_IDE_DMA			"disable_ide_dma"
-#define B_SAFEMODE_DISABLE_IOAPIC			"disable_ioapic"
-#define B_SAFEMODE_DISABLE_ACPI				"disable_acpi"
-#define B_SAFEMODE_DISABLE_APIC				"disable_apic"
-#define B_SAFEMODE_DISABLE_X2APIC			"disable_x2apic"
-#define B_SAFEMODE_DISABLE_SMEP_SMAP		"disable_smep_smap"
-#define B_SAFEMODE_DISABLE_APM				"disable_apm"
-#define B_SAFEMODE_DISABLE_SMP				"disable_smp"
-#define B_SAFEMODE_DISABLE_PAT				"disable_pat"
-#define B_SAFEMODE_DISABLE_HYPER_THREADING	"disable_hyperthreading"
-#define B_SAFEMODE_FAIL_SAFE_VIDEO_MODE		"fail_safe_video_mode"
 #define B_SAFEMODE_4_GB_MEMORY_LIMIT		"4gb_memory_limit"
 #define B_SAFEMODE_256_TB_MEMORY_LIMIT		"256tb_memory_limit"
+#define B_SAFEMODE_DISABLE_USER_ADD_ONS		"disable_user_addons"
+#define B_SAFEMODE_FAIL_SAFE_VIDEO_MODE		"fail_safe_video_mode"
+
+#define B_SAFEMODE_DISABLE_ACPI				"disable_acpi"
+#define B_SAFEMODE_DISABLE_APIC				"disable_apic"
+#define B_SAFEMODE_DISABLE_APM				"disable_apm"
+#define B_SAFEMODE_DISABLE_HYPER_THREADING	"disable_hyperthreading"
+#define B_SAFEMODE_DISABLE_IDE_DMA			"disable_ide_dma"
+#define B_SAFEMODE_DISABLE_IOAPIC			"disable_ioapic"
+#define B_SAFEMODE_DISABLE_PAT				"disable_pat"
+#define B_SAFEMODE_DISABLE_SMEP_SMAP		"disable_smep_smap"
+#define B_SAFEMODE_DISABLE_SMP				"disable_smp"
+#define B_SAFEMODE_DISABLE_X2APIC			"disable_x2apic"
 
 
 #endif	/* _SYSTEM_SAFEMODE_DEFS_H */
