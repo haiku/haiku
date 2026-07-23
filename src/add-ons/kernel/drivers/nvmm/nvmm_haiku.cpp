6f168cbe77c0d211bea884c8980da086c09a2c76
Augustin Cavalier <waddlesplash@gmail.com>
2026-07-23 13:04:47 -0400

    nvmm: Don't ever use B_FULL_LOCK.
    vm_map_cache doesn't handle mapping pages, and it seems we don't need
    to implement that here.
    
    Change-Id: Ib8ff3e181c066275e2b55093485d44788632a110
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/11339
    Reviewed-by: waddlesplash <waddlesplash@gmail.com>

--- src/add-ons/kernel/drivers/nvmm/nvmm_haiku.cpp
+++ src/add-ons/kernel/drivers/nvmm/nvmm_haiku.cpp
@@ -582,12 +582,8 @@ os_vmobj_map(os_vmmap_t *map, vaddr_t *addr, vsize_t size, os_vmobj_t *vmobj,
 		return status;
 
 	uint32 wiring = B_LAZY_LOCK;
-	if (!wired && dynamic_cast<VMAnonymousCache*>(vmobj->cache) != NULL) {
+	if (!wired && dynamic_cast<VMAnonymousCache*>(vmobj->cache) != NULL)
 		wiring = B_NO_LOCK;
-	} else if (vmobj->cache->page_count == ((vmobj->cache->virtual_end
-			- vmobj->cache->virtual_base + B_PAGE_SIZE - 1) / B_PAGE_SIZE)) {
-		wiring = B_FULL_LOCK;
-	}
 
 	uint32 flags = fixed ? CREATE_AREA_UNMAP_ADDRESS_RANGE : 0;
 	bool kernel = false;
