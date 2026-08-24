35977f1bce20068f7173fc8c60f2bceee07301be
Przemysław Buczkowski <prem@prem.moe>
2026-08-24 17:26:45 +0100

    arm64: harden system_time()
    * fixed integer overflow, causing a crash after 5 hours runtime
    * add a synchronization barrier to prevent system_time() from going
      backwards
    * use virtual instead of physical timer, for consistency with the kernel
    
    Change-Id: I2cecc466befad65329fbfda2e01a56d4e4190181
    Reviewed-on: https://review.haiku-os.org/c/haiku/+/11631
    Reviewed-by: waddlesplash <waddlesplash@gmail.com>

--- src/system/libroot/os/arch/arm64/system_time.c
+++ src/system/libroot/os/arch/arm64/system_time.c
@@ -1,12 +1,11 @@
 /*
- * Copyright 2019 Haiku, Inc. All Rights Reserved.
+ * Copyright 2019-2026 Haiku, Inc. All Rights Reserved.
  * Distributed under the terms of the MIT License.
  */
 #include <OS.h>
 
 #include <arch_cpu.h>
 #include <libroot_private.h>
-#include <real_time_data.h>
 
 
 bigtime_t
@@ -14,7 +13,10 @@ system_time(void)
 {
 	uint64 ticks;
 	uint64 freq;
-	asm volatile("mrs %0, CNTPCT_EL0": "=r" (ticks));
+	// Prevent reordering which could cause time to jump back.
+	arm64_isb();
+	asm volatile("mrs %0, CNTVCT_EL0" : "=r"(ticks));
 	asm volatile("mrs %0, CNTFRQ_EL0": "=r" (freq));
-	return (ticks * 1000000) / freq;
+
+	return (ticks / freq) * 1000000 + (ticks % freq) * 1000000 / freq;
 }
